199a681306cb05d95865ff075d139061277ee8c8
GitJournal <app@gitjournal.io>
2022-07-24 12:16:43 +0800

    [2022-07-24 04:15:28]
    FILE:
    tests/music_analysis/download_exciting_bgm.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/music_analysis/download_exciting_bgm.py
+++ tests/music_analysis/download_exciting_bgm.py
@@ -1,7 +1,11 @@
 download_path = "exciting_bgm.mp3" # is the extension right?
 
-import requests
-
-baseUrl = "http://localhost:4000/"
+import reque
+baseUrl = "http://localhost:4000"
 
 keyword = "last friday night" # american pop music?
+
+search_result = requests.get(baseUrl+"/cloudsearch", params={"keyword": keyword})
+
+print(search_result.content)
+breakpoint()
