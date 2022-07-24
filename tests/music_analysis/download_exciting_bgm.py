a62d3e9e07abc299071485f94f3269b6c07707eb
GitJournal <app@gitjournal.io>
2022-07-24 13:59:09 +0800

    [2022-07-24 05:58:05]
    FILE:
    tests/music_analysis/download_exciting_bgm.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/music_analysis/download_exciting_bgm.py
+++ tests/music_analysis/download_exciting_bgm.py
@@ -5,13 +5,17 @@ baseUrl = "http://localhost:4000"
 
 keywords = "last friday night" # american pop music?
 
-login_status = requests.get(baseUrl+"/login/status")
-login_status_json = login_status.json()
-print(login_status_json)
 # {'data': {'code': 200, 'account': {'id': 7935782775, 'userName': '0_fxg_pxw@163.com', 'type': 0, 'status': -10, 'whitelistAuthority': 0, 'createTime': 1657240405751, 'tokenVersion': 0, 'ban': 0, 'baoyueVersion': 0, 'donateVersion': 0, 'vipType': 0, 'anonimousUser': False, 'paidFee': False}, 'profile': None}}
 # breakpoint()
 phone, password = "19825089619","dbH361210110"
 login_response = requests.get(baseUrl+"/login/cellphone",params={"phone": phone,"password": password})
+
+
+login_status = requests.get(baseUrl+"/login/status")
+login_status_json = login_status.json()
+print(login_status_json)
+
+breakpoint()
 
 search_result = requests.get(baseUrl+"/cloudsearch", params={"keywords": keywords})
 
