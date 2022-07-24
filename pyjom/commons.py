67b5f3a9ab4362f7bec3d9881d786ae74153e0ef
GitJournal <app@gitjournal.io>
2022-07-24 22:05:47 +0800

    [2022-07-24 14:04:18]
    FILE:
    pyjom/commons.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/commons.py
+++ pyjom/commons.py
@@ -197,6 +197,7 @@ def append_sublist(main_dict,sublist_key,item):
     main_dict[sublist_key] = main_dict.get(sublist_key,[])+[item]
 
 def update_subdict(mdict,key,subdict):
+    print("UPDATING SUBDICT", mdict,key, subdict)
     if key not in mdict: mdict[key]=subdict
     else: mdict[key].update(subdict)
     return mdict
