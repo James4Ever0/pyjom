07d24286f01d92d72a3fa5f7a2f44ad9be56faa0
GitJournal <app@gitjournal.io>
2022-07-24 12:21:26 +0800

    [2022-07-24 04:20:11]
    FILE:
    tests/music_analysis/download_exciting_bgm.py
    tests/music_analysis/search_result.json
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:
    /media/root/help/pyjom/tests/music_analysis/search_result.json

--- tests/music_analysis/download_exciting_bgm.py
+++ tests/music_analysis/download_exciting_bgm.py
@@ -3,9 +3,9 @@ download_path = "exciting_bgm.mp3" # is the extension right?
 import requests
 baseUrl = "http://localhost:4000"
 
-keyword = "last friday night" # american pop music?
+keywords = "last friday night" # american pop music?
 
-search_result = requests.get(baseUrl+"/cloudsearch", params={"keyword": keyword})
+search_result = requests.get(baseUrl+"/cloudsearch", params={"keywords": keywords})
 
-print(search_result.content)
+print(search_result.json())
 breakpoint()
