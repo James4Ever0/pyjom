06700f4127686326cd0a95e735ad03b8cf1e9c50
GitJournal <app@gitjournal.io>
2022-07-24 12:19:18 +0800

    [2022-07-24 04:18:02]
    FILE:
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/music_analysis/download_exciting_bgm.py
+++ tests/music_analysis/download_exciting_bgm.py
@@ -1,6 +1,6 @@
 download_path = "exciting_bgm.mp3" # is the extension right?
 
-import reque
+import requests
 baseUrl = "http://localhost:4000"
 
 keyword = "last friday night" # american pop music?
