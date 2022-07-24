e3cedf3a05d1dc60c69cca3beb8b24c4b6d66bb1
GitJournal <app@gitjournal.io>
2022-07-24 14:01:07 +0800

    [2022-07-24 06:00:00]
    FILE:
    tests/music_analysis/download_exciting_bgm.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/music_analysis/download_exciting_bgm.py
+++ tests/music_analysis/download_exciting_bgm.py
@@ -7,9 +7,12 @@ keywords = "last friday night" # american pop music?
 
 # {'data': {'code': 200, 'account': {'id': 7935782775, 'userName': '0_fxg_pxw@163.com', 'type': 0, 'status': -10, 'whitelistAuthority': 0, 'createTime': 1657240405751, 'tokenVersion': 0, 'ban': 0, 'baoyueVersion': 0, 'donateVersion': 0, 'vipType': 0, 'anonimousUser': False, 'paidFee': False}, 'profile': None}}
 # breakpoint()
-phone, password = "19825089619","dbH361210110"
-login_response = requests.get(baseUrl+"/login/cellphone",params={"phone": phone,"password": password})
-
+# phone, password = "19825089619","dbH361210110"
+# login_response = requests.get(baseUrl+"/login/cellphone",params={"phone": phone,"password": password})
+# login_response_json = login_response.json()
+# # {'code': -460, 'message': '网络太拥挤，请稍候再试！'}
+# # what the fuck is this shit?
+# print(login_response_json)
 
 login_status = requests.get(baseUrl+"/login/status")
 login_status_json = login_status.json()
