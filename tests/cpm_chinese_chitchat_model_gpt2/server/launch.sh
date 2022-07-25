1319b316b7f7108ee27677e73a9c6c78ddc08eed
GitJournal <app@gitjournal.io>
2022-07-25 16:57:05 +0800

    [2022-07-25 08:55:58]
    FILE:
    tests/cpm_chinese_chitchat_model_gpt2/server/launch.sh
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/cpm_chinese_chitchat_model_gpt2/server/launch.sh
+++ tests/cpm_chinese_chitchat_model_gpt2/server/launch.sh
@@ -1,4 +1,5 @@
 cd ../GPT2-chitchat
 # there is no argparse support.
 # python3 -m uvicorn interact_fastapi:app --reload --port 8932 
-python3 interact_fastapi.py --no_cuda --model_path ../model
+python3 interact_fastapi.py --no_cuda --model_path ../model # use cuda somehow, checking if that saves my cpu?
+#python3 interact_fastapi.py --no_cuda --model_path ../model
