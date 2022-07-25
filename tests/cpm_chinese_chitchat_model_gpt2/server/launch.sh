6abfedfb9b460c5de1cac4a050c04721268de791
GitJournal <app@gitjournal.io>
2022-07-25 16:59:04 +0800

    [2022-07-25 08:57:56]
    FILE:
    tests/cpm_chinese_chitchat_model_gpt2/server/launch.sh
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/cpm_chinese_chitchat_model_gpt2/server/launch.sh
+++ tests/cpm_chinese_chitchat_model_gpt2/server/launch.sh
@@ -1,5 +1,7 @@
 cd ../GPT2-chitchat
 # there is no argparse support.
 # python3 -m uvicorn interact_fastapi:app --reload --port 8932 
-python3 interact_fastapi.py --no_cuda --model_path ../model # use cuda somehow, checking if that saves my cpu?
+python3 interact_fastapi.py --model_path ../model # use cuda somehow, checking if that saves my cpu?
+# but that will not work for yolov5 detection.
+# anyway i am trying to be economic.
 #python3 interact_fastapi.py --no_cuda --model_path ../model
