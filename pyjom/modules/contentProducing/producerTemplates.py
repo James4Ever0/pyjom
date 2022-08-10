06f0d1a38b36a2ff3463f64b03dc85300ec6187b
GitJournal <app@gitjournal.io>
2022-08-10 22:33:58 +0800

    [2022-08-10 14:31:50]
    FILE:
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/contentProducing/producerTemplates.py
+++ pyjom/modules/contentProducing/producerTemplates.py
@@ -272,7 +272,7 @@ def getRenderList(total_cuts, demanded_cut_spans):
     return render_list
 
 def renderList2MediaLang(renderList, slient=True, bgm=None): # this is just a primitive. need to improve in many ways.
-    scriptBase = Medialang()
+    # maybe we should not use this too often.
 
 def petsWithMusicProducer(filtered_info, meta_info, config={}):
     # what is this config? how the fuck we can arrange it?
