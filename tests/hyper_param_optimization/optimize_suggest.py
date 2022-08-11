d247df1f24639c5d018e761db1bd6c28d5a5ac29
jamesbrown <randomvoidmail@foxmail.com>
2022-08-12 01:51:02 +0800

    [2022-08-11 17:49:01]
    FILE:
    tests/hyper_param_optimization/optimize_suggest.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/hyper_param_optimization/optimize_suggest.py
+++ tests/hyper_param_optimization/optimize_suggest.py
@@ -1,8 +1,15 @@
-from hyperopt import tpe, fmin, hp
-def function (x):
+from hyperopt import tpe, fmin, hp, STATUS_OK, STATUS_FAIL
+import requests
+def function(x):
     print("calculating function...",x)
-    result = x**2
-space = hp.uniform(0,2)
-result = fmin(function, space=space, algo=tpe.suggest, max_evals=100)
+    # result = x**2
+    status = STATUS_FAIL
+    try:
+        r = requests.get('https://www.baidu.com/', timeout=x)
+        if r.status_code == 200:
+            status = STATUS_OK
+    return {"loss":x, "status":status}
+space = hp.uniform("param",1,2)
+result = fmin(fn=function, space=space, algo=tpe.suggest, max_evals=100)
 
 print(result)
