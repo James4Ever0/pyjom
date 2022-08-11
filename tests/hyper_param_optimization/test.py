f11ee6136c935a629dbf2ffec151c2ecd99e24ad
jamesbrown <randomvoidmail@foxmail.com>
2022-08-11 21:40:03 +0800

    [2022-08-11 13:39:25]
    FILE:
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/hyper_param_optimization/test.py
+++ tests/hyper_param_optimization/test.py
@@ -3,4 +3,7 @@
 
 from hyperopt import hp
 
-space = hp.choice('a',[('case 1',1+hp.lognormal('c1',0,1)), ('case 2', hp.uniform('c2',-10,10))])
+space = hp.choice(
+    "a",
+    [("case 1", 1 + hp.lognormal("c1", 0, 1)), ("case 2", hp.uniform("c2", -10, 10))],
+)
