07148185cc93180c8b2b71f57262a5c73c7cdd04
jamesbrown <randomvoidmail@foxmail.com>
2022-08-12 01:46:56 +0800

    [2022-08-11 17:44:56]
    FILE:
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/hyper_param_optimization/optimize_suggest.py
+++ tests/hyper_param_optimization/optimize_suggest.py
@@ -1,6 +1,6 @@
 from hyperopt import tpe, fmin, hp
 def function (x):
-    print("")
+    print("calculating function...",x)
     result = x**2
 space = hp.uniform(0,2)
 result = fmin(function, space=space, algo=tpe.suggest, max_evals=100)
