1abd6f1ffb1e0c1bf8e7c3da2fc98f2d79905529
jamesbrown <randomvoidmail@foxmail.com>
2022-08-11 23:36:56 +0800

    [2022-08-11 15:36:54]
    FILE:
    tests/hyper_param_optimization/test.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/hyper_param_optimization/test.py
+++ tests/hyper_param_optimization/test.py
@@ -7,12 +7,16 @@ from hyperopt import hp
 #     "a",
 #     [("case 1", 1 + hp.lognormal("c1", 0, 1)), ("case 2", hp.uniform("c2", -10, 10))],
 # )
-space = hp.choice("lambda",[lambda :1, lambda:2]) # if it is lambda, function will not resolve. however, after passing this thing into the main criterion function, it will utilize the lambda function.
-
 import hyperopt.pyll.stochastic as stochastic
-from hyperopt.pyll import scope
+
+space = hp.choice("lambda",[lambda :1, lambda:2]) # if it is lambda, function will not resolve. however, after passing this thing into the main criterion function, it will utilize the lambda function.
 
 for _ in range(10):
     sample = stochastic.sample(space)
     print("SAMPLE:", sample) # this will return the tuple. can we put some custom functions here?
     # there must be some integrations with custom functions. for example: scikit-learn
+
+print("_______________________________") # splited.
+
+from hyperopt.pyll import scope
+
