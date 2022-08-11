0f2abb0637377a46ec3570f20459d519bf8b7d42
jamesbrown <randomvoidmail@foxmail.com>
2022-08-12 01:52:55 +0800

    [2022-08-11 17:52:35]
    FILE:
    tests/hyper_param_optimization/optimize_suggest.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/hyper_param_optimization/optimize_suggest.py
+++ tests/hyper_param_optimization/optimize_suggest.py
@@ -1,15 +1,17 @@
 from hyperopt import tpe, fmin, hp, STATUS_OK, STATUS_FAIL
 import requests
 def function(x):
-    print("calculating function...",x)
+    print("trying timeout:",x)
     # result = x**2
     status = STATUS_FAIL
     try:
         r = requests.get('https://www.baidu.com/', timeout=x)
         if r.status_code == 200:
             status = STATUS_OK
+    except:
+        print("FAILED WITH TIMEOUT:", x)
     return {"loss":x, "status":status}
-space = hp.uniform("param",1,2)
+space = hp.uniform("param",0,2)
 result = fmin(fn=function, space=space, algo=tpe.suggest, max_evals=100)
 
 print(result)
