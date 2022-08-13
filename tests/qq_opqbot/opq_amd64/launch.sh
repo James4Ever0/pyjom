21a695db1f84386ee9938183e0800916cc108392
GitJournal <app@gitjournal.io>
2022-08-13 08:53:41 +0800

    [2022-08-13 00:53:38]
    FILE:
    tests/qq_opqbot/opq_amd64/launch.sh
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/qq_opqbot/opq_amd64/launch.sh
+++ tests/qq_opqbot/opq_amd64/launch.sh
@@ -1,5 +1,5 @@
 ps aux | grep OPQBot | grep -v grep | awk '{print $2}' | xargs -iabc kill -s KILL abc
-./OPQBot
+env http_proxy="" https_proxy="" all_proxy="" ./OPQBot
 # spelling error. just copy and paste ok?
 # @REM click http://localhost:8781/v1/Login/GetQRcode to scan qrcode and login. is it needed to be done every time?
 # @REM click http://localhost:8781/v1/ClusterInfo to check login status.
