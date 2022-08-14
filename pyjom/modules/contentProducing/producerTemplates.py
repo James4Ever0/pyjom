9bae549babf9dc7f082495b55db83183bd058f6d
GitJournal <app@gitjournal.io>
2022-08-14 16:04:41 +0800

    [2022-08-14 08:04:38]
    FILE:
    pyjom/modules/contentProducing/producerTemplates.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/contentProducing/producerTemplates.py
+++ pyjom/modules/contentProducing/producerTemplates.py
@@ -270,8 +270,8 @@ def getRenderList(total_cuts, demanded_cut_spans, noRepeat=True):
                         sameSourceOfLastClip = False
                         if len(usedCuts) > 1:
                             lastClip = usedCuts[-1] # this was wrong. usedCuts could have length == 1
-                            # if filename in lastClip:
-                            #     sameSourceOfLastClip = True # this will detect if the next clip is of the same source of last clip
+                            if filename in lastClip:
+                                sameSourceOfLastClip = True # this will detect if the next clip is of the same source of last clip
                         isRepeat = (cut_str in usedCuts)
                         # isRepeat = (cut_str in usedCuts) or sameSourceOfLastClip
                         if isRepeat: continue # repeated cuts!
