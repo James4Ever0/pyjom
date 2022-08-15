9132cc36e1d7ef2a0da1cbbc27d1ebee26ddb36e
GitJournal <app@gitjournal.io>
2022-08-15 18:12:46 +0800

    [2022-08-15 10:12:44]
    FILE:
    tests/editly_test_video_render_with_bgm/test.sh
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/editly_test_video_render_with_bgm/test.sh
+++ tests/editly_test_video_render_with_bgm/test.sh
@@ -1,3 +1,6 @@
 editly test.json5 --fast # this will suffice. json5 will specify all specs?
+
+# without --keep-source-audio, will we not hear anything from the source video?
+
 # json5: json for humans
 # this much likely to bring python dict and json objects into a single readable format.
