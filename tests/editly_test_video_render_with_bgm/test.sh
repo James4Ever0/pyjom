ef8f79b2b09c520ea36b4ab41cfc36493e26975b
GitJournal <app@gitjournal.io>
2022-08-15 18:57:13 +0800

    [2022-08-15 10:57:11]
    FILE:
    tests/editly_test_video_render_with_bgm/test.sh
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/editly_test_video_render_with_bgm/test.sh
+++ tests/editly_test_video_render_with_bgm/test.sh
@@ -1,6 +1,6 @@
 # run in headless linux machine! test both xvfp specs?
 xvfb-run -s "-ac -screen 0 1280x1024x24" editly test.json5  # this will suffice. json5 will specify all specs?
-# xvfb-run -s "-ac -screen 0 1920x1080x24" editly test.json5 --fast # this will suffice. json5 will specify all specs? this 'fast' setting definitely reduced the output resolution to 334x188 15fps, which just saves my time in final production or remote preview
+# xvfb-run -s "-ac -screen 0 1920x1080x24" editly test.json5 --fast # this will suffice. json5 will specify all specs? this 'fast' setting definitely reduced the output resolution to 334x188 15fps, which just saves my time in final production or remote preview from n2n/frp
 
 # without --keep-source-audio, will we not hear anything from the source video?
 
