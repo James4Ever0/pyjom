3eb3c497d348f9fae89864517d8410ef5bfdd438
GitJournal <app@gitjournal.io>
2022-08-16 00:17:54 +0800

    [2022-08-15 16:17:51]
    FILE:
    pyjom/medialang/processors/dotProcessor/videoProcessor.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/medialang/processors/dotProcessor/videoProcessor.py
+++ pyjom/medialang/processors/dotProcessor/videoProcessor.py
@@ -4,3 +4,6 @@ from pyjom.medialang.commons import *
 def dotVideoProcessor(item, previous):
     # this is initially decided to output mp4, however you might want to decorate it.
     print("_________INSIDE DOT VIDEO PROCESSOR_________")
+    print("ITEM:", item)
+    print("PREVIOUS:", previous)
+    print("_________INSIDE DOT VIDEO PROCESSOR_________")
