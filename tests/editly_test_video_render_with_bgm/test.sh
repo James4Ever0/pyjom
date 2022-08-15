1685e15d355dfea42690175eae3f769bf3ec9f94
GitJournal <app@gitjournal.io>
2022-08-15 18:01:48 +0800

    [2022-08-15 10:01:46]
    FILE:
    tests/editly_test_video_render_with_bgm/test.sh
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/editly_test_video_render_with_bgm/test.sh
+++ tests/editly_test_video_render_with_bgm/test.sh
@@ -1,3 +1,3 @@
-editly --json test.json --width --height --fps 60 -f
+editly --json test.json --width --height --fps 60 -f --out sample.mp4
 # json5: json for humans
 # this much likely to bring python dict and json objects into a single readable format.
