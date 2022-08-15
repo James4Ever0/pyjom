a1031ebd5cb8266694afc82286df52dcabcb5c9f
GitJournal <app@gitjournal.io>
2022-08-15 18:12:19 +0800

    [2022-08-15 10:12:17]
    FILE:
    tests/editly_test_video_render_with_bgm/test.sh
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/editly_test_video_render_with_bgm/test.sh
+++ tests/editly_test_video_render_with_bgm/test.sh
@@ -1,3 +1,3 @@
-editly test.json5 --fast # this will suffice. json will 
+editly test.json5 --fast # this will suffice. json5 will specify all specs?
 # json5: json for humans
 # this much likely to bring python dict and json objects into a single readable format.
