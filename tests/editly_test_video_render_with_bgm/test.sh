2de736f4ff72bcb19f9b004e1050b266ebe837ea
GitJournal <app@gitjournal.io>
2022-08-15 18:52:44 +0800

    [2022-08-15 10:52:41]
    FILE:
    tests/editly_test_video_render_with_bgm/test.sh
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/editly_test_video_render_with_bgm/test.sh
+++ tests/editly_test_video_render_with_bgm/test.sh
@@ -1,3 +1,4 @@
+# run in headless linux machine!
 xvfb-run -s "-ac -screen 0 1280x1024x24" editly test.json5 --fast # this will suffice. json5 will specify all specs?
 
 # without --keep-source-audio, will we not hear anything from the source video?
