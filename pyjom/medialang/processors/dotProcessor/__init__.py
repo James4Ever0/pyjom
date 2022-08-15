4141390e318ebdb7c35dd108375567e6b3910c64
GitJournal <app@gitjournal.io>
2022-08-16 00:15:34 +0800

    [2022-08-15 16:15:30]
    FILE:
    pyjom/medialang/processors/dotProcessor/__init__.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/medialang/processors/dotProcessor/__init__.py
+++ pyjom/medialang/processors/dotProcessor/__init__.py
@@ -1,4 +1,5 @@
 from pyjom.medialang.processors.dotProcessor.jsonProcessor import *
 from pyjom.medialang.processors.dotProcessor.videoProcessor import *
+from pyjom.commons import keywordDecorator
 
 dotProcessors = {".json":dotJsonProcessor, ".mp4": dotVideoProcessor}
