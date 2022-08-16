f9331d4cc3abc7c9d3688e6b736e9e01e89278ff
GitJournal <app@gitjournal.io>
2022-08-16 19:27:18 +0800

    [2022-08-16 11:27:15]
    FILE:
    pyjom/medialang/processors/dotProcessor/jsonProcessor.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/medialang/processors/dotProcessor/jsonProcessor.py
+++ pyjom/medialang/processors/dotProcessor/jsonProcessor.py
@@ -2,7 +2,7 @@ from pyjom.medialang.functions import *
 from pyjom.medialang.commons import *
 
 
-def dotJsonProcessor(item, previous):
+def dotJsonProcessor(item, previous, verbose=True):
     # must contain something.
     args = item.args
     processorName = args["processor"]
