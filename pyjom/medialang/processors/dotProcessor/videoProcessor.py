900d5bb535c7542338d1a40be165a0d0660d89d7
GitJournal <app@gitjournal.io>
2022-08-16 16:26:12 +0800

    [2022-08-16 08:26:09]
    FILE:
    pyjom/medialang/processors/dotProcessor/videoProcessor.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/medialang/processors/dotProcessor/videoProcessor.py
+++ pyjom/medialang/processors/dotProcessor/videoProcessor.py
@@ -2,13 +2,13 @@ from pyjom.medialang.functions import *
 from pyjom.medialang.commons import *
 
 def dotVideoProcessor(item, previous,format="mp4"):
-    print("DOTVIDEO ARGS:", item, previous, format)
+    # print("DOTVIDEO ARGS:", item, previous, format)
     # this item is the video output config, medialang item.
     itemArgs = item.args
     backend = itemArgs["backend"]
     fast = itemArgs["fast"]
     bgm = itemArgs["bgm"]
-    print(backend, fast,bgm)
+    print(backend, fast, bgm)
 
     # the "previous" is the clips, now fucked.
     # this is initially decided to output mp4, however you might want to decorate it.
