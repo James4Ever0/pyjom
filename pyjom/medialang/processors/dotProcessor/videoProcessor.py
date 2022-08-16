6dfe756b6010b3547fc0769211054843d3f57c8e
GitJournal <app@gitjournal.io>
2022-08-16 16:30:32 +0800

    [2022-08-16 08:30:30]
    FILE:
    pyjom/medialang/processors/dotProcessor/videoProcessor.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/medialang/processors/dotProcessor/videoProcessor.py
+++ pyjom/medialang/processors/dotProcessor/videoProcessor.py
@@ -8,8 +8,8 @@ def dotVideoProcessor(item, previous,format=None):
     if format is None:
         format = item.path.split(".")[-1]
     backend = itemArgs.get("backend","editly")
-    fast = itemArgs.get("fast","editly")
-    bgm = itemArgs.get("bgm","editly")
+    fast = itemArgs.get("fast",True)
+    bgm = itemArgs.get("bgm",None)
     print(format, backend, fast, bgm)
 
     # the "previous" is the clips, now fucked, filled with non-existant intermediate mpegts files, but no source is out there.
