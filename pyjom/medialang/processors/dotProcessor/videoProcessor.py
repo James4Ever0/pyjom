4d1a49587a7ffba5d07f54d3a15b07aa962aa7ff
GitJournal <app@gitjournal.io>
2022-08-16 16:23:48 +0800

    [2022-08-16 08:23:46]
    FILE:
    pyjom/medialang/processors/dotProcessor/videoProcessor.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/medialang/processors/dotProcessor/videoProcessor.py
+++ pyjom/medialang/processors/dotProcessor/videoProcessor.py
@@ -3,6 +3,9 @@ from pyjom.medialang.commons import *
 
 def dotVideoProcessor(item, previous,format="mp4"):
     print("DOTVIDEO ARGS:", item, previous, format)
+    # this item is the video output config, medialang item.
+
+    # the "previous" is the clips, now fucked.
     # this is initially decided to output mp4, however you might want to decorate it.
     print("_________INSIDE DOT VIDEO PROCESSOR_________")
     print("ITEM:", item)
