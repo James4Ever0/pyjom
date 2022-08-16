2e1b0c940dd745ef169da9a030381347e698c1be
GitJournal <app@gitjournal.io>
2022-08-16 19:29:42 +0800

    [2022-08-16 11:29:38]
    FILE:
    pyjom/medialang/processors/mediaProcessor/filesystemProcessor.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/medialang/processors/mediaProcessor/filesystemProcessor.py
+++ pyjom/medialang/processors/mediaProcessor/filesystemProcessor.py
@@ -19,13 +19,13 @@ def imageFsProcessor(imagePath,args={},previous = None):
     newImagePath = getTmpMediaName()
     return newImagePath
 
-def fsProcessor(item,previous=None):
+def fsProcessor(item,previous=None, verbose=True):
     path = item.path # it exists!
     fbase = os.path.basename(path)
     args = item.args
     mediatype = getFileType(fbase) # mediatype not sure yet.
-    print("media path:",path)
-    print("media type:",mediatype)
+        print("media path:",path)
+        print("media type:",mediatype)
     # handle to ffmpeg.
     mediaFunctions = {"video":videoFsProcessor,"audio":audioFsProcessor,"image":imageFsProcessor}
     data = mediaFunctions[mediatype](path,args=args,previous=previous)
