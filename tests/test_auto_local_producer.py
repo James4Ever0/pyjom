1b8f4fcb80c4bc4cd0f497ddfe6016c015a1a91f
GitJournal <app@gitjournal.io>
2022-08-16 19:25:37 +0800

    [2022-08-16 11:25:34]
    FILE:
    tests/test_auto_local_producer.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/test_auto_local_producer.py
+++ tests/test_auto_local_producer.py
@@ -85,4 +85,4 @@ if __name__ == "__main__":
     # completeTest()
     # so we don't have to run it all the time. really?
     scriptFilePath = "/root/Desktop/works/pyjom/tests/medialang_tests/aef2ab90-6414-4b55-a40e-63014e5648a8.mdl"
-    PMRT_0(scriptFilePath)
+    PMRT_0(scriptFilePath, verbose=False)
