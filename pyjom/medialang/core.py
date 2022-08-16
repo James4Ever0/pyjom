8ca473069fd1d2d33437944bcc8fadb4ee7023da
GitJournal <app@gitjournal.io>
2022-08-16 17:47:23 +0800

    [2022-08-16 09:47:20]
    FILE:
    pyjom/medialang/core.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/medialang/core.py
+++ pyjom/medialang/core.py
@@ -435,7 +435,7 @@ class Medialang:
                 for index1, item in enumerate(resource.items):
                     mdata = self.objectExecutor(item, previous=mdata)
                     print("input {}-{}:".format(index0, index1), mdata) # this is the wrong data array!
-                    mdata_array.append({"item":item, "cache": mdata}) # where you store all the intermediate files per clip
+                    mdata_array.append({"item":item, "cache": mdata}) # where you store all the intermediate files per clip.
                 data_array.append(copy.deepcopy(mdata_array))
             data = copy.deepcopy(data_array) # so this is your freaking data! let's decide your approach all inside that dotProcessor instead of generating trash here!
             for item in targets.items:
