8074b9aefb3a9ad7fc7ade9a80e65ee63509345d
GitJournal <app@gitjournal.io>
2022-08-17 16:58:29 +0800

    [2022-08-17 08:58:26]
    FILE:
    pyjom/medialang/processors/dotProcessor/videoProcessor.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/medialang/processors/dotProcessor/videoProcessor.py
+++ pyjom/medialang/processors/dotProcessor/videoProcessor.py
@@ -21,4 +21,6 @@ def dotVideoProcessor(item, previous, format=None, verbose=True):
         print("_________INSIDE DOT VIDEO PROCESSOR_________")
     if backend == "editly":
         # iterate through all items.
-        
+        template = {
+            
+        }
