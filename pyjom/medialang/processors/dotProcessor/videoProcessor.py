6a175a2674d8722a2d577dd501047113e32fafde
GitJournal <app@gitjournal.io>
2022-08-17 18:38:14 +0800

    [2022-08-17 10:38:11]
    FILE:
    pyjom/medialang/processors/dotProcessor/videoProcessor.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/medialang/processors/dotProcessor/videoProcessor.py
+++ pyjom/medialang/processors/dotProcessor/videoProcessor.py
@@ -86,5 +86,5 @@ def dotVideoProcessor(item, previous, format=None, verbose=True):
                 template["clips"].append(clip)
                 # then just execute this template, or let's just view it.
             print("________________editly template________________")
-            print(json.dumps(template, ensure_ascii=False, indent=4))
+            print(json.dumps(template, ensure_ascii=False, indent=4)) # let's view it elsewhere?
             print("________________editly template________________")
