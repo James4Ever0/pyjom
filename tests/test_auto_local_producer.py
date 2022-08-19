182095e9f6679861cff85122810cfe7ce5a06706
GitJournal <app@gitjournal.io>
2022-08-19 12:35:13 +0800

    [2022-08-19 04:35:10]
    FILE:
    tests/test_auto_local_producer.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/test_auto_local_producer.py
+++ tests/test_auto_local_producer.py
@@ -81,9 +81,43 @@ def PMRT_0(scriptFilePath = "", verbose=True):
         medialangScript = f.read()
     partialMedialangRenderTest(medialangScript, verbose=verbose)
 
-import contextlib
+# from contextlib import AbstractContextManager
 
-class 
+
+# class suppress2(AbstractContextManager):
+#     """Context manager to suppress specified exceptions
+
+#     After the exception is suppressed, execution proceeds with the next
+#     statement following the with statement.
+
+#          with suppress(FileNotFoundError):
+#              os.remove(somefile)
+#          # Execution still resumes here if the file was already removed
+#     """
+
+#     def __init__(self, *exceptions):
+#         self._exceptions = exceptions
+
+#     def __enter__(self):
+#         print(dir(self))
+#         pass
+
+#     def __exit__(self, exctype, excinst, exctb):
+#         # Unlike isinstance and issubclass, CPython exception handling
+#         # currently only looks at the concrete type hierarchy (ignoring
+#         # the instance and subclass checking hooks). While Guido considers
+#         # that a bug rather than a feature, it's a fairly hard one to fix
+#         # due to various internal implementation details. suppress provides
+#         # the simpler issubclass based semantics, rather than trying to
+#         # exactly reproduce the limitations of the CPython interpreter.
+#         #
+#         # See http://bugs.python.org/issue12029 for more details
+#         print("EXCTYPE", exctype)
+#         print("EXCINST", excinst)
+#         print("EXCTB",exctb) # exception
+#         print(dir(exctb))
+#         breakpoint()
+#         return exctype is not None and issubclass(exctype, self._exceptions)
 
 if __name__ == "__main__":
     # completeTest()
