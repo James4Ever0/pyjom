cd5dd139495b72bb91c6884a00f9a23f0603c86e
GitJournal <app@gitjournal.io>
2022-08-20 00:32:44 +0800

    [2022-08-19 16:32:42]
    FILE:
    pyjom/medialang/processors/dotProcessor/videoProcessor.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/medialang/processors/dotProcessor/videoProcessor.py
+++ pyjom/medialang/processors/dotProcessor/videoProcessor.py
@@ -92,6 +92,7 @@ def dotVideoProcessor(item, previous, format=None, verbose=True, medialangTmpDir
                                     "resizeMode": "contain",
                                     "cutFrom": cutFrom,
                                     "cutTo": cutTo,
+                                    "mixVolume": 
                                 }
                             removeKeys = []
                             for key, elem in layer.items():
