3a1e809bd9307f4412a13de433da933743fadd07
GitJournal <app@gitjournal.io>
2022-08-19 11:47:48 +0800

    [2022-08-19 03:47:45]
    FILE:
    tests/test_auto_local_producer.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/test_auto_local_producer.py
+++ tests/test_auto_local_producer.py
@@ -89,7 +89,7 @@ if __name__ == "__main__":
     scriptFilePath = "/root/Desktop/works/pyjom/tests/medialang_tests/aef2ab90-6414-4b55-a40e-63014e5648a8.mdl"
     # a special hack
     import tempfile
-    with medialangTmpDir(dir="/dev/shm/medialang",suffix=None, prefix=None) as medialangTmpDir:
+    with tmpdir(path="/dev/shm/medialang",suffix=None, prefix=None) as medialangTmpDir:
         print("MEDIALANG SUPER TMPDIR:", medialangTmpDir)
         result = PMRT_0(scriptFilePath, verbose=False)
         data, data_array = result
