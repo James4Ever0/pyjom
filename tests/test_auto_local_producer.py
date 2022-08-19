9004f42f0e56f985476fe7723849e88aa71efd90
GitJournal <app@gitjournal.io>
2022-08-19 11:40:55 +0800

    [2022-08-19 03:40:51]
    FILE:
    tests/test_auto_local_producer.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/test_auto_local_producer.py
+++ tests/test_auto_local_producer.py
@@ -87,6 +87,9 @@ if __name__ == "__main__":
     # completeTest()
     # so we don't have to run it all the time. really?
     scriptFilePath = "/root/Desktop/works/pyjom/tests/medialang_tests/aef2ab90-6414-4b55-a40e-63014e5648a8.mdl"
+    # a special hack
+    import tempfile
+    with tempfile.TemporaryDirectory()
     result = PMRT_0(scriptFilePath, verbose=False)
     data, data_array = result
     # data -> editly json
