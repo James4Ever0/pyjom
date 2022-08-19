2f867df1a428fcde58657c4d6f1da86bb8c37f79
GitJournal <app@gitjournal.io>
2022-08-19 11:38:42 +0800

    [2022-08-19 03:38:39]
    FILE:
    pyjom/medialang/processors/dotProcessor/videoProcessor.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/medialang/processors/dotProcessor/videoProcessor.py
+++ pyjom/medialang/processors/dotProcessor/videoProcessor.py
@@ -88,4 +88,4 @@ def dotVideoProcessor(item, previous, format=None, verbose=True):
             # print("________________editly template________________")
             # print(json.dumps(template, ensure_ascii=False, indent=4)) # let's view it elsewhere? or in `less`?
             # print("________________editly template________________")
-            # return template
+            return template
