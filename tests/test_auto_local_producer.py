b6fefb4fa570adbc41d0a80f99c6dd8e6905739b
GitJournal <app@gitjournal.io>
2022-08-19 12:47:50 +0800

    [2022-08-19 04:47:47]
    FILE:
    tests/test_auto_local_producer.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/test_auto_local_producer.py
+++ tests/test_auto_local_producer.py
@@ -74,7 +74,8 @@ def completeTest():
 def partialMedialangRenderTest(medialangScript, verbose=True):
     # copy that script to my dear clipboard please?
     medialangObject = Medialang(script=medialangScript, verbose=verbose)
-    medialangObject.execute()
+    result = medialangObject.execute()
+    return result
 
 def PMRT_0(scriptFilePath = "", verbose=True):
     with open(scriptFilePath,"r") as f:
