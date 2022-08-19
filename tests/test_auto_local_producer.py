46213e5245ceaad53372239eec9c1ebf177667af
GitJournal <app@gitjournal.io>
2022-08-19 13:26:11 +0800

    [2022-08-19 05:26:08]
    FILE:
    tests/test_auto_local_producer.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/test_auto_local_producer.py
+++ tests/test_auto_local_producer.py
@@ -123,7 +123,8 @@ if __name__ == "__main__":
         result = PMRT_0(scriptFilePath, verbose=False)
         editly_json, medialang_item_list = result # this just return none!
         # data -> editly json
-        
+        outPath = editly_json["outPath"]
+        print()
         import json
 
         # data_array -> input of dot processor? check it out.
