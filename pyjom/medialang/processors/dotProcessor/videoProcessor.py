06b969be2bcfe52a332f866515f5c6ab8efcfa36
GitJournal <app@gitjournal.io>
2022-08-19 14:00:28 +0800

    [2022-08-19 06:00:25]
    FILE:
    pyjom/medialang/processors/dotProcessor/videoProcessor.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/medialang/processors/dotProcessor/videoProcessor.py
+++ pyjom/medialang/processors/dotProcessor/videoProcessor.py
@@ -20,6 +20,7 @@ def dotVideoProcessor(item, previous, format=None, verbose=True):
     )  # so all things will be assumed to put directly into editly render json, unless explicitly specified under other medialang or other backend and need to be resolved into media file format before rendering. sure?
     fast = itemArgs.get("fast", True)
     bgm = itemArgs.get("bgm", None)
+    
     print(format, backend, fast, bgm)
 
     # the "previous" is the clips, was fucked, filled with non-existant intermediate mpegts files, but no source was out there.
