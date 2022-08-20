802f601194f650e0bfa2b8da3fcd57c450e8a996
GitJournal <app@gitjournal.io>
2022-08-20 23:10:49 +0800

    [2022-08-20 15:10:46]
    FILE:
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/karaoke_effects/test_pylrc.py
+++ tests/karaoke_effects/test_pylrc.py
@@ -1,6 +1,6 @@
 import pylrc
 
-lrc_file = open('example.lrc')
+lrc_file = open('/root/Desktop/works/pyjom/tests/music_analysis/exciting_bgm.lrc')
 lrc_string = ''.join(lrc_file.readlines())
 lrc_file.close()
 
