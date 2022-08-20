cf6690697f091e4d61a76e424c88892401dfabde
GitJournal <app@gitjournal.io>
2022-08-20 15:00:16 +0800

    [2022-08-20 07:00:14]
    FILE:
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- externals/MEGAMOJI/src/utils/array.ts
+++
@@ -1,8 +0,0 @@
-export function nearestIndex(array: number[], value: number): number {
-  return array.map((item, ix) => ({
-    ix,
-    diff: Math.abs(item - value),
-  })).sort((a, b) => (
-    a.diff - b.diff
-  ))[0].ix;
-}
--- externals/MEGAMOJI/src/utils/dom.ts
+++
@@ -1,9 +0,0 @@
-export const elementIsContained = (element: Element | null, parent: Element): boolean => {
-  if (!element) {
-    return false;
-  } else if (element === parent) {
-    return true;
-  } else {
-    return elementIsContained(element.parentElement, parent);
-  }
-};
