43d2b85aa4433108226ed67a6862579cf8577518
GitJournal <app@gitjournal.io>
2022-08-20 22:52:11 +0800

    [2022-08-20 14:52:09]
    FILE:
    tests/karaoke_effects/lrc2ass_py3/test.sh
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/karaoke_effects/lrc2ass_py3/test.sh
+++ tests/karaoke_effects/lrc2ass_py3/test.sh
@@ -1,2 +1,2 @@
 rootpath=/Users/jamesbrown/desktop/works/pyjom_remote/
-mpv --fs --no-audio --sub-file="$rootpath/tests/karaoke_effects/lrc2ass_py3/output.ass" "$rootpath/samples/video/karaoke_effects_source.mp4"
+mpv --fs --no-audio --sub-file="$rootpath/tests/karaoke_effects/lrc2ass_py3/output.s1.ass" "$rootpath/samples/video/karaoke_effects_source.mp4"
