e6780c54af15167f10140636a7df6f5aef620f4b
GitJournal <app@gitjournal.io>
2022-08-20 23:27:47 +0800

    [2022-08-20 15:27:43]
    FILE:
    tests/karaoke_effects/lrc2ass_py3/lrc2ass_py3_changing.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/karaoke_effects/lrc2ass_py3/lrc2ass_py3_changing.py
+++ tests/karaoke_effects/lrc2ass_py3/lrc2ass_py3_changing.py
@@ -289,6 +289,7 @@ for i in range(0, len(line_indexs)):
             if line_end == next_line_start:
                 print("警告: 行\"{0}\"没有结束时间标签，已经把下一行的开始时间当作结束时间".format(line,))
         print('警告: 行\"{0}\"没有结束时间标签，自动选择\"{1}\"作为结束时间\n'.format(line, line_end))
+        print("START:", "END:",line_end)
 
         line_end_time = int(line_end.split(':')[0]) * 60 + int(line_end.split(':')[1]) * 60 * 100 + \
                         int(line_end.split(':')[2].split('.')[0]) * 100 + int(line_end.split('.')[1])
