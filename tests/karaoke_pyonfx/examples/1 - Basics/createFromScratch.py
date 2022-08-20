65a5c85f4bdce7c87e1acb4fcee8f40e80acba26
GitJournal <app@gitjournal.io>
2022-08-20 17:06:47 +0800

    [2022-08-20 09:06:44]
    FILE:
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/karaoke_pyonfx/examples/1 - Basics/createFromScratch.py
+++ tests/karaoke_pyonfx/examples/1 - Basics/createFromScratch.py
@@ -1,11 +1,13 @@
 from pyonfx import *
-os.system("touch "output_scratch.ass"")
+import os
+os.system("touch output_scratch.ass")
 io = Ass("output_scratch.ass",path_output="output_scratch.ass")
 
 newLine = Line()
 newLine.text = "hello world"
 newLine.start_time = 0
 newLine.end_time = 10
+newLine.comment=False
 
 io.write_line(newLine)
 io.save()
