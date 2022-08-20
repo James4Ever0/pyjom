d6304a176026445b448a05c46114317f33e3dfde
GitJournal <app@gitjournal.io>
2022-08-21 00:13:54 +0800

    [2022-08-20 16:13:51]
    FILE:
    tests/karaoke_effects/pyonfx_test/macos_view_best_example.sh
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/karaoke_effects/pyonfx_test/macos_view_best_example.sh
+++ tests/karaoke_effects/pyonfx_test/macos_view_best_example.sh
@@ -1,2 +1,2 @@
 rootpath=/Users/jamesbrown/desktop/works/pyjom_remote/
-mpv --fs --no-audio --sub-file="$rootpath/tests/karaoke_effects/pyonfx_test/examples/2 - Beginner/Output.ass" "$rootpath/samples/video/karaoke_effects_source.mp4"
+mpv --fs  --sub-file="$rootpath/tests/karaoke_effects/pyonfx_test/examples/2 - Beginner/Output.ass" "$rootpath/samples/video/karaoke_effects_source.mp4"
