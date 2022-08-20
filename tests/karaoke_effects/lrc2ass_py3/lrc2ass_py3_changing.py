d63c7928f52a5eaafcb46eb674490f48d0752902
GitJournal <app@gitjournal.io>
2022-08-20 23:01:44 +0800

    [2022-08-20 15:01:41]
    FILE:
    tests/karaoke_effects/lrc2ass_py3/lrc2ass_py3_changing.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/karaoke_effects/lrc2ass_py3/lrc2ass_py3_changing.py
+++ tests/karaoke_effects/lrc2ass_py3/lrc2ass_py3_changing.py
@@ -256,7 +256,7 @@ for i in range(0, len(line_indexs)):
             def getNumber(mString):
                 import parse
                 result = parse.parse("{d1:d}:{d2:d}:{d3:d}.{d4:d}",mString)
-                
+                d1, d2, d3, d4 = result["d1"], result["d2"]
             def compMin()
             line_end = compMin(next_line_start, line_end)
             if line_end == next_line_start:
