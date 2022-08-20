6557f87c2d82fcb49623335f088789d1879a016c
GitJournal <app@gitjournal.io>
2022-08-20 23:32:31 +0800

    [2022-08-20 15:32:25]
    FILE:
    tests/karaoke_effects/lrc2ass_py3/localTest.sh
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

---
+++ tests/karaoke_effects/lrc2ass_py3/localTest.sh
@@ -0,0 +1,5 @@
+rootpath=/Users/jamesbrown/desktop/works/pyjom_remote/
+mpv --fs --no-audio --sub-file="$rootpath/tests/karaoke_effects/lrc2ass_py3/output.s2.ass" "$rootpath/samples/video/karaoke_effects_source.mp4"
+
+## NOT OK! THIS DUMB SHIT LIBRARY FUCKED MY MIND ##
+## the effect is bad. the time is not right. everything fucked. ##
