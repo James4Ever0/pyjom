8b094d5ac5c6010b819396aea7f82aa16198a8f0
GitJournal <app@gitjournal.io>
2022-08-22 02:24:01 +0800

    [2022-08-21 18:23:58]
    FILE:
    pyjom/medialang/processors/dotProcessor/videoProcessor.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/medialang/processors/dotProcessor/videoProcessor.py
+++ pyjom/medialang/processors/dotProcessor/videoProcessor.py
@@ -15,7 +15,7 @@ def executeEditlyScript(medialangTmpDir, editly_json):
     assert returncode == 0
     print("RENDER SUCCESSFUL")
 
-def ffmpegVideoPreProductionFilter(filepath, start=None, end=None, cachePath=None, filters=[""]): # what is the type of this shit?
+def ffmpegVideoPreProductionFilter(filepath, start=None, end=None, cachePath=None, filters=["PIP_DETECTION"]): # what is the type of this shit?
     return processedFilePath, processed
 
 def dotVideoProcessor(item, previous, format=None, verbose=True, medialangTmpDir="/dev/shm/medialang/"):
