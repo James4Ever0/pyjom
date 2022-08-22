e98ac4327cacb7c2bc1855b4d3ab5172ab0c3b18
GitJournal <app@gitjournal.io>
2022-08-22 13:08:22 +0800

    [2022-08-22 05:08:19]
    FILE:
    tests/ffmpeg_python_test/test.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/ffmpeg_python_test/test.py
+++ tests/ffmpeg_python_test/test.py
@@ -18,9 +18,9 @@ def cropVideoRegion():
     # just use simple concat. right?
     # 334x188
     # not only crop, but ZOOM!
-    stream_0 = ffmpeg.input("output.mp4",ss=0, to=2).filter("crop",x,y,width,height)
-    stream_1 = ffmpeg.input("output.mp4",ss=2, to=4).filter("crop",x,y,width,height)
-    stream_2 = ffmpeg.input("output.mp4",ss=4, to=6).filter("crop",x,y,width,height)
+    stream_0 = ffmpeg.input("output.mp4",ss=0, to=2).filter("crop",x,y,width,height).filter("scale",defaultWidth, defaultHeight)
+    stream_1 = ffmpeg.input("output.mp4",ss=2, to=4).filter("crop",x,y,width,height).filter("scale",defaultWidth, defaultHeight)
+    stream_2 = ffmpeg.input("output.mp4",ss=4, to=6).filter("crop",x,y,width,height).filter("scale",defaultWidth, defaultHeight)
 
     stream = ffmpeg.output(stream, "pipCrop.mp4")
     ffmpeg.run(stream, overwrite_output=True)
