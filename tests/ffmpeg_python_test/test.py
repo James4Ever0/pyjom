0c9626c89e8f1dfae48cba621e1ea7b052e93204
GitJournal <app@gitjournal.io>
2022-08-22 12:31:59 +0800

    [2022-08-22 04:31:56]
    FILE:
    tests/ffmpeg_python_test/test.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/ffmpeg_python_test/test.py
+++ tests/ffmpeg_python_test/test.py
@@ -1,11 +1,13 @@
 import ffmpeg
 
-# input_source = "/root/Desktop/works/pyjom/samples/video/karaoke_effects_source.mp4"
+    input_source = "/root/Desktop/works/pyjom/samples/video/karaoke_effects_source.mp4"
 
-# stream = ffmpeg.input(input_source,ss=4, to=10) # from 4 to 10 seconds?
-# # stream = ffmpeg.hflip(stream)
-# # we just need to crop this.
-# stream = ffmpeg.output(stream, 'output.mp4')
+    stream = ffmpeg.input(input_source,ss=4, to=10) # from 4 to 10 seconds?
+    # stream = ffmpeg.hflip(stream)
+    # we just need to crop this.
+    stream = ffmpeg.output(stream, 'output.mp4')
+    ffmpeg.run(stream, overwrite_output=True)
+
 
 # pipCrop in some span?
 
