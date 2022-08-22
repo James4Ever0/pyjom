9a81da4805fe0f6bfb736cc8423cb862cf0b450a
GitJournal <app@gitjournal.io>
2022-08-22 10:48:49 +0800

    [2022-08-22 02:48:47]
    FILE:
    pyjom/medialang/processors/dotProcessor/videoProcessor.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/medialang/processors/dotProcessor/videoProcessor.py
+++ pyjom/medialang/processors/dotProcessor/videoProcessor.py
@@ -16,7 +16,7 @@ def executeEditlyScript(medialangTmpDir, editly_json):
     print("RENDER SUCCESSFUL")
 
 def ffmpegVideoPreProductionFilter(filepath, start=None, end=None, cachePath=None, filters=["pipCrop","textRemoval","logoRemoval"]): # what is the type of this shit?
-    # enable that 'fast' flag?
+    # enable that 'fast' flag? or we use low_resolution ones?
     # anyway it will get processed? or not?
     # uncertain. very uncertain.
     ffmpeg.
