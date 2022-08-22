d35cebf963c11e10acd0da06628f124bf1e6d09d
GitJournal <app@gitjournal.io>
2022-08-22 21:25:04 +0800

    [2022-08-22 13:25:01]
    FILE:
    tests/ffmpeg_python_test/test.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/ffmpeg_python_test/test.py
+++ tests/ffmpeg_python_test/test.py
@@ -106,6 +106,8 @@ def delogoTest():
     stream_0_video = stream_0.video
     stream_0_audio = stream_0.audio
     stream_1 = ffmpeg.input("output.mp4", ss=3, to=6)
+    stream_1_video = stream_1.video
+    stream_1_audio = stream_1.audio
     # we must specify the time first.
     # it is like a compiler! ffmpeg commandline (also its library, mind-blowingly crazy and complex) really sucks. thanks, ffmpeg-python wrapper.
 
