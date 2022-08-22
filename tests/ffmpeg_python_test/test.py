49a4fab6bf15b2d5539207aa284991a09ca3e452
GitJournal <app@gitjournal.io>
2022-08-22 16:31:41 +0800

    [2022-08-22 08:31:38]
    FILE:
    tests/ffmpeg_python_test/test.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/ffmpeg_python_test/test.py
+++ tests/ffmpeg_python_test/test.py
@@ -46,7 +46,8 @@ def cropVideoRegion():
     x, y, width, height = getRandomCrop(defaultWidth, defaultHeight)
     stream_2 = ffmpeg.input("output.mp4",ss=4, to=6).crop(x,y,width, height).filter("pad",x=math.floor((defaultWidth-width)/2), y=math.floor((defaultHeight-height)/2), width=defaultWidth, height=defaultHeight,color="black")
 
-    stream = ffmpeg.concat(stream_0, stream_1, stream_2,)
+    stream = ffmpeg.concat(stream_0, stream_1, stream_2)
+    # there is no audio down here! fuck.
 
     stream = ffmpeg.output(stream, "pipCrop.mp4")
     ffmpeg.run(stream, overwrite_output=True)
