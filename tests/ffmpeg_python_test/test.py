52cde7de65ec6c9045a2e40e3f5d623d29eec62a
GitJournal <app@gitjournal.io>
2022-08-22 17:24:23 +0800

    [2022-08-22 09:24:20]
    FILE:
    tests/ffmpeg_python_test/test.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/ffmpeg_python_test/test.py
+++ tests/ffmpeg_python_test/test.py
@@ -44,7 +44,7 @@ def cropVideoRegion():
     minRatio = min(defaultWidth/width, defaultHeight/height)
     newWidth = math.floor(minRatio*width)
     newHeight = math.floor(minRatio*height)
-    stream_1 = ffmpeg.input("output.mp4",ss=2, to=4).crop(x,y,width, height).filter("resize").filter("pad",x=math.floor((defaultWidth-newWidth)/2), y=math.floor((defaultHeight-newHeight)/2), width=defaultWidth, height=defaultHeight,color="black")
+    stream_1 = ffmpeg.input("output.mp4",ss=2, to=4).crop(x,y,width, height).filter("scale").filter("pad",x=math.floor((defaultWidth-newWidth)/2), y=math.floor((defaultHeight-newHeight)/2), width=defaultWidth, height=defaultHeight,color="black")
 
     
     x, y, width, height = getRandomCrop(defaultWidth, defaultHeight)
