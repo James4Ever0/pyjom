2d12b658e10faa40ce6427ee16d0e8353397f79a
GitJournal <app@gitjournal.io>
2022-08-22 17:13:37 +0800

    [2022-08-22 09:13:34]
    FILE:
    tests/ffmpeg_python_test/test.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/ffmpeg_python_test/test.py
+++ tests/ffmpeg_python_test/test.py
@@ -61,10 +61,10 @@ def concatVideoWithAudio():
     stream_0 = ffmpeg.input("output.mp4",ss=0, t=3)
     stream_1 = ffmpeg.input("output.mp4",ss=3, t=6)
 
-    stream = ffmpeg.concat(stream_0.video, stream_0.audio, stream_1.video, stream_1.audio, v=1, a=1).node
+    stream = ffmpeg.concat(stream_0.video, stream_0.audio, stream_1.video, stream_1.audio, v=1, a=1)
     # print(stream)
     # breakpoint()
-    stream = ffmpeg.output(stream[0], stream[1], "concatVideo.mp4")
+    stream = ffmpeg.output(stream, "concatVideo.mp4")
     # print(stream.get_args())
     stream.run(overwrite_output=True)
 
