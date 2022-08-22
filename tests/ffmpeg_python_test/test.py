48bd6b96ce92ff8e2b207078f6e3546dc86324fd
GitJournal <app@gitjournal.io>
2022-08-22 11:25:02 +0800

    [2022-08-22 03:24:59]
    FILE:
    tests/ffmpeg_python_test/test.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/ffmpeg_python_test/test.py
+++ tests/ffmpeg_python_test/test.py
@@ -2,7 +2,8 @@ import ffmpeg
 
 input_source = ""
 
-stream = ffmpeg.input(input_source)
-stream = ffmpeg.hflip(stream)
+stream = ffmpeg.input(input_source,)
+# stream = ffmpeg.hflip(stream)
+# we just need to crop this.
 stream = ffmpeg.output(stream, 'output.mp4')
 ffmpeg.run(stream, overwrite_output=True)
