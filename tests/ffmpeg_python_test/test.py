f0f1071b4e7e4dbee7a8460346ec42c22795963f
GitJournal <app@gitjournal.io>
2022-08-22 15:58:37 +0800

    [2022-08-22 07:58:34]
    FILE:
    tests/ffmpeg_python_test/test.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/ffmpeg_python_test/test.py
+++ tests/ffmpeg_python_test/test.py
@@ -22,6 +22,8 @@ def cropVideoRegion():
     infoData = info.getInfo()
     # print(infoData)
     # breakpoint()
+    defaultWidth = infoData["videoWidth"]
+    defaultHeight = infoData["videoHeight"]
     # not only crop, but ZOOM!
     stream_0 = ffmpeg.input("output.mp4",ss=0, to=2).crop(x,y,width,height).filter("scale",defaultWidth, defaultHeight)
     stream_1 = ffmpeg.input("output.mp4",ss=2, to=4).crop(x,y,width,height).filter("scale",defaultWidth, defaultHeight)
