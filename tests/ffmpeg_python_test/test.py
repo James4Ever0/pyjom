4751536a8c17996d5f4d1bffc446b79bbd1807aa
GitJournal <app@gitjournal.io>
2022-08-22 11:28:36 +0800

    [2022-08-22 03:28:34]
    FILE:
    tests/ffmpeg_python_test/test.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/ffmpeg_python_test/test.py
+++ tests/ffmpeg_python_test/test.py
@@ -2,7 +2,7 @@ import ffmpeg
 
 input_source = ""
 
-stream = ffmpeg.input(input_source,ss=4, to=)
+stream = ffmpeg.input(input_source,ss=4, to=10) # from 4 to 10 seconds?
 # stream = ffmpeg.hflip(stream)
 # we just need to crop this.
 stream = ffmpeg.output(stream, 'output.mp4')
