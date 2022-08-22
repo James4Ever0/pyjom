c52ea407a7a09c6ed4233bf9e1bedbbcd85333cf
GitJournal <app@gitjournal.io>
2022-08-22 11:21:12 +0800

    [2022-08-22 03:21:09]
    FILE:
    tests/ffmpeg_python_test/test.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/ffmpeg_python_test/test.py
+++ tests/ffmpeg_python_test/test.py
@@ -1,6 +1,7 @@
 import ffmpeg
 
-stream = ffmpeg.input('input.mp4')
+
+stream = ffmpeg.input(input_source)
 stream = ffmpeg.hflip(stream)
 stream = ffmpeg.output(stream, 'output.mp4')
 ffmpeg.run(stream)
