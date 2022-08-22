15061d3244ff3011cdf6a7aec8779b64cad83883
GitJournal <app@gitjournal.io>
2022-08-22 16:26:06 +0800

    [2022-08-22 08:26:03]
    FILE:
    tests/ffmpeg_python_test/test.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/ffmpeg_python_test/test.py
+++ tests/ffmpeg_python_test/test.py
@@ -33,12 +33,14 @@ def cropVideoRegion():
     defaultHeight = infoData["videoHeight"]
     # not only crop, but ZOOM!
     import math
-    
+
     x, y, width, height = getRandomCrop(defaultWidth, defaultHeight)
     stream_0 = ffmpeg.input("output.mp4",ss=0, to=2).crop(x,y,width, height).filter("pad",x=math.floor((defaultWidth-width)/2), y=math.floor((defaultHeight-height)/2), width=defaultWidth, height=defaultHeight,color="black")
 
 
-    stream_1 = ffmpeg.input("output.mp4",ss=2, to=4).crop(*getRandomCrop(defaultWidth, defaultHeight))
+
+    x, y, width, height = getRandomCrop(defaultWidth, defaultHeight)
+    stream_1 = ffmpeg.input("output.mp4",ss=2, to=4).crop(x,y,width, height).filter("pad",x=math.floor((defaultWidth-width)/2), y=math.floor((defaultHeight-height)/2), width=defaultWidth, height=defaultHeight,color="black")
 
     stream_2 = ffmpeg.input("output.mp4",ss=4, to=6).crop(*getRandomCrop(defaultWidth, defaultHeight))
 
