52eaf53ac1bfe4eee9a6ca28d1d636f42d733650
GitJournal <app@gitjournal.io>
2022-08-22 16:02:42 +0800

    [2022-08-22 08:02:40]
    FILE:
    tests/ffmpeg_python_test/test.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/ffmpeg_python_test/test.py
+++ tests/ffmpeg_python_test/test.py
@@ -9,7 +9,7 @@ def basicTrimVideoProcess():
     stream = ffmpeg.output(stream, 'output.mp4')
     ffmpeg.run(stream, overwrite_output=True)
 
-
+def getRandomCrop(width, height)
 # pipCrop in some span?
 
 def cropVideoRegion():
@@ -26,8 +26,8 @@ def cropVideoRegion():
     defaultHeight = infoData["videoHeight"]
     # not only crop, but ZOOM!
     stream_0 = ffmpeg.input("output.mp4",ss=0, to=2).crop(*getRandomCrop(defaultWidth, defaultHeight)).filter("scale",defaultWidth, defaultHeight)
-    stream_1 = ffmpeg.input("output.mp4",ss=2, to=4).crop(x,y,width,height).filter("scale",defaultWidth, defaultHeight)
-    stream_2 = ffmpeg.input("output.mp4",ss=4, to=6).crop(x,y,width,height).filter("scale",defaultWidth, defaultHeight)
+    stream_1 = ffmpeg.input("output.mp4",ss=2, to=4).crop(*getRandomCrop(defaultWidth, defaultHeight)).filter("scale",defaultWidth, defaultHeight)
+    stream_2 = ffmpeg.input("output.mp4",ss=4, to=6).crop(*getRandomCrop(defaultWidth, defaultHeight)).filter("scale",defaultWidth, defaultHeight)
 
     stream = ffmpeg.output(stream, "pipCrop.mp4")
     ffmpeg.run(stream, overwrite_output=True)
