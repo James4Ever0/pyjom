2ecc50cd174ef7b16a1d92541e5ea4339eb49d33
jamesbrown <randomvoidmail@foxmail.com>
2022-08-24 10:24:05 +0800

    [2022-08-24 02:24:04]
    FILE:
    tests/interval_set_math_operations/continual_sympy.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/interval_set_math_operations/continual_sympy.py
+++ tests/interval_set_math_operations/continual_sympy.py
@@ -121,6 +121,7 @@ for k,v in normalCats.items():
 #
 #  finalCats.update({"empty":emptyIntervalTupleList})
 finalCats.update({"empty":finalCats[()]})
+finalCats[()]
 
 print("_____FINAL CATS_____")
 print(finalCats)
