1926505a4d3710cdbdeb1657f9a88ef8757c656b
jamesbrown <randomvoidmail@foxmail.com>
2022-08-24 10:34:42 +0800

    [2022-08-24 02:34:41]
    FILE:
    tests/interval_set_math_operations/continual_less_sympy.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/interval_set_math_operations/continual_less_sympy.py
+++ tests/interval_set_math_operations/continual_less_sympy.py
@@ -32,7 +32,8 @@ def mergeOverlappedInIntervalTupleList(intervalTupleList):
 mSet = mergeOverlappedInIntervalTupleList([(0,1), (2,3)])
 mSet2 = mergeOverlappedInIntervalTupleList([(0.5,1.5),(1.6,2.5)])
 
-
+print("MSET", mSet)
+print("MSET2", mSet2)
 
 mSetCandidates = [mSet, mSet2]
 mSetUnified = [x for y in mSetCandidates for x in y]
