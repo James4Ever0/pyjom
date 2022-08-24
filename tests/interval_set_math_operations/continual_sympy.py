b96c3ffe84529d561a7b37d519a204ac9bde1ed3
jamesbrown <randomvoidmail@foxmail.com>
2022-08-24 10:03:55 +0800

    [2022-08-24 02:03:54]
    FILE:
    tests/interval_set_math_operations/continual_sympy.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/interval_set_math_operations/continual_sympy.py
+++ tests/interval_set_math_operations/continual_sympy.py
@@ -97,11 +97,11 @@ for k,v in normalCats.items():
   ##### union to tuple list, could be replaced #####
   ## REPLACED ##
   print("M_FINAL_UNION_CANDIDATE",mFinalUnionCandidate)
-  breakpoint()
 
   mFinalIntervalListCandidate = mergeOverlappedInIntervalTupleList(mFinalUnionCandidate)
   print("M_FINAL_INTERVAL_LIST_CANDIDATE", mFinalIntervalListCandidate)
 
+  breakpoint()
   finalCats.update({k:mFinalIntervalListCandidate.copy()})
 # this whole calculation could just be exponential. goddamn it?
 # before that, we need to get the "empty" out. but is that really necessary? i think it is, as an important feature.
