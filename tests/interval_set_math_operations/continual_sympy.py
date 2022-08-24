527cbc61e8d10e28a250c551405b795c4c1ef7d3
jamesbrown <randomvoidmail@foxmail.com>
2022-08-24 09:40:36 +0800

    [2022-08-24 01:40:36]
    FILE:
    tests/interval_set_math_operations/continual_sympy.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/interval_set_math_operations/continual_sympy.py
+++ tests/interval_set_math_operations/continual_sympy.py
@@ -55,7 +55,7 @@ subIntervals = list(subIntervals)
 # for subIntervals, it's still not real interval but tuple at above line.
 
 reversedCats = {}
-subIntervalUnion = None
+subIntervalUnion = sympy.Em
 for subIntervalIndex, (start, end) in enumerate(subIntervals):
   subIntervalCandidate = sympy.Interval(start, end)
   if subIntervalUnion is None:
