e3fbb2d69d488249bd422f279828242b528fcbe2
GitJournal <app@gitjournal.io>
2022-09-03 18:27:55 +0800

    [2022-09-03 10:27:51]
    FILE:
    pyjom/medialang/processors/dotProcessor/videoProcessor.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/medialang/processors/dotProcessor/videoProcessor.py
+++ pyjom/medialang/processors/dotProcessor/videoProcessor.py
@@ -70,7 +70,7 @@ def detectTextRegionOverTime(videoPath, start, end, sample_rate = 10):
     for index in range(maxListIndex):
         rangeStart, rangeEnd = index-convSpan, index+convSpan
         rangeStart, rangeEnd = max(0, rangeStart), min(maxListIndex, rangeEnd)
-        
+        mConvList = detectionList[:]
 
 
 def getPreviewPixels(defaultWidth, defaultHeight, maxPixel):
