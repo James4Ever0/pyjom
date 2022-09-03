27e960ec1b722de6315cdeacda173dfe9063cc6a
GitJournal <app@gitjournal.io>
2022-09-03 17:59:51 +0800

    [2022-09-03 09:59:48]
    FILE:
    tests/unittest_convolution_bilibili_translate_text_detect.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_convolution_bilibili_translate_text_detect.py
+++ tests/unittest_convolution_bilibili_translate_text_detect.py
@@ -120,7 +120,7 @@ for intKey in range(minKey, maxKey+1):
     contours = cv2.findContours(currentBlackPictureBlurred, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     contours = contours[0] if len(contours) == 2 else contours[1]
 
-    currentBoundingBoxesVisualize = 
+    currentBoundingBoxesVisualize = getBlackPicture()
 
 
     cv2.imshow("IMAGE", currentBoundingBoxesVisualize)
