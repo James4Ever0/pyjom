ccb36d94ebd98a4fce2b475f49ec973c6e689dc9
GitJournal <app@gitjournal.io>
2022-09-03 21:54:52 +0800

    [2022-09-03 13:54:49]
    FILE:
    pyjom/mathlib.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/mathlib.py
+++ pyjom/mathlib.py
@@ -128,4 +128,5 @@ def getContinualMappedNonSympyMergeResult(mRangesDict, concatSymbol = "|",noEmpt
 
 def getContinualMappedNonSympyMergeResultWithRangedEmpty(mRangesDict,start, end, concatSymbol="|"):
     import uuid
+    emptySetName = str(uuid.uuid4())
     
