35eb95c0c989a3600aa548a5d1c537efb0e2b7b5
GitJournal <app@gitjournal.io>
2022-09-03 21:37:07 +0800

    [2022-09-03 13:37:03]
    FILE:
    pyjom/medialang/processors/dotProcessor/videoProcessor.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/medialang/processors/dotProcessor/videoProcessor.py
+++ pyjom/medialang/processors/dotProcessor/videoProcessor.py
@@ -142,6 +142,7 @@ def detectTextRegionOverTime(videoPath, start, end, sample_rate = 10, mergeThres
             mRangesDict.update(mRangesDict.get(tupleY,[])+[ranges[index]])
     # print(mRangesDict)
     # breakpoint()
+    delogo_
     finalCatsMapped = getContinualMappedNonSympyMergeResult(mRangesDict, noEmpty=True) # need to use string input!
     return finalCatsMapped
 
