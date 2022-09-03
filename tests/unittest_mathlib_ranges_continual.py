a63a51a31902f3763142a40efb1b02ea14a51082
GitJournal <app@gitjournal.io>
2022-09-03 21:27:02 +0800

    [2022-09-03 13:27:00]
    FILE:
    tests/unittest_mathlib_ranges_continual.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_mathlib_ranges_continual.py
+++ tests/unittest_mathlib_ranges_continual.py
@@ -9,5 +9,5 @@ result_0 = getContinualNonSympyMergeResult(inputList)
 print(result_0)
 print("_"*20)
 
-result_1 = getContinualNonSympyMergeResult(mRangesDict)
+result_1 = getContinualMappedNonSympyMergeResult(mRangesDict)
 print(result_1)
