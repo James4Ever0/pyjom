2d5942ea2a744f050f45b3582c54621e4d249363
GitJournal <app@gitjournal.io>
2022-09-03 11:39:42 +0800

    [2022-09-03 03:39:39]
    FILE:
    tests/unittest_ffmpegVideoPreProductionFilter.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_ffmpegVideoPreProductionFilter.py
+++ tests/unittest_ffmpegVideoPreProductionFilter.py
@@ -1,4 +1,6 @@
 from test_commons import *
 from pyjom.medialang.processors.dotProcessor import ffmpegVideoPreProductionFilter
 import tempfile
+
 tempDir = '/dev/shm/medialang'
+with tempfile.TemporaryDirectory()
