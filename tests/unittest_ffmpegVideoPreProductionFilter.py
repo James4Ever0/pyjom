cf7bb8487fb72066a715d843f8d6fa4629840426
GitJournal <app@gitjournal.io>
2022-09-03 11:42:36 +0800

    [2022-09-03 03:42:33]
    FILE:
    tests/unittest_ffmpegVideoPreProductionFilter.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_ffmpegVideoPreProductionFilter.py
+++ tests/unittest_ffmpegVideoPreProductionFilter.py
@@ -1,8 +1,9 @@
 from test_commons import *
 from pyjom.medialang.processors.dotProcessor import ffmpegVideoPreProductionFilter
 import tempfile
-
+videoPath = 
 tempDir = '/dev/shm/medialang' # anyway we just want something else...
 with tempfile.TemporaryDirectory(prefix = tempDir) as allocatedTmpDir:
     print("Allocated tmpDir:", allocatedTmpDir)
+    videoPath
     videoFile = os.path.join(allocatedTmpDir,videoFileName)
