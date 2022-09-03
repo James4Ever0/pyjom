b11a89bee4c792aa0f422155ffc1da896d2dd688
GitJournal <app@gitjournal.io>
2022-09-03 12:18:37 +0800

    [2022-09-03 04:18:33]
    FILE:
    tests/unittest_ffmpegVideoPreProductionFilter.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_ffmpegVideoPreProductionFilter.py
+++ tests/unittest_ffmpegVideoPreProductionFilter.py
@@ -14,7 +14,7 @@ def getPreviewPixels(defaultWidth, defaultHeight, maxPixel):
     if defaultWidth < defaultHeight:
         reverseFlag = True
     maxDim = max(mList)
-    shrinkRatio = 
+    shrinkRatio = maxDim()
 
 with tempfile.TemporaryDirectory(prefix = tempDir) as allocatedTmpDir:
     print("Allocated tmpDir:", allocatedTmpDir)
