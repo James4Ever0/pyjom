107ed8140aa3a7d9d5d6b26c1e136d9c6664c262
GitJournal <app@gitjournal.io>
2022-09-03 15:32:10 +0800

    [2022-09-03 07:32:06]
    FILE:
    tests/test_convolution_bilibili_translate_text_detect.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/test_convolution_bilibili_translate_text_detect.py
+++ tests/test_convolution_bilibili_translate_text_detect.py
@@ -3,4 +3,4 @@ import json
 # easy gig, you said.
 targetFile = "/root/Desktop/works/pyjom/tests/bilibili_practices/bilibili_video_translate/japan_day.json"
 
-mJson 
+mJson = json.loads(open(targetFile, 'r',encoding='utf-8')r)
