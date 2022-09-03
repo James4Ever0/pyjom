adf8b414c2555278ee89fcc02c01e40f22592eb6
GitJournal <app@gitjournal.io>
2022-09-03 17:26:27 +0800

    [2022-09-03 09:26:24]
    FILE:
    tests/unittest_convolution_bilibili_translate_text_detect.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_convolution_bilibili_translate_text_detect.py
+++ tests/unittest_convolution_bilibili_translate_text_detect.py
@@ -79,7 +79,7 @@ for intKey in range(minKey, maxKey+1):
         loc0 = (x0,y0)
         loc1 = (x1,y1)
         cv2.rectangle(blackPicture, loc0, loc1, 255,cv2.FILLED) # we fill so we can merge shits.
-    blackPictureBlurred =cv2.GaussianBlur(blackPicture, (3, 33), 0)
+    blackPictureBlurred =cv2.GaussianBlur(blackPicture, (33,33), 0)
     cv2.imshow("IMAGE", blackPictureBlurred)
     cv2.waitKey(10)
     print("showing image:", intKey)
