c0151a30233eecb319f4c20a58611880336361ed
GitJournal <app@gitjournal.io>
2022-09-03 15:38:05 +0800

    [2022-09-03 07:37:58]
    FILE:
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/test_convolution_bilibili_translate_text_detect.py
+++ tests/test_convolution_bilibili_translate_text_detect.py
@@ -12,7 +12,10 @@ minKey, maxKey = min(mIntKeys), max(mIntKeys)
 for intKey in range(minKey, maxKey+1):
     strKey = str(intKey)
     target = mJson[strKey]
-    for 
+    for item in target:
+        location = item[0]
+        text, confidence = item[1]
+        print(location)
     # print(intKey,target)
     # this time we do not care about the text inside.
     # breakpoint()
