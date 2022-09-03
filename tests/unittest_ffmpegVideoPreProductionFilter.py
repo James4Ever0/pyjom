02d5365a3424faa5d1c3e6ce2c06411a738c3c84
GitJournal <app@gitjournal.io>
2022-09-03 12:20:04 +0800

    [2022-09-03 04:20:00]
    FILE:
    tests/unittest_ffmpegVideoPreProductionFilter.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_ffmpegVideoPreProductionFilter.py
+++ tests/unittest_ffmpegVideoPreProductionFilter.py
@@ -15,7 +15,7 @@ def getPreviewPixels(defaultWidth, defaultHeight, maxPixel):
     #     reverseFlag = True
     maxDim = max(mList)
     shrinkRatio = maxPixel/maxDim
-    getRounded = lambda num, rounder: num
+    getRounded = lambda num, rounder: (num//rounder)*rounder
     newFrameWork = []
 
 with tempfile.TemporaryDirectory(prefix = tempDir) as allocatedTmpDir:
