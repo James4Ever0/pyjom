7de58c7bd33af42c26ecb6c2355e3369a6e848d3
GitJournal <app@gitjournal.io>
2022-09-03 18:23:12 +0800

    [2022-09-03 10:23:08]
    FILE:
    pyjom/medialang/processors/dotProcessor/videoProcessor.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/medialang/processors/dotProcessor/videoProcessor.py
+++ pyjom/medialang/processors/dotProcessor/videoProcessor.py
@@ -55,9 +55,10 @@ def detectTextRegionOverTime(videoPath, start, end, sample_rate = 10):
         detection, recognition = reader.detect(frame)
         if detection == [[]]:
             continue
-        print("frame number:",index)
-        for boundingBox in detection[0]:
-            print(boundingBox)
+        # print("frame number:",index)
+        # for boundingBox in detection[0]:
+        #     print(boundingBox)
+        detection[0].copy()
         # print(detection)
         # breakpoint()
     del reader # can it really free memory?
