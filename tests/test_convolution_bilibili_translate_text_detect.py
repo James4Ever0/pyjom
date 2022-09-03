6e17d6567be9a25b584c222e95d90a2784745d8d
GitJournal <app@gitjournal.io>
2022-09-03 15:51:39 +0800

    [2022-09-03 07:51:36]
    FILE:
    tests/test_convolution_bilibili_translate_text_detect.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/test_convolution_bilibili_translate_text_detect.py
+++ tests/test_convolution_bilibili_translate_text_detect.py
@@ -37,7 +37,7 @@ for intKey in range(minKey, maxKey+1):
     if len(convolutionBoundingBoxSpan)> convolutionSpan:
         convolutionBoundingBoxSpan.pop(0)
     # do your calculation!
-    
+    currentNonOverlappingBoxes = 
     # print(intKey,target)
     # this time we do not care about the text inside.
     # breakpoint()
