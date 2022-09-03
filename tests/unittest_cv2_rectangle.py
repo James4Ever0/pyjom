e6bf504abc575c28a29f87502d29851414919e2e
GitJournal <app@gitjournal.io>
2022-09-03 17:12:14 +0800

    [2022-09-03 09:12:10]
    FILE:
    tests/unittest_cv2_rectangle.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_cv2_rectangle.py
+++ tests/unittest_cv2_rectangle.py
@@ -2,3 +2,10 @@ from test_commons import *
 from pyjom.commons import *
 import cv2
 
+import numpy as np
+
+def getBlackPicture(width, height):
+    blackPicture =  np.zeros((height, width,3), dtype = "uint8") # this is grayscale.
+    return blackPicture
+
+blackPicture = getBlackPicture(500,500)
