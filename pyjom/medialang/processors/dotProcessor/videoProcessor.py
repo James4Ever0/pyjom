21c224eb316ec7d518563f9bedc1104de0c46eb4
GitJournal <app@gitjournal.io>
2022-09-03 20:45:40 +0800

    [2022-09-03 12:45:37]
    FILE:
    pyjom/medialang/processors/dotProcessor/videoProcessor.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/medialang/processors/dotProcessor/videoProcessor.py
+++ pyjom/medialang/processors/dotProcessor/videoProcessor.py
@@ -184,6 +184,7 @@ def ffmpegVideoPreProductionFilter(filepath, start=None, end=None, cachePath=Non
     assert end is not None
     stream = ffmpeg.input(filepath,ss=start, to=end) # from 4 to 10 seconds?
     if preview:
+        deltaRatio
         previewWidth, previewHeight = getVideoPreviewPixels(filepath)
         stream = stream.filter('scale',previewWidth, previewHeight)
     # stream = ffmpeg.hflip(stream)
