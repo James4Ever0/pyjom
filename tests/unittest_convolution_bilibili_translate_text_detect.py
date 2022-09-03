d57ab9646d9b009ddc01d10634fe43cb0a4f05ea
GitJournal <app@gitjournal.io>
2022-09-03 17:29:02 +0800

    [2022-09-03 09:28:57]
    FILE:
    tests/unittest_convolution_bilibili_translate_text_detect.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_convolution_bilibili_translate_text_detect.py
+++ tests/unittest_convolution_bilibili_translate_text_detect.py
@@ -38,7 +38,8 @@ minKey, maxKey = min(mIntKeys), max(mIntKeys)
 from imutils.object_detection import non_max_suppression
 
 
-def getConvBlurredCurrentShot(blurredSpan,)
+def getConvBlurredCurrentShot(blurredSpan, ):
+    # honor the most the latest one. 
 
 convolutionSpan = 5
 
