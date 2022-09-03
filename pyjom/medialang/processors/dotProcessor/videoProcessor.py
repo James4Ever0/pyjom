476b76cfe9749b6092970a12683cd34382909dac
GitJournal <app@gitjournal.io>
2022-09-03 21:06:29 +0800

    [2022-09-03 13:06:26]
    FILE:
    pyjom/medialang/processors/dotProcessor/videoProcessor.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/medialang/processors/dotProcessor/videoProcessor.py
+++ pyjom/medialang/processors/dotProcessor/videoProcessor.py
@@ -145,7 +145,7 @@ def detectTextRegionOverTime(videoPath, start, end, sample_rate = 10, mergeThres
     mSetCandidates = [mRangesDict[key] for key in mKeyMaps]
     # the next step will automatically merge all overlapped candidates.
     finalCats = getContinualNonSympyMergeResult(mSetCandidates)
-    newFinalCats = {:}
+    finalCatsMapped = {mKeyMaps[k]:finalCats[k]}
     return newFinalRectList
 
 
