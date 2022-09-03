6da8af682222fbdf506e24aad1ecaa5afc8c9ec3
GitJournal <app@gitjournal.io>
2022-09-03 12:38:47 +0800

    [2022-09-03 04:38:44]
    FILE:
    pyjom/medialang/processors/dotProcessor/videoProcessor.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/medialang/processors/dotProcessor/videoProcessor.py
+++ pyjom/medialang/processors/dotProcessor/videoProcessor.py
@@ -7,6 +7,7 @@ import ffmpeg
 
 def getVideoFrameIterator(videoPath, start, end, sample_rate):
     cap = cv2.VideoCapture(videoPath)
+    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     success, img = cap.read() # ignore first frame.
     fno = 0
     while success:
