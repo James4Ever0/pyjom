4dea8b09c320f5db29a459f3426ce3082c4e05f9
GitJournal <app@gitjournal.io>
2022-09-03 19:44:59 +0800

    [2022-09-03 11:44:56]
    FILE:
    tests/unittest_iterate_and_merge_alike_text_regions.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_iterate_and_merge_alike_text_regions.py
+++ tests/unittest_iterate_and_merge_alike_text_regions.py
@@ -93,6 +93,8 @@ for item in sample:
     for elem in item:
         for prevElem in prevList:
             if alike(prevElem, elem,10):
-                mAlike = True
-                
+                # mAlike = True
+                elem = prevElem
+                break
+        
     prevList = newItem.copy()
