ae752b83551147f6c7df71514c0d197facf087d3
GitJournal <app@gitjournal.io>
2022-09-03 21:23:12 +0800

    [2022-09-03 13:23:09]
    FILE:
    tests/unittest_mathlib_ranges_continual.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_mathlib_ranges_continual.py
+++ tests/unittest_mathlib_ranges_continual.py
@@ -1,6 +1,6 @@
 from test_commons import *
 from pyjom.mathlib import *
 
-inputList = [[(0,1),(1,1.1) (2,3)], [(0.5,1.5),(1.6,2.5)]]
+inputList = [[(0, 1), (1, 1.1), (2, 3)], [(0.5, 1.5), (1.6, 2.5)]]
 
 mRangesDict = {"sample_%s" % num: [num]}
