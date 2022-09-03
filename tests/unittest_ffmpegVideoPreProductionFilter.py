4d0935732536bc8c681e05eae857a99326dd86a0
GitJournal <app@gitjournal.io>
2022-09-03 13:00:41 +0800

    [2022-09-03 05:00:38]
    FILE:
    tests/unittest_ffmpegVideoPreProductionFilter.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_ffmpegVideoPreProductionFilter.py
+++ tests/unittest_ffmpegVideoPreProductionFilter.py
@@ -45,8 +45,8 @@ with tempfile.TemporaryDirectory(prefix = tempDir) as allocatedTmpDir:
     end = getVideoDuration(videoPath)
 
     if test_text_detector:
-        from pyjom.medialang.processors.dotProcessor import detectTextRegionOverTime
-        detectTextRegionOverTime()
+        from pyjom.medialang.processors.dotProcessor import regions = detectTextRegionOverTime
+        detectTextRegionOverTime(videoPath, start, end)
 
     if test_ffmpeg:
         output = ffmpegVideoPreProductionFilter(videoPath, cachePath = cachePath, start=start, end=end, filters=filters, preview=True) # resolution? make it sufficiently low!
