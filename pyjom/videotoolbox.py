1f192e726b07532ee5680e88f25b329859d8d270
GitJournal <app@gitjournal.io>
2022-09-04 22:55:38 +0800

    [2022-09-04 14:55:33]
    FILE:
    pyjom/videotoolbox.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/videotoolbox.py
+++ pyjom/videotoolbox.py
@@ -310,7 +310,7 @@ def detectStationaryLogoOverTime(filepath,start,end,sample_size=60):
     # you can do this later, will you?
     gx, gy, gxlist, gylist = estimate_watermark_imgSet(imageSet)
     print(gx.shape, gy.shape)
-    breakpoint()
+    breakpoint() # nothing here! fuck.
     # print(len(imageSet))
     # cropped_gx, cropped_gy, watermark_location = crop_watermark(gx, gy,location=True)
 
