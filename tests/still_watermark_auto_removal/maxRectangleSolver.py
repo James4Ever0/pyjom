e2f72c3ddcd9fb2951a7378cc4a883c037173188
GitJournal <app@gitjournal.io>
2022-09-04 19:58:06 +0800

    [2022-09-04 11:58:02]
    FILE:
    tests/still_watermark_auto_removal/maxRectangleSolver.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/still_watermark_auto_removal/maxRectangleSolver.py
+++ tests/still_watermark_auto_removal/maxRectangleSolver.py
@@ -13,7 +13,8 @@ yValid = [0, height]
 mRects = []
 
 def checkContains(rect, point):
-    xPoints = []
+    xPoints = [p[0] for p in rect]
+    yPoints = [p[1] for p in rect]
 
 def checkOverlapAsymmetric(rect0, rect1):
     for point in rect0:
