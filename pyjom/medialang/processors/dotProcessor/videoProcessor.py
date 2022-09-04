aad99c15ce62e085f7dc1676a51ff1146233f4a8
GitJournal <app@gitjournal.io>
2022-09-04 16:10:41 +0800

    [2022-09-04 08:10:38]
    FILE:
    pyjom/medialang/processors/dotProcessor/videoProcessor.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/medialang/processors/dotProcessor/videoProcessor.py
+++ pyjom/medialang/processors/dotProcessor/videoProcessor.py
@@ -314,7 +314,7 @@ def ffmpegVideoPreProductionFilter(
                     commandParams = parse.parse(
                         "delogo_{x:d}_{y:d}_{w:d}_{h:d}", renderCommand
                     )
-                    pri
+                    print(defaultWidth, defaultHeight)
                     mX, mY, mW, mH = commandParams['x'], commandParams['y'], commandParams['w'], commandParams['h']
                     mX1, mY1 = mX+mW, mY+mH
                     if mX1>defaultWidth or mY1>defaultHeight: # opecv to be blamed?
