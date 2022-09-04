ff071ad960fb12e5cab6778b6ee8227d64e9a8f6
GitJournal <app@gitjournal.io>
2022-09-04 23:13:13 +0800

    [2022-09-04 15:13:09]
    FILE:
    tests/unittest_video_sampler.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_video_sampler.py
+++ tests/unittest_video_sampler.py
@@ -3,7 +3,7 @@ from pyjom.videotoolbox import getVideoFrameSampler
 
 videoPath = "/root/Desktop/works/pyjom/samples/video/LkS8UkiLL.mp4"
 
-imageSet = getVideoFrameSampler(videoPath, 0,5,60,iterate=False)
+imageSet = getVideoFrameSampler(videoPath, 0,5,60)
 print(imageSet)
 print(type(imageSet))
 breakpoint()
