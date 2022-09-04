dbe8f592400e650da4cb16802646024b87a79cc6
GitJournal <app@gitjournal.io>
2022-09-04 19:43:24 +0800

    [2022-09-04 11:43:20]
    FILE:
    tests/still_watermark_auto_removal/maxRectangleSolver.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/still_watermark_auto_removal/maxRectangleSolver.py
+++ tests/still_watermark_auto_removal/maxRectangleSolver.py
@@ -5,9 +5,15 @@ data = json.loads(open("test_special.json",'r').read())
 canvas = data['canvas']
 rectangles = data['rectangles']
 
+
+
 width, height = canvas
 
 xValid = [0, width]
 yValid = [0, height]
 
 for x,y, mWidth, mHeight in rectangles:
+    xValid.append(x)
+    xValid.append(x+mWidth)
+    yValid.append(y)
+    yValid.append(y+mHeight)
