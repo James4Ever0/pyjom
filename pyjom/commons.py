8ec4a20e34eb56264f20465b25d96e66c2c5972b
GitJournal <app@gitjournal.io>
2022-09-04 17:48:06 +0800

    [2022-09-04 09:48:02]
    FILE:
    pyjom/commons.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/commons.py
+++ pyjom/commons.py
@@ -21,14 +21,6 @@ import random
 # site_path = pathlib.Path([x for x in site.getsitepackages() if "site-packages" in x][0])
 os.environ["USE_NVIDIA_OPENCV"] = "yes"
 if os.environ["USE_NVIDIA_OPENCV"] == "yes":
-    site_path = pathlib.Path("/usr/local/lib/python3.9/site-packages")
-    cv2_libs_dir = site_path / 'cv2' / f'python-{sys.version_info.major}.{sys.version_info.minor}'
-    print(cv2_libs_dir)
-    cv2_libs = sorted(cv2_libs_dir.glob("*.so"))
-    if len(cv2_libs) == 1:
-        print("INSERTING:",cv2_libs[0].parent)
-        sys.path.insert(1, str(cv2_libs[0].parent))
-
 
 mimetypes.init()
 
