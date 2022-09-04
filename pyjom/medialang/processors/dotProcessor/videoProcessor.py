12065460580fe61e0d6619e12ff5c4bc810384b6
GitJournal <app@gitjournal.io>
2022-09-04 16:53:31 +0800

    [2022-09-04 08:53:27]
    FILE:
    pyjom/medialang/processors/dotProcessor/videoProcessor.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/medialang/processors/dotProcessor/videoProcessor.py
+++ pyjom/medialang/processors/dotProcessor/videoProcessor.py
@@ -276,6 +276,7 @@ def ffmpegVideoPreProductionFilter(
         # pass
     if "logoRemoval" in filters:
         detectStationaryLogoOverTime(filepath, start, end) # output logo mask. or not.
+        # estimate the shape with multiple rectangles? packing algorithm?
         # pass
     if "pipCrop" in filters:
         # remember: if pip crop makes any of our logoRemoval or textRemoval filters invalid, we do not execute them.
