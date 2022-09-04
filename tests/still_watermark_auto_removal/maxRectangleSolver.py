66aa1cce25e38db7e3e2654504183837967f24a9
GitJournal <app@gitjournal.io>
2022-09-04 19:44:45 +0800

    [2022-09-04 11:44:42]
    FILE:
    tests/still_watermark_auto_removal/maxRectangleSolver.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/still_watermark_auto_removal/maxRectangleSolver.py
+++ tests/still_watermark_auto_removal/maxRectangleSolver.py
@@ -17,4 +17,5 @@ for x,y, mWidth, mHeight in rectangles:
     xValid.append(x+mWidth)
     yValid.append(y)
     yValid.append(y+mHeight)
-    p0, p1, p2, p3 = (x,y), (x+mWidth,y),(x+mWidth, y+mHeight)
+    p0, p1, p2, p3 = (x,y), (x+mWidth,y),(x+mWidth, y+mHeight), (x, y+mHeight)
+    
