a4c2e4dae7e4ce1260d2e7ad4223ddf5769a3826
GitJournal <app@gitjournal.io>
2022-09-04 16:34:47 +0800

    [2022-09-04 08:34:44]
    FILE:
    pyjom/medialang/processors/dotProcessor/videoProcessor.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/medialang/processors/dotProcessor/videoProcessor.py
+++ pyjom/medialang/processors/dotProcessor/videoProcessor.py
@@ -321,13 +321,13 @@ def ffmpegVideoPreProductionFilter(
                     commandParams = parse.parse(
                         "delogo_{x:d}_{y:d}_{w:d}_{h:d}", renderCommand
                     )
-                    print(defaultWidth, defaultHeight)
-                    mX, mY, mW, mH = commandParams['x'], commandParams['y'], commandParams['w'], commandParams['h']
-                    mX1, mY1 = mX+mW, mY+mH
-                    if mX1>defaultWidth or mY1>defaultHeight: # opecv to be blamed?
-                        print("ERROR:")
-                        print(mX1,defaultWidth,mY1,defaultHeight)
-                        breakpoint()
+                    # print(defaultWidth, defaultHeight)
+                    # mX, mY, mW, mH = commandParams['x'], commandParams['y'], commandParams['w'], commandParams['h']
+                    # mX1, mY1 = mX+mW, mY+mH
+                    # if mX1>defaultWidth or mY1>defaultHeight: # opecv to be blamed?
+                    #     print("ERROR:")
+                    #     print(mX1,defaultWidth,mY1,defaultHeight)
+                    #     breakpoint()
                     # we also need to consider if this is necessary.
                     stream = delogoFilter(stream, commandParams)
 
