d612dc7dbc65b40e0adc25fdb8e0e2c967d3fd3f
GitJournal <app@gitjournal.io>
2022-09-04 15:25:09 +0800

    [2022-09-04 07:25:05]
    FILE:
    pyjom/medialang/processors/dotProcessor/videoProcessor.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/medialang/processors/dotProcessor/videoProcessor.py
+++ pyjom/medialang/processors/dotProcessor/videoProcessor.py
@@ -291,7 +291,7 @@ def ffmpegVideoPreProductionFilter(
 
     for renderCommandString, commandTimeSpan in renderList:
         mStart, mEnd = commandTimeSpan
-        print()
+        print("CLIP TIMESPAN:", mStart, 
         stream = ffmpeg.input(filepath, ss=mStart, to=mEnd).video # no audio? seriously?
         if renderCommandString == "empty":
             pass  # do not continue since maybe we have preview filter below?
