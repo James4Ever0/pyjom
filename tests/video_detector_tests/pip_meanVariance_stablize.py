46cfc90c46131078b3d2b54a38ac06a4983943c2
GitJournal <app@gitjournal.io>
2022-09-05 13:49:11 +0800

    [2022-09-05 05:49:08]
    FILE:
    tests/video_detector_tests/pip_meanVariance_stablize.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/video_detector_tests/pip_meanVariance_stablize.py
+++ tests/video_detector_tests/pip_meanVariance_stablize.py
@@ -46,9 +46,9 @@ xLeftPointsFilteredDiff3 = np.diff(xLeftPointsFilteredDiff)
 
 # xLeftPointsFilteredDiff3Filtered = Kalman1D(xLeftPointsFilteredDiff3)
 derivative3Threshold = 3
-xLeftPointsSignal = (abs(xLeftPointsFilteredDiff3) < derivative3Threshold).astype(np.uint8)
+xLeftPointsSignal = (abs(xLeftPointsFilteredDiff3) < derivative3Threshold).astype(np.uint8).tolist()
 
-xLeftPointsSignalFiltered = 
+xLeftPointsSignalFiltered = extract_span()
 
 import matplotlib.pyplot as plt
 
