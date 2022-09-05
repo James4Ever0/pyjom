005f8fce703ee2ab5ae569679adc7a18edad86f4
GitJournal <app@gitjournal.io>
2022-09-05 23:35:04 +0800

    [2022-09-05 15:35:01]
    FILE:
    pyjom/medialang/processors/dotProcessor/videoProcessor.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/medialang/processors/dotProcessor/videoProcessor.py
+++ pyjom/medialang/processors/dotProcessor/videoProcessor.py
@@ -114,6 +114,7 @@ def ffmpegVideoPreProductionFilter(
         mStart = max(start, mStart)
         mEnd = min(videoDuration, end)
         clipDuration = mEnd-mStart
+        if clipDuration <=0: continue # this clip is shit.
         if mStart <0 or mEnd 
         # print("CLIP TIMESPAN:", mStart, mEnd)
         stream = ffmpeg.input(
