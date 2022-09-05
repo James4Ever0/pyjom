be63669318aa11c659ab10c3a8c6825e09cc561d
GitJournal <app@gitjournal.io>
2022-09-05 11:32:41 +0800

    [2022-09-05 03:32:37]
    FILE:
    tests/video_detector_tests/rectangle_test.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/video_detector_tests/rectangle_test.py
+++ tests/video_detector_tests/rectangle_test.py
@@ -1,19 +1,20 @@
 # motion detectors are used to track objects. though you may want to separate objects with it.
+import pybgs as bgs
+import cv2
 import numpy as np
 
 import pathlib
 import sys
 
-    site_path = pathlib.Path("/usr/local/lib/python3.9/site-packages")
-    cv2_libs_dir = site_path / 'cv2' / f'python-{sys.version_info.major}.{sys.version_info.minor}'
-    print(cv2_libs_dir)
-    cv2_libs = sorted(cv2_libs_dir.glob("*.so"))
-    if len(cv2_libs) == 1:
-        print("INSERTING:",cv2_libs[0].parent)
-        sys.path.insert(1, str(cv2_libs[0].parent))
+site_path = pathlib.Path("/usr/local/lib/python3.9/site-packages")
+cv2_libs_dir = site_path / 'cv2' / \
+    f'python-{sys.version_info.major}.{sys.version_info.minor}'
+print(cv2_libs_dir)
+cv2_libs = sorted(cv2_libs_dir.glob("*.so"))
+if len(cv2_libs) == 1:
+    print("INSERTING:", cv2_libs[0].parent)
+    sys.path.insert(1, str(cv2_libs[0].parent))
 
-import cv2
-import pybgs as bgs
 
 # suspect by static image analysis, and then create bounding box over the thing.
 # check image quality.
@@ -23,54 +24,57 @@ import pybgs as bgs
 # also donga have strange things you may dislike, e.g.: when only part of the image changes.
 
 # algorithm = bgs.FrameDifference() # this is not stable since we have more boundaries. shall we group things?
-# can we use something else? 
+# can we use something else?
 algorithm = bgs.WeightedMovingVariance()
-video_file = "../../samples/video/LiEIfnsvn.mp4" # this one with cropped boundaries. 
+# this one with cropped boundaries.
+video_file = "../../samples/video/LiEIfnsvn.mp4"
 
 capture = cv2.VideoCapture(video_file)
 while not capture.isOpened():
-  capture = cv2.VideoCapture(video_file) 
-  cv2.waitKey(1000)
-  print("Wait for the header")
+    capture = cv2.VideoCapture(video_file)
+    cv2.waitKey(1000)
+    print("Wait for the header")
 
 pos_frame = capture.get(1)
 while True:
-  flag, frame = capture.read()
-  
-  if flag:
-    pos_frame = capture.get(1)
-    img_output = algorithm.apply(frame)
-    img_bgmodel = algorithm.getBackgroundModel()
-    _, contours = cv2.findContours(img_output, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    # maybe you should merge all active areas.
-    if contours is not None:
-            # continue
-        counted = False
-        for contour in contours:
-            [x, y, w, h] = cv2.boundingRect(img_output)
-            if not counted:
-                min_x, min_y = x,y
-                max_x, max_y = x+w, y+h
-                counted = True
-            else:
-                min_x = min(min_x, x)
-                min_y = min(min_y, y)
-                max_x = max(max_x, x+w)
-                max_y = max(max_y, y+h)
-                # only create one single bounding box.
-        cv2.rectangle(frame,(min_x,min_y),(max_x,max_y), (255,0,0), 2)
-        # how to stablize this shit?
-    cv2.imshow('video', frame)
-    
-    cv2.imshow('img_output', img_output)
-    cv2.imshow('img_bgmodel', img_bgmodel)
+    flag, frame = capture.read()
 
-  else:
-    cv2.waitKey(1000)
-    break
-  
-  if 0xFF & cv2.waitKey(10) == 27:
-    break
-  
+    if flag:
+        pos_frame = capture.get(1)
+        img_output = algorithm.apply(frame)
+        img_bgmodel = algorithm.getBackgroundModel()
+        _, contours = cv2.findContours(
+            img_output, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        # maybe you should merge all active areas.
+        if contours is not None:
+            # continue
+            counted = False
+            for contour in contours:
+                [x, y, w, h] = cv2.boundingRect(img_output)
+                if not counted:
+                    min_x, min_y = x, y
+                    max_x, max_y = x+w, y+h
+                    counted = True
+                else:
+                    min_x = min(min_x, x)
+                    min_y = min(min_y, y)
+                    max_x = max(max_x, x+w)
+                    max_y = max(max_y, y+h)
+                    # only create one single bounding box.
+            cv2.rectangle(frame, (min_x, min_y),
+                          (max_x, max_y), (255, 0, 0), 2)
+            # how to stablize this shit?
+        cv2.imshow('video', frame)
+
+        cv2.imshow('img_output', img_output)
+        cv2.imshow('img_bgmodel', img_bgmodel)
+
+    else:
+        cv2.waitKey(1000)
+        break
+
+    if 0xFF & cv2.waitKey(10) == 27:
+        break
+
 
 cv2.destroyAllWindows()
