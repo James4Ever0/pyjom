15f6f0b3d98326f226802f243f6476b52da27449
GitJournal <app@gitjournal.io>
2022-09-05 23:23:12 +0800

    [2022-09-05 15:23:09]
    FILE:
    pyjom/videotoolbox.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/videotoolbox.py
+++ pyjom/videotoolbox.py
@@ -459,5 +459,7 @@ def detectPipRegionOverTime(videoPath, start, end, method = "skim", algo='frame_
         sample_rate = max(1,estimated_sample_rate)
     iterator = getVideoFrameIterator(videoPath, start, end, sample_rate=sample_rate)
     for frame in iterator:
+        [x, y, w, h] = cv2.boundingRect(img_output) # wtf is this?
+        area = w*h
         img_output = algorithm.apply(frame)
         pipFrames.append([(min_x, min_y), (max_x, max_y)].copy())
