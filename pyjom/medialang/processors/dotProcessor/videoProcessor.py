92470071cd9c34ea51838318821a07b1b3fb6aec
GitJournal <app@gitjournal.io>
2022-09-05 10:53:35 +0800

    [2022-09-05 02:53:32]
    FILE:
    pyjom/medialang/processors/dotProcessor/videoProcessor.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/medialang/processors/dotProcessor/videoProcessor.py
+++ pyjom/medialang/processors/dotProcessor/videoProcessor.py
@@ -150,7 +150,7 @@ def ffmpegVideoPreProductionFilter(
                         # shall we warn you?
                         # print("SOMEHOW DELOGO IS NOT WORKING PROPERLY")
                         # breakpoint()
-                        # maybe it's not out of bounds
+                        # maybe it's not because of out of bounds error
                         print("_" * 30)
                         print("ABNORMAL DELOGO FILTER PARAMS:", commandParams)
                         print(
