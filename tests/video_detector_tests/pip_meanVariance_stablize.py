6400bcdad4e7a31bd138167ccaa29c4f9437288d
GitJournal <app@gitjournal.io>
2022-09-05 14:39:59 +0800

    [2022-09-05 06:39:55]
    FILE:
    tests/video_detector_tests/pip_meanVariance_stablize.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/video_detector_tests/pip_meanVariance_stablize.py
+++ tests/video_detector_tests/pip_meanVariance_stablize.py
@@ -125,8 +125,8 @@ else:
                     newTargetSequential[index+1][0] = commandString
     # bring this sequential into dict again.
     answer = sequentialToMergedRanges(newTargetSequential)
-    print("_"*30, "ANSWER")
-
+    print("_"*30, "ANSWER","_"*30)
+    for elem in ans
                     
 
 import matplotlib.pyplot as plt
