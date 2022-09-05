a81357b8f1e33ba9573725b2e79a6c5bd8ca10ac
GitJournal <app@gitjournal.io>
2022-09-05 19:57:13 +0800

    [2022-09-05 11:57:09]
    FILE:
    tests/video_detector_tests/pip_meanVariance_stablize.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/video_detector_tests/pip_meanVariance_stablize.py
+++ tests/video_detector_tests/pip_meanVariance_stablize.py
@@ -2,6 +2,7 @@
 from mathlib import *
 # from ...pyjom.mathlib import sequentialToMergedRanges
 
+def 
 
 def kalmanStablePipRegionExporter(data, defaultWidth, defaultHeight):
     defaultWidth, defaultHeight = int(defaultWidth), int(defaultHeight)
