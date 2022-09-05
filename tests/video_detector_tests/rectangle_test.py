9d3a114d6a3dc73f295b95bd720c76f8ab946c63
GitJournal <app@gitjournal.io>
2022-09-05 11:30:52 +0800

    [2022-09-05 03:30:49]
    FILE:
    tests/video_detector_tests/rectangle_test.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/video_detector_tests/rectangle_test.py
+++ tests/video_detector_tests/rectangle_test.py
@@ -10,9 +10,9 @@ import pybgs as bgs
 
 # also donga have strange things you may dislike, e.g.: when only part of the image changes.
 
-algorithm = bgs.FrameDifference() # this is not stable since we have more boundaries. shall we group things?
+# algorithm = bgs.FrameDifference() # this is not stable since we have more boundaries. shall we group things?
 # can we use something else? 
-algorithm = bgs.WeightedMovingMean
+algorithm = bgs.WeightedMovingVariance()
 video_file = "../../samples/video/LiEIfnsvn.mp4" # this one with cropped boundaries. 
 
 capture = cv2.VideoCapture(video_file)
