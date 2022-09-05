a81ce9eb42bb2d065bde0e99e874109a36c4f1b3
GitJournal <app@gitjournal.io>
2022-09-05 16:48:23 +0800

    [2022-09-05 08:48:19]
    FILE:
    tests/video_detector_tests/pip_meanVariance_stablize.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/video_detector_tests/pip_meanVariance_stablize.py
+++ tests/video_detector_tests/pip_meanVariance_stablize.py
@@ -182,7 +182,7 @@ else:
             currentItemDuration = getSpanDuration(currentItem[1])
             nextItemCommand = nextItem[0]
             nextItemDuration = getSpanDuration(nextItem[1])
-            if currentItem.duration
+            if currentItemDuration < 
 
 
 # import matplotlib.pyplot as plt
