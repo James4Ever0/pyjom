4c8dcf0bc18781d5eb72b4a15130461565787ffc
GitJournal <app@gitjournal.io>
2022-09-05 20:09:16 +0800

    [2022-09-05 12:09:12]
    FILE:
    tests/video_detector_tests/pip_meanVariance_stablize.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/video_detector_tests/pip_meanVariance_stablize.py
+++ tests/video_detector_tests/pip_meanVariance_stablize.py
@@ -23,7 +23,7 @@ def kalmanStablePipRegionExporter(data, defaultWidth, defaultHeight):
     def listToSequentialRanges(mList,label):
         resultDict = {}
         for index, elem in enumerate(mList):
-            resultDict.update({"":[(index, index+1)])
+            resultDict.update({"":resultDict.get(mKey,[])+[(index, index+1)])
     xLeftPoints = getMerged(data[:,0,0])
     yLeftPoints = getMerged(data[:,0,1])
     xRightPoints = getMerged(data[:,1,0])
