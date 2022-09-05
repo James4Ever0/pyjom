b9fddd519a4f0c02692fa06b45a7fc649d25599e
GitJournal <app@gitjournal.io>
2022-09-05 15:29:02 +0800

    [2022-09-05 07:28:58]
    FILE:
    tests/video_detector_tests/rectangle_test.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/video_detector_tests/rectangle_test.py
+++ tests/video_detector_tests/rectangle_test.py
@@ -87,5 +87,5 @@ while True:
 # we process this shit elsewhere.
 
 with open("pip_meanVariance.json", 'w') as f:
-    f.write(json.dumps({"data":pipFrames, "width", "height"))
+    f.write(json.dumps({"data":pipFrames, "width":defaultWidth, "height":defaultHeight))
 print("DATA DUMPED")
