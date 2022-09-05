b810ae4404ff1cac652ccfeb4e518213e07cacc6
GitJournal <app@gitjournal.io>
2022-09-05 16:51:33 +0800

    [2022-09-05 08:51:29]
    FILE:
    tests/video_detector_tests/pip_meanVariance_stablize.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/video_detector_tests/pip_meanVariance_stablize.py
+++ tests/video_detector_tests/pip_meanVariance_stablize.py
@@ -188,9 +188,9 @@ else:
                     commandDictSequential[i][0] = nextItemCommand
                     noAlter=False
             if nextItemDuration < itemDurationThreshold:
-            if nextItemCommand != currentItemCommand:
-                commandDictSequential[i][0] = nextItemCommand
-                noAlter=False
+                if nextItemCommand != currentItemCommand:
+                    commandDictSequential[i+1][0] = currentItemCommand
+                    noAlter=False
 
 
 # import matplotlib.pyplot as plt
