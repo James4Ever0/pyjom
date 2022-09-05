9c040b8f91255835ddd81ac67988066b6913f81d
GitJournal <app@gitjournal.io>
2022-09-05 17:12:26 +0800

    [2022-09-05 09:12:22]
    FILE:
    tests/video_detector_tests/pip_meanVariance_stablize.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/video_detector_tests/pip_meanVariance_stablize.py
+++ tests/video_detector_tests/pip_meanVariance_stablize.py
@@ -212,6 +212,7 @@ else:
         commandArguments = parse.parse(formatString, key)
         x,y,w,h = commandArguments['xleft'], commandArguments['yleft'], commandArguments['xright']-commandArguments['xleft'], commandArguments['yright']-commandArguments['yleft']
         cropCommand = "crop_{}_{}_{}_{}".format(x,y,w,h)
+        print(cropCommand)
         print(elem)
         # the parser shall be in x,y,w,h with keywords.
         # we might want to parse the command string and reengineer this shit.
