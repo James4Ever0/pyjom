ca4b87e1a4dc79c962ed777e946d487709f97c9f
GitJournal <app@gitjournal.io>
2022-09-05 20:59:06 +0800

    [2022-09-05 12:59:02]
    FILE:
    tests/video_detector_tests/pip_meanVariance_stablize.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/video_detector_tests/pip_meanVariance_stablize.py
+++ tests/video_detector_tests/pip_meanVariance_stablize.py
@@ -33,11 +33,11 @@ def sampledStablePipRegionExporter(data, defaultWidth, defaultHeight):
         mArray = get1DArrayEMA(mArray)
         mArray = getAlikeValueMerged(mArray, threshold=threshold)
         return listToRangedDictWithLabel(mArray, label)
-
-    xLeftPoints = pointsToRangedDictWithLabel(data[:, 0, 0], "xleft")
-    yLeftPoints = pointsToRangedDictWithLabel(data[:, 0, 1], "yleft")
-    xRightPoints = pointsToRangedDictWithLabel(data[:, 1, 0], "xright")
-    yRightPoints = pointsToRangedDictWithLabel(data[:, 1, 1], "yright")
+    threshold = 
+    xLeftPoints = pointsToRangedDictWithLabel(data[:, 0, 0], "xleft", threshold = threshold)
+    yLeftPoints = pointsToRangedDictWithLabel(data[:, 0, 1], "yleft", threshold = threshold)
+    xRightPoints = pointsToRangedDictWithLabel(data[:, 1, 0], "xright", threshold = threshold)
+    yRightPoints = pointsToRangedDictWithLabel(data[:, 1, 1], "yright", threshold = threshold)
 
     commandDict = {}
     for mDict in [xLeftPoints, yLeftPoints, xRightPoints, yRightPoints]:
