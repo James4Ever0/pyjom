c3a1aed0fef446414236f9fbac1bb2d2b353ebcb
GitJournal <app@gitjournal.io>
2022-09-05 20:03:37 +0800

    [2022-09-05 12:03:33]
    FILE:
    tests/video_detector_tests/pip_meanVariance_stablize.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/video_detector_tests/pip_meanVariance_stablize.py
+++ tests/video_detector_tests/pip_meanVariance_stablize.py
@@ -13,7 +13,11 @@ def kalmanStablePipRegionExporter(data, defaultWidth, defaultHeight):
     import numpy as np
 
     data = np.array(data)
-    from pykalman import KalmanFilter
+    xLeftPoints = data[:,0,0]
+    yLeftPoints = data[:,0,1]
+    xRightPoints = data[:,1,0]
+    yRightPoints = data[:,1,1]
+
 
     def Kalman1D(observations,damping=0.2):
         # To return the smoothed time series data
