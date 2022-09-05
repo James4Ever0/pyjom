42d162c9e2830f4ca0d11ebaad7b4ba26bba8fa8
GitJournal <app@gitjournal.io>
2022-09-05 11:06:28 +0800

    [2022-09-05 03:06:25]
    FILE:
    pyjom/videotoolbox.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/videotoolbox.py
+++ pyjom/videotoolbox.py
@@ -428,4 +428,4 @@ def detectStationaryLogoOverTime(filepath,start,end,sample_size=60):
         # breakpoint()
         return {delogoCommandSet: [(start, end)]}
     
-def detectPipRegionOverTime(videoPath, start, e)
+def detectPipRegionOverTime(videoPath, start, end):
