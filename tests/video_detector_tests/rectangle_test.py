32b6de4fd22bfdb4c017811e73636c5045397f7b
GitJournal <app@gitjournal.io>
2022-09-05 20:16:03 +0800

    [2022-09-05 12:16:00]
    FILE:
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/video_detector_tests/rectangle_test.py
+++ tests/video_detector_tests/rectangle_test.py
@@ -61,7 +61,7 @@ for index in progressbar.progressbar(range(total_frames)):
         pos_frame = capture.get(1)
         img_output = algorithm.apply(frame)
         imgThresh = img_output
-        # imgMorph = cv2.GaussianBlur(img_output, (3,3), 0)
+        imgMorph = cv2.GaussianBlur(img_output, (3,3), 0)
         # _,imgThresh = cv2.threshold(imgMorph, 1, 255, cv2.THRESH_BINARY)
         img_bgmodel = algorithm.getBackgroundModel()
         _, contours = cv2.findContours(
