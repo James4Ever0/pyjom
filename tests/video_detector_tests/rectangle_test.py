786764024448e7194d86ace0c1591b67ceade8a2
GitJournal <app@gitjournal.io>
2022-09-05 11:52:06 +0800

    [2022-09-05 03:52:03]
    FILE:
    tests/video_detector_tests/rectangle_test.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/video_detector_tests/rectangle_test.py
+++ tests/video_detector_tests/rectangle_test.py
@@ -32,7 +32,7 @@ video_file = "../../samples/video/LiEIfnsvn.mp4"
 # denoising, moving average, sampler and  similar merge.
 # moving average span: -20 frame to +20 frame
 
-# 选区间之内相似的最多的那种
+# denoising: 选区间之内相似的最多的那种
 
 capture = cv2.VideoCapture(video_file)
 while not capture.isOpened():
