edb93c4654d42a917f923be4b8e687d8ae2d40b0
GitJournal <app@gitjournal.io>
2022-09-05 16:44:16 +0800

    [2022-09-05 08:44:12]
    FILE:
    tests/video_detector_tests/pip_meanVariance_stablize.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/video_detector_tests/pip_meanVariance_stablize.py
+++ tests/video_detector_tests/pip_meanVariance_stablize.py
@@ -172,6 +172,7 @@ else:
     commandDictSequential = mergedRangesToSequential(commandDict)
     for i in range(len(commandDictSequential)-1):
         currentItem = commandDictSequential[i]
+        nextItem = commandDictSequential[]
 
 # import matplotlib.pyplot as plt
 
