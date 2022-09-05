5cdc737a15344b20e3578d681f301c974c60c207
GitJournal <app@gitjournal.io>
2022-09-05 13:11:36 +0800

    [2022-09-05 05:11:33]
    FILE:
    tests/video_detector_tests/pip_meanVariance_stablize.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/video_detector_tests/pip_meanVariance_stablize.py
+++ tests/video_detector_tests/pip_meanVariance_stablize.py
@@ -49,7 +49,7 @@ def extract_span(mlist, target=0):
 xLeftPointsSignalList = xLeftPointsSignal.tolist()
 print(xLeftPointsSignal.shape)
 # print(xLeftPointsSignalList)
-spanLengthMinThreshold = 10
+spanLengthMinThreshold = 30
 kalmanMaxSlope = 0.10
 xLeftSpans = extract_span(xLeftPointsSignalList, target=1)
 
