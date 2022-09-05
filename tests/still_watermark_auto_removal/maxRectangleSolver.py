3392290145f36837c6b48b3d8bdb60ccdc2ba1e4
GitJournal <app@gitjournal.io>
2022-09-05 09:57:39 +0800

    [2022-09-05 01:57:35]
    FILE:
    tests/still_watermark_auto_removal/maxRectangleSolver.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/still_watermark_auto_removal/maxRectangleSolver.py
+++ tests/still_watermark_auto_removal/maxRectangleSolver.py
@@ -40,9 +40,10 @@ def getRectDiagonalPoints(rect):
     yPoints = [p[1] for p in rect]
     maxX, minX = max(xPoints), min(xPoints)
     maxY, minY = max(yPoints), min(yPoints)
-    p0, p1 = D2Point(minX, minY), D2Point)
+    p0, p1 = D2Point(minX, minY), D2Point(maxX, maxY)
+    return p0, p1
+
 def do_overlap(l1, r1, l2, r2):
-     
     # if rectangle has area 0, no overlap
     if l1.x == r1.x or l1.y == r1.y or r2.x == l2.x or l2.y == r2.y:
         return False
