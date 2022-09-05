aa25b2d1840c1c7c5ab814ac875bf4ef24516db1
GitJournal <app@gitjournal.io>
2022-09-05 10:37:33 +0800

    [2022-09-05 02:37:31]
    FILE:
    pyjom/medialang/processors/dotProcessor/videoProcessor.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/medialang/processors/dotProcessor/videoProcessor.py
+++ pyjom/medialang/processors/dotProcessor/videoProcessor.py
@@ -145,7 +145,7 @@ def ffmpegVideoPreProductionFilter(
                         print("VALID BOUNDARIES:", defaultWidth, defaultHeight)
                         continue
                     else:
-                        commandParams = {'x',mX, 'y':mY, 'w', mW, mH}
+                        commandParams = {'x':mX, 'y':mY, 'w': mW, 'h':mH}
                     # mX1, mY1 = mX+mW, mY+mH
                     # if mX1>defaultWidth or mY1>defaultHeight: # opecv to be blamed?
                     #     print("DELOGO ERROR:")
