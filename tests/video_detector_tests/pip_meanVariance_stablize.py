ce01d4f6000ca6599aadd42ab5179b8e5de6a95b
GitJournal <app@gitjournal.io>
2022-09-05 20:37:00 +0800

    [2022-09-05 12:36:56]
    FILE:
    tests/video_detector_tests/pip_meanVariance_stablize.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/video_detector_tests/pip_meanVariance_stablize.py
+++ tests/video_detector_tests/pip_meanVariance_stablize.py
@@ -58,12 +58,12 @@ def sampledStablePipRegionExporter(data, defaultWidth, defaultHeight):
             nextItemCommand = nextItem[0]
             nextItemDuration = getSpanDuration(nextItem[1])
             if currentItemDuration < itemDurationThreshold:
-                if nextItemCommand != currentItemCommand:
+                if nextItemCommand != currentItemCommand and currentItemDuration >= itemDurationThreshold:
                     # print("HERE0",i, currentItemCommand, nextItemCommand)
                     commandDictSequential[i][0] = nextItemCommand
                     # noAlter=False
             if nextItemDuration < itemDurationThreshold:
-                if nextItemCommand != currentItemCommand and currentItemDuration >= itemDurationThreshold:
+                if nextItemCommand != currentItemCommand :
                     # print("HERE1",i, currentItemCommand, nextItemCommand)
                     commandDictSequential[i + 1][0] = currentItemCommand
                     # noAlter=False
