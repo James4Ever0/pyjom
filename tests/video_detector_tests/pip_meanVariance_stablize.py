2c0214ca5f778b5ddda5d5fb61186f013f87478d
GitJournal <app@gitjournal.io>
2022-09-05 20:42:50 +0800

    [2022-09-05 12:42:46]
    FILE:
    tests/video_detector_tests/pip_meanVariance_stablize.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/video_detector_tests/pip_meanVariance_stablize.py
+++ tests/video_detector_tests/pip_meanVariance_stablize.py
@@ -370,7 +370,7 @@ if __name__ == "__main__":
     if objective == "continual":
         dataDict = json.loads(open("pip_meanVariance.json", "r").read())
     elif objective == 'continual_najie':
-        dataDict = json.loads(open("pip_meanVariance.json", "r").read())
+        dataDict = json.loads(open("pip_meanVarianceSisterNa.json", "r").read())
     elif objective == "discrete":
         dataDict = json.loads(open("pip_discrete_meanVariance.json", "r").read())
     else:
