cf597a9f6dddb6c10ad95c804834a3da8042e794
GitJournal <app@gitjournal.io>
2022-09-05 22:19:31 +0800

    [2022-09-05 14:19:28]
    FILE:
    pyjom/videotoolbox.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/videotoolbox.py
+++ pyjom/videotoolbox.py
@@ -445,8 +445,6 @@ def detectPipRegionOverTime(videoPath, start, end, method = "skim", algo='frame_
         iterator = getVideoFrameIterator(videoPath,start,end)
         for frame in iterator:
             img_output = algorithm.apply(frame)
-            _, contours = cv2.findContours(
-            imgThresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
             if contours is not None:
             # continue
                 counted = False
