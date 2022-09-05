51e518e8e4761f71a8b7f4d326001fd5c46ec555
GitJournal <app@gitjournal.io>
2022-09-05 14:31:27 +0800

    [2022-09-05 06:31:24]
    FILE:
    tests/video_detector_tests/pip_meanVariance_stablize.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/video_detector_tests/pip_meanVariance_stablize.py
+++ tests/video_detector_tests/pip_meanVariance_stablize.py
@@ -105,6 +105,7 @@ if (newTargetSequentialUpdated) == 1:
         # the whole thing is empty now. no need to investigate.
         print("NO STATIC PIP FOUND HERE.")
 else:
+    newTargetSequential
     for index in range(len(newTargetSequential)-1):
         elem = newTargetSequential[index]
         commandString, commandTimeSpan = elem
