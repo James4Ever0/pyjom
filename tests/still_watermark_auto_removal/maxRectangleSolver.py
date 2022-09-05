f3f7f19844268114412ca01842a6fc3ec282a622
GitJournal <app@gitjournal.io>
2022-09-05 09:58:19 +0800

    [2022-09-05 01:58:16]
    FILE:
    tests/still_watermark_auto_removal/maxRectangleSolver.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/still_watermark_auto_removal/maxRectangleSolver.py
+++ tests/still_watermark_auto_removal/maxRectangleSolver.py
@@ -56,7 +56,7 @@ def do_overlap(l1, r1, l2, r2):
     return True
 
 def checkOverlap(rect0, rect1):
-    rect0
+    getRectDiagonalPoints(rect0)
     # if checkOverlapAsymmetric(rect0, rect1):
     #     return True
     # if checkOverlapAsymmetric(rect1, rect0):
