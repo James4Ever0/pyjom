a6d23447f8659ace3cc47b0c2f8a33dddb5090c4
GitJournal <app@gitjournal.io>
2022-09-05 23:54:14 +0800

    [2022-09-05 15:54:10]
    FILE:
    pyjom/videotoolbox.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/videotoolbox.py
+++ pyjom/videotoolbox.py
@@ -847,4 +847,5 @@ def detectPipRegionOverTime(videoPath, start, end, method = "skim", algo='frame_
         resultDict = sampledStablePipRegionExporter(pipFrames, defaultWidth, defaultHeight)
     finalResultDict = {}
     for key, value in resultDict.items():
+        updatedValueAlignedToSeconds = [await]
         finalResultDict.update()
