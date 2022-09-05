96cd9a212677f7db572f0297fff6922bbea804cf
GitJournal <app@gitjournal.io>
2022-09-05 15:23:24 +0800

    [2022-09-05 07:23:21]
    FILE:
    tests/video_detector_tests/pip_meanVariance_stablize.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/video_detector_tests/pip_meanVariance_stablize.py
+++ tests/video_detector_tests/pip_meanVariance_stablize.py
@@ -140,9 +140,10 @@ yRightPoints = data[:,1,1]
 
 mPoints = [xLeftPoints, yLeftPoints, xRightPoints, yRightPoints]
 
+
 for mPoint in mPoints:
     answer = getSinglePointStableState(mPoint)
-    if answer == {}:
+    answers = []
     # print("_"*30, "ANSWER","_"*30)
     # for elem in answer.items():
     #     print(elem)
