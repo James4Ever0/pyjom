2da0c41fbd24b4a4376d8829fbaca19ec0c59907
GitJournal <app@gitjournal.io>
2022-09-05 11:58:31 +0800

    [2022-09-05 03:58:28]
    FILE:
    tests/video_detector_tests/rectangle_test.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/video_detector_tests/rectangle_test.py
+++ tests/video_detector_tests/rectangle_test.py
@@ -70,7 +70,7 @@ while True:
                     # only create one single bounding box.
             # cv2.rectangle(frame, (min_x, min_y),
             #               (max_x, max_y), (255, 0, 0), 2)
-            pipFrames.append([(min_x, min_y),           (max_x, max_y)])
+            pipFrames.append([(min_x, min_y),(max_x, max_y)].copy())
             # how to stablize this shit?
         # cv2.imshow('video', frame)
         # cv2.imshow('img_output', img_output)
