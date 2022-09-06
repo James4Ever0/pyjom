43cc9fa97c0b82c72c24fc43dd509689f0c73b89
GitJournal <app@gitjournal.io>
2022-09-06 13:15:35 +0800

    [2022-09-06 05:15:32]
    FILE:
    pyjom/videotoolbox.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/videotoolbox.py
+++ pyjom/videotoolbox.py
@@ -492,8 +492,8 @@ def sampledStablePipRegionExporter(data, defaultWidth, defaultHeight):
         mArray = getAlikeValueMerged(mArray, threshold=threshold)
         return listToRangedDictWithLabel(mArray, label)
 
-    # threshold = int(max(defaultWidth, defaultHeight) * 0.02734375)
-    thres
+    threshold = int(max(defaultWidth, defaultHeight) * 0.02734375)
+    # threshold = 
     xLeftPoints = pointsToRangedDictWithLabel(
         data[:, 0, 0], "xleft", threshold=threshold
     )
