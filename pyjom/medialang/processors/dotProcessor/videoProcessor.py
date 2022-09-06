89aad6cd73da60963b35e294b56ab026f872b2d0
GitJournal <app@gitjournal.io>
2022-09-06 11:59:00 +0800

    [2022-09-06 03:58:56]
    FILE:
    pyjom/medialang/processors/dotProcessor/videoProcessor.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/medialang/processors/dotProcessor/videoProcessor.py
+++ pyjom/medialang/processors/dotProcessor/videoProcessor.py
@@ -32,7 +32,8 @@ def ffmpegVideoPreProductionFilter(
     # uncertain. very uncertain.
     def wideScreen1080(stream, mWidth=1920, mHeight = 1080):
         width='max(iw, ceil(ih*max({}/{}, iw/ih)))'.format(mWidth, mHeight)
-        height='max(ih, ceil(iw*max(1080/1920, ih/iw)))'
+        height='max(ih, ceil(iw*max({}/{}, ih/iw)))'.format(mHeight, mWidth)
+        
         return stream.filter("pad",width=width, height=height, x=x, y=y,color='black').filter('scale','1920','1080')
 
     assert cachePath is not None
