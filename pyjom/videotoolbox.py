e69b796859f8a19e56e6a4edb2f48778b07298e8
GitJournal <app@gitjournal.io>
2022-09-06 15:02:57 +0800

    [2022-09-06 07:02:54]
    FILE:
    pyjom/videotoolbox.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/videotoolbox.py
+++ pyjom/videotoolbox.py
@@ -901,7 +901,8 @@ def detectPipRegionOverTime(
         # for _ in range(downScale):
         downScaledFrame = cv2.resize(frame, (int(defaultWidth/downScale), int(defaultHeight/downScale)))
         img_output = algorithm.apply(downScaledFrame)
-        if index %batch != batch-1: 
+        if index %batch != batch-1:
+            continue
         [x, y, w, h] = cv2.boundingRect(img_output)  # wtf is this?
         x,y,w,h = x*downScale, y*downScale, w*downScale, h*downScale
         area = w * h
