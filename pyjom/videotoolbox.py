d7a1399e7818eb5fe0b5642805e940ebe346cd0d
GitJournal <app@gitjournal.io>
2022-09-06 13:02:04 +0800

    [2022-09-06 05:02:00]
    FILE:
    pyjom/videotoolbox.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/videotoolbox.py
+++ pyjom/videotoolbox.py
@@ -591,25 +591,6 @@ def kalmanStablePipRegionExporter(data, defaultWidth, defaultHeight):
 
     data = np.array(data)
 
-    from pykalman import KalmanFilter
-
-    def Kalman1D(observations, damping=0.2):
-        # To return the smoothed time series data
-        observation_covariance = damping
-        initial_value_guess = observations[0]
-        transition_matrix = 1
-        transition_covariance = 0.1
-        initial_value_guess
-        kf = KalmanFilter(
-            initial_state_mean=initial_value_guess,
-            initial_state_covariance=observation_covariance,
-            observation_covariance=observation_covariance,
-            transition_covariance=transition_covariance,
-            transition_matrices=transition_matrix,
-        )
-        pred_state, state_cov = kf.smooth(observations)
-        return pred_state
-
     def getSinglePointStableState(
         xLeftPoints,
         signalFilterThreshold=10,
