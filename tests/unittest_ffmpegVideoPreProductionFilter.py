7d5f3d2275ecf0c69ecb038d2f877672e5859d3a
GitJournal <app@gitjournal.io>
2022-09-06 13:47:50 +0800

    [2022-09-06 05:47:46]
    FILE:
    tests/unittest_ffmpegVideoPreProductionFilter.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_ffmpegVideoPreProductionFilter.py
+++ tests/unittest_ffmpegVideoPreProductionFilter.py
@@ -6,7 +6,7 @@ videoPaths = {
     "text":"/root/Desktop/works/pyjom/samples/video/dog_with_text.mp4",
     "logo":"/root/Desktop/works/pyjom/samples/video/LkS8UkiLL.mp4",
     # "pip":"/root/Desktop/works/pyjom/samples/video/LiGlReJ4i.mp4",
-    'pip':'# video_file = "../../samples/video/LiEIfnsvn.mp4"'
+    'pip':'/root/Desktop/works/pyjom/samples/video/LiEIfnsvn.mp4' #
 }
 tempDir = '/dev/shm/medialang' # anyway we just want something else...
 
