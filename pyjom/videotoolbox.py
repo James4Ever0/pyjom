762d6d9becae7ca1d7f98196efb2c9ea3c8d60d9
GitJournal <app@gitjournal.io>
2022-09-06 14:21:29 +0800

    [2022-09-06 06:21:25]
    FILE:
    pyjom/videotoolbox.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/videotoolbox.py
+++ pyjom/videotoolbox.py
@@ -763,7 +763,7 @@ def kalmanStablePipRegionExporter(data, defaultWidth, defaultHeight,downScale=1,
         finalCommandDict = {}
     else:
         # defaultCoord = [0, 0, defaultWidth, defaultHeight]  # deal with it later?
-        defaultCoord = [int(np.mean(array)) for array in ]
+        defaultCoord = [int(np.mean(array)) for array in mPoints]
         defaults = [{str(defaultCoord[index]): [(0, len(data))]} for index in range(4)]
         for index in range(4):
             if answers[index] == {}:
