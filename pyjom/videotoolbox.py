1086ab32917c4bcfe3ab0e1f568bac893f6106bb
GitJournal <app@gitjournal.io>
2022-09-06 13:43:03 +0800

    [2022-09-06 05:43:01]
    FILE:
    pyjom/videotoolbox.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/videotoolbox.py
+++ pyjom/videotoolbox.py
@@ -835,7 +835,7 @@ def kalmanStablePipRegionExporter(data, defaultWidth, defaultHeight,downScale=1)
 
 
 def detectPipRegionOverTime(
-    videoPath, start, end, method="skim", algo="frame_difference", downScale=4
+    videoPath, start, end, method="framewise", algo="frame_difference", downScale=4
 ):  # shall be some parameters here.
     # if it is 'skim' we will sample it every 20 frames.
     defaultWidth, defaultHeight = getVideoWidthHeight(videoPath)
