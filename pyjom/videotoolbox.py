feba0b6e4978dcbbd300777a9a3afba0e9b072a1
GitJournal <app@gitjournal.io>
2022-09-06 13:32:40 +0800

    [2022-09-06 05:32:37]
    FILE:
    pyjom/videotoolbox.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/videotoolbox.py
+++ pyjom/videotoolbox.py
@@ -549,12 +549,12 @@ def sampledStablePipRegionExporter(data, defaultWidth, defaultHeight):
         noAlter = beforeChange == afterChange
         if noAlter:
             break
-    if len(commandDictSequential)>=2:
-        _, timespan = commandDictSequential[0]
-        nextCommand, nextTimeSpan = commandDictSequential[1]
-        nextStart, nextEnd = nextTimeSpan
-        commandDictSequential[1] = [nextCommand,(timespan[0], nextEnd)]
-        commandDictSequential.pop(0)
+    # if len(commandDictSequential)>=2:
+    #     _, timespan = commandDictSequential[0]
+    #     nextCommand, nextTimeSpan = commandDictSequential[1]
+    #     nextStart, nextEnd = nextTimeSpan
+    #     commandDictSequential[1] = [nextCommand,(timespan[0], nextEnd)]
+    #     commandDictSequential.pop(0)
     preFinalCommandDict = sequentialToMergedRanges(commandDictSequential)
     finalCommandDict = {}
     for key, elem in preFinalCommandDict.items():
