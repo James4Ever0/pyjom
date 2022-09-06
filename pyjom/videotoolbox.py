9abc7e747b03822d9ed972ad6e285af09c4b7a51
GitJournal <app@gitjournal.io>
2022-09-06 14:16:21 +0800

    [2022-09-06 06:16:17]
    FILE:
    pyjom/videotoolbox.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/videotoolbox.py
+++ pyjom/videotoolbox.py
@@ -748,6 +748,7 @@ def kalmanStablePipRegionExporter(data, defaultWidth, defaultHeight,downScale=1,
     import math
     suggestedSignalFilterThreshold = int(10*math.sqrt(downScale))
     suggestedStdThreshold = 1*math.sqrt(math.sqrt(downScale))
+    suggestedSlopeThreshold = 0.2*math.sqrt(math.sqrt(downScale))
     for mPoint in mPoints:
         answer = getSinglePointStableState(mPoint,signalFilterThreshold=max(10, suggestedSignalFilterThreshold),
         stdThreshold = max(1, suggestedStdThreshold), slopeThreshold=max(0.2,suggestedSlopeThreshold), commandFloatMergeThreshold=max(10,suggestedCommandFloatMergeThreshold))
