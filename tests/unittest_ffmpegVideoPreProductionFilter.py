776298a699391c494ee85f8b433bb01ac4d4302d
GitJournal <app@gitjournal.io>
2022-09-06 13:47:35 +0800

    [2022-09-06 05:47:31]
    FILE:
    pyjom/videotoolbox.py
    tests/unittest_ffmpegVideoPreProductionFilter.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_ffmpegVideoPreProductionFilter.py
+++ tests/unittest_ffmpegVideoPreProductionFilter.py
@@ -5,7 +5,8 @@ import tempfile
 videoPaths = {
     "text":"/root/Desktop/works/pyjom/samples/video/dog_with_text.mp4",
     "logo":"/root/Desktop/works/pyjom/samples/video/LkS8UkiLL.mp4",
-    "pip":"/root/Desktop/works/pyjom/samples/video/LiGlReJ4i.mp4",
+    # "pip":"/root/Desktop/works/pyjom/samples/video/LiGlReJ4i.mp4",
+    'pip':'# video_file = "../../samples/video/LiEIfnsvn.mp4"'
 }
 tempDir = '/dev/shm/medialang' # anyway we just want something else...
 
