57e01cd37d4d6ec901e883567d3c34143680bba1
GitJournal <app@gitjournal.io>
2022-09-06 11:32:12 +0800

    [2022-09-06 03:32:08]
    FILE:
    pyjom/medialang/processors/dotProcessor/videoProcessor.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/medialang/processors/dotProcessor/videoProcessor.py
+++ pyjom/medialang/processors/dotProcessor/videoProcessor.py
@@ -45,7 +45,7 @@ def ffmpegVideoPreProductionFilter(
                 "scale",
                 "ceil((in_w*{})/4)*4".format(previewRatio),
                 "ceil((in_h*{})/4)*4".format(previewRatio),
-            ).filter("pad",'width'=)
+            ).filter("pad",width=width, height=height, x=x, y=y)
 
     # stream = ffmpeg.hflip(stream)
     # this fliping may be useful for copyright evasion, but not very useful for filtering. it just adds more computational burden.
