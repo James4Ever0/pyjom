28b8c2a41cf3f8a8a89ba1f5367b9c9feb9357c5
GitJournal <app@gitjournal.io>
2022-09-06 12:46:02 +0800

    [2022-09-06 04:45:59]
    FILE:
    pyjom/videotoolbox.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/videotoolbox.py
+++ pyjom/videotoolbox.py
@@ -866,7 +866,7 @@ def detectPipRegionOverTime(
     else:
         videoFrameRate = getVideoFrameRate(videoPath)
         totalFramesInSegment = (end - start) * videoFrameRate
-        minSampleSize = 80
+        minSampleSize = 80*4
         min_sample_rate = int(totalFramesInSegment / minSampleSize)
         estimated_sample_rate = min(5, min_sample_rate)
         sample_rate = max(1, estimated_sample_rate)
