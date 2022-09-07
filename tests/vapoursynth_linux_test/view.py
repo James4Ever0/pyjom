c2058a113831b717d39a85b00794a2afacca5dbe
GitJournal <app@gitjournal.io>
2022-09-08 00:02:50 +0800

    [2022-09-07 16:02:47]
    FILE:
    tests/vapoursynth_linux_test/view.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/vapoursynth_linux_test/view.py
+++ tests/vapoursynth_linux_test/view.py
@@ -9,6 +9,16 @@ import sys
 import platform
 import timeit
 
+site_path = pathlib.Path("/usr/local/lib/python3.9/site-packages")
+cv2_libs_dir = (
+    site_path / "cv2" / f"python-{sys.version_info.major}.{sys.version_info.minor}"
+)
+print(cv2_libs_dir)
+cv2_libs = sorted(cv2_libs_dir.glob("*.so"))
+if len(cv2_libs) == 1:
+    print("INSERTING:", cv2_libs[0].parent)
+    sys.path.insert(1, str(cv2_libs[0].parent))
+
 
 import vapoursynth as vs
 from vapoursynth import core
