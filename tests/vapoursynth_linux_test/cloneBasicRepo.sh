914da6de95542c375b113b1e573e4bcf0c14f3de
GitJournal <app@gitjournal.io>
2022-09-07 23:06:56 +0800

    [2022-09-07 15:06:54]
    FILE:
    tests/vapoursynth_linux_test/cloneBasicRepo.sh
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/vapoursynth_linux_test/cloneBasicRepo.sh
+++ tests/vapoursynth_linux_test/cloneBasicRepo.sh
@@ -1,3 +1,3 @@
 git clone https://github.com/FFMS/ffms2
 # git clone https://github.com/vapoursynth/vsrepo
-git clone https://github.com/Bl4Cc4t/homebrew-vsplugins # checking how to 
+git clone https://github.com/Bl4Cc4t/homebrew-vsplugins # checking how to build these things properly on linux/macos
