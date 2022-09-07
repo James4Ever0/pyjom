c413a2dfdfc26f10658bf9926f7580f3aa0430ef
GitJournal <app@gitjournal.io>
2022-09-08 00:18:30 +0800

    [2022-09-07 16:18:26]
    FILE:
    tests/vapoursynth_linux_test/view_test.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/vapoursynth_linux_test/view_test.py
+++ tests/vapoursynth_linux_test/view_test.py
@@ -1,6 +1,20 @@
 videoPath = "/root/Desktop/works/pyjom/samples/video/dog_with_text.mp4"
 # videoPath = "/Users/jamesbrown/desktop/works/pyjom_remote/samples/video/dog_with_text.mp4"
 
+
+
+# The VideoFrame and AudioFrame classes contains one picture/audio chunk and all the metadata associated with it. It is possible to access the raw data using either get_read_ptr(plane) or get_write_ptr(plane) and get_stride(plane) with ctypes.
+
+# A more Python friendly wrapping is also available where each plane/channel can be accessed as a Python array using frame[plane/channel].
+
+# To get a frame simply call get_frame(n) on a clip. Should you desire to get all frames in a clip, use this code:
+
+# for frame in clip.frames():
+#     # Do stuff with your frame
+#     pass
+
+
+
 from vapoursynth import core
 video = core.ffms2.Source(source=videoPath)
 # video = core.std.Transpose(video)
