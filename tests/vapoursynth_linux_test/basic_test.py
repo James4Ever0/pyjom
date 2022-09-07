5219e1f32a97fa770dcad85ebcef8838045b5619
GitJournal <app@gitjournal.io>
2022-09-07 23:28:26 +0800

    [2022-09-07 15:28:23]
    FILE:
    tests/vapoursynth_linux_test/basic_test.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/vapoursynth_linux_test/basic_test.py
+++ tests/vapoursynth_linux_test/basic_test.py
@@ -1,4 +1,4 @@
-videoPath = "/Users/jamesbrown/desktop/works/pyjom/samples/video/dog_with_text.mp4"
+videoPath = "/root/Desktop/works/pyjom/samples/video/dog_with_text.mp4"
 # videoPath = "/Users/jamesbrown/desktop/works/pyjom_remote/samples/video/dog_with_text.mp4"
 
 from vapoursynth import core
