911bb747e8ed9feea0c0b11b8c9e443aeff256f0
GitJournal <app@gitjournal.io>
2022-09-07 22:21:15 +0800

    [2022-09-07 14:21:12]
    FILE:
    tests/vapoursynth_linux_test/make_vapoursynth_autoload.sh
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/vapoursynth_linux_test/make_vapoursynth_autoload.sh
+++ tests/vapoursynth_linux_test/make_vapoursynth_autoload.sh
@@ -3,4 +3,5 @@ mkdir -p "$HOME/Library/Application Support/VapourSynth/"
 touch "$HOME/Library/Application Support/VapourSynth/vapoursynth.conf"
 mkdir -p /Library/vapoursynth/plugins
 mkdir -p /Users/jamesbrown/vapoursynth/plugins
-echo ""
+echo "UserPluginDir=/Users/jamesbrown/vapoursynth/plugins" > "$HOME/Library/Application Support/VapourSynth/vapoursynth.conf"
+SystemPluginDir=/Library/vapoursynth/plugins"
