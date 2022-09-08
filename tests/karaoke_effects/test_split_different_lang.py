dfd1c6edf412cb5f379ddbe05b562a857ade6f82
GitJournal <app@gitjournal.io>
2022-09-08 19:10:34 +0800

    [2022-09-08 11:10:31]
    FILE:
    tests/karaoke_effects/test_split_different_lang.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/karaoke_effects/test_split_different_lang.py
+++ tests/karaoke_effects/test_split_different_lang.py
@@ -1,15 +1,22 @@
 # example of TDD.
 tests = [
-    ["リンの麺は終わった",'リンの麺は終わった'], 
-# only japanese
-    ["リンの麺は終わった Lina的面吃完了没有","リンの麺は終わった Lina的面吃完了没有"], # japanese with chinese containing english
-["Lina I miss you Lina我想你了","Lina I miss you Lina我想你了"], # english with chinese containing english
-["向前冲 冲 冲","向前冲 冲 冲"], # only chinese
-["go go go","go go go"], # chinese containing english (overall)
-
+    ["リンの麺は終わった", "リンの麺は終わった"],
+    # only japanese
+    [
+        "リンの麺は終わった Lina的面吃完了没有",
+        "リンの麺は終わった Lina的面吃完了没有",
+    ],  # japanese with chinese containing english
+    [
+        "Lina I miss you Lina我想你了",
+        "Lina I miss you Lina我想你了",
+    ],  # english with chinese containing english
+    ["向前冲 冲 冲", "向前冲 冲 冲"],  # only chinese
+    ["go go go", "go go go"],  # chinese containing english (overall)
+]
 # build a classifier for this? wtf?
 
 # whatlang?
+
 
 def lastSpaceSpliter(text):
     text = text.strip()
@@ -21,7 +28,8 @@ def lastSpaceSpliter(text):
             # do it right now, and return the value here.
             mTuple = (text[0:index], text[index:])
             return mTuple, True
-    return text, False # not a list.
+    return text, False  # not a list.
+
 
 # if there is a single shit failed to pass this 'lastSpaceSpliter' test, this is not a bilingual lrc file from netease.
 
