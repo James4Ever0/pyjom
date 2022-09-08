c06f4064b915301f424807488375f5711c6532b8
GitJournal <app@gitjournal.io>
2022-09-08 11:38:12 +0800

    [2022-09-08 03:38:08]
    FILE:
    tests/karaoke_effects/test_pylrc.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/karaoke_effects/test_pylrc.py
+++ tests/karaoke_effects/test_pylrc.py
@@ -14,6 +14,7 @@ for sub in subs:
     startTime = sub.time
     text = sub.text
     textArray.append((startTime, text))
+    
     # print(sub.time) # single shit.
     # print(dir(sub))
     # print(text)
