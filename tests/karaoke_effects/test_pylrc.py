3961e4c062645dc579aed6525486d8b560f4fbca
GitJournal <app@gitjournal.io>
2022-09-08 11:39:24 +0800

    [2022-09-08 03:39:21]
    FILE:
    tests/karaoke_effects/test_pylrc.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/karaoke_effects/test_pylrc.py
+++ tests/karaoke_effects/test_pylrc.py
@@ -7,13 +7,13 @@ lrc_file.close()
 subs = pylrc.parse(lrc_string)
 
 lyricDurationThresholds = (1,4)
+
 textArray = []
 
 for sub in subs:
     startTime = sub.time
     text = sub.text
     textArray.append((startTime, text))
-
     # print(sub.time) # single shit.
     # print(dir(sub))
     # print(text)
