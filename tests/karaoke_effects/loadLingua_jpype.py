9398734c3d635be7ceac0a95992ae49b8f3707bf
GitJournal <app@gitjournal.io>
2022-09-09 02:48:27 +0800

    [2022-09-08 18:48:24]
    FILE:
    tests/karaoke_effects/loadLingua_jpype.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/karaoke_effects/loadLingua_jpype.py
+++ tests/karaoke_effects/loadLingua_jpype.py
@@ -13,16 +13,17 @@ from com.github.pemistahl.lingua.api import *
 # detector = LanguageDetectorBuilder.fromAllLanguages().withLowAccuracyMode().build()
 linguaDetector = LanguageDetectorBuilder.fromAllLanguages().build() # 3.5GB just for detecting language! it is somehow crazy.
 
-sample = 'hello world'
-# sample = 'lina你吃早饭了没有'
+if __name__ == "__main__":
+    sample = 'hello world'
+    # sample = 'lina你吃早饭了没有'
 
-result = linguaDetector.detectLanguageOf(sample)
-print(result, type(result)) # <java class 'com.github.pemistahl.lingua.api.Language'>
-# but we can convert it into string.
-strResult = str(result)
-print(strResult, type(strResult))
-import math
+    result = linguaDetector.detectLanguageOf(sample)
+    print(result, type(result)) # <java class 'com.github.pemistahl.lingua.api.Language'>
+    # but we can convert it into string.
+    strResult = str(result)
+    print(strResult, type(strResult))
+    import math
 
-print("CALLING MATH: %d" % math.sqrt(4))
-# shutdownJVM()
-# what if we do not shut this down?
+    print("CALLING MATH: %d" % math.sqrt(4))
+    # shutdownJVM()
+    # what if we do not shut this down?
