664843d7bd95dd9d95ec7d4f9969c42ec30bac2c
GitJournal <app@gitjournal.io>
2022-09-08 19:09:15 +0800

    [2022-09-08 11:09:12]
    FILE:
    tests/karaoke_effects/test_split_different_lang.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/karaoke_effects/test_split_different_lang.py
+++ tests/karaoke_effects/test_split_different_lang.py
@@ -1,6 +1,6 @@
 # example of TDD.
-tests = [["リンの麺は終わった",'リンの麺は終わった', # only japanese
-    "リンの麺は終わった Lina的面吃完了没有", # japanese with chinese containing english
+tests = [["リンの麺は終わった",'リンの麺は終わった'], # only japanese
+    ["リンの麺は終わった Lina的面吃完了没有",'', # japanese with chinese containing english
 "Lina I miss you Lina我想你了", # english with chinese containing english
 "向前冲 冲 冲", # only chinese
 "go go go", # chinese containing english (overall)
