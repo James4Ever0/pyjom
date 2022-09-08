68d76cb025fa64787193287aa852a02204809f44
GitJournal <app@gitjournal.io>
2022-09-08 19:28:55 +0800

    [2022-09-08 11:28:52]
    FILE:
    tests/karaoke_effects/test_split_different_lang.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/karaoke_effects/test_split_different_lang.py
+++ tests/karaoke_effects/test_split_different_lang.py
@@ -60,15 +60,15 @@ for test in tests:
         nativeLangFlagStandard = "Cmn"
         foreignLangString = " ".join(foreignLangList)
         nativeLangString = " ".join(nativeLangList)
-        # import whatlang
-        # foreignLangFlag = whatlang.detect_language(foreignLangString)
-        # nativeLangFlag = whatlang.detect_language(nativeLangString)
-        import cld3
-        nativeLangFlagStandard = "zh"
-        foreignLangFlag = cld3.get_language(foreignLangString)
-        nativeLangFlag = cld3.get_language(nativeLangString)
-        print(foreignLangFlag)
-        print(nativeLangFlag)
+        import whatlang
+        foreignLangFlag = whatlang.detect_language(foreignLangString)
+        nativeLangFlag = whatlang.detect_language(nativeLangString)
+        # import cld3
+        # nativeLangFlagStandard = "zh"
+        # foreignLangFlag = cld3.get_language(foreignLangString)
+        # nativeLangFlag = cld3.get_language(nativeLangString)
+        # print(foreignLangFlag)
+        # print(nativeLangFlag)
         # breakpoint()
         if foreignLangFlag[0] != nativeLangFlagStandard and nativeLangFlag[0] == nativeLangFlagStandard:
             # this is for sure the bilingual shit.
