547dd1c1704b868ffd7a6d7651af2af5d330764e
GitJournal <app@gitjournal.io>
2022-09-08 19:13:16 +0800

    [2022-09-08 11:13:13]
    FILE:
    tests/karaoke_effects/test_split_different_lang.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/karaoke_effects/test_split_different_lang.py
+++ tests/karaoke_effects/test_split_different_lang.py
@@ -39,5 +39,4 @@ for test in tests:
     if sum(flags) < len(flags)*0.8:
         print("NOT A BILIGUAL LYRICS FILE")
     else:
-        
-    result, flag = 
+        # process this shit separately.
