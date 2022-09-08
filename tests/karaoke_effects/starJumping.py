eb5550697ef51e1a72e5b8d9114292ce7c18d622
GitJournal <app@gitjournal.io>
2022-09-08 16:13:50 +0800

    [2022-09-08 08:13:47]
    FILE:
    tests/karaoke_effects/starJumping.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/karaoke_effects/starJumping.py
+++ tests/karaoke_effects/starJumping.py
@@ -286,7 +286,8 @@ def kanji(line, l):
 
         l.start_time =  syl.end_time + 100
         # l.start_time = line.start_time + syl.end_time + 100
-        l.end_time = line.end_time - 25 * (len(line.syls) - syl.i) + delay + 100
+        l.end_time = line.end_time 
+        # l.end_time = line.end_time - 25 * (len(line.syls) - syl.i) + delay + 100
         l.dur = l.end_time - l.start_time
 
         l.text = (
