533c5c4ff722e179339540e3d9970253fdab96de
GitJournal <app@gitjournal.io>
2022-09-08 11:54:20 +0800

    [2022-09-08 03:54:17]
    FILE:
    tests/karaoke_effects/test_pylrc.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/karaoke_effects/test_pylrc.py
+++ tests/karaoke_effects/test_pylrc.py
@@ -1,8 +1,8 @@
 import pylrc
+import MediaInfo
 
-
-
-songLength = 
+musicPath= ""
+songLength = ""
 
 lrc_file = open('/root/Desktop/works/pyjom/tests/music_analysis/exciting_bgm.lrc')
 lrc_string = ''.join(lrc_file.readlines())
