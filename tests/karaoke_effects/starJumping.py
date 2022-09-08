0e743474e0dc817dcc343f56ae2a6a260586184f
GitJournal <app@gitjournal.io>
2022-09-08 15:35:28 +0800

    [2022-09-08 07:35:25]
    FILE:
    tests/karaoke_effects/starJumping.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/karaoke_effects/starJumping.py
+++ tests/karaoke_effects/starJumping.py
@@ -177,7 +177,7 @@ def romaji(line, l):
     # for syl in Utils.all_non_empty(line.syls):
     #     l.layer = 0
 
-    #     l.start_time = syl.start_time + 100
+    #     l.start_time = syl.e + 100
     #     # l.start_time = line.start_time + syl.end_time + 100
     #     l.end_time = line.end_time - 25 * (len(line.syls) - syl.i) + delay + 100
     #     # l.end_time = line.end_time - 25 * (len(line.syls) - syl.i) + delay + 100
