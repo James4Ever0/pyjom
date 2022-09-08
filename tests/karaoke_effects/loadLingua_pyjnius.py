0211eafa248dcc5334bd03e271eb7be694d7368e
GitJournal <app@gitjournal.io>
2022-09-08 22:23:50 +0800

    [2022-09-08 14:23:47]
    FILE:
    tests/karaoke_effects/loadLingua_pyjnius.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/karaoke_effects/loadLingua_pyjnius.py
+++ tests/karaoke_effects/loadLingua_pyjnius.py
@@ -2,7 +2,7 @@ import jnius_config
 jnius_config.add_options('-Xrs', '-Xmx4096')
 jnius_config.set_classpath('.', "/root/Desktop/works/pyjom/tests/karaoke_effects/classpath/lingua.jar")
 import jnius
-
+jnius.autoclass('java.lang.System').out.println('Hello world')
 # detector = jnius.autoclass('com.github.pemistahl.lingua.api.LanguageDetectorBuilder').fromAllLanguages().build()
 
 # sample = 'hello world'
