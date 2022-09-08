385c04dd78ab488fd0995c27479ca8b4aa356453
GitJournal <app@gitjournal.io>
2022-09-08 20:38:01 +0800

    [2022-09-08 12:37:59]
    FILE:
    tests/karaoke_effects/loadLingua_jpype.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/karaoke_effects/loadLingua_jpype.py
+++ tests/karaoke_effects/loadLingua_jpype.py
@@ -2,7 +2,7 @@ from jpype import *
 
 addClassPath("/root/Desktop/works/pyjom/tests/karaoke_effects/classpath/lingua.jar")
 
-startJVM(getDefaultJVMPath(), "-ea")
+startJVM(getDefaultJVMPath())
 java.lang.System.out.println("Calling Java Print from Python using Jpype!")
 
 from com.github.pemistahl.lingua.api import *
