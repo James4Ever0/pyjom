f8824985475f262f30a175854d827f399f1f64ae
GitJournal <app@gitjournal.io>
2022-09-09 02:54:41 +0800

    [2022-09-08 18:54:39]
    FILE:
    tests/karaoke_effects/test_detect_different_language.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/karaoke_effects/test_detect_different_language.py
+++ tests/karaoke_effects/test_detect_different_language.py
@@ -33,6 +33,6 @@ print(whatlang.detect_language('你好'))
 # from loadLingua_jpype import getLinguaDetectedLanguageLabel
 
 # print(getLinguaDetectedLanguageLabel('你好'))
-# from loadLingua_jpype import getLinguaDetectedLanguageLabel
+from loadLingua_pyjnius import getLinguaDetectedLanguageLabel
 
-# print(getLinguaDetectedLanguageLabel('你好'))
+print(getLinguaDetectedLanguageLabel('你好'))
