179fa311dec269c7b6f7acdc70d6a65bc52be6e7
GitJournal <app@gitjournal.io>
2022-09-08 18:58:12 +0800

    [2022-09-08 10:58:09]
    FILE:
    tests/karaoke_effects/test_detect_different_language.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/karaoke_effects/test_detect_different_language.py
+++ tests/karaoke_effects/test_detect_different_language.py
@@ -5,7 +5,7 @@ text_content = sample
 _, _, _, detected_language = cld2.detect(text_content,  returnVectors=True)
 print(detected_language) # unknown! fucking shit
 # ((0, 323, 'FRENCH', 'fr'), (323, 64, 'ENGLISH', 'en'))
-# this vector tool is ve
+# this vector tool is very unstable. so as the freaking cld3 'get_frequent_language' shit
 
 # ((0, 30, 'Unknown', 'un'),)
 
