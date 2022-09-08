62ef7c7e6b6af2905ae1080e7be9ca75a298c943
GitJournal <app@gitjournal.io>
2022-09-08 18:35:39 +0800

    [2022-09-08 10:35:36]
    FILE:
    tests/karaoke_effects/test_detect_different_language.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/karaoke_effects/test_detect_different_language.py
+++ tests/karaoke_effects/test_detect_different_language.py
@@ -3,5 +3,5 @@ sample = 'hello world'
 import pycld2 as cld2
 text_content = sample
 _, _, _, detected_language = cld2.detect(text_content,  returnVectors=True)
-print(detected_language)
+print(detected_language) # unknown! fucking shit
 # ((0, 323, 'FRENCH', 'fr'), (323, 64, 'ENGLISH', 'en'))
