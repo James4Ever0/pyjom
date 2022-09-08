817055969af645c1ef0166fc21f8495183b39493
GitJournal <app@gitjournal.io>
2022-09-08 16:14:22 +0800

    [2022-09-08 08:14:19]
    FILE:
    tests/karaoke_effects/starJumping.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/karaoke_effects/starJumping.py
+++ tests/karaoke_effects/starJumping.py
@@ -215,11 +215,13 @@ def kanji(line, l):
     # Leadin Effect
     for syl in Utils.all_non_empty(line.syls):
         l.layer = 0
+        l.start_time = line.start_time
 
-        l.start_time = (
-            line.start_time + 25 * syl.i - delay - 80
-        )  # Remove 80 to start_time to let leadin finish a little bit earlier than the main effect of the first syllable
+        # l.start_time = (
+        #     line.start_time + 25 * syl.i - delay - 80
+        # )  # Remove 80 to start_time to let leadin finish a little bit earlier than the main effect of the first syllable
         l.end_time = line.start_time + syl.start_time
+        # l.end_time = line.start_time + syl.start_time
         l.dur = l.end_time - l.start_time
 
         l.text = (
