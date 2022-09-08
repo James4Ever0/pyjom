3e58af0fb2f3c71e93a60fee113b69057f20c93f
GitJournal <app@gitjournal.io>
2022-09-08 14:14:15 +0800

    [2022-09-08 06:14:12]
    FILE:
    tests/karaoke_effects/starJumping.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/karaoke_effects/starJumping.py
+++ tests/karaoke_effects/starJumping.py
@@ -368,6 +368,7 @@ for elem in newTextArray:
         syl.text = word
         syl.start_time =lineMod.start_time+ index*sylDuration
         syl.end_time = syl.start_time+sylDuration
+        syl.duration = sylDuration
         sylList.append(syl)
     lineMod.syls = sylList
     # pyonfx.ass_core.Syllable
