f1189c334dfc9b8617fd9cd34a663869c3904085
GitJournal <app@gitjournal.io>
2022-09-08 18:38:08 +0800

    [2022-09-08 10:38:05]
    FILE:
    tests/karaoke_effects/test_detect_different_language.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/karaoke_effects/test_detect_different_language.py
+++ tests/karaoke_effects/test_detect_different_language.py
@@ -1,7 +1,11 @@
-sample = 'last friday night 影響包含對氣候的變化以及自然資源的枯竭程度'
+sample = 'last friday night 影響包含'
 
 import pycld2 as cld2
 text_content = sample
 _, _, _, detected_language = cld2.detect(text_content,  returnVectors=True)
 print(detected_language) # unknown! fucking shit
 # ((0, 323, 'FRENCH', 'fr'), (323, 64, 'ENGLISH', 'en'))
+
+import cld3
+
+result = cld3.get_frequent_languages(sample)
