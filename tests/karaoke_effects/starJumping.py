3aac434b394805d590911137126c4df2334364c8
GitJournal <app@gitjournal.io>
2022-09-08 15:29:46 +0800

    [2022-09-08 07:29:43]
    FILE:
    tests/karaoke_effects/starJumping.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/karaoke_effects/starJumping.py
+++ tests/karaoke_effects/starJumping.py
@@ -42,7 +42,7 @@ def romaji(line, l):
     for syl in Utils.all_non_empty(line.syls):
         l.layer = 0
         # l.start_time = syl.end_time
-        l.start_time = line.start_time +25 * syl.i
+        l.start_time = line.start_time
 
         # l.start_time = (
         #     line.start_time + 25 * syl.i - delay - 80
@@ -68,135 +68,135 @@ def romaji(line, l):
 
         io.write_line(l)
 
-    # Main Effect
-    for syl in Utils.all_non_empty(line.syls):
-        l.layer = 1
+    # # Main Effect
+    # for syl in Utils.all_non_empty(line.syls):
+    #     l.layer = 1
 
-        l.start_time = syl.start_time
-        # l.start_time = line.start_time + syl.start_time
-        l.end_time = syl.end_time + 100
-        l.dur = l.end_time - l.start_time
+    #     l.start_time = syl.start_time
+    #     # l.start_time = line.start_time + syl.start_time
+    #     l.end_time = syl.end_time + 100
+    #     l.dur = l.end_time - l.start_time
 
-        c1 = "&H81F4FF&"
-        c3 = "&H199AAA&"
-        # Change color if inline_fx is m1
-        if syl.inline_fx == "m1":
-            c1 = "&H8282FF&"
-            c3 = "&H191AAA&"
+    #     c1 = "&H81F4FF&"
+    #     c3 = "&H199AAA&"
+    #     # Change color if inline_fx is m1
+    #     if syl.inline_fx == "m1":
+    #         c1 = "&H8282FF&"
+    #         c3 = "&H191AAA&"
 
-        on_inline_effect_2 = ""
-        # Apply rotation if inline_fx is m2
-        if syl.inline_fx == "m2":
-            on_inline_effect_2 = "\\t(0,%d,\\frz%.3f)\\t(%d,%d,\\frz0)" % (
-                l.dur / 4,
-                random.uniform(-40, 40),
-                l.dur / 4,
-                l.dur,
-            )
+    #     on_inline_effect_2 = ""
+    #     # Apply rotation if inline_fx is m2
+    #     if syl.inline_fx == "m2":
+    #         on_inline_effect_2 = "\\t(0,%d,\\frz%.3f)\\t(%d,%d,\\frz0)" % (
+    #             l.dur / 4,
+    #             random.uniform(-40, 40),
+    #             l.dur / 4,
+    #             l.dur,
+    #         )
 
-        l.text = (
-            "{\\an5\\pos(%.3f,%.3f)%s\\t(0,80,\\fscx105\\fscy105\\1c%s\\3c%s)\\t(80,%d,\\fscx100\\fscy100\\1c%s\\3c%s)}%s"
-            % (
-                syl.center,
-                syl.middle,
-                on_inline_effect_2,
-                c1,
-                c3,
-                l.dur - 80,
-                line.styleref.color1,
-                line.styleref.color3,
-                syl.text,
-            )
-        )
+    #     l.text = (
+    #         "{\\an5\\pos(%.3f,%.3f)%s\\t(0,80,\\fscx105\\fscy105\\1c%s\\3c%s)\\t(80,%d,\\fscx100\\fscy100\\1c%s\\3c%s)}%s"
+    #         % (
+    #             syl.center,
+    #             syl.middle,
+    #             on_inline_effect_2,
+    #             c1,
+    #             c3,
+    #             l.dur - 80,
+    #             line.styleref.color1,
+    #             line.styleref.color3,
+    #             syl.text,
+    #         )
+    #     )
 
-        io.write_line(l)
+    #     io.write_line(l)
 
-        # Animating star shape that jumps over the syllables
-        # Jump-in to the first syl
-        jump_height = 18
-        if syl.i == 0:
-            FU = FrameUtility(line.start_time - line.leadin / 2, line.start_time)
-            for s, e, i, n in FU:
-                l.start_time = s
-                l.end_time = e
-                frame_pct = i / n
+    #     # Animating star shape that jumps over the syllables
+    #     # Jump-in to the first syl
+    #     jump_height = 18
+    #     if syl.i == 0:
+    #         FU = FrameUtility(line.start_time - line.leadin / 2, line.start_time)
+    #         for s, e, i, n in FU:
+    #             l.start_time = s
+    #             l.end_time = e
+    #             frame_pct = i / n
 
-                x = syl.center - syl.width * (1 - frame_pct)
-                y = syl.top - math.sin(frame_pct * math.pi) * jump_height
+    #             x = syl.center - syl.width * (1 - frame_pct)
+    #             y = syl.top - math.sin(frame_pct * math.pi) * jump_height
 
-                alpha = 255
-                alpha += FU.add(0, syl.duration, -255)
-                alpha = Convert.alpha_dec_to_ass(int(alpha))
+    #             alpha = 255
+    #             alpha += FU.add(0, syl.duration, -255)
+    #             alpha = Convert.alpha_dec_to_ass(int(alpha))
 
-                l.text = (
-                    "{\\alpha%s\\pos(%.3f,%.3f)\\bord1\\blur1\\1c%s\\3c%s\\p1}%s"
-                    % (alpha, x, y, c1, c3, star)
-                )
-                io.write_line(l)
+    #             l.text = (
+    #                 "{\\alpha%s\\pos(%.3f,%.3f)\\bord1\\blur1\\1c%s\\3c%s\\p1}%s"
+    #                 % (alpha, x, y, c1, c3, star)
+    #             )
+    #             io.write_line(l)
 
-        # Jump to the next syl or to the end of line
-        jump_width = (
-            line.syls[syl.i + 1].center - syl.center
-            if syl.i != len(line.syls) - 1
-            else syl.width
-        )
-        # FU = FrameUtility(
-        #     line.start_time + syl.start_time, line.start_time + syl.end_time
-        # )
-        FU = FrameUtility(
-            syl.start_time, syl.end_time
-        )
-        for s, e, i, n in FU:
-            l.start_time = s
-            l.end_time = e
-            frame_pct = i / n
+    #     # Jump to the next syl or to the end of line
+    #     jump_width = (
+    #         line.syls[syl.i + 1].center - syl.center
+    #         if syl.i != len(line.syls) - 1
+    #         else syl.width
+    #     )
+    #     # FU = FrameUtility(
+    #     #     line.start_time + syl.start_time, line.start_time + syl.end_time
+    #     # )
+    #     FU = FrameUtility(
+    #         syl.start_time, syl.end_time
+    #     )
+    #     for s, e, i, n in FU:
+    #         l.start_time = s
+    #         l.end_time = e
+    #         frame_pct = i / n
 
-            x = syl.center + frame_pct * jump_width
-            y = syl.top - math.sin(frame_pct * math.pi) * jump_height
+    #         x = syl.center + frame_pct * jump_width
+    #         y = syl.top - math.sin(frame_pct * math.pi) * jump_height
 
-            alpha = 0
-            # Last jump should fade-out
-            if syl.i == len(line.syls) - 1:
-                alpha += FU.add(0, syl.duration, 255)
-            alpha = Convert.alpha_dec_to_ass(int(alpha))
+    #         alpha = 0
+    #         # Last jump should fade-out
+    #         if syl.i == len(line.syls) - 1:
+    #             alpha += FU.add(0, syl.duration, 255)
+    #         alpha = Convert.alpha_dec_to_ass(int(alpha))
 
-            l.text = "{\\alpha%s\\pos(%.3f,%.3f)\\bord1\\blur1\\1c%s\\3c%s\\p1}%s" % (
-                alpha,
-                x,
-                y,
-                c1,
-                c3,
-                star,
-            )
-            io.write_line(l)
+    #         l.text = "{\\alpha%s\\pos(%.3f,%.3f)\\bord1\\blur1\\1c%s\\3c%s\\p1}%s" % (
+    #             alpha,
+    #             x,
+    #             y,
+    #             c1,
+    #             c3,
+    #             star,
+    #         )
+    #         io.write_line(l)
 
-    # Leadout Effect
-    for syl in Utils.all_non_empty(line.syls):
-        l.layer = 0
+    # # Leadout Effect
+    # for syl in Utils.all_non_empty(line.syls):
+    #     l.layer = 0
 
-        l.start_time = syl.start_time + 100
-        # l.start_time = line.start_time + syl.end_time + 100
-        l.end_time = line.end_time - 25 * (len(line.syls) - syl.i) + delay + 100
-        # l.end_time = line.end_time - 25 * (len(line.syls) - syl.i) + delay + 100
-        l.dur = l.end_time - l.start_time
+    #     l.start_time = syl.start_time + 100
+    #     # l.start_time = line.start_time + syl.end_time + 100
+    #     l.end_time = line.end_time - 25 * (len(line.syls) - syl.i) + delay + 100
+    #     # l.end_time = line.end_time - 25 * (len(line.syls) - syl.i) + delay + 100
+    #     l.dur = l.end_time - l.start_time
 
-        l.text = (
-            "{\\an5\\move(%.3f,%.3f,%.3f,%.3f,%d,%d)\\t(%d,%d,\\blur2)\\fad(0,%d)}%s"
-            % (
-                syl.center,
-                syl.middle,
-                syl.center + math.cos(syl.i / 2) * off_x,
-                syl.middle + math.sin(syl.i / 4) * off_y,
-                l.dur - delay,
-                l.dur,
-                l.dur - delay,
-                l.dur,
-                delay,
-                syl.text,
-            )
-        )
+    #     l.text = (
+    #         "{\\an5\\move(%.3f,%.3f,%.3f,%.3f,%d,%d)\\t(%d,%d,\\blur2)\\fad(0,%d)}%s"
+    #         % (
+    #             syl.center,
+    #             syl.middle,
+    #             syl.center + math.cos(syl.i / 2) * off_x,
+    #             syl.middle + math.sin(syl.i / 4) * off_y,
+    #             l.dur - delay,
+    #             l.dur,
+    #             l.dur - delay,
+    #             l.dur,
+    #             delay,
+    #             syl.text,
+    #         )
+    #     )
 
-        io.write_line(l)
+    #     io.write_line(l)
 
 
 def kanji(line, l):
