5d2c45706f693cbc36d736589ade8f035d3d75f1
GitJournal <app@gitjournal.io>
2022-09-08 12:04:47 +0800

    [2022-09-08 04:04:44]
    FILE:
    tests/karaoke_effects/test_pylrc.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/karaoke_effects/test_pylrc.py
+++ tests/karaoke_effects/test_pylrc.py
@@ -56,6 +56,7 @@ for index,elem in enumerate(newTextArray):
     nextIndex = index+1
     if nextIndex < len(newTextArray):
         nextElem = newTextArray[nextIndex]
-    if nextElem is None:
+        end = 
+    else:
         end = np.mean(lyricDurations)+start
         end = min(musicDuration, end)
