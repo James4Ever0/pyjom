57fc80f4a1ffe7aaf76be847156bb78909aa9481
GitJournal <app@gitjournal.io>
2022-09-09 20:32:02 +0800

    [2022-09-09 12:31:59]
    FILE:
    tests/karaoke_effects/lyricToAss.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/karaoke_effects/lyricToAss.py
+++ tests/karaoke_effects/lyricToAss.py
@@ -1,11 +1,12 @@
 # this is the complete process.
 from lyrictoolbox import *
 
-def lrcToAnimatedAss(musicPath, lrcPath): # will be moved to lyrictoolbox, and more styles incoming
+def lrcToAnimatedAss(musicPath, lrcPath, assPath): # will be moved to lyrictoolbox, and more styles incoming
     textArray = lrcToTextArray(musicPath, lrcPath)
 
 if __name__ == "__main__":
     musicPath= "/root/Desktop/works/pyjom/tests/music_analysis/exciting_bgm.mp3"
     lrcPath = '/root/Desktop/works/pyjom/tests/music_analysis/exciting_bgm.lrc'
-    assPath = ''
+    import os
+    assPath = os.path.abspath('test.ass')
     lrcToAnimatedAss(musicPath, lrcPath,assPath)
