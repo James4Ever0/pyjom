1a68f477dfd4a54cb213e75bfea6c5f327da0048
GitJournal <app@gitjournal.io>
2022-09-09 15:05:52 +0800

    [2022-09-09 07:05:49]
    FILE:
    tests/karaoke_effects/clash_auto_update.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/karaoke_effects/clash_auto_update.py
+++ tests/karaoke_effects/clash_auto_update.py
@@ -126,7 +126,8 @@ def updateClashYaml(clashYamlPath = 'Clash3.yaml', control_port = 9911):
     getClashYaml(clashYamlPath = clashYamlPath)
     import requests
     import json
-                try:
+    full_config_path = os.path.abspath(clashYamlPath)
+            try:
                 r = requests.put("http://localhost:{}/configs".format(control_port),data=json.dumps({"path":full_config_path},ensure_ascii=False).encode())
                 assert r.status_code == 204
                 # might be the problem. TODO: check why the fuck clash server cannot decode the config in utf-8 'unexpected end of data'
