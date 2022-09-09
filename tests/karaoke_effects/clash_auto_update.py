68209b6eb98984ba9da5bc8c60e765953e02cd29
GitJournal <app@gitjournal.io>
2022-09-09 14:25:41 +0800

    [2022-09-09 06:25:38]
    FILE:
    tests/karaoke_effects/clash_auto_update.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/karaoke_effects/clash_auto_update.py
+++ tests/karaoke_effects/clash_auto_update.py
@@ -24,6 +24,7 @@ import yaml
 from loadSomeCustomClashYaml import goYamlToPyYaml, pyYamlToGoYaml
 
 def jsonLocate(jsonObj,location=[]):
+    try:
     if location!=[]:
         return jsonLocate(jsonObj[location[0]],location[1:])
     return jsonObj
