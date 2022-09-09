22dbfcc06816f0b71830b2bce5313f12919ca531
GitJournal <app@gitjournal.io>
2022-09-09 20:18:41 +0800

    [2022-09-09 12:18:38]
    FILE:
    tests/karaoke_effects/lyricToAss.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/karaoke_effects/lyricToAss.py
+++ tests/karaoke_effects/lyricToAss.py
@@ -1,5 +1,5 @@
 # this is the complete process.
 
 if __name__ == "__main__":
-musicPath= "/root/Desktop/works/pyjom/tests/music_analysis/exciting_bgm.mp3"
-lrc_file = open('/root/Desktop/works/pyjom/tests/music_analysis/exciting_bgm.lrc')
+    musicPath= "/root/Desktop/works/pyjom/tests/music_analysis/exciting_bgm.mp3"
+    lrcPath = '/root/Desktop/works/pyjom/tests/music_analysis/exciting_bgm.lrc
