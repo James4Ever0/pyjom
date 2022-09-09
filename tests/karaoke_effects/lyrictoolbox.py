92ab85be6664847461337cf511d2931aa2b84662
GitJournal <app@gitjournal.io>
2022-09-09 20:52:30 +0800

    [2022-09-09 12:52:27]
    FILE:
    tests/karaoke_effects/lyrictoolbox.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/karaoke_effects/lyrictoolbox.py
+++ tests/karaoke_effects/lyrictoolbox.py
@@ -284,4 +284,434 @@ def getTextListTranslated(test):
     return newLyricArray
 
 def textArrayWithTranslatedListToAss(textArray, translatedList, assPath):
+from pyonfx import *
+import random
+import math
+
+io = Ass("/root/Desktop/works/pyjom/tests/karaoke_effects/in2.ass", path_output="starJumping.ass")
+meta, styles, lines = io.get_data()
+
+# Creating the star and extracting all the color changes from the input file
+star = Shape.star(5, 4, 10)
+CU = ColorUtility(lines)
+
+
+def romaji(line, l):
+    # Setting up a delay, we will use it as duration time of the leadin and leadout effects
+    delay = 100
+    # Setting up offset variables, we will use them for the \move in leadin and leadout effects
+    off_x = 35
+    off_y = 15
+
+    # Leadin Effect
+    mDelay = 0
+    # lastStartTime = line.start_time
+    for syl in Utils.all_non_empty(line.syls):
+        l.layer = 0
+        # l.start_time = syl.end_time
+        l.start_time = line.start_time
+
+        # l.start_time = (
+        #     line.start_time + 25 * syl.i - delay - 80
+        # )  # Remove 80 to start_time to let leadin finish a little bit earlier than the main effect of the first syllable
+        # l.end_time = lastStartTime # wtf?
+        # lastStartTime = syl.start_time
+        l.end_time = syl.start_time
+        # l.end_time = line.start_time + syl.start_time # wtf?
+        l.dur = l.end_time - l.start_time
+        if l.dur <=0: continue
+
+        l.text = (
+            "{\\an5\\move(%.3f,%.3f,%.3f,%.3f,0,%d)\\blur2\\t(0,%d,\\blur0)\\fad(%d,0)}%s"
+            % (
+                syl.center + math.cos(syl.i / 2) * off_x,
+                syl.middle + math.sin(syl.i / 4) * off_y,
+                syl.center,
+                syl.middle,
+                delay,
+                delay,
+                delay,
+                syl.text,
+            )
+        )
+
+        io.write_line(l)
+
+    # Main Effect
+    for syl in Utils.all_non_empty(line.syls):
+        l.layer = 1
+
+        l.start_time = syl.start_time
+        # l.start_time = line.start_time + syl.start_time
+        l.end_time = syl.end_time + 100
+        l.dur = l.end_time - l.start_time
+
+        c1 = "&H81F4FF&"
+        c3 = "&H199AAA&"
+        # Change color if inline_fx is m1
+        if syl.inline_fx == "m1":
+            c1 = "&H8282FF&"
+            c3 = "&H191AAA&"
+
+        on_inline_effect_2 = ""
+        # Apply rotation if inline_fx is m2
+        if syl.inline_fx == "m2":
+            on_inline_effect_2 = "\\t(0,%d,\\frz%.3f)\\t(%d,%d,\\frz0)" % (
+                l.dur / 4,
+                random.uniform(-40, 40),
+                l.dur / 4,
+                l.dur,
+            )
+
+        l.text = (
+            "{\\an5\\pos(%.3f,%.3f)%s\\t(0,80,\\fscx105\\fscy105\\1c%s\\3c%s)\\t(80,%d,\\fscx100\\fscy100\\1c%s\\3c%s)}%s"
+            % (
+                syl.center,
+                syl.middle,
+                on_inline_effect_2,
+                c1,
+                c3,
+                l.dur - 80,
+                line.styleref.color1,
+                line.styleref.color3,
+                syl.text,
+            )
+        )
+
+        io.write_line(l)
+
+        # Animating star shape that jumps over the syllables
+        # Jump-in to the first syl
+        jump_height = 18
+        if syl.i == 0:
+            FU = FrameUtility(line.start_time - line.leadin / 2, line.start_time)
+            for s, e, i, n in FU:
+                l.start_time = s
+                l.end_time = e
+                frame_pct = i / n
+
+                x = syl.center - syl.width * (1 - frame_pct)
+                y = syl.top - math.sin(frame_pct * math.pi) * jump_height
+
+                alpha = 255
+                alpha += FU.add(0, syl.duration, -255)
+                alpha = Convert.alpha_dec_to_ass(int(alpha))
+
+                l.text = (
+                    "{\\alpha%s\\pos(%.3f,%.3f)\\bord1\\blur1\\1c%s\\3c%s\\p1}%s"
+                    % (alpha, x, y, c1, c3, star)
+                )
+                io.write_line(l)
+
+        # Jump to the next syl or to the end of line
+        jump_width = (
+            line.syls[syl.i + 1].center - syl.center
+            if syl.i != len(line.syls) - 1
+            else syl.width
+        )
+        # FU = FrameUtility(
+        #     line.start_time + syl.start_time, line.start_time + syl.end_time
+        # )
+        FU = FrameUtility(
+            syl.start_time, syl.end_time
+        )
+        for s, e, i, n in FU:
+            l.start_time = s
+            l.end_time = e
+            frame_pct = i / n
+
+            x = syl.center + frame_pct * jump_width
+            y = syl.top - math.sin(frame_pct * math.pi) * jump_height
+
+            alpha = 0
+            # Last jump should fade-out
+            if syl.i == len(line.syls) - 1:
+                alpha += FU.add(0, syl.duration, 255)
+            alpha = Convert.alpha_dec_to_ass(int(alpha))
+
+            l.text = "{\\alpha%s\\pos(%.3f,%.3f)\\bord1\\blur1\\1c%s\\3c%s\\p1}%s" % (
+                alpha,
+                x,
+                y,
+                c1,
+                c3,
+                star,
+            )
+            io.write_line(l)
+
+    # Leadout Effect
+    for syl in Utils.all_non_empty(line.syls):
+        l.layer = 0
+
+        l.start_time = syl.end_time
+        # l.start_time = line.start_time + syl.end_time + 100
+        l.end_time = line.end_time 
+        # l.end_time = line.end_time - 25 * (len(line.syls) - syl.i) + delay + 100
+        # l.end_time = line.end_time - 25 * (len(line.syls) - syl.i) + delay + 100
+        l.dur = l.end_time - l.start_time
+        if l.dur <= 0: continue
+
+        l.text = (
+            "{\\an5\\move(%.3f,%.3f,%.3f,%.3f,%d,%d)\\t(%d,%d,\\blur2)\\fad(0,%d)}%s"
+            % (
+                syl.center,
+                syl.middle,
+                syl.center + math.cos(syl.i / 2) * off_x,
+                syl.middle + math.sin(syl.i / 4) * off_y,
+                l.dur - delay,
+                l.dur,
+                l.dur - delay,
+                l.dur,
+                delay,
+                syl.text,
+            )
+        )
+
+        io.write_line(l)
+
+
+def kanji(line, l):
+    # Setting up a delay, we will use it as duration time of the leadin and leadout effects
+    delay = 300
+    # Setting up offset variables, we will use them for the \move in leadin and leadout effects
+    off_x = 35
+    off_y = 15
+
+    # Leadin Effect
+    for syl in Utils.all_non_empty(line.syls):
+        l.layer = 0
+        l.start_time = line.start_time
+
+        # l.start_time = (
+        #     line.start_time + 25 * syl.i - delay - 80
+        # )  # Remove 80 to start_time to let leadin finish a little bit earlier than the main effect of the first syllable
+        l.end_time =  syl.start_time
+        # l.end_time = line.start_time + syl.start_time
+        l.dur = l.end_time - l.start_time
+        if l.dur<=0: continue
+
+        l.text = (
+            "{\\an5\\move(%.3f,%.3f,%.3f,%.3f,0,%d)\\blur2\\t(0,%d,\\blur0)\\fad(%d,0)}%s"
+            % (
+                syl.center + math.cos(syl.i / 2) * off_x,
+                syl.middle + math.sin(syl.i / 4) * off_y,
+                syl.center,
+                syl.middle,
+                delay,
+                delay,
+                delay,
+                syl.text,
+            )
+        )
+
+        io.write_line(l)
+
+    # Main Effect
+    for syl in Utils.all_non_empty(line.syls):
+        l.layer = 1
+
+        l.start_time = syl.start_time
+        # l.start_time = line.start_time + syl.start_time
+        l.end_time = syl.end_time + 100
+        l.dur = l.end_time - l.start_time
+
+        c1 = "&H81F4FF&"
+        c3 = "&H199AAA&"
+        # Change color if effect field is m1
+        if line.effect == "m1":
+            c1 = "&H8282FF&"
+            c3 = "&H191AAA&"
+
+        on_inline_effect_2 = ""
+        # Apply rotation if effect field is m2
+        if line.effect == "m2":
+            on_inline_effect_2 = "\\t(0,%d,\\frz%.3f)\\t(%d,%d,\\frz0)" % (
+                l.dur / 4,
+                random.uniform(-40, 40),
+                l.dur / 4,
+                l.dur,
+            )
+
+        l.text = (
+            "{\\an5\\pos(%.3f,%.3f)%s\\t(0,80,\\fscx105\\fscy105\\1c%s\\3c%s)\\t(80,%d,\\fscx100\\fscy100\\1c%s\\3c%s)}%s"
+            % (
+                syl.center,
+                syl.middle,
+                on_inline_effect_2,
+                c1,
+                c3,
+                l.dur - 80,
+                line.styleref.color1,
+                line.styleref.color3,
+                syl.text,
+            )
+        )
+
+        io.write_line(l)
+
+    # Leadout Effect
+    for syl in Utils.all_non_empty(line.syls):
+        l.layer = 0
+
+        l.start_time =  syl.end_time + 100
+        # l.start_time = line.start_time + syl.end_time + 100
+        l.end_time = line.end_time 
+        # l.end_time = line.end_time - 25 * (len(line.syls) - syl.i) + delay + 100
+        l.dur = l.end_time - l.start_time
+        if l.dur<=0: continue
+
+        l.text = (
+            "{\\an5\\move(%.3f,%.3f,%.3f,%.3f,%d,%d)\\t(%d,%d,\\blur2)\\fad(0,%d)}%s"
+            % (
+                syl.center,
+                syl.middle,
+                syl.center + math.cos(syl.i / 2) * off_x,
+                syl.middle + math.sin(syl.i / 4) * off_y,
+                l.dur - delay,
+                l.dur,
+                l.dur - delay,
+                l.dur,
+                delay,
+                syl.text,
+            )
+        )
+
+        io.write_line(l)
+
+
+def sub(line, l):
+    # Translation Effect
+    l.layer = 0
+
+    l.start_time = line.start_time - line.leadin / 2
+    l.end_time = line.end_time + line.leadout / 2
+    l.dur = l.end_time - l.start_time
+
+    # Getting interpolated color changes (notice that we do that only after having set up all the times, that's important)
+    colors = CU.get_color_change(l)
+
+    # Base text
+    l.text = "{\\an5\\pos(%.3f,%.3f)\\fad(%d,%d)}%s" % (
+        line.center,
+        line.middle,
+        line.leadin / 2,
+        line.leadout / 2,
+        line.text,
+    )
+    io.write_line(l)
+
+    # Random clipped text colorated
+    l.layer = 1
+    for i in range(1, int(line.width / 80)):
+        x_clip = line.left + random.uniform(0, line.width)
+        y_clip = line.top - 5
+
+        clip = (
+            x_clip,
+            y_clip,
+            x_clip + random.uniform(10, 30),
+            y_clip + line.height + 10,
+        )
+
+        l.text = "{\\an5\\pos(%.3f,%.3f)\\fad(%d,%d)\\clip(%d,%d,%d,%d)%s}%s" % (
+            line.center,
+            line.middle,
+            line.leadin / 2,
+            line.leadout / 2,
+            clip[0],
+            clip[1],
+            clip[2],
+            clip[3],
+            colors,
+            line.text,
+        )
+        io.write_line(l)
+
+
+for line in lines:
+    # Generating lines
+    if line.styleref.alignment >= 7:
+        lineModSource = line.copy()
+        break
+    elif line.styleref.alignment >= 4:
+        lineModSource = line.copy()
+        break
     
+from test_pylrc import *
+# just a test.
+shiftAdjust = 600
+for elem in newTextArray:
+    lineMod = lineModSource.copy()
+    lineMod.start_time = max(0,elem['start']*1000 - shiftAdjust)
+    lineMod.end_time = elem['end']*1000 - shiftAdjust
+    lineMod.duration = lineMod.end_time  - lineMod.start_time
+    lineMod.text = elem['text'].strip().replace("  ","")
+    # print(lineMod)
+    lineMod.words = lineMod.text.split(" ")
+    # lineMod.syls = 
+    # print(dir(lineMod))
+    # breakpoint()
+    sylList = []
+    wordCount = len(lineMod.words)
+    sylDuration = (lineMod.end_time - lineMod.start_time)/wordCount
+    textLength = len(lineMod.text)
+    charShift = 30
+    absWordCenterShiftList = []
+    prevWordShift = 0
+    CENTER = 1600/2
+    wordWidthList = []
+    for word in lineMod.words:
+        wordWidth = len(word)* charShift
+        wordWidthList.append(wordWidth)
+        wordLength = len(word)+1
+        wordCenterShift = (charShift*wordLength)/2
+        wordShift = (charShift*wordLength)
+        absWordCenterShift = CENTER - (textLength*charShift)/2 + prevWordShift + wordCenterShift
+        absWordCenterShiftList.append(absWordCenterShift)
+        prevWordShift += wordShift
+    # CENTER + centerShift*charShift
+    getCenter = lambda index: absWordCenterShiftList[index]
+    getWidth = lambda index: wordWidthList[index]
+    mSylYShift = 700
+    for index, word in enumerate(lineMod.words):
+        syl = Syllable()
+        syl.text = word
+        syl.i = index
+        syl.center = getCenter(index)
+        syl.width = getWidth(index)
+        syl.top= 25+mSylYShift
+        syl.inline_fx = 'm2'
+        syl.middle = 49.0+mSylYShift
+        syl.bottom = 73.0+mSylYShift
+        syl.start_time = lineMod.start_time+ index*sylDuration
+        syl.end_time = syl.start_time+sylDuration
+        syl.duration = sylDuration
+        sylList.append(syl)
+    lineMod.syls = sylList
+    # print(lineMod.syls)
+    # breakpoint()
+    # pyonfx.ass_core.Syllable
+    source = lineMod.copy()
+    target = lineMod.copy()
+    romaji(source, target)
+    lineMod2 = lineMod.copy()
+    translateShift = -200
+    for i in range(len(lineMod2.syls)):
+        lineMod2.syls[i].top += translateShift
+        lineMod2.syls[i].middle += translateShift
+        lineMod2.syls[i].bottom += translateShift
+
+    source = lineMod2.copy()
+    target = lineMod2.copy()
+    # elif line.styleref.alignment >= 4:
+    kanji(source, target)
+    # else:
+    #     sub(line, line.copy())
+
+
+# how to deal with the tra
+
+io.save()
+# io.open_aegisub()
+sample_video = "/root/Desktop/works/pyjom/samples/video/karaoke_effects_source.mp4"
+io.open_mpv(video_path=sample_video) # ain't see shit...
