917d18f9c4625f97e691aeba6da104953d39d8f6
GitJournal <app@gitjournal.io>
2022-09-09 18:00:38 +0800

    [2022-09-09 10:00:35]
    FILE:
    tests/karaoke_effects/fastapi_translator.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/karaoke_effects/fastapi_translator.py
+++ tests/karaoke_effects/fastapi_translator.py
@@ -128,7 +128,7 @@ def changeProxy(useDirect=False, suggestSingleElemProbability=0.1):
     print("RESPONSE:", r.text)
     import parse
 
-    proxyName = parse.parse("", r.text)
+    proxyName = parse.parse("refresh proxy to {}", r.text)
     print("PROXY REFRESHED")
     return proxyName
 
