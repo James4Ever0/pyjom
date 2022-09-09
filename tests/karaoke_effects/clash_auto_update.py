bf3e2dbbfecaf904a32a3c9c6da830bf1252670e
GitJournal <app@gitjournal.io>
2022-09-09 14:33:10 +0800

    [2022-09-09 06:33:08]
    FILE:
    tests/karaoke_effects/clash_auto_update.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/karaoke_effects/clash_auto_update.py
+++ tests/karaoke_effects/clash_auto_update.py
@@ -35,7 +35,7 @@ def jsonLocate(jsonObj,location=[]):
         return None
 
 ## FIND DELAY ##
-def find_candidates(timeout=3000, urltest="https://m.tujia.com", test_url = "http://localhost:9090/proxies/", location = ["proxies","✋ 手动选择","all"], forbidden_names = ["DIRECT","REJECT","GLOBAL"]):
+def find_candidates(timeout=3000, urltest="https://m.tujia.com", test_url = "http://localhost:9911/proxies/", location = ["proxies","✋ 手动选择","all"], forbidden_names = ["DIRECT","REJECT","GLOBAL"]):
     import requests
     r = requests.get(test_url)
     import json
