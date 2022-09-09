b3a475adab432175cd6886b3460c36a1c147724c
GitJournal <app@gitjournal.io>
2022-09-09 20:22:14 +0800

    [2022-09-09 12:22:10]
    FILE:
    tests/karaoke_effects/lyrictoolbox.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/karaoke_effects/lyrictoolbox.py
+++ tests/karaoke_effects/lyrictoolbox.py
@@ -12,4 +12,62 @@ def getMusicDuration(musicPath):
 
 def lrcToTextArray(musicPath, lrcPath):
     assert lrcPath.endswith('.lrc')
-    
+    musicDuration = getMusicDuration(musicPath)
+
+lrc_file = open('/root/Desktop/works/pyjom/tests/music_analysis/exciting_bgm.lrc')
+lrc_string = ''.join(lrc_file.readlines())
+lrc_file.close()
+
+subs = pylrc.parse(lrc_string)
+
+lyricDurationThresholds = (0.5,4)
+
+textArray = []
+for sub in subs:
+    startTime = sub.time
+    text = sub.text
+    textArray.append((startTime, text))
+
+textArray.sort(key = lambda x: x[0])
+
+lastStartTime = textArray[0][0]
+
+newTextArray = [{'start':textArray[0][0],'text':textArray[0][1]}]
+
+for startTime, text in textArray[1:]:
+    if startTime-lastStartTime < lyricDurationThresholds[0]:
+        continue
+    else:
+        lastStartTime = startTime
+        newTextArray.append({'text': text, 'start': startTime})
+
+# now calculate the end time, please?
+# you may want to translate this if you have to.
+# when it does not contains anything in chinese.
+
+# using deepl?
+# put that aside please? focus on this shit...
+import numpy as np
+
+lyricDurations = [np.mean(lyricDurationThresholds)]
+
+for index,elem in enumerate(newTextArray):
+    text = elem['text']
+    start = elem['start']
+    nextIndex = index+1
+    if nextIndex < len(newTextArray):
+        nextElem = newTextArray[nextIndex]
+        nextStart = nextElem['start']
+        end = nextStart-start
+        if end > lyricDurationThresholds[0] and end < lyricDurationThresholds[1]:
+            lyricDurations.append(end)
+        end = min(end, lyricDurationThresholds[1], musicDuration-start)+ start
+    else:
+        end = np.mean(lyricDurations)+start
+        end = min(musicDuration, end)
+    newTextArray[index].update({'end':end})
+
+# for elem in newTextArray:
+#     print(elem)
+    return newTextArray
+# great. now let's do the freaking work?
