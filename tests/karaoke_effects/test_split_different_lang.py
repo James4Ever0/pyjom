9385ecf3d7a1167d0e614d1138b550e8cb50cafa
GitJournal <app@gitjournal.io>
2022-09-09 09:34:38 +0800

    [2022-09-09 01:34:35]
    FILE:
    tests/karaoke_effects/test_split_different_lang.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/karaoke_effects/test_split_different_lang.py
+++ tests/karaoke_effects/test_split_different_lang.py
@@ -118,4 +118,5 @@ for test in tests:
     # what you are going to do with this shit?
     if not isBilingual:
         print('checking main language')
-        for elem in test]
+        lyricString = " ".join(test)
+        mainLanguage = 
