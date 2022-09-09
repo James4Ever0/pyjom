1740258010071a536e0c8d25f0dc8ba3969f5b05
GitJournal <app@gitjournal.io>
2022-09-09 13:28:29 +0800

    [2022-09-09 05:28:26]
    FILE:
    tests/karaoke_effects/fastapi_translator.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/karaoke_effects/fastapi_translator.py
+++ tests/karaoke_effects/fastapi_translator.py
@@ -4,10 +4,14 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+import paddlehub as hub
+language_translation_model = hub.Module(name='baidu_translate')
+language_recognition_model = hub.Module(name='baidu_language_recognition')
+
 @app.get("/")
 def read_root():
     return {"message": "unified translator hooked on some clash server"}
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
+def read_item(item_id: int, backend: Union[str, None] = None,):
     return {"item_id": item_id, "q": q}
