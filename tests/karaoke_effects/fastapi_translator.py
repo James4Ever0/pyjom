2fa5e5d3dfc85c624ea9db9fe86717fde3fe74ea
GitJournal <app@gitjournal.io>
2022-09-09 16:19:40 +0800

    [2022-09-09 08:19:37]
    FILE:
    tests/karaoke_effects/fastapi_translator.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/karaoke_effects/fastapi_translator.py
+++ tests/karaoke_effects/fastapi_translator.py
@@ -1,5 +1,18 @@
 import os
 
+# before that, we need to fix cv2
+import pathlib
+import sys
+site_path = pathlib.Path("/usr/local/lib/python3.9/site-packages")
+cv2_libs_dir = (
+    site_path / "cv2" / f"python-{sys.version_info.major}.{sys.version_info.minor}"
+)
+print(cv2_libs_dir)
+cv2_libs = sorted(cv2_libs_dir.glob("*.so"))
+if len(cv2_libs) == 1:
+    print("INSERTING:", cv2_libs[0].parent)
+    sys.path.insert(1, str(cv2_libs[0].parent))
+
 clash_http_port = 8381
 os.environ['http_proxy'] = "http://127.0.0.1:{}".format(clash_http_port)
 os.environ['https_proxy'] = "http://127.0.0.1:{}".format(clash_http_port)
