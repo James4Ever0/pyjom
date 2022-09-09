a881ecfbb6bfdf46f4532687dcb1b20bcca4f1d7
GitJournal <app@gitjournal.io>
2022-09-09 20:20:40 +0800

    [2022-09-09 12:20:37]
    FILE:
    tests/karaoke_effects/lyrictoolbox.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

---
+++ tests/karaoke_effects/lyrictoolbox.py
@@ -0,0 +1,12 @@
+import pylrc
+from MediaInfo import MediaInfo
+
+def getMusicDuration(musicPath):
+    info = MediaInfo(filename = musicPath)
+    info = info.getInfo()
+    # print(info)
+    # breakpoint()
+    length = info['duration']
+    length = float(length)
+    return length
+
