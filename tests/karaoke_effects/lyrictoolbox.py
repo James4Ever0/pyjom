cb24e50d61ff029253e50e85fbfad5aabc7e4019
GitJournal <app@gitjournal.io>
2022-09-09 21:31:16 +0800

    [2022-09-09 13:31:12]
    FILE:
    tests/karaoke_effects/lyrictoolbox.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/karaoke_effects/lyrictoolbox.py
+++ tests/karaoke_effects/lyrictoolbox.py
@@ -638,6 +638,7 @@ def textArrayWithTranslatedListToAss(textArray, translatedList, assPath):
     # from test_pylrc import *
     # just a test.
     shiftAdjust = 600
+    newTextArray = 
     for elem in newTextArray:
         lineMod = lineModSource.copy()
         lineMod.start_time = max(0,elem['start']*1000 - shiftAdjust)
