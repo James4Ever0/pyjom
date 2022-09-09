9db71109b608bca0aebee049541d2888f787ad76
GitJournal <app@gitjournal.io>
2022-09-09 13:34:08 +0800

    [2022-09-09 05:34:04]
    FILE:
    tests/karaoke_effects/fastapi_translator.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/karaoke_effects/fastapi_translator.py
+++ tests/karaoke_effects/fastapi_translator.py
@@ -24,6 +24,20 @@ def baiduTranslator(text): # target language must be chinese.
         return None
 
 def deeplTranslator(text):
+        import requests
+        port = 8281
+        # env ROCKET_PORT=8281 ./executable_deepl
+        url = "http://localhost:{}/translate".format(port)
+        data = {"text": text, "source_lang": "auto", "target_lang": "ZH"}
+        r = requests.post(url, json=data)
+        response = r.json()
+        code = response['code']
+        if code == 200:
+            translatedText =  response['data']
+        else:
+            print("DEEPL RESPONSE ERROR. PLEASE CHECK")
+            print(response)
+            breakpoint()
 
 @app.get("/")
 def read_root():
