8204c44b1f81bbf242dfe73dd63c55982868db18
GitJournal <app@gitjournal.io>
2022-09-09 09:47:06 +0800

    [2022-09-09 01:47:03]
    FILE:
    tests/karaoke_effects/test_split_different_lang.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/karaoke_effects/test_split_different_lang.py
+++ tests/karaoke_effects/test_split_different_lang.py
@@ -147,3 +147,4 @@ if __name__ == "__main__":
                 else:
                     foreignText = text
                     nativeText = translate(foreignText)
+                newLyricArray.append(foreignText, nativeText)
