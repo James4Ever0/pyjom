339e348a6e4695fa403c4c22b2fc8f0ab886c404
GitJournal <app@gitjournal.io>
2022-09-09 14:24:04 +0800

    [2022-09-09 06:23:58]
    FILE:
    tests/karaoke_effects/loadSomeCustomClashYaml.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

---
+++ tests/karaoke_effects/loadSomeCustomClashYaml.py
@@ -0,0 +1,37 @@
+import yaml
+
+
+# yaml.add_constructor(mCustomLoaderTag, create_ref, Loader)
+def goYamlToPyYaml(docString):
+    docString = docString.replace("!<str>","!!str")
+    return docString
+def pyYamlToGoYaml(docString):
+    docString = docString.replace("!!str","!<str>")
+    return docString
+
+if __name__ == "__main__":
+
+    fileName = "Clash.yaml"
+
+    docString = open(fileName,"r").read()
+
+    mCustomLoaderTag = u'!<str>'
+
+    class Ref(object):
+        def __init__(self, data):
+            self.data = data
+        def __repr__(self):
+            return"\"%s\"" % self.data
+
+
+    def create_ref(loader,node):
+        # print(dir(loader))
+        # breakpoint()
+        value = loader.costruct_string(node)
+        return Ref(value)
+
+    class Loader(yaml.SafeLoader):
+        pass
+    docString = goYamlToPyYaml(docString)
+    a = yaml.load(docString)
+    print(a)
