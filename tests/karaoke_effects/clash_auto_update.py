577db4d90b7e1a3943598430bb76581f9b419b8c
GitJournal <app@gitjournal.io>
2022-09-09 15:06:25 +0800

    [2022-09-09 07:06:21]
    FILE:
    tests/karaoke_effects/clash_auto_update.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/karaoke_effects/clash_auto_update.py
+++ tests/karaoke_effects/clash_auto_update.py
@@ -132,10 +132,10 @@ def updateClashYaml(clashYamlPath = 'Clash3.yaml', control_port = 9911):
                 assert r.status_code == 204
                 # might be the problem. TODO: check why the fuck clash server cannot decode the config in utf-8 'unexpected end of data'
                 print("SUCCESSFULLY UPDATED THIS PROXY LIST")
+                return True
             except:
                 import traceback
                 traceback.print_exc()
                 # breakpoint()
                 print("SOME ERROR WHILE FETCHING CLASH OPENIT SCRIPT")
-                # TODO: fix this shit
-                time.sleep(3)
+                return False
