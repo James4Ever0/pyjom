06dc5d7e75334f17999a0e233a55b975a4ca2d37
GitJournal <app@gitjournal.io>
2022-09-09 10:04:53 +0800

    [2022-09-09 02:04:50]
    FILE:
    tests/karaoke_effects/test_split_different_lang.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/karaoke_effects/test_split_different_lang.py
+++ tests/karaoke_effects/test_split_different_lang.py
@@ -161,7 +161,9 @@ def translate(text, backend="deepl"):
     elif backend == 'baidu':
         # use paddlehub.
         # where is the damn code?
-        
+            language_code = language_recognition_model.recognize(text_prompts)
+            if language_code != 'zh':
+                text_prompts = language_translation_model.translate(text_prompts, language_code, 'zh')
 
 
 if __name__ == "__main__":
