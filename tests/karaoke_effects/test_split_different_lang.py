a8c61634e50aaaf084294d4b595edccec2551b25
GitJournal <app@gitjournal.io>
2022-09-09 10:07:15 +0800

    [2022-09-09 02:07:12]
    FILE:
    tests/karaoke_effects/test_split_different_lang.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/karaoke_effects/test_split_different_lang.py
+++ tests/karaoke_effects/test_split_different_lang.py
@@ -138,6 +138,16 @@ def getLyricsLanguageType(test):
 
 
 # before that, we need to fix cv2
+site_path = pathlib.Path("/usr/local/lib/python3.9/site-packages")
+    cv2_libs_dir = (
+        site_path / "cv2" / f"python-{sys.version_info.major}.{sys.version_info.minor}"
+    )
+    print(cv2_libs_dir)
+    cv2_libs = sorted(cv2_libs_dir.glob("*.so"))
+    if len(cv2_libs) == 1:
+        print("INSERTING:", cv2_libs[0].parent)
+        sys.path.insert(1, str(cv2_libs[0].parent))
+
 
 import paddlehub as hub
 language_translation_model = hub.Module(name='baidu_translate')
