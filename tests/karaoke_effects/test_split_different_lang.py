ab89b3a5733ec3acfd2ab6ec3dc15838c9e892df
GitJournal <app@gitjournal.io>
2022-09-09 16:18:50 +0800

    [2022-09-09 08:18:42]
    FILE:
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/karaoke_effects/test_split_different_lang.py
+++ tests/karaoke_effects/test_split_different_lang.py
@@ -140,21 +140,6 @@ def getLyricsLanguageType(test):
     return isBilingual, needToTranslate
 
 
-# before that, we need to fix cv2
-import pathlib
-import sys
-site_path = pathlib.Path("/usr/local/lib/python3.9/site-packages")
-cv2_libs_dir = (
-    site_path / "cv2" / f"python-{sys.version_info.major}.{sys.version_info.minor}"
-)
-print(cv2_libs_dir)
-cv2_libs = sorted(cv2_libs_dir.glob("*.so"))
-if len(cv2_libs) == 1:
-    print("INSERTING:", cv2_libs[0].parent)
-    sys.path.insert(1, str(cv2_libs[0].parent))
-
-
-
 def translate(text, backend="random"): # deepl is shit. fucking shit.
     # import time
     # time.sleep(delay)
