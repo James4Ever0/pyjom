f312d45e5ad55a8b114f069600892f3b9b449205
GitJournal <app@gitjournal.io>
2022-09-09 10:21:51 +0800

    [2022-09-09 02:21:48]
    FILE:
    tests/karaoke_effects/test_split_different_lang.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/karaoke_effects/test_split_different_lang.py
+++ tests/karaoke_effects/test_split_different_lang.py
@@ -162,6 +162,7 @@ def translate(text, backend="baidu", delay = 2): # deepl is shit. fucking shit.
     import time
     time.sleep(delay)
     assert backend in ["deepl", "baidu"]
+    
     if backend == "deepl":
         import requests
         port = 8281
