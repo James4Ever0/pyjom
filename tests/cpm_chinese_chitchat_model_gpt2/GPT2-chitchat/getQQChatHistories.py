094f9b2957af24c5c2eda3b23af1148a1c1a307a
GitJournal <app@gitjournal.io>
2022-09-10 15:11:56 +0800

    [2022-09-10 07:11:48]
    FILE:
    tests/cpm_chinese_chitchat_model_gpt2/GPT2-chitchat/getQQChatHistories.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

---
+++ tests/cpm_chinese_chitchat_model_gpt2/GPT2-chitchat/getQQChatHistories.py
@@ -0,0 +1,23 @@
+    import pandas as pd
+    # load sample data
+    dataPaths = ["/root/Desktop/works/pyjom/tasks/qq/qq_red_packet_collect/logs/redPacketLog_0.log","/root/Desktop/works/pyjom/tasks/qq/qq_red_packet_collect/logs/redPacketLog_1.log"]
+    # choose not to clean everything after the training, yet.
+    # import requests
+    tag = '[GROUP_TEXT_MESSAGE] '
+    for dataPath in dataPaths:
+        dataArray = []
+        import json
+        import parse
+        with open(dataPath, 'r') as f:
+            data = f.read()
+            for line in data.split('\n'):
+                line = line.strip()
+                if line.startswith(tag):
+                    try:
+                        mJson = parse.parse(tag+"{JSON}", line)
+                        mJson = json.loads(mJson['JSON'])
+                        dataArray.append(mJson)
+                    except:
+                        pass
+    df = pd.DataFrame(dataArray)
+    breakpoint()
