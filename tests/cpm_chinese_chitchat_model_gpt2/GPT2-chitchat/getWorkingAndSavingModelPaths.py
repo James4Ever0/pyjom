25c9b4743f05850adecd31d8c44632834af6a5b6
GitJournal <app@gitjournal.io>
2022-09-10 15:03:05 +0800

    [2022-09-10 07:03:01]
    FILE:
    tests/cpm_chinese_chitchat_model_gpt2/GPT2-chitchat/getWorkingAndSavingModelPaths.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/cpm_chinese_chitchat_model_gpt2/GPT2-chitchat/getWorkingAndSavingModelPaths.py
+++ tests/cpm_chinese_chitchat_model_gpt2/GPT2-chitchat/getWorkingAndSavingModelPaths.py
@@ -15,7 +15,7 @@ def getModelAndPaths(modelClass,modelPaths = ["/media/root/parrot/pyjom/tests/cp
         try:
             model = modelClass.from_pretrained(modelPath)
             workingModelPath = modelPath
-            print("USIG MODEL: %s" % workingModelPath)
+            print("using model: %s" % workingModelPath)
         except:
             pass
     if workingModelPath:
