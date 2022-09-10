edd31199ebcbe6e704c58871d65e745fc61ee0fb
GitJournal <app@gitjournal.io>
2022-09-10 17:55:31 +0800

    [2022-09-10 09:55:27]
    FILE:
    tests/cpm_chinese_chitchat_model_gpt2/GPT2-chitchat/gpt2_watchdog.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/cpm_chinese_chitchat_model_gpt2/GPT2-chitchat/gpt2_watchdog.py
+++ tests/cpm_chinese_chitchat_model_gpt2/GPT2-chitchat/gpt2_watchdog.py
@@ -3,7 +3,10 @@ import schedule
 
 def checkGPT2Status():
     gpt2status = getGPT2Status()
-    
+    if gpt2status == 'train':
+        trained = getGPT2Trained()
+        if not trained:
+            
 
 schedule.every(1).minute.do(checkGPT2Status) # shall place a flag if the training is complete.
 # schedule.every(1).minute.do(checkGPT2TrainServer)
