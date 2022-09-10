c1c7f15cd6ccb46e4617d376266a3a25f5963b6a
GitJournal <app@gitjournal.io>
2022-09-10 17:54:07 +0800

    [2022-09-10 09:54:04]
    FILE:
    tests/cpm_chinese_chitchat_model_gpt2/GPT2-chitchat/gpt2_watchdog.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/cpm_chinese_chitchat_model_gpt2/GPT2-chitchat/gpt2_watchdog.py
+++ tests/cpm_chinese_chitchat_model_gpt2/GPT2-chitchat/gpt2_watchdog.py
@@ -1,5 +1,5 @@
 import requests
 import schedule
 
-schedule.every(1).minute.do(checkGPT2Server)
-schedule.every(1).minute.do(checkGPT2Server)
+schedule.every(1).minute.do(checkGPT2EvalServer)
+schedule.every(1).minute.do(checkGPT2TrainServer)
