f5cbc636459d20e13052f1854169a2e3241b83fd
GitJournal <app@gitjournal.io>
2022-09-10 15:11:35 +0800

    [2022-09-10 07:11:31]
    FILE:
    tests/cpm_chinese_chitchat_model_gpt2/GPT2-chitchat/train_model_fastapi.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/cpm_chinese_chitchat_model_gpt2/GPT2-chitchat/train_model_fastapi.py
+++ tests/cpm_chinese_chitchat_model_gpt2/GPT2-chitchat/train_model_fastapi.py
@@ -268,29 +268,7 @@ def getTextEncoded(text):
 
 # port = 8729
 if __name__ == '__main__':
-    import pandas as pd
-    # load sample data
-    dataPaths = ["/root/Desktop/works/pyjom/tasks/qq/qq_red_packet_collect/logs/redPacketLog_0.log","/root/Desktop/works/pyjom/tasks/qq/qq_red_packet_collect/logs/redPacketLog_1.log"]
-    # choose not to clean everything after the training, yet.
-    # import requests
-    tag = '[GROUP_TEXT_MESSAGE] '
-    for dataPath in dataPaths:
-        dataArray = []
-        import json
-        import parse
-        with open(dataPath, 'r') as f:
-            data = f.read()
-            for line in data.split('\n'):
-                line = line.strip()
-                if line.startswith(tag):
-                    try:
-                        mJson = parse.parse(tag+"{JSON}", line)
-                        mJson = json.loads(mJson['JSON'])
-                        dataArray.append(mJson)
-                    except:
-                        pass
-    df = pd.DataFrame(dataArray)
-    breakpoint()
+
 #     # magic config from hackernoon.
 #     app.run(port=port, threaded=True, use_reloader=False)
 #     # https://hackernoon.com/deploying-deep-learning-models-with-model-server
