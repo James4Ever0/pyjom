e542834900dc1618010d8d96c5438eb4f5c2983f
GitJournal <app@gitjournal.io>
2022-09-10 15:43:05 +0800

    [2022-09-10 07:43:02]
    FILE:
    tests/cpm_chinese_chitchat_model_gpt2/GPT2-chitchat/liveTrainingData.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/cpm_chinese_chitchat_model_gpt2/GPT2-chitchat/liveTrainingData.py
+++ tests/cpm_chinese_chitchat_model_gpt2/GPT2-chitchat/liveTrainingData.py
@@ -60,9 +60,9 @@ def removeUnnecessaryPunctuation(text):
 
 if __name__ == '__main__':
     for source, target in getQQGroupChatData():
-        source = removeUnnecessaryPunctuation(source)
-        target = removeUnnecessaryPunctuation(target)
-        if len(source) > 3 and len(target)>3:
-            print("SOURCE: %s" % source)
-            print("TARGET: %s" % target)
-            print("_________________")
+        # source = removeUnnecessaryPunctuation(source)
+        # target = removeUnnecessaryPunctuation(target)
+        # if len(source) > 3 and len(target)>3:
+        #     print("SOURCE: %s" % source)
+        #     print("TARGET: %s" % target)
+        #     print("_________________")
