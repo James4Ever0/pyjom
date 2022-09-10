a496c973056bb5613b1b0e0f3ecc5f2ce6c840f3
GitJournal <app@gitjournal.io>
2022-09-10 15:12:41 +0800

    [2022-09-10 07:12:37]
    FILE:
    tests/cpm_chinese_chitchat_model_gpt2/GPT2-chitchat/liveTrainingData.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/cpm_chinese_chitchat_model_gpt2/GPT2-chitchat/liveTrainingData.py
+++ tests/cpm_chinese_chitchat_model_gpt2/GPT2-chitchat/liveTrainingData.py
@@ -1,4 +1,4 @@
-    
+def getQQGroupChatData():
     import pandas as pd
     # load sample data
     dataPaths = ["/root/Desktop/works/pyjom/tasks/qq/qq_red_packet_collect/logs/redPacketLog_0.log","/root/Desktop/works/pyjom/tasks/qq/qq_red_packet_collect/logs/redPacketLog_1.log"]
