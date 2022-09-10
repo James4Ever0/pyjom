c02ee75d399423f492d8efa40d76444db2665fcb
GitJournal <app@gitjournal.io>
2022-09-10 16:07:29 +0800

    [2022-09-10 08:07:27]
    FILE:
    tests/cpm_chinese_chitchat_model_gpt2/GPT2-chitchat/train_model_fastapi.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/cpm_chinese_chitchat_model_gpt2/GPT2-chitchat/train_model_fastapi.py
+++ tests/cpm_chinese_chitchat_model_gpt2/GPT2-chitchat/train_model_fastapi.py
@@ -309,7 +309,7 @@ if __name__ == '__main__':
     epochs = 30
     warmup_steps = 4000
     gradient_accumulation_steps = 4
-    t_total = len(train_dataloader_source) // args.gradient_accumulation_steps * 30
+    t_total = len(train_dataloader_source) // gradient_accumulation_steps * 30
     print("T_TOTAL:",t_total)
     scheduler = transformers.get_linear_schedule_with_warmup(
         optimizer, num_warmup_steps=warmup_steps, num_training_steps=t_total
