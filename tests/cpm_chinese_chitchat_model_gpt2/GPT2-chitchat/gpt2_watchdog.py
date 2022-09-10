ae90e4156bede312e6116d853133193e68121ba3
GitJournal <app@gitjournal.io>
2022-09-10 18:17:32 +0800

    [2022-09-10 10:17:29]
    FILE:
    tests/cpm_chinese_chitchat_model_gpt2/GPT2-chitchat/gpt2_watchdog.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/cpm_chinese_chitchat_model_gpt2/GPT2-chitchat/gpt2_watchdog.py
+++ tests/cpm_chinese_chitchat_model_gpt2/GPT2-chitchat/gpt2_watchdog.py
@@ -2,21 +2,27 @@ import requests
 import schedule
 import os
 import datetime
-os.chdir("/root/Desktop/works/pyjom/tests/cpm_chinese_chitchat_model_gpt2/GPT2-chitchat/")
+
+os.chdir(
+    "/root/Desktop/works/pyjom/tests/cpm_chinese_chitchat_model_gpt2/GPT2-chitchat/"
+)
+
 
 def getNow():
     return datetime.datetime.now()
-    
+
+
 def getGPT2Status():
     hour = getNow().hour
     if hour > 2 and hour < 8:
-        return 'train'
+        return "train"
     else:
-        return 'eval'
+        return "eval"
+
 
 def getGPT2TrainedStatus():
     try:
-        with open('trained.log', 'r+') as f:
+        with open("trained.log", "r+") as f:
             content = f.read()
         content = datetime.datetime.fromisoformat(content)
         day = content.day
@@ -24,32 +30,42 @@ def getGPT2TrainedStatus():
         return day == now.day
     except:
         import traceback
+
         traceback.print_exc()
         print("SOME ERROR WHEN CHECKING GPT2 TRAINED STATUS")
         breakpoint()
     return False
 
+
 # import os
 def startGPT2Training():
     print("START TRAINING")
-    os.system('/usr/bin/python3 train_model_fastapi.py')
+    os.system("/usr/bin/python3 train_model_fastapi.py")
+
 
 def markGPT2Trained():
-    with open('trained.log', 'r+') as f:
+    with open("trained.log", "r+") as f:
         content = getNow().isoformat()
         f.write(content)
     print("GPT2 TRAINED STATUS MARKED")
 
+
 import subprocess
+
 process = None
+
 
 def getGPT2Running():
     global process
     return process != None
 
+
 def startGPT2Server():
     global process
-    process = subprocess.Popen(['/usr/bin/python3','interact_fastapi.py','--model_path', '../model' ])
+    process = subprocess.Popen(
+        ["/usr/bin/python3", "interact_fastapi.py", "--model_path", "../model"]
+    )
+
 
 def terminateGPT2():
     global process
@@ -59,15 +75,17 @@ def terminateGPT2():
             print("GPT2 SERVER TERMINATED")
         except:
             import traceback
+
             traceback.print_exc()
             print("ERROR WHEN TERMINATING GPT2 SERVER")
             breakpoint()
     else:
         print("GPT2 IS NOT RUNNING. NO NEED TO TERMINATE")
 
+
 def checkGPT2Status():
     gpt2status = getGPT2Status()
-    if gpt2status == 'train':
+    if gpt2status == "train":
         terminateGPT2()
         trained = getGPT2TrainedStatus()
         if not trained:
@@ -75,18 +93,22 @@ def checkGPT2Status():
             markGPT2Trained()
         else:
             print("GPT2 is Trained Today.")
-    elif gpt2status == 'eval':
+    elif gpt2status == "eval":
         running = getGPT2Running()
         if not running:
             startGPT2Server()
         else:
             print("GPT2 is running...")
 
-schedule.every(1).minute.do(checkGPT2Status) # shall place a flag if the training is complete.
+
+schedule.every(1).minute.do(
+    checkGPT2Status
+)  # shall place a flag if the training is complete.
 # schedule.every(1).minute.do(checkGPT2TrainServer)
 
 import time
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     checkGPT2Status()
     while True:
         time.sleep(10)
