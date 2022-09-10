76708dce9ee2eada5e01f458bba8c2c200101b7a
GitJournal <app@gitjournal.io>
2022-09-10 16:44:56 +0800

    [2022-09-10 08:44:52]
    FILE:
    tests/cpm_chinese_chitchat_model_gpt2/GPT2-chitchat/train_model_fastapi.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/cpm_chinese_chitchat_model_gpt2/GPT2-chitchat/train_model_fastapi.py
+++ tests/cpm_chinese_chitchat_model_gpt2/GPT2-chitchat/train_model_fastapi.py
@@ -185,7 +185,7 @@ def train_epoch(model, train_dataloader, optimizer, scheduler, logger,
     # epoch_correct_num:每个epoch中,output预测正确的word的数量
     # epoch_total_num: 每个epoch中,output预测的word的总数量
     epoch_correct_num, epoch_total_num = 0, 0
-
+    import progressbar
     for batch_idx, (input_ids, labels) in enumerate(train_dataloader):
         # 捕获cuda out of memory exception
         try:
