29e88754dd5753b14ccee1790bb5ebf86c4d6a9e
GitJournal <app@gitjournal.io>
2022-09-10 14:12:13 +0800

    [2022-09-10 06:12:10]
    FILE:
    tests/cpm_chinese_chitchat_model_gpt2/GPT2-chitchat/train_model_fastapi.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/cpm_chinese_chitchat_model_gpt2/GPT2-chitchat/train_model_fastapi.py
+++ tests/cpm_chinese_chitchat_model_gpt2/GPT2-chitchat/train_model_fastapi.py
@@ -144,7 +144,7 @@ tokenizer = BertTokenizerFast(
     vocab_file=args.vocab_path, sep_token="[SEP]", pad_token="[PAD]", cls_token="[CLS]")
 # tokenizer = BertTokenizer(vocab_file=args.voca_path)
 
-def get
+def getPathAccessTime(path):
 
 modelPaths = []
 # sort them with time.
