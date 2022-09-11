2ebe54ac13bba96fb5822691613aac01c5935ab4
GitJournal <app@gitjournal.io>
2022-09-11 15:23:43 +0800

    [2022-09-11 07:23:41]
    FILE:
    tests/random_giphy_gifs/test_api.js
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/random_giphy_gifs/test_api.js
+++ tests/random_giphy_gifs/test_api.js
@@ -1,7 +1,7 @@
 // Require with custom API key
 // const myBetaApiKey = 'IoJVsWoxDPKBr6gOcCgOPWAB25773hqP';
 const myBetaApiKey = "Gc7131jiJuvI7IdN0HZ1D7nh0ow5BU6g"; // some common web browser based things.
-// can we prepare some k
+// can we prepare some key server? i don't know. wtf is this shit?
 
 var giphy = require('giphy-api')(myBetaApiKey);
 const JsonFormat = require("json-format")
