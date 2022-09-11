31806ad2fbe56929698f0b00938b9e21e401cf44
GitJournal <app@gitjournal.io>
2022-09-11 11:28:04 +0800

    [2022-09-11 03:28:00]
    FILE:
    tests/youtube_shorts_heuristic_search/heuristic_model.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/youtube_shorts_heuristic_search/heuristic_model.py
+++ tests/youtube_shorts_heuristic_search/heuristic_model.py
@@ -4,3 +4,5 @@ seed = 'dog cute'
 def getSearchQueryFromHeuristicSpace(seed):
     # less likely to repeat, and more possibility to get needed videos.
     return searchQuery
+
+# this heuristic search 
