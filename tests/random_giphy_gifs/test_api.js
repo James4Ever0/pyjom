13cb2b45ff3b53868067e7892e7c20833cf9c6a6
GitJournal <app@gitjournal.io>
2022-09-11 13:16:49 +0800

    [2022-09-11 05:16:46]
    FILE:
    tests/random_giphy_gifs/test_api.js
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/random_giphy_gifs/test_api.js
+++ tests/random_giphy_gifs/test_api.js
@@ -2,11 +2,9 @@
 const myBetaApiKey = 'IoJVsWoxDPKBr6gOcCgOPWAB25773hqP';
 var giphy = require('giphy-api')(myBetaApiKey);
 const fs = require("fs");
-const jsonFormat = require("json-format");
 
 function writeJsonToFile(json, filename) {
-    // let data = JSON.stringify(json);
-    let data = jsonFormat(json);
+    let data = JSON.stringify(json);
     fs.writeFile(filename, data, function(err) {
         if (err) {
             console.error(err);
@@ -18,24 +16,28 @@ function writeJsonToFile(json, filename) {
 // // Require with the public beta key
 // var giphy = require('giphy-api')(); // banned. cannot use this public api.
 // it may timeout!
-
-// giphy.search({
-//     q: 'pokemon',
-//     rating: 'g'
-// }, function(err, res) {
-//     // Res contains gif data!
-//     console.log('ERROR?', err); //null if normal.
+giphy.search({
+    q: 'pokemon',
+    rating: 'g'
+}, function(err, res) {
+    // Res contains gif data!
+    console.log('ERROR?', err); //null if normal.
+    // save it to json?
+    writeJsonToFile(res, 'pokemon_test.json');
+});    console.log('ERROR?', err); //null if normal.
 //     // save it to json?
 //     writeJsonToFile(res, 'pokemon_test.json');
 // });
 
 
-giphy.search({
-    q: 'pokemon',
-    rating: 'y'
-}, function(err, res) {
-    // Res contains gif data!
-    console.log('ERROR?', err); //null if normal.
-    // save it to json?
-    writeJsonToFile(res, 'pokemon_test_youth.json');
-});
+// giphy.search({
+//     q: 'pokemon',
+//     rating: 'y'
+// }, function(err, res) {
+//     // Res contains gif data!
+//     console.log('ERROR?', err); //null if normal.
+//     // save it to json?
+//     writeJsonToFile(res, 'pokemon_test_youth.json');
+// });
+
+// question: is that still image?
