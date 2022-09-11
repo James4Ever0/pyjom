73fa4526d873c2e45cf009ce1583970e7795586e
GitJournal <app@gitjournal.io>
2022-09-11 15:15:12 +0800

    [2022-09-11 07:15:09]
    FILE:
    tests/random_giphy_gifs/can_we_get_tag_info_about_this.sh
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/random_giphy_gifs/can_we_get_tag_info_about_this.sh
+++ tests/random_giphy_gifs/can_we_get_tag_info_about_this.sh
@@ -1,3 +1,3 @@
 curl -o samoyed.html "https://giphy.com/gifs/roverdotcom-rover-samoyed-gifofdogs-AgO9VR2a9KW1MSP73I"
 
-# tag is probably handled by internal recommendation engine.
+# tag is probably used by internal recommendation engine.
