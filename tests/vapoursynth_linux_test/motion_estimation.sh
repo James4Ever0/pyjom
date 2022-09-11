3ba39dbbf7d0fe1b32dc9295ea5d66500cc4ff87
GitJournal <app@gitjournal.io>
2022-09-12 00:36:40 +0800

    [2022-09-11 16:36:38]
    FILE:
    tests/vapoursynth_linux_test/motion_estimation.sh
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/vapoursynth_linux_test/motion_estimation.sh
+++ tests/vapoursynth_linux_test/motion_estimation.sh
@@ -6,7 +6,7 @@
 # not just toy, but can find PIP
 # picture in picture, crop detect?
 
-ffmpeg -i "/root/Desktop/works/pyjom/samples/video/LiEIfnsvn.mp4" -flags2 +export_mvs -vf "mestimate=epzs:mb_size=16:search_param=7,cropdetect=mode=mvedges,metadata=mode=print" -f null - # no printing?
+ffmpeg -i "/root/Desktop/works/pyjom/samples/video/LiEIfnsvn.mp4" -flags2 +export_mvs -vf "fps=1,mestimate=epzs:mb_size=16:search_param=7,cropdetect=mode=mvedges,metadata=mode=print" -f null - # no printing?
 # ffmpeg -i "/root/Desktop/works/pyjom/samples/video/LiEIfnsvn.mp4" -vf "mestimate,cropdetect=mode=mvedges,metadata=mode=print" -f null -
 
 # get help on filter:
