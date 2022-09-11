23dc5886d63ab672f4dfc4e48386371885119ee5
GitJournal <app@gitjournal.io>
2022-09-11 22:15:00 +0800

    [2022-09-11 14:14:54]
    FILE:
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/vapoursynth_linux_test/motion_estimation.sh
+++ tests/vapoursynth_linux_test/motion_estimation.sh
@@ -3,3 +3,5 @@
 
 # ffmpeg -i "/root/Desktop/works/pyjom/tests/random_giphy_gifs/samoyed.gif" -vf "mestimate=epzs:mb_size=16:search_param=7, codecview=mv=pf+bf+bb"  mestimate_output.mp4 -y
 
+# not just toy, but can find PIP
+# pict
