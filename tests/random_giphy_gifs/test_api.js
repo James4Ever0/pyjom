d547d2f94b5b87c04c2636afb2bd5674dfd9ba78
GitJournal <app@gitjournal.io>
2022-09-11 12:45:12 +0800

    [2022-09-11 04:45:09]
    FILE:
    tests/random_giphy_gifs/test_api.js
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/random_giphy_gifs/test_api.js
+++ tests/random_giphy_gifs/test_api.js
@@ -4,12 +4,12 @@ var giphy = require('giphy-api')(myBetaApiKey);
 const fs = require("fs");
 
 function writeJsonToFile(json, filename) {
-    let data = JSON.stringify(user);
+    let data = JSON.stringify(json);
     fs.writeFile(filename, data, function(err) {
         if (err) {
             console.error(err);
         } else {
-            console.log(filename + " has been saved with the user data");
+            console.log(filename + " has been saved with the json data");
         }
     });
 }
