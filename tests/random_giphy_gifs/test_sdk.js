ae041ec9d21cb32a09d24dd37c1eb33eb2185e7e
GitJournal <app@gitjournal.io>
2022-09-11 14:47:05 +0800

    [2022-09-11 06:47:02]
    FILE:
    tests/random_giphy_gifs/test_sdk.js
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/random_giphy_gifs/test_sdk.js
+++ tests/random_giphy_gifs/test_sdk.js
@@ -43,7 +43,7 @@ async function test(){
 // var data = await gf.related(relatedId, { limit: 50 })
 // await writeJsonToFile(data,'related.json')
 
-const { category } = await gf.categories()
+const { data:categories } = await gf.categories()
 categories.forEach((category) => {
     console.log(category) // ICategory
 })
