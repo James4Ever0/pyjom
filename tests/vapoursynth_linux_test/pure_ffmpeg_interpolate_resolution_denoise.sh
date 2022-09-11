9ff39bbdc835261a1c7d4295fef0974d55603eee
GitJournal <app@gitjournal.io>
2022-09-11 18:33:14 +0800

    [2022-09-11 10:33:12]
    FILE:
    tests/vapoursynth_linux_test/pure_ffmpeg_interpolate_resolution_denoise.sh
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/vapoursynth_linux_test/pure_ffmpeg_interpolate_resolution_denoise.sh
+++ tests/vapoursynth_linux_test/pure_ffmpeg_interpolate_resolution_denoise.sh
@@ -1,2 +1,2 @@
-ffmpeg -y -i "/root/Desktop/works/pyjom/tests/random_giphy_gifs/samoyed.gif" -vf "scale=w=iw*2:h=ih*2:flags=lanczos,minterpolate,hqdn3d" -r 60 ffmpeg_samoyed.mp4
+ffmpeg -y -i "/root/Desktop/works/pyjom/tests/random_giphy_gifs/samoyed.gif" -vf "hqdn3d,scale=w=iw*2:h=ih*2:flags=lanczos,minterpolate," -r 60 ffmpeg_samoyed.mp4
 # ffmpeg -y -i "/root/Desktop/works/pyjom/tests/random_giphy_gifs/samoyed.gif" -filter "minterpolate=mi_mode=2" -r 60 ffmpeg_samoyed.mp4
