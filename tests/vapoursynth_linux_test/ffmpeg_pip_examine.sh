1be3498747fa7871d397a83a39a0fb3a19920aba
GitJournal <app@gitjournal.io>
2022-09-12 01:02:19 +0800

    [2022-09-11 17:02:16]
    FILE:
    tests/vapoursynth_linux_test/ffmpeg_pip_examine.sh
    tests/vapoursynth_linux_test/pip_examine.sh
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/vapoursynth_linux_test/ffmpeg_pip_examine.sh
+++ tests/vapoursynth_linux_test/ffmpeg_pip_examine.sh
@@ -1,2 +1,3 @@
 mkdir pip_examine
-cat pip_motion_cropdetect.log | awk -F 't:' '{print $2}' | awk '{print "ffmpeg -ss " $1 " -i /root/Desktop/works/pyjom/samples/video/LiEIfnsvn.mp4 -vframes 1 pip_examine/screenshot_" i++ ".jpg" }' > pip_exam
+cat pip_motion_cropdetect.log | awk -F 't:' '{print $2}' | awk '{print "ffmpeg -ss " $1 " -i /root/Desktop/works/pyjom/samples/video/LiEIfnsvn.mp4 -vframes 1 pip_examine/screenshot_" i++ ".jpg" }' > pip_examine.sh
+bash pip_examine.sh
