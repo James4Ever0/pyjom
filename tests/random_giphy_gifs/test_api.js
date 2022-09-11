4587e18f5a8b8ecc8e4e49c7840e83ba8df1e24e
GitJournal <app@gitjournal.io>
2022-09-11 12:31:17 +0800

    [2022-09-11 04:31:14]
    FILE:
    tests/random_giphy_gifs/test_api.js
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/random_giphy_gifs/test_api.js
+++ tests/random_giphy_gifs/test_api.js
@@ -1,5 +1,5 @@
 // Require with custom API key
 const myApiKey = '';
-// var giphy = require('giphy-api')(myApi);
+// var giphy = require('giphy-api')(myApiKey);
 // // Require with the public beta key
 var giphy = require('giphy-api')();
