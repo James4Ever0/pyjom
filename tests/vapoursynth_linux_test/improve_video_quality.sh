d6e3b3352e7104abb8c6d07d1826052993e30443
GitJournal <app@gitjournal.io>
2022-09-11 18:09:09 +0800

    [2022-09-11 10:09:01]
    FILE:
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/vapoursynth_linux_test/improve_video_quality.sh
+++ tests/vapoursynth_linux_test/improve_video_quality.sh
@@ -1,2 +1,3 @@
 vspipe -c y4m denoise_and_upscale_samoyed.py - | ffmpeg -y -i pipe: -vf scale=w=in_w*2:h=in_h*2:flags=lanczos improved.mp4
 # vspipe -c y4m denoise_and_upscale_samoyed.py - | ffmpeg -y -i pipe: output.bmp
+# https://write.corbpie.com/upscaling-and-downscaling-video-with-ffmpeg/
