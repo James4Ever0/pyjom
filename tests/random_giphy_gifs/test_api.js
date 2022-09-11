185817dbcd4208ed47988ca89326b45685a18d9a
GitJournal <app@gitjournal.io>
2022-09-11 13:23:58 +0800

    [2022-09-11 05:23:55]
    FILE:
    tests/random_giphy_gifs/test_api.js
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/random_giphy_gifs/test_api.js
+++ tests/random_giphy_gifs/test_api.js
@@ -16,15 +16,15 @@ function writeJsonToFile(json, filename) {
 // // Require with the public beta key
 // var giphy = require('giphy-api')(); // banned. cannot use this public api.
 // it may timeout!
-giphy.search({
-    q: 'pokemon',
-    rating: 'g'
-}, function(err, res) {
-    // Res contains gif data!
-    console.log('ERROR?', err); //null if normal.
-    // save it to json?
-    writeJsonToFile(res, 'pokemon_test.json');
-});   
+// giphy.search({
+//     q: 'pokemon',
+//     rating: 'g'
+// }, function(err, res) {
+//     // Res contains gif data!
+//     console.log('ERROR?', err); //null if normal.
+//     // save it to json?
+//     writeJsonToFile(res, 'pokemon_test.json');
+// });   
 //     // save it to json?
 //     writeJsonToFile(res, 'pokemon_test.json');
 // });
