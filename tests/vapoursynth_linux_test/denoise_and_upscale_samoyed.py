d2e8895c8f479beed75102a78a8be5695ee0abb6
GitJournal <app@gitjournal.io>
2022-09-11 15:34:47 +0800

    [2022-09-11 07:34:44]
    FILE:
    tests/vapoursynth_linux_test/denoise_and_upscale_samoyed.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/vapoursynth_linux_test/denoise_and_upscale_samoyed.py
+++ tests/vapoursynth_linux_test/denoise_and_upscale_samoyed.py
@@ -3,7 +3,7 @@
 
 # apply some filter on video size and duration first, please?
 
-videoPath = "/root/Desktop/works/pyjom/tests/random_giphy_gifs/samo.gif"
+videoPath = "/root/Desktop/works/pyjom/tests/random_giphy_gifs/samoyed.gif"
 # videoPath = "/root/Desktop/works/pyjom/tests/random_giphy_gifs/pikachu.gif"
 
 import vapoursynth
