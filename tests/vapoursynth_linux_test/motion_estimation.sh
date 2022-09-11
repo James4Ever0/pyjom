d1c9ed0e9f2dfaac9603a4c0e7457fd679276ce9
GitJournal <app@gitjournal.io>
2022-09-11 22:07:52 +0800

    [2022-09-11 14:07:50]
    FILE:
    tests/vapoursynth_linux_test/motion_estimation.sh
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/vapoursynth_linux_test/motion_estimation.sh
+++ tests/vapoursynth_linux_test/motion_estimation.sh
@@ -1,2 +1,2 @@
 
-ffmpeg -i  -vf "mestimate=epzs:mb_size=16:search_param=7, codecview=mv=pf+bf+bb" -c:v libx265 mestimate_output.mp4 -y
+ffmpeg -i  -vf "mestimate=epzs:mb_size=16:search_param=7, codecview=mv=pf+bf+bb"  mestimate_output.mp4 -y
