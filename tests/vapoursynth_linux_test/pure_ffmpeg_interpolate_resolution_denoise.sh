d9fcb067e0aa0a1b6e30d5ef81cd0aa55a2ec92d
GitJournal <app@gitjournal.io>
2022-09-12 00:24:16 +0800

    [2022-09-11 16:24:14]
    FILE:
    tests/vapoursynth_linux_test/pure_ffmpeg_interpolate_resolution_denoise.sh
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/vapoursynth_linux_test/pure_ffmpeg_interpolate_resolution_denoise.sh
+++ tests/vapoursynth_linux_test/pure_ffmpeg_interpolate_resolution_denoise.sh
@@ -10,7 +10,7 @@
 
 # strange shit.
 # env LD_LIBRARY_PATH=/root/anaconda3/pkgs/cudatoolkit-10.0.130-0/lib/:/root/anaconda3/pkgs/cudnn-7.6.5-cuda10.0_0/lib/:$LD_LIBRARY_PATH ffmpeg -i "/root/Desktop/works/pyjom/tests/random_giphy_gifs/samoyed.gif" -y -vf "sr=dnn_backend=tensorflow:model=./sr/espcn.pb:,yaepblur,hqdn3d"  ffmpeg_samoyed_srcnn.mp4
-env LD_LIBRARY_PATH=/root/anaconda3/pkgs/cudatoolkit-10.0.130-0/lib/:/root/anaconda3/pkgs/cudnn-7.6.5-cuda10.0_0/lib/:$LD_LIBRARY_PATH ffmpeg -i "/root/Desktop/works/pyjom/tests/random_giphy_gifs/samoyed.gif" -y -vf "sr=dnn_backend=tensorflow:model=./sr/espcn.pb,yaepblur,hqdn3d,dctdnoiz"  ffmpeg_samoyed_srcnn.mp4
+env LD_LIBRARY_PATH=/root/anaconda3/pkgs/cudatoolkit-10.0.130-0/lib/:/root/anaconda3/pkgs/cudnn-7.6.5-cuda10.0_0/lib/:$LD_LIBRARY_PATH ffmpeg -i "/root/Desktop/works/pyjom/tests/random_giphy_gifs/samoyed.gif" -y -vf "sr=dnn_backend=tensorflow:model=./sr/espcn.pb,yaepblur,hqdn3d,dctdnoiz"  ffmpeg_samoyed_dctdnoiz.mp4
 
 # dctdnoiz is not for real time processing. it is slow.
 # but somehow it makes the picture great. is it?
