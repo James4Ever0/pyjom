b494a84f17e64a6a3f7424266cd221ccb5fd151e
GitJournal <app@gitjournal.io>
2022-09-11 17:46:07 +0800

    [2022-09-11 09:46:04]
    FILE:
    tests/vapoursynth_linux_test/denoise_and_upscale_samoyed.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/vapoursynth_linux_test/denoise_and_upscale_samoyed.py
+++ tests/vapoursynth_linux_test/denoise_and_upscale_samoyed.py
@@ -51,8 +51,10 @@ scale = 2
 # video = core.resize.Lanczos(clip=video, format=vs.YUV420P10, matrix_s="709",
 #                         primaries_in_s="2020",  primaries_s="709",
 #                         transfer_in_s="linear", transfer_s="709")
-video = core.rcnv.RealCUGAN(video , scale=scale, 
-                  gpu_id=gpu_id, model=1)
+
+
+# video = core.rcnv.RealCUGAN(video , scale=scale, 
+#                   gpu_id=gpu_id, model=1)
 
 # solution from tonemap?
 # https://github.com/ifb/vapoursynth-tonemap/issues/2
