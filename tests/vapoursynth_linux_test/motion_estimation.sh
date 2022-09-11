45b210d03ab0a359ec8e0ce0c637811560322122
GitJournal <app@gitjournal.io>
2022-09-11 22:10:16 +0800

    [2022-09-11 14:10:14]
    FILE:
    tests/vapoursynth_linux_test/motion_estimation.sh
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/vapoursynth_linux_test/motion_estimation.sh
+++ tests/vapoursynth_linux_test/motion_estimation.sh
@@ -1,2 +1,4 @@
 
+# output motion vectors.
+
 ffmpeg -i "/root/Desktop/works/pyjom/tests/random_giphy_gifs/samoyed.gif" -vf "mestimate=epzs:mb_size=16:search_param=7, codecview=mv=pf+bf+bb"  mestimate_output.mp4 -y
