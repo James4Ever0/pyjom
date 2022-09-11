7cd69491e3ae8b49d6890f49a7e68ace6d2df5d2
GitJournal <app@gitjournal.io>
2022-09-11 13:00:49 +0800

    [2022-09-11 05:00:42]
    FILE:
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/random_giphy_gifs/download_webp.sh
+++ tests/random_giphy_gifs/download_webp.sh
@@ -1,3 +1,3 @@
-# curl -o pikachu.webp "https://media0.giphy.com/media/fSvqyvXn1M3btN8sDh/giphy.webp?cid=c32f918edh7reod7g89e9oyy0717c9jstsdms9wqs8sm6a5b&rid=giphy.webp&ct=g"
+# curl -o pikachu.webp "https://media0.giphy.com/media/fSvqyvXn1M3btN8sDh/giphy.webp?cid=c32f918edh7reod7g89e9oyy0717c9jstsdms9wqs8sm6a5b&rid=giphy.webp&ct=g" # not supported. ffmpeg does not buy it.
 # very fucking slow if not using proxy.
 curl -o pikachu.gif "https://media0.giphy.com/media/fSvqyvXn1M3btN8sDh/giphy.gif?cid=c32f918edh7reod7g89e9oyy0717c9jstsdms9wqs8sm6a5b&rid=giphy.gif&ct=g"
