4d48e0e92fb54ea6e528b4f918171e3c6d2849eb
GitJournal <app@gitjournal.io>
2022-09-11 16:43:11 +0800

    [2022-09-11 08:43:04]
    FILE:
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/vapoursynth_linux_test/basic_test.py
+++ tests/vapoursynth_linux_test/basic_test.py
@@ -6,7 +6,5 @@ video = core.ffms2.Source(source=videoPath)
 video = core.std.Transpose(video)
 video.set_output()
 
-
-
 # vspipe is a wrapper around existing apis. vapoursynth can only generate raw frame data so we cannot encode video here alone. maybe we need opencv for this?
 # opencv preview https://github.com/UniversalAl/view
