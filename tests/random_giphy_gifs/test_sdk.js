3b4ec87f39793bf186e72c970519c92fc8972641
GitJournal <app@gitjournal.io>
2022-09-11 14:24:11 +0800

    [2022-09-11 06:24:08]
    FILE:
    tests/random_giphy_gifs/test_sdk.js
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/random_giphy_gifs/test_sdk.js
+++ tests/random_giphy_gifs/test_sdk.js
@@ -23,13 +23,15 @@ function writeJsonToFile(json, filename) {
     });
 }
 
-let data = await gf.trending({ limit: 10 }) // a promise
 // console.log(data)
 // https://bobbyhadz.com/blog/javascript-error-err-require-esm-of-es-module-node-fetch
 // fucking hell?
 // data.then((result) =>{console.log('TRENDING OUTPUT');
 // writeJsonToFile(result, 'trending.json')
 // })
-async function test(){writeJsonToFile(data,'trending.json')
+async function test(){
+let data = await gf.trending({ limit: 10 }) // a promise
+    
+    writeJsonToFile(data,'trending.json')
 }
 test()
