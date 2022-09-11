19e4a8d1d6530bc9e65076092295011a48114c5b
GitJournal <app@gitjournal.io>
2022-09-11 17:16:12 +0800

    [2022-09-11 09:16:09]
    FILE:
    tests/vapoursynth_linux_test/denoise_and_upscale_samoyed.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/vapoursynth_linux_test/denoise_and_upscale_samoyed.py
+++ tests/vapoursynth_linux_test/denoise_and_upscale_samoyed.py
@@ -39,7 +39,7 @@ scale = 2
 # video = core.srmdnv.SRMD(video,scale=scale, noise=noise, 
 #                   gpu_id=gpu_id)
 
-video = core.rcnv.RealCUGAN(video , scale=scale, noise=noise, 
+video = core.rcnv.RealCUGAN(video , scale=scale, 
                   gpu_id=gpu_id, model=1)
 
 video.set_output()
