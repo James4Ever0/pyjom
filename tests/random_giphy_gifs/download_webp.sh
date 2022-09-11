f48bb7fc1e6763a8f5ec2d39e9fa0d1eeac13806
GitJournal <app@gitjournal.io>
2022-09-11 12:54:50 +0800

    [2022-09-11 04:54:48]
    FILE:
    tests/random_giphy_gifs/download_webp.sh
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/random_giphy_gifs/download_webp.sh
+++ tests/random_giphy_gifs/download_webp.sh
@@ -1,1 +1,1 @@
-curl - "https://media0.giphy.com/media/fSvqyvXn1M3btN8sDh/giphy.webp?cid=c32f918edh7reod7g89e9oyy0717c9jstsdms9wqs8sm6a5b&rid=giphy.webp&ct=g"
+curl -o pikachu.webp "https://media0.giphy.com/media/fSvqyvXn1M3btN8sDh/giphy.webp?cid=c32f918edh7reod7g89e9oyy0717c9jstsdms9wqs8sm6a5b&rid=giphy.webp&ct=g"
