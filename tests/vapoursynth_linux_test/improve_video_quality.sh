1c86c5c4e0a262feb788f0ae58374505f1c8a58d
GitJournal <app@gitjournal.io>
2022-09-11 18:06:08 +0800

    [2022-09-11 10:06:05]
    FILE:
    tests/vapoursynth_linux_test/improve_video_quality.sh
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/vapoursynth_linux_test/improve_video_quality.sh
+++ tests/vapoursynth_linux_test/improve_video_quality.sh
@@ -1,2 +1,2 @@
-vspipe -c y4m denoise_and_upscale_samoyed.py - | ffmpeg -y -i pipe: -vf scale=w:flags=lanczos improved.mp4
+vspipe -c y4m denoise_and_upscale_samoyed.py - | ffmpeg -y -i pipe: -vf scale=w=in_w*2:h=in_h*2:flags=lanczos improved.mp4
 # vspipe -c y4m denoise_and_upscale_samoyed.py - | ffmpeg -y -i pipe: improved.mp4
