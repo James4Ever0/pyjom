c8d0c3af3a49fd601b3ea3d402c1a6a62d36cef6
GitJournal <app@gitjournal.io>
2022-09-11 17:52:08 +0800

    [2022-09-11 09:52:04]
    FILE:
    tests/vapoursynth_linux_test/denoise_and_upscale_samoyed.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/vapoursynth_linux_test/denoise_and_upscale_samoyed.py
+++ tests/vapoursynth_linux_test/denoise_and_upscale_samoyed.py
@@ -63,7 +63,7 @@ scale = 2
 #                         primaries_in_s="2020",  primaries_s="709",
 #                         transfer_in_s="linear", transfer_s="709")
 
-vi
+video = core.resize.Bicubic(clip =video, format = vs.YUV420P10, matrix_s='709')
 video.set_output()
 
 # maybe this shit is very freaking slow.
