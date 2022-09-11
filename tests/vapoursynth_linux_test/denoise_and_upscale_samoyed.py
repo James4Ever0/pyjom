28d99d9fa290bb68376de02b9c571d745b099ded
GitJournal <app@gitjournal.io>
2022-09-11 19:15:44 +0800

    [2022-09-11 11:15:42]
    FILE:
    tests/vapoursynth_linux_test/denoise_and_upscale_samoyed.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/vapoursynth_linux_test/denoise_and_upscale_samoyed.py
+++ tests/vapoursynth_linux_test/denoise_and_upscale_samoyed.py
@@ -2,7 +2,17 @@
 # is this necessary?
 
 # apply some filter on video size and duration first, please?
-
+import pathlib
+import sys
+site_path = pathlib.Path("/usr/local/lib/python3.9/site-packages")
+cv2_libs_dir = (
+    site_path / "cv2" / f"python-{sys.version_info.major}.{sys.version_info.minor}"
+)
+print(cv2_libs_dir)
+cv2_libs = sorted(cv2_libs_dir.glob("*.so"))
+if len(cv2_libs) == 1:
+    print("INSERTING:", cv2_libs[0].parent)
+    sys.path.insert(1, str(cv2_libs[0].parent))
 
 
 
