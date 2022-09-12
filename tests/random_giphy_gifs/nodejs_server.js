d0d5f53cbaee085f0d9dfd2ab2a2aea34596d88c
GitJournal <app@gitjournal.io>
2022-09-12 10:01:45 +0800

    [2022-09-12 02:01:41]
    FILE:
    tests/random_giphy_gifs/nodejs_server.js
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/random_giphy_gifs/nodejs_server.js
+++ tests/random_giphy_gifs/nodejs_server.js
@@ -3,7 +3,10 @@ const http = require('http');
 const requestListener = function(req, res) {
     // use 'less' to scan this beast?
     console.log("REQUEST AT:", req.url, req.method)
-    res.writeHead(200);
+    if (req.url == "/") {
+        res.writeHead(200);
+
+    }
     res.end('Hello, World!');
 }
 
