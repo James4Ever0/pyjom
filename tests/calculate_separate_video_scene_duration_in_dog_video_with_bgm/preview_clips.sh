e939353b6ed9f1fe9e4ba247fb6d304a8b250cd7
GitJournal <app@gitjournal.io>
2022-09-12 21:13:56 +0800

    [2022-09-12 13:13:50]
    FILE:
    tests/calculate_separate_video_scene_duration_in_dog_video_with_bgm/preview_clips.sh
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

---
+++ tests/calculate_separate_video_scene_duration_in_dog_video_with_bgm/preview_clips.sh
@@ -0,0 +1,126 @@
+ffplay -ss 00:00:00.000 -t 7.833 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:00:07.833 -t 6.567 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:00:14.400 -t 1.467 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:00:15.867 -t 2.033 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:00:17.900 -t 3.167 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:00:21.067 -t 3.2 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:00:24.267 -t 3.3 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:00:27.567 -t 3.767 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:00:31.333 -t 2.067 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:00:33.400 -t 0.6 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:00:34.000 -t 3.567 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:00:37.567 -t 3.167 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:00:40.733 -t 3.567 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:00:44.300 -t 6.4 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:00:50.700 -t 5.667 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:00:56.366 -t 3.433 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:00:59.800 -t 2.2 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:01:02.000 -t 2.9 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:01:04.900 -t 0.8 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:01:05.700 -t 1.5 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:01:07.200 -t 2.067 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:01:09.266 -t 1.3 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:01:10.566 -t 2.933 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:01:13.500 -t 1.7 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:01:15.200 -t 1.6 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:01:16.800 -t 3.467 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:01:20.266 -t 1.633 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:01:21.900 -t 1.467 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:01:23.366 -t 3.367 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:01:26.733 -t 1.667 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:01:28.400 -t 1.4 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:01:29.800 -t 3.567 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:01:33.366 -t 0.733 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:01:34.100 -t 0.6 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:01:34.700 -t 0.7 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:01:35.400 -t 0.967 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:01:36.366 -t 0.733 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:01:37.100 -t 0.8 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:01:37.900 -t 4.0 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:01:41.900 -t 1.0 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:01:42.900 -t 2.133 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:01:45.033 -t 3.0 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:01:48.033 -t 1.6 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:01:49.633 -t 3.0 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:01:52.633 -t 3.1 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:01:55.733 -t 4.033 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:01:59.766 -t 6.633 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:02:06.400 -t 6.3 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:02:12.699 -t 1.633 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:02:14.333 -t 3.833 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:02:18.166 -t 2.433 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:02:20.599 -t 0.6 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:02:21.199 -t 1.567 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:02:22.766 -t 3.3 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:02:26.066 -t 5.1 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:02:31.166 -t 3.467 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:02:34.633 -t 4.833 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:02:39.466 -t 3.033 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:02:42.499 -t 3.033 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:02:45.533 -t 2.367 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:02:47.899 -t 3.167 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:02:51.066 -t 2.9 -i sample.mp4 -autoexit 
+sleep 3
+ffplay -ss 00:02:53.966 -t 4.933 -i sample.mp4 -autoexit 
+sleep 3
