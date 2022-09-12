e5fb6c23004a6f4031add7320cff0f55b032d1d2
GitJournal <app@gitjournal.io>
2022-09-12 18:15:12 +0800

    [2022-09-12 10:15:09]
    FILE:
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/tencent_video_recommendation_extraction/phantomjs_test.js
+++ tests/tencent_video_recommendation_extraction/phantomjs_test.js
@@ -3,6 +3,7 @@ page.open('http://v.qq.com/x/page/m0847y71q98.html", function(status) {
   console.log("Status: " + status);
   if(status === "success") {
     page.render('example.png');
+	  page.evaluate(()=>console.log(document.body.innerHTML));
   }
   phantom.exit();
 });
