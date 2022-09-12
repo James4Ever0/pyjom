ee07c461f8d4d0a265ecfc3948a58bee474cc56a
GitJournal <app@gitjournal.io>
2022-09-12 18:06:07 +0800

    [2022-09-12 10:06:04]
    FILE:
    tests/tencent_video_recommendation_extraction/loop_all_tags.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/tencent_video_recommendation_extraction/loop_all_tags.py
+++ tests/tencent_video_recommendation_extraction/loop_all_tags.py
@@ -6,4 +6,9 @@ data = open("dump.html",'r').read()
 soup = BeautifulSoup(data)
 
 for elem in soup.find_all():
-    print(elem.attrs)
+    # print(elem.attrs)
+    # for further examination
+    attrs = elem.attrs
+    for key in ['src', 'href']:
+        if key in attrs.keys():
+            print(key, attrs[key])
