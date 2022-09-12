5c12c2fec7009beccfc9cf7b3b389a46fb8ff427
GitJournal <app@gitjournal.io>
2022-09-12 20:28:12 +0800

    [2022-09-12 12:28:10]
    FILE:
    tests/calculate_separate_video_scene_duration_in_dog_video_with_bgm/load_data.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/calculate_separate_video_scene_duration_in_dog_video_with_bgm/load_data.py
+++ tests/calculate_separate_video_scene_duration_in_dog_video_with_bgm/load_data.py
@@ -1,25 +1,26 @@
 import pandas
 
-metric = 'video.stats.csv'
+metric = "video.stats.csv"
 metric = pandas.read_csv(metric)
 
-scenes = 'sample_scenes.csv'
- 
-with open(scenes, 'r') as f:
+scenes = "sample_scenes.csv"
+
+with open(scenes, "r") as f:
     content = f.read()
     lines = content.split("\n")
     timecodeList = lines[0]
     scenes = "\n".join(lines[1:])
     from io import StringIO
+
     scenes = StringIO(scenes)
 
-timecodeList =timecodeList.split(",")
+timecodeList = timecodeList.split(",")
 timecodeList[0] = "00:00:00.000"
 
 scenes = pandas.read_csv(scenes)
 
-for index,row in scenes.iterrows():
+for index, row in scenes.iterrows():
     # print(row)
     # breakpoint()
-    start, end = row['Start Timecode'],row['End Timecode']
+    start, end = row["Start Timecode"], row["End Timecode"]
     print(start, end)
