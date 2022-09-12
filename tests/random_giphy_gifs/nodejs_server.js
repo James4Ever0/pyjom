f1783f68f560bb2450a91f6637e91a958190a091
GitJournal <app@gitjournal.io>
2022-09-12 10:15:08 +0800

    [2022-09-12 02:15:05]
    FILE:
    tests/random_giphy_gifs/nodejs_server.js
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/random_giphy_gifs/nodejs_server.js
+++ tests/random_giphy_gifs/nodejs_server.js
@@ -2,7 +2,15 @@ const http = require('http');
 const url = require('url');
 
 function randomAPIKey() {
-    var items = ['Yes', 'No', 'Maybe'];
+    var items = [ = "L8eXbxrbPETZxlvgXN9kIEzQ55Df04v0"
+        window.GIPHY_FE_WEB_API_KEY = "Gc7131jiJuvI7IdN0HZ1D7nh0ow5BU6g"
+        window.GIPHY_FE_FOUR_O_FOUR_API_KEY = "MRwXFtxAnaHo3EUMrSefHWmI0eYz5aGe"
+        window.GIPHY_FE_STORIES_AND_GIPHY_TV_API_KEY = "3eFQvabDx69SMoOemSPiYfh9FY0nzO9x"
+        window.GIPHY_FE_DEFAULT_API_SERVICE_KEY = "5nt3fDeGakBKzV6lHtRM1zmEBAs6dsIc"
+        window.GIPHY_FE_GET_POST_HEADERS_KEY = "e0771ed7b244ec9c942bea646ad08e6bf514f51a"
+        window.GIPHY_FE_MEDIUM_BLOG_API_KEY = "i3dev0tcpgvcuaocfmdslony2q9er7tvfndxcszm"
+        window.GIPHY_FE_EMBED_KEY = "eDs1NYmCVgdHvI1x0nitWd5ClhDWMpRE"
+    ];
     var item = items[Math.floor(Math.random() * items.length)];
     return item
 }
