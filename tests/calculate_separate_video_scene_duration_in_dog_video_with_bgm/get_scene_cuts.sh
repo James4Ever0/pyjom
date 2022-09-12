dda54149293728792450229ad0d4ea144b0451ae
GitJournal <app@gitjournal.io>
2022-09-12 20:58:59 +0800

    [2022-09-12 12:58:56]
    FILE:
    tests/calculate_separate_video_scene_duration_in_dog_video_with_bgm/get_scene_cuts.sh
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/calculate_separate_video_scene_duration_in_dog_video_with_bgm/get_scene_cuts.sh
+++ tests/calculate_separate_video_scene_duration_in_dog_video_with_bgm/get_scene_cuts.sh
@@ -1,16 +1,16 @@
 scenedetect -i sample.mp4 -s video.stats.csv detect-content list-scenes -f sample_scenes.csv
 # for dynamic analysis:
 # https://github.com/Breakthrough/PySceneDetect/README.md
-from scenedetect import open_video, SceneManager, split_video_ffmpeg
-from scenedetect.detectors import ContentDetector
-from scenedetect.video_splitter import split_video_ffmpeg
+# from scenedetect import open_video, SceneManager, split_video_ffmpeg
+# from scenedetect.detectors import ContentDetector
+# from scenedetect.video_splitter import split_video_ffmpeg
 
-def split_video_into_scenes(video_path, threshold=27.0):
-    # Open our video, create a scene manager, and add a detector.
-    video = open_video(video_path)
-    scene_manager = SceneManager()
-    scene_manager.add_detector(
-        ContentDetector(threshold=threshold))
-    scene_manager.detect_scenes(video, show_progress=True)
-    scene_list = scene_manager.get_scene_list()
-    split_video_ffmpeg(video_path, scene_list, show_progress=True)
+# def split_video_into_scenes(video_path, threshold=27.0):
+#     # Open our video, create a scene manager, and add a detector.
+#     video = open_video(video_path)
+#     scene_manager = SceneManager()
+#     scene_manager.add_detector(
+#         ContentDetector(threshold=threshold))
+#     scene_manager.detect_scenes(video, show_progress=True)
+#     scene_list = scene_manager.get_scene_list()
+#     split_video_ffmpeg(video_path, scene_list, show_progress=True)
