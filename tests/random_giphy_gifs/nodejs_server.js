4c711f8e3d6d78a78d6879259b90bbe6f8486e2c
GitJournal <app@gitjournal.io>
2022-09-12 11:01:32 +0800

    [2022-09-12 03:01:29]
    FILE:
    tests/random_giphy_gifs/nodejs_server.js
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/random_giphy_gifs/nodejs_server.js
+++ tests/random_giphy_gifs/nodejs_server.js
@@ -27,7 +27,8 @@ function getQueryParams(reqUrl) {
 }
 
 const typeArray = ['gifs', 'text', 'videos', 'stickers']
-const ratingArray = ['y', 'g','pg','pg-13','p'
+const ratingArray = ['y', 'g','pg','pg-13','r']
+
 function fallbackDefault(params, tag, valid, defaultParam) {
     param = params.get(tag)
     if (valid.indexOf(param) == -1) {
