eb71e5bae19b112ee85dc4f57ce535bbe1555b3d
GitJournal <app@gitjournal.io>
2022-09-12 20:46:24 +0800

    [2022-09-12 12:46:18]
    FILE:
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/calculate_separate_video_scene_duration_in_dog_video_with_bgm/generate_random_clip_lengths.py
+++ tests/calculate_separate_video_scene_duration_in_dog_video_with_bgm/generate_random_clip_lengths.py
@@ -6,10 +6,11 @@ std, mean = 1.6674874515595588, 2.839698412698412
 nMin, nMax = 0.6, 7.833
 # while you need to make sure the value is in bound.
 
-import random
+# import random
+from scipy.stats import truncnorm
 
-inBound = lambda number: min(nMax, max(nMin, number))
-randomFunction = lambda: inBound(random.gauss(mean, std))
+# inBound = lambda number: min(nMax, max(nMin, number))
+# randomFunction = lambda: inBound(random.gauss(mean, std))
 
 for _ in range(30):
     print(randomFunction())
