59bd2df40f71a87f93c45481029ecf8ebe6fa372
GitJournal <app@gitjournal.io>
2022-09-12 12:13:23 +0800

    [2022-09-12 04:13:20]
    FILE:
    tests/random_giphy_gifs/nodejs_server.js
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/random_giphy_gifs/nodejs_server.js
+++ tests/random_giphy_gifs/nodejs_server.js
@@ -107,7 +107,7 @@ const requestListener = function(req, res) {
             res.writeHead(200);
             res.end('nodejs giphy server');
         } else if (validEntries.indexOf(req.url.split("?")[0]) != -1) {
-            callback = (result) => res.end(getResultParsed(result, ['text', 'sticker'])
+            callback = (result) => res.end(getResultParsed(result, ['text', 'sticker']))
 
                 params = getQueryParams(req.url) q = params.get('q') type = fallbackDefault(params, 'type', typeArray, typeArray[0]) rating = fallbackDefault(params, 'rating', ratingArray, ratingArray[1]) limit = fallbackDefault(params, 'limit', limitArray, 100) offset = fallbackDefault(params, 'offset', offsetArray, randInt(100, 500)) sort = fallbackDefault(params, 'sort', sortArray, sortArray[1]) console.log('search keywords:', q) if (q != null) {
                     if (req.url.startsWith('/random')) {
