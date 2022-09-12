b190824eceb2d160f8bbb3da6518e3cf716a523a
GitJournal <app@gitjournal.io>
2022-09-12 20:36:31 +0800

    [2022-09-12 12:36:28]
    FILE:
    tests/calculate_separate_video_scene_duration_in_dog_video_with_bgm/load_data.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/calculate_separate_video_scene_duration_in_dog_video_with_bgm/load_data.py
+++ tests/calculate_separate_video_scene_duration_in_dog_video_with_bgm/load_data.py
@@ -35,5 +35,6 @@ import numpy
 
 std = numpy.std(lengths)
 mean = numpy.mean(lengths)
-print(std, mean, min(lengths), max(lengths))
+print(std, mean)
 # 1.6674874515595588 2.839698412698412
+print(min(lengths), max(lengths))
