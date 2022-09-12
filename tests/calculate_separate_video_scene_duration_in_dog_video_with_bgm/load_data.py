92fa83552e2b6ac1bd89d716047584e18cf7b2e1
GitJournal <app@gitjournal.io>
2022-09-12 22:10:36 +0800

    [2022-09-12 14:10:33]
    FILE:
    tests/calculate_separate_video_scene_duration_in_dog_video_with_bgm/load_data.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/calculate_separate_video_scene_duration_in_dog_video_with_bgm/load_data.py
+++ tests/calculate_separate_video_scene_duration_in_dog_video_with_bgm/load_data.py
@@ -73,5 +73,5 @@ elif flag == "render":
         start2, end2 = getTimeString(start2), getTimeString(end2)
         output = "output/%d.flv" % index
         print("ffmpeg -y -ss %s -to %s -i %s  %s" % (start2, end2, filename, output))
-elif flag == "filter":
-    for i
+elif flag == "filter": # to make sure 
+    candidates = []
