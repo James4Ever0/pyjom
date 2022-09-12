85f47f2114a483faf6fc50c7673d05a6a06a4894
GitJournal <app@gitjournal.io>
2022-09-12 20:50:12 +0800

    [2022-09-12 12:50:09]
    FILE:
    tests/calculate_separate_video_scene_duration_in_dog_video_with_bgm/generate_random_clip_lengths.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/calculate_separate_video_scene_duration_in_dog_video_with_bgm/generate_random_clip_lengths.py
+++ tests/calculate_separate_video_scene_duration_in_dog_video_with_bgm/generate_random_clip_lengths.py
@@ -13,6 +13,7 @@ from scipy.stats import truncnorm
 a, b = (myclip_a - loc) / scale, (myclip_b - loc) / scale
 
 randVar = truncnorm(a,b)
+randomFunction = lambda: randVar.rvs(1)
 # inBound = lambda number: min(nMax, max(nMin, number))
 # randomFunction = lambda: inBound(random.gauss(mean, std))
 
