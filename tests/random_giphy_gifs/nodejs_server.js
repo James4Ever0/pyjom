c32fdf031de1fb30e57e0f6bfc1ec6e03921bbe9
GitJournal <app@gitjournal.io>
2022-09-12 11:20:59 +0800

    [2022-09-12 03:20:57]
    FILE:
    tests/random_giphy_gifs/nodejs_server.js
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/random_giphy_gifs/nodejs_server.js
+++ tests/random_giphy_gifs/nodejs_server.js
@@ -17,11 +17,12 @@ function getResultParsed(result, typeFilter){
     if ('data' in result){
         for (elem of result['data']){
             dataType = elem['type']
+            if dataType == 'gif'
             dataId = elem['id']
             dataUrl = elem['url']
             title = elem['title']
             for (image of result['images']){
-                
+
             }
         }
     }
