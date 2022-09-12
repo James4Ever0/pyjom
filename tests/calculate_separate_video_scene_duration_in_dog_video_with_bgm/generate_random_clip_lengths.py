5bef942dc6fc7696d83d4dfd5c0325236fa7c814
GitJournal <app@gitjournal.io>
2022-09-12 20:46:04 +0800

    [2022-09-12 12:46:02]
    FILE:
    tests/calculate_separate_video_scene_duration_in_dog_video_with_bgm/generate_random_clip_lengths.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/calculate_separate_video_scene_duration_in_dog_video_with_bgm/generate_random_clip_lengths.py
+++ tests/calculate_separate_video_scene_duration_in_dog_video_with_bgm/generate_random_clip_lengths.py
@@ -1,7 +1,7 @@
 std, mean = 1.6674874515595588, 2.839698412698412
 # using gaussian distribution
 # accepting both mean and standard deviation
-# this is truncated gaussian, not 
+# this is truncated gaussian, not just normal distribution
 
 nMin, nMax = 0.6, 7.833
 # while you need to make sure the value is in bound.
