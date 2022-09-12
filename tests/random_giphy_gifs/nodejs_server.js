0e383409962476d651ae126e2e21e678c536cf05
GitJournal <app@gitjournal.io>
2022-09-12 11:06:07 +0800

    [2022-09-12 03:06:04]
    FILE:
    tests/random_giphy_gifs/nodejs_server.js
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/random_giphy_gifs/nodejs_server.js
+++ tests/random_giphy_gifs/nodejs_server.js
@@ -1,37 +1,37 @@
-const http = require('http');
+const http=require('http');
 // const url = require('url');
-const { GiphyFetch } = require('@giphy/js-fetch-api')
+const {GiphyFetch}=require('@giphy/js-fetch-api')
 
 
 
 function randomAPIKey() {
-    var items = [
-        "L8eXbxrbPETZxlvgXN9kIEzQ55Df04v0", "Gc7131jiJuvI7IdN0HZ1D7nh0ow5BU6g", "MRwXFtxAnaHo3EUMrSefHWmI0eYz5aGe", "3eFQvabDx69SMoOemSPiYfh9FY0nzO9x", "5nt3fDeGakBKzV6lHtRM1zmEBAs6dsIc", "eDs1NYmCVgdHvI1x0nitWd5ClhDWMpRE", "Gc7131jiJuvI7IdN0HZ1D7nh0ow5BU6g", 'IoJVsWoxDPKBr6gOcCgOPWAB25773hqP', 'lTRWAEGHjB1AkfO0sk2XTdujaPB5aH7X', '6esYBEm9OG3wAifbBFZ2mA0Ml6Ic0rvy', 'sXpGFDGZs0Dv1mmNFvYaGUvYwKX0PWIh'
+    var items=[
+        "L8eXbxrbPETZxlvgXN9kIEzQ55Df04v0","Gc7131jiJuvI7IdN0HZ1D7nh0ow5BU6g","MRwXFtxAnaHo3EUMrSefHWmI0eYz5aGe","3eFQvabDx69SMoOemSPiYfh9FY0nzO9x","5nt3fDeGakBKzV6lHtRM1zmEBAs6dsIc","eDs1NYmCVgdHvI1x0nitWd5ClhDWMpRE","Gc7131jiJuvI7IdN0HZ1D7nh0ow5BU6g",'IoJVsWoxDPKBr6gOcCgOPWAB25773hqP','lTRWAEGHjB1AkfO0sk2XTdujaPB5aH7X','6esYBEm9OG3wAifbBFZ2mA0Ml6Ic0rvy','sXpGFDGZs0Dv1mmNFvYaGUvYwKX0PWIh'
     ];
     // deleted some unqualified api keys because they look different in length
-    var item = items[Math.floor(Math.random() * items.length)];
+    var item=items[Math.floor(Math.random()*items.length)];
     return item
 }
 
-async function getRandomGifs(keywords, type, rating, callback){
-    gf = new GiphyFetch(randomAPIKey())
-    result = await gf.random({tag: keywords, type: type, rating: rating})
+async function getRandomGifs(keywords,type,rating,callback) {
+    gf=new GiphyFetch(randomAPIKey())
+    result=await gf.random({tag: keywords,type: type,rating: rating})
     callback(result)
 }
 
 function getQueryParams(reqUrl) {
-    current_url = new URL('http://localhost' + reqUrl)
-    params = current_url.searchParams
-    console.log('query parameters:', params)
+    current_url=new URL('http://localhost'+reqUrl)
+    params=current_url.searchParams
+    console.log('query parameters:',params)
     return params
 }
 
-const typeArray = ['gifs', 'text', 'videos', 'stickers']
-const ratingArray = ['y', 'g','pg','pg-13','r']
+const typeArray=['gifs','text','videos','stickers']
+const ratingArray=['y','g','pg','pg-13','r']
 
-function fallbackDefault(params, tag, valid, defaultParam) {
-    param = params.get(tag)
-    if (valid.indexOf(param) == -1) {
+function fallbackDefault(params,tag,valid,defaultParam) {
+    param=params.get(tag)
+    if(valid.indexOf(param)==-1) {
         // type = 'gifs'
         console.log(tag+" undefined. falling back to default: "+defaultParam)
         return defaultParam
@@ -39,26 +39,28 @@ function fallbackDefault(params, tag, valid, defaultParam) {
     return param
 }
 
-const validEntries = ['/random','/related','/trending','/search']
+const validEntries=['/random','/related','/trending','/search']
 
-const requestListener = function(req, res) {
+const requestListener=function(req,res) {
     // use 'less' to scan this beast?
-    console.log("REQUEST AT:", req.url, req.method)
-    if (req.url == "/") {
+    console.log("REQUEST AT:",req.url,req.method)
+    if(req.url=="/") {
         res.writeHead(200);
         res.end('nodejs giphy server');
-    } else if (validEntries.indexOf(req.url.split("?")[0]) !=-1) {
-        params = getQueryParams(req.url)
-        q = params.get('q')
-        type = fallbackDefault(params, 'type',typeArray,typeArray[0])
+    } else if(validEntries.indexOf(req.url.split("?")[0])!=-1) {
+        params=getQueryParams(req.url)
+        q=params.get('q')
+        type=fallbackDefault(params,'type',typeArray,typeArray[0])
         // rating = fallbackDefault(params, 'rating',ratingArray, ratingArray[1])
         // no rating? wtf?
-        console.log('search keywords:', q)
-        if (q != null) {
-            req.url.startsWith('/random')
-        res.end(gifs)
+        console.log('search keywords:',q)
+        if(q!=null) {
+            if(req.url.startsWith('/random')){
+                gifs = 
+            }
+            res.end(gifs)
         }
-        else{
+        else {
             res.end('no search keywords.')
         }
 
@@ -70,12 +72,12 @@ const requestListener = function(req, res) {
     }
 }
 
-const server = http.createServer(requestListener);
-port = 8902
+const server=http.createServer(requestListener);
+port=8902
 server.listen(port);
-console.log('server running on http://localhost:' + port);
-console.log('server running on http://localhost:' + port);
-console.log('server running on http://localhost:' + port);
-console.log('server running on http://localhost:' + port);
-console.log('server running on http://localhost:' + port);
-console.log('server running on http://localhost:' + port);
+console.log('server running on http://localhost:'+port);
+console.log('server running on http://localhost:'+port);
+console.log('server running on http://localhost:'+port);
+console.log('server running on http://localhost:'+port);
+console.log('server running on http://localhost:'+port);
+console.log('server running on http://localhost:'+port);
