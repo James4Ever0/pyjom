59b511b0140b929e213cbe0115467da7af37337d
GitJournal <app@gitjournal.io>
2022-09-12 17:27:36 +0800

    [2022-09-12 09:27:30]
    FILE:
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/tencent_video_recommendation_extraction/dump_page.py
+++ tests/tencent_video_recommendation_extraction/dump_page.py
@@ -1,7 +1,7 @@
 from playwright.sync_api import sync_playwright
 
 def run(playwright):
-    webkit = playwright.webkit
+    webkit = playwright.chromium
     browser = webkit.launch()
     context = browser.new_context()
     page = context.new_page()
