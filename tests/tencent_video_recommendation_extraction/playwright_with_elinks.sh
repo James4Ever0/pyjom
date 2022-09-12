0f07277c947b0232f727495044b8605833f97d74
GitJournal <app@gitjournal.io>
2022-09-12 17:38:44 +0800

    [2022-09-12 09:38:42]
    FILE:
    tests/tencent_video_recommendation_extraction/playwright_with_elinks.sh
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/tencent_video_recommendation_extraction/playwright_with_elinks.sh
+++ tests/tencent_video_recommendation_extraction/playwright_with_elinks.sh
@@ -2,3 +2,4 @@ python3 dump_page.py
 elinks -dump dump.html > dump.log
 
 # please find recommended video id in <div data-vid="<vid>">
+# or in //puui.qpic.cn/vpic_cover/<vid>/<vid>_old_ori.jpg/s640x360?max_age=7776000
