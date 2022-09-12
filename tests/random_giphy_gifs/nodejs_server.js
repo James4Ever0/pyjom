03302a6a825375c8952059c3df99caa9cf0e440b
GitJournal <app@gitjournal.io>
2022-09-12 11:35:12 +0800

    [2022-09-12 03:35:10]
    FILE:
    tests/random_giphy_gifs/nodejs_server.js
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/random_giphy_gifs/nodejs_server.js
+++ tests/random_giphy_gifs/nodejs_server.js
@@ -46,7 +46,7 @@ async function getRandomGifs(keywords,type,callback) {
 }
 async function getSearchGifs(keywords, sort, limit,type){
     // sort in 'recent', 'relevant'
-    result = await getGF().search(keywords, {})
+    result = await getGF().search(keywords, {sort:sort, limit:limit, })
 }
 
 function getQueryParams(reqUrl) {
