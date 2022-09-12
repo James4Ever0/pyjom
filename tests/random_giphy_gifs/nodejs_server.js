291ef70fca9686f0dcb0de5c6ca7b83c6c5b40eb
GitJournal <app@gitjournal.io>
2022-09-12 10:01:57 +0800

    [2022-09-12 02:01:54]
    FILE:
    tests/random_giphy_gifs/nodejs_server.js
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/random_giphy_gifs/nodejs_server.js
+++ tests/random_giphy_gifs/nodejs_server.js
@@ -1,16 +1,16 @@
 const http = require('http');
 
 const requestListener = function(req, res) {
-    // use 'less' to scan this beast?
-    console.log("REQUEST AT:", req.url, req.method)
-    if (req.url == "/") {
-        res.writeHead(200);
+        // use 'less' to scan this beast?
+        console.log("REQUEST AT:", req.url, req.method)
+        if (req.url == "/") {
+            res.writeHead(200);
+            res.end('nodejs );
 
-    }
-    res.end('Hello, World!');
-}
+            }
+        }
 
-const server = http.createServer(requestListener);
-port = 8902
-server.listen(port);
-console.log('server running on http://localhost:' + port);
+        const server = http.createServer(requestListener);
+        port = 8902
+        server.listen(port);
+        console.log('server running on http://localhost:' + port);
