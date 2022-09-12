18a6a4320579fffc0391dc7bb8ff6671a412164d
GitJournal <app@gitjournal.io>
2022-09-12 10:11:09 +0800

    [2022-09-12 02:11:05]
    FILE:
    tests/random_giphy_gifs/nodejs_server.js
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/random_giphy_gifs/nodejs_server.js
+++ tests/random_giphy_gifs/nodejs_server.js
@@ -1,26 +1,26 @@
 const http = require('http');
 const url = require('url');
 const requestListener = function(req, res) {
-        // use 'less' to scan this beast?
-        console.log("REQUEST AT:", req.url, req.method)
-        if (req.url == "/") {
-            res.writeHead(200);
-            res.end('nodejs giphy server');
-        } else if (req.url.startsWith('/random')) {
-            current_url = new URL('http://localhost' + req.url)
-            params = current_url.searchParams
-            console.log(params, typeof(params))
-            console.log(params.get('q'))
-            console.log(params.get('def')
-                // console.log(req.params)
-                res.end('random gifs:')
-            }
-            else {
-                res.end('not being right')
-            }
-        }
+    // use 'less' to scan this beast?
+    console.log("REQUEST AT:", req.url, req.method)
+    if (req.url == "/") {
+        res.writeHead(200);
+        res.end('nodejs giphy server');
+    } else if (req.url.startsWith('/random')) {
+        current_url = new URL('http://localhost' + req.url)
+        params = current_url.searchParams
+        console.log(params, typeof(params))
+        console.log()
+        console.log(params.get('def'))
 
-        const server = http.createServer(requestListener);
-        port = 8902
-        server.listen(port);
-        console.log('server running on http://localhost:' + port);
+        // console.log(req.params)
+        res.end('random gifs:')
+    } else {
+        res.end('not being right')
+    }
+}
+
+const server = http.createServer(requestListener);
+port = 8902
+server.listen(port);
+console.log('server running on http://localhost:' + port);
