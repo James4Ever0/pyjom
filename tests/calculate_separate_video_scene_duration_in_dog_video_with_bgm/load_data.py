9d8d0321a0ca353a30145aa4c6a984e266c58b85
GitJournal <app@gitjournal.io>
2022-09-12 20:51:56 +0800

    [2022-09-12 12:51:54]
    FILE:
    tests/calculate_separate_video_scene_duration_in_dog_video_with_bgm/load_data.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/calculate_separate_video_scene_duration_in_dog_video_with_bgm/load_data.py
+++ tests/calculate_separate_video_scene_duration_in_dog_video_with_bgm/load_data.py
@@ -31,15 +31,15 @@ for index, row in scenes.iterrows():
     lengths.append(length)
     # print(length, type(length)) # float.
 
-if calculate_
-import numpy
+if calculate_statistics:
+    import numpy
 
-std = numpy.std(lengths)
-mean = numpy.mean(lengths)
-print(std, mean)
-# 1.6674874515595588 2.839698412698412
-print(min(lengths), max(lengths))
-min(lengths), max(lengths)
+    std = numpy.std(lengths)
+    mean = numpy.mean(lengths)
+    print(std, mean)
+    # 1.6674874515595588 2.839698412698412
+    print(min(lengths), max(lengths))
+    min(lengths), max(lengths)
 # 0.6 7.833
 # strange though.
 # shall we adjust this accordingly? how to generate this shit?
