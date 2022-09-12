e7b9fce18799911ee1f869fe514745b7b73c5c64
GitJournal <app@gitjournal.io>
2022-09-12 22:16:51 +0800

    [2022-09-12 14:16:48]
    FILE:
    tests/calculate_separate_video_scene_duration_in_dog_video_with_bgm/load_data.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/calculate_separate_video_scene_duration_in_dog_video_with_bgm/load_data.py
+++ tests/calculate_separate_video_scene_duration_in_dog_video_with_bgm/load_data.py
@@ -76,5 +76,6 @@ elif flag == "render":
 elif flag == "filter": # to make sure the selected set will be evenly spaced. no two elements will get closer to each other than 5 seconds.
     import random
     fakeQualificationFunction = lambda: random.randrange(0.6,7.833)
+    fakeAcceptFunction = lambda: random.random() > 0.5
     # select the closest one! must be closer than 0.9 to 1.1
     candidates = []
