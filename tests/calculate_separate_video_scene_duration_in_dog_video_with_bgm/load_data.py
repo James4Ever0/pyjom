e9bf0a6f0fabbd314bc4480e70318c8fc1f75111
GitJournal <app@gitjournal.io>
2022-09-12 20:21:48 +0800

    [2022-09-12 12:21:44]
    FILE:
    tests/calculate_separate_video_scene_duration_in_dog_video_with_bgm/load_data.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/calculate_separate_video_scene_duration_in_dog_video_with_bgm/load_data.py
+++ tests/calculate_separate_video_scene_duration_in_dog_video_with_bgm/load_data.py
@@ -8,6 +8,7 @@ scenes = 'sample_scenes.csv'
 with open(scenes, 'r') as f:
     content = f.read()
     lines = content.split("\n")
-    
+    timecodeList = lines[0]
+    scenes = lines[1:]
 
 scenes = pandas.read_csv(scenes)
