475357578f7ffe8acdefeeda02c65fc294e05adb
GitJournal <app@gitjournal.io>
2022-09-12 09:56:46 +0800

    [2022-09-12 01:56:39]
    FILE:
    tests/random_giphy_gifs/nodejs_server.js
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

---
+++ tests/random_giphy_gifs/nodejs_server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const requestListener = function(req, res) {
+    print(req)
+    res.writeHead(200);
+    res.end('Hello, World!');
+}
+
+const server = http.createServer(requestListener);
+server.listen(8080);
