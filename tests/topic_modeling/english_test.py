26047fa22be082129e82bec8bf3aceaae5fe7e86
GitJournal <app@gitjournal.io>
2022-09-13 21:41:58 +0800

    [2022-09-13 13:41:56]
    FILE:
    tests/topic_modeling/english_test.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/topic_modeling/english_test.py
+++ tests/topic_modeling/english_test.py
@@ -57,8 +57,9 @@ indeed the vaguest idea where the wood and river in question were."""
 
 lemma_word1 = []
 for token in doc:
-    print("LEMMA", token.lemma_)
-    print(dir(token))
-    breakpoint()
+    # print("LEMMA", token.lemma_)
+    # not reliable.
+    # print(dir(token))
+    # breakpoint()
     lemma_word1.append(token.lemma_)
 print(lemma_word1)  # there is no such -PRON- thing.
