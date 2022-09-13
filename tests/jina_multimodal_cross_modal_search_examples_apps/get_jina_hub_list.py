ec29fc0d10594d457969357ab1e4e3629655c11a
GitJournal <app@gitjournal.io>
2022-09-13 18:47:04 +0800

    [2022-09-13 10:47:01]
    FILE:
    tests/jina_multimodal_cross_modal_search_examples_apps/get_jina_hub_list.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/jina_multimodal_cross_modal_search_examples_apps/get_jina_hub_list.py
+++ tests/jina_multimodal_cross_modal_search_examples_apps/get_jina_hub_list.py
@@ -1,5 +1,5 @@
 import requests
 
 url = 'https://api.hubble.jina.ai/v2/rpc/executor.list'
-query = {"sort":"-activities.metaMatched","pageIndex":3,"pageSize":16,"search":"","author":"","keywords":[],"withAnonymous":true}
-requests.get(
+query = {"sort":"-activities.metaMatched","pageIndex":3,"pageSize":16,"search":"","author":"","keywords":[],"withAnonymous":True}
+requests.post(
