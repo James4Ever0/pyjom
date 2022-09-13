8f9798384be315de6e82c9264f943bd6cc09b807
GitJournal <app@gitjournal.io>
2022-09-13 22:47:54 +0800

    [2022-09-13 14:47:51]
    FILE:
    tests/topic_modeling/poc_english_preprocessing.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/topic_modeling/poc_english_preprocessing.py
+++ tests/topic_modeling/poc_english_preprocessing.py
@@ -25,6 +25,7 @@ stop_words = set([elem.lower() for elem in stopwords.words("english")])
 lemma_word1 = []
 # this shit has the lang tag. it might be useful for language detection. really?
 for token in doc:
+    if token.pos_ in ['PRON']
     # elem = (token.pos_, token.text)
     if token.text.lower() in stop_words:
         continue
