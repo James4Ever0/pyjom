0c6dad0c96257031e8a6a997ab40e4c6ee398225
GitJournal <app@gitjournal.io>
2022-09-13 23:39:09 +0800

    [2022-09-13 15:39:06]
    FILE:
    tests/topic_modeling/poc_english_topic_modeling.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/topic_modeling/poc_english_topic_modeling.py
+++ tests/topic_modeling/poc_english_topic_modeling.py
@@ -51,7 +51,7 @@ Stem_words = Stem_words.reshape(5,-1)
 # row, col = Stem_words.shape
 # exit()
 # for reasons that shit can understand.
-for row in np.nditer(Stem_words,1):
+for row in np.nditer(1,Stem_words):
     print(row)
 
 from sklearn.feature_extraction.text import CountVectorizer
