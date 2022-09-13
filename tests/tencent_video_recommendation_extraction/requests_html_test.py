35bf9e4d2e2e1f5c30dd729c10058c5e2118ec42
GitJournal <app@gitjournal.io>
2022-09-14 02:30:33 +0800

    [2022-09-13 18:30:30]
    FILE:
    tests/tencent_video_recommendation_extraction/requests_html_test.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/tencent_video_recommendation_extraction/requests_html_test.py
+++ tests/tencent_video_recommendation_extraction/requests_html_test.py
@@ -1,7 +1,7 @@
 from requests_html import HTMLSession # use pyppeteer.
 session = HTMLSession()
 
-url='https://www.baidu.com/'
+# url='https://www.baidu.com/'
 
 r = session.get(url)
 print(r.html.absolute_links)
