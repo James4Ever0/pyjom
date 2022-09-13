0cefe7a0c0b3f94d6b222a13d99908915ad843c6
GitJournal <app@gitjournal.io>
2022-09-14 02:11:12 +0800

    [2022-09-13 18:11:10]
    FILE:
    tests/readbility_webpage_to_markdown_simplification/test_node_readbility.js
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

---
+++ tests/readbility_webpage_to_markdown_simplification/test_node_readbility.js
@@ -0,0 +1,19 @@
+var read = require('node-readability');
+
+read('http://howtonode.org/really-simple-file-uploads', function(err, article, meta) {
+  // Main Article
+  console.log(article.content);
+  // Title
+  console.log(article.title);
+
+  // HTML Source Code
+  console.log(article.html);
+  // DOM
+  console.log(article.document);
+
+  // Response Object from Request Lib
+  console.log(meta);
+
+  // Close article to clean up jsdom and prevent leaks
+  article.close();
+});
