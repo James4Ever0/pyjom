cc33a6e4d808e569e4650804c06cbef87b988444
GitJournal <app@gitjournal.io>
2022-09-13 21:45:42 +0800

    [2022-09-13 13:45:39]
    FILE:
    tests/topic_modeling/english_test.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/topic_modeling/english_test.py
+++ tests/topic_modeling/english_test.py
@@ -50,9 +50,7 @@ import en_core_web_sm
 nlp = en_core_web_sm.load()
 
 doc = nlp(
-    """He determined to drop his litigation with the monastry, and relinguish his claims to the wood-cuting and 
-fishery rihgts at once. He was the more ready to do this becuase the rights had become much less valuable, and he had 
-indeed the vaguest idea where the wood and river in question were."""
+    """He determined to drop his litigation with the monastry, and relinguish his claims to the wood-cuting and fishery rihgts at once. He was the more ready to do this becuase the rights had become much less valuable, and he had indeed the vaguest idea where the wood and river in question were."""
 )
 
 lemma_word1 = []
