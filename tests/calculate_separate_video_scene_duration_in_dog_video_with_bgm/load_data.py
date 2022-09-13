a629ce7d11f1ce5e75b76f39304f44326ae38357
GitJournal <app@gitjournal.io>
2022-09-13 09:15:39 +0800

    [2022-09-13 01:15:36]
    FILE:
    tests/calculate_separate_video_scene_duration_in_dog_video_with_bgm/load_data.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/calculate_separate_video_scene_duration_in_dog_video_with_bgm/load_data.py
+++ tests/calculate_separate_video_scene_duration_in_dog_video_with_bgm/load_data.py
@@ -86,6 +86,7 @@ elif flag == "filter": # to make sure the selected set will be evenly spaced. no
     )
     getTimeString = lambda timeObject: timeObject.strftime("%H:%M:%S.%f")
     mTimeDelta = datetime.timedelta(milliseconds=100)  # 0.1 seconds
+    standardStartD = datetime.datetime(year=1900, month=1, day=1)
 
     for index, (start, end, duration) in enumerate(sceneCuts):
         estimatedDuration = duration - 0.2
