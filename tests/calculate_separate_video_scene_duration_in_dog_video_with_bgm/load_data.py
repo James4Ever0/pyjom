019344a82ac078be4e6f066c0e9be8675b2490e8
GitJournal <app@gitjournal.io>
2022-09-13 09:34:04 +0800

    [2022-09-13 01:34:01]
    FILE:
    tests/calculate_separate_video_scene_duration_in_dog_video_with_bgm/load_data.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/calculate_separate_video_scene_duration_in_dog_video_with_bgm/load_data.py
+++ tests/calculate_separate_video_scene_duration_in_dog_video_with_bgm/load_data.py
@@ -108,7 +108,8 @@ elif flag == "filter": # to make sure the selected set will be evenly spaced. no
     neighborThreshold = 5
     def getNeighborIndexs(index, candidates, neighborThreshold):
         assert neighborThreshold  > 0
-        
+        leftNeighbors = candidates[:index]
+        rightNeighbors = candidates[index+1:]
 
     while True:
         target = fakeQualificationFunction()
