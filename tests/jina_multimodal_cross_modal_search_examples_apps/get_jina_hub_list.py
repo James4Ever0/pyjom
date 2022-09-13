4eda5d8d5aac805dbad09e76e9ead600f73172b0
GitJournal <app@gitjournal.io>
2022-09-13 18:49:07 +0800

    [2022-09-13 10:49:04]
    FILE:
    tests/jina_multimodal_cross_modal_search_examples_apps/get_jina_hub_list.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/jina_multimodal_cross_modal_search_examples_apps/get_jina_hub_list.py
+++ tests/jina_multimodal_cross_modal_search_examples_apps/get_jina_hub_list.py
@@ -1,8 +1,8 @@
 import requests
 import time
-def getJson()
+def getJson(pageIndex=1, pageSize=16):
     url = 'https://api.hubble.jina.ai/v2/rpc/executor.list'
-    query = {"sort":"-activities.metaMatched","pageIndex":3,"pageSize":16,"search":"","author":"","keywords":[],"withAnonymous":True}
+    query = {"sort":"-activities.metaMatched","pageIndex":pageIndex,"pageSize":16,"search":"","author":"","keywords":[],"withAnonymous":True}
     r = requests.post(url,json=query)
     jsonData = r.json()
 total = jsonData["meta"]["total"]
