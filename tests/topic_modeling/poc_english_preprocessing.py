614ef9000c0f774d42367172c809cbe4d7ed6632
GitJournal <app@gitjournal.io>
2022-09-13 22:45:47 +0800

    [2022-09-13 14:45:44]
    FILE:
    tests/topic_modeling/poc_english_preprocessing.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/topic_modeling/poc_english_preprocessing.py
+++ tests/topic_modeling/poc_english_preprocessing.py
@@ -33,36 +33,3 @@ print(filtered_sentence) # 2nd step
 print(Stem_words) # 3rd step
 
 # from textblob lib import Word method
-# if textblobTest:
-from textblob import Word
-
-text = """He determined to drop his litigation with the monastry, and relinguish his claims to the wood-cuting and 
-fishery rihgts at once. He was the more ready to do this becuase the rights had become much less valuable, and he had 
-indeed the vaguest idea where the wood and river in question were."""
-
-lem = []
-for i in text.split():
-    word1 = Word(i).lemmatize("n")
-    word2 = Word(word1).lemmatize("v")
-    word3 = Word(word2).lemmatize("a")
-    lem.append(Word(word3).lemmatize())
-print(lem) # incorrect and shitty. don't know what is its use
-
-import en_core_web_sm
-
-nlp = en_core_web_sm.load()
-
-doc = nlp(
-    """He determined to drop his litigation with the monastry, and relinguish his claims to the wood-cuting and fishery rihgts at once. He was the more ready to do this becuase the rights had become much less valuable, and he had indeed the vaguest idea where the wood and river in question were."""
-)
-
-# the sentence spliter includes unwanted "\n" char
-
-lemma_word1 = []
-# this shit has the lang tag. it might be useful for language detection. really?
-for token in doc:
-    elem = (token.pos_, token.text)
-    # breakpoint()
-    lemma_word1.append(elem)
-print(lemma_word1)  # there is no such -PRON- thing.
-# 2nd step.
