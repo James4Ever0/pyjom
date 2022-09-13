d8deda74a2507d27dfef7c4e24845c1025f24a56
GitJournal <app@gitjournal.io>
2022-09-13 11:26:16 +0800

    [2022-09-13 03:26:13]
    FILE:
    pyjom/primitives/onlinePrimitives.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/primitives/onlinePrimitives.py
+++ pyjom/primitives/onlinePrimitives.py
@@ -1,7 +1,7 @@
 from pyjom.main import *
 
 class OnlineAutoContentProducer(ContentProducer):
-    def __init__(self, source=None, enable_log=True,processor_filters={},producer_filters={}, path_replacers = [], template="pets_with_music_online", template_config = {}):
+    def __init__(self, source=None, enable_log=True,processor_filters={},producer_filters={}, template="pets_with_music_online", template_config = {}):
         super().__init__()
         assert source is not None
         self.source = source
@@ -14,7 +14,7 @@ class OnlineAutoContentProducer(ContentProducer):
                 "info": keywordDecorator(OnlineFetcher, source=source), # can you do that?
                 "processor": keywordDecorator(OnlineProcessor,filters=processor_filters), # this is the second thing. how do you process this?
                 # "reviewer": filesystemReviewer,
-                "producer": keywordDecorator(FilesystemProducer, filters=producer_filters, template=template,template_config = template_config),
+                "producer": keywordDecorator(OnlineProducer, filters=producer_filters, template=template,template_config = template_config),
             }
         )
 
