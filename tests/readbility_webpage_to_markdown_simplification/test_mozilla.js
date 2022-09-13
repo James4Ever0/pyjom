80e7b9b57438f00f678d6c1bb6bc63cbd72df82a
GitJournal <app@gitjournal.io>
2022-09-14 02:40:36 +0800

    [2022-09-13 18:40:33]
    FILE:
    tests/readbility_webpage_to_markdown_simplification/test_mozilla.js
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/readbility_webpage_to_markdown_simplification/test_mozilla.js
+++ tests/readbility_webpage_to_markdown_simplification/test_mozilla.js
@@ -1,7 +1,8 @@
 var { Readability } = require('@mozilla/readability');
-var { JSDOM } = require('jsdom');
-var doc = new JSDOM("<body>Look at this cat: <img src='./cat.jpg'></body>", {
+const { JSDOM } = require('jsdom');
+var doc = JSDOM("<body>Look at this cat: <img src='./cat.jpg'></body>", {
   url: "https://www.baidu.com"
 });
 let reader = new Readability(doc.window.document);
 let article = reader.parse();
+print(article.title);
