547cacafe4a289ccc933814fffe74c1f5af2cf27
GitJournal <app@gitjournal.io>
2022-09-13 20:20:41 +0800

    [2022-09-13 12:20:39]
    FILE:
    pyjom/modules/topicGenerator/onlineTopicGenerator.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/topicGenerator/onlineTopicGenerator.py
+++ pyjom/modules/topicGenerator/onlineTopicGenerator.py
@@ -6,9 +6,7 @@ def topicModeling(sentences: list[str], lang='en'): # specify language please?
     # python does not enforce type checking. use third party tool such as linter instead.
     if lang == 'en':
         from gensim.parsing.preprocessing import remove_stopwords
-result = remove_stopwords("""He determined to drop his litigation with the monastry, and relinguish his claims to the 
-wood-cuting and fishery rihgts at once. He was the more ready to do this becuase the rights had become much less valuable, 
-and he had indeed the vaguest idea where the wood and river in question were.""")
+        result = remove_stopwords(text)
 
 @decorator
 def OnlineTopicGenerator(source='giphy',topic = 'samoyed'):
