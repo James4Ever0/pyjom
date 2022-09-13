84f7a4595f3fe90ebc2992bce32e3dc82f70c982
GitJournal <app@gitjournal.io>
2022-09-13 09:30:36 +0800

    [2022-09-13 01:30:32]
    FILE:
    tests/calculate_separate_video_scene_duration_in_dog_video_with_bgm/load_data.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/calculate_separate_video_scene_duration_in_dog_video_with_bgm/load_data.py
+++ tests/calculate_separate_video_scene_duration_in_dog_video_with_bgm/load_data.py
@@ -110,7 +110,7 @@ elif flag == "filter": # to make sure the selected set will be evenly spaced. no
         target = fakeQualificationFunction()
         isSimilar = lambda a,b, threshold: min(a,b)/max(a,b) >= threshold
         similarThreshold = 0.9
-        if len()
+        if len(bannedIndexs) == len(shuffledCandidates):)
         for index,startCutDatetime, endCutDatetime, estimatedDurationAfterCut in shuffledCandidates:
             if index in bannedIndexs: continue
             if isSimilar(estimatedDurationAfterCut, target, similarThreshold):
