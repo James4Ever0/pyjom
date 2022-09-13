fbae8fd4f16c51999a37963df82a37c5f3d68222
GitJournal <app@gitjournal.io>
2022-09-14 02:20:16 +0800

    [2022-09-13 18:20:13]
    FILE:
    tests/readbility_webpage_to_markdown_simplification/test_readability.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/readbility_webpage_to_markdown_simplification/test_readability.py
+++ tests/readbility_webpage_to_markdown_simplification/test_readability.py
@@ -3,7 +3,6 @@ from readability import Document
 url='https://zhuanlan.zhihu.com/p/384614837'
 response = requests.get(url)
 doc = Document(response.text)
-print(doc.sanitize)
 print(doc.summary())
 print()
 print(doc.title())
