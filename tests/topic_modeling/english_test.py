dcd33e0cf1c8096bab33bd1835a64a6194577774
GitJournal <app@gitjournal.io>
2022-09-13 20:28:29 +0800

    [2022-09-13 12:28:22]
    FILE:
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

---
+++ tests/topic_modeling/english_test.py
@@ -0,0 +1,28 @@
+
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize 
+from nltk.stem import PorterStemmer
+
+set(stopwords.words('english'))
+
+text = """He determined to drop his litigation with the monastry, and relinguish his claims to the wood-cuting and 
+fishery rihgts at once. He was the more ready to do this becuase the rights had become much less valuable, and he had 
+indeed the vaguest idea where the wood and river in question were."""
+
+stop_words = set(stopwords.words('english')) 
+  
+word_tokens = word_tokenize(text) 
+    
+filtered_sentence = [] 
+  
+for w in word_tokens: 
+    if w not in stop_words: 
+        filtered_sentence.append(w) 
+
+Stem_words = []
+ps =PorterStemmer()
+for w in filtered_sentence:
+    rootWord=ps.stem(w)
+    Stem_words.append(rootWord)
+print(filtered_sentence)
+print(Stem_words)
