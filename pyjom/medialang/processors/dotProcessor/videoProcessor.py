9f5124dc26ee46747996cbfa3272d78322822574
GitJournal <app@gitjournal.io>
2022-09-13 09:06:56 +0800

    [2022-09-13 01:06:54]
    FILE:
    pyjom/medialang/processors/dotProcessor/videoProcessor.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/medialang/processors/dotProcessor/videoProcessor.py
+++ pyjom/medialang/processors/dotProcessor/videoProcessor.py
@@ -24,10 +24,7 @@ def ffmpegVideoPreProductionFilter(
     start=None,
     end=None,
     cachePath=None,
-    filters=["pipCrop", "textRemoval", "logoRemoval","randomFlip",
-    "superResolution",
-    "minterpolate",
-    "denoising",],
+    filters=["pipCrop", "textRemoval", "logoRemoval","randomFlip","superResolution","minterpolate","denoising"],
     preview=True
 ):  # what is the type of this shit?
     # enable that 'fast' flag? or we use low_resolution ones? not good since that will ruin our detection system!
