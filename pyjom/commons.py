6eeabb35eedd08aa9c662eb0b5bcf54648d4abe7
GitJournal <app@gitjournal.io>
2022-09-13 11:37:16 +0800

    [2022-09-13 03:37:12]
    FILE:
    pyjom/commons.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/commons.py
+++ pyjom/commons.py
@@ -35,6 +35,30 @@ if os.environ["USE_NVIDIA_OPENCV"] == "yes":
 mimetypes.init()
 
 
+
+def waitForServerUp(port, message, timeout=1):
+    import requests
+
+    while True:
+        try:
+            url = "http://localhost:{}".format(port)
+            r = requests.get(url, timeout=timeout)
+            if type(message) == str:
+                text = r.text.strip('"').strip("'")
+            else:
+                text = r.json()
+            print("SERVER AT PORT %d RESPONDS:" % port, [text])
+            assert text == message
+            print("SERVER AT PORT %d IS UP" % port)
+            break
+        except:
+            import traceback
+            traceback.print_exc()
+            print("SERVER AT PORT %d MIGHT NOT BE UP" % port)
+            print("EXPECTED MESSAGE:", [message])
+            import time
+            time.sleep(1)
+
 class D2Point:
     def __init__(self, x, y):
         self.x = x
