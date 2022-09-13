a706333fb9ed90775e1ec523f8f15976db552993
GitJournal <app@gitjournal.io>
2022-09-13 11:29:28 +0800

    [2022-09-13 03:29:24]
    FILE:
    pyjom/modules/topicGenerator/__init__.py
    pyjom/modules/topicGenerator/onlineTopicGenerator.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/topicGenerator/__init__.py
+++ pyjom/modules/topicGenerator/__init__.py
@@ -1,3 +1,4 @@
 from pyjom.modules.topicGenerator.dummyTopicGenerator import *
 from pyjom.modules.topicGenerator.weiboTopicGenerator import *
 from pyjom.modules.topicGenerator.localTopicGenerator import *
+from pyjom.modules.topicGenerator.onlineTopicGenerator import *
--- pyjom/modules/topicGenerator/onlineTopicGenerator.py
+++ pyjom/modules/topicGenerator/onlineTopicGenerator.py
@@ -1,1 +1,3 @@
 from pyjom.commons import *
+
+def
