ec425130e7fb64bf66ed242a2b30204915e03c71
GitJournal <app@gitjournal.io>
2022-09-13 11:49:17 +0800

    [2022-09-13 03:49:14]
    FILE:
    pyjom/modules/topicGenerator/onlineTopicGenerator.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/topicGenerator/onlineTopicGenerator.py
+++ pyjom/modules/topicGenerator/onlineTopicGenerator.py
@@ -11,6 +11,6 @@ def OnlineTopicGenerator(source='giphy',topic = 'samoyed'):
         waitForServerUp(8902, "nodejs giphy server")
         init=True
         while True:
-            requests.get("http://localhost:8902/random", params = {'q':keywords, 'rating':'g'})
-            requests.get("http://localhost:8902/related", params = {'q':})
+            requests.get("http://localhost:8902/random", params = {'q':keywords, 'rating':'g'}) # may you get stickers?
+            requests.get("http://localhost:8902/related", params = {'q':randomPictureId, })
             if init: init=False
