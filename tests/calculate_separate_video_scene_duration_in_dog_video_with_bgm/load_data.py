ad204b132a3d2bfbe9eebabb7f3e10737038cd24
GitJournal <app@gitjournal.io>
2022-09-13 09:32:26 +0800

    [2022-09-13 01:32:23]
    FILE:
    tests/calculate_separate_video_scene_duration_in_dog_video_with_bgm/load_data.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/calculate_separate_video_scene_duration_in_dog_video_with_bgm/load_data.py
+++ tests/calculate_separate_video_scene_duration_in_dog_video_with_bgm/load_data.py
@@ -121,7 +121,7 @@ elif flag == "filter": # to make sure the selected set will be evenly spaced. no
                     print("Accepting candidate",(index,startCutDatetime, endCutDatetime, estimatedDurationAfterCut))
                     print("target:", target)
                     bannedIndexs.append(index)
-                    getNeighborIndexs(index, candidates, neighborThreshold)
+                    neighborIndexs = getNeighborIndexs(index, candidates, neighborThreshold)
                     for neighborIndex in neighborIndexes:
-
+                        bannedIndexs.append(neighBo)
         random.shuffle(shuffledCandidates)
