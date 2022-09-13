d51ddd51a4fa1a7c5f9a6535f089bdf151f4df44
GitJournal <app@gitjournal.io>
2022-09-13 18:45:26 +0800

    [2022-09-13 10:45:23]
    FILE:
    tests/jina_multimodal_cross_modal_search_examples_apps/get_jina_hub_list.sh
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/jina_multimodal_cross_modal_search_examples_apps/get_jina_hub_list.sh
+++ tests/jina_multimodal_cross_modal_search_examples_apps/get_jina_hub_list.sh
@@ -15,3 +15,6 @@
 #   -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36' \
 #   --data-raw '{"sort":"-activities.metaMatched","pageIndex":3,"pageSize":16,"search":"","author":"","keywords":[],"withAnonymous":true}' \
 #   --compressed
+
+ --data-raw '{"sort":"-activities.metaMatched","pageIndex":3,"pageSize":16,"search":"","author":"","keywords":[],"withAnonymous":true}' \
+#   --compressed
