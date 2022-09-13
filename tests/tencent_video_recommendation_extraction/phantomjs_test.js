9bdf354bb232823e0f9c0fe39b7fc6a09cd2826f
GitJournal <app@gitjournal.io>
2022-09-14 02:41:26 +0800

    [2022-09-13 18:41:23]
    FILE:
    tests/tencent_video_recommendation_extraction/phantomjs_test.js
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/tencent_video_recommendation_extraction/phantomjs_test.js
+++ tests/tencent_video_recommendation_extraction/phantomjs_test.js
@@ -1,10 +1,10 @@
 var page = require('webpage').create();
 page.open('http://v.qq.com/x/page/m0847y71q98.html', function(status) {
-	//console.log("Status: " + status);
-	if(status === "success") {
-	//	page.render('example.png');
-		result = page.evaluate(()=>document.body.innerHTML);
-		console.log(result)
-	}
-	phantom.exit();
+    //console.log("Status: " + status);
+    if (status === "success") {
+        //	page.render('example.png');
+        result = page.evaluate(() => document.body.innerHTML);
+        console.log(result)
+    }
+    phantom.exit();
 });
