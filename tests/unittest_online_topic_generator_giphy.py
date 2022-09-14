5ee187009ae641445d4a773bd97e0757ea0ab82d
GitJournal <app@gitjournal.io>
2022-09-14 12:29:49 +0800

    [2022-09-14 04:29:46]
    FILE:
    tests/unittest_online_topic_generator_giphy.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_online_topic_generator_giphy.py
+++ tests/unittest_online_topic_generator_giphy.py
@@ -9,7 +9,7 @@ sprint("LABEL:",label)
 # # 'pyjom.commons.OnlineTopicGenerator'
 # breakpoint()
 
-with tmpdir("/dev/shm/m")
+with tmpdir("/dev/shm/medialang/online_test") as 
 for x in elem:
     print("X",x)
     # X ('sr8jYZVVsCmxddga8w', {'height': 480, 'width': 474, 'url': 'https://media0.giphy.com/media/sr8jYZVVsCmxddga8w/giphy.gif'})
