9cd0666a65ce9dd35ca708af4dc2c91468eab791
GitJournal <app@gitjournal.io>
2022-09-14 13:46:00 +0800

    [2022-09-14 05:45:58]
    FILE:
    pyjom/primitives/onlinePrimitives.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/primitives/onlinePrimitives.py
+++ pyjom/primitives/onlinePrimitives.py
@@ -11,7 +11,7 @@ class OnlineAutoContentProducer(ContentProducer):
         self.identifier.typeFix(type(self).__name__)
         self.methodsList.update(
             {
-                "topic":keywordDecorator(OnlineTopicGenerator, source=source, topic=topic),  # how to generate this?
+                "topic":keywordDecorator(OnlineTopicGenerator, source=source, metaTopic=metaTopic),  # how to generate this?
                 "info": keywordDecorator(OnlineFetcher, source=source), # can you do that?
                 "processor": keywordDecorator(OnlineProcessor,filters=processor_filters), # this is the second thing. how do you process this?
                 # "reviewer": filesystemReviewer,
