e489d784c18d996bc7c5f02a8c6e4a7ab2fb94c0
GitJournal <app@gitjournal.io>
2022-09-14 11:18:04 +0800

    [2022-09-14 03:18:02]
    FILE:
    pyjom/modules/topicGenerator/onlineTopicGenerator.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/topicGenerator/onlineTopicGenerator.py
+++ pyjom/modules/topicGenerator/onlineTopicGenerator.py
@@ -5,7 +5,6 @@ import requests
 # import jieba
 from typing import Literal
 
-
 def topicModeling(sentences: list[str], lang="en"):  # specify language please?
     # python does not enforce type checking. use third party tool such as linter instead.
     if lang == "en":
