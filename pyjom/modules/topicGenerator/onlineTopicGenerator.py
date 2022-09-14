d2b13f38109b61f5f5966101b9487d1553f947c4
GitJournal <app@gitjournal.io>
2022-09-14 11:02:06 +0800

    [2022-09-14 03:02:02]
    FILE:
    pyjom/modules/topicGenerator/onlineTopicGenerator.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/topicGenerator/onlineTopicGenerator.py
+++ pyjom/modules/topicGenerator/onlineTopicGenerator.py
@@ -24,7 +24,8 @@ def topicSelection(topics, selected_topic_set, mode:Literal['combined','separate
     print("no topic this time")
     return None
 
-def get_topic_string()
+def get_topic_string(topic):
+    random.choice()
 
 @decorator
 def OnlineTopicGenerator(source='giphy',topic = [['samoyed','dog','cat'],['funny','cute']]):
