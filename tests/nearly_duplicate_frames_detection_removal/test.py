79e4b85a28a80ceaac2bb15bb01e1e079d342ecc
GitJournal <app@gitjournal.io>
2022-09-14 23:28:26 +0800

    [2022-09-14 15:28:23]
    FILE:
    tests/nearly_duplicate_frames_detection_removal/test.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/nearly_duplicate_frames_detection_removal/test.py
+++ tests/nearly_duplicate_frames_detection_removal/test.py
@@ -3,3 +3,4 @@ source = "/root/Desktop/works/pyjom/samples/video/nearly_duplicate_frames_detect
 # is it still image?
 # we can also detect more shits. right?
 import scenedetect
+scenedetect.detect()
