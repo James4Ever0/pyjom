310a73bde71760353dd3bb0c5bc30b7dc44d2d0b
GitJournal <app@gitjournal.io>
2022-09-15 01:29:50 +0800

    [2022-09-14 17:29:47]
    FILE:
    tests/nearly_duplicate_frames_detection_removal/pyav_effective_fps.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/nearly_duplicate_frames_detection_removal/pyav_effective_fps.py
+++ tests/nearly_duplicate_frames_detection_removal/pyav_effective_fps.py
@@ -1,3 +1,6 @@
 import av
 source = "/root/Desktop/works/pyjom/samples/video/nearly_duplicate_frames_detection_30fps_blend.mp4"  # this is evil. it defeats my shit.
 
+container = av.open(source)
+
+for frame in container.decode(video=0):
