24b2e565c5d8d7f47c41b5494d93ca8440096be1
GitJournal <app@gitjournal.io>
2022-09-14 11:08:12 +0800

    [2022-09-14 03:08:09]
    FILE:
    pyjom/modules/topicGenerator/onlineTopicGenerator.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/topicGenerator/onlineTopicGenerator.py
+++ pyjom/modules/topicGenerator/onlineTopicGenerator.py
@@ -24,13 +24,13 @@ def topicWordSelection(topics, selected_topic_set, mode:Literal['combined','sepa
     print("no topic word this time")
     return None
 
-def get_topic_string(topic):
+def get_metaTopic_string(topic):
     candidates = [random.choice(x) for x in topic]
     samples = random.sample(candidates, random.randint(1, len(candidates)))
     return " ".join(samples)
 
 @decorator
-def OnlineTopicGenerator(source='giphy',topic = [['samoyed','dog','cat'],['funny','cute']]):
+def OnlineTopicGenerator(source='giphy',metaTopic = [['samoyed','dog','cat'],['funny','cute']]):
     selected_topic_set = {topic} # common way to initialize a set.
     if source == 'giphy':
         waitForServerUp(8902, "nodejs giphy server")
