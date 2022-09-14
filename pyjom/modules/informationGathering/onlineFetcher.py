ed37457a9c6f8489041b729ac789dd760816e74f
GitJournal <app@gitjournal.io>
2022-09-14 17:43:55 +0800

    [2022-09-14 09:43:53]
    FILE:
    pyjom/modules/informationGathering/onlineFetcher.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/informationGathering/onlineFetcher.py
+++ pyjom/modules/informationGathering/onlineFetcher.py
@@ -1,5 +1,6 @@
 from pyjom.commons import *
 from typing import Literal
+from lazero.network import download
 
 @decorator
 def OnlineFetcher(infoList, source:Literal['giphy']='giphy', frame_size_filter:dict={'width':{'min':200,'max':1000}, 'height':{'min':200,'max':1000}}, tempdir='/dev/shm/medialang/online'):
@@ -22,4 +23,4 @@ def OnlineFetcher(infoList, source:Literal['giphy']='giphy', frame_size_filter:d
                 if not (os.path.exists(download_path) and os.path.isfile(download_path)):
                     # no need to download
                 download(url, download_path, threads=-0.3, size_filter={"min":0.4, "max":50})
-                yield source_id, downloadPath
+                yield source_id, download_ath
