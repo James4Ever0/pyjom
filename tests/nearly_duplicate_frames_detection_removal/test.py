b76984e931639f6947e1fec11a33bc394f833f05
GitJournal <app@gitjournal.io>
2022-09-14 23:17:44 +0800

    [2022-09-14 15:17:41]
    FILE:
    tests/nearly_duplicate_frames_detection_removal/test.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/nearly_duplicate_frames_detection_removal/test.py
+++ tests/nearly_duplicate_frames_detection_removal/test.py
@@ -1,4 +1,4 @@
-source = "/root/Desktop/works/pyjom/samples/video/nearly_duplicate_frames_detection.gif"
+source = "/root/Desktop/works/pyjom/samples/video/nearly_duplicate_frames_detection_30fps.gif"
 
 # is it still image?
 # we can also detect more shits. right?
