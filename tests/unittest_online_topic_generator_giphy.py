532499f61c6ba97a597d46daf759ab508fed19e4
GitJournal <app@gitjournal.io>
2022-09-14 12:38:49 +0800

    [2022-09-14 04:38:46]
    FILE:
    tests/unittest_online_topic_generator_giphy.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_online_topic_generator_giphy.py
+++ tests/unittest_online_topic_generator_giphy.py
@@ -11,7 +11,8 @@ sprint("LABEL:",label)
 path = "/dev/shm/medialang/online_test"
 with tmpdir(path=path) as testDir:
     print("TESTDIR:", testDir)
-    for x in elem:
+    for  in elem:
         print("X",x)
+        url = x
         # X ('sr8jYZVVsCmxddga8w', {'height': 480, 'width': 474, 'url': 'https://media0.giphy.com/media/sr8jYZVVsCmxddga8w/giphy.gif'})
         breakpoint()
