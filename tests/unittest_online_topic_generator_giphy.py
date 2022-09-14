2fae84ddeb90e0aeee94c93513003180f87dd733
GitJournal <app@gitjournal.io>
2022-09-14 12:39:58 +0800

    [2022-09-14 04:39:54]
    FILE:
    tests/unittest_online_topic_generator_giphy.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_online_topic_generator_giphy.py
+++ tests/unittest_online_topic_generator_giphy.py
@@ -15,6 +15,6 @@ with tmpdir(path=path) as testDir:
     for asset_id, meta in elem:
         print("X",asset_id, meta)
         url = meta['url']
-        download_path = os.path.join(path, 
+        download_path = os.path.join(path, asset_id+
         # X ('sr8jYZVVsCmxddga8w', {'height': 480, 'width': 474, 'url': 'https://media0.giphy.com/media/sr8jYZVVsCmxddga8w/giphy.gif'})
         breakpoint()
