0701be75ae8704670ae717f75c445d681995a942
GitJournal <app@gitjournal.io>
2022-09-14 17:20:44 +0800

    [2022-09-14 09:20:42]
    FILE:
    pyjom/modules/informationGathering/onlineFetcher.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/informationGathering/onlineFetcher.py
+++ pyjom/modules/informationGathering/onlineFetcher.py
@@ -4,8 +4,8 @@ from typing import Literal
 @decorator
 def OnlineFetcher(infoList, source:Literal['giphy']='giphy', frame_size_filter:dict={'width':200, 'height':200}):
     # how do you chain this shit up?
-    minWidth = frame_size_filter.get('width')
-    minWidth = frame_size_filter.get('height')
+    min_width = frame_size_filter.get('width')
+    min_height = frame_size_filter.get('height')
     for info in infoList: # generator most likely
         if source=='giphy':
             # (id, {width, height, url})
