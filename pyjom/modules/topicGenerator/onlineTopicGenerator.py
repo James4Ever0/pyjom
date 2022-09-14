2fe9475a817f64caf90f0e4518ad23d7366a5f1f
GitJournal <app@gitjournal.io>
2022-09-14 10:49:00 +0800

    [2022-09-14 02:48:55]
    FILE:
    pyjom/modules/topicGenerator/onlineTopicGenerator.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/topicGenerator/onlineTopicGenerator.py
+++ pyjom/modules/topicGenerator/onlineTopicGenerator.py
@@ -29,7 +29,8 @@ def OnlineTopicGenerator(source='giphy',topic = 'samoyed'):
         init=True
         while True:
             mRandomPicture = requests.get("http://localhost:8902/random", params = {'q':keywords, 'rating':'g'}) # may you get stickers?
-            randomPictureId = ['data'][0]['id']
+            mRandomPictureJson = mRandomPicture.json()
+            randomPictureId = mRandomPictureJson['data'][0]['id']
             mRelatedPictures = requests.get("http://localhost:8902/related", params = {'q':randomPictureId})
 
             if init: init=False
