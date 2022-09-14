2075cbd86d9696d97305404d3a9371145fb572cc
GitJournal <app@gitjournal.io>
2022-09-14 11:51:20 +0800

    [2022-09-14 03:51:16]
    FILE:
    tests/unittest_online_topic_generator_giphy.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_online_topic_generator_giphy.py
+++ tests/unittest_online_topic_generator_giphy.py
@@ -2,7 +2,8 @@ from test_commons import *
 from pyjom.modules.topicGenerator import OnlineTopicGenerator
 
 label, elem =  OnlineTopicGenerator()
-orint(label)
+sprint(label)
+breakpoint()
 for x in elem:
     print("X",x)
     breakpoint()
