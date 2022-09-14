076843ab05dcf9ad9470356d0d38c0cf30118fb1
GitJournal <app@gitjournal.io>
2022-09-14 16:22:44 +0800

    [2022-09-14 08:22:40]
    FILE:
    __main__.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- __main__.py
+++ __main__.py
@@ -5,7 +5,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-f",'--ffmpeg-filters', help="show ffmpeg filters")
 parser.add_argument('-r','--random', help='randomize output')
 flags = parser.parse_args()
-
+randomize=False
+if flags.r:
+    randomize=True
 if flags.f:
-    if flags.r:
-        randomize=True
