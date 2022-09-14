1e98568374bcd8eaa1bf61e1502300da604ee4c2
GitJournal <app@gitjournal.io>
2022-09-14 23:33:57 +0800

    [2022-09-14 15:33:55]
    FILE:
    tests/nearly_duplicate_frames_detection_removal/test.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/nearly_duplicate_frames_detection_removal/test.py
+++ tests/nearly_duplicate_frames_detection_removal/test.py
@@ -14,7 +14,7 @@ import scenedetect
 
 stats_file_path = "output.csv"
 cuts = scenedetect.detect(
-    video_path=source, stats_file_path=stats_file_path, show_progress=True
+    video_path=source, stats_file_path=stats_file_path, show_progress=True, detector=ContentDetector()
 )
 
 import pandas
