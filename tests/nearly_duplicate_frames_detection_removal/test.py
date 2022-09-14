5c24a66b37b5032f0edf26200e7ec0744efcedfe
GitJournal <app@gitjournal.io>
2022-09-14 23:30:07 +0800

    [2022-09-14 15:30:02]
    FILE:
    tests/nearly_duplicate_frames_detection_removal/test.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/nearly_duplicate_frames_detection_removal/test.py
+++ tests/nearly_duplicate_frames_detection_removal/test.py
@@ -1,9 +1,14 @@
-source = "/root/Desktop/works/pyjom/samples/video/nearly_duplicate_frames_detection_30fps.gif" # this is evil. it defeats my shit.
+source = "/root/Desktop/works/pyjom/samples/video/nearly_duplicate_frames_detection_30fps.gif"  # this is evil. it defeats my shit.
 
 # is it still image?
 # we can also detect more shits. right?
 import scenedetect
-cuts = scenedetect.detect(video_path=source, stats_file_path="output.csv", show_progress=True)
+
+stats_file_path = "output.csv"
+cuts = scenedetect.detect(
+    video_path=source, stats_file_path=stats_file_path, show_progress=True
+)
 
 import pandas
+
 pandas.read_csv(stats_file_path)
