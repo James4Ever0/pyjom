da673fb42a06ab3d96536e97ba2df7452e398b69
GitJournal <app@gitjournal.io>
2022-09-14 17:52:04 +0800

    [2022-09-14 09:52:00]
    FILE:
    pyjom/modules/informationGathering/onlineFetcher.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/informationGathering/onlineFetcher.py
+++ pyjom/modules/informationGathering/onlineFetcher.py
@@ -20,9 +20,5 @@ def OnlineFetcher(infoList, source:Literal['giphy']='giphy', frame_size_filter:d
                 extension = url.split("?")[0].split(".")[-1]
                 basename = ".".join([asset_id, extension])
                 download_path = os.path.join(tempdir, basename)
-                if not (os.path.exists(download_path) and os.path.isfile(download_path)):
-                    # no need to download
-                    download(url, download_path, threads=-0.3, size_filter={"min":0.4, "max":50})
-                else:
-                    print("FILE ALREADY DOWNLOADED")
-                yield source_id, download_ath
+                download(url, download_path, threads=-0.3, size_filter={"min":0.4, "max":50})
+                yield source_id, download_path
