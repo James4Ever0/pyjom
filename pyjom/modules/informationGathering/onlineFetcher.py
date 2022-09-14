1e4496e0ecc4de036043e2848b9bfe33fd46f126
GitJournal <app@gitjournal.io>
2022-09-14 14:02:04 +0800

    [2022-09-14 06:02:00]
    FILE:
    pyjom/modules/informationGathering/onlineFetcher.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/informationGathering/onlineFetcher.py
+++ pyjom/modules/informationGathering/onlineFetcher.py
@@ -4,4 +4,5 @@ from pyjom.commons import *
 @decorator
 def OnlineFetcher(infoList):
     # how do you chain this shit up?
-    for info in infoList:
+    for info in infoList: # generator most likely
+        if 
