be9556eee86d0401601528d1d1569d3fb6759e6b
GitJournal <app@gitjournal.io>
2022-09-14 11:11:22 +0800

    [2022-09-14 03:11:20]
    FILE:
    pyjom/modules/topicGenerator/onlineTopicGenerator.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/topicGenerator/onlineTopicGenerator.py
+++ pyjom/modules/topicGenerator/onlineTopicGenerator.py
@@ -1,16 +1,21 @@
 from pyjom.commons import *
 from pyjom.languagetoolbox import englishTopicModeling
 import requests
+
 # import jieba
 from typing import Literal
 
-def topicModeling(sentences: list[str], lang='en'): # specify language please?
+
+def topicModeling(sentences: list[str], lang="en"):  # specify language please?
     # python does not enforce type checking. use third party tool such as linter instead.
-    if lang == 'en':
+    if lang == "en":
         topics = englishTopicModeling(sentences)
         return topics
 
-def topicWordSelection(topics, selected_topic_set, mode:Literal['combined','separate']='combined'):
+
+def topicWordSelection(
+    topics, selected_topic_set, mode: Literal["combined", "separate"] = "combined"
+):
     import random
     mTopics = topics.copy()
     random.shuffle(mTopics)
@@ -19,37 +24,48 @@ def topicWordSelection(topics, selected_topic_set, mode:Literal['combined','sepa
         words = [x for x in words if x not in selected_topic_set]
         if len(words) > 0:
             word = random.choice(words)
-            selected_topic_set.add(word) # no need to go elsewhere.
+            selected_topic_set.add(word)  # no need to go elsewhere.
             return word
     print("no topic word this time")
     return None
+
 
 def getMetaTopicString(metaTopic):
     candidates = [random.choice(x) for x in metaTopic]
     samples = random.sample(candidates, random.randint(1, len(candidates)))
     return " ".join(samples)
 
+
 @decorator
-def OnlineTopicGenerator(source='giphy',metaTopic = [['samoyed','dog','cat'],['funny','cute']]):
-    getKeywords = lambda:  getMetaTopicString(metaTopic)
-    selected_topic_set = {*np.array(metaTopic).reshape(-1).tolist()} # common way to initialize a set.
-    if source == 'giphy':
+def OnlineTopicGenerator(
+    source="giphy", metaTopic=[["samoyed", "dog", "cat"], ["funny", "cute"]]
+):
+    getKeywords = lambda: getMetaTopicString(metaTopic)
+    selected_topic_set = {
+        *np.array(metaTopic).reshape(-1).tolist()
+    }  # common way to initialize a set.
+    if source == "giphy":
         waitForServerUp(8902, "nodejs giphy server")
-        init=True
+        init = True
         keywords = getKeywords()
         while True:
-            if random.random()> 0.5:
-                mRandomPicture = requests.get("http://localhost:8902/random", params = {'q':keywords, 'rating':'g'}) # may you get stickers?
+            if random.random() > 0.5:
+                mRandomPicture = requests.get(
+                    "http://localhost:8902/random",
+                    params={"q": keywords, "rating": "g"},
+                )  # may you get stickers?
                 mRandomPictureJson = mRandomPicture.json()
-                randomPictureId = mRandomPictureJson['data'][0]['id']
+                randomPictureId = mRandomPictureJson["data"][0]["id"]
             else:
                 mSearchPicture = requests.get("http://localhost:8902/search", params={})
                 mSearchPictureJson = mSearchPicture.json()
-                randomPictureId = random.choice(mSearchPictureJson['data'])['id']
+                randomPictureId = random.choice(mSearchPictureJson["data"])["id"]
 
-            mRelatedPictures = requests.get("http://localhost:8902/related", params = {'q':randomPictureId})
+            mRelatedPictures = requests.get(
+                "http://localhost:8902/related", params={"q": randomPictureId}
+            )
             mRelatedPicturesJson = mRelatedPictures.json()
-            sentences = [x['title'] for x in mRelatedPicturesJson['data']]
+            sentences = [x["title"] for x in mRelatedPicturesJson["data"]]
             topics = topicModeling(sentences)
             selectedWord = topicWordSelection(topics, selected_topic_set)
-            keywords = " ".join([getKeywords(),selectedWord]) # for next iteration.
+            keywords = " ".join([getKeywords(), selectedWord])  # for next iteration.
