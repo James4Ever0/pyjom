bee0c4c4987fe63a55edb758974f1ef17c7d1709
GitJournal <app@gitjournal.io>
2022-09-14 13:48:39 +0800

    [2022-09-14 05:48:36]
    FILE:
    pyjom/primitives/onlinePrimitives.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/primitives/onlinePrimitives.py
+++ pyjom/primitives/onlinePrimitives.py
@@ -1,20 +1,44 @@
 from pyjom.main import *
 
+
 class OnlineAutoContentProducer(ContentProducer):
-    def __init__(self, source=None, enable_log=True,processor_filters={},producer_filters={}, template="pets_with_music_online", template_config = {}, metaTopic={"static":[["samoyed", "dog", "cat"], ["funny", "cute"]], "dynamic":[]}): # something in this metaTopic is not droppable.
+    def __init__(
+        self,
+        source=None,
+        enable_log=True,
+        processor_filters={},
+        producer_filters={},
+        template="pets_with_music_online",
+        template_config={},
+        metaTopic={
+            "static": [["samoyed", "dog", "cat"], ["funny", "cute"]],
+            "dynamic": [],
+        },
+    ):  # something in this metaTopic is not droppable.
         super().__init__()
         assert source is not None
         self.source = source
-        self.metaTopic = metaTopic # 所谓的超话 超级话题
+        self.metaTopic = metaTopic  # 所谓的超话 超级话题
         if enable_log:
             self.log_location = "logs/local/"
         self.identifier.typeFix(type(self).__name__)
         self.methodsList.update(
             {
-                "topic":keywordDecorator(OnlineTopicGenerator, source=source, metaTopic=metaTopic),  # how to generate this?
-                "info": keywordDecorator(OnlineFetcher, source=source), # can you do that?
-                "processor": keywordDecorator(OnlineProcessor,filters=processor_filters), # this is the second thing. how do you process this?
+                "topic": keywordDecorator(
+                    OnlineTopicGenerator, source=source, metaTopic=metaTopic
+                ),  # how to generate this?
+                "info": keywordDecorator(
+                    OnlineFetcher, source=source
+                ),  # can you do that?
+                "processor": keywordDecorator(
+                    OnlineProcessor, filters=processor_filters
+                ),  # this is the second thing. how do you process this?
                 # "reviewer": filesystemReviewer,
-                "producer": keywordDecorator(OnlineProducer, filters=producer_filters, template=template,template_config = template_config),
+                "producer": keywordDecorator(
+                    OnlineProducer,
+                    filters=producer_filters,
+                    template=template,
+                    template_config=template_config,
+                ),
             }
         )
