d85de8934c3667673748c2e31110d906d7231bdd
GitJournal <app@gitjournal.io>
2022-09-15 01:35:41 +0800

    [2022-09-14 17:35:38]
    FILE:
    tests/nearly_duplicate_frames_detection_removal/pyav_effective_fps.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/nearly_duplicate_frames_detection_removal/pyav_effective_fps.py
+++ tests/nearly_duplicate_frames_detection_removal/pyav_effective_fps.py
@@ -2,10 +2,12 @@ import av
 # source = "/root/Desktop/works/pyjom/samples/video/nearly_duplicate_frames_detection_30fps_blend.mp4"  # this is evil. it defeats my shit.
 # KEYFRAME PERCENT: 1.36 %
 
-source = "/root/Desktop/works/pyjom/samples/video/dog_with_text.mp4"  # this is evil. it defeats my shit.
+# source = "/root/Desktop/works/pyjom/samples/video/dog_with_text.mp4"  # this is evil. it defeats my shit.
 # KEYFRAME PERCENT: 0.76 %
 # wtf?
 # even smaller.
+
+source = "/root/Desktop/works/pyjom/samples/video/karaoke_effects_source.mp4"
 
 container = av.open(source)
 
