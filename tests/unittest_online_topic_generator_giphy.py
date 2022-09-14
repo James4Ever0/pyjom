c27fb8f07a5d7de690920779cb91d19a45b3dcb6
GitJournal <app@gitjournal.io>
2022-09-14 12:42:52 +0800

    [2022-09-14 04:42:50]
    FILE:
    tests/unittest_online_topic_generator_giphy.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_online_topic_generator_giphy.py
+++ tests/unittest_online_topic_generator_giphy.py
@@ -5,18 +5,20 @@ from lazero.network import download
 from lazero.filesystem import tmpdir
 
 elem, label = OnlineTopicGenerator()
-sprint("LABEL:",label)
+sprint("LABEL:", label)
 # # 'pyjom.commons.OnlineTopicGenerator'
 # breakpoint()
 path = "/dev/shm/medialang/online_test"
 import os
+
 with tmpdir(path=path) as testDir:
     print("TESTDIR:", testDir)
     for asset_id, meta in elem:
-        print("X",asset_id, meta)
-        url = meta['url']
+        print("X", asset_id, meta)
+        url = meta["url"]
         extension = url.split("?")[0].split(".")[-1]
-        download_path = os.path.join(path,".".join([ asset_id,extension]))
+        basename = ".".join([asset_id, extension])
+        download_path = os.path.join(path, basename)
         download(url, download_path)
         # X ('sr8jYZVVsCmxddga8w', {'height': 480, 'width': 474, 'url': 'https://media0.giphy.com/media/sr8jYZVVsCmxddga8w/giphy.gif'})
         breakpoint()
