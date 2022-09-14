0438e9f5c2acab7b2506f4340c2f7266da169d22
GitJournal <app@gitjournal.io>
2022-09-14 13:59:56 +0800

    [2022-09-14 05:59:52]
    FILE:
    pyjom/modules/informationGathering/__init__.py
    pyjom/modules/informationGathering/onlineFetcher.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/informationGathering/__init__.py
+++ pyjom/modules/informationGathering/__init__.py
@@ -2,3 +2,4 @@ from pyjom.modules.informationGathering.dummyInfoGather import *
 from pyjom.modules.informationGathering.dummyFetcher import *
 from pyjom.modules.informationGathering.weiboInfo import *
 from pyjom.modules.informationGathering.localFetcher import *
+from pyjom.modules.informationGathering.onlineFetcher import *
