1d3bbc9f090d9745b6aee740a357955046ba35bf
GitJournal <app@gitjournal.io>
2022-09-14 10:31:07 +0800

    [2022-09-14 02:31:03]
    FILE:
    pyjom/languagetoolbox.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/languagetoolbox.py
+++ pyjom/languagetoolbox.py
@@ -1,6 +1,29 @@
 englishNLP = None
 englishStopWords = None
 porterStemmer = None
+    def print_topics(model, feature_names, n_top_words):
+        # 首先是遍历模型中存储的话题序号和话题内容
+        for topic_idx, topic in enumerate(model.components_):
+            # 然后打印话题的序号以及指定数量的最高频的关键词
+            message = "topic #%d:" % topic_idx
+            mList = [feature_names[i] for i in topic.argsort()[: -n_top_words - 1 : -1]]
+            mListStr = " ".join(
+                mList
+            )
+            message += mListStr
+            mSet  = set(mList) # the set contains word groups like 'river question'
+            cDict = {k:mList.count(k) for k in mSet}
+            mRealList = mListStr.split(" ")
+            mRealList = [x.strip() for x in mRealList if len(x.strip()) > 1] # usually things shorter than 2 letters are no good.
+            mRealSet = set(mRealList)
+            cRealDict = {k:mRealList.count(k) for k in mRealSet}
+
+            print("MESSAGE",message)
+            print("SET", mSet)
+            print("COUNT DICT", cDict) # pointless to count here?
+            print("RealSET", mRealSet)
+            print("RealCOUNT DICT", cRealDict)
+        print()
 
 def englishSentencePreprocessing(text, unwantedPOS=["PRON", "CCONJ", "ADP", "PART", "PUNCT", "AUX"]):
     global englishNLP, englishStopWords, porterStemmer
@@ -60,29 +83,6 @@ def englishTopicModeling(sentences, n_top_words = 10):
     lda = LatentDirichletAllocation(n_components=5)
     lda.fit(x_train)
 
-    def print_topics(model, feature_names, n_top_words):
-        # 首先是遍历模型中存储的话题序号和话题内容
-        for topic_idx, topic in enumerate(model.components_):
-            # 然后打印话题的序号以及指定数量的最高频的关键词
-            message = "topic #%d:" % topic_idx
-            mList = [feature_names[i] for i in topic.argsort()[: -n_top_words - 1 : -1]]
-            mListStr = " ".join(
-                mList
-            )
-            message += mListStr
-            mSet  = set(mList) # the set contains word groups like 'river question'
-            cDict = {k:mList.count(k) for k in mSet}
-            mRealList = mListStr.split(" ")
-            mRealList = [x.strip() for x in mRealList if len(x.strip()) > 1] # usually things shorter than 2 letters are no good.
-            mRealSet = set(mRealList)
-            cRealDict = {k:mRealList.count(k) for k in mRealSet}
-
-            print("MESSAGE",message)
-            print("SET", mSet)
-            print("COUNT DICT", cDict) # pointless to count here?
-            print("RealSET", mRealSet)
-            print("RealCOUNT DICT", cRealDict)
-        print()
 
 
     print_topics(lda, tfidf.get_feature_names(), n_top_words)
