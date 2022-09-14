943704747dbc1ef51de6b40f12ca23eeabedd04c
GitJournal <app@gitjournal.io>
2022-09-14 17:38:32 +0800

    [2022-09-14 09:38:28]
    FILE:
    pyjom/modules/informationGathering/onlineFetcher.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/informationGathering/onlineFetcher.py
+++ pyjom/modules/informationGathering/onlineFetcher.py
@@ -4,11 +4,13 @@ from typing import Literal
 @decorator
 def OnlineFetcher(infoList, source:Literal['giphy']='giphy', frame_size_filter:dict={'width':{'min':200,'max':1000}, 'height':{'min':200,'max':1000}}, tempdir='/dev/shm/medialang/online'):
     # how do you chain this shit up?
+    assert os.path.isabs(tempdir)
+    assert os.path.exists(tempdir)
     for info in infoList: # generator most likely
         if source=='giphy':
             (source_id, frameMeta) = info
             flag = frameSizeFilter(frameMeta, frame_size_filter)
             if flag:
                 # this time it is selected.
-                downloadPath = 
+                downloadPath = tempdir
                 yield downloadPath
