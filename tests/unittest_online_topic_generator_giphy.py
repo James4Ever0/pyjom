74b3f7c979e3274544278f547cd445bb1b7369c1
GitJournal <app@gitjournal.io>
2022-09-14 11:52:45 +0800

    [2022-09-14 03:52:41]
    FILE:
    tests/unittest_online_topic_generator_giphy.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_online_topic_generator_giphy.py
+++ tests/unittest_online_topic_generator_giphy.py
@@ -2,7 +2,7 @@ from test_commons import *
 from pyjom.modules.topicGenerator import OnlineTopicGenerator
 from lazero.utils import sprint
 
-elem, label =  OnlineTopicGenerator()
+elem, label = OnlineTopicGenerator()
 sprint(label)
 breakpoint()
 
