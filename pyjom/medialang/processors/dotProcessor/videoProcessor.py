019e9705e793204f2fdaf74d3487258b7dabd135
GitJournal <app@gitjournal.io>
2022-09-16 01:38:13 +0800

    [2022-09-15 17:38:09]
    FILE:
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/medialang/processors/dotProcessor/videoProcessor.py
+++ pyjom/medialang/processors/dotProcessor/videoProcessor.py
@@ -130,7 +130,7 @@ def ffmpegVideoPreProductionFilter(
         # estimate the shape with multiple rectangles? packing algorithm?
         # polygon to rectangle? decomposition?
         # pass
-    simpleFiltersValueMap = {x: -2 for x in simpleFilters}
+    simpleFiltersValueMap = {x: -2 for x in simpleFilters+['randomFlip']}
     commandValueMap = {
         "empty": -1,
         "delogo": 0,
