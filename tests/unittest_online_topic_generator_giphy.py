ad67132a6ddaa9fd1019d46ad9815402600c6650
GitJournal <app@gitjournal.io>
2022-09-15 22:57:40 +0800

    [2022-09-15 14:57:36]
    FILE:
    tests/unittest_online_topic_generator_giphy.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_online_topic_generator_giphy.py
+++ tests/unittest_online_topic_generator_giphy.py
@@ -47,7 +47,7 @@ with tmpdir(path=tmpPath) as testDir:
             duration_valid = checkMinMaxDict(duration,duration_filter)
             fps_valid = checkMinMaxDict(fps_float,fps_filter)
             valid = True
-            def checkValid
+            for mValid, mFilter, flag in 
                 valid = checkMinMaxDict(mValue,mFilter)
                 if not valid:
                     print("skipping due to invalid %s: %s" % (flsg,fps_float))
