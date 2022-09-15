9acd27047c4505a57c2c24925ffeade7ee299bcb
GitJournal <app@gitjournal.io>
2022-09-15 17:45:16 +0800

    [2022-09-15 09:45:13]
    FILE:
    tests/nearly_duplicate_frames_detection_removal/knn_similar_color_extraction.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/nearly_duplicate_frames_detection_removal/knn_similar_color_extraction.py
+++ tests/nearly_duplicate_frames_detection_removal/knn_similar_color_extraction.py
@@ -1,6 +1,7 @@
 # i'd say i want centrality below 10 percent. what's the catch?
 
 src = "/root/Desktop/works/pyjom/samples/image/miku_on_green.png"
+# for this one we have double centers. fuck.
 
 # with text. a meme.
 # src = "/root/Desktop/works/pyjom/samples/image/dog_saturday_night.bmp"
