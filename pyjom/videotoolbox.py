b6fe7903abaa15e0adf8af9834889879a953773c
GitJournal <app@gitjournal.io>
2022-09-15 22:17:20 +0800

    [2022-09-15 14:17:17]
    FILE:
    pyjom/videotoolbox.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/videotoolbox.py
+++ pyjom/videotoolbox.py
@@ -1231,7 +1231,7 @@ def getEffectiveFPS(videoPath, tempdir = "/dev/shm/medialang/get_effective_fps",
         mpdecimate_args_choices = [None, "hi=1:lo=1:frac=1:max=0", "hi=200:lo=200:frac=1:max=0"]
 
         result = mpdecimate_export_duplicate_clip_ranges(
-            source, mpdecimate_args=mpdecimate_args_choices[0]
+            source, mpdecimate_args=mpdecimate_args
             # ,video_size="480x480"
         )
 
