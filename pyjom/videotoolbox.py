7b6ae7f38c9e51db8049f4407254a97c28100ca3
GitJournal <app@gitjournal.io>
2022-09-15 22:01:45 +0800

    [2022-09-15 14:01:41]
    FILE:
    pyjom/videotoolbox.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/videotoolbox.py
+++ pyjom/videotoolbox.py
@@ -996,6 +996,7 @@ def detectPipRegionOverTime(
 from lazero.filesystem import tmpdir
 def getEffectiveFPS(videoPath, convert_fps_target=15, tempdir = "/dev/shm/medialang/get_effective_fps"):
     # use ffmpeg to covert the target first!
+    with tmpdir(path=tempdir) as 
     return effectiveFPS
 
 # this is a generator, not a list!
