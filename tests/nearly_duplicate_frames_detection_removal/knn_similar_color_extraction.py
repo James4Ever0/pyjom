068e261623bb597a792ec6e78547951c0f035b32
GitJournal <app@gitjournal.io>
2022-09-15 15:18:31 +0800

    [2022-09-15 07:18:28]
    FILE:
    tests/nearly_duplicate_frames_detection_removal/knn_similar_color_extraction.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/nearly_duplicate_frames_detection_removal/knn_similar_color_extraction.py
+++ tests/nearly_duplicate_frames_detection_removal/knn_similar_color_extraction.py
@@ -2,3 +2,6 @@ src = "/root/Desktop/works/pyjom/samples/image/similar_color_extraction.bmp" # u
 
 import numpy as np
 from lazero.utils.importers import cv2_custom_build_init
+cv2_custom_build_init()
+
+import cv2
