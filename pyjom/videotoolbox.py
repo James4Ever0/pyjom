2fdcf002481deb3fff1af4304de57ab067274e54
GitJournal <app@gitjournal.io>
2022-09-15 22:04:57 +0800

    [2022-09-15 14:04:54]
    FILE:
    pyjom/videotoolbox.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/videotoolbox.py
+++ pyjom/videotoolbox.py
@@ -1196,6 +1196,7 @@ def getEffectiveFPS(videoPath, convert_fps_target=15, tempdir = "/dev/shm/medial
 
         # source = "/root/Desktop/works/pyjom/samples/video/kitty_flash.gif" # 9.50 fps freaking hell.
         # how about 15fps or something 
+        import uuid
         convertedVideoPath = 
         source = convertedVideoPath# very unlikely to go higher.
         # 15fps is just fine for shit like this.
