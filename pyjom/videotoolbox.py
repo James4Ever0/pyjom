ee35fcaf181da65c2af39cef1b4635eed144e916
GitJournal <app@gitjournal.io>
2022-09-16 00:21:59 +0800

    [2022-09-15 16:21:54]
    FILE:
    pyjom/videotoolbox.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/videotoolbox.py
+++ pyjom/videotoolbox.py
@@ -512,6 +512,7 @@ def detectStationaryLogoOverTime(
         currentRect = [(x, y), (x + w, y + h)]
         boundingRects.append(currentRect.copy())
     print("TOTAL {} STATIONARY LOGOS.".format(len(cnts2)))
+    delogoCommandList = []
     for index, (mStart, mEnd) in enumerate(start_end_list):
         for currentRect in boundingRects:
             if cornersOnly:
