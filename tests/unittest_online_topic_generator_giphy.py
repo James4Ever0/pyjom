7aebf0aeb0392cd39f1d8c64f9e1c8527ea950aa
GitJournal <app@gitjournal.io>
2022-09-16 02:06:05 +0800

    [2022-09-15 18:06:01]
    FILE:
    tests/unittest_online_topic_generator_giphy.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_online_topic_generator_giphy.py
+++ tests/unittest_online_topic_generator_giphy.py
@@ -61,6 +61,7 @@ with tmpdir(path=tmpPath) as testDir:
                 [get_duration, duration_filter, checkMinMaxDict,"duration", ],
                 [get_fps_float, fps_filter, checkMinMaxDict,"fps", ],
                 [getVideoColorCentrality, video_color_filter, "video_color_centrality"],
+                [get, video_color_filter, "vi"],
             ]
             for function, mFilter,filterFunc, flag in mList:
                 mValue = function(local_video_location)
