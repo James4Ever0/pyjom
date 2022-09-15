93dc0f8728e44e6753fa2bb44974c48e06015cc1
GitJournal <app@gitjournal.io>
2022-09-15 19:32:45 +0800

    [2022-09-15 11:32:42]
    FILE:
    pyjom/videotoolbox.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/videotoolbox.py
+++ pyjom/videotoolbox.py
@@ -14,7 +14,8 @@ def getEffectiveFPS(videoPath, convert_fps_target=15):
 def getVideoColorCentrality(videoPath,
     denoise=True,
     frame_sample_limit=3,
-    centrality_threshold=
+    centrality_threshold=,# for early return.
+    mean_centrality_threshold=,
     sample_size_limit=5000,
     epsilon=0.01,  # shit man.
     shift=2,
