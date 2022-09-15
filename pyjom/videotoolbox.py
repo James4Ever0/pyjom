b298c882f4653d204b2d2ac6e01a1b3815fc05c2
GitJournal <app@gitjournal.io>
2022-09-15 19:33:55 +0800

    [2022-09-15 11:33:52]
    FILE:
    pyjom/videotoolbox.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/videotoolbox.py
+++ pyjom/videotoolbox.py
@@ -21,7 +21,7 @@ def getVideoColorCentrality(videoPath,
     n_clusters=5,
     batch_size=45,
     max_no_improvement=10):
-    return centrality,"mean":mean_centrality}, "max_nearby_center_percentage":{'max':max_max_nearby_center_percentage, 'mean':mean_max_nearby_center_percentage}}
+    yield centrality,max_nearby_center_percentage}}
 
 
 def checkXYWH(XYWH, canvas, minArea=20):
