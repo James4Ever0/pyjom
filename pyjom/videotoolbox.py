b7cacc96c749dd33152713664e7adfc23f686e8f
GitJournal <app@gitjournal.io>
2022-09-16 00:20:32 +0800

    [2022-09-15 16:20:29]
    FILE:
    pyjom/videotoolbox.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/videotoolbox.py
+++ pyjom/videotoolbox.py
@@ -541,7 +541,7 @@ def detectStationaryLogoOverTime(
         # cv2.rectangle(myMask2, (x, y), (x + w, y + h), 255, -1)
     # get the final dictionary
     if len(mFinalDelogoFilters) == 0:
-        return {}
+        value = {}
     else:
         delogoCommandSet = "|".join(mFinalDelogoFilters)
         delogoCommandList.append({delogoCommandSet: [(start, end)]})
