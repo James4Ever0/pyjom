d3991e75ebdcff544f87dca58bb311fff6f78537
GitJournal <app@gitjournal.io>
2022-09-15 14:56:13 +0800

    [2022-09-15 06:56:10]
    FILE:
    tests/nearly_duplicate_frames_detection_removal/fast_vqa_test.sh
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/nearly_duplicate_frames_detection_removal/fast_vqa_test.sh
+++ tests/nearly_duplicate_frames_detection_removal/fast_vqa_test.sh
@@ -1,3 +1,3 @@
 cd FAST-VQA
 VIDEO="/root/Desktop/works/pyjom/samples/video/nearly_duplicate_frames_detection_30fps.mp4"
-python3 vqa.py -o ./options/fast/f3dvqa-b.yml -v $VIDEO
+python3 vqa.py -o ./options/fast/f3dvqa-b.yml -v $VIDEO -d cpu
