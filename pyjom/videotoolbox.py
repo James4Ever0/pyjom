b1e295d9b84e88b1d61658d69de499a251ff8ad6
GitJournal <app@gitjournal.io>
2022-09-15 19:29:02 +0800

    [2022-09-15 11:29:00]
    FILE:
    pyjom/videotoolbox.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/videotoolbox.py
+++ pyjom/videotoolbox.py
@@ -19,7 +19,8 @@ sample_size_limit=5000,
     n_clusters=5,
     batch_size=45,
     max_no_improvement=10):
-    return 
+    return centrality, max_nearby_center_percentage
+
 
 def checkXYWH(XYWH, canvas, minArea=20):
     import math
