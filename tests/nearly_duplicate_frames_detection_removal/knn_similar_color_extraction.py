735483ee8b328157a825c961b5e637a81e8e8946
GitJournal <app@gitjournal.io>
2022-09-15 16:46:08 +0800

    [2022-09-15 08:46:04]
    FILE:
    tests/nearly_duplicate_frames_detection_removal/knn_similar_color_extraction.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/nearly_duplicate_frames_detection_removal/knn_similar_color_extraction.py
+++ tests/nearly_duplicate_frames_detection_removal/knn_similar_color_extraction.py
@@ -68,8 +68,8 @@ label_percentage = {x: labels.count(x)/sample_size for x in range(n_clusters)}
 
 for center in cluster_centers:
     # fetch area nearby given center
-    center_int = center.
-    upper = center_int +2
-    lower= center_int-3
+    center_int = center.astype(np.uint8)
+    upper = center_int +5
+    lower= center_int-5
     mask = cv2.inRange(image, lower, upper)
 	output = cv2.bitwise_and(image, image, mask = mask)
