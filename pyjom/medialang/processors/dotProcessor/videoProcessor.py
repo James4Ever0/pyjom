c9071d48a7e34dc7ce595edf1dca90b628d3dbdd
GitJournal <app@gitjournal.io>
2022-09-16 02:00:06 +0800

    [2022-09-15 18:00:03]
    FILE:
    pyjom/medialang/processors/dotProcessor/videoProcessor.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/medialang/processors/dotProcessor/videoProcessor.py
+++ pyjom/medialang/processors/dotProcessor/videoProcessor.py
@@ -103,7 +103,7 @@ def ffmpegVideoPreProductionFilter(
                 args.append(elem)
         return args, kwargs
 
-    def scaleFilterWithStringParams(stream, commandString):
+    def stringFilterParser(stream, commandString):
         filterName = commandString.split("_")[0]
         filterPrefix = "{}_".format(filterName)
         filterCommandString = commandString[len(filterPrefix):]
