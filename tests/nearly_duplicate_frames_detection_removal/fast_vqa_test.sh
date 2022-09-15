041bb457a253782396da84546f1440735486cd44
GitJournal <app@gitjournal.io>
2022-09-15 15:06:52 +0800

    [2022-09-15 07:06:48]
    FILE:
    tests/nearly_duplicate_frames_detection_removal/fast_vqa_test.sh
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/nearly_duplicate_frames_detection_removal/fast_vqa_test.sh
+++ tests/nearly_duplicate_frames_detection_removal/fast_vqa_test.sh
@@ -2,5 +2,7 @@ cd FAST-VQA
 # VIDEO="/root/Desktop/works/pyjom/samples/video/nearly_duplicate_frames_detection_30fps.mp4"
 # The quality score of the video is 0.11833.
 VIDEO="/root/Desktop/works/pyjom/samples/video/kitty_flash_15fps.mp4"
+# The quality score of the video is 0.12778.
 
+# nothing serious. it does not produce significant shits.
 python3 vqa.py -o ./options/fast/f3dvqa-b.yml -v $VIDEO -d cpu
