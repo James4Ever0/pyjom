1629adcb22a473ab57b014eb42d90faf1cc5a816
GitJournal <app@gitjournal.io>
2022-09-15 17:59:43 +0800

    [2022-09-15 09:59:37]
    FILE:
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/nearly_duplicate_frames_detection_removal/knn_similar_color_extraction.py
+++ tests/nearly_duplicate_frames_detection_removal/knn_similar_color_extraction.py
@@ -64,7 +64,7 @@
 # CENTRALITY: 3.91 %
 
 # now the 八点半配音
-src = "/root/Desktop/works/pyjom/samples/image/is_this_duck.bmp"
+# src = "/root/Desktop/works/pyjom/samples/image/is_this_duck.bmp"
 # CENTER: [252.66293811 177.62005966 126.37844892]
 # POSITIVE COUNT: 222893
 # SUM: 668679.0 MIN: 0 MAX: 3
