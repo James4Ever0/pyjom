ccc443e1c647df092b92ce715821edc1248047ab
GitJournal <app@gitjournal.io>
2022-09-15 19:33:27 +0800

    [2022-09-15 11:33:24]
    FILE:
    pyjom/videotoolbox.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/videotoolbox.py
+++ pyjom/videotoolbox.py
@@ -11,6 +11,7 @@ from pyjom.imagetoolbox import *
 def getEffectiveFPS(videoPath, convert_fps_target=15):
     return effectiveFPS
 
+# this is a generator, not a 
 def getVideoColorCentrality(videoPath,
     denoise=True,
     frame_sample_limit=3,
@@ -20,7 +21,7 @@ def getVideoColorCentrality(videoPath,
     n_clusters=5,
     batch_size=45,
     max_no_improvement=10):
-    return {"centrality":{"max":max_centrality,"mean":mean_centrality}, "max_nearby_center_percentage":{'max':max_max_nearby_center_percentage, 'mean':mean_max_nearby_center_percentage}}
+    return centrality,"mean":mean_centrality}, "max_nearby_center_percentage":{'max':max_max_nearby_center_percentage, 'mean':mean_max_nearby_center_percentage}}
 
 
 def checkXYWH(XYWH, canvas, minArea=20):
