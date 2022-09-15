bcbf95e53d859c7fe69e0d47c772f325593bcf86
GitJournal <app@gitjournal.io>
2022-09-15 10:11:41 +0800

    [2022-09-15 02:11:37]
    FILE:
    tests/unittest_online_topic_generator_giphy.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_online_topic_generator_giphy.py
+++ tests/unittest_online_topic_generator_giphy.py
@@ -4,16 +4,18 @@ from lazero.utils import sprint
 from lazero.network import download
 from lazero.filesystem import tmpdir
 
-elem, label = OnlineTopicGenerator()
+elems, label = OnlineTopicGenerator()
 sprint("LABEL:", label)
 # # 'pyjom.commons.OnlineTopicGenerator'
 # breakpoint()
 path = "/dev/shm/medialang/online_test"
 import os
 
+flag = 'only_'
+
 with tmpdir(path=path) as testDir:
     print("TESTDIR:", testDir)
-    for asset_id, meta in elem:
+    for asset_id, meta in elems:
         print("X", asset_id, meta)
         url = meta["url"]
         extension = url.split("?")[0].split(".")[-1]
