eecedd629802aa9b5b21c7b62b4d3d035c9bd9a8
GitJournal <app@gitjournal.io>
2022-09-15 17:33:22 +0800

    [2022-09-15 09:33:19]
    FILE:
    tests/nearly_duplicate_frames_detection_removal/knn_similar_color_extraction.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/nearly_duplicate_frames_detection_removal/knn_similar_color_extraction.py
+++ tests/nearly_duplicate_frames_detection_removal/knn_similar_color_extraction.py
@@ -9,7 +9,6 @@
 
 # the filter: removegrain
 # src = "/root/Desktop/works/pyjom/samples/image/kitty_flash.bmp"  # use some filter first, or rather not to?
-
 # CENTER: [1.37254902 2.34313725 9.46078431]
 # POSITIVE COUNT: 2600
 # SUM: 7800.0 MIN: 0 MAX: 3
