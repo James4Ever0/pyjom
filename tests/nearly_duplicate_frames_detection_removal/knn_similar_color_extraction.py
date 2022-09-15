120935eb59e4340aa86ea403f61032ff8f86b40a
GitJournal <app@gitjournal.io>
2022-09-15 17:33:07 +0800

    [2022-09-15 09:33:04]
    FILE:
    tests/nearly_duplicate_frames_detection_removal/knn_similar_color_extraction.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/nearly_duplicate_frames_detection_removal/knn_similar_color_extraction.py
+++ tests/nearly_duplicate_frames_detection_removal/knn_similar_color_extraction.py
@@ -16,12 +16,14 @@
 # NEARBY CENTER PERCENTAGE: 3.91 %
 
 # now the 八点半配音
-src = "/root/Desktop/works/pyjom/samples/image/is_this_duck.bmp"
+# src = "/root/Desktop/works/pyjom/samples/image/is_this_duck.bmp"
 # CENTER: [252.66293811 177.62005966 126.37844892]
 # POSITIVE COUNT: 222893
 # SUM: 668679.0 MIN: 0 MAX: 3
 # NEARBY CENTER PERCENTAGE: 37.79 %
-# src = "/root/Desktop/works/pyjom/samples/image/pig_really.bmp"
+# likely to be the blue.
+
+src = "/root/Desktop/works/pyjom/samples/image/pig_really.bmp"
 
 import numpy as np
 from lazero.utils.importers import cv2_custom_build_init
