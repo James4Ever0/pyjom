8eceb1ff24c42c353e1321777d9e576ab5bbf365
GitJournal <app@gitjournal.io>
2022-09-15 19:20:10 +0800

    [2022-09-15 11:20:08]
    FILE:
    pyjom/imagetoolbox.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/imagetoolbox.py
+++ pyjom/imagetoolbox.py
@@ -15,10 +15,10 @@ def getColorCentrality(image,):
     shape = image.shape
     if len(shape) != 3:
         print("weird shit.")
-        breakpoint() # for further improvements?
+        
     if shape[2] != 3:
         print("depth not right.")
-        breakpoint() # for further improvements?
+        
     # for i in range(3):
     #     image[:,:,i] = i
 
