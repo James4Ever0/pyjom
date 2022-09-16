9b772bdb2ceff26c8cce69f8a141f2d05d6c89e6
GitJournal <app@gitjournal.io>
2022-09-16 23:43:31 +0800

    [2022-09-16 15:43:29]
    FILE:
    pyjom/medialang/functions/detectors/__init__.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/medialang/functions/detectors/__init__.py
+++ pyjom/medialang/functions/detectors/__init__.py
@@ -23,9 +23,9 @@ def processInputWrapperFunction(function,wrapperFunction):
     return mFunction
 
 medialangDetectors = {
-    "subtitle_detector": processInputWrapperFunction(mediaSubtitleDetector,,
-    "framediff_detector": processInputWrapperFunction(videoDiffDetector,,
-    "blackout_detector": processInputWrapperFunction(blackoutDetector,,
-    "yolov5_detector": processInputWrapperFunction(yolov5_Detector,,
-    "frameborder_detector": processInputWrapperFunction(frameborder_Detector,,
+    "subtitle_detector": processInputWrapperFunction(mediaSubtitleDetector,getMedialangInputFixed),
+    "framediff_detector": processInputWrapperFunction(videoDiffDetector,getMedialangInputFixed),
+    "blackout_detector": processInputWrapperFunction(blackoutDetector,getMedialangInputFixed),
+    "yolov5_detector": processInputWrapperFunction(yolov5_Detector,getMedialangInputFixed),
+    "frameborder_detector": processInputWrapperFunction(frameborder_Detector,getMedialangInputFixed),
 }
