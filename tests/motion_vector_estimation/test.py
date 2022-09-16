5906efdf272dc14eaa99d8a5f3ebb58457aab217
GitJournal <app@gitjournal.io>
2022-09-16 12:33:45 +0800

    [2022-09-16 04:33:41]
    FILE:
    tests/motion_vector_estimation/test.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/motion_vector_estimation/test.py
+++ tests/motion_vector_estimation/test.py
@@ -21,7 +21,7 @@ def checkMacroBlock(value):
 
 while True:
     success, frame, motion_vectors, frame_type, timestamp = cap.read()
-    print(frame.shape)
+    frame, frame.shape
     # breakpoint()
     if success:
         # what is the content of this motion vector?
