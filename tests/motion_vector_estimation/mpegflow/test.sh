fba44b61bb47d5c72c36a490a3990ca5960a323e
GitJournal <app@gitjournal.io>
2022-09-16 22:43:44 +0800

    [2022-09-16 14:43:41]
    FILE:
    tests/motion_vector_estimation/mpegflow/test.sh
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/motion_vector_estimation/mpegflow/test.sh
+++ tests/motion_vector_estimation/mpegflow/test.sh
@@ -1,3 +1,3 @@
 
 
-mkdir -p examples/vis_dump && ./mpegflow examples/mpi_sintel_final_alley_1.avi | ./vis examples/mpi_sintel_final_alley_1.avi examples/vis_dump
+# mkdir -p examples/vis_dump && ./mpegflow $VIDEO | ./vis $VIDEO examples/vis_dump
