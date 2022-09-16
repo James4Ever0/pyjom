1878c55d921a50d41b0f6108e55aff7a31399c19
GitJournal <app@gitjournal.io>
2022-09-17 00:58:54 +0800

    [2022-09-16 16:58:50]
    FILE:
    tests/unittest_filesystemreviewer.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_filesystemreviewer.py
+++ tests/unittest_filesystemreviewer.py
@@ -31,7 +31,7 @@ def extractYolov5DetectionData(detectionData):
     filepath, review_data = detectionData['review']['review']
     timeseries_data = review_data['yolov5_detector']['yolov5_detector']
     for frame in timeseries_data:
-        
+        timestamp, frameNumber, 
 
 
 for result in resultGenerator:
