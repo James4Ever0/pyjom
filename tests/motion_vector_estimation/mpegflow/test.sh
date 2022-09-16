40bc6c424a8b123e385e96a352d039a76864fc95
GitJournal <app@gitjournal.io>
2022-09-16 22:45:30 +0800

    [2022-09-16 14:45:27]
    FILE:
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/motion_vector_estimation/mpegflow/test.sh
+++ tests/motion_vector_estimation/mpegflow/test.sh
@@ -1,5 +1,5 @@
 VIDEO="/root/Desktop/works/pyjom/samples/video/cute_cat_gif.mp4"
 
-./mpegflow $VIDEO > output.txt
+# ./mpegflow $VIDEO > output.txt
 
-# mkdir -p examples/vis_dump && ./mpegflow $VIDEO | ./vis $VIDEO examples/vis_dump
+mkdir -p examples/vis_dump && ./mpegflow $VIDEO | ./vis $VIDEO examples/vis_dump
