aa99140ff7f2ec3a80369ff871b3276dadbb5c52
GitJournal <app@gitjournal.io>
2022-09-16 20:22:20 +0800

    [2022-09-16 12:22:17]
    FILE:
    tests/motion_vector_estimation/test.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/motion_vector_estimation/test.py
+++ tests/motion_vector_estimation/test.py
@@ -8,12 +8,12 @@
 # source = "/root/Desktop/works/pyjom/samples/video/kitty_flash_15fps.gif"
 
 # without mestimate
-source = "/root/Desktop/works/pyjom/samples/video/cat_invalid_eye_rolling_without_mestimate.mp4"
-# source = "/root/Desktop/works/pyjom/samples/video/kitty_flash_15fps_without_mestimate.mp4"
+# source = "/root/Desktop/works/pyjom/samples/video/cat_invalid_eye_rolling_without_mestimate.mp4"
+source = "/root/Desktop/works/pyjom/samples/video/kitty_flash_15fps_without_mestimate.mp4"
 
 # with mestimate
-source = "/root/Desktop/works/pyjom/samples/video/cat_invalid_eye_rolling_without_mestimate.mp4"
-# source = "/root/Desktop/works/pyjom/samples/video/kitty_flash_15fps_without_mestimate.mp4"
+# source = "/root/Desktop/works/pyjom/samples/video/cat_invalid_eye_rolling_with_mestimate.mp4"
+# source = "/root/Desktop/works/pyjom/samples/video/kitty_flash_15fps_with_mestimate.mp4"
 
 from lazero.utils.importers import cv2_custom_build_init
 from sniffio import current_async_library
