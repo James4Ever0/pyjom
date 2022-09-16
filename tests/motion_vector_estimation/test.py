df38a6c23c87428bf36972fbaef0f4a007f0e660
GitJournal <app@gitjournal.io>
2022-09-16 12:35:41 +0800

    [2022-09-16 04:35:38]
    FILE:
    tests/motion_vector_estimation/test.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/motion_vector_estimation/test.py
+++ tests/motion_vector_estimation/test.py
@@ -5,7 +5,9 @@ from lazero.utils.importers import cv2_custom_build_init
 
 cv2_custom_build_init()
 from mvextractor.videocap import VideoCap
-from caer.video.frames_and_fps import count_frames_and_
+from caer.video.frames_and_fps import count_frames
+
+framesCount = count_frames(source)
 
 cap = VideoCap()
 cap.open(source)  # wtf is going on here?
