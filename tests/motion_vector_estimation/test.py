1bdf4b13be4a405a1cd161fa434733507d8cd45a
GitJournal <app@gitjournal.io>
2022-09-16 20:06:48 +0800

    [2022-09-16 12:06:45]
    FILE:
    tests/motion_vector_estimation/test.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/motion_vector_estimation/test.py
+++ tests/motion_vector_estimation/test.py
@@ -115,8 +115,8 @@ def averageMotionVectors(motion_vector_list):
 motion_area_ratio_array = []
 # average_weighted_motion_vector_array = []
 # average_global_weighted_motion_vector_array = []
-average_weighted_motion_vector_cartesiaon_array = []
-average_global_weighted_motion_vector_cartesiaon_array = []
+average_weighted_motion_vector_cartesian_array = []
+average_global_weighted_motion_vector_cartesian_array = []
 average_weighted_motion_vectors_filtered_cartesian_distance_array = []
 average_global_weighted_motion_vectors_filtered_cartesian_distance_array = []
 
