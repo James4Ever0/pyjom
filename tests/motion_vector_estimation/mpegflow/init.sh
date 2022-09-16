6c6c865b954f6bcd719127c8a251e570aa96df1b
GitJournal <app@gitjournal.io>
2022-09-16 21:12:18 +0800

    [2022-09-16 13:12:15]
    FILE:
    tests/motion_vector_estimation/mpegflow/init.sh
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/motion_vector_estimation/mpegflow/init.sh
+++ tests/motion_vector_estimation/mpegflow/init.sh
@@ -1,2 +1,2 @@
-curl -O https://github.com/vadimkantorov/mpegflow/releases/download/1.0/mpegflow
-curl -O https://github.com/vadimkantorov/mpegflow/releases/download/1.0/vis
+curl -L -O https://github.com/vadimkantorov/mpegflow/releases/download/1.0/mpegflow
+curl -L -O https://github.com/vadimkantorov/mpegflow/releases/download/1.0/vis
