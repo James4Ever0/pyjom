5fc5893d08e501f1bdc170a168a00be2dc521e6d
GitJournal <app@gitjournal.io>
2022-09-16 22:26:26 +0800

    [2022-09-16 14:26:23]
    FILE:
    tests/unittest_filesystemreviewer.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_filesystemreviewer.py
+++ tests/unittest_filesystemreviewer.py
@@ -1,12 +1,12 @@
 from test_commons import *
-from pyjom.modules.contentReviewer import 
+from pyjom.modules.contentReviewer import filesystemReviewer
 
 autoArgs = {"subtitle_detector": {"timestep": 0.2},"yolov5_detector":{"model":"yolov5x"}}
 template_names = ["yolov5_detector.mdl.j2"]
 semiauto=False
 dummy_auto=False
 
-filesystemReviewer(
+reviewer = filesystemReviewer(
                     auto=True,
                     semiauto=semiauto,
                     dummy_auto=dummy_auto,
