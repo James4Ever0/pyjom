9b36b7a658d929b210a4ad25e25c6739dcd02f09
GitJournal <app@gitjournal.io>
2022-09-16 12:32:54 +0800

    [2022-09-16 04:32:51]
    FILE:
    tests/motion_vector_estimation/test.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/motion_vector_estimation/test.py
+++ tests/motion_vector_estimation/test.py
@@ -43,8 +43,8 @@ while True:
                 # src_x, src_y may not apply the same rule.
                 # assert src_x % 16 == 8
                 # assert src_y % 16 == 8
-                assert checkMacroBlock(dst_x) % 16 == 8
-                assert checkMacroBlock(dst_y) % 16 == 8
+                assert checkMacroBlock(dst_x) is not None
+                assert checkMacroBlock(dst_y) is not None
             except:
                 # print('source',src_x, src_y)
                 print('destionation',dst_x, dst_y)
