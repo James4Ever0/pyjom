49493ce1073b6b0e66bb9605d87ece5c4819fea6
GitJournal <app@gitjournal.io>
2022-09-16 22:46:32 +0800

    [2022-09-16 14:46:29]
    FILE:
    tests/motion_vector_estimation/mpegflow/test.sh
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/motion_vector_estimation/mpegflow/test.sh
+++ tests/motion_vector_estimation/mpegflow/test.sh
@@ -2,5 +2,6 @@ VIDEO="/root/Desktop/works/pyjom/samples/video/cute_cat_gif.mp4"
 
 # ./mpegflow $VIDEO > output.txt
 
-# it does not help because the c
+# it does not help because the .so file is fake. you need a real one.
+
 # mkdir -p examples/vis_dump && ./mpegflow $VIDEO | ./vis $VIDEO examples/vis_dump
