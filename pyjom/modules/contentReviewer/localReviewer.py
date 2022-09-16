e13ee83e585d65653064a58dcdb7e96c0beccb94
GitJournal <app@gitjournal.io>
2022-09-16 23:18:52 +0800

    [2022-09-16 15:18:49]
    FILE:
    pyjom/modules/contentReviewer/localReviewer.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/contentReviewer/localReviewer.py
+++ pyjom/modules/contentReviewer/localReviewer.py
@@ -3,7 +3,25 @@ from pyjom.modules.contentCensoring.core import localCensor
 import json
 
 
-def filesystemReviewerNoGenerator
+def filesystemReviewerNoGenerator(elem,):
+    print("element inside:")
+    print("_" * 20)
+    _, pretty_printed = jsonPrettyPrint(elem)
+    print(pretty_printed)
+    print("ELEMENT", elem)
+    breakpoint()
+    review, source = localCensor(
+        elem,
+        auto=auto,
+        semiauto=semiauto,
+        dummy_auto=dummy_auto,
+        args=args,
+        template_names=template_names,
+    )  # unnoticed source.
+    print("review:")
+    # breakpoint()
+    print(json.dumps(review, indent=4))
+    reviewResult = {"review": review, "source": source}
 
 @decorator
 def filesystemReviewer(
