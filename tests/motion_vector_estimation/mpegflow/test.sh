fc9a9d6ac1e956fb91c9a758a363ef98f4f1f262
GitJournal <app@gitjournal.io>
2022-09-16 22:46:14 +0800

    [2022-09-16 14:46:11]
    FILE:
    tests/motion_vector_estimation/mpegflow/test.sh
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/motion_vector_estimation/mpegflow/test.sh
+++ tests/motion_vector_estimation/mpegflow/test.sh
@@ -2,4 +2,5 @@ VIDEO="/root/Desktop/works/pyjom/samples/video/cute_cat_gif.mp4"
 
 # ./mpegflow $VIDEO > output.txt
 
-mkdir -p examples/vis_dump && ./mpegflow $VIDEO | ./vis $VIDEO examples/vis_dump
+# it does not help because the c
+# mkdir -p examples/vis_dump && ./mpegflow $VIDEO | ./vis $VIDEO examples/vis_dump
