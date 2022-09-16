5ea5cf0a573862701a89ba08e9b23a6ee8e670f4
GitJournal <app@gitjournal.io>
2022-09-16 11:50:07 +0800

    [2022-09-16 03:50:04]
    FILE:
    tests/motion_vector_estimation/test.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/motion_vector_estimation/test.py
+++ tests/motion_vector_estimation/test.py
@@ -12,7 +12,8 @@ cap.open(source) # wtf is going on here?
 while True:
     success, frame, motion_vectors, frame_type,timestamp = cap.read()
     if success:
-        print(motion_vectors)
-        print(motion_vectors.shape)
+        
+        # print(motion_vectors)
+        # print(motion_vectors.shape)
     else:
         break
