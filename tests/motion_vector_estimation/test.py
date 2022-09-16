3b8627b4037d18c2cfa065b4124c3549e84fd816
GitJournal <app@gitjournal.io>
2022-09-16 17:29:32 +0800

    [2022-09-16 09:29:29]
    FILE:
    tests/motion_vector_estimation/test.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/motion_vector_estimation/test.py
+++ tests/motion_vector_estimation/test.py
@@ -54,8 +54,8 @@ for _ in progressbar.progressbar(range(framesCount)):
         # df = pd.DataFrame(motion_vectors)
         # df = pd.DataFrame(motion_vectors,index=['source_index','unk0','unk1','src_x','src_y','dst_x','dst_y','motion_x','motion_y','motion_scale'])
         # breakpoint()
-        motion_vectors_simplified = motion_vectors[motion_vectors[:,],[0,5,6,7,8,9]]
-        motion_vectors_scale = motion_vectors_simplified[:,[9]]
+        motion_vectors_simplified = motion_vectors[motion_vectors[:,0] <0,[0,5,6,7,8,9]]
+        motion_vectors_scale = motion_vectors_simplified[:,[5]]
         motion_vectors_
         print(motion_vectors_scale.shape)
         breakpoint()
