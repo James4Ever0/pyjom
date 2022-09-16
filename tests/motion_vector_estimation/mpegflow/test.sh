cab1d8d989322f05b2b3a1eb60a37a08c87cd0ab
GitJournal <app@gitjournal.io>
2022-09-16 22:48:07 +0800

    [2022-09-16 14:48:05]
    FILE:
    tests/motion_vector_estimation/mpegflow/test.sh
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/motion_vector_estimation/mpegflow/test.sh
+++ tests/motion_vector_estimation/mpegflow/test.sh
@@ -6,3 +6,5 @@ VIDEO="/root/Desktop/works/pyjom/samples/video/cute_cat_gif.mp4"
 # you may download it from web, or just use docker
 
 # mkdir -p examples/vis_dump && ./mpegflow $VIDEO | ./vis $VIDEO examples/vis_dump
+
+# maybe this shit is not good at all...
