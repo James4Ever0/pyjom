81b886fb001220b927ded0853bc302f596dc4d63
GitJournal <app@gitjournal.io>
2022-09-16 22:46:55 +0800

    [2022-09-16 14:46:48]
    FILE:
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/motion_vector_estimation/mpegflow/test.sh
+++ tests/motion_vector_estimation/mpegflow/test.sh
@@ -3,5 +3,6 @@ VIDEO="/root/Desktop/works/pyjom/samples/video/cute_cat_gif.mp4"
 # ./mpegflow $VIDEO > output.txt
 
 # it does not help because the .so file is fake. you need a real one.
+# you may download it from web, or just use docker
 
 # mkdir -p examples/vis_dump && ./mpegflow $VIDEO | ./vis $VIDEO examples/vis_dump
