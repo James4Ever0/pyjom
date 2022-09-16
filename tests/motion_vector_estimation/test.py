feb432ce4c4430fc969709a1816ef355dfc8a704
GitJournal <app@gitjournal.io>
2022-09-16 20:08:56 +0800

    [2022-09-16 12:08:52]
    FILE:
    tests/motion_vector_estimation/test.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/motion_vector_estimation/test.py
+++ tests/motion_vector_estimation/test.py
@@ -305,6 +305,8 @@ for _ in progressbar.progressbar(range(framesCount)):
         max_cartesian = max(motion_vectors_filtered_cartesian_distance)
 
         motion_area_ratio_array.append(motion_area_ratio)
+        print(average_weighted_motion_vector)
+        breakpoint()
         average_weighted_motion_vector_cartesian=cartesianDistance(average_weighted_motion_vector)
         average_weighted_motion_vector_cartesian_array.append(average_weighted_motion_vector_cartesian)
         average_global_weighted_motion_vector_cartesian = cartesianDistance(average_global_weighted_motion_vector)
