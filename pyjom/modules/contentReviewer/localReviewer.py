a974de1cadbf5cd37f75cf0fb2b5700b7b3a94e5
GitJournal <app@gitjournal.io>
2022-09-16 23:20:31 +0800

    [2022-09-16 15:20:28]
    FILE:
    pyjom/modules/contentReviewer/localReviewer.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/contentReviewer/localReviewer.py
+++ pyjom/modules/contentReviewer/localReviewer.py
@@ -26,7 +26,7 @@ def filesystemReviewerCoreAnalyzer(elem,auto=False, semiauto=True, dummy_auto=Tr
 def filesystemReviewerNoGenerator(content):
     mreview = []
     for elem in content:
-        reviewResult = filesystem
+        reviewResult = filesystemReviewerCoreAnalyzer(content,auto=False, semiauto=True, dummy_auto=True, args={}, template_names=[]):
         mreview.append(reviewResult)
     return mreview
 
