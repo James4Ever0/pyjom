9e2bc72349fdeb1c77062b2565197cb8a75b695d
GitJournal <app@gitjournal.io>
2022-09-16 19:30:36 +0800

    [2022-09-16 11:30:33]
    FILE:
    tests/motion_vector_estimation/test.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/motion_vector_estimation/test.py
+++ tests/motion_vector_estimation/test.py
@@ -242,11 +242,11 @@ for _ in progressbar.progressbar(range(framesCount)):
 
         weighted_motion_vectors_filtered_cartesian_distance = motion_vectors_filtered_cartesian_distance * cartesianWeights
 
-        np.sum(weighted_motion_vectors_filtered_cartesian_distance) = np.sum(weighted_motion_vectors_filtered_cartesian_distance)
+        sum_weighted_motion_vectors_filtered_cartesian_distance = np.sum(weighted_motion_vectors_filtered_cartesian_distance)
 
-        average_weighted_motion_vectors_filtered_cartesian_distance = /cartesianWeights
+        average_weighted_motion_vectors_filtered_cartesian_distance = sum_weighted_motion_vectors_filtered_cartesian_distance/cartesianWeights
 
-        average_global_weighted_motion_vectors_filtered_cartesian_distance = 
+        average_global_weighted_motion_vectors_filtered_cartesian_distance = sum_weighted_motion_vectors_filtered_cartesian_distance/
 
 
         min_cartesian = min(motion_vectors_filtered_cartesian_distance)
