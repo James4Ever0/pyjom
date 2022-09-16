956d9d6ee76dc884ab6c96fde9fdd63f14b175aa
GitJournal <app@gitjournal.io>
2022-09-16 15:30:06 +0800

    [2022-09-16 07:30:03]
    FILE:
    tests/motion_vector_estimation/test.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/motion_vector_estimation/test.py
+++ tests/motion_vector_estimation/test.py
@@ -93,7 +93,7 @@ for _ in progressbar.progressbar(range(framesCount)):
         motion_vectors_dict_averaged = {key: averageMotionVectors(motion_vectors_dict[key]) for key in motion_vectors_dict.keys()}
         for key, average_motion_vector in motion_vectors_dict_averaged.items():
             print("destination coords", key)
-            print('averageMotionVectors', average_motion_vector)
+            print('average motion vector', average_motion_vector)
         # print(motion_vectors.shape)
     else:
         break
