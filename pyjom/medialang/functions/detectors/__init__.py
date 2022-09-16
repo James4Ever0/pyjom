9f368008da75259982bb5ae0d81d91bca632baf5
GitJournal <app@gitjournal.io>
2022-09-16 23:38:46 +0800

    [2022-09-16 15:38:43]
    FILE:
    pyjom/medialang/functions/detectors/__init__.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/medialang/functions/detectors/__init__.py
+++ pyjom/medialang/functions/detectors/__init__.py
@@ -11,8 +11,7 @@ def getMedialangInputFixed(medialangPathsInput):
     for fbase0 in medialangPathsInput:
         if type(fbase0) == str:
             yield fbase0
-        elif type(fbase0) == list and len(fbase0) == 1:
-            if type(fbase0[0] == dict):
+        elif type(fbase0) == list and len(fbase0) == 1 and type(fbase0[0] == dict) and :
                 
         else:
             print('weird medialang detector input')
