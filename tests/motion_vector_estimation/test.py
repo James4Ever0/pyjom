99763867610af0cbdfb32d23846b39c26deadc06
GitJournal <app@gitjournal.io>
2022-09-16 12:03:03 +0800

    [2022-09-16 04:02:59]
    FILE:
    tests/motion_vector_estimation/test.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/motion_vector_estimation/test.py
+++ tests/motion_vector_estimation/test.py
@@ -2,19 +2,31 @@
 
 source = "/root/Desktop/works/pyjom/samples/video/dog_with_text.mp4"
 from lazero.utils.importers import cv2_custom_build_init
+
 cv2_custom_build_init()
 from mvextractor.videocap import VideoCap
 
 
 cap = VideoCap()
-cap.open(source) # wtf is going on here?
+cap.open(source)  # wtf is going on here?
 # if there is nothing we will breakup
 while True:
-    success, frame, motion_vectors, frame_type,timestamp = cap.read()
+    success, frame, motion_vectors, frame_type, timestamp = cap.read()
     if success:
         # what is the content of this motion vector?
         # print(motion_vectors)
-        source_index, _,_,src_x,src_y, dst_x,dst_y,motion_x, motion_y,
+        (
+            source_index,
+            _,
+            _,
+            src_x,
+            src_y,
+            dst_x,
+            dst_y,
+            motion_x,
+            motion_y,
+            motion_scale,
+        ) = motion_vectors
         # print(motion_vectors.shape)
     else:
         break
