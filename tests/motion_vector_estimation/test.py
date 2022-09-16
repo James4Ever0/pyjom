ac5696724de96b8f430d4ceba3d02acb17d8ea79
GitJournal <app@gitjournal.io>
2022-09-16 19:52:22 +0800

    [2022-09-16 11:52:19]
    FILE:
    tests/motion_vector_estimation/test.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/motion_vector_estimation/test.py
+++ tests/motion_vector_estimation/test.py
@@ -367,7 +367,7 @@ for _ in progressbar.progressbar(range(framesCount)):
 # print('max_dst_y', max_dst_y)
 import matplotlib.pyplot as plt
 
-a, b = 2, 3
+a, b = 3,1
 figure, axis = plt.subplots(a, b)
 
 data = [
