db053fc169e0d940a52b6778fae8cc39aab70ce9
GitJournal <app@gitjournal.io>
2022-09-17 16:37:59 +0800

    [2022-09-17 08:37:55]
    FILE:
    tests/hmm_test/test.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/hmm_test/test.py
+++ tests/hmm_test/test.py
@@ -1,6 +1,6 @@
 import numpy as np
 from hmmlearn import hmm
-np.random.seed(42)
+# np.random.seed(42)
 model = hmm.GaussianHMM(n_components=3, covariance_type="full")
 # model.startprob_ = np.array([0.6, 0.3, 0.1])
 # model.transmat_ = np.array([[0.7, 0.2, 0.1],
@@ -9,6 +9,8 @@ model = hmm.GaussianHMM(n_components=3, covariance_type="full")
 # model.means_ = np.array([[0.0, 0.0], [3.0, -3.0], [5.0, 10.0]])
 # model.covars_ = np.tile(np.identity(2), (3, 1, 1))
 # not fitteed since we do not manually specify all the parameters.
+
+X = np.random(100,2)
 # X, Z = model.sample(100)
 # print(X) # the observations.
 # # (100, 2)
