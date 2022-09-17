434e328e0a8c2878f4a950edee8150c563cacceb
GitJournal <app@gitjournal.io>
2022-09-17 11:35:40 +0800

    [2022-09-17 03:32:59]
    FILE:
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/nsfw_violence_drug_detection/nsfwjs_test.js
+++ tests/nsfw_violence_drug_detection/nsfwjs_test.js
@@ -40,6 +40,6 @@ const load_model = async () => {
 }
 
 // Keep the model in memory, make sure it's loaded only once
-load_model().then(() => app.listen(8080))
+load_model().then(() => app.listen(8511))
 
 // curl --request POST localhost:8080/nsfw --header 'Content-Type: multipart/form-data' --data-binary 'image=@/full/path/to/picture.jpg'
