56416f5b60b022ab943fef9de1a5e374033087c3
GitJournal <app@gitjournal.io>
2022-09-17 14:27:21 +0800

    [2022-09-17 06:27:17]
    FILE:
    tests/nsfw_violence_drug_detection/porndetect_test.sh
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/nsfw_violence_drug_detection/porndetect_test.sh
+++ tests/nsfw_violence_drug_detection/porndetect_test.sh
@@ -10,15 +10,12 @@ echo
 curl -X POST -F 'image=@/root/Desktop/works/pyjom/samples/image/dick.bmp' http://localhost:8511/nsfw # something is wrong with bmp decoder.
 echo
 echo
-curl -X POST -F 'image=@/root/Desktop/works/pyjom/samples/image/dick.jpeg' http://localhost:8511/nsfw
+curl -X POST -F 'image=@/root/Desktop/works/pyjom/samples/image/dick2.jpeg' http://localhost:8511/nsfw
 echo
 echo
-curl -X POST -F 'image=@/root/Desktop/works/pyjom/samples/image/dick.jpeg' http://localhost:8511/nsfw
+curl -X POST -F 'image=@/root/Desktop/works/pyjom/samples/image/dick3.jpeg' http://localhost:8511/nsfw
 echo
 echo
-curl -X POST -F 'image=@/root/Desktop/works/pyjom/samples/image/dick.jpeg' http://localhost:8511/nsfw
-echo
-echo
-curl -X POST -F 'image=@/root/Desktop/works/pyjom/samples/image/dick.jpeg' http://localhost:8511/nsfw
+curl -X POST -F 'image=@/root/Desktop/works/pyjom/samples/image/dick4.jpeg' http://localhost:8511/nsfw
 echo
 echo
