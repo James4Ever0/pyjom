605796e3930b332d9a6f89a7fef20ba39e2be7cb
GitJournal <app@gitjournal.io>
2022-09-17 13:13:57 +0800

    [2022-09-17 05:13:49]
    FILE:
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/nsfw_violence_drug_detection/post_jpg_to_nsfwjs.sh
+++ tests/nsfw_violence_drug_detection/post_jpg_to_nsfwjs.sh
@@ -2,4 +2,7 @@ curl http://localhost:8511/
 
 curl -X POST -F 'image=@/root/Desktop/works/pyjom/samples/image/dog_saturday_night.jpg' http://localhost:8511/nsfw
 
-# curl -X POST -F 'image=@/root/Desktop/works/pyjom/samples/image/dog_saturday_night.bmp' http://localhost:8511/nsfw
+curl -X POST -F 'image=@/root/Desktop/works/pyjom/samples/image/dog_saturday_night.bmp' http://localhost:8511/nsfw
+
+curl -X POST -F 'image=@/root/Desktop/works/pyjom/samples/image/dog_saturday_night.bmp' http://localhost:8511/nsfw
+
