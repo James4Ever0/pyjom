478f50e6175041e88a4220c55de93815f8e0d30a
GitJournal <app@gitjournal.io>
2022-09-17 14:15:27 +0800

    [2022-09-17 06:15:24]
    FILE:
    tests/nsfw_violence_drug_detection/nsfwjs_test.js
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/nsfw_violence_drug_detection/nsfwjs_test.js
+++ tests/nsfw_violence_drug_detection/nsfwjs_test.js
@@ -1,5 +1,5 @@
-// import { createRequire } from "module";
-// const require = createRequire(import.meta.url);
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
 
 const express = require('express')
 const multer = require('multer')
