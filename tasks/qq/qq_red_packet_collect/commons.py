723b11e2cd6ece980b5d6bfe98e837635f0a9b4f
GitJournal <app@gitjournal.io>
2022-09-17 20:09:34 +0800

    [2022-09-17 12:09:30]
    FILE:
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tasks/qq/qq_red_packet_collect/commons.py
+++ tasks/qq/qq_red_packet_collect/commons.py
@@ -36,7 +36,7 @@ def replaceDuplicateChars(sentence: str, maxRepeat: int = 3):
         sentence = replaceDuplicateChar(sentence, char, maxRepeat=maxRepeat)
     return sentence
 
-
+# this is not 
 def replaceDuplicateWords(sentence: str, removeWordLengthThreshold:int = 2):
     # TODO: remove duplicate words inside, using jieba.
     import jieba
