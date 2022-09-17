e1825e5cc4b58f652bea0eed2d2bfe289b2576d2
GitJournal <app@gitjournal.io>
2022-09-17 14:27:08 +0800

    [2022-09-17 06:27:05]
    FILE:
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/nsfw_violence_drug_detection/porndetect_test.sh
+++ tests/nsfw_violence_drug_detection/porndetect_test.sh
@@ -10,3 +10,15 @@ echo
 curl -X POST -F 'image=@/root/Desktop/works/pyjom/samples/image/dick.bmp' http://localhost:8511/nsfw # something is wrong with bmp decoder.
 echo
 echo
+curl -X POST -F 'image=@/root/Desktop/works/pyjom/samples/image/dick.jpeg' http://localhost:8511/nsfw
+echo
+echo
+curl -X POST -F 'image=@/root/Desktop/works/pyjom/samples/image/dick.jpeg' http://localhost:8511/nsfw
+echo
+echo
+curl -X POST -F 'image=@/root/Desktop/works/pyjom/samples/image/dick.jpeg' http://localhost:8511/nsfw
+echo
+echo
+curl -X POST -F 'image=@/root/Desktop/works/pyjom/samples/image/dick.jpeg' http://localhost:8511/nsfw
+echo
+echo
