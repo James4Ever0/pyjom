4fe46341892cc7e1bd43521d3cbc0f96ca0d6a03
GitJournal <app@gitjournal.io>
2022-09-17 17:30:04 +0800

    [2022-09-17 09:30:00]
    FILE:
    tests/hmm_test/test.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/hmm_test/test.py
+++ tests/hmm_test/test.py
@@ -4,6 +4,7 @@ from hmmlearn import hmm
 
 # hmmlearn is simply unsupervised learning.
 # for supervised sequence learning use seqlearn instead
+#  pomegranate
 
 model = hmm.GaussianHMM(n_components=3, covariance_type="full")
 # model.startprob_ = np.array([0.6, 0.3, 0.1])
