94f4936b0056410cf00a1e83304d502f1282ac7d
GitJournal <app@gitjournal.io>
2022-09-17 16:29:38 +0800

    [2022-09-17 08:29:35]
    FILE:
    tests/hmm_test/test.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/hmm_test/test.py
+++ tests/hmm_test/test.py
@@ -9,3 +9,6 @@ model.transmat_ = np.array([[0.7, 0.2, 0.1],
 model.means_ = np.array([[0.0, 0.0], [3.0, -3.0], [5.0, 10.0]])
 model.covars_ = np.tile(np.identity(2), (3, 1, 1))
 X, Z = model.sample(100)
+print(X)
+print(Z)
+breakpoint()
