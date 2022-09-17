f7bb87fc46bd362990e3a63c55c011bfb73f6de3
GitJournal <app@gitjournal.io>
2022-09-17 13:21:55 +0800

    [2022-09-17 05:21:52]
    FILE:
    tests/nsfw_violence_drug_detection/nsfwjs_test.js
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/nsfw_violence_drug_detection/nsfwjs_test.js
+++ tests/nsfw_violence_drug_detection/nsfwjs_test.js
@@ -2,7 +2,7 @@ const express = require('express')
 const multer = require('multer')
 const jpeg = require('jpeg-js')
 const bmp = require('bmp-js')
-// const png = require('png-js')
+    // const png = require('png-js')
 
 const tf = require('@tensorflow/tfjs-node')
 const nsfw = require('nsfwjs')
@@ -16,36 +16,34 @@ let _model
 // it will normalize and resize the image if needed.
 // shall we check for gif?
 
-const convert = async(img,type) => {
+const convert = async(img, type) => {
     // Decoded image in UInt8 Byte array
-    if( type == 'image/jpeg')
-       { const image = await jpeg.decode(img, true)} //wtf?
+    if (type == 'image/jpeg') { const image = await jpeg.decode(img, true) } //wtf?
     // order: rgba
-    else if (type == 'image/png'){
+    else if (type == 'image/png') {
         const image = await png.decode(img, true)
-    }
-    else if (type == 'image/bmp'){
+    } else if (type == 'image/bmp') {
         const image = await bmp.decode(img, true)
     }
 
     const numChannels = 3
     const numPixels = image.width * image.height
     const values = new Int32Array(numPixels * numChannels)
-    // are you sure about the width?
+        // are you sure about the width?
 
     for (let i = 0; i < numPixels; i++)
         for (let c = 0; c < numChannels; ++c)
-        if (type == 'bmp'){
-            // ABGR
-            values[i * numChannels + c] = image.data[i * 4 + 3-c]
-        }else{
-            values[i * numChannels + c] = image.data[i * 4 + c]
-        }
+            if (type == 'bmp') {
+                // ABGR
+                values[i * numChannels + c] = image.data[i * 4 + 3 - c]
+            } else {
+                values[i * numChannels + c] = image.data[i * 4 + c]
+            }
 
     return tf.tensor3d(values, [image.height, image.width, numChannels], 'int32')
 }
 
-app.get('/', async(req, res) =>{
+app.get('/', async(req, res) => {
     res.send('nsfw nodejs server')
 })
 
@@ -53,23 +51,24 @@ app.post('/nsfw', upload.single('image'), async(req, res) => {
     if (!req.file) res.status(400).send('Missing image multipart/form-data')
     else {
         try {
-            console.log('file uploaded:',req.file)
-            if (req.file.fieldname == 'image'){
+            console.log('file uploaded:', req.file)
+            if (req.file.fieldname == 'image') {
                 type = req.file.mimetype // deal with it later.
                 extension = req.file.originalname.split(".").slice(-1)[0].toLowerCase()
-                if (extension == 'gif' || type == 'image/png'){
-const image = req.file.buffer
-const predictions = await _model.classifyGif(image)
-image.dispose()
-res.json(predictions)
-                }else {if (extension == 'bmp'){
-                    type='image/bmp'
+                if (extension == 'gif' || type == 'image/png') {
+                    const image = req.file.buffer
+                    const predictions = await _model.classifyGif(image, { topk: 3, fps: 1 })
+                    image.dispose()
+                    res.json(predictions)
+                } else {
+                    if (extension == 'bmp') {
+                        type = 'image/bmp'
+                    }
+                    const image = await convert(req.file.buffer, type) // here we have buffer.
+                    const predictions = await _model.classify(image)
+                    image.dispose()
+                    res.json(predictions)
                 }
-                const image = await convert(req.file.buffer, type) // here we have buffer.
-                const predictions = await _model.classify(image)
-                image.dispose()
-                res.json(predictions)
-            }
             }
             // we need some file format hints.
 
