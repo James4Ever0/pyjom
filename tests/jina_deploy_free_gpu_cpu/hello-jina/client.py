c4c61a3cccb3d070989972aa037dcacfe1f832ca
GitJournal <app@gitjournal.io>
2022-09-17 22:09:47 +0800

    [2022-09-17 14:09:43]
    FILE:
    tests/jina_deploy_free_gpu_cpu/hello-jina/client.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/jina_deploy_free_gpu_cpu/hello-jina/client.py
+++ tests/jina_deploy_free_gpu_cpu/hello-jina/client.py
@@ -3,9 +3,9 @@ from jina import Client, DocumentArray
 if __name__ == '__main__':
     c = Client(host='grpc://0.0.0.0:54321')
     while True:
-    docArray = DocumentArray.empty(1)
-    docArray[0].text = input("jina>")
-    da = c.post('/', docArray)
-    response = da[0].text
-    # print(da.texts)
-    print(response)
+        docArray = DocumentArray.empty(1)
+        docArray[0].text = input("jina>")
+        da = c.post('/', docArray)
+        response = da[0].text
+        # print(da.texts)
+        print(response)
