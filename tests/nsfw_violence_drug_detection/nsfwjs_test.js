c30cbeb3989df61164bc09cbf5eeb8fbc7a1fbdc
GitJournal <app@gitjournal.io>
2022-09-17 14:23:33 +0800

    [2022-09-17 06:23:30]
    FILE:
    tests/nsfw_violence_drug_detection/nsfwjs_test.js
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/nsfw_violence_drug_detection/nsfwjs_test.js
+++ tests/nsfw_violence_drug_detection/nsfwjs_test.js
@@ -1,5 +1,13 @@
 // import { createRequire } from "module";
 // const require = createRequire(import.meta.url);
+var HeaderTypes;
+(function (HeaderTypes) {
+    HeaderTypes[HeaderTypes["BITMAP_INFO_HEADER"] = 40] = "BITMAP_INFO_HEADER";
+    HeaderTypes[HeaderTypes["BITMAP_V2_INFO_HEADER"] = 52] = "BITMAP_V2_INFO_HEADER";
+    HeaderTypes[HeaderTypes["BITMAP_V3_INFO_HEADER"] = 56] = "BITMAP_V3_INFO_HEADER";
+    HeaderTypes[HeaderTypes["BITMAP_V4_HEADER"] = 108] = "BITMAP_V4_HEADER";
+    HeaderTypes[HeaderTypes["BITMAP_V5_HEADER"] = 124] = "BITMAP_V5_HEADER";
+})(HeaderTypes || (HeaderTypes = {}));
 
 class BmpDecoder {
     constructor(buffer, { toRGBA } = { toRGBA: false }) {
