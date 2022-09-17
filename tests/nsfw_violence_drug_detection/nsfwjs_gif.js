f5e809135de6d5ca5ae2538b84878e38637ab02f
GitJournal <app@gitjournal.io>
2022-09-17 12:53:33 +0800

    [2022-09-17 04:53:30]
    FILE:
    tests/nsfw_violence_drug_detection/nsfwjs_gif.js
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/nsfw_violence_drug_detection/nsfwjs_gif.js
+++ tests/nsfw_violence_drug_detection/nsfwjs_gif.js
@@ -3,7 +3,7 @@ const tf = require('@tensorflow/tfjs-node')
 const nsfw = require('nsfwjs')
 
 
-filepath = "/root/Desktop/works/pyjom/samples/video/kitty_flash_15fps_with_mestimate.mp4"
+filepath = "/root/Desktop/works/pyjom/samples/video/kitty_flash_15fps_with_mestimate."
 
 // no other classes?
 // filepath = "/root/Desktop/works/pyjom/samples/video/cat_invalid_eye_rolling.gif"
