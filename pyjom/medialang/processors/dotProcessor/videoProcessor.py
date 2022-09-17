a7a4f7eb641d949329e109c5d01841ec332b43dc
GitJournal <app@gitjournal.io>
2022-09-17 10:33:32 +0800

    [2022-09-17 02:33:28]
    FILE:
    pyjom/medialang/processors/dotProcessor/videoProcessor.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/medialang/processors/dotProcessor/videoProcessor.py
+++ pyjom/medialang/processors/dotProcessor/videoProcessor.py
@@ -144,6 +144,7 @@ def ffmpegVideoPreProductionFilter(
         ) # this need to be improvised. if it is long, we need to do another check.
         stationaryLogoDicts.update(
             detectStationaryLogoOverTime(filepath, video_start, video_end, cornersOnly=False, top_k=3 # are you sure? wtf?
+            # i mean 
         )
         # reprocess these things. really?
         mDict.update(stationaryLogoDicts)  # output logo mask. or not.
