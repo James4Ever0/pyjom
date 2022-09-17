c6b55fe56daaec96fa0ac01426e665c7471bd4e5
GitJournal <app@gitjournal.io>
2022-09-17 12:40:17 +0800

    [2022-09-17 04:40:14]
    FILE:
    tests/nsfw_violence_drug_detection/nsfwjs_gif.js
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/nsfw_violence_drug_detection/nsfwjs_gif.js
+++ tests/nsfw_violence_drug_detection/nsfwjs_gif.js
@@ -2,3 +2,35 @@ const nsfw = require('nsfwjs')
 
 
 filepath = ""
+
+const fs = require('fs');
+
+// Store file data chunks in this array
+let chunks = [];
+// We can use this variable to store the final data
+let fileBuffer;
+
+// Read file into stream.Readable
+let fileStream = fs.createReadStream('text.txt');
+
+// An error occurred with the stream
+fileStream.once('error', (err) => {
+    // Be sure to handle this properly!
+    console.error(err); 
+});
+
+// File is done being read
+fileStream.once('end', () => {
+    // create the final data Buffer from data chunks;
+    fileBuffer = Buffer.concat(chunks);
+    
+    // Of course, you can do anything else you need to here, like emit an event!
+});
+
+// Data is flushed from fileStream in chunks,
+// this callback will be executed for each chunk
+fileStream.on('data', (chunk) => {
+    chunks.push(chunk); // push data chunk to array
+
+    // We can perform actions on the partial data we have so far!
+});
