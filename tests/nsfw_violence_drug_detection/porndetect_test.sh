317be204c4e6183f92b45238515b60beda480fbe
GitJournal <app@gitjournal.io>
2022-09-17 14:01:47 +0800

    [2022-09-17 06:01:44]
    FILE:
    tests/nsfw_violence_drug_detection/post_jpg_to_nsfwjs.sh
    tests/nsfw_violence_drug_detection/porndetect_test.sh
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

---
+++ tests/nsfw_violence_drug_detection/porndetect_test.sh
@@ -0,0 +1,12 @@
+curl -X POST -F 'image=@/root/Desktop/works/pyjom/samples/image/porn_shemale.jpeg' http://localhost:8511/nsfw
+echo
+echo
+curl -X POST -F 'image=@/root/Desktop/works/pyjom/samples/image/porn_shemale.bmp' http://localhost:8511/nsfw
+echo
+echo
+curl -X POST -F 'image=@/root/Desktop/works/pyjom/samples/image/porn_shemale.jpeg' http://localhost:8511/nsfw
+echo
+echo
+curl -X POST -F 'image=@/root/Desktop/works/pyjom/samples/image/porn_shemale.bmp' http://localhost:8511/nsfw
+echo
+echo
