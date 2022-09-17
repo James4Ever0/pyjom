2476b7153e5dece58268302a5d71910012334551
GitJournal <app@gitjournal.io>
2022-09-17 17:47:44 +0800

    [2022-09-17 09:47:37]
    FILE:
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/hmm_test/seqlearn_test.py
+++ tests/hmm_test/seqlearn_test.py
@@ -2,7 +2,7 @@ from seqlearn.perceptron import StructuredPerceptron # it's like mini neural net
 
 # the lengths_train marked each individual sequence's length as an array.
 import numpy as np
-X_train = np.random.random(5,4) # one-hot encoded? not? features=4
+X_train = np.random.random((5,4)) # one-hot encoded? not? features=4
 y_train = np.random.randint(0,5,(5,)) # the freaking label.
 
 lengths_train = [1,1,2,1] # may i apologize. sum=5
