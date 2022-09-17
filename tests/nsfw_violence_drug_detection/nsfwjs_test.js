f9d955fdb9598f625511a520c73e694ecd191de5
GitJournal <app@gitjournal.io>
2022-09-17 13:07:29 +0800

    [2022-09-17 05:07:25]
    FILE:
    tests/nsfw_violence_drug_detection/nsfwjs_test.js
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/nsfw_violence_drug_detection/nsfwjs_test.js
+++ tests/nsfw_violence_drug_detection/nsfwjs_test.js
@@ -54,7 +54,8 @@ app.post('/nsfw', upload.single('image'), async(req, res) => {
     else {
         try {
             console.log('file uploaded:',req.file)
-            const image = await convert(req.file.buffer) // here we have buffer.
+            type = 'jpg'
+            const image = await convert(req.file.buffer, type) // here we have buffer.
             // we need some file format hints.
             const predictions = await _model.classify(image)
             image.dispose()
