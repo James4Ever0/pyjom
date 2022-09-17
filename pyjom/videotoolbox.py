c4dc0536a60565eec7acc14c33f3dde970ed114a
GitJournal <app@gitjournal.io>
2022-09-17 10:36:30 +0800

    [2022-09-17 02:36:26]
    FILE:
    pyjom/videotoolbox.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/videotoolbox.py
+++ pyjom/videotoolbox.py
@@ -365,7 +365,7 @@ def detectStationaryLogoOverTime(
         return fourCorners
 
     fourCorners = None
-    cornerArea = getFourCorners()
+    cornerArea = getFourCorners(0,0,defaultWidth,defaultHeight)
     if cornersOnly:
         defaultFourCorners = getFourCorners(0, 0, defaultWidth, defaultHeight)
         if pipCropDicts in [None, {}]:
