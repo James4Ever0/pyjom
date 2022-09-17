5c4572f127775b029ba6f796f9d91b3b9998a99b
GitJournal <app@gitjournal.io>
2022-09-17 22:07:45 +0800

    [2022-09-17 14:07:41]
    FILE:
    tests/jina_deploy_free_gpu_cpu/hello-jina/executor1/executor.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/jina_deploy_free_gpu_cpu/hello-jina/executor1/executor.py
+++ tests/jina_deploy_free_gpu_cpu/hello-jina/executor1/executor.py
@@ -1,20 +1,23 @@
 from jina import Executor, requests, DocumentArray
+
 # remember our good old program? our shell?
 # proper name is: reverse shell
 # hackish? no?
 
 import subprocess
 
+
 class MyExecutor(Executor):
     @requests
     def foo(self, docs: DocumentArray, **kwargs):
         try:
             command = docs[0].text
-            commandList = command.split(' ')
+            commandList = command.split(" ")
             response = subprocess.check_output(commandList)
             docs[0].text = response
         # docs[1].text = 'goodbye, world!'
         except:
             import traceback
+
             error = traceback.format_exc()
-            docs[0].text =[ 'error!',error]
+            docs[0].text = "\n".join(["error!", error])
