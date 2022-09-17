89f9d34349985df041672aa88d6bd5dde80c2a5b
GitJournal <app@gitjournal.io>
2022-09-17 18:18:03 +0800

    [2022-09-17 10:17:59]
    FILE:
    tests/hmm_test_speech_recognization_time_series/pomegranate_test.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

---
+++ tests/hmm_test_speech_recognization_time_series/pomegranate_test.py
@@ -0,0 +1,5 @@
+# this library goes way advanced than hmmlearn/seqlearn
+# it provides convenient methods for training and prediction.
+# also lots of different models
+
+# https://pomegranate.readthedocs.io/en/latest/HiddenMarkovModel.html
