20d6a0573cae84dea8ede378214d27e260d1bfe4
GitJournal <app@gitjournal.io>
2022-09-17 17:37:58 +0800

    [2022-09-17 09:37:55]
    FILE:
    tests/hmm_test/seqlearn_test.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/hmm_test/seqlearn_test.py
+++ tests/hmm_test/seqlearn_test.py
@@ -1,9 +1,6 @@
 from seqlearn.perceptron import StructuredPerceptron
 
-from seqlearn.datasets import load_conll
-def features(sequence, i):
-...     yield "word=" + sequence[i].lower()
-...     if sequence[i].isupper():
-...         yield "Uppercase"
-clf = StructuredPerceptron()
+
+
+
 clf.fit(X_train, y_train, lengths_train)
