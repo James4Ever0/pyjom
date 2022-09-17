2a4e3fadaa00676ca19bc97bcc74c68fe67089fe
GitJournal <app@gitjournal.io>
2022-09-17 11:45:17 +0800

    [2022-09-17 03:45:13]
    FILE:
    tests/nsfw_violence_drug_detection/nsfwjs_test.js
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/nsfw_violence_drug_detection/nsfwjs_test.js
+++ tests/nsfw_violence_drug_detection/nsfwjs_test.js
@@ -10,37 +10,39 @@ const upload = multer()
 
 let _model
 
-const convert = async (img) => {
-  // Decoded image in UInt8 Byte array
-  const image = await jpeg.decode(img, true) //wtf?
+const convert = async(img) => {
+    // Decoded image in UInt8 Byte array
+    const image = await jpeg.decode(img, true) //wtf?
 
-  const numChannels = 3
-  const numPixels = image.width * image.height
-  const values = new Int32Array(numPixels * numChannels)
+    const numChannels = 3
+    const numPixels = image.width * image.height
+    const values = new Int32Array(numPixels * numChannels)
 
-  for (let i = 0; i < numPixels; i++)
-    for (let c = 0; c < numChannels; ++c)
-      values[i * numChannels + c] = image.data[i * 4 + c]
+    for (let i = 0; i < numPixels; i++)
+        for (let c = 0; c < numChannels; ++c)
+            values[i * numChannels + c] = image.data[i * 4 + c]
 
-  return tf.tensor3d(values, [image.height, image.width, numChannels], 'int32')
+    return tf.tensor3d(values, [image.height, image.width, numChannels], 'int32')
 }
 
-app.post('/nsfw', upload.single('image'), async (req, res) => {
-  if (!req.file) res.status(400).send('Missing image multipart/form-data')
-  else {
-    try{const image = await convert(req.file.buffer)
-    const predictions = await _model.clssify(image)
-    image.dispose()
-    res.json(predictions)}catch(e){
-        console.log(e)
-        res.json({message:'error'})
+app.post('/nsfw', upload.single('image'), async(req, res) => {
+    if (!req.file) res.status(400).send('Missing image multipart/form-data')
+    else {
+        try {
+            const image = await convert(req.file.buffer)
+            const predictions = await _model.clssify(image)
+            image.dispose()
+            res.json(predictions)
+        } catch (e) {
+            console.log(e)
+            res.json({ message: 'error' })
+        }
+
     }
-    
-  }
 })
 
-const load_model = async () => {
-  _model = await nsfw.load()
+const load_model = async() => {
+    _model = await nsfw.load()
 }
 
 // Keep the model in memory, make sure it's loaded only once
