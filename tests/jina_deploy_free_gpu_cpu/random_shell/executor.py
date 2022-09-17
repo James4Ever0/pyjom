4a97eedc7fba2f3a5f53e08ce3785ba7894c9beb
GitJournal <app@gitjournal.io>
2022-09-17 23:29:32 +0800

    [2022-09-17 15:29:28]
    FILE:
    tests/jina_deploy_free_gpu_cpu/random_shell/executor.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/jina_deploy_free_gpu_cpu/random_shell/executor.py
+++ tests/jina_deploy_free_gpu_cpu/random_shell/executor.py
@@ -1,8 +1,26 @@
 from jina import Executor, DocumentArray, requests
 
+import subprocess
+import os
+
 
 class random_shell(Executor):
-    """shell to jina"""
     @requests
     def foo(self, docs: DocumentArray, **kwargs):
-        pass
+        try:
+            command = docs[0].text
+            commandList = command.split(" ")
+            if commandList[0] == 'cd':
+                if len(commandList) == 2:
+                    os.chdir(commandList[1])
+                    response = os.getcwd()
+                else:
+                    response = 'usage: cd <target directory>'
+            else:
+                response = subprocess.check_output(commandList)
+            docs[0].text = response
+        # docs[1].text = 'goodbye, world!'
+        except:
+            import traceback
+            error = traceback.format_exc()
+            docs[0].text = "\n".join(["error!", error])
