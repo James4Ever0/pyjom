d0ae21915b356c163f9c1f760cc15353d528e281
GitJournal <app@gitjournal.io>
2022-09-17 13:46:27 +0800

    [2022-09-17 05:46:24]
    FILE:
    tests/nsfw_violence_drug_detection/post_jpg_to_nsfwjs.sh
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/nsfw_violence_drug_detection/post_jpg_to_nsfwjs.sh
+++ tests/nsfw_violence_drug_detection/post_jpg_to_nsfwjs.sh
@@ -1,15 +1,22 @@
 curl http://localhost:8511/
 echo
+echo
 curl -X POST -F 'image=@/root/Desktop/works/pyjom/samples/image/dog_saturday_night.jpg' http://localhost:8511/nsfw
+echo
 echo
 curl -X POST -F 'image=@/root/Desktop/works/pyjom/samples/image/dog_saturday_night.bmp' http://localhost:8511/nsfw
 echo
+echo
 curl -X POST -F 'image=@/root/Desktop/works/pyjom/samples/video/cute_cat_gif.gif' http://localhost:8511/nsfw
+echo
 echo
 curl -X POST -F 'image=@/root/Desktop/works/pyjom/samples/image/dog_with_text.png' http://localhost:8511/nsfw
 echo
+echo
 curl -X POST -F 'image=@/root/Desktop/works/pyjom/samples/image/dog_with_text.jpg' http://localhost:8511/nsfw
 echo
+echo
 curl -X POST -F 'image=@/root/Desktop/works/pyjom/samples/image/dog_with_text.bmp' http://localhost:8511/nsfw
+echo
 echo
 # but the bmp looks right. is that the format issue?
