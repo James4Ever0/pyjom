59a5280b4aedc4a88e4ba969c612bce38245e5e8
GitJournal <app@gitjournal.io>
2022-09-18 16:16:23 +0800

    [2022-09-18 08:16:20]
    FILE:
    tests/unittest_nsfw_video_score.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_nsfw_video_score.py
+++ tests/unittest_nsfw_video_score.py
@@ -131,7 +131,7 @@ test_flag = "nsfw_video"
 source = "/root/Desktop/works/pyjom/samples/video/cute_cat_gif.gif"
 import numpy as np
 
-def processNSFWServerReply
+def processNSFWServerImageReply()
 
 def processNSFWReportArray(
     NSFWReportArray,
