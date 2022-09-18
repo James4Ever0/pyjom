46a989ad3bd48de060ca4a9c5a380212eb4fab91
GitJournal <app@gitjournal.io>
2022-09-18 13:12:17 +0800

    [2022-09-18 05:12:15]
    FILE:
    tests/unittest_nsfw_video_score.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_nsfw_video_score.py
+++ tests/unittest_nsfw_video_score.py
@@ -144,6 +144,7 @@ elif test_flag == "nsfw":
                 print("RESPONSE:", r.json())
             # [{'className': 'Neutral', 'probability': 0.9995943903923035}, {'className': 'Drawing', 'probability': 0.00019544694805517793}, {'className': 'Porn', 'probability': 0.00013213469355832785}, {'className': 'Sexy', 'probability': 6.839347042841837e-05}, {'className': 'Hentai', 'probability': 9.632151886762585e-06}]
 elif test_flag == "nsfw_image":
+    source = '/root/Desktop/works/pyjom/samples/image/kitty_flash.bmp'
     # source = '/root/Desktop/works/pyjom/samples/image/pig_really.bmp'
     # RESPONSE: [{'className': 'Neutral', 'probability': 0.9634107351303101}, {'className': 'Porn', 'probability': 0.0244674663990736}, {'className': 'Drawing', 'probability': 0.006115634460002184}, {'className': 'Hentai', 'probability': 0.003590137232095003}, {'className': 'Sexy', 'probability': 0.002416097791865468}]
     # source = "/root/Desktop/works/pyjom/samples/image/dog_with_text.bmp"
