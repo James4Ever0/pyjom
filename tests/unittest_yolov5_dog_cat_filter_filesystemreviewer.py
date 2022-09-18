156e384f868a06a73f96ab6bf9155d5a0224fd2b
GitJournal <app@gitjournal.io>
2022-09-18 17:04:03 +0800

    [2022-09-18 09:04:01]
    FILE:
    tests/unittest_yolov5_dog_cat_filter_filesystemreviewer.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_yolov5_dog_cat_filter_filesystemreviewer.py
+++ tests/unittest_yolov5_dog_cat_filter_filesystemreviewer.py
@@ -49,7 +49,7 @@ def extractYolov5DetectionData(detectionData, mimetype='video'):
             print('location:', location)
             print('confidence:', confidence)
             print('identity:', identity)
-    data_dict.update({'filepath':filepath,'type'})
+    data_dict.update({'path':filepath,'type':mimetype, })
 
 for result in resultGenerator: # this is for each file.
     from lazero.utils.logger import sprint
