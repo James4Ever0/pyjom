b7c1455d92e9e9f3196d102d2a03046dd4817100
GitJournal <app@gitjournal.io>
2022-09-18 19:51:54 +0800

    [2022-09-18 11:51:47]
    FILE:
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_nsfw_video_score.py
+++ tests/unittest_nsfw_video_score.py
@@ -4,6 +4,7 @@ from test_commons import *
 import requests
 from lazero.network.checker import waitForServerUp
 from pyjom.videotoolbox import getVideoFrameIteratorWithFPS
+from typing import Literal
 
 gateway = "http://localhost:8511/"
 
@@ -17,104 +18,7 @@ import cv2
 # suggest you not to use this shit.
 import math
 
-
-def scanImageWithWindowSizeAutoResize(
-    image,
-    width,
-    height,
-    return_direction=False,
-    threshold=0.1,  # minimum 'fresh' area left for scanning
-):  # shall you use torch? no?
-    shape = image.shape
-    assert len(shape) == 3
-    ih, iw, channels = shape
-    targetWidth = max(width, math.floor(iw * height / ih))
-    targetHeight = max(height, math.floor(ih * width / iw))
-    resized = cv2.resize(
-        image, (targetWidth, targetHeight), interpolation=cv2.INTER_CUBIC
-    )
-    # determine scan direction here.
-    imageSeries = []
-    if targetWidth / targetHeight == width / height:
-        imageSeries = [resized]  # as image series.
-        direction = None
-    elif targetWidth / targetHeight < width / height:
-        direction = "vertical"
-        # the scanning is along the vertical axis, which is the height.
-        index = 0
-        while True:
-            start, end = height * index, height * (index + 1)
-            if start < targetHeight:
-                if end > targetHeight:
-                    if 1 - (end - targetHeight) / targetHeight >= threshold:
-                        end = targetHeight
-                        start = targetHeight - height
-                    else:
-                        break
-                # other conditions, just fine
-            else:
-                break  # must exit since nothing to scan.
-            cropped = resized[start:end, :, :]  # height, width, channels
-            imageSeries.append(cropped)
-            index += 1
-    else:
-        direction = "horizontal"
-        index = 0
-        while True:
-            start, end = width * index, width * (index + 1)
-            if start < targetWidth:
-                if end > targetWidth:
-                    if 1 - (end - targetWidth) / targetWidth >= threshold:
-                        end = targetWidth
-                        start = targetWidth - width
-                    else:
-                        break
-                # other conditions, just fine
-            else:
-                break  # must exit since nothing to scan.
-            cropped = resized[:, start:end, :]  # height, width, channels
-            imageSeries.append(cropped)
-            index += 1
-    if return_direction:
-        return imageSeries, direction
-    else:
-        return imageSeries
-
-
-from typing import Literal
-
-
-def resizeImageWithPadding(
-    image,
-    width,
-    height,
-    border_type: Literal["constant_black", "replicate"] = "constant_black",
-):
-    shape = image.shape
-    assert len(shape) == 3
-    ih, iw, channels = shape
-    targetWidth = min(width, math.floor(iw * height / ih))
-    targetHeight = min(height, math.floor(ih * width / iw))
-    resized = cv2.resize(
-        image, (targetWidth, targetHeight), interpolation=cv2.INTER_CUBIC
-    )
-    BLACK = [0] * channels
-    top = max(0, math.floor((height - targetHeight) / 2))
-    bottom = max(0, height - targetHeight - top)
-    left = max(0, math.floor((width - targetWidth) / 2))
-    right = max(0, width - targetWidth - left)
-    if border_type == "constant_black":
-        padded = cv2.copyMakeBorder(
-            resized, top, bottom, left, right, cv2.BORDER_CONSTANT, value=BLACK
-        )
-    elif border_type == "replicate":
-        padded = cv2.copyMakeBorder(
-            resized, top, bottom, left, right, cv2.BORDER_REPLICATE, value=BLACK
-        )
-    else:
-        raise Exception("unknown border_type: %s" % border_type)
-    return padded
-
+from pyjom.imagetoolbox import 
 
 from lazero.filesystem import tmpdir, tmpfile
 
