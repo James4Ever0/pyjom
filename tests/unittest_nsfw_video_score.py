2271b647d404af5908bb51c2e8f7780ef75ff800
GitJournal <app@gitjournal.io>
2022-09-18 12:03:24 +0800

    [2022-09-18 04:03:21]
    FILE:
    tests/unittest_nsfw_video_score.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_nsfw_video_score.py
+++ tests/unittest_nsfw_video_score.py
@@ -34,7 +34,7 @@ def scanImageWithWindowSize(image, width, height, return_direction=False):
         direction = "vertical"
         # the scanning is along the vertical axis, which is the height.
         while True:
-            cropped = 
+            cropped = resized[,,:]
             imageSeries.append(cropped)
     else:
         direction = "horizontal"
