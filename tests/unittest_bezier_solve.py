d032b20c0736a1f376902283923e922afa57a39c
GitJournal <app@gitjournal.io>
2022-09-18 23:35:32 +0800

    [2022-09-18 15:35:29]
    FILE:
    tests/unittest_bezier_solve.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_bezier_solve.py
+++ tests/unittest_bezier_solve.py
@@ -1,17 +1,19 @@
 import bezier
 import numpy as np
 
-skew = 0.2 # skew: (-0.5, 0.5)
-x_start, y_start = 0,0
-x_end, y_end = 1,1
+skew = 0.2  # skew: (-0.5, 0.5)
+x_start, y_start = 0, 0
+x_end, y_end = 1, 1
 
-x_diff = x_end-x_start
-y_diff = y_end-y_start
+x_diff = x_end - x_start
+y_diff = y_end - y_start
 
-nodes1 = np.asfortranarray([
-    [x_start, x_diff*(0.5+skew), x_end],
-    [y_start, y_diff*(0.5-skew), y_end],
-])
+nodes1 = np.asfortranarray(
+    [
+        [x_start, x_diff * (0.5 + skew), x_end],
+        [y_start, y_diff * (0.5 - skew), y_end],
+    ]
+)
 curve1 = bezier.Curve(nodes1, degree=2)
 
 # import seaborn
@@ -19,6 +21,7 @@ curve1 = bezier.Curve(nodes1, degree=2)
 
 axis = curve1.plot(num_pts=256)
 import matplotlib.pyplot as plt
+
 # plt.plot(axis)
 plt.show()
 # print(axis)
