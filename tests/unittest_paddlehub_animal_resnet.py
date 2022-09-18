e4546e71855cf643241fe2b1bb6c5a16c5ed7cd3
GitJournal <app@gitjournal.io>
2022-09-18 20:20:31 +0800

    [2022-09-18 12:20:28]
    FILE:
    tests/unittest_paddlehub_animal_resnet.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_paddlehub_animal_resnet.py
+++ tests/unittest_paddlehub_animal_resnet.py
@@ -18,5 +18,7 @@ for frame in getVideoFrameIteratorWithFPS(source, -1, -1, fps=1):
     )
     result = classifier.classification(images=[padded_resized_frame],top_k=3,use_gpu=False) # check it?
     # RESULT: [{'美国银色短毛猫': 0.23492032289505005, '虎斑猫': 0.14728288352489471, '美国银虎斑猫': 0.13097935914993286}]
+    # so what is the major categories?
+    # thanks to 
     sprint("RESULT:", result)
     breakpoint()
