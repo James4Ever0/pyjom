af4a16f26f10fd87ba53e1ebf2291ea0c8bd6dd7
GitJournal <app@gitjournal.io>
2022-09-18 12:56:07 +0800

    [2022-09-18 04:56:04]
    FILE:
    tests/unittest_nsfw_video_score.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_nsfw_video_score.py
+++ tests/unittest_nsfw_video_score.py
@@ -126,6 +126,8 @@ elif test_flag == "scanning":
             cv2.imshow("SCANNED %d" % index, image)
             cv2.waitKey(0)
 elif test_flag == "nsfw":
+    source = "/root/Desktop/works/pyjom/samples/video/cute_cat_gif.gif"
+    # use another source.
     with tmpdir(path=tmpdirPath) as T:
         for frame in getVideoFrameIteratorWithFPS(source, -1, -1, fps=1):
             padded_resized_frame = resizeImageWithPadding(frame, 224, 224)
