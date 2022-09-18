65b15265d443eadcee6ca91f25a829827fe1260b
GitJournal <app@gitjournal.io>
2022-09-18 11:42:43 +0800

    [2022-09-18 03:42:40]
    FILE:
    tests/unittest_nsfw_video_score.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_nsfw_video_score.py
+++ tests/unittest_nsfw_video_score.py
@@ -67,7 +67,7 @@ tmpdirPath = "/dev/shm/medialang/nsfw"
 with tmpdir(path=tmpdirPath) as T:
     for frame in getVideoFrameIteratorWithFPS(source, -1,-1, fps=1):
         padded_resized_frame = resizeImageWithPadding(frame, 224,224)
-        jpg_path = 
+        jpg_path = os.path.join(tmpdirPath, frame)
 r = requests.post(
     gateway,
 )  # post gif?
