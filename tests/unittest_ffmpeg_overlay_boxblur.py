ae7a89d89ecbc32536facb4e4e47355ba6ca8006
GitJournal <app@gitjournal.io>
2022-09-18 09:26:44 +0800

    [2022-09-18 01:26:40]
    FILE:
    tests/unittest_ffmpeg_overlay_boxblur.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_ffmpeg_overlay_boxblur.py
+++ tests/unittest_ffmpeg_overlay_boxblur.py
@@ -20,4 +20,4 @@ layer_1 = video_stream.filter('scale',w=1080, h='ih*1080/iw')
 output_stream = layer_0.overlay(layer_1,x=0, y='floor((H-h)/2)')
 
 
-ffmpeg.output(output_stream, 'output.mp4')
+ffmpeg.output(output_stream, '/dev/shm/output.mp4')
