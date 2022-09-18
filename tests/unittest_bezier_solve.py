1bc374bdf2aa76d8071f13cf212bbdee94bb82ef
GitJournal <app@gitjournal.io>
2022-09-18 23:45:46 +0800

    [2022-09-18 15:45:43]
    FILE:
    tests/unittest_bezier_solve.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_bezier_solve.py
+++ tests/unittest_bezier_solve.py
@@ -18,11 +18,11 @@ curve1 = bezier.Curve(nodes1, degree=2)
 
 # import seaborn
 # seaborn.set()
-test_case = 'solve':
-test_case == "plot"
-axis = curve1.plot(num_pts=256)
-import matplotlib.pyplot as plt
+test_case = 'evaluate':
+if test_case == "plot"
+    axis = curve1.plot(num_pts=256)
+    import matplotlib.pyplot as plt
 
-# plt.plot(axis)
-plt.show()
+    # plt.plot(axis)
+    plt.show()
 # print(axis)
