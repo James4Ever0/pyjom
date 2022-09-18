52984f12eba715e4d2d2b0a60a119ad7dff2f1ea
GitJournal <app@gitjournal.io>
2022-09-18 10:45:06 +0800

    [2022-09-18 02:45:02]
    FILE:
    pyjom/videotoolbox.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/videotoolbox.py
+++ pyjom/videotoolbox.py
@@ -145,7 +145,8 @@ def getVideoFrameSampler(videoPath, start, end, sample_size=60, iterate=False):
     else:
         return nonIterator(cap, samplePopulation)
 
-def getVideoFrameIteratorWithFPS(videoPath, start, end, fps=1)
+def getVideoFrameIteratorWithFPS(videoPath, start, end, fps=1):
+    # 
 
 def getVideoFrameIterator(videoPath, start, end, sample_rate=1, batch=1):
     assert batch >= 1
