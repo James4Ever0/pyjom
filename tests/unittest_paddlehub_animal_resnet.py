9d2fb0d22c5943b12ba41c79fde713e50a231cb6
GitJournal <app@gitjournal.io>
2022-09-18 20:23:20 +0800

    [2022-09-18 12:23:16]
    FILE:
    tests/unittest_paddlehub_animal_resnet.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_paddlehub_animal_resnet.py
+++ tests/unittest_paddlehub_animal_resnet.py
@@ -12,16 +12,18 @@ from lazero.utils.logger import sprint
 classifier = hub.Module(name="resnet50_vd_animals")
 # 'ResNet50vdAnimals' object has no attribute 'gpu_predictor'
 # no gpu? really?
-test_
-for frame in getVideoFrameIteratorWithFPS(source, -1, -1, fps=1):
-    padded_resized_frame = resizeImageWithPadding(
-        frame, 224, 224, border_type="replicate"
-    )
-    result = classifier.classification(images=[padded_resized_frame],top_k=3,use_gpu=False) # check it?
-    # RESULT: [{'美国银色短毛猫': 0.23492032289505005, '虎斑猫': 0.14728288352489471, '美国银虎斑猫': 0.13097935914993286}]
-    # so what is the major categories?
-    # thanks to chinese, we are never confused.
-    # check the labels, shall we?
-    # what about samoyed?
-    sprint("RESULT:", result)
-    breakpoint()
+test_flag ="image"
+
+if test_flag == "video":
+    for frame in getVideoFrameIteratorWithFPS(source, -1, -1, fps=1):
+        padded_resized_frame = resizeImageWithPadding(
+            frame, 224, 224, border_type="replicate"
+        )
+        result = classifier.classification(images=[padded_resized_frame],top_k=3,use_gpu=False) # check it?
+        # RESULT: [{'美国银色短毛猫': 0.23492032289505005, '虎斑猫': 0.14728288352489471, '美国银虎斑猫': 0.13097935914993286}]
+        # so what is the major categories?
+        # thanks to chinese, we are never confused.
+        # check the labels, shall we?
+        # what about samoyed?
+        sprint("RESULT:", result)
+        breakpoint()
