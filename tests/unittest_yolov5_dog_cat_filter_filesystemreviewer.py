804dfabc801cf9fa92d0842c59b8d5e0bdec4e64
GitJournal <app@gitjournal.io>
2022-09-18 18:01:21 +0800

    [2022-09-18 10:01:18]
    FILE:
    tests/unittest_yolov5_dog_cat_filter_filesystemreviewer.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_yolov5_dog_cat_filter_filesystemreviewer.py
+++ tests/unittest_yolov5_dog_cat_filter_filesystemreviewer.py
@@ -87,7 +87,8 @@ def calculateVideoMeanDetectionConfidence(dataList, identities=['dog','cat'], fr
         elif framewise_strategy == 'max':
             frame_detection_dict = {key: max(frame_detection_dict.get(key,[0])) for key in identities}
         # now update the report dict.
-        if timespan_strategy in 'mean':
+        if timespan_strategy in ['mean', 'max']:
+            report[]
 
 
 for result in resultGenerator: # this is for each file.
