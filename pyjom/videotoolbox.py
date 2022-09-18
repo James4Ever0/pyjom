75905512845313ac002578e4aec669614ea3d075
GitJournal <app@gitjournal.io>
2022-09-18 10:45:52 +0800

    [2022-09-18 02:45:49]
    FILE:
    pyjom/videotoolbox.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/videotoolbox.py
+++ pyjom/videotoolbox.py
@@ -158,7 +158,8 @@ def getVideoFrameIterator(videoPath, start, end, sample_rate=1, batch=1):
     startFrame = int(start * fps)
     stopFrame = int(end * fps)
 
-    startFrame = 
+    startFrame = max(0, total_frames-1)
+    endFrame = min()
     # success, img = cap.read() # ignore first frame.
     # https://vuamitom.github.io/2019/12/13/fast-iterate-through-video-frames.html
     # to speed up the process we need to decompose the cap.read() method
