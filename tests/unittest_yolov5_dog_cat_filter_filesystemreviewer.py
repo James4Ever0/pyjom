d61e38bdc24efda0e286bafb9a50d27e77b491e5
GitJournal <app@gitjournal.io>
2022-09-18 18:49:18 +0800

    [2022-09-18 10:49:15]
    FILE:
    tests/unittest_yolov5_dog_cat_filter_filesystemreviewer.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_yolov5_dog_cat_filter_filesystemreviewer.py
+++ tests/unittest_yolov5_dog_cat_filter_filesystemreviewer.py
@@ -161,7 +161,10 @@ from pyjom.commons import checkMinMaxDict
 
 def detectionConfidenceFilter(
     detectionConfidence: dict,
-    filter_dict={"dog": {"min": 0.5}, "cat": {"min": 0.5}}, # both have certainty of 0.69 or something. consider to change this value higher?
+    filter_dict={
+        "dog": {"min": 0.5},
+        "cat": {"min": 0.5},
+    },  # both have certainty of 0.69 or something. consider to change this value higher?
     logic: Literal["AND", "OR"] = "OR",
 ):  # what is the logic here? and? or?
     assert logic in ["AND", "OR"]
