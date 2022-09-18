abcbaa783ee53707b79eff4e0716ef3e871f7d1e
GitJournal <app@gitjournal.io>
2022-09-18 10:06:49 +0800

    [2022-09-18 02:06:46]
    FILE:
    pyjom/medialang/processors/dotProcessor/videoProcessor.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/medialang/processors/dotProcessor/videoProcessor.py
+++ pyjom/medialang/processors/dotProcessor/videoProcessor.py
@@ -302,7 +302,10 @@ def ffmpegVideoPreProductionFilter(
                                 stream = cropFilter(stream, commandParams)
                                 # TODO: the main shit happens here is that if pip region is detected, it (the crop region) will not maintain the width to height ratio. you might need padding, and that's what we about to do here. you may also extract that clip as standalone material.
                                 # more inspection is needed for comprehensive reasoning.
-        stream = paddingFilter(stream)
+        if paddingBlur:
+            stream = paddingBlurFilter(stream)
+        else:
+            stream = paddingFilter(stream)
         if preview:  # final filter? need us to crop this?
             stream = previewFilter(stream)
             # do nothing here! (no fx.)
