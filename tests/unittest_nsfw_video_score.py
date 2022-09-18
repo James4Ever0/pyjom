19611f18455023f028f8249a3e8faae822ad697e
GitJournal <app@gitjournal.io>
2022-09-18 12:41:18 +0800

    [2022-09-18 04:41:15]
    FILE:
    tests/unittest_nsfw_video_score.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_nsfw_video_score.py
+++ tests/unittest_nsfw_video_score.py
@@ -115,7 +115,7 @@ if test_flag == "padding":
 elif test_flag == "scanning":
     for frame in getVideoFrameIteratorWithFPS(source, -1, -1, fps=1):
         scanned_array = scanImageWithWindowSizeAutoResize(frame, 224, 224)
-        for image in scanned_array:
+        for index, image in enumerate(scanned_array):
             cv2.imshow("SCANNED", image)
 elif test_flag == "nsfw":
     with tmpdir(path=tmpdirPath) as T:
