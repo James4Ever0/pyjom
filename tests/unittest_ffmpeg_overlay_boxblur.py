0f84238ea7f92be8dae614d3a73486269ebb181a
GitJournal <app@gitjournal.io>
2022-09-18 09:31:18 +0800

    [2022-09-18 01:31:16]
    FILE:
    tests/unittest_ffmpeg_overlay_boxblur.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_ffmpeg_overlay_boxblur.py
+++ tests/unittest_ffmpeg_overlay_boxblur.py
@@ -28,6 +28,6 @@ with tmpdir(path=path) as T:
     filepath = os.path.join(path, "output.mp4")
     args = ffmpeg.get_args(output_stream)
     print(args)
-    # ffmpeg.output(output_stream,filepath)
+    ffmpeg.output(output_stream,filepath)
     print("output file location:", filepath)
     breakpoint()
