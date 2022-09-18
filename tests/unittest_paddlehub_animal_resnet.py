2b0778c2ca697550a627f0bd644b71d397797b2f
GitJournal <app@gitjournal.io>
2022-09-18 20:16:11 +0800

    [2022-09-18 12:16:08]
    FILE:
    tests/unittest_paddlehub_animal_resnet.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_paddlehub_animal_resnet.py
+++ tests/unittest_paddlehub_animal_resnet.py
@@ -9,9 +9,8 @@ import cv2
 
 classifier = hub.Module(name="resnet50_vd_animals")
 
-
 for frame in getVideoFrameIteratorWithFPS(source, -1, -1, fps=1):
     padded_resized_frame = resizeImageWithPadding(
         frame, 224, 224, border_type="replicate"
     )
-    result = classifier.classification(images=[cv2.imread('/PATH/TO/IMAGE')])
+    result = classifier.classification(images=[padded_resized_frame])
