27f0f63d4983297e8bb04841be789c18b20ea285
GitJournal <app@gitjournal.io>
2022-09-18 23:46:44 +0800

    [2022-09-18 15:46:41]
    FILE:
    tests/unittest_bezier_solve.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_bezier_solve.py
+++ tests/unittest_bezier_solve.py
@@ -26,6 +26,7 @@ if test_case == "plot"
     # plt.plot(axis)
     plt.show()
 elif test_case == "evaluate":
-    data = input("x> ")
+    x = input("x> ")
+    try:
     x = float(x)
 # print(axis)
