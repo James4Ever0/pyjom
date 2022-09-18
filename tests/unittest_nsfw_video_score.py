776c7b2dc11f2a42138a3ce0ae308f678ddea461
GitJournal <app@gitjournal.io>
2022-09-18 15:52:36 +0800

    [2022-09-18 07:52:33]
    FILE:
    tests/unittest_nsfw_video_score.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_nsfw_video_score.py
+++ tests/unittest_nsfw_video_score.py
@@ -115,8 +115,10 @@ import os
 test_flag = "nsfw_image"
 # test_flag = "scanning"
 # test_flag = "paddinging"
+
 source = "/root/Desktop/works/pyjom/samples/video/cute_cat_gif.gif"
 import numpy as np
+
 def processNSFWReportArray(NSFWReportArray, average_classes = ['Neutral'],
             get_max_classes = ['Drawing','Porn','Sexy','Hentai']):
     assert set(average_classes).intersection(set(get_max_classes)) == set()
