f34af959de42ce1eceea6bc8c9084fc6a57f3c36
GitJournal <app@gitjournal.io>
2022-09-18 11:25:09 +0800

    [2022-09-18 03:25:06]
    FILE:
    tests/unittest_nsfw_video_score.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_nsfw_video_score.py
+++ tests/unittest_nsfw_video_score.py
@@ -18,7 +18,7 @@ def resizeWithPadding(image, width, height):
     targetWidth = min(width, min(math.floor(iw*height/ih)))
     targetHeight =  min(height, min(math.floor(ih*width/iw)))
     resized = cv2.resize(image, (targetWidth, targetHeight), interpolation=cv2.INTER_CUBIC)
-    cv2.copy
+    cv2.copyMakeBorder(resized,)
 
 from lazero.filesystem import tmpdir
 
