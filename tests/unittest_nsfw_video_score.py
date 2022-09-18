9d8f454a9a3e86affd91f14a011038f463f752b1
GitJournal <app@gitjournal.io>
2022-09-18 11:27:49 +0800

    [2022-09-18 03:27:47]
    FILE:
    tests/unittest_nsfw_video_score.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_nsfw_video_score.py
+++ tests/unittest_nsfw_video_score.py
@@ -19,7 +19,7 @@ def resizeWithPadding(image, width, height):
     targetHeight =  min(height, min(math.floor(ih*width/iw)))
     resized = cv2.resize(image, (targetWidth, targetHeight), interpolation=cv2.INTER_CUBIC)
     BLACK = 
-    top = max(0,math.floor((width-targetWidth)/2))
+    top = max(0,math.floor((height-targetHeight)/2))
     bottom = max(0,targetHeight-top)
     left = max(0,math.floor((width-targetWidth)/2))
     right = max(0,targetWidth-top)
