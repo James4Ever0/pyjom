6ccfd40461993421b098c8121700f0e7f44e7779
GitJournal <app@gitjournal.io>
2022-09-18 11:55:31 +0800

    [2022-09-18 03:55:27]
    FILE:
    tests/unittest_nsfw_video_score.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_nsfw_video_score.py
+++ tests/unittest_nsfw_video_score.py
@@ -70,10 +70,10 @@ with tmpdir(path=tmpdirPath) as T:
     for frame in getVideoFrameIteratorWithFPS(source, -1,-1, fps=1):
         padded_resized_frame = resizeImageWithPadding(frame, 224,224)
         jpg_path = os.path.join(tmpdirPath, "{}.jpg".format(uuid.uuid4()))
-        with 
-        cv2.imwrite(jpg_path, padded_resized_frame)
-        r = requests.post(
-            gateway,
-        )  # post gif? or just jpg?
+        with tmpfile(path = jpg_path) as TF:
+            cv2.imwrite(jpg_path, padded_resized_frame)
+            r = requests.post(
+                gateway,
+            )  # post gif? or just jpg?
 # you can only post gif now, or you want to post some other formats?
 # if you post shit, you know it will strentch your picture and produce unwanted shits.
