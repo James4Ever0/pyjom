b4b93d70bf56df8c98a58057ed0b0f430c62a87d
GitJournal <app@gitjournal.io>
2022-09-18 23:21:56 +0800

    [2022-09-18 15:21:54]
    FILE:
    tests/unittest_bezier_solve.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_bezier_solve.py
+++ tests/unittest_bezier_solve.py
@@ -7,9 +7,10 @@ nodes1 = np.asfortranarray([
 ])
 curve1 = bezier.Curve(nodes1, degree=2)
 
-import seaborn
-seaborn.set()
+# import seaborn
+# seaborn.set()
 
 axis = curve1.plot(num_pts=256)
 import matplotlib.pyplot as plt
+plt.plot(axis)
 plt.show()
