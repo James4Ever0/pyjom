24a91225757eae1450a1a505369f15eeafa43fd6
GitJournal <app@gitjournal.io>
2022-09-18 18:10:19 +0800

    [2022-09-18 10:10:16]
    FILE:
    tests/unittest_yolov5_dog_cat_filter_filesystemreviewer.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_yolov5_dog_cat_filter_filesystemreviewer.py
+++ tests/unittest_yolov5_dog_cat_filter_filesystemreviewer.py
@@ -140,7 +140,7 @@ def calculateVideoMeanDetectionConfidence(
 
 from pyjom.commons import checkMinMaxDict
 
-def detectionIdentityFilter(, identites)
+def detectionIdentityFilter(detection, filter_dict = {}):
 
 for result in resultGenerator:  # this is for each file.
     from lazero.utils.logger import sprint
