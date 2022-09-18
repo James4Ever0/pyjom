1cec69260a5cdce7d707b5ad49d0a25afc70c220
GitJournal <app@gitjournal.io>
2022-09-18 12:00:40 +0800

    [2022-09-18 04:00:38]
    FILE:
    tests/unittest_nsfw_video_score.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_nsfw_video_score.py
+++ tests/unittest_nsfw_video_score.py
@@ -63,7 +63,7 @@ def resizeImageWithPadding(image, width, height, border_type='constant_black'):
             resized, top, bottom, left, right, cv2.BORDER_REPLICATE, value=BLACK
         )
     else:
-        raise Exception('border_type must be)
+        raise Exception('unknown border_type: %s' % border_type)must be in)
     return padded
 
 
