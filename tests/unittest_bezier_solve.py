a0c97085d63121aacd9fc71b532ee13c82263f47
GitJournal <app@gitjournal.io>
2022-09-18 23:31:37 +0800

    [2022-09-18 15:31:34]
    FILE:
    tests/unittest_bezier_solve.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_bezier_solve.py
+++ tests/unittest_bezier_solve.py
@@ -3,8 +3,8 @@ import numpy as np
 
 skew = 0.2
 nodes1 = np.asfortranarray([
-    [0.0, 0.5+skew, 1.0],
-    [0.0, 0.5-skew, 1.0],
+    [x_start, 0.5+skew, 1.0],
+    [y_start, 0.5-skew, 1.0],
 ])
 curve1 = bezier.Curve(nodes1, degree=2)
 
