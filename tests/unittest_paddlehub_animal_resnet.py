6a47b914bc1d76d5cd7dc2188ffbdfde34406391
GitJournal <app@gitjournal.io>
2022-09-18 20:11:42 +0800

    [2022-09-18 12:11:39]
    FILE:
    tests/unittest_paddlehub_animal_resnet.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_paddlehub_animal_resnet.py
+++ tests/unittest_paddlehub_animal_resnet.py
@@ -1,8 +1,8 @@
 source = ""
 
-
+from test_commons import *
 from pyjom.videotoolbox import getVideoFrameIteratorWithFPS
-
+from pyjom.imagetoolbox import resizeImageWithPadding
 
 for frame in getVideoFrameIteratorWithFPS(source, -1, -1, fps=1):
     padded_resized_frame = resizeImageWithPadding(
