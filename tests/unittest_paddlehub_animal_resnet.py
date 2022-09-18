768d7dc2b87adfe6b4190745eaa10c56021111f0
GitJournal <app@gitjournal.io>
2022-09-18 20:15:47 +0800

    [2022-09-18 12:15:44]
    FILE:
    tests/unittest_paddlehub_animal_resnet.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_paddlehub_animal_resnet.py
+++ tests/unittest_paddlehub_animal_resnet.py
@@ -4,6 +4,13 @@ from test_commons import *
 from pyjom.videotoolbox import getVideoFrameIteratorWithFPS
 from pyjom.imagetoolbox import resizeImageWithPadding
 
+import paddlehub as hub
+import cv2
+
+classifier = hub.Module(name="resnet50_vd_animals")
+
+result = classifier.classification(images=[cv2.imread('/PATH/TO/IMAGE')])
+
 for frame in getVideoFrameIteratorWithFPS(source, -1, -1, fps=1):
     padded_resized_frame = resizeImageWithPadding(
         frame, 224, 224, border_type="replicate"
