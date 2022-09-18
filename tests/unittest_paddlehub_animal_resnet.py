326f8cf36d499349afd72b227ca8028604b66a9c
GitJournal <app@gitjournal.io>
2022-09-18 20:25:33 +0800

    [2022-09-18 12:25:30]
    FILE:
    tests/unittest_paddlehub_animal_resnet.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_paddlehub_animal_resnet.py
+++ tests/unittest_paddlehub_animal_resnet.py
@@ -1,4 +1,4 @@
-source = "/root/Desktop/works/pyjom/samples/video/kitty_flash_15fps.gif" # check that kitty video!
+source = "/root/Desktop/works/pyjom/samples/video/kitty_flash_15fps.gif"  # check that kitty video!
 
 from test_commons import *
 from pyjom.videotoolbox import getVideoFrameIteratorWithFPS
@@ -12,14 +12,16 @@ from lazero.utils.logger import sprint
 classifier = hub.Module(name="resnet50_vd_animals")
 # 'ResNet50vdAnimals' object has no attribute 'gpu_predictor'
 # no gpu? really?
-test_flag ="image"
+test_flag = "image"
 
 if test_flag == "video":
     for frame in getVideoFrameIteratorWithFPS(source, -1, -1, fps=1):
         padded_resized_frame = resizeImageWithPadding(
             frame, 224, 224, border_type="replicate"
         )
-        result = classifier.classification(images=[padded_resized_frame],top_k=3,use_gpu=False) # check it?
+        result = classifier.classification(
+            images=[padded_resized_frame], top_k=3, use_gpu=False
+        )  # check it?
         # RESULT: [{'美国银色短毛猫': 0.23492032289505005, '虎斑猫': 0.14728288352489471, '美国银虎斑猫': 0.13097935914993286}]
         # so what is the major categories?
         # thanks to chinese, we are never confused.
@@ -27,12 +29,16 @@ if test_flag == "video":
         # what about samoyed?
         sprint("RESULT:", result)
         breakpoint()
-elif test_flag == 'image':
+elif test_flag == "image":
     source = "/root/Desktop/works/pyjom/samples/image/samoyed.jpeg"
     frame = cv2.imread(source)
-            padded_resized_frame = resizeImageWithPadding(
-            frame, 224, 224, border_type="replicate"
-        )
-        result = classifier.classification(images=[padded_resized_frame],top_k=3,use_gpu=False) 
+    padded_resized_frame = resizeImageWithPadding(
+        frame, 224, 224, border_type="replicate"
+    )
+    result = classifier.classification(
+        images=[padded_resized_frame], top_k=3, use_gpu=False
+    )
+    sprint("RESULT:", result)
+    breakpoint()
 else:
     raise Exception("unknown test flag: %s" % test_flag)
