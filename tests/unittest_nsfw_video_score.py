9c70621ab246f4280c12b6bd617e9ef91972912f
GitJournal <app@gitjournal.io>
2022-09-18 15:43:55 +0800

    [2022-09-18 07:43:53]
    FILE:
    tests/unittest_nsfw_video_score.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_nsfw_video_score.py
+++ tests/unittest_nsfw_video_score.py
@@ -147,6 +147,7 @@ elif test_flag == "nsfw":
                 responses.append(response_json) # there must be at least one response, i suppose?
             average_classes = ['Neutral']
             get_max_classes = ['Drawing','Porn','Sexy','Hentai'] # we don't want drawing dogs.
+            
             # [{'className': 'Neutral', 'probability': 0.9995943903923035}, {'className': 'Drawing', 'probability': 0.00019544694805517793}, {'className': 'Porn', 'probability': 0.00013213469355832785}, {'className': 'Sexy', 'probability': 6.839347042841837e-05}, {'className': 'Hentai', 'probability': 9.632151886762585e-06}]
 elif test_flag == "nsfw_image":
     source = '/root/Desktop/works/pyjom/samples/image/kitty_flash.bmp'
