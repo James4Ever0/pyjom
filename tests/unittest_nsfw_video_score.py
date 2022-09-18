bdeb436405c906a43bc5db61f64da014aad866e4
GitJournal <app@gitjournal.io>
2022-09-18 12:03:00 +0800

    [2022-09-18 04:02:56]
    FILE:
    tests/unittest_nsfw_video_score.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_nsfw_video_score.py
+++ tests/unittest_nsfw_video_score.py
@@ -33,6 +33,8 @@ def scanImageWithWindowSize(image, width, height, return_direction=False):
     elif targetWidth/targetHeight < width/height:
         direction = "vertical"
         # the scanning is along the vertical axis, which is the height.
+        while True:
+            
     else:
         direction = "horizontal"
     if return_direction:
