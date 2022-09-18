8aa4c30886d0a60f7a425dd7cc01a81aa28900f4
GitJournal <app@gitjournal.io>
2022-09-18 10:27:53 +0800

    [2022-09-18 02:27:50]
    FILE:
    tests/unittest_nsfw_video_score.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_nsfw_video_score.py
+++ tests/unittest_nsfw_video_score.py
@@ -8,3 +8,4 @@ from lazero.filesystem import tmpdir
 
 r = requests.post(gateway,) # post gif?
 # you can only post gif now, or you want to post some other formats?
+# if you post shit, you know it will strentch your picture and produce unwanted shits.
