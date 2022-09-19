235809c01d0868853cc2ccff8d1af148249f27ff
GitJournal <app@gitjournal.io>
2022-09-19 15:34:17 +0800

    [2022-09-19 07:34:14]
    FILE:
    tests/unittest_online_topic_generator_giphy.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_online_topic_generator_giphy.py
+++ tests/unittest_online_topic_generator_giphy.py
@@ -5,7 +5,8 @@ from lazero.utils import sprint
 from lazero.network import download
 from lazero.filesystem import tmpdir
 
-clash_refresher_url = "http://127.0.0.1:8677"
+clash_refresher_port = 8677
+clash_refresher_url = "http://127.0.0.1:{}".format(clash_refresher_port)
 
 "clash update controller"
 
