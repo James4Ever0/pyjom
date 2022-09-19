705fb61357a525de789bb6cde78f127a23628118
GitJournal <app@gitjournal.io>
2022-09-19 20:59:42 +0800

    [2022-09-19 12:59:40]
    FILE:
    pyjom/modules/contentProducing/onlineProducer.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/contentProducing/onlineProducer.py
+++ pyjom/modules/contentProducing/onlineProducer.py
@@ -4,8 +4,8 @@ from pyjom.modules.contentProducing.producerTemplates import getProducerTemplate
 @decorator
 def OnlineProducer(processed_info_generator, source = 'giphy',template=None, template_configs=None):
     # template_configs is a generator, it generate configs.
-    print("PROCESSED_INFO_GENERATOR: ", processed_info_generator)
-    breakpoint()
+    # print("PROCESSED_INFO_GENERATOR: ", processed_info_generator)
+    # breakpoint()
     if source == 'giphy':
         template_function = getProducerTemplate[template]
         # print("TEMPLATE FUNCTION ACQUIRED %s" % template_function)
