f286aa8a74558947206c0457f6f5b6d111ac7497
GitJournal <app@gitjournal.io>
2022-09-19 18:53:19 +0800

    [2022-09-19 10:53:17]
    FILE:
    pyjom/modules/contentProducing/onlineProducer.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/contentProducing/onlineProducer.py
+++ pyjom/modules/contentProducing/onlineProducer.py
@@ -2,9 +2,10 @@ from pyjom.commons import *
 from pyjom.modules.contentProducing.producerTemplates import getProducerTemplate
 
 @decorator
-def OnlineProducer(processed_info, template=None, template_config={}):
+def OnlineProducer(processed_info, source = 'giphy',template=None, template_config={}):
+    if source == 'giphy':
     template_function = getProducerTemplate[template]
-    exported_media_locations = template_function(processed_info,config=template_config)
+    exported_media_locations = template_function(processed_info,config=template_config) # a generator!
     # i guess the title/tags/cover are actually belongs to the poster, not producer.
     for exported_media_location in exported_media_locations:
         yield exported_media_location
