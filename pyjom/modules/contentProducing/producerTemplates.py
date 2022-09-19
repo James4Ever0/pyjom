5c977e768e6f3f558c20f4dcd99dd460dcbe21a1
GitJournal <app@gitjournal.io>
2022-09-19 20:29:00 +0800

    [2022-09-19 12:28:56]
    FILE:
    pyjom/modules/contentProducing/producerTemplates.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/contentProducing/producerTemplates.py
+++ pyjom/modules/contentProducing/producerTemplates.py
@@ -500,7 +500,7 @@ def petsWithMusicOnlineProducer(dataGenerator, configs, tempdir='/dev/shm/medial
         # [{'span':(start,end),'cut':{'span':(start,end)},'source':videoSource},...]
         # if lyric_path:
         ass_file_path = ".".join([getRandomFileName, 'lrc'])
-        ass_file_path = os.path.join(tempdir)
+        ass_file_path = os.path.join(tempdir, ass_file_path)
         lrcToAnimatedAss(music, lyric_path, ass_file_path)
         data_ids = []
         for data in dataGenerator:
