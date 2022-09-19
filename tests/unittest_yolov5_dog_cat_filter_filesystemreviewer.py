4d5b599f049afbf203cfe7a5fa5dd406e21120eb
GitJournal <app@gitjournal.io>
2022-09-19 13:02:03 +0800

    [2022-09-19 05:02:01]
    FILE:
    tests/unittest_yolov5_dog_cat_filter_filesystemreviewer.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_yolov5_dog_cat_filter_filesystemreviewer.py
+++ tests/unittest_yolov5_dog_cat_filter_filesystemreviewer.py
@@ -192,7 +192,7 @@ def detectionConfidenceFilter(
 
 # {'input_bias': 0.0830047243746045, 'skew': -0.4986098769473948}
 
-def bezierResnet50
+def bezierPaddleHubResnet50VideoDogCatDetector():
 
 for result in resultGenerator:  # this is for each file.
     # sprint(result)
