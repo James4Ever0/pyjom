d562e0ffd90bae491235e6dec8b49f32030ac983
GitJournal <app@gitjournal.io>
2022-09-19 18:48:18 +0800

    [2022-09-19 10:48:15]
    FILE:
    pyjom/modules/contentProducing/onlineProducer.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/contentProducing/onlineProducer.py
+++ pyjom/modules/contentProducing/onlineProducer.py
@@ -4,4 +4,4 @@ from pyjom.modules.contentProducing.producerTemplates import getProducerTemplate
 @decorator
 def OnlineProducer(processed_info, template=None, template_config={}):
     template_function = getProducerTemplate[template]
-    template_function(processed_info,config=template_config)
+    exported_media_location = template_function(processed_info,config=template_config)
