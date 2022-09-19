e2b7e5b5d60622f3663a9d3c26deb06f527f7968
GitJournal <app@gitjournal.io>
2022-09-19 19:04:49 +0800

    [2022-09-19 11:04:45]
    FILE:
    pyjom/modules/contentProducing/producerTemplates.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/contentProducing/producerTemplates.py
+++ pyjom/modules/contentProducing/producerTemplates.py
@@ -492,7 +492,7 @@ def petsWithMusicOnlineProducer(dataGenerator, configs):
     # we only have one song here. you fucking know that?
         music, font, policy, policy_names, music_metadata, music_duration, maxtime, mintime, lyric_path, demanded_cut_spans, standard_bpm_spans = getMusicInfoParsed(config)
         # check for 'demanded_cut_spans' now!
-        demanded_cut_spans = {}
+        render_list = {}
         for data in dataGenerator:
 
         yield rendered_media_location # another generator?
