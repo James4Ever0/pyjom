bfc5217d04f0ff6d72bbe24fd96cd8f2f88904b1
GitJournal <app@gitjournal.io>
2022-09-19 13:50:57 +0800

    [2022-09-19 05:50:54]
    FILE:
    tests/unittest_yolov5_dog_cat_filter_filesystemreviewer.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_yolov5_dog_cat_filter_filesystemreviewer.py
+++ tests/unittest_yolov5_dog_cat_filter_filesystemreviewer.py
@@ -353,11 +353,13 @@ videoPaths = [
 for videoPath in videoPaths:  # this is for each file.
     # sprint(result)
     filter_result = yolov5VideoDogCatDetector(videoPath)
-    sprint("FILTER PASSED?", filter_result)
+    # sprint("FILTER PASSED?", filter_result)
     if not filter_result:
         sprint("CHECKING WITH BEZIER CURVE AND RESNET50")
         filter_result = bezierPaddleHubResnet50VideoDogCatDetector(videoPath)
     if not filter_result:
         print("FILTER FAILED")
+    else:
+        print("FILTER PASSED")
     # if not passed, hit it with the bezier curve and resnet50
     # breakpoint()
