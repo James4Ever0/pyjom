44fdd662b3a2b52909f6bf3e178c4aef8a97ce97
GitJournal <app@gitjournal.io>
2022-09-19 11:19:30 +0800

    [2022-09-19 03:19:27]
    FILE:
    tests/unittest_paddlehub_animal_resnet.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_paddlehub_animal_resnet.py
+++ tests/unittest_paddlehub_animal_resnet.py
@@ -55,6 +55,8 @@ classifier = hub.Module(name="resnet50_vd_animals")
 # no gpu? really?
 test_flag = "image"
 
+def 
+
 if test_flag == "video":
     for frame in getVideoFrameIteratorWithFPS(source, -1, -1, fps=1):
         padded_resized_frame = resizeImageWithPadding(
@@ -63,9 +65,6 @@ if test_flag == "video":
         result = classifier.classification(
             images=[padded_resized_frame], top_k=3, use_gpu=False
         )  # check it?
-        resultDict = result[0]
-        resultList = [(key,value) for key,value in resultDict.items()]
-        resultList.sort(key=lambda item: -item[1])
         # RESULT: [{'美国银色短毛猫': 0.23492032289505005, '虎斑猫': 0.14728288352489471, '美国银虎斑猫': 0.13097935914993286}]
         # so what is the major categories?
         # thanks to chinese, we are never confused.
