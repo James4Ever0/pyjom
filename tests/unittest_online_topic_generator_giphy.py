c25b47f5ad913de8d16ab0c52dbefc71274825ff
GitJournal <app@gitjournal.io>
2022-09-19 15:50:59 +0800

    [2022-09-19 07:50:57]
    FILE:
    tests/unittest_online_topic_generator_giphy.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_online_topic_generator_giphy.py
+++ tests/unittest_online_topic_generator_giphy.py
@@ -50,7 +50,7 @@ with tmpdir(path=tmpPath) as testDir:
         # set_proxy()
         newElems, label = OnlineFetcher(elems, tempdir=tmpPath) # infinite video generators.
         for elem in newElems:
-            waitForServerUp(clash_refresher_port,"clash update controller")
+            # waitForServerUp(clash_refresher_port,"clash update controller")
             sprint(elem)
             (item_id, local_video_location) = elem
             # what is the freaking response?
