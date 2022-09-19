80726348dd20bc6710049bf1cc5557091bb4cc3b
GitJournal <app@gitjournal.io>
2022-09-19 21:20:44 +0800

    [2022-09-19 13:20:42]
    FILE:
    pyjom/modules/informationGathering/onlineFetcher.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/informationGathering/onlineFetcher.py
+++ pyjom/modules/informationGathering/onlineFetcher.py
@@ -19,36 +19,36 @@ def OnlineFetcher(
 
     assert os.path.isabs(tempdir)
     with tmpdir(path=tempdir) as TD:
-    assert os.path.isdir(tempdir)
-    assert os.path.exists(tempdir)
-    for info in infoList:  # generator most likely
-        if source == "giphy":
-            (source_id, frameMeta) = info
-            width, height = frameMeta["width"], frameMeta["height"]
-            asset_id = "video_[{}_{}]_[{}x{}]".format(source, source_id, width, height)
-            flag = frameSizeFilter(frameMeta, frame_size_filter)
-            if flag:
-                # this time it is selected.
-                url = frameMeta["url"]
-                extension = url.split("?")[0].split(".")[-1]
-                basename = ".".join([asset_id, extension])
-                download_path = os.path.join(tempdir, basename)
-                try:
-                    result = download(
-                        url,
-                        download_path,
-                        threads=threads,
-                        size_filter={"min": 0.4, "max": 50},
-                        use_multithread=use_multithread,
-                    )
-                    if result:
-                        yield source_id, download_path
-                    else:
-                        print("not downloading source:", source_id)
-                        print("skipping:", frameMeta)
-                        # print("____WTF IS GOING ON WITH THE DOWNLOADER?____")
-                        # breakpoint()
-                except:
-                    import traceback
-                    traceback.print_exc()
-                    print("error fetching assets from giphy")
+        assert os.path.isdir(tempdir)
+        assert os.path.exists(tempdir)
+        for info in infoList:  # generator most likely
+            if source == "giphy":
+                (source_id, frameMeta) = info
+                width, height = frameMeta["width"], frameMeta["height"]
+                asset_id = "video_[{}_{}]_[{}x{}]".format(source, source_id, width, height)
+                flag = frameSizeFilter(frameMeta, frame_size_filter)
+                if flag:
+                    # this time it is selected.
+                    url = frameMeta["url"]
+                    extension = url.split("?")[0].split(".")[-1]
+                    basename = ".".join([asset_id, extension])
+                    download_path = os.path.join(tempdir, basename)
+                    try:
+                        result = download(
+                            url,
+                            download_path,
+                            threads=threads,
+                            size_filter={"min": 0.4, "max": 50},
+                            use_multithread=use_multithread,
+                        )
+                        if result:
+                            yield source_id, download_path
+                        else:
+                            print("not downloading source:", source_id)
+                            print("skipping:", frameMeta)
+                            # print("____WTF IS GOING ON WITH THE DOWNLOADER?____")
+                            # breakpoint()
+                    except:
+                        import traceback
+                        traceback.print_exc()
+                        print("error fetching assets from giphy")
