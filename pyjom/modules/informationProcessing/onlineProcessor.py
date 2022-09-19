ea8f82e1dd38cf89ed23de5399da937b0e31cbe0
GitJournal <app@gitjournal.io>
2022-09-19 16:31:44 +0800

    [2022-09-19 08:31:41]
    FILE:
    pyjom/modules/informationProcessing/onlineProcessor.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/informationProcessing/onlineProcessor.py
+++ pyjom/modules/informationProcessing/onlineProcessor.py
@@ -14,28 +14,6 @@ def onlineProcessor(info, use_proxy=False,clash_refresher_port = 8677,proxy_url 
     flag = "topic_with_fetcher"
 
     with tmpdir(path=tmpPath) as testDir:
-        # print("TESTDIR:", testDir)
-        if flag == "only_topic_generator":
-            # print("HERE??",1)
-            for asset_id, meta in elems:
-                print("X", asset_id, meta)
-                url = meta["url"]
-                extension = url.split("?")[0].split(".")[-1]
-                basename = ".".join([asset_id, extension])
-                download_path = os.path.join(tmpPath, basename)
-                try:
-                    download(
-                        url,
-                        download_path,
-                        threads=6,
-                        size_filter={"min": 0.4, "max": 50},
-                        use_multithread=True
-                    )
-                except:
-                    print("Error when download file")
-                # X ('sr8jYZVVsCmxddga8w', {'height': 480, 'width': 474, 'url': 'https://media0.giphy.com/media/sr8jYZVVsCmxddga8w/giphy.gif'})
-                # breakpoint()
-                # seems good. now we check the cat/dog.
         elif flag == "topic_with_fetcher":
             sprint("checking online fetcher")
             # print("HERE??",2)
