2832785435d1d7d15b56aa5249ef133b9753df92
GitJournal <app@gitjournal.io>
2022-09-19 20:29:40 +0800

    [2022-09-19 12:29:37]
    FILE:
    pyjom/modules/contentProducing/producerTemplates.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/contentProducing/producerTemplates.py
+++ pyjom/modules/contentProducing/producerTemplates.py
@@ -533,7 +533,7 @@ def petsWithMusicOnlineProducer(dataGenerator, configs, tempdir='/dev/shm/medial
         # using a ffmpeg filter.
 
         final_output_location = ".".join([getRandomFileName, 'mp4'])
-        final_output_location = os.path.join(tempdir,    final_output_location)
+        final_output_location = os.path.join(tempdir,final_output_location)
         import ffmpeg
         ffmpeg.input(rendered_media_location).filter('ass', ass_file_path).output(final_output_location).run(overwrite_output=True)
         yield final_output_location # another generator?
