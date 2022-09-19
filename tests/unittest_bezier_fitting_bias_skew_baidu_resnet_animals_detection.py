2f62e2ecc142b1393861f4261a1b6b804b14a61d
GitJournal <app@gitjournal.io>
2022-09-19 12:02:38 +0800

    [2022-09-19 04:02:35]
    FILE:
    tests/unittest_bezier_fitting_bias_skew_baidu_resnet_animals_detection.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_bezier_fitting_bias_skew_baidu_resnet_animals_detection.py
+++ tests/unittest_bezier_fitting_bias_skew_baidu_resnet_animals_detection.py
@@ -85,7 +85,8 @@ skew = -0.5
 curve_function_kwargs={"start": (0, 0), "end": (1, 1), "skew": skew} # maximize the output.
 
 for test_param, target_output in test_params:
-    for label, confidence in test_param:
+    for index, (label, confidence) in test_param:
+
         output = multiParameterExponentialNetwork(*params,input_bias=input_bias,curve_function_kwargs = curve_function_kwargs)
         print('output:',output)
         print('target_output:', target_output)
