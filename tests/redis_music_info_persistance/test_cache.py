9428f2b3e390758c020721b53f1b0c3bf9ac1102
GitJournal <app@gitjournal.io>
2022-09-19 23:05:29 +0800

    [2022-09-19 15:05:22]
    FILE:
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/redis_music_info_persistance/test_cache.py
+++ tests/redis_music_info_persistance/test_cache.py
@@ -1,6 +1,9 @@
 from redis_cache.redis_cache import RedisCache
 from redis_cache.rediscache import cache_it
 
+
+redisCache = RedisCache(redisAddress, redisPort)
+
 def test_function(parameter):
     print('hello world')
     print('parameter:',parameter)
