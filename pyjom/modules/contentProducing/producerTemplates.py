33841fa3f4d549ad17ff5e3debdc6ee2864544a7
GitJournal <app@gitjournal.io>
2022-09-19 17:13:37 +0800

    [2022-09-19 09:13:33]
    FILE:
    pyjom/modules/contentProducing/producerTemplates.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/contentProducing/producerTemplates.py
+++ pyjom/modules/contentProducing/producerTemplates.py
@@ -100,7 +100,7 @@ def getMusicCutSpansCandidates(
     candidates = sorted_lyrics_nearby_bpm_candidates + sorted_remained_bpm_candidates
     return candidates, standard_bpm_spans
 
-def getMusicCutSpansGaussian(music, music_duration, mintime=0.6, maxtime=7)
+def getMusicCutSpansGaussian(music, music_duration, mintime=0.6, maxtime=7, )
 
 def getMusicCutSpans(
     music, music_duration, lyric_path, maxtime, mintime, mbeat_time_tolerance=0.8
