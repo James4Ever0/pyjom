bba3f506deea39ffef26dfb6c47acc019e6047ee
GitJournal <app@gitjournal.io>
2022-09-19 18:40:46 +0800

    [2022-09-19 10:40:43]
    FILE:
    pyjom/modules/contentProducing/producerTemplates.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/contentProducing/producerTemplates.py
+++ pyjom/modules/contentProducing/producerTemplates.py
@@ -454,7 +454,7 @@ def petsWithMusicProducer(filtered_info, meta_info, config={}):
     )  # this might be an infinity loop.
     # but why the fuck we got 10 minutes long of the freaking video?
     print(render_list)  # empty render list! wtf?
-    #
+    # why the fuck we have duplicated clips? why the fuck?
 
     breakpoint()  # WTF IS GOING ON? LEADING TO 10 MINS OF CRAP?
     medialangObject = renderList2MediaLang(
