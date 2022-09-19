bb302e335098229c77d60ea7115c06cd865be2e0
GitJournal <app@gitjournal.io>
2022-09-19 14:22:52 +0800

    [2022-09-19 06:22:49]
    FILE:
    pyjom/videotoolbox.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/videotoolbox.py
+++ pyjom/videotoolbox.py
@@ -1895,8 +1895,8 @@ def yolov5_bezier_paddlehub_resnet50_dog_cat_video_filter(videoPath, debug=False
         filter_result = bezierPaddleHubResnet50VideoDogCatDetector(videoPath, debug=debug,
             input_bias=0.0830047243746045,
     skew=-0.4986098769473948,
-    threshold=0.5,
-    dog_label_file_path="/root/Desktop/works/pyjom/tests/animals_paddlehub_classification_resnet/dogcat_label_file_path,
+    threshold=threshold,
+    dog_label_file_path=dog_label_file_path,
     cat_label_file_path=cat_label_file_path,)
     if not filter_result:
         if debug:
