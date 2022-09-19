be873723a7fad1b19cf51cd9517d6a000594fba7
GitJournal <app@gitjournal.io>
2022-09-19 23:07:44 +0800

    [2022-09-19 15:07:42]
    FILE:
    tests/redis_music_info_persistance/test_cache.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/redis_music_info_persistance/test_cache.py
+++ tests/redis_music_info_persistance/test_cache.py
@@ -6,7 +6,8 @@ redisPort = 0
 redisCache = RedisCache(redisAddress, redisPort)
 oneDay = 60*60*24 # one day?
 redisExpire =oneDay*7 # god damn it!
-@cache_it(limit=3, expire=redisExpire,)
+
+@cache_it(limit=3, expire=redisExpire)
 def test_function(parameter):
     print('hello world')
     print('parameter:',parameter)
