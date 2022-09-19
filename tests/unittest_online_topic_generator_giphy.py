b9fd5a1ed183a56475aa2329b468b488e47c6f2c
GitJournal <app@gitjournal.io>
2022-09-19 16:08:53 +0800

    [2022-09-19 08:08:50]
    FILE:
    tests/unittest_online_topic_generator_giphy.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_online_topic_generator_giphy.py
+++ tests/unittest_online_topic_generator_giphy.py
@@ -5,10 +5,10 @@ from lazero.utils import sprint
 from lazero.network import download, waitForServerUp
 from lazero.filesystem import tmpdir
 
-# clash_refresher_port = 8677
-# clash_refresher_url = "http://127.0.0.1:{}".format(clash_refresher_port)
+clash_refresher_port = 8677
+clash_refresher_url = "http://127.0.0.1:{}".format(clash_refresher_port)
 
-# waitForServerUp(clash_refresher_port,"clash update controller")
+waitForServerUp(clash_refresher_port,"clash update controller")
 
 elems, function_label = OnlineTopicGenerator()
 sprint("FUNCTION LABEL:", function_label)
