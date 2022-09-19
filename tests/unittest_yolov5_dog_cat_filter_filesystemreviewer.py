982ab0eca1e4e3e74b94a4dd27efc1ea72b4ef3c
GitJournal <app@gitjournal.io>
2022-09-19 13:55:31 +0800

    [2022-09-19 05:55:28]
    FILE:
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_yolov5_dog_cat_filter_filesystemreviewer.py
+++ tests/unittest_yolov5_dog_cat_filter_filesystemreviewer.py
@@ -354,7 +354,7 @@ videoPaths = [
 for videoPath in videoPaths:  # this is for each file.
     # sprint(result)
     sprint("checking video: %s" % videoPath)
-    filter_result = yolov5VideoDogCatDetector(videoPath) # this is for short video. not for long video.
+    filter_result = yolov5VideoDogCatDetector(videoPath) # this is for short video. not for long video. long video needs to be sliced into smaller chunks
     # sprint("FILTER PASSED?", filter_result)
     if not filter_result:
         sprint("CHECKING WITH BEZIER CURVE AND RESNET50")
