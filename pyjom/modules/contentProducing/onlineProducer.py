6de9625b9096d934c79833a2437b8a3a12e5d078
GitJournal <app@gitjournal.io>
2022-09-19 18:48:43 +0800

    [2022-09-19 10:48:40]
    FILE:
    pyjom/modules/contentProducing/onlineProducer.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/contentProducing/onlineProducer.py
+++ pyjom/modules/contentProducing/onlineProducer.py
@@ -5,4 +5,4 @@ from pyjom.modules.contentProducing.producerTemplates import getProducerTemplate
 def OnlineProducer(processed_info, template=None, template_config={}):
     template_function = getProducerTemplate[template]
     exported_media_location = template_function(processed_info,config=template_config)
-    # i guess the title/tags are 
+    # i guess the title/tags are actually belongs to the poster.
