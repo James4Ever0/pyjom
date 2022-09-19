9c09bb6b459a286114b81475c606c0b9ed7841d4
GitJournal <app@gitjournal.io>
2022-09-19 19:55:22 +0800

    [2022-09-19 11:55:19]
    FILE:
    pyjom/modules/contentProducing/producerTemplates.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/contentProducing/producerTemplates.py
+++ pyjom/modules/contentProducing/producerTemplates.py
@@ -494,7 +494,7 @@ def petsWithMusicOnlineProducer(dataGenerator, configs):
         # check for 'demanded_cut_spans' now!
         render_list = [] # what is this freaking render_list?
         # [{'span':(start,end),'cut':{'span':(start,end)},'source':videoSource},...]
-        standard_bpm_spans.sort(lambda span: (span[1]-span[0]))
+        standard_bpm_spans.sort(lambda span: abs((span[1]-span[0])-))
         data_ids = []
         for data in dataGenerator:
             # what is the format of the data?
