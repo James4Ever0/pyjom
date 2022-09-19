68d0964428b8961434716d976b9e0895e82a9b60
GitJournal <app@gitjournal.io>
2022-09-19 16:47:23 +0800

    [2022-09-19 08:47:20]
    FILE:
    pyjom/modules/contentProducing/__init__.py
    pyjom/modules/contentProducing/onlineProducer.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/contentProducing/__init__.py
+++ pyjom/modules/contentProducing/__init__.py
@@ -1,2 +1,3 @@
 from pyjom.modules.contentProducing.dummyProducer import *
 from pyjom.modules.contentProducing.localProducer import *
+from pyjom.modules.contentProducing.onlineProducer import *
--- pyjom/modules/contentProducing/onlineProducer.py
+++ pyjom/modules/contentProducing/onlineProducer.py
@@ -1,1 +1,3 @@
 
+@
+def OnlineProducer()
