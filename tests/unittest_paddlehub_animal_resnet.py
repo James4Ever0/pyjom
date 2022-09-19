a74667c903745cf948035288ea43272d1da53832
GitJournal <app@gitjournal.io>
2022-09-19 10:52:12 +0800

    [2022-09-19 02:52:09]
    FILE:
    tests/unittest_paddlehub_animal_resnet.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_paddlehub_animal_resnet.py
+++ tests/unittest_paddlehub_animal_resnet.py
@@ -7,7 +7,8 @@ from pyjom.imagetoolbox import resizeImageWithPadding
 import paddlehub as hub
 import cv2
 
-def labelReader()
+def labelFileReader(filename):
+    with open(filename, 'r') as
 
 dog_labels = ["狗", "犬", "梗"]
 cat_labels = ["猫"]  # ends with this, and not containing forbidden words.
