f2bddaffd44123d73d60ab383f0b9657a5d69e6c
GitJournal <app@gitjournal.io>
2022-09-19 12:21:05 +0800

    [2022-09-19 04:21:02]
    FILE:
    tests/unittest_bezier_fitting_bias_skew_baidu_resnet_animals_detection.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_bezier_fitting_bias_skew_baidu_resnet_animals_detection.py
+++ tests/unittest_bezier_fitting_bias_skew_baidu_resnet_animals_detection.py
@@ -150,5 +150,5 @@ space = (
 best = fmin(objective, space, algo=tpe.suggest, max_evals=100)
 sprint("BEST:", best)
 # sprint("EVAL:",space_eval(space, best))
-best_loss = objective(['skew'],['input_bias'])
+best_loss = objective((best['skew'],best['input_bias']))
 sprint("BEST LOSS:",best_loss)
