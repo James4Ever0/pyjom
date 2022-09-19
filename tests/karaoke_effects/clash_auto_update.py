c413db223f37d8c06d3fea59991427143780bdf0
GitJournal <app@gitjournal.io>
2022-09-19 16:05:45 +0800

    [2022-09-19 08:05:42]
    FILE:
    tests/karaoke_effects/clash_auto_update.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/karaoke_effects/clash_auto_update.py
+++ tests/karaoke_effects/clash_auto_update.py
@@ -140,7 +140,8 @@ def refineClashYaml(clashYamlPath="Clash3.yaml", advanced=False):
             if proxy['name'] == "":
                 updateIndex = index
                 updatedProxy = proxy.copy()
-                updatedProxy['proxies'] = [elem for elem in proxy['proxies'] if elem not in []]
+                updatedProxy['proxies'] = [elem for elem in proxy['proxies'] if elem not in ['☁️ 全球直连',
+      - 🌐 节点选择]]
                 break
         data['proxies'][updateIndex] = updatedProxy
         # for item in data['proxies']:
