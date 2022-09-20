817c29b258556219b85e7cc32a02d5ff069b5ee3
GitJournal <app@gitjournal.io>
2022-09-20 16:12:38 +0800

    [2022-09-20 08:12:35]
    FILE:
    tests/test_auto_dog_video_giphy_online_producer.sh
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/test_auto_dog_video_giphy_online_producer.sh
+++ tests/test_auto_dog_video_giphy_online_producer.sh
@@ -1,1 +1,3 @@
 # env LD_LIBRARY_PATH=/usr/local/lib python3 test_auto_dog_video_giphy_online_producer.py 
+tmux kill-session -t online_dog_cat_generator_test
+tmuxp load test_auto_dog_video_giphy_online_producer.yaml
