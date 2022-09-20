c5271348ed3b46e91440ac0aaf71be280b8eda34
GitJournal <app@gitjournal.io>
2022-09-20 20:21:54 +0800

    [2022-09-20 12:21:50]
    FILE:
    tests/pyidm_yd_dlp_download_manager_multithread/test.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/pyidm_yd_dlp_download_manager_multithread/test.py
+++ tests/pyidm_yd_dlp_download_manager_multithread/test.py
@@ -8,5 +8,6 @@ dest = "./test.gif"
 obj = SmartDL(url, dest)
 obj.start()
 # [*] 0.23 Mb / 0.37 Mb @ 88.00Kb/s [##########--------] [60%, 2s left]
-
+print('DOWNLOAD FINISHED')
 path = obj.get_dest()
+print("DOWNLOADED AT:", path)
