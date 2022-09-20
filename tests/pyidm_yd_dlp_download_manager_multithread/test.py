44ad213ba2d45486fe69ca10f8d9b3a733b2fe00
GitJournal <app@gitjournal.io>
2022-09-20 20:36:10 +0800

    [2022-09-20 12:36:07]
    FILE:
    tests/pyidm_yd_dlp_download_manager_multithread/test.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/pyidm_yd_dlp_download_manager_multithread/test.py
+++ tests/pyidm_yd_dlp_download_manager_multithread/test.py
@@ -1,16 +1,17 @@
 url = "https://media3.giphy.com/media/wTrXRamYhQzsY/giphy.gif?cid=dda24d502m79hkss38jzsxteewhs4e3ocd3iqext2285a3cq&rid=giphy.gif&ct=g"
 
+if option == 1:
 import yt_dlp
 # import pyidm
 
 yt_dlp.YoutubeDL()
+elif option == 2:
+    from pySmartDL import SmartDL
 
-from pySmartDL import SmartDL
-
-dest = "./test.gif"
-obj = SmartDL(url, dest, threads=20)
-obj.start()
-# [*] 0.23 Mb / 0.37 Mb @ 88.00Kb/s [##########--------] [60%, 2s left]
-print('DOWNLOAD FINISHED')
-path = obj.get_dest()
-print("DOWNLOADED AT:", path)
+    dest = "./test.gif"
+    obj = SmartDL(url, dest, threads=20)
+    obj.start()
+    # [*] 0.23 Mb / 0.37 Mb @ 88.00Kb/s [##########--------] [60%, 2s left]
+    print('DOWNLOAD FINISHED')
+    path = obj.get_dest()
+    print("DOWNLOADED AT:", path)
