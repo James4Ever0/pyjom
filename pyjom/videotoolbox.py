4b2cbdffd2512f0757b1f1be1dbb006b3de7f4c7
GitJournal <app@gitjournal.io>
2022-09-20 18:03:17 +0800

    [2022-09-20 10:03:12]
    FILE:
    pyjom/videotoolbox.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/videotoolbox.py
+++ pyjom/videotoolbox.py
@@ -2479,7 +2479,7 @@ def motionVectorEstimation(
 ########################### TEXT AREA CALCULATION #########################
 # threshold: {'max':0.3} for our new filter?
 from pyjom.imagetoolbox import getImageTextAreaRatio
-def getVideoTextAreaRatio(videoPath, mode:Literal['max','mean','mean_no_missing']='max',langs:tuple=('en',), gpu=True, recognizer=False,debug=False, fps=10):
+def getVideoTextAreaRatio(videoPath, mode:Literal['max','mean','mean_no_missing','list']='max',langs:tuple=('en',), gpu=True, recognizer=False,debug=False, fps=10):
 iterator = getVideoFrameIteratorWithFPS(videoPath, start=-1, end=-1, fps=fps)
 
 for frame in iterator:
