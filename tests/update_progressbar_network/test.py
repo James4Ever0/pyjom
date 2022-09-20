d5a2aa384eb1a514f6ce90eb93c3da6620aa2df0
GitJournal <app@gitjournal.io>
2022-09-20 12:14:09 +0800

    [2022-09-20 04:14:04]
    FILE:
    tests/update_progressbar_network/test.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/update_progressbar_network/test.py
+++ tests/update_progressbar_network/test.py
@@ -1,10 +1,12 @@
 # try to update progressbar via network.
 
 from tqdm import tqdm
-
+t = None
 def hello():
 
 def reset(): # pass the iteration count
+    global t
+    if t is not None:
 
     t = tqdm(total=total)
 
