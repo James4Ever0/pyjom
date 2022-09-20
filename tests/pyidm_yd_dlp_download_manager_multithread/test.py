d7c9eb8ce0db09194f39634fa698912fb8d63384
GitJournal <app@gitjournal.io>
2022-09-20 20:20:45 +0800

    [2022-09-20 12:20:42]
    FILE:
    tests/pyidm_yd_dlp_download_manager_multithread/test.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/pyidm_yd_dlp_download_manager_multithread/test.py
+++ tests/pyidm_yd_dlp_download_manager_multithread/test.py
@@ -1,3 +1,14 @@
 url = "https://media3.giphy.com/media/wTrXRamYhQzsY/giphy.gif?cid=dda24d502m79hkss38jzsxteewhs4e3ocd3iqext2285a3cq&rid=giphy.gif&ct=g"
 
-import pyidm
+# import pyidm
+
+from pySmartDL import SmartDL
+
+url = "https://github.com/iTaybb/pySmartDL/raw/master/test/7za920.zip"
+dest = "C:\\Downloads\\" # or '~/Downloads/' on linux
+
+obj = SmartDL(url, dest)
+obj.start()
+# [*] 0.23 Mb / 0.37 Mb @ 88.00Kb/s [##########--------] [60%, 2s left]
+
+path = obj.get_dest()
