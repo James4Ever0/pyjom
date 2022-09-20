103fb8b1690f7d8acc68272763a5fd9f19e0ca0a
GitJournal <app@gitjournal.io>
2022-09-20 20:46:59 +0800

    [2022-09-20 12:46:55]
    FILE:
    tests/pyidm_yd_dlp_download_manager_multithread/test.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/pyidm_yd_dlp_download_manager_multithread/test.py
+++ tests/pyidm_yd_dlp_download_manager_multithread/test.py
@@ -1,6 +1,6 @@
 url = "https://media3.giphy.com/media/wTrXRamYhQzsY/giphy.gif?cid=dda24d502m79hkss38jzsxteewhs4e3ocd3iqext2285a3cq&rid=giphy.gif&ct=g"
 
-option = 1
+option = 3
 
 if option == 1:
     import yt_dlp
