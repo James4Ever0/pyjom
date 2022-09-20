d8eba8628bb45f58afd3a436154db0dc1c563ec7
GitJournal <app@gitjournal.io>
2022-09-21 00:57:13 +0800

    [2022-09-20 16:57:09]
    FILE:
    tests/bilibili_search_api_modification_section_params_get_related_videos/test_search_section_pets.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_search_api_modification_section_params_get_related_videos/test_search_section_pets.py
+++ tests/bilibili_search_api_modification_section_params_get_related_videos/test_search_section_pets.py
@@ -10,14 +10,6 @@
 # 最多收藏
 
 
-class order:
-    综合排序 = None
-    最多点击 = "click"
-    最新发布 = "pubdate"
-    最多弹幕 = "dm"
-    最多收藏 = "stow"
-
-
 # https://search.bilibili.com/all?keyword=%E9%A9%AC%E5%85%8B%E6%80%9D%E4%BD%A9%E6%81%A93&from_source=webtop_search&spm_id_from=333.1007&tids=36&order=stow&duration=1
 # https://search.bilibili.com/all?keyword=%E9%A9%AC%E5%85%8B%E6%80%9D%E4%BD%A9%E6%81%A93&from_source=webtop_search&spm_id_from=333.1007&tids=36&order=stow&duration=2
 # https://search.bilibili.com/all?keyword=%E9%A9%AC%E5%85%8B%E6%80%9D%E4%BD%A9%E6%81%A93&from_source=webtop_search&spm_id_from=333.1007&tids=36&order=stow&duration=3
