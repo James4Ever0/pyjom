d1a6725ec10fffc1db4d38737f9cf39bcb2ecf9e
GitJournal <app@gitjournal.io>
2022-09-20 15:54:13 +0800

    [2022-09-20 07:54:10]
    FILE:
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/update_progressbar_network/client.py
+++ tests/update_progressbar_network/client.py
@@ -6,5 +6,5 @@ waitForServerUp(port=port, message=message)
 
 import requests
 
-requests.get('http://localhost:8576',proxies=None,)
+requests.get('http://localhost:8576/reset',proxies=None,)
 for _ in range(200):
