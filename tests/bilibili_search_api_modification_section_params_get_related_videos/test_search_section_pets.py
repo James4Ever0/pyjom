745b435e27092d5294a549e83ea8ec29b2f07dc9
GitJournal <app@gitjournal.io>
2022-09-21 00:35:09 +0800

    [2022-09-20 16:35:05]
    FILE:
    tests/bilibili_search_api_modification_section_params_get_related_videos/test_search_section_pets.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_search_api_modification_section_params_get_related_videos/test_search_section_pets.py
+++ tests/bilibili_search_api_modification_section_params_get_related_videos/test_search_section_pets.py
@@ -17,11 +17,12 @@ class order:
    最多收藏 = 'stow'
 
 # https://search.bilibili.com/all?keyword=%E9%A9%AC%E5%85%8B%E6%80%9D%E4%BD%A9%E6%81%A93&from_source=webtop_search&spm_id_from=333.1007&tids=36&order=stow&duration=1
+# https://search.bilibili.com/all?keyword=%E9%A9%AC%E5%85%8B%E6%80%9D%E4%BD%A9%E6%81%A93&from_source=webtop_search&spm_id_from=333.1007&tids=36&order=stow&duration=2
 
 class duration:
     全部时长 = None
     _10分钟以下 = 1
-    _10_30分钟 = 
+    _10_30分钟 = 2
 
 class bilibiliSearchParams:
     order = order
