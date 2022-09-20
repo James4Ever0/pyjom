3028f5128e0e4f16341dbccc07e02b2c691fd480
GitJournal <app@gitjournal.io>
2022-09-21 00:18:09 +0800

    [2022-09-20 16:18:03]
    FILE:
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_search_api_modification_section_params_get_related_videos/test_search_section_pets.py
+++ tests/bilibili_search_api_modification_section_params_get_related_videos/test_search_section_pets.py
@@ -3,3 +3,4 @@
 # https://search.bilibili.com/all?keyword=%E9%A9%AC%E5%85%8B%E6%80%9D%E4%BD%A9%E6%81%A93&from_source=webtop_search&spm_id_from=333.1007&tids=36&order=click
 # 最多点击
 # https://search.bilibili.com/all?keyword=%E9%A9%AC%E5%85%8B%E6%80%9D%E4%BD%A9%E6%81%A93&from_source=webtop_search&spm_id_from=333.1007&tids=36&order=pubdate
+# 最新发布
