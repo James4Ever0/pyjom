ef04f4cdfc12aff9d3602b8067d07686b9def968
GitJournal <app@gitjournal.io>
2022-09-20 13:38:29 +0800

    [2022-09-20 05:38:26]
    FILE:
    pyjom/mathlib.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/mathlib.py
+++ pyjom/mathlib.py
@@ -311,5 +311,6 @@ def getCursorOfMaxAverageInWindow(referenceData, windowSize, dataDuration, super
     moving_sum_span +=1
     moving_sum = np.convolve(interpolated_fp, np.ones(moving_sum_span),'valid')
     max_index = np.argmax(moving_sum)
-    cursor = 
-    dataDuration-windowSize,
+    cursor = interpolated_fp[max_index]
+    cursor = min(dataDuration-windowSize,cursor)
+    return cursor
