96bc6c10549ab66a3037f7944d5ca75777815ead
GitJournal <app@gitjournal.io>
2022-09-20 18:00:19 +0800

    [2022-09-20 10:00:16]
    FILE:
    pyjom/videotoolbox.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/videotoolbox.py
+++ pyjom/videotoolbox.py
@@ -2479,6 +2479,32 @@ def motionVectorEstimation(
 ########################### TEXT AREA CALCULATION #########################
 
 from pyjom.imagetoolbox import getImageTextAreaRatio
-def getVideoTextAreaRatio(videoPath, mode:Literal['max','mean','mean_no_missing'],langs:tuple=('en',), gpu=True, recognizer=False):
+def getVideoTextAreaRatio(videoPath, mode:Literal['max','mean','mean_no_missing'],langs:tuple=('en',), gpu=True, recognizer=False,debug=False):
+iterator = getVideoFrameIteratorWithFPS(videoPath, start=-1, end=-1, fps=10)
+elif test_subject == 'image':
+    imagePath = "/root/Desktop/works/pyjom/samples/image/dog_saturday_night.bmp"
+    iterator = [cv2.imread(imagePath)]
+else:
+    raise Exception("unknown test_subject:", test_subject)
+
+# threshold: {'max':0.3}
+for frame in iterator:
+    height, width = frame.shape[:2]
+    img = np.zeros((height, width, 3))
+    detection, recognition = reader.detect(frame)  # not very sure.
+    if detection == [[]]:
+        diagonalRects = []
+    else:
+        diagonalRects = [LRTBToDiagonal(x) for x in detection[0]]
+    for x1, y1, x2, y2 in diagonalRects:
+        w, h = x2 - x1, y2 - y1
+        x, y = x1, y1
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), -1)
+    # calculate the portion of the text area.
+    textArea = np.sum(img)
+    textAreaRatio = (textArea/255)/(width*height)
+    print("text area: {:.2f} %".format(textAreaRatio))
+    cv2.imshow("img", img)
+    cv2.waitKey(0)
 
 ########################### TEXT AREA CALCULATION #########################
