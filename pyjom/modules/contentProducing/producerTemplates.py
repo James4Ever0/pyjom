e25e286d5f0c790e903010b3715d5912ab60e561
GitJournal <app@gitjournal.io>
2022-09-20 16:34:35 +0800

    [2022-09-20 08:34:32]
    FILE:
    pyjom/modules/contentProducing/producerTemplates.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/contentProducing/producerTemplates.py
+++ pyjom/modules/contentProducing/producerTemplates.py
@@ -593,6 +593,7 @@ def petsWithMusicOnlineProducer(
                         append_render_list = True
                         # break
                     elif checkMinMaxDict(speed_delta, {"min":1.2,'max':3}):
+                        case='trim'
                         append_render_list = True
                         from pyjom.videotoolbox import motionVectorEstimation
                         dataDict = motionVectorEstimation(videoSource)
