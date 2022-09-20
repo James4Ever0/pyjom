9edf9ff42914989366cc8be2f47fc1078ec5ad35
GitJournal <app@gitjournal.io>
2022-09-20 15:57:28 +0800

    [2022-09-20 07:57:25]
    FILE:
    tests/update_progressbar_network/client.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/update_progressbar_network/client.py
+++ tests/update_progressbar_network/client.py
@@ -9,4 +9,6 @@ import requests
 while True:
     requests.get('http://localhost:8576/reset',proxies=None,params = {'total':200})
     for _ in range(200):
+        import time
+        time.sleep(0.1)
         requests.get('http://localhost:8576/update',proxies=None)
