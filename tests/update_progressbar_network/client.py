21c7e07f87799bad447a5f39bc3b7c0d34b60413
GitJournal <app@gitjournal.io>
2022-09-20 16:01:28 +0800

    [2022-09-20 08:01:25]
    FILE:
    tests/update_progressbar_network/client.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/update_progressbar_network/client.py
+++ tests/update_progressbar_network/client.py
@@ -1,12 +1,12 @@
-from lazero.network import waitForServerUp
+import requests
 
 class netProgressbar:
     def __init__(self, port = 8576, message = 'progressbar server'):
+        from lazero.network import waitForServerUp
         self.port = port
         self.message = message
         waitForServerUp(port=port, message=message)
 
-import requests
 
 while True:
     requests.get('http://localhost:8576/reset',proxies=None,params = {'total':200})
