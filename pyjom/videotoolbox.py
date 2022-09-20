0cab82684d704e2d1880e3b40287bd03aaec22d3
GitJournal <app@gitjournal.io>
2022-09-20 17:30:13 +0800

    [2022-09-20 09:30:09]
    FILE:
    pyjom/videotoolbox.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/videotoolbox.py
+++ pyjom/videotoolbox.py
@@ -249,9 +249,7 @@ def detectTextRegionOverTime(
     detectionList = []
     # use some merging technique over time.
     # convolution?
-    import easyocr
-    # no metal? no dbnet18?
-    reader = easyocr.Reader(langs, gpu=True, recognizer=False)
+    
     # how many percent sure?
     # reader = easyocr.Reader(["en","ch_sim"],gpu=False, recognizer=False) # no metal? no dbnet18?
     # are you classifying the thing putting boxes into different category?
