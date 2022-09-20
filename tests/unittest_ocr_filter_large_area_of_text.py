b0433bc0103ab725b130d36d7f1be2987fa58960
GitJournal <app@gitjournal.io>
2022-09-20 17:21:36 +0800

    [2022-09-20 09:21:32]
    FILE:
    tests/unittest_ocr_filter_large_area_of_text.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_ocr_filter_large_area_of_text.py
+++ tests/unittest_ocr_filter_large_area_of_text.py
@@ -4,17 +4,20 @@ import pytesseract
 from pytesseract import Output
 from lazero.utils.importers import cv2_custom_build_init
 cv2_custom_build_init()
-# import cv2
+import cv2
 # img = cv2.imread('image.jpg')
 from pyjom.videotoolbox import getVideoFrameIteratorWithFPS
 
+videoPath = ""
+
 iterator = getVideoFrameIteratorWithFPS(videoPath, start=-1, end=-1, fps=10)
 
-d = pytesseract.image_to_data(img, output_type=Output.DICT)
-n_boxes = len(d['level'])
-for i in range(n_boxes):
-    (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
-    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+for img in iterator:
+    d = pytesseract.image_to_data(img, output_type=Output.DICT)
+    n_boxes = len(d['level'])
+    for i in range(n_boxes):
+        (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-cv2.imshow('img', img)
-cv2.waitKey(0)
+    cv2.imshow('img', img)
+    cv2.waitKey(0)
