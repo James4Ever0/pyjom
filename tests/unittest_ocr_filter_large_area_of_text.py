8a1045ca5890e7e290c4691e5485c65b205ba06b
GitJournal <app@gitjournal.io>
2022-09-20 17:47:04 +0800

    [2022-09-20 09:47:00]
    FILE:
    tests/unittest_ocr_filter_large_area_of_text.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_ocr_filter_large_area_of_text.py
+++ tests/unittest_ocr_filter_large_area_of_text.py
@@ -37,6 +37,6 @@ for frame in iterator:
         cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), -1)
     # calculate the portion of the text area.
     textArea = np.sum(img)
-    textAreaRatio = 
+    textAreaRatio = (textArea/255)/
     cv2.imshow("img", img)
     cv2.waitKey(0)
