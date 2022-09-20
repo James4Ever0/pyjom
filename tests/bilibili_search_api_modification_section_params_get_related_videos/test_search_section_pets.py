b5ed5f08c008638b6326171f88b18118ed589034
GitJournal <app@gitjournal.io>
2022-09-21 01:01:46 +0800

    [2022-09-20 17:01:43]
    FILE:
    tests/bilibili_search_api_modification_section_params_get_related_videos/test_search_section_pets.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_search_api_modification_section_params_get_related_videos/test_search_section_pets.py
+++ tests/bilibili_search_api_modification_section_params_get_related_videos/test_search_section_pets.py
@@ -30,7 +30,7 @@
 # https://search.bilibili.com/live?keyword=%E9%A9%AC%E5%85%8B%E6%80%9D%E4%BD%A9%E6%81%A93&from_source=webtop_search&spm_id_from=333.1007&duration=4&tids=24&search_type=live_room
 
 class bilibiliSearchParams:
-    class path:
+    class _path:
         综合 = "all"
         视频 = "video"
         番剧 = "bangumi"
