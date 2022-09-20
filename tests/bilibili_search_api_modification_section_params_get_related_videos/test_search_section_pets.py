057cb57b6e50d4afd64e0a220c54bc678979d2b2
GitJournal <app@gitjournal.io>
2022-09-21 00:53:22 +0800

    [2022-09-20 16:53:19]
    FILE:
    tests/bilibili_search_api_modification_section_params_get_related_videos/test_search_section_pets.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_search_api_modification_section_params_get_related_videos/test_search_section_pets.py
+++ tests/bilibili_search_api_modification_section_params_get_related_videos/test_search_section_pets.py
@@ -51,7 +51,9 @@ class articleOrder:
 # https://search.bilibili.com/live?keyword=%E9%A9%AC%E5%85%8B%E6%80%9D%E4%BD%A9%E6%81%A93&from_source=webtop_search&spm_id_from=333.1007&duration=4&tids=24&search_type=live_room
 
 class liveSearchParams:
-    全部 = 
+    全部 = None
+    live_user
+    live_room
 
 class searchEntry:
     all
