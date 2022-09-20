a1ca7c58713f5056b5748ab1290555dd63f2af91
GitJournal <app@gitjournal.io>
2022-09-20 17:43:53 +0800

    [2022-09-20 09:43:50]
    FILE:
    tests/unittest_ocr_filter_large_area_of_text.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_ocr_filter_large_area_of_text.py
+++ tests/unittest_ocr_filter_large_area_of_text.py
@@ -3,8 +3,10 @@ from test_commons import *
 # import pytesseract
 # from pytesseract import Output
 from lazero.utils.importers import cv2_custom_build_init
+
 cv2_custom_build_init()
 import cv2
+
 # img = cv2.imread('image.jpg')
 from pyjom.videotoolbox import getVideoFrameIteratorWithFPS
 
@@ -16,20 +18,22 @@ iterator = getVideoFrameIteratorWithFPS(videoPath, start=-1, end=-1, fps=10)
 
 detectionList = []
 from pyjom.imagetoolbox import getEasyOCRReader, LRTBToDiagonal
-reader = getEasyOCRReader(('en',))
+
+reader = getEasyOCRReader(("en",))
 
 import numpy as np
+
 for frame in iterator:
     height, width = frame.shape[:2]
-    img = np.zeros(height, width, 3)
+    img = np.zeros((height, width, 3))
     detection, recognition = reader.detect(frame)  # not very sure.
     if detection == [[]]:
         diagonalRects = []
     else:
         diagonalRects = [LRTBToDiagonal(x) for x in detection[0]]
     for x1, y1, x2, y2 in diagonalRects:
-        w,h = x2-x1, y2-y1
-        x,y = x1,y1
+        w, h = x2 - x1, y2 - y1
+        x, y = x1, y1
         cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), -1)
-    cv2.imshow('img', img)
+    cv2.imshow("img", img)
     cv2.waitKey(0)
