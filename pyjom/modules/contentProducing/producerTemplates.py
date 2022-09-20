75261712ec5ca0dc792aecfa0f3a2acac12f55be
GitJournal <app@gitjournal.io>
2022-09-20 13:04:09 +0800

    [2022-09-20 05:04:06]
    FILE:
    pyjom/modules/contentProducing/producerTemplates.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/contentProducing/producerTemplates.py
+++ pyjom/modules/contentProducing/producerTemplates.py
@@ -589,7 +589,8 @@ def petsWithMusicOnlineProducer(
                         # break
                     elif checkMinMaxDict(speed_delta, {"min":1.2,'max':2}):
                         append_render_list = True
-                        cursor = random.uniform(0,dataDuration-closest_span_duration) # this is not exactly right. not even good.
+                        ['average_global_weighted_motion_vectors_filtered_cartesian_distance']
+                        # cursor = random.uniform(0,dataDuration-closest_span_duration) # this is not exactly right. not even good.
                         # you should utilize the 'motion vector' stuff.
                         mStart, mEnd = 0+cursor, closest_span_duration+cursor
                         candidate['cut']['span'] = (mStart, mEnd)
