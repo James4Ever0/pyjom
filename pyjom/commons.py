a7d458ac076dd3c678e78b2e05f31f910238e091
GitJournal <app@gitjournal.io>
2022-09-20 11:25:51 +0800

    [2022-09-20 03:25:50]
    FILE:
    pyjom/commons.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/commons.py
+++ pyjom/commons.py
@@ -20,11 +20,11 @@ import random
 import redis
 from redis_lru import RedisLRU
 
-from functools import lru_cache
+# from functools import lru_cache
 oneDay = 60*60*24 # one day?
 redisExpire =oneDay*7 # god damn it!
 
-@lru_cache(maxsize=1)
+# @lru_cache(maxsize=1)
 def redisLRUCache(ttl=redisExpire,redisAddress = "127.0.0.1",redisPort = 9291,max_size=20,debug=True):
     client = redis.StrictRedis(host=redisAddress, port=redisPort)
     cache = RedisLRU(client,max_size=max_size,debug=debug)
