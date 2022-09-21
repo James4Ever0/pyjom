af972c4ecf846b447b86347f7f6a741cc2328f27
GitJournal <app@gitjournal.io>
2022-09-21 19:22:32 +0800

    [2022-09-21 11:22:26]
    FILE:
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_search_api_modification_section_params_get_related_videos/searchDataParser.py
+++ tests/bilibili_search_api_modification_section_params_get_related_videos/searchDataParser.py
@@ -36,33 +36,4 @@ if test_subject == "search_all":
     with open("search_result_all.json", "r") as f:
         data = f.read()
         data = json.loads(data)
-    results = data['result']
-    for elem in results:
-        try:
-            if elem['result_type'] == 'video':
-                for video in elem['data']:
-                    # be warned cause all these things might fail.
-                    try:
-                        if video['type'] == 'video':
-                            bvid = video['bvid']
-                            tag = video['tag']
-                            tags = tag.split(",")
-                            categoryId = int(video['typeid'])
-                            categoryName = video['typename']
-                            title = video['title'] # remove those markers, please?
-                            title = clearHtmlTags(title)
-                            duration = video['duration'] # this is not recommended. we need seconds.
-                            play = video['play'] # select some hot videos.
-                            cover = video['pic']
-                            cover = linkFixer(cover)
-                            description = video['description']
-                            duration_seconds = videoDurationStringToSeconds(duration)
-                            for metadata in (bvid,tags,categoryId, categoryName,title, duration_seconds, play, cover, description):
-                                print(metadata)
-                            from lazero.utils.logger import sprint
-                            sprint()
-                    except:
-                        traceError('error iterating video metadata')
-                        continue
-        except:
-            traceError('error iterating data results')
+    
