d65abf5ceb21687c1ac963928ef05a1433065536
GitJournal <app@gitjournal.io>
2022-09-21 16:42:53 +0800

    [2022-09-21 08:42:51]
    FILE:
    tests/bilibili_search_api_modification_section_params_get_related_videos/searchDataParser.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_search_api_modification_section_params_get_related_videos/searchDataParser.py
+++ tests/bilibili_search_api_modification_section_params_get_related_videos/searchDataParser.py
@@ -42,7 +42,7 @@ if test_subject == "search_all":
                             cover = video['pic']
                             description = video['description']
                             duration_seconds = videoDurationStringToSeconds(duration)
-                            print(bvid,tags,categoryId, categoryName,)
+                            print(bvid,tags,categoryId, categoryName,title, duration_seconds, play, cover, description)
                     except:
                         continue
         except:
