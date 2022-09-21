39f628485c5a4912aabcdb2a528a68540cc92328
GitJournal <app@gitjournal.io>
2022-09-21 13:10:52 +0800

    [2022-09-21 05:10:49]
    FILE:
    pyjom/models/yolov5/ultralytics_yolov5_master/utils/downloads.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/models/yolov5/ultralytics_yolov5_master/utils/downloads.py
+++ pyjom/models/yolov5/ultralytics_yolov5_master/utils/downloads.py
@@ -59,8 +59,8 @@ def attempt_download(file, repo='ultralytics/yolov5'):  # from utils.downloads i
         # GitHub assets
         file.parent.mkdir(parents=True, exist_ok=True)  # make parent dir (if required)
         try:
-            with requests.get(f'https://api.github.com/repos/{repo}/releases/latest').json() as response:
-                response= response.json() # github api
+            with requests.get(f'https://api.github.com/repos/{repo}/releases/latest').json() as conn:
+                response= conn.json() # github api
                 assets = [x['name'] for x in response['assets']]  # release assets, i.e. ['yolov5s.pt', 'yolov5m.pt', ...]
                 tag = response['tag_name']  # i.e. 'v1.0'
         except Exception:  # fallback plan
