f67f4f65345ed08bdb2a317fc0098f7211535330
GitJournal <app@gitjournal.io>
2022-09-21 14:55:42 +0800

    [2022-09-21 06:55:38]
    FILE:
    tests/bilibili_search_api_modification_section_params_get_related_videos/testApi.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_search_api_modification_section_params_get_related_videos/testApi.py
+++ tests/bilibili_search_api_modification_section_params_get_related_videos/testApi.py
@@ -34,7 +34,7 @@ info=sync(v.get_info())
 # print(info)
 with open('video_info.json', 'w+') as f:
     f.write(json.dumps(info, indent=4, ensure_ascii=False))
-# -> pages to access all related videos.
+# -> pages to access all parted videos.
 # ->
 # able to get from search
 
