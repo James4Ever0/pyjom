8ef926be22981d46949e1d53a15c14d61caa92dc
GitJournal <app@gitjournal.io>
2022-09-21 18:16:29 +0800

    [2022-09-21 10:16:26]
    FILE:
    tests/unittest_check_video_corrput.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_check_video_corrput.py
+++ tests/unittest_check_video_corrput.py
@@ -1,19 +1,25 @@
 import ffmpeg
-not_nice ["invalid","failed",'error']
+
+not_nice = ["invalid", "failed", "error"]
 videoPath = "/root/Desktop/works/pyjom/samples/video/corrupt_video.gif"
-corrupted=False
+corrupted = False
 try:
-    stdout, stderr = ffmpeg.input(videoPath).output("null", f="null").run(capture_stdout=True,capture_stderr=True)
-    stderr_lower = stderr.decode('utf-8').lower()
+    stdout, stderr = (
+        ffmpeg.input(videoPath)
+        .output("null", f="null")
+        .run(capture_stdout=True, capture_stderr=True)
+    )
+    stderr_lower = stderr.decode("utf-8").lower()
     for word in not_nice:
         if word in stderr_lower:
             print("video is corrupted")
-            corrupted=True
+            corrupted = True
             break
 except:
     import traceback
+
     traceback.print_exc()
-    corrupted=True
+    corrupted = True
     print("corrupt video")
 
 if not corrupted:
