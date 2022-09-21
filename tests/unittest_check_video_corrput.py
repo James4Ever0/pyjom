189b1ef3b7d82345ce42c78c6c46eca1912ca869
GitJournal <app@gitjournal.io>
2022-09-21 18:16:17 +0800

    [2022-09-21 10:16:14]
    FILE:
    tests/unittest_check_video_corrput.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_check_video_corrput.py
+++ tests/unittest_check_video_corrput.py
@@ -3,10 +3,8 @@ not_nice ["invalid","failed",'error']
 videoPath = "/root/Desktop/works/pyjom/samples/video/corrupt_video.gif"
 corrupted=False
 try:
-    stdout, stderr = ffmpeg.input(videoPath)
-        .output("null", f="null")
-        .run(capture_stdout=True,capture_stderr=True
-    stderr_lower = stder.decode('utf-8').lower()
+    stdout, stderr = ffmpeg.input(videoPath).output("null", f="null").run(capture_stdout=True,capture_stderr=True)
+    stderr_lower = stderr.decode('utf-8').lower()
     for word in not_nice:
         if word in stderr_lower:
             print("video is corrupted")
