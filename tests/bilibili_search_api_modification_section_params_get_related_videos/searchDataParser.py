2183033aa5b00149d243dcd3a88c135bf502d06c
GitJournal <app@gitjournal.io>
2022-09-21 16:25:59 +0800

    [2022-09-21 08:25:57]
    FILE:
    tests/bilibili_search_api_modification_section_params_get_related_videos/searchDataParser.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_search_api_modification_section_params_get_related_videos/searchDataParser.py
+++ tests/bilibili_search_api_modification_section_params_get_related_videos/searchDataParser.py
@@ -6,4 +6,6 @@ if test_subject == "search_all":
     with open("search_result_all.json", "r") as f:
         data = f.read()
         data = json.loads(data)
-    data['result']
+    results = data['result']
+    for elem in results:
+        if elem['']
