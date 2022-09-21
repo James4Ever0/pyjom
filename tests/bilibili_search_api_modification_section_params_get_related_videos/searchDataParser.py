a6fd913ea68f3b14016b77bf9976abb9eaa00650
GitJournal <app@gitjournal.io>
2022-09-21 19:44:01 +0800

    [2022-09-21 11:43:54]
    FILE:
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_search_api_modification_section_params_get_related_videos/searchDataParser.py
+++ tests/bilibili_search_api_modification_section_params_get_related_videos/searchDataParser.py
@@ -99,5 +99,6 @@ elif test_subject =='video_info':
     # no tag out here.
     season = data['ugc_season'] # we only care about this thing.
     season_cover = season['cover']
+    sections = season['sections']
 else:
     raise Exception("unknown test_subject:", test_subject)
