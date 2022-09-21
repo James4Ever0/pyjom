c24f811194b18e98caca0d91562dcc3a6c6128d5
GitJournal <app@gitjournal.io>
2022-09-21 13:29:32 +0800

    [2022-09-21 05:29:29]
    FILE:
    tests/launch_test_enviorment.sh
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/launch_test_enviorment.sh
+++ tests/launch_test_enviorment.sh
@@ -2,3 +2,4 @@ cd /root/Desktop/works/pyjom/tests/karaoke_effects/
 gnome-terminal -- bash /root/Desktop/works/pyjom/tests/karaoke_effects/load_translator.sh
 
 cd /root/Desktop/works/pyjom/tests/redis_music_info_persistance
+gnome-terminal -- bash /root/Desktop/works/
