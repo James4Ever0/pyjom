6652e976d5e93e87662e172015cbe835e5945b55
GitJournal <app@gitjournal.io>
2022-09-21 12:23:56 +0800

    [2022-09-21 04:23:53]
    FILE:
    tests/bilibili_search_api_modification_section_params_get_related_videos/testApi.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_search_api_modification_section_params_get_related_videos/testApi.py
+++ tests/bilibili_search_api_modification_section_params_get_related_videos/testApi.py
@@ -2,15 +2,16 @@ from bilibili_api import sync, search
 
 BSP = search.bilibiliSearchParams()
 
-result = sync(
-    search.search(
-        keyword="汪汪",
-        params={"tids": BSP.all.tids.动物圈.tid, "duration": BSP.all.duration._10分钟以下},
-        page=1
-    )
-)
+# result = sync(
+#     search.search(
+#         keyword="汪汪",
+#         params={"tids": BSP.all.tids.动物圈.tid, "duration": BSP.all.duration._10分钟以下},
+#         page=1
+#     )
+# )
 
 # print(result)
+# how to get suggested keyword?
 import json
 result_str = json.dumps(result, ensure_ascii=False, indent=4)
 with open("search_result_all.json",'w+') as f:
