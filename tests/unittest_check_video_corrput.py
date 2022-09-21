4552769fd7cf885c5c730057e97485c262fce7c4
GitJournal <app@gitjournal.io>
2022-09-21 18:14:11 +0800

    [2022-09-21 10:14:07]
    FILE:
    tests/unittest_check_video_corrput.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_check_video_corrput.py
+++ tests/unittest_check_video_corrput.py
@@ -6,10 +6,10 @@ try:
         .input(videoPath)
         .output("null", f="null")
         .run(capture_stdout=True,capture_stderr=True
-except ffmpeg._run.Error:
-    print("corrupt video")
-else:
+    else:
     stderr_lower = stder.decode
     for word in not_nice:
         if word in
     print("video is fine")
+except ffmpeg._run.Error:
+    print("corrupt video")
