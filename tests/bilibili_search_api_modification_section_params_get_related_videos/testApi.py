f7244c017d35b29eeae1c35e7ba246a38f486d35
GitJournal <app@gitjournal.io>
2022-09-21 14:43:46 +0800

    [2022-09-21 06:43:44]
    FILE:
    tests/bilibili_search_api_modification_section_params_get_related_videos/testApi.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_search_api_modification_section_params_get_related_videos/testApi.py
+++ tests/bilibili_search_api_modification_section_params_get_related_videos/testApi.py
@@ -33,7 +33,7 @@ v=video.Video(bvid=bvid)
 info=sync(v.get_info())
 # print(info)
 with open('video_info.json', 'r') as f:
-    f.write(json.dumps(info, indent=4, ensure_ascii=False)
+    f.write(json.dumps(info, indent=4, ensure_ascii=False))
 
 # able to get from search
 
