4b6988f2e8498e6cb14b21b75fc9daccc9620abe
GitJournal <app@gitjournal.io>
2022-09-21 18:20:31 +0800

    [2022-09-21 10:20:28]
    FILE:
    pyjom/modules/informationProcessing/onlineProcessor.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/informationProcessing/onlineProcessor.py
+++ pyjom/modules/informationProcessing/onlineProcessor.py
@@ -72,7 +72,7 @@ def OnlineProcessor(
                 videoTextAreaRatioFilter = {"max": 0.3}
                 valid = True
                 mList = [
-                    [corruptVideoFilter,None, ]
+                    [corruptVideoFilter,None, dummyFilterFunction,'video corruption filter'],
                     [get_duration, duration_filter, checkMinMaxDict, "duration"],
                     [get_fps_float, fps_filter, checkMinMaxDict, "fps"],
                     [
