94acb3e675ff5006fbab6201a7fef3b78d1efe26
GitJournal <app@gitjournal.io>
2022-09-23 01:10:54 +0800

    [2022-09-22 17:06:30]
    FILE:
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_extract_cat_cover_from_video.py
+++ tests/unittest_extract_cat_cover_from_video.py
@@ -21,11 +21,11 @@ with tmpfile(path=path) as TF:
     from caer.video.frames_and_fps import get_duration
     duration = get_duration(path)
     mSampleSize = int(duration/2) # fps = 0.5 or something?
-    for frame in getVideoFrameSampler(path, -1,-1,sample_size=mSampleSize,iterator=True):
+    for frame in getVideoFrameSampler(path, -1,-1,sample_size=mSampleSize,iterate=True):
+        
         detections = bezierPaddleHubResnet50ImageDogCatDetector(frame)
         mDetections = [x for x in detections if x['identity'] == dog_or_cat]
         mDetections.sort(key=lambda x: -x['confidence']) # select the best one.
         if len(mDetections)>0:
             best_confidence = mDetections[0]['confidence']
             if checkMinMaxDict(best_confidence, confidence_threshold):
-                
