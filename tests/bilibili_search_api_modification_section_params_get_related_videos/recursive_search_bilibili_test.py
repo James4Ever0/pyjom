171278fa8f423dece53a64ae01280a641df6ce34
GitJournal <app@gitjournal.io>
2022-09-22 23:09:58 +0800

    [2022-09-22 15:09:55]
    FILE:
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_search_api_modification_section_params_get_related_videos/recursive_search_bilibili_test.py
+++ tests/bilibili_search_api_modification_section_params_get_related_videos/recursive_search_bilibili_test.py
@@ -7,7 +7,7 @@ sys.path.append(".")
 os.environ["http_proxy"] = ""
 os.environ["https_proxy"] = ""
 
-from lazero.utils.importers import 
+from lazero.utils.importers import cv2_custom_build_init
 
 # metatopic = {
 #     "optional": [
