35ed72d0689714d3d6c79fbbcf7b52985766ae5f
GitJournal <app@gitjournal.io>
2022-09-22 23:35:08 +0800

    [2022-09-22 15:35:05]
    FILE:
    tests/unittest_extract_cat_cover_from_video.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_extract_cat_cover_from_video.py
+++ tests/unittest_extract_cat_cover_from_video.py
@@ -4,6 +4,6 @@ from lazero.filesystem.temp import tmpfile
 
 import yt_dlp
 # import pyidm
-path = "./randomName.mp4"
+path = "/randomName.mp4"
 x = yt_dlp.YoutubeDL({"outtmpl":path,'format':'[ext=mp4]'})
 y = x.download([url])
