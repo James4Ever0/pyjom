c525b95c7c3b34fdece5dbf19ab53941f2083c96
GitJournal <app@gitjournal.io>
2022-09-22 23:05:07 +0800

    [2022-09-22 15:05:04]
    FILE:
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_search_api_modification_section_params_get_related_videos/recursive_search_bilibili_test.py
+++ tests/bilibili_search_api_modification_section_params_get_related_videos/recursive_search_bilibili_test.py
@@ -294,8 +294,8 @@ def getBilibiliPostMetadataForDogCat(sleepTime=2):
                         if len(bgm_list) > 3:
                             # time to yield something.
                             # detect this thing!
-                            filtered_cover_list = []
-                            mCover = random.choice(filtered_cover_list) # what is this cover list?
+                            # filtered_cover_list = []
+                            # mCover = random.choice(filtered_cover_list) # what is this cover list?
                             reference_histogram_cover = random.choice(cover_list)
                             mTagSeries = randomChoiceTagList(
                                 tag_list
