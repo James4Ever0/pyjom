a1c98f383569bacf890ea2ad8e8ec66e4a33f60e
GitJournal <app@gitjournal.io>
2022-09-22 12:23:40 +0800

    [2022-09-22 04:23:39]
    FILE:
    tests/bilibili_search_api_modification_section_params_get_related_videos/recursive_search_bilibili_test.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_search_api_modification_section_params_get_related_videos/recursive_search_bilibili_test.py
+++ tests/bilibili_search_api_modification_section_params_get_related_videos/recursive_search_bilibili_test.py
@@ -32,3 +32,12 @@ print(metatopicString)
 from bilibili_api import sync, search
 
 BSP = search.bilibiliSearchParams()
+
+
+# result = sync(
+#     search.search(
+#         keyword="汪汪",
+#         params={"tids": BSP.all.tids.动物圈.tid, "duration": BSP.all.duration._10分钟以下},
+#         page=1
+#     )
+# )
