130e10817f9db808e6a51eecf536b34928644348
GitJournal <app@gitjournal.io>
2022-09-22 10:17:05 +0800

    [2022-09-22 02:17:01]
    FILE:
    tests/bilibili_search_api_modification_section_params_get_related_videos/searchDataParser.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_search_api_modification_section_params_get_related_videos/searchDataParser.py
+++ tests/bilibili_search_api_modification_section_params_get_related_videos/searchDataParser.py
@@ -35,7 +35,7 @@ def extractLinks(description, extract_bgm=True):
     if extract_bgm:
         for line in desc_without_link_per_line:
             bgmCandidateTemplates = ["{}：", "{} ","{}:"]
-            fixers = [x ]
+            fixers = [x.format("") for x in bgmCandidateTemplates]
             bgmCandidates = [x.format('bgm') for x in bgmCandidateTemplates]
             for candidate in bgmCandidates:
                 if line.lower().startswith(candidate):
