3487847c7c5719d1b9363437c6258fcbee903621
GitJournal <app@gitjournal.io>
2022-09-22 18:10:50 +0800

    [2022-09-22 10:10:42]
    FILE:
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_search_api_modification_section_params_get_related_videos/recursive_search_bilibili_test.py
+++ tests/bilibili_search_api_modification_section_params_get_related_videos/recursive_search_bilibili_test.py
@@ -48,8 +48,8 @@ from pyjom.modules.topicGenerator.onlineTopicGenerator import getMetaTopicString
 from bilibili_api import sync, search
 from lazero.utils.tools import flattenUnhashableList  # one of my classic methods
 
-def filterTitleWithCoreTopicList():
-    
+def filterTitleListWithCoreTopicList(titleList):
+
 
 def randomChoiceTagList(tag_list):
     import random
