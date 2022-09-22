5b1a9127469a19de34129e50566cf46d0af716fe
GitJournal <app@gitjournal.io>
2022-09-23 01:01:55 +0800

    [2022-09-22 17:01:52]
    FILE:
    tests/unittest_extract_cat_cover_from_video.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_extract_cat_cover_from_video.py
+++ tests/unittest_extract_cat_cover_from_video.py
@@ -27,4 +27,5 @@ with tmpfile(path=path) as TF:
         mDetections.sort(key=lambda x: -x['confidence']) # select the best one.
         if len(mDetections)>0:
             best_confidence = mDetections[0]['confidence']
-            if (best_confidence, confidence_threshold)
+            if checkMinMaxDict(best_confidence, confidence_threshold):
+                
