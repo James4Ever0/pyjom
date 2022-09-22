ac2e102af081d1437515e6bbfa617da176f691d4
GitJournal <app@gitjournal.io>
2022-09-22 15:56:54 +0800

    [2022-09-22 07:56:51]
    FILE:
    tests/bilibili_search_api_modification_section_params_get_related_videos/recursive_search_bilibili_test.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_search_api_modification_section_params_get_related_videos/recursive_search_bilibili_test.py
+++ tests/bilibili_search_api_modification_section_params_get_related_videos/recursive_search_bilibili_test.py
@@ -55,6 +55,7 @@ metatopicString = getMetaTopicString(dog_metatopic)
 import random
 getDogTid = lambda: random.choice([BSP.all.tids.动物圈.tid,BSP.all.tids.动物圈.汪星人])
 getCatTid = lambda: random.choice([BSP.all.tids.动物圈.tid,BSP.all.tids.动物圈.喵星人])
+
 getRandomPage = lambda: random.randint(1,50)
 print(metatopicString)
 
