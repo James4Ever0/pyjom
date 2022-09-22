fd57c6666032791bcf22675ea592a325ae91787b
GitJournal <app@gitjournal.io>
2022-09-23 00:25:49 +0800

    [2022-09-22 16:25:45]
    FILE:
    tests/unittest_extract_cat_cover_from_video.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_extract_cat_cover_from_video.py
+++ tests/unittest_extract_cat_cover_from_video.py
@@ -10,4 +10,4 @@ from pyjom.videotoolbox import getVideoFrameIteratorWithFPS
 with tmpfile(path=path) as TF:
     x = yt_dlp.YoutubeDL({"outtmpl":path,'format':'[ext=mp4]'})
     y = x.download([videoLink])
-    
+    for frame in getVideoFrameIteratorWithFPS(path, start=):
