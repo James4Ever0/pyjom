90f9a3b966043f727b26738d809836a7c3ad101d
GitJournal <app@gitjournal.io>
2022-09-22 23:14:05 +0800

    [2022-09-22 15:13:59]
    FILE:
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_photo_histogram_match_0.2.py
+++ tests/unittest_photo_histogram_match_0.2.py
@@ -28,10 +28,6 @@ def str2bool(v):
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-s", "--source", required = True,
-	help = "Path to the source image")
-ap.add_argument("-t", "--target", required = True,
-	help = "Path to the target image")
 ap.add_argument("-c", "--clip", type = str2bool, default = 't',
 	help = "Should np.clip scale L*a*b* values before final conversion to BGR? "
 		   "Approptiate min-max scaling used if False.")
