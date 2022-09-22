eb7213cbfc2f0703a85009925873a202c99cb4ef
GitJournal <app@gitjournal.io>
2022-09-22 12:37:30 +0800

    [2022-09-22 04:37:22]
    FILE:
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/contentProducing/producerTemplates.py
+++ pyjom/modules/contentProducing/producerTemplates.py
@@ -543,6 +543,7 @@ def petsWithMusicOnlineProducer(
         )
         for config in configs:
             parsed_result = getMusicInfoParsed(config)
+            render_ass = config'render_ass']
             # print(parsed_result)
             # breakpoint()
             # we only have one song here. you fucking know that?
