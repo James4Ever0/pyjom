058297b853480127ab54cec78f845b8c23a425dc
GitJournal <app@gitjournal.io>
2022-09-22 16:53:12 +0800

    [2022-09-22 08:53:09]
    FILE:
    tests/bilibili_search_api_modification_section_params_get_related_videos/recursive_search_bilibili_test.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_search_api_modification_section_params_get_related_videos/recursive_search_bilibili_test.py
+++ tests/bilibili_search_api_modification_section_params_get_related_videos/recursive_search_bilibili_test.py
@@ -79,6 +79,7 @@ def getBilibiliPostMetadataForDogCat():
     getTid = {"dog": getDogTid, "cat": getCatTid}
 
     getRandomPage = lambda: random.randint(1, 5)  # broad range!
+    # getRandomPage = lambda: random.randint(1, 50)  # broad range!
     while True:
         try:
 
