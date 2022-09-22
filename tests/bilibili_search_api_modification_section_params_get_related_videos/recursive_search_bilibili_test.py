8544ea08f0fd38e5c9adf72f907e3fdea641d662
GitJournal <app@gitjournal.io>
2022-09-22 14:34:51 +0800

    [2022-09-22 06:34:46]
    FILE:
    tests/bilibili_search_api_modification_section_params_get_related_videos/recursive_search_bilibili_test.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_search_api_modification_section_params_get_related_videos/recursive_search_bilibili_test.py
+++ tests/bilibili_search_api_modification_section_params_get_related_videos/recursive_search_bilibili_test.py
@@ -103,7 +103,7 @@ def updateMyLists(videoMetadata,duration_limit = {"min":70, 'max':5*60},play_lim
             cover_list += [cover]
             title_list += [title] # this for topic modeling?
             description_list += [description]
-            tag_list += tags # this?
+            tag_list += [tags] # are you sure? this will make the tag
 
 def updateMyListsWithIterable(iterable,duration_limit = {"min":70, 'max':5*60},play_limit = {"min": 10000}):
     for videoMetadata in iterable:
