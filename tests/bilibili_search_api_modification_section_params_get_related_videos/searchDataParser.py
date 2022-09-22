fbcbabecde8d33d0fc4609cee52f364f40eaf5da
GitJournal <app@gitjournal.io>
2022-09-22 16:16:58 +0800

    [2022-09-22 08:16:54]
    FILE:
    tests/bilibili_search_api_modification_section_params_get_related_videos/searchDataParser.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_search_api_modification_section_params_get_related_videos/searchDataParser.py
+++ tests/bilibili_search_api_modification_section_params_get_related_videos/searchDataParser.py
@@ -103,7 +103,10 @@ def removeTitleTags(title, author):
         title_tags = [x.lstrip("【").rstrip("】").strip() for x in title_tags]
         title_tags = [x for x in title_tags if len(x)>0]
         for title_tag in title_tags:
-            if 
+            for keyword in author_keywords:
+                if len(keyword)>1:
+                    if keyword in title_tag:
+                        
 
     return title, title_tags
 
