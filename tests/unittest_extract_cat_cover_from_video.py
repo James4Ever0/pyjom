d29372b13087747cb902a689f9a0816ebf7317ba
GitJournal <app@gitjournal.io>
2022-09-23 01:16:33 +0800

    [2022-09-22 17:16:31]
    FILE:
    tests/unittest_extract_cat_cover_from_video.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_extract_cat_cover_from_video.py
+++ tests/unittest_extract_cat_cover_from_video.py
@@ -7,8 +7,9 @@ import yt_dlp
 
 path = "/dev/shm/testVideo.mp4"
 from test_commons import *
+import cv2
 from pyjom.videotoolbox import getVideoFrameSampler
-from pyjom.imagetoolbox import bezierPaddleHubResnet50ImageDogCatDetector, getImageTextAreaRatio
+from pyjom.imagetoolbox import bezierPaddleHubResnet50ImageDogCatDetector, getImageTextAreaRatio, imageFourCornersInpainting
 from pyjom.commons import checkMinMaxDict
 
 dog_or_cat = "dog"
@@ -22,11 +23,23 @@ with tmpfile(path=path) as TF:
     from caer.video.frames_and_fps import get_duration
     duration = get_duration(path)
     mSampleSize = int(duration/2) # fps = 0.5 or something?
+    processed_frame = None
     for frame in getVideoFrameSampler(path, -1,-1,sample_size=mSampleSize,iterate=True):
         text_area_ratio = getImageTextAreaRatio(frame)
-        detections = bezierPaddleHubResnet50ImageDogCatDetector(frame)
-        mDetections = [x for x in detections if x['identity'] == dog_or_cat]
-        mDetections.sort(key=lambda x: -x['confidence']) # select the best one.
-        if len(mDetections)>0:
-            best_confidence = mDetections[0]['confidence']
-            if checkMinMaxDict(best_confidence, confidence_threshold):
+        if checkMinMaxDict(text_area_ratio,text_area_threshold):
+            detections = bezierPaddleHubResnet50ImageDogCatDetector(frame)
+            mDetections = [x for x in detections if x['identity'] == dog_or_cat]
+            mDetections.sort(key=lambda x: -x['confidence']) # select the best one.
+            if len(mDetections)>0:
+                best_confidence = mDetections[0]['confidence']
+                if checkMinMaxDict(best_confidence, confidence_threshold):
+                    target = getImageTextAreaRatio(frame, inpaint=True)
+                    target = imageFourCornersInpainting(target)
+                    processed_frame = target
+                    break
+    if processed_frame is not None:
+        print("COVER IMAGE FOUND!")
+        cv2.imshow("image", processed_frame)
+        cv2.waitKey(0)
+    else:
+        print("COVER NOT FOUND FOR %s" % videoLink)
