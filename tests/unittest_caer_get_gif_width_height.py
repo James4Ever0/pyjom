95aded954a06de23a471d6aa636ee5d2a348fe67
GitJournal <app@gitjournal.io>
2022-09-22 21:52:58 +0800

    [2022-09-22 13:52:54]
    FILE:
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_caer_get_gif_width_height.py
+++ tests/unittest_caer_get_gif_width_height.py
@@ -3,3 +3,4 @@ from caer.video.frames_and_fps import get_res
 videoPath = "/root/Desktop/works/pyjom/samples/video/cat_invalid_eye_rolling.gif"
 
 width, height = get_res(videoPath)
+print(width, height)
