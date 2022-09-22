60670e582db9a35968d19ce1a8837208be2bfcca
GitJournal <app@gitjournal.io>
2022-09-22 12:00:31 +0800

    [2022-09-22 04:00:30]
    FILE:
    tests/bilibili_search_api_modification_section_params_get_related_videos/searchDataParser.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_search_api_modification_section_params_get_related_videos/searchDataParser.py
+++ tests/bilibili_search_api_modification_section_params_get_related_videos/searchDataParser.py
@@ -233,7 +233,7 @@ def parseVideoInfo(videoInfo, debug=False):
             # http://i2.hdslb.com/bfs/archive/c5a0d18ee077fb6a4ac0970ccb0a3788e137d14f.jpg
     return primaryVideoInfo, secondaryVideoInfoList
 
-def parseVideoRelated(videoRelatedData, debug=False, generator=True):
+def parseVideoRelated(videoRelatedData, debug=False):
     data = videoRelatedData
     # if not generator:
     #     return generatorToList(parseVideoRelated(data, debug=debug,generator=True))
