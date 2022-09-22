72e07359f34689b1534bb553105a08fdab053b93
GitJournal <app@gitjournal.io>
2022-09-22 12:56:52 +0800

    [2022-09-22 04:56:48]
    FILE:
    pyjom/modules/contentProducing/producerTemplates.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/contentProducing/producerTemplates.py
+++ pyjom/modules/contentProducing/producerTemplates.py
@@ -448,6 +448,11 @@ def petsWithMusicProducer(filtered_info, meta_info, config={}):
     # what is this config? how the fuck we can arrange it?
     # config = {"music":{"filepath":"","lyric_path":""},"font":{"filepath":"","fontsize":30}, "policy":{"some_policy_name":{}},"meta":{"maxtime":3, "mintime":1}}
     # how to auto-warp the AAS subtitle?
+    musicPath = config.get('music',"")
+    from pyjom.commons import corruptMediaFilter
+    report = corruptMediaFilter(musicPath)
+    if not report:
+        return False
     
     (
         music,
