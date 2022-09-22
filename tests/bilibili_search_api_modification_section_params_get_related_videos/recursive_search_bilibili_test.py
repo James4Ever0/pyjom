c46faa0691325d72e7deb469d70b2dbea727230c
GitJournal <app@gitjournal.io>
2022-09-22 14:36:25 +0800

    [2022-09-22 06:36:22]
    FILE:
    tests/bilibili_search_api_modification_section_params_get_related_videos/recursive_search_bilibili_test.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_search_api_modification_section_params_get_related_videos/recursive_search_bilibili_test.py
+++ tests/bilibili_search_api_modification_section_params_get_related_videos/recursive_search_bilibili_test.py
@@ -129,7 +129,8 @@ if len(bvid_list)>0:
 
 # now what do you want? suggested keywords?
 suggested_queries = sync(search.get_suggest_keywords(keyword = metatopicString))
-
+if( suggested_queries) !=list: 
+    suggested_queries = []
 # now we need to collect the keywords.
 # notice: we can only update this for selected topic like cat or dog. these keywords might not be shared.
 
