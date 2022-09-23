21553f4bcd511a6b2053a2030e79d0495938774b
GitJournal <app@gitjournal.io>
2022-09-23 16:44:51 +0800

    [2022-09-23 08:44:50]
    FILE:
    tests/unittest_dog_cat_detections.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_dog_cat_detections.py
+++ tests/unittest_dog_cat_detections.py
@@ -85,7 +85,7 @@ if count>=1:
     x_f, y_f, w_f, h_f = framework_XYWH
     croppedImageCover = img[y_f:y_f+h_f,x_f:x_f+w_f,:]
     # resize image
-    croppedImageCover = cv2.resize()
+    croppedImageCoverResized = cv2.resize(cropppedImageCover, ())
     # print(selected_col_dict)
     # print(count)
     # breakpoint()
