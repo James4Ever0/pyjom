e4887827dc654e3d37f8363dfc044212c05936b5
GitJournal <app@gitjournal.io>
2022-09-23 14:13:50 +0800

    [2022-09-23 06:13:46]
    FILE:
    tests/unittest_dog_cat_detections.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_dog_cat_detections.py
+++ tests/unittest_dog_cat_detections.py
@@ -9,6 +9,8 @@ localModelDir = '/root/Desktop/works/pyjom/pyjom/models/yolov5/ultralytics_yolov
 os.environ["YOLOV5_MODEL_DIR"] = '/root/Desktop/works/pyjom/pyjom/models/yolov5/' # this is strange. must be a hack in the localModelDir
 model = torch.hub.load(localModelDir, 'yolov5s',source="local")  # or yolov5m, yolov5l, yolov5x, custom
 
+dog_or_cat = "dog"
+
 # Images
 # img = '/media/root/help/pyjom/samples/image/miku_on_green.png'  # or file, Path, PIL, OpenCV, numpy, list
 # img = "/root/Desktop/works/pyjom/samples/image/dog_with_text.jpg"
