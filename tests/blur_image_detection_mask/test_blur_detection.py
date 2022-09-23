91d58d90b2f334c3831ecb1431579d68b1e24ae9
GitJournal <app@gitjournal.io>
2022-09-23 20:44:02 +0800

    [2022-09-23 12:44:02]
    FILE:
    tests/blur_image_detection_mask/test_blur_detection.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/blur_image_detection_mask/test_blur_detection.py
+++ tests/blur_image_detection_mask/test_blur_detection.py
@@ -2,8 +2,9 @@ import blur_detector
 from lazero.utils.importers import cv2_custom_build_init
 cv2_custom_build_init()
 import cv2
+imagePath = 
 if __name__ == '__main__':
-    img = cv2.imread('image_name', 0)
+    img = cv2.imread(imagePath)
     blur_map = blur_detector.detectBlur(img, downsampling_factor=4, num_scales=4, scale_start=2, num_iterations_RF_filter=3)
     cv2.imshow('ori_img', img)
     cv2.imshow('blur_map', blur_map)
