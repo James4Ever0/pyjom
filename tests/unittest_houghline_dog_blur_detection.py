56e2480886a8f7e7016a4305ae2b8b04c3ea8996
GitJournal <app@gitjournal.io>
2022-09-23 17:28:02 +0800

    [2022-09-23 09:27:57]
    FILE:
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_houghline_dog_blur_detection.py
+++ tests/unittest_houghline_dog_blur_detection.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 # command used for reading an image from the disk, cv2.imread function is used
-img1 = cv2.imread('educba.png')
+img1 = cv2.imread(imagePath)
 gray1 = cv2 *. * cvt * Color * (* img1 *, * cv2 *. * COLOR * _ * BGR2GRAY *) * *
 edges1 = cv2.Canny * (gray1, * 50 *, * 150 *, * aperture * Size * * = * * 3 *) *
 lines1 = cv2 *. * Hough * Lines(edges1, * 1, * np *. * pi * / * 180 *, * * 200)
