9bfb7448fba51f77ee2997c343ba40c34bb08c67
GitJournal <app@gitjournal.io>
2022-09-23 22:39:26 +0800

    [2022-09-23 14:39:25]
    FILE:
    tests/blur_image_detection_mask/test_blur_detection.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/blur_image_detection_mask/test_blur_detection.py
+++ tests/blur_image_detection_mask/test_blur_detection.py
@@ -3,6 +3,7 @@ cv2_custom_build_init()
 import blur_detector
 import cv2
 imagePath = "/root/Desktop/works/pyjom/samples/image/dog_blue_sky_split_line.png"
+
 if __name__ == '__main__':
     img = cv2.imread(imagePath,0)
     blur_map = blur_detector.detectBlur(img, downsampling_factor=4, num_scales=4, scale_start=2, num_iterations_RF_filter=3)
