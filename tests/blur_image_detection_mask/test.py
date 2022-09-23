468c4ad06633f99942f6813ee488cbc3c38c62dd
GitJournal <app@gitjournal.io>
2022-09-23 20:15:01 +0800

    [2022-09-23 12:15:00]
    FILE:
    tests/blur_image_detection_mask/test.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/blur_image_detection_mask/test.py
+++ tests/blur_image_detection_mask/test.py
@@ -4,11 +4,11 @@ import numpy
 import BlurDetection
 
 # img_path = raw_input("Please Enter Image Path: ")
-img_path = ""
+img_path = "/root/Desktop/works/pyjom/samples/image/dog_blue_sky_split_line.png"
 assert os.path.exists(img_path), "img_path does not exists"
 img = cv2.imread(img_path)
 img_fft, val, blurry = BlurDetection.blur_detector(img)
-print "this image {0} blurry".format(["isn't", "is"][blurry])
+print("this image {0} blurry".format(["isn't", "is"][blurry]))
 msk, val = BlurDetection.blur_mask(img)
 BlurDetection.scripts.display('img', img)
 BlurDetection.scripts.display('msk', msk)
