5c4c0b5f2fd306580f44c3e9ce98cf027bd2b5b6
GitJournal <app@gitjournal.io>
2022-09-23 15:54:00 +0800

    [2022-09-23 07:54:00]
    FILE:
    tests/unittest_dog_cat_detections.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_dog_cat_detections.py
+++ tests/unittest_dog_cat_detections.py
@@ -60,10 +60,11 @@ new_df = df.loc[(df['area_ratio'] >= area_threshold) & (df['confidence'] >= conf
 # count = new_df.count(axis=0)
 count = len(new_df)
 # print("COUNT: %d" % count)
+if count>=1:
 selected_col = new_df.iloc(0) # it is a dict-like object.
 # print(new_df)
 selected_col_dict = dict(selected_col)
-print(selected_col)
+print(selected_col_dict)
 print(count)
 breakpoint()
 # # results.save()
