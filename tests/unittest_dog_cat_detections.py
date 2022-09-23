7dd82a1068f29d2eeaf520ea0c90b4141238f8fa
GitJournal <app@gitjournal.io>
2022-09-23 15:32:58 +0800

    [2022-09-23 07:32:58]
    FILE:
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_dog_cat_detections.py
+++ tests/unittest_dog_cat_detections.py
@@ -1,28 +1,36 @@
 import torch
 import os
 from lazero.utils.importers import cv2_custom_build_init
+
 cv2_custom_build_init()
 import cv2
+
 os.environ["http_proxy"] = ""
 os.environ["https_proxy"] = ""
 
 # Model
-localModelDir = '/root/Desktop/works/pyjom/pyjom/models/yolov5/ultralytics_yolov5_master/'
+localModelDir = (
+    "/root/Desktop/works/pyjom/pyjom/models/yolov5/ultralytics_yolov5_master/"
+)
 # import os
-os.environ["YOLOV5_MODEL_DIR"] = '/root/Desktop/works/pyjom/pyjom/models/yolov5/' # this is strange. must be a hack in the localModelDir
-model = torch.hub.load(localModelDir, 'yolov5s',source="local")  # or yolov5m, yolov5l, yolov5x, custom
+os.environ[
+    "YOLOV5_MODEL_DIR"
+] = "/root/Desktop/works/pyjom/pyjom/models/yolov5/"  # this is strange. must be a hack in the localModelDir
+model = torch.hub.load(
+    localModelDir, "yolov5s", source="local"
+)  # or yolov5m, yolov5l, yolov5x, custom
 
 dog_or_cat = "dog"
 
 # Images
 # img = '/media/root/help/pyjom/samples/image/miku_on_green.png'  # or file, Path, PIL, OpenCV, numpy, list
 # img = "/root/Desktop/works/pyjom/samples/image/dog_with_text.jpg"
-imgPath='/root/Desktop/works/pyjom/samples/image/dog_blue_sky.png'
+imgPath = "/root/Desktop/works/pyjom/samples/image/dog_blue_sky.png"
 
 img = cv2.imread(imgPath)
 
 defaultHeight, defaultWidth = img.shape[:2]
-total_area = defaultHeight*defaultWidth
+total_area = defaultHeight * defaultWidth
 
 # Inference
 results = model(img)
@@ -34,14 +42,16 @@ animal_detection_dataframe = results.pandas().xyxy[0]
 # results.show()
 # # results.print() # or .show(),
 
-area= (animal_detection_dataframe['xmax']-animal_detection_dataframe['xmin'])*(animal_detection_dataframe['ymax']-animal_detection_dataframe['ymin'])
+area = (animal_detection_dataframe["xmax"] - animal_detection_dataframe["xmin"]) * (
+    animal_detection_dataframe["ymax"] - animal_detection_dataframe["ymin"]
+)
 
-animal_detection_dataframe['area_ratio'] = area/total_area
+animal_detection_dataframe["area_ratio"] = area / total_area
 
 area_threshold = 0
-confidence_threshold = 0 # this is image quality maybe.
+confidence_threshold = 0  # this is image quality maybe.
 
-y_expansion_rate = 0.1 # to make the starting point on y axis less "headless"
+y_expansion_rate = 0.1  # to make the starting point on y axis less "headless"
 
 # # results.save()
 # # # print(type(results),dir(results))
