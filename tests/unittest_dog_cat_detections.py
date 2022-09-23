5bd94ad8f552874c04faf572231ab28c0e42ab66
GitJournal <app@gitjournal.io>
2022-09-23 16:41:55 +0800

    [2022-09-23 08:41:51]
    FILE:
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_dog_cat_detections.py
+++ tests/unittest_dog_cat_detections.py
@@ -78,7 +78,7 @@ if count>=1:
 
     y0_altered = max(int(y0-(y1-y0)*y_expansion_rate),0)
     height_current = min((y1-y0_altered),allowedHeight) # reasonable?
-    width_current = min(int((height_current/defaultCropHeight)*defaultCropWidth), defaultWidth) # just for safety. not for 
+    width_current = min(int((height_current/defaultCropHeight)*defaultCropWidth), defaultWidth) # just for safety. not for mathematical accuracy.
     # height_current = min(allowedHeight, int((width_current/defaultCropWidth)*defaultCropHeight))
     x0_framework = max((x1-width_current),0)
     # print(selected_col_dict)
