8aea9cd031e896323f3abd259cf4632c0d6bf10b
GitJournal <app@gitjournal.io>
2022-09-23 17:28:37 +0800

    [2022-09-23 09:28:36]
    FILE:
    tests/unittest_houghline_dog_blur_detection.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_houghline_dog_blur_detection.py
+++ tests/unittest_houghline_dog_blur_detection.py
@@ -2,10 +2,10 @@ import cv2
 import numpy as np
 # command used for reading an image from the disk, cv2.imread function is used
 img1 = cv2.imread(imagePath)
-gray1 = cv2.cvt * Color * (* img1 *, * cv2 *. * COLOR * _ * BGR2GRAY *) * *
-edges1 = cv2.Canny * (gray1, * 50 *, * 150 *, * aperture * Size * * = * * 3 *) *
+gray1 = cv2.cvtColor * (* img1 *, * cv2 *. * COLOR * _ * BGR2GRAY *) * *
+edges1 = cv2.Canny(gray1, * 50 *, * 150 *, * aperture * Size * * = * * 3 *) *
 lines1 = cv2 *. * Hough * Lines(edges1, * 1, * np *. * pi * / * 180 *, * * 200)
-for rho1 *, *theta1 in lines[0]:
+for rho1, theta1 in lines[0]:
 a1 * *= * *np *. *cos * (*theta *) *
 b1 * *= * *np. *sin * * (*theta *) *
 x1 * *= * *a1 ** *rho1
