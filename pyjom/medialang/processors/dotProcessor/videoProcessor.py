3ae44230dc2d8f7adecd3e5a3e0a55347954f2d0
GitJournal <app@gitjournal.io>
2022-09-24 00:23:18 +0800

    [2022-09-23 16:23:17]
    FILE:
    pyjom/medialang/processors/dotProcessor/videoProcessor.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/medialang/processors/dotProcessor/videoProcessor.py
+++ pyjom/medialang/processors/dotProcessor/videoProcessor.py
@@ -24,7 +24,7 @@ def ffmpegVideoPreProductionFilter(
     start=None,
     end=None,
     cachePath=None,
-    filters:List[Literal["minterpolate", "removegrain", "bilateral",'randomFlip','superResolution',"pipCrop", "textRemoval", "logoRemoval"]]=["pipCrop", "textRemoval", "logoRemoval",'randomFlip','superResolution','minterpolate'],
+    filters:List[Literal["minterpolate", "removegrain", "bilateral",'randomFlip','superResolution',"pipCrop", "textRemoval", "logoRemoval"]]=["pipCrop", "textRemoval", "logoRemoval",'randomFlip','superResolution','minterpolate','removegrain'],
     preview=True,
     # padding=True,
     paddingBlur=True,
