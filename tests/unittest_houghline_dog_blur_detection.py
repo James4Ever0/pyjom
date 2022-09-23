baaa2d3fc98820eb5e3b2fc3edd6436b0bd80c4b
GitJournal <app@gitjournal.io>
2022-09-23 19:19:09 +0800

    [2022-09-23 11:19:08]
    FILE:
    tests/unittest_houghline_dog_blur_detection.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_houghline_dog_blur_detection.py
+++ tests/unittest_houghline_dog_blur_detection.py
@@ -5,8 +5,8 @@ import numpy as np
 # command used for reading an image from the disk, cv2.imread function is used
 imagePath = "/root/Desktop/works/pyjom/samples/image/dog_blue_sky_split_line.png"
 img1 = cv2.imread(imagePath)
-gray1 = cv2.cvtColor(img1,cv2.COLOR_BGR2GRAY)
-edges1 = cv2.Canny(gray1,50,150,apertureSize=1)
+# gray1 = cv2.cvtColor(img1,cv2.COLOR_BGR2GRAY)
+# edges1 = cv2.Canny(gray1,50,150,apertureSize=1)
 
 cv2.imshow("EDGE",edges1)
 cv2.waitKey(0)
