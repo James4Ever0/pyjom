eb756b4bd5179fd024c403ceb5edf6a9ceaf7b52
GitJournal <app@gitjournal.io>
2022-09-24 00:09:48 +0800

    [2022-09-23 16:09:44]
    FILE:
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_ffmpeg_cropdetect_for_image_parse_log.py
+++ tests/unittest_ffmpeg_cropdetect_for_image_parse_log.py
@@ -3,7 +3,7 @@ mediaPath = "/root/Desktop/works/pyjom/samples/image/dog_blue_sky_split_line.png
 # ffmpeg -loop 1 -i /root/Desktop/works/pyjom/samples/image/dog_blue_sky_split_line.png -t 15 -vf cropdetect -f null -
 
 stdout, stderr = (
-            ffmpeg.input(mediaPath, )
+            ffmpeg.input(mediaPath, loop=1)
             .output("null", f="null")
             .run(capture_stdout=True, capture_stderr=True)
         )
