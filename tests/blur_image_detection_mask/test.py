0a6ad4b10c54eb120c910564d6af65295d5a0689
GitJournal <app@gitjournal.io>
2022-09-23 23:06:36 +0800

    [2022-09-23 15:06:34]
    FILE:
    tests/blur_image_detection_mask/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/blur_image_detection_mask/test.py
+++ tests/blur_image_detection_mask/test.py
@@ -1,8 +1,10 @@
 import os
 from lazero.utils.importers import cv2_custom_build_init
+
 cv2_custom_build_init()
 import cv2
 import numpy
+
 # import logger
 import BlurDetection
 
@@ -12,19 +14,21 @@ assert os.path.exists(img_path), "img_path does not exists"
 img = cv2.imread(img_path)
 img_fft, val, blurry = BlurDetection.blur_detector(img)
 print("this image {0} blurry".format(["isn't", "is"][blurry]))
-msk, result, blurry= BlurDetection.blur_mask(img, max_thresh=190)
+msk, result, blurry = BlurDetection.blur_mask(img, max_thresh=190)
+
 
 def display(title, img, max_size=200000):
-    assert isinstance(img, numpy.ndarray), 'img must be a numpy array'
-    assert isinstance(title, str), 'title must be a string'
-    scale = numpy.sqrt(min(1.0, float(max_size)/(img.shape[0]*img.shape[1])))
-    print('image is being scaled by a factor of {0}'.format(scale))
-    shape = (int(scale*img.shape[1]), int(scale*img.shape[0]))
+    assert isinstance(img, numpy.ndarray), "img must be a numpy array"
+    assert isinstance(title, str), "title must be a string"
+    scale = numpy.sqrt(min(1.0, float(max_size) / (img.shape[0] * img.shape[1])))
+    print("image is being scaled by a factor of {0}".format(scale))
+    shape = (int(scale * img.shape[1]), int(scale * img.shape[0]))
     img = cv2.resize(img, shape)
     cv2.imshow(title, img)
 
+
 # BlurDetection.scripts.display('img', img)
-display('img', img)
-display('msk', msk)
+display("img", img)
+display("msk", msk)
 # BlurDetection.scripts.display('msk', msk)
 cv2.waitKey(0)
