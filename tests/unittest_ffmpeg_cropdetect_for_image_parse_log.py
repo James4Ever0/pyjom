4c8a7121f68fface8b2c696eff88a3e0ec3385ad
GitJournal <app@gitjournal.io>
2022-09-24 00:11:31 +0800

    [2022-09-23 16:11:30]
    FILE:
    tests/unittest_ffmpeg_cropdetect_for_image_parse_log.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_ffmpeg_cropdetect_for_image_parse_log.py
+++ tests/unittest_ffmpeg_cropdetect_for_image_parse_log.py
@@ -1,11 +1,13 @@
 import ffmpeg
+
 # mediaPath = "/root/Desktop/works/pyjom/samples/image/dog_blue_sky_split_line.png"
-mediaPath = "/root/Desktop/works/pyjom/samples/image/dog_with_black_borders.png" # use the image with black background.
+mediaPath = "/root/Desktop/works/pyjom/samples/image/dog_with_black_borders.png"  # use the image with black background.
 # ffmpeg -loop 1 -i /root/Desktop/works/pyjom/samples/image/dog_blue_sky_split_line.png -t 15 -vf cropdetect -f null -
 
 stdout, stderr = (
-            ffmpeg.input(mediaPath, loop=1,t=15).filter("cropdetect")
-            .output("null", f="null")
-            .run(capture_stdout=True, capture_stderr=True)
-        )
+    ffmpeg.input(mediaPath, loop=1, t=15)
+    .filter("cropdetect")
+    .output("null", f="null")
+    .run(capture_stdout=True, capture_stderr=True)
+)
 stderr_lower = stderr.decode("utf-8")
