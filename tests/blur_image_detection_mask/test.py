edee84ec6a71dc7e7359dc32ed11866de9c2e341
GitJournal <app@gitjournal.io>
2022-09-23 23:33:44 +0800

    [2022-09-23 15:33:39]
    FILE:
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/blur_image_detection_mask/test.py
+++ tests/blur_image_detection_mask/test.py
@@ -13,6 +13,7 @@ import BlurDetection
 # img_path = "/root/Desktop/works/pyjom/samples/image/blur_sample.webp"
 # img_path = "/root/Desktop/works/pyjom/samples/image/blur_sample_2.webp"
 img_path="/root/Desktop/works/pyjom/samples/image/dog_with_black_borders.png"
+# ffmpeg -loop 1 -i /root/Desktop/works/pyjom/samples/image/dog_blue_sky_split_line.png -t 15 -vf cropdetect -f null -
 # img_path="/root/Desktop/works/pyjom/samples/image/husky_cry.png"
 assert os.path.exists(img_path), "img_path does not exists"
 img = cv2.imread(img_path)
