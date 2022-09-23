3e9a8d543f59d0e0ce21d6f325ddbf4532877655
GitJournal <app@gitjournal.io>
2022-09-23 14:43:44 +0800

    [2022-09-23 06:43:40]
    FILE:
    tests/unittest_dog_cat_detections.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_dog_cat_detections.py
+++ tests/unittest_dog_cat_detections.py
@@ -28,6 +28,8 @@ animal_detection_dataframe = results.pandas().xyxy[0]
 
 area_ratio = (animal_detection_dataframe['xmax']-animal_detection_dataframe['xmin'])*(animal_detection_dataframe['ymax']-animal_detection_dataframe['ymin'])
 
+animal_detection_dataframe[]
+
 area_threshold = 0
 confidence_threshold = 0 # this is image quality maybe.
 
