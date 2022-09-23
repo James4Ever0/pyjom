6755acc581d109510161c0d373c7d4c2f10a2072
GitJournal <app@gitjournal.io>
2022-09-23 18:56:56 +0800

    [2022-09-23 10:56:55]
    FILE:
    pyjom/imagetoolbox.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/imagetoolbox.py
+++ pyjom/imagetoolbox.py
@@ -67,7 +67,7 @@ from typing import Literal
 def partial_blur(image0,mask,kernel=(200,200)):
     # need improvement. malnly the boundary.
     height, width = image0.shape[:2]
-    kernel_w = width/
+    kernel_w =( width/100)
     kernel = (kernel_w, kernel_h)
     mask_total = mask
     inv_mask_total = 255-mask_total
