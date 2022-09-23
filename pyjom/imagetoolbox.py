d48e9cca798742e694a9578cf263517c43e6abce
GitJournal <app@gitjournal.io>
2022-09-23 19:05:54 +0800

    [2022-09-23 11:05:53]
    FILE:
    pyjom/imagetoolbox.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/imagetoolbox.py
+++ pyjom/imagetoolbox.py
@@ -65,7 +65,7 @@ def getImageTextAreaRecognized(
 from typing import Literal, Tuple
 
 
-def partial_blur(image0, mask, kernel:[None, Tuple[int, int]]=None):
+def partial_blur(image0, mask, kernel:None, Tuple[int, int]]=None):
     # need improvement. malnly the boundary.
     if kernel is None:
         height, width = image0.shape[:2]
