9a4e1499866515d188d30a878c2ab1c1a385c891
GitJournal <app@gitjournal.io>
2022-09-23 16:59:53 +0800

    [2022-09-23 08:59:53]
    FILE:
    tests/unittest_dog_cat_detections.py
    
    DIR:
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_dog_cat_detections.py
+++ tests/unittest_dog_cat_detections.py
@@ -48,50 +48,65 @@ area = (animal_detection_dataframe["xmax"] - animal_detection_dataframe["xmin"])
 
 animal_detection_dataframe["area_ratio"] = area / total_area
 
-area_threshold = 0.08 # min area?
+area_threshold = 0.08  # min area?
 confidence_threshold = 0.7  # this is image quality maybe.
 
 y_expansion_rate = 0.03  # to make the starting point on y axis less "headless"
 
 df = animal_detection_dataframe
 
-new_df = df.loc[(df['area_ratio'] >= area_threshold) & (df['confidence'] >= confidence_threshold) & (df['name'] == dog_or_cat)].sort_values(by=['confidence']) # this one is for 0.13
+new_df = df.loc[
+    (df["area_ratio"] >= area_threshold)
+    & (df["confidence"] >= confidence_threshold)
+    & (df["name"] == dog_or_cat)
+].sort_values(
+    by=["confidence"]
+)  # this one is for 0.13
 
 # count = new_df.count(axis=0)
 count = len(new_df)
 # print("COUNT: %d" % count)
 
-defaultCropWidth, defaultCropHeight = 1920 , 1080
+defaultCropWidth, defaultCropHeight = 1920, 1080
 # this is just to maintain the ratio.
 
 # you shall find the code elsewhere?
 
-allowedHeight = min(int(defaultWidth/defaultCropWidth*defaultHeight), defaultHeight)
+allowedHeight = min(int(defaultWidth / defaultCropWidth * defaultHeight), defaultHeight)
 
-if count>=1:
-    selected_col = new_df.iloc[0] # it is a dict-like object.
+if count >= 1:
+    selected_col = new_df.iloc[0]  # it is a dict-like object.
     # print(new_df)
     # breakpoint()
     selected_col_dict = dict(selected_col)
     # these are floating point shits.
     # {'xmin': 1149.520263671875, 'ymin': 331.6445007324219, 'xmax': 1752.586181640625, 'ymax': 1082.3826904296875, 'confidence': 0.9185908436775208, 'class': 16, 'name': 'dog', 'area_ratio': 0.13691652620239364}
-    x0, y0, x1, y1 = [int(selected_col[key]) for key in ['xmin','ymin','xmax','ymax']]
+    x0, y0, x1, y1 = [
+        int(selected_col[key]) for key in ["xmin", "ymin", "xmax", "ymax"]
+    ]
 
-    y0_altered = max(int(y0-(y1-y0)*y_expansion_rate),0)
-    height_current = min((y1-y0_altered),allowedHeight) # reasonable?
-    width_current = min(int((height_current/defaultCropHeight)*defaultCropWidth), defaultWidth) # just for safety. not for mathematical accuracy.
+    y0_altered = max(int(y0 - (y1 - y0) * y_expansion_rate), 0)
+    height_current = min((y1 - y0_altered), allowedHeight)  # reasonable?
+    width_current = min(
+        int((height_current / defaultCropHeight) * defaultCropWidth), defaultWidth
+    )  # just for safety. not for mathematical accuracy.
     # height_current = min(allowedHeight, int((width_current/defaultCropWidth)*defaultCropHeight))
     # (x1+x0)/2-width_current/2
     import random
-    x0_framework =random.randint(max((x1-width_current),0), min((x0+width_current), defaultWidth))
-    framework_XYWH = (x0_framework,y0_altered, width_current, height_current)
+
+    x0_framework = random.randint(
+        max((x1 - width_current), 0), min((x0 + width_current), defaultWidth)
+    )
+    framework_XYWH = (x0_framework, y0_altered, width_current, height_current)
     x_f, y_f, w_f, h_f = framework_XYWH
-    croppedImageCover = img[y_f:y_f+h_f,x_f:x_f+w_f,:]
+    croppedImageCover = img[y_f : y_f + h_f, x_f : x_f + w_f, :]
     # breakpoint()
     # resize image
-    croppedImageCoverResized = cv2.resize(croppedImageCover, (defaultCropWidth, defaultCropHeight))
-    cv2.imshow("CROPPED IMAGE COVER",croppedImageCover)
-    cv2.imshow("CROPPED IMAGE COVER RESIZED",croppedImageCoverResized)
+    croppedImageCoverResized = cv2.resize(
+        croppedImageCover, (defaultCropWidth, defaultCropHeight)
+    )
+    cv2.imshow("CROPPED IMAGE COVER", croppedImageCover)
+    cv2.imshow("CROPPED IMAGE COVER RESIZED", croppedImageCoverResized)
     # print(selected_col_dict)
     # print(count)
     # breakpoint()
