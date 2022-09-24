b18372d81c13f241489bcd6e39ab42a93b576230
GitJournal <app@gitjournal.io>
2022-09-24 20:58:57 +0800

    [2022-09-24 12:58:53]
    FILE:
    tests/bilibili_search_api_modification_section_params_get_related_videos/recursive_search_bilibili_test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_search_api_modification_section_params_get_related_videos/recursive_search_bilibili_test.py
+++ tests/bilibili_search_api_modification_section_params_get_related_videos/recursive_search_bilibili_test.py
@@ -314,6 +314,7 @@ def getBilibiliPostMetadataForDogCat(sleepTime=2):
                                 if cropped_image is not None:
                                     cropped_image_color_transfered = imageHistogramMatch(cropped_image, reference_histogram_cover)
                                     cropped_image_color_transfered_fliped = cv2.flip(cropped_image_color_transfered,1)
+                                    cover_target = cropped_image_color_transfered_fliped
                                 # r = requests.get(cover)
                                 # content = r.content
                                 # # corrupted or not?
