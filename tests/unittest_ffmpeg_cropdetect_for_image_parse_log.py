b77dec7971b604bf2da2a7f7fae6ceeb01a59348
GitJournal <app@gitjournal.io>
2022-09-24 12:39:36 +0800

    [2022-09-24 04:39:34]
    FILE:
    tests/unittest_ffmpeg_cropdetect_for_image_parse_log.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_ffmpeg_cropdetect_for_image_parse_log.py
+++ tests/unittest_ffmpeg_cropdetect_for_image_parse_log.py
@@ -36,7 +36,8 @@ for line in stderr_decoded.split("\n"):
     result = parse.parse(formatString,line)
     if result is not None:
         # print(result)
-        common_crops.append(cropstring)
+        cropString = "{}_{}"
+        common_crops.append(cropString)
         # breakpoint()
     # [Parsed_cropdetect_0 @ 0x56246a16cbc0] x1:360 x2:823 y1:0 y2:657 w:464 h:656 x:360 y:2 pts:3 t:0.120000 crop=464:656:360:2
     # this crop usually will never change. but let's count?
