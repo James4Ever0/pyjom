767f198da5f23f0db2f2efa2f11c9790bb7e09f5
GitJournal <app@gitjournal.io>
2022-09-24 20:43:24 +0800

    [2022-09-24 12:43:20]
    FILE:
    pyjom/imagetoolbox.py
    tests/unittest_extract_cat_cover_from_video.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_extract_cat_cover_from_video.py
+++ tests/unittest_extract_cat_cover_from_video.py
@@ -55,53 +55,6 @@ if not video_fine:
 from caer.video.frames_and_fps import get_duration
 
 
-def imageDogCatCoverCropAdvanced(
-    frame,
-    dog_or_cat="dog",
-    confidence_threshold={"min": 0.7},
-    yolov5_confidence_threshold=0.4,
-    text_area_threshold={"max": 0.2},
-    gpu=True,
-    # area_threshold = 0.2
-):
-    processed_frame = None
-    frame = imageLoader(frame)
-
-
-    detections = bezierPaddleHubResnet50ImageDogCatDetector(
-        frame, use_gpu=False
-    )  # no gpu avaliable
-    mDetections = [x for x in detections if x["identity"] == dog_or_cat]
-    mDetections.sort(key=lambda x: -x["confidence"])  # select the best one.
-    if len(mDetections) > 0:
-        best_confidence = mDetections[0]["confidence"]
-        print("BEST CONFIDENCE:", best_confidence)
-        if checkMinMaxDict(best_confidence, confidence_threshold):
-            # target = getImageTextAreaRatio(frame, inpaint=True, gpu=gpu)
-            # target = imageFourCornersInpainting(target)
-            # processed_frame = target
-            # break
-            text_area_ratio = getImageTextAreaRatio(frame)
-            # text_area_ratio = getImageTextAreaRatio(frame, gpu=gpu)
-            print("TEXT AREA RATIO", text_area_ratio)
-            # if animalCropDiagonalRect is not None:
-            if checkMinMaxDict(text_area_ratio, text_area_threshold):
-                mFrame = getImageTextAreaRatio(frame, inpaint=True)
-                mFrame = imageFourCornersInpainting(mFrame)
-                mFrame = imageCropoutBlackArea(mFrame)
-                mFrame = imageCropoutBlurArea(mFrame)
-                # cv2.imshow("PRE_FINAL_IMAGE", mFrame)
-                # cv2.waitKey(0)
-                processed_frame = imageDogCatDetectionForCoverExtraction(
-                    mFrame,
-                    dog_or_cat=dog_or_cat,
-                    confidence_threshold=yolov5_confidence_threshold,
-                    # area_threshold=0.15,
-                    crop=True,
-                    debug=True,
-                )
-    return processed_frame
-
 
 duration = get_duration(path)
 mSampleSize = int(duration / 2)  # fps = 0.5 or something?
