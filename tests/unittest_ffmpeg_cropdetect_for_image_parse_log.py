6612b9f0fd3e3c394ffdb4b69520fe09abdbbf50
GitJournal <app@gitjournal.io>
2022-09-24 12:38:24 +0800

    [2022-09-24 04:38:23]
    FILE:
    tests/unittest_ffmpeg_cropdetect_for_image_parse_log.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_ffmpeg_cropdetect_for_image_parse_log.py
+++ tests/unittest_ffmpeg_cropdetect_for_image_parse_log.py
@@ -4,7 +4,9 @@ import ffmpeg
 mediaPath = "/root/Desktop/works/pyjom/samples/image/dog_with_black_borders.png"  # use the image with black background.
 # ffmpeg -loop 1 -i /root/Desktop/works/pyjom/samples/image/dog_blue_sky_split_line.png -t 15 -vf cropdetect -f null -
 
+import cv2
 
+image = cv2.imread(mediaPath)
 
 stdout, stderr = (
     ffmpeg.input(mediaPath, loop=1, t=15)
