8482ae986f496179391f19611e2107fde11459bf
GitJournal <app@gitjournal.io>
2022-09-24 17:26:28 +0800

    [2022-09-24 09:26:23]
    FILE:
    tests/unittest_extract_cat_cover_from_video.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_extract_cat_cover_from_video.py
+++ tests/unittest_extract_cat_cover_from_video.py
@@ -80,6 +80,7 @@ for frame in getVideoFrameSampler(path, -1, -1, sample_size=mSampleSize, iterate
             mFrame = imageCropoutBlurArea(mFrame)
             mFrame = imageFourCornersInpainting(mFrame)
             cv2.imshow("PRE_FINAL_IMAGE", mFrame)
+            cv2.waitKey(0)
             processed_frame = imageDogCatDetectionForCoverExtraction(
                 mFrame,
                 dog_or_cat=dog_or_cat,
