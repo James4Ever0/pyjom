82e69a163053ada5611534bb860e0500838af920
GitJournal <app@gitjournal.io>
2022-09-24 15:22:38 +0800

    [2022-09-24 07:22:38]
    FILE:
    pyjom/imagetoolbox.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/imagetoolbox.py
+++ pyjom/imagetoolbox.py
@@ -580,7 +580,7 @@ def bezierPaddleHubResnet50ImageDogCatDetector(
     return detections
 
 
-def imageCropoutBlackArea(image,cropped_area_threshold = 0.1, debug=False):
+def imageCropoutBlackArea(image,cropped_area_threshold = 0.1, debug=False,max_thresh=120,max_thresh=1):
     image = imageLoader(image)
     height, width = image.shape[:2]
     total_area = height * width
