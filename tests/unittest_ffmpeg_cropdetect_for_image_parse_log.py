d45cf1ae91f0d9231bc64157ea8d4204492b2507
GitJournal <app@gitjournal.io>
2022-09-24 12:53:38 +0800

    [2022-09-24 04:53:36]
    FILE:
    tests/unittest_ffmpeg_cropdetect_for_image_parse_log.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_ffmpeg_cropdetect_for_image_parse_log.py
+++ tests/unittest_ffmpeg_cropdetect_for_image_parse_log.py
@@ -44,12 +44,13 @@ for line in stderr_decoded.split("\n"):
         common_crops.append(cropString)
     # [Parsed_cropdetect_0 @ 0x56246a16cbc0] x1:360 x2:823 y1:0 y2:657 w:464 h:656 x:360 y:2 pts:3 t:0.120000 crop=464:656:360:2
     # this crop usually will never change. but let's count?
+area = 0
 if len(common_crops) > 0:
     common_crops_count_tuple_list = [(cropString, common_crops.count(cropString)) for cropString in set(common_crops)]
     common_crops_count_tuple_list.sort(key= lambda x: -x[1])
     selected_crop_string = common_crops_count_tuple_list[0][0]
 
-    result = parse.parse('{w:d}:{h:d}:{x:d}:{y:d}', selected_crop_string)
+    result = parse.parse('{w:d}_{h:d}_{x:d}_{y:d}', selected_crop_string)
     w,h,x,y = [result[key] for key in ['w','h','x','y']]
     x1, y1 = min(x+w, width), min(y+h, height)
     if x<x1 and y<y1:
