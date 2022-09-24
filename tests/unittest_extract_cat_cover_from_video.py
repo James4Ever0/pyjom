878b8ad6a43358af3f9e3667fad6784d43f9553d
GitJournal <app@gitjournal.io>
2022-09-24 17:44:14 +0800

    [2022-09-24 09:44:10]
    FILE:
    tests/unittest_extract_cat_cover_from_video.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_extract_cat_cover_from_video.py
+++ tests/unittest_extract_cat_cover_from_video.py
@@ -76,8 +76,8 @@ for frame in getVideoFrameSampler(path, -1, -1, sample_size=mSampleSize, iterate
         print("TEXT AREA RATIO", text_area_ratio)
         # if animalCropDiagonalRect is not None:
         if checkMinMaxDict(text_area_ratio, text_area_threshold):
-            mFrame = imageFourCornersInpainting(mFrame)
             mFrame = getImageTextAreaRatio(frame, inpaint=True)
+            mFrame = imageFourCornersInpainting(mFrame)
             mFrame = imageCropoutBlackArea(mFrame)
             mFrame = imageCropoutBlurArea(mFrame)
             # cv2.imshow("PRE_FINAL_IMAGE", mFrame)
