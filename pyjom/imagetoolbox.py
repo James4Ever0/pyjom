6a2557bfdbd454658df0979089fd2b038c43006f
GitJournal <app@gitjournal.io>
2022-09-24 16:13:03 +0800

    [2022-09-24 08:13:02]
    FILE:
    pyjom/imagetoolbox.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/imagetoolbox.py
+++ pyjom/imagetoolbox.py
@@ -7,6 +7,7 @@ from lazero.utils.tools import flattenUnhashableList
 
 def imageCropWithDiagonalRectangle(image, diagonalRectangle):
     x0,y0, x1, y1 = flattenUnhashableLIst(diagonalRectangle)
+    
 
 def draw_bounding_box_with_contour(
     contours, image, area_threshold=20, debug=False
