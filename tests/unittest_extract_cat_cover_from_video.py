0054f9eaf256b2932bada078ccb74cd8fb032976
GitJournal <app@gitjournal.io>
2022-09-24 20:39:15 +0800

    [2022-09-24 12:39:12]
    FILE:
    tests/unittest_extract_cat_cover_from_video.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_extract_cat_cover_from_video.py
+++ tests/unittest_extract_cat_cover_from_video.py
@@ -55,10 +55,11 @@ if not video_fine:
 from caer.video.frames_and_fps import get_duration
 
 
-def imageDogCatCoverCropAdvanced(, dog_or_cat='dog',confidence_threshold = {"min": 0.7},
+def imageDogCatCoverCropAdvanced(frame, dog_or_cat='dog',confidence_threshold = {"min": 0.7},
 yolov5_confidence_threshold=0.4,
 text_area_threshold = {"max": 0.2},
 gpu = True):
+    imageLoader()
 
     detections = bezierPaddleHubResnet50ImageDogCatDetector(frame, use_gpu=False) # no gpu avaliable
     mDetections = [x for x in detections if x["identity"] == dog_or_cat]
