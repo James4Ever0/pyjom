65267797c995872ecabd3aaa2f32aff74c0fa94a
GitJournal <app@gitjournal.io>
2022-09-24 20:55:55 +0800

    [2022-09-24 12:55:50]
    FILE:
    tests/bilibili_search_api_modification_section_params_get_related_videos/recursive_search_bilibili_test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_search_api_modification_section_params_get_related_videos/recursive_search_bilibili_test.py
+++ tests/bilibili_search_api_modification_section_params_get_related_videos/recursive_search_bilibili_test.py
@@ -299,6 +299,8 @@ def getBilibiliPostMetadataForDogCat(sleepTime=2):
                             # detect this thing!
                             # filtered_cover_list = []
                             random.shuffle(cover_list)
+                            reference_histogram_cover = random.choice(cover_list)
+
                             for cover in cover_list:
                                 import os
                                 os.environ['http'] = ""
@@ -308,13 +310,12 @@ def getBilibiliPostMetadataForDogCat(sleepTime=2):
                                 # import requests
                                 cropped_image = imageDogCatCoverCropAdvanced(image)
                                 if cropped_image is not None:
-                                    
+                                    cropped_image = 
                                 # r = requests.get(cover)
                                 # content = r.content
                                 # # corrupted or not?
                                 # image = cv2.imdecode(content, cv2.IMREAD_COLOR)
                             # mCover = random.choice(filtered_cover_list) # what is this cover list?
-                            reference_histogram_cover = random.choice(cover_list)
                             mTagSeries = randomChoiceTagList(
                                 tag_list
                             )  # a collection of tags.
