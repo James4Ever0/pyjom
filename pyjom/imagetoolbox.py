e67efab1a30e786c04b961dcd6ea5e47e68f2158
GitJournal <app@gitjournal.io>
2022-09-24 14:58:28 +0800

    [2022-09-24 06:58:25]
    FILE:
    pyjom/imagetoolbox.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/imagetoolbox.py
+++ pyjom/imagetoolbox.py
@@ -550,7 +550,7 @@ import ffmpeg
 # it must be a existing image.
 from lazero.filesystem.temp import tmpfile
 import uuid
-path = '/dev/shm/{}.png'.format(str(uuid.uuid4()))
+path = '/dev/shm/ffmpeg/{}.png'.format(str(uuid.uuid4()))
 with tmpfile(path=path) as TF:
 mediaPath = path
 stdout, stderr = (
