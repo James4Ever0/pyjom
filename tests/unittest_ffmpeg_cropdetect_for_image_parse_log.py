bed426dfc49abda6604979c0e5d2d8d748db26ba
GitJournal <app@gitjournal.io>
2022-09-24 12:29:26 +0800

    [2022-09-24 04:29:26]
    FILE:
    tests/unittest_ffmpeg_cropdetect_for_image_parse_log.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_ffmpeg_cropdetect_for_image_parse_log.py
+++ tests/unittest_ffmpeg_cropdetect_for_image_parse_log.py
@@ -24,7 +24,7 @@ stderr_decoded = stderr.decode("utf-8")
 for line in stderr_decoded.split("\n"):
     line = line.replace("\n", "").strip()
     import parse
-    formatString='[{}] x1:{x1} x2:{x2} y1:{y1} y2:{y2} w:{w} h:{h} x:{x} y:{y} pts:{pts}'
+    formatString='[{}] x1:{x1} x2:{x2} y1:{y1} y2:{y2} w:{w} h:{h} x:{x} y:{y} pts:{pts} t:{t} crop='
     print(line)
     # [Parsed_cropdetect_0 @ 0x56246a16cbc0] x1:360 x2:823 y1:0 y2:657 w:464 h:656 x:360 y:2 pts:3 t:0.120000 crop=464:656:360:2
     # this crop usually will never change. but let's count?
