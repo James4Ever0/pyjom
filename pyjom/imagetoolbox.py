670b04f1c750aa2f49a39315733cc9461641f478
GitJournal <app@gitjournal.io>
2022-09-24 22:41:19 +0800

    [2022-09-24 14:41:16]
    FILE:
    pyjom/imagetoolbox.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/imagetoolbox.py
+++ pyjom/imagetoolbox.py
@@ -892,7 +892,7 @@ def getImageBestConfidenceWithBezierDogCatDetector(frame, dog_or_cat:Literal['do
 
 def filterImageBestConfidenceWithBezierDogCatDetector(frame, dog_or_cat:Literal['dog','cat']='dog',debug=False,confidence_threshold={"min": 0.7}):
     best_confidence = getImageBestConfidenceWithBezierDogCatDetector(frame, dog_or_cat=dog_or_cat, debug=debug)
-        if checkMinMaxDict(best_confidence, confidence_threshold):
+    return checkMinMaxDict(best_confidence, confidence_threshold)
 
 
 def imageDogCatCoverCropAdvanced(
