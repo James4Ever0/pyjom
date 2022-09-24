35cfae76daedad0ad753487a01fa551e58536299
GitJournal <app@gitjournal.io>
2022-09-24 17:04:16 +0800

    [2022-09-24 09:04:13]
    FILE:
    tests/unittest_extract_cat_cover_from_video.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_extract_cat_cover_from_video.py
+++ tests/unittest_extract_cat_cover_from_video.py
@@ -61,7 +61,7 @@ duration = get_duration(path)
 mSampleSize = int(duration / 2)  # fps = 0.5 or something?
 processed_frame = None
 for frame in getVideoFrameSampler(path, -1, -1, sample_size=mSampleSize, iterate=True):
-    animalCropDiagonalRect = imageDogCatDetectionForCoverExtraction(mFrame, dog_or_cat=dog_or_cat,confidence_threshold=confidence_threshold, crop=False) # you must use gpu this time.
+    animalCropDiagonalRect = imageDogCatDetectionForCoverExtraction(frame, dog_or_cat=dog_or_cat,confidence_threshold=confidence_threshold, crop=False) # you must use gpu this time.
     if animalCropDiagonalRect is not None:
     # if checkMinMaxDict(text_area_ratio, text_area_threshold):
         text_area_ratio = getImageTextAreaRatio(frame)
