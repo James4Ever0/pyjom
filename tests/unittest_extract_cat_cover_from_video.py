119ac252956980f88e4374695f8815adfd3fc157
GitJournal <app@gitjournal.io>
2022-09-24 20:32:23 +0800

    [2022-09-24 12:32:20]
    FILE:
    tests/unittest_extract_cat_cover_from_video.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_extract_cat_cover_from_video.py
+++ tests/unittest_extract_cat_cover_from_video.py
@@ -74,36 +74,6 @@ for frame in getVideoFrameSampler(path, -1, -1, sample_size=mSampleSize, iterate
     # we need to identify this shit.
     # if checkMinMaxDict(text_area_ratio, text_area_threshold):
 
-    detections = bezierPaddleHubResnet50ImageDogCatDetector(frame, use_gpu=False) # no gpu avaliable
-    mDetections = [x for x in detections if x["identity"] == dog_or_cat]
-    mDetections.sort(key=lambda x: -x["confidence"])  # select the best one.
-    if len(mDetections) > 0:
-        best_confidence = mDetections[0]["confidence"]
-        print("BEST CONFIDENCE:", best_confidence)
-        if checkMinMaxDict(best_confidence, confidence_threshold):
-            # target = getImageTextAreaRatio(frame, inpaint=True, gpu=gpu)
-            # target = imageFourCornersInpainting(target)
-            # processed_frame = target
-            # break
-            text_area_ratio = getImageTextAreaRatio(frame)
-            # text_area_ratio = getImageTextAreaRatio(frame, gpu=gpu)
-            print("TEXT AREA RATIO", text_area_ratio)
-            # if animalCropDiagonalRect is not None:
-            if checkMinMaxDict(text_area_ratio, text_area_threshold):
-                mFrame = getImageTextAreaRatio(frame, inpaint=True)
-                mFrame = imageFourCornersInpainting(mFrame)
-                mFrame = imageCropoutBlackArea(mFrame)
-                mFrame = imageCropoutBlurArea(mFrame)
-                # cv2.imshow("PRE_FINAL_IMAGE", mFrame)
-                # cv2.waitKey(0)
-                processed_frame = imageDogCatDetectionForCoverExtraction(
-                    mFrame,
-                    dog_or_cat=dog_or_cat,
-                    confidence_threshold=yolov5_confidence_threshold,
-                    # area_threshold=0.15,
-                    crop=True,
-                    debug=True,
-                )
                 if processed_frame is not None:
                     # blurValue = imageCropoutBlurArea(processed_frame, value=True)
                     # print("BLUR VALUE:", blurValue)
