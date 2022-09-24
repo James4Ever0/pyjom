48569e768dc47e1419c393d0a1af1098c3e3740b
GitJournal <app@gitjournal.io>
2022-09-24 17:03:29 +0800

    [2022-09-24 09:03:28]
    FILE:
    tests/unittest_extract_cat_cover_from_video.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_extract_cat_cover_from_video.py
+++ tests/unittest_extract_cat_cover_from_video.py
@@ -61,10 +61,11 @@ duration = get_duration(path)
 mSampleSize = int(duration / 2)  # fps = 0.5 or something?
 processed_frame = None
 for frame in getVideoFrameSampler(path, -1, -1, sample_size=mSampleSize, iterate=True):
-    text_area_ratio = getImageTextAreaRatio(frame)
-    # text_area_ratio = getImageTextAreaRatio(frame, gpu=gpu)
-    print("TEXT AREA RATIO", text_area_ratio)
+
     if checkMinMaxDict(text_area_ratio, text_area_threshold):
+        text_area_ratio = getImageTextAreaRatio(frame)
+        # text_area_ratio = getImageTextAreaRatio(frame, gpu=gpu)
+        print("TEXT AREA RATIO", text_area_ratio)
         mFrame = getImageTextAreaRatio(frame, inpaint=True)
         animalCropDiagonalRect = imageDogCatDetectionForCoverExtraction(mFrame, dog_or_cat=dog_or_cat,confidence_threshold=confidence_threshold, crop=False) # you must use gpu this time.
         if animalCropDiagonalRect is not None:
