e6a5f3aaea95fdb59a869e198c7dbb449125d787
GitJournal <app@gitjournal.io>
2022-09-24 20:11:27 +0800

    [2022-09-24 12:11:24]
    FILE:
    tests/unittest_extract_cat_cover_from_video.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_extract_cat_cover_from_video.py
+++ tests/unittest_extract_cat_cover_from_video.py
@@ -30,7 +30,7 @@ from pyjom.commons import checkMinMaxDict
 dog_or_cat = "dog"
 confidence_threshold = {"min": 0.7}
 # blurValue_threshold ={'max': 0.94}
-yolov5_confidence_threshold=0.9
+yolov5_confidence_threshold=0.8
 # confidence_threshold = 0.7
 # confidence_threshold = {"min": 0.7}
 text_area_threshold = {"max": 0.2}
