7609d11d88d08804b16be4da4f05bdff1286fe70
GitJournal <app@gitjournal.io>
2022-09-24 22:58:43 +0800

    [2022-09-24 14:58:42]
    FILE:
    pyjom/imagetoolbox.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/imagetoolbox.py
+++ pyjom/imagetoolbox.py
@@ -850,7 +850,7 @@ def imageDogCatDetectionForCoverExtraction(
         import random
 
         x0_framework = random.randint(
-            max((x1 - width_current), 0), min(x0), defaultWidth)
+            max((x1 - width_current), 0), min(x0, defaultWidth-width_current)
         )
         framework_XYWH = (x0_framework, y0_altered, width_current, height_current)
         x_f, y_f, w_f, h_f = framework_XYWH
