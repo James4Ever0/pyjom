68df73b8ada24664a0ebebfedb569e241bc46a77
GitJournal <app@gitjournal.io>
2022-09-24 14:32:02 +0800

    [2022-09-24 06:32:01]
    FILE:
    tests/blur_image_detection_mask/BlurDetection_install/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/blur_image_detection_mask/BlurDetection_install/test.py
+++ tests/blur_image_detection_mask/BlurDetection_install/test.py
@@ -117,9 +117,8 @@ def draw_bounding_box_with_contour(
     else:
         if x0 is None:
             height, width = image.shape[:2]
-            return 0, 0, width, height
-        else:
-            return x0, y0, x1, y1
+            x0,y0, x1, y1 = 0, 0, width, height
+        return x0, y0, x1, y1
 
 
 # BlurDetection.scripts.display('msk', msk)
