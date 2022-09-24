a2207bd3864385416807d0dc5ca640dc3e66e7d6
GitJournal <app@gitjournal.io>
2022-09-24 12:51:15 +0800

    [2022-09-24 04:51:14]
    FILE:
    tests/unittest_ffmpeg_cropdetect_for_image_parse_log.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_ffmpeg_cropdetect_for_image_parse_log.py
+++ tests/unittest_ffmpeg_cropdetect_for_image_parse_log.py
@@ -43,7 +43,7 @@ for line in stderr_decoded.split("\n"):
         common_crops.append(cropString)
     # [Parsed_cropdetect_0 @ 0x56246a16cbc0] x1:360 x2:823 y1:0 y2:657 w:464 h:656 x:360 y:2 pts:3 t:0.120000 crop=464:656:360:2
     # this crop usually will never change. but let's count?
-if len(common_crops_count_tuple_list) > 0:
+if len(common_crops) > 0:
     common_crops_count_tuple_list = [(cropString, common_crops.count(cropString)) for cropString in set(common_crops)]
     common_crops_count_tuple_list.sort(key= lambda x: -x[1])
     selected_crop_string = common_crops_count_tuple_list[0][0]
