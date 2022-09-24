2a4d04e812e649ed5352f93a59fc5ed5ac921840
GitJournal <app@gitjournal.io>
2022-09-24 21:53:52 +0800

    [2022-09-24 13:53:49]
    FILE:
    tests/bilibili_search_api_modification_section_params_get_related_videos/recursive_search_bilibili_test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_search_api_modification_section_params_get_related_videos/recursive_search_bilibili_test.py
+++ tests/bilibili_search_api_modification_section_params_get_related_videos/recursive_search_bilibili_test.py
@@ -322,7 +322,7 @@ def getBilibiliPostMetadataForDogCat(sleepTime=2):
                                 from pyjom.imagetoolbox import imageLoader, imageDogCatCoverCropAdvanced, imageHistogramMatch
                                 image = imageLoader(cover)
                                 # import requests
-                                cropped_image = imageDogCatCoverCropAdvanced(image, dog_or_cat=dog_or_cat_original)
+                                cropped_image = imageDogCatCoverCropAdvanced(image, dog_or_cat=dog_or_cat_original, area_threshold=0.7)
                                 if cropped_image is not None:
                                     cropped_image_color_transfered = imageHistogramMatch(cropped_image, reference_histogram_cover)
                                     cropped_image_color_transfered_fliped = cv2.flip(cropped_image_color_transfered,1)
