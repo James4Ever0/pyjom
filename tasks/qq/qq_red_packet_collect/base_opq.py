074742a72865fa797864407b57ea0a06d8a6e235
GitJournal <app@gitjournal.io>
2022-09-25 20:19:58 +0800

    [2022-09-25 12:19:55]
    FILE:
    tasks/qq/qq_red_packet_collect/base_opq.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tasks/qq/qq_red_packet_collect/base_opq.py
+++ tasks/qq/qq_red_packet_collect/base_opq.py
@@ -51,12 +51,13 @@ bot = Botoy(qq = my_qq, port = opq_server["port"], host=opq_server["host"], log=
 
 try:
     myGroupList = action.getGroupList()
+    myGroupDict = {elem['GroupId'] : elem['GroupName'] for elem in myGroupList}
 except:
+    myGroupDict = {}
     pass
 # [{'GroupId': 118794, 'GroupMemberCount': 2818, 'GroupName': '攻防世界交流群', 'GroupNotice': '第四届“第五空间”网络安全大赛\n1、报名网站：\x01https://ctf.360.net/5space\x02\n2、 报名时间：8月30日--9月9日（沿用DSCTF）\n', 'GroupOwner': 41495, 'GroupTotalCount': 3000},...]
 # from lazero.utils.logger import sprint
 # sprint(myGroupList)
-myGroupDict = {elem['GroupId'] : elem['GroupName'] for elem in myGroupList}
 # breakpoint()
 def getGroupName(group_id):
     global myGroupList, action
@@ -67,6 +68,7 @@ def getGroupName(group_id):
             print("NO GROUP LIST AVALIABLE.")
             return
     group_id = int(group_id)
+    groupName = myGroupList
     return groupName
 
 #  	搜索群组 添加好友
