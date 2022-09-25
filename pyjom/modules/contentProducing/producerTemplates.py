43fdc8d2c9c0b90bcb54a56176e9c455f354f2b0
GitJournal <app@gitjournal.io>
2022-09-25 23:23:52 +0800

    [2022-09-25 15:23:50]
    FILE:
    pyjom/modules/contentProducing/producerTemplates.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/contentProducing/producerTemplates.py
+++ pyjom/modules/contentProducing/producerTemplates.py
@@ -554,6 +554,7 @@ def petsWithMusicOnlineProducer(
     tempdir="/dev/shm/medialang/pets_with_music_online",
     remove_unused=True,
     fast: bool = True,
+    medialangScript
 ):
     import uuid
 
