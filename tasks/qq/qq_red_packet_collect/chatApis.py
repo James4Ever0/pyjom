6c419ea0edcac0dcfb536fd9ee56fb8018a07150
GitJournal <app@gitjournal.io>
2022-09-25 20:31:06 +0800

    [2022-09-25 12:31:03]
    FILE:
    tasks/qq/qq_red_packet_collect/chatApis.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tasks/qq/qq_red_packet_collect/chatApis.py
+++ tasks/qq/qq_red_packet_collect/chatApis.py
@@ -86,7 +86,7 @@ def chatOwnThink(msg: str, group_id, retryFlag=False,timeout=5):
 
 def chatXiaoIce(msg, group_id, retryFlag=False, timeout=5):
     import requests
-    topic = getGroupNameFromDict
+    topic = getGroupNameFromDict()
     r = requests.get("http://localhost:8735/chat",params={"topic":topic,"message":msg})
     if r.status_code == 200:
         try:
