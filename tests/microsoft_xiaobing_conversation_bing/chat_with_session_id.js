388c242a6c2083312d73ed932537337eda017363
GitJournal <app@gitjournal.io>
2022-09-25 18:35:08 +0800

    [2022-09-25 10:35:05]
    FILE:
    tests/microsoft_xiaobing_conversation_bing/chat_with_session_id.js
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/microsoft_xiaobing_conversation_bing/chat_with_session_id.js
+++ tests/microsoft_xiaobing_conversation_bing/chat_with_session_id.js
@@ -444,7 +444,7 @@ const requestListener = function (req, res){
   } else if (req.url.split("?")[0] == 'chat'){
     callback = (result) => {
       res.writeHead(200);
-      content = {"msg": success,'reply': result}
+      content = {"msg": 'success','reply': result}
       res.end(JSON.stringify(content))
   }
   params = getQueryParams(req.url)
