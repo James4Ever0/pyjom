e99e1c7d3c7d35e69c18d476f9b1804f925bf0fd
GitJournal <app@gitjournal.io>
2022-09-25 19:00:52 +0800

    [2022-09-25 11:00:50]
    FILE:
    tasks/qq/qq_red_packet_collect/chatApis.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tasks/qq/qq_red_packet_collect/chatApis.py
+++ tasks/qq/qq_red_packet_collect/chatApis.py
@@ -88,10 +88,10 @@ def chatXiaoIce(msg, group_id, retryFlag=False, timeout=5):
 >>> import requests
 >>> r = requests.get("http://localhost:8735/chat",params={"topic":"python","message":"吃了没有"})
 if r.status_code == 200:
-
-    content = r.json()
-    assert content['msg'] == 'success'
-    reply = content['reply']
+    try:
+        content = r.json()
+        assert content['msg'] == 'success'
+        reply = content['reply']
 
 # changed. non_standard.
 def getChatApiReply(msg: str, group_id, chatApiIndex = 0,retryFlag=False,timeout=15): # 15 seconds of grace time.
