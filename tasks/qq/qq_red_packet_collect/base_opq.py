9c38d4a6467a3d66fc773bd1bffd1ca41078d14c
GitJournal <app@gitjournal.io>
2022-09-25 20:17:35 +0800

    [2022-09-25 12:17:32]
    FILE:
    tasks/qq/qq_red_packet_collect/base_opq.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tasks/qq/qq_red_packet_collect/base_opq.py
+++ tasks/qq/qq_red_packet_collect/base_opq.py
@@ -54,12 +54,13 @@ try:
 except:
     pass
 # [{'GroupId': 118794, 'GroupMemberCount': 2818, 'GroupName': '攻防世界交流群', 'GroupNotice': '第四届“第五空间”网络安全大赛\n1、报名网站：\x01https://ctf.360.net/5space\x02\n2、 报名时间：8月30日--9月9日（沿用DSCTF）\n', 'GroupOwner': 41495, 'GroupTotalCount': 3000},...]
-from lazero.utils.logger import sprint
-sprint(myGroupList)
-breakpoint()
+# from lazero.utils.logger import sprint
+# sprint(myGroupList)
+# breakpoint()
 def getGroupName(group_id):
-    group_id = int(group_id)
     global myGroupList
+    if myGroupList is None:
+    group_id = int(group_id)
     return groupName
 
 #  	搜索群组 添加好友
