56063fcdd286b4d2b5c4fca56b3e2c775c65b689
GitJournal <app@gitjournal.io>
2022-09-25 23:57:48 +0800

    [2022-09-25 15:57:45]
    FILE:
    pyjom/videotoolbox.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/videotoolbox.py
+++ pyjom/videotoolbox.py
@@ -673,8 +673,8 @@ def detectStationaryLogoOverTime(
             # cv2.rectangle(myMask2, (x, y), (x + w, y + h), 255, -1)
         # get the final dictionary
         values = []
-        if len(mFinalDelogoFilters) == 0:
-        else:
+        if len(mFinalDelogoFilters) != 0:
+        # else:
             delogoCommandSet = "|".join(mFinalDelogoFilters)
             # values = []
             for (mStart, mEnd) in start_end_ranges:
