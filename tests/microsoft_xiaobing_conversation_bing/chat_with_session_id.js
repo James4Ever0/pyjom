9bfdceca9690e40c5ed566eae121b7e6ffe94b16
GitJournal <app@gitjournal.io>
2022-09-25 17:39:29 +0800

    [2022-09-25 09:39:26]
    FILE:
    tests/microsoft_xiaobing_conversation_bing/chat_with_session_id.js
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/microsoft_xiaobing_conversation_bing/chat_with_session_id.js
+++ tests/microsoft_xiaobing_conversation_bing/chat_with_session_id.js
@@ -412,6 +412,7 @@ function getContents(opts) {
 }
 // module.exports = { iceAI_word };
 let test_request = "不会吧"
+query = 'python'
 let config = newChatId(query)
 response = iceAI_word(test_request, config)
 console.log("RESPONSE:", response)
