051131861c3901ca24ebde1d949657c8e1f6134d
GitJournal <app@gitjournal.io>
2022-09-25 21:04:57 +0800

    [2022-09-25 13:04:53]
    FILE:
    tasks/qq/qq_red_packet_collect/botoy_redpacket_collect.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tasks/qq/qq_red_packet_collect/botoy_redpacket_collect.py
+++ tasks/qq/qq_red_packet_collect/botoy_redpacket_collect.py
@@ -298,6 +298,7 @@ def group(ctx: GroupMsg, groupInitReplyDelayRange=(4, 15)):
             Content = ctx.Content  # this is string.
             Content_json = json.loads(Content)
             content_text = Content_json["Content"]
+            breakpoint()
             UserExt = Content_json["UserExt"]
             for elem in UserExt:
                 QQNick = elem["QQNick"]
