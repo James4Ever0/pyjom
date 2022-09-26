935786e7744f6d0bcf14bbc45cda6dfb7305a769
GitJournal <app@gitjournal.io>
2022-09-26 14:30:43 +0800

    [2022-09-26 06:30:36]
    FILE:
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_search_api_modification_section_params_get_related_videos/recursive_search_bilibili_test.py
+++ tests/bilibili_search_api_modification_section_params_get_related_videos/recursive_search_bilibili_test.py
@@ -383,7 +383,7 @@ def getBilibiliPostMetadataForDogCat(sleepTime=2):
                                     )  # a collection of tags.
                                     mTitle = random.choice(filtered_title_list)
                                     mBgm = random.choice(bgm_list)
-                                    yield cover_target, mTagSeries, mTitle, mBgm, dog_or_cat_original
+                                    yield cover_target, mTagSeries, mTitle, mBgm, mDescription, dog_or_cat_original
                                     clearMyLists()
         except:
             import time
