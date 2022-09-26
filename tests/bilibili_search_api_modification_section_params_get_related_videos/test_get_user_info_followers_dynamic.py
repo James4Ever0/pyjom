350e9a71b4d38e1f1453a378b6f8091d4f2f732c
GitJournal <app@gitjournal.io>
2022-09-26 14:06:26 +0800

    [2022-09-26 06:06:23]
    FILE:
    tests/bilibili_search_api_modification_section_params_get_related_videos/test_get_user_info_followers_dynamic.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_search_api_modification_section_params_get_related_videos/test_get_user_info_followers_dynamic.py
+++ tests/bilibili_search_api_modification_section_params_get_related_videos/test_get_user_info_followers_dynamic.py
@@ -2,7 +2,7 @@ from bilibili_api import user, sync
 
 u = user.User(660303135)
 
-data = sync(u.get_relation_info())
+# data = sync(u.get_relation_info())
 # ["follower"]
 # {'mid': 660303135, 'following': 34, 'whisper': 0, 'black': 0, 'follower': 1158}
 # get followers less than 200 but view greater than 3000.
@@ -41,8 +41,8 @@ potentialMethods = [
 import json
 
 mdata = {}
-
-for key in potentialMethods:
+import progressbar
+for key in progressbar.progressbar(potentialMethods):
     command = "u.{}()".format(key)
     try:
         result = sync(eval(command))
