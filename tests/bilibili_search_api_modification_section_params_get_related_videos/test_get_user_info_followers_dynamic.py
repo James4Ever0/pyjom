6bc20bdd9f4ecf8d614deb39ea6d5296cea5b4ed
GitJournal <app@gitjournal.io>
2022-09-26 13:56:12 +0800

    [2022-09-26 05:56:09]
    FILE:
    tests/bilibili_search_api_modification_section_params_get_related_videos/test_get_user_info_followers_dynamic.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_search_api_modification_section_params_get_related_videos/test_get_user_info_followers_dynamic.py
+++ tests/bilibili_search_api_modification_section_params_get_related_videos/test_get_user_info_followers_dynamic.py
@@ -13,3 +13,4 @@ print(dir(u))
 # you can also get followings to get the 'target video'
 # ['credential', 'get_all_followings', 'get_article_list', 'get_articles', 'get_audios', 'get_channel_list', 'get_channel_videos_season', 'get_channel_videos_series', 'get_channels', 'get_cheese', 'get_dynamics', 'get_followers', 'get_followings', 'get_live_info', 'get_overview_stat', 'get_relation_info', 'get_subscribed_bangumi', 'get_uid', 'get_up_stat', 'get_user_info', 'get_videos', 'modify_relation', 'top_followers']
 breakpoint()
+# {'video': 79, 'bangumi': 15, 'cinema': 4, 'channel': {'master': 2, 'guest': 2}, 'favourite': {'master': 7, 'guest': 7}, 'tag': 0, 'article': 0, 'playlist': 0, 'album': 3, 'audio': 0, 'pugv': 0, 'season_num': 1}
