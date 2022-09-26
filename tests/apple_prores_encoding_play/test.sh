ae9d71bfb0625e3be5e0b02b71a2b82f27f98d13
GitJournal <app@gitjournal.io>
2022-09-26 15:21:24 +0800

    [2022-09-26 07:21:21]
    FILE:
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/apple_prores_encoding_play/test.sh
+++ tests/apple_prores_encoding_play/test.sh
@@ -1,6 +1,6 @@
 videoPath="/root/Desktop/works/pyjom/samples/video/cute_cat_gif.mp4"
 # prores_aw
-ffmpeg -hwaccel vulkan -i $videoPath  -c:v prores_ks  output.mkv
+ffmpeg -hwaccel vulkan -i $videoPath -c:v prores_ks  output.mkv
 # https://ottverse.com/ffmpeg-convert-to-apple-prores-422-4444-hq/#:~:text=FFmpeg%20contains%20two%20ProRes%20encoders%2C%20the%20prores-aw%20and,option%20to%20choose%20the%20ProRes%20profile%20to%20encode.
 # videoPath="/Users/jamesbrown/Desktop/works/pyjom_remote/samples/video/cute_cat_gif.mp4"
 # ffmpeg -hwaccel videotoolbox -i $videoPath -c:v prores_ks  \
