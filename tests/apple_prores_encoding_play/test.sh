74260bb9fb68aaef4c63f1e029e336dde7f831ee
GitJournal <app@gitjournal.io>
2022-09-26 15:14:54 +0800

    [2022-09-26 07:14:51]
    FILE:
    tests/apple_prores_encoding_play/test.sh
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/apple_prores_encoding_play/test.sh
+++ tests/apple_prores_encoding_play/test.sh
@@ -1,11 +1,12 @@
 # videoPath="/root/Desktop/works/pyjom/samples/video/cute_cat_gif.mp4"
-# ffmpeg -i $videoPath output.ProRes
+# ffmpeg -i $videoPath output.mkv
 videoPath="/Users/jamesbrown/Desktop/works/pyjom_remote/samples/video/cute_cat_gif.mp4"
 ffmpeg -hwaccel videotoolbox -i $videoPath -c:v prores_ks  \
 -profile:v 4 \
 -vendor apl0 \
 -bits_per_mb 8000 \
--pix_fmt yuva444p10le \ output.mov
+-pix_fmt yuva444p10le \ 
+output.mov
 # Do remember to store the output in either of these three formats that are allowed as containers for the ProRes format.
 
 # .mov (QuickTime)
