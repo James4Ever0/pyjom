c18865b8e952f8f419ec12b0bdd284fcb8b121a4
GitJournal <app@gitjournal.io>
2022-09-26 15:07:05 +0800

    [2022-09-26 07:07:02]
    FILE:
    tests/apple_prores_encoding_play/test.sh
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/apple_prores_encoding_play/test.sh
+++ tests/apple_prores_encoding_play/test.sh
@@ -1,3 +1,3 @@
 videoPath="/root/Desktop/works/pyjom/samples/video/cute_cat_gif.mp4"
-ffmpeg -hwaccel vulkan -i $videoPath output.prores
+ffmpeg -hwaccel vulkan -i $videoPath -v:c prores output.prores
 # ffmpeg -hwaccel videotoolbox -i $videoPath output.prores
