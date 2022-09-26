1163dde51fdcffa90a44a405c0b656960233377f
GitJournal <app@gitjournal.io>
2022-09-26 15:16:58 +0800

    [2022-09-26 07:16:55]
    FILE:
    tests/apple_prores_encoding_play/test.sh
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/apple_prores_encoding_play/test.sh
+++ tests/apple_prores_encoding_play/test.sh
@@ -1,13 +1,13 @@
-# videoPath="/root/Desktop/works/pyjom/samples/video/cute_cat_gif.mp4"
-# ffmpeg -i $videoPath  -c:v prores_ks  output.mkv
+videoPath="/root/Desktop/works/pyjom/samples/video/cute_cat_gif.mp4"
+ffmpeg -i $videoPath  -c:v prores_ks  output.mkv
 # https://ottverse.com/ffmpeg-convert-to-apple-prores-422-4444-hq/#:~:text=FFmpeg%20contains%20two%20ProRes%20encoders%2C%20the%20prores-aw%20and,option%20to%20choose%20the%20ProRes%20profile%20to%20encode.
-videoPath="/Users/jamesbrown/Desktop/works/pyjom_remote/samples/video/cute_cat_gif.mp4"
-ffmpeg -hwaccel videotoolbox -i $videoPath -c:v prores_ks  \
--profile:v 4 \
--vendor apl0 \
--bits_per_mb 8000 \
--pix_fmt yuva444p10le \ 
-output.mov
+# videoPath="/Users/jamesbrown/Desktop/works/pyjom_remote/samples/video/cute_cat_gif.mp4"
+# ffmpeg -hwaccel videotoolbox -i $videoPath -c:v prores_ks  \
+# -profile:v 4 \
+# -vendor apl0 \
+# -bits_per_mb 8000 \
+# -pix_fmt yuva444p10le \ 
+# output.mov
 # Do remember to store the output in either of these three formats that are allowed as containers for the ProRes format.
 
 # .mov (QuickTime)
