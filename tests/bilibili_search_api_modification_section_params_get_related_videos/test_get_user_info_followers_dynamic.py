696af382fb14621a58496cedfec0c831b5e14803
GitJournal <app@gitjournal.io>
2022-09-26 13:48:32 +0800

    [2022-09-26 05:48:26]
    FILE:
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_search_api_modification_section_params_get_related_videos/test_get_user_info_followers_dynamic.py
+++ tests/bilibili_search_api_modification_section_params_get_related_videos/test_get_user_info_followers_dynamic.py
@@ -2,4 +2,9 @@ from bilibili_api import user, sync
 
 u = user.User(660303135)
 
-data = sync(u.get_relation_info())["follower"])
+data = sync(u.get_relation_info())
+# ["follower"]
+print(data)
+print(data.keys())
+print(dir(u))
+breakpoint()
