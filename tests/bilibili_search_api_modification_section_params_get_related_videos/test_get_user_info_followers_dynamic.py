fb010f4234b2f0419ed3b82bac2f9a40e9c0d39f
GitJournal <app@gitjournal.io>
2022-09-26 13:49:40 +0800

    [2022-09-26 05:49:37]
    FILE:
    tests/bilibili_search_api_modification_section_params_get_related_videos/test_get_user_info_followers_dynamic.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_search_api_modification_section_params_get_related_videos/test_get_user_info_followers_dynamic.py
+++ tests/bilibili_search_api_modification_section_params_get_related_videos/test_get_user_info_followers_dynamic.py
@@ -5,7 +5,7 @@ u = user.User(660303135)
 data = sync(u.get_relation_info())
 # ["follower"]
 # {'mid': 660303135, 'following': 34, 'whisper': 0, 'black': 0, 'follower': 1158}
-# get 
+# get followers less than 200 but view greater than 3000
 print(data)
 print(data.keys())
 print(dir(u))
