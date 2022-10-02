9376607929efcee722571bfcb5157a946947f46a
GitJournal <app@gitjournal.io>
2022-10-02 23:18:35 +0800

    [2022-10-02 15:18:32]
    FILE:
    tests/jina_deploy_free_gpu_cpu/semantic_search_encoder_multilingual/test.sh
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/jina_deploy_free_gpu_cpu/semantic_search_encoder_multilingual/test.sh
+++ tests/jina_deploy_free_gpu_cpu/semantic_search_encoder_multilingual/test.sh
@@ -1,2 +1,2 @@
 env JINA_MP_START_METHOD=spawn python3 test.py
-# still we are having issue with the txtai, which cannot load our model
+# still we are having issue with the txtai, which cannot load our model for whatever reason.
