a298f86343b15174f831f98ab5c5da56a7b8a3df
GitJournal <app@gitjournal.io>
2022-10-02 22:58:31 +0800

    [2022-10-02 14:58:28]
    FILE:
    tests/jina_deploy_free_gpu_cpu/semantic_search_encoder_multilingual/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/jina_deploy_free_gpu_cpu/semantic_search_encoder_multilingual/test.py
+++ tests/jina_deploy_free_gpu_cpu/semantic_search_encoder_multilingual/test.py
@@ -2,6 +2,7 @@ from executor import semantic_search_encoder_multilingual
 from jina import Flow
 import os
 
+if __name__ == "__main__":
 os.environ["http_proxy"] = ""
 os.environ["https_proxy"] = ""
 f = Flow(port=12345).add(uses=semantic_search_encoder_multilingual, replicas=0)
