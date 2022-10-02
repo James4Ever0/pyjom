f683078b07f0eae5cc2c0df0888d196064f7d1e6
GitJournal <app@gitjournal.io>
2022-10-02 22:38:31 +0800

    [2022-10-02 14:38:27]
    FILE:
    tests/jina_deploy_free_gpu_cpu/semantic_search_encoder_multilingual/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/jina_deploy_free_gpu_cpu/semantic_search_encoder_multilingual/test.py
+++ tests/jina_deploy_free_gpu_cpu/semantic_search_encoder_multilingual/test.py
@@ -1,5 +1,5 @@
 from executor import semantic_search_encoder_multilingual
-f = Flow(port=12345).add(uses=FooExec, replicas=3).add(uses=BarExec, replicas=2)
+f = Flow(port=12345).add(uses=semantic_search_encoder_multilingual, replicas=1)
 
 with f:
     f.block()
