db2c87d1b0c067539a760b1f6c4538cd1d8fd6f5
GitJournal <app@gitjournal.io>
2022-10-02 22:44:08 +0800

    [2022-10-02 14:44:05]
    FILE:
    tests/jina_deploy_free_gpu_cpu/semantic_search_encoder_multilingual/test_client.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/jina_deploy_free_gpu_cpu/semantic_search_encoder_multilingual/test_client.py
+++ tests/jina_deploy_free_gpu_cpu/semantic_search_encoder_multilingual/test_client.py
@@ -4,5 +4,6 @@ c = Client(port=12345)
 docArray = DocumentArray.empty(1)
 docArray[0].text = 'hello world'
 r = c.post('/', docArray)
-
-print(r[0])
+msg = r[0].msg
+if msg == 'success':
+    print(r[0])
