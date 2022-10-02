4c3284a4e9fd3f6ec9df47d58182f2ce6f4ced97
GitJournal <app@gitjournal.io>
2022-10-02 22:23:55 +0800

    [2022-10-02 14:23:48]
    FILE:
    tests/jina_deploy_free_gpu_cpu/semantic_search_encoder_multilingual/client.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

---
+++ tests/jina_deploy_free_gpu_cpu/semantic_search_encoder_multilingual/client.py
@@ -0,0 +1,18 @@
+from jina import Client, DocumentArray
+# host = 'grpc://0.0.0.0:54321'
+# host = 'grpcs://3fcd103a37.wolf.jina.ai'
+# container_id = '7f015443e8'
+# host = 'grpcs://{}.wolf.jina.ai'.format(container_id)
+host = ""
+if __name__ == '__main__':
+    c = Client(host=host)
+    while True:
+        docArray = DocumentArray.empty(1)
+        docArray[0].text = command = input("jina> ")
+        if command == 'exit':
+            print('exiting jina')
+            break
+        da = c.post('/', docArray)
+        response = da[0].text
+        # print(da.texts)
+        print(response)
