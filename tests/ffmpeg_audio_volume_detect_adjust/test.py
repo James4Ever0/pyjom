5e72b3a5cec49b09d4148f362bc3843ece502b24
GitJournal <app@gitjournal.io>
2022-10-07 00:34:08 +0800

    [2022-10-06 16:34:07]
    FILE:
    tests/ffmpeg_audio_volume_detect_adjust/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/ffmpeg_audio_volume_detect_adjust/test.py
+++ tests/ffmpeg_audio_volume_detect_adjust/test.py
@@ -3,7 +3,7 @@
 # we can also detect if the stream does not have audio stream.
 import ffmpeg
 
-def create_black_video_without_audio(length):
+def create_black_video_without_audio(length, mediapath):
     # ffmpeg -f lavfi -i color=c=black:s=1280x720:r=5 -i audio.mp3 -crf 0 -c:a copy -shortest output.mp4
     ffmpeg.input()
 
