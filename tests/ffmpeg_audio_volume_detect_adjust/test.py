228098bfc1ccfc68d0984023ed8815ed6d711390
GitJournal <app@gitjournal.io>
2022-10-07 00:32:42 +0800

    [2022-10-06 16:32:42]
    FILE:
    tests/ffmpeg_audio_volume_detect_adjust/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/ffmpeg_audio_volume_detect_adjust/test.py
+++ tests/ffmpeg_audio_volume_detect_adjust/test.py
@@ -9,4 +9,5 @@ def create_black_video_without_audio(length):
 def create_test_video_with_editly(audio): # length is calculated by the audio length.
     ...
 
-def detect_volume_average():
+def detect_volume_average(mediapath):
+    ...
