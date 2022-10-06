8883697324db23bdae528b1c4ff2dbf4392ecd5b
GitJournal <app@gitjournal.io>
2022-10-07 00:42:03 +0800

    [2022-10-06 16:42:02]
    FILE:
    tests/ffmpeg_audio_volume_detect_adjust/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/ffmpeg_audio_volume_detect_adjust/test.py
+++ tests/ffmpeg_audio_volume_detect_adjust/test.py
@@ -8,7 +8,7 @@ def create_black_video_without_audio(length, mediapath):
     # ffmpeg -f lavfi -i color=c=black:s=1280x720:r=5 -i audio.mp3 -crf 0 -c:a copy -shortest output.mp4
     # length is in seconds.
     videoInput = "color=c=black:s=1280x720:r=5"
-    ffmpeg.input(videoInput, f="lavfi", t=length).output(mediapath, crf=0).run()
+    ffmpeg.input(videoInput, f="lavfi", t=length).output(mediapath, crf=0).run(overwrite_output=True)
 
 
 def create_test_video_with_editly(audio):  # length is calculated by the audio length.
