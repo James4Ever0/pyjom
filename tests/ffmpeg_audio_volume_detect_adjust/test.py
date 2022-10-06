86b7690bfc71311d2acfcb1393f503c1246b03c0
GitJournal <app@gitjournal.io>
2022-10-07 00:39:04 +0800

    [2022-10-06 16:39:03]
    FILE:
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/ffmpeg_audio_volume_detect_adjust/test.py
+++ tests/ffmpeg_audio_volume_detect_adjust/test.py
@@ -3,14 +3,17 @@
 # we can also detect if the stream does not have audio stream.
 import ffmpeg
 
+
 def create_black_video_without_audio(length, mediapath):
     # ffmpeg -f lavfi -i color=c=black:s=1280x720:r=5 -i audio.mp3 -crf 0 -c:a copy -shortest output.mp4
     # length is in seconds.
     videoInput = "color=c=black:s=1280x720:r=5"
-    ffmpeg.input(videoInput,f='lavfi',t=length).output(mediapath,crf=0).run()
+    ffmpeg.input(videoInput, f="lavfi", t=length).output(mediapath, crf=0).run()
 
-def create_test_video_with_editly(audio): # length is calculated by the audio length.
+
+def create_test_video_with_editly(audio):  # length is calculated by the audio length.
     ...
+
 
 def detect_volume_average(mediapath):
     ...
