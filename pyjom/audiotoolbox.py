45fccafb305a39c2cb5e044da030ed05e55cb697
GitJournal <app@gitjournal.io>
2022-10-07 00:43:16 +0800

    [2022-10-06 16:43:15]
    FILE:
    pyjom/audiotoolbox.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/audiotoolbox.py
+++ pyjom/audiotoolbox.py
@@ -1,3 +1,5 @@
 # first and foremost is the audio correction, the volume detector, the audio detector.
 # https://trac.ffmpeg.org/wiki/AudioVolume
-# but first how to get the audio length?
+
+# but first how to get the audio file length?
+# for video we have caer. but for audio?
