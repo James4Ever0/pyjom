7a2100b6919af2d4c91d2ddc76468e7a9a39c027
GitJournal <app@gitjournal.io>
2022-10-08 01:42:25 +0800

    [2022-10-07 17:42:24]
    FILE:
    tests/ffmpeg_audio_volume_detect_adjust/test_get_audio_volume.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/ffmpeg_audio_volume_detect_adjust/test_get_audio_volume.py
+++ tests/ffmpeg_audio_volume_detect_adjust/test_get_audio_volume.py
@@ -1,6 +1,9 @@
 import test
 from pyjom.audiotoolbox import getAudioDuration
 
-audioPath = ""
+audioPath = (
+    "/root/Desktop/works/pyjom/tests/music_analysis/exciting_bgm.mp3",
+)  # 320000
+
 audioDuration = getAudioDuration(audioPath)
-print('audioDuration:', audioDuration)
+print("audioDuration:", audioDuration)
