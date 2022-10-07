38ebecb09328e551bf12948bfc64193e1249eafb
GitJournal <app@gitjournal.io>
2022-10-07 13:40:23 +0800

    [2022-10-07 05:40:23]
    FILE:
    tests/ffmpeg_audio_volume_detect_adjust/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/ffmpeg_audio_volume_detect_adjust/test.py
+++ tests/ffmpeg_audio_volume_detect_adjust/test.py
@@ -1,7 +1,8 @@
 # ffmpeg -i video.avi -af "volumedetect"
 # shall we get the output?
 # we can also detect if the stream does not have audio stream.
-from ..
+import sys
+sys.path.append("")
 import ffmpeg
 
 
