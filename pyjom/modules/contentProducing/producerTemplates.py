572fe49c8328eef432c2fa5ffa7ce5615f9177c5
GitJournal <app@gitjournal.io>
2022-10-08 02:17:40 +0800

    [2022-10-07 18:17:39]
    FILE:
    pyjom/modules/contentProducing/producerTemplates.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/contentProducing/producerTemplates.py
+++ pyjom/modules/contentProducing/producerTemplates.py
@@ -703,7 +703,7 @@ def petsWithMusicOnlineProducer(
                 # maybe we need render the lyric file separately.
                 # normalization starts here.
                 rendered_media_location = getRandomFileName('mp4')# so where exactly is the file?
-                (editly_outputPath)
+                adjustVolumeInMedia(editly_outputPath, rendered_media_location)
                 # using a ffmpeg filter.
                 print('RENDERED MEDIA LOCATION:',rendered_media_location)
                 if debug:
