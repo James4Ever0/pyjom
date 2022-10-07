8336fa0b6e6c00feaf489b9f117af23236ba91d8
GitJournal <app@gitjournal.io>
2022-10-08 00:09:13 +0800

    [2022-10-07 16:09:11]
    FILE:
    tests/ffmpeg_audio_volume_detect_adjust/test_get_media_bitrate.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/ffmpeg_audio_volume_detect_adjust/test_get_media_bitrate.py
+++ tests/ffmpeg_audio_volume_detect_adjust/test_get_media_bitrate.py
@@ -2,4 +2,5 @@ from test import getAudioBitrate
 
 mediaPaths = ["","/root/Desktop/works/pyjom/tests/ffmpeg_audio_volume_detect_adjust/normalized.mp4"]
 for mediaPath in mediaPaths:
+    print('media path:', mediaPath)
     result = getAudioBitrate(mediaPath)
