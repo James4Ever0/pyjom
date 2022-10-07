0ccdb7448d68fdebc5dbe9b03d46b4c1823bfda0
GitJournal <app@gitjournal.io>
2022-10-07 15:24:12 +0800

    [2022-10-07 07:24:11]
    FILE:
    tests/ffmpeg_audio_volume_detect_adjust/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/ffmpeg_audio_volume_detect_adjust/test.py
+++ tests/ffmpeg_audio_volume_detect_adjust/test.py
@@ -124,6 +124,7 @@ if __name__ == "__main__":
     # ______________________________
     output_path = create_test_video_with_editly(audiopath)
     detect_volume_average(output_path, debug=True)
+    # volume changed!
     # MEDIA PATH: volDetect_test.mp4
     # VOLUME: {'mean': -16.8, 'max': -2.0}
     # ERROR STATUS: False
