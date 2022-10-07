87c5eca04455789c57d988a6a5c58b2b3e451e7e
GitJournal <app@gitjournal.io>
2022-10-07 16:01:37 +0800

    [2022-10-07 08:01:36]
    FILE:
    tests/ffmpeg_audio_volume_detect_adjust/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/ffmpeg_audio_volume_detect_adjust/test.py
+++ tests/ffmpeg_audio_volume_detect_adjust/test.py
@@ -128,7 +128,7 @@ def adjustVolumeInMedia(mediaPath, outputPath,targets={'mean':}):# must set targ
     if outputPath.lower().endswith(".mp4"):
         commandline += ["-c:a", "aac"]
     if target_level:
-        commandline +=['']
+        commandline +=['-t', (target_level)]
     commandline += [mediaPath]
     status = subprocess.run(commandline)  # is it even successful?
     returncode = status.returncode
