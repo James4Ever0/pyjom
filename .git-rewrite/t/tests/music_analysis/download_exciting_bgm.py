ec7e9b7ec8b41d44fbe68b19711bd551968b58c9
GitJournal <app@gitjournal.io>
2022-10-07 19:12:16 +0800

    [2022-10-07 11:12:14]
    FILE:
    .git-rewrite/t/tests/music_analysis/download_exciting_bgm.py
    .git-rewrite/t/tests/music_analysis/.gitignore
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- .git-rewrite/t/tests/music_analysis/download_exciting_bgm.py
+++ .git-rewrite/t/tests/music_analysis/download_exciting_bgm.py
@@ -3,9 +3,28 @@ download_path = "exciting_bgm.mp3" # is the extension right?
 import requests
 baseUrl = "http://localhost:4000"
 
-keyword = "last friday night" # american pop music?
+keywords = "last friday night" # american pop music?
 
-search_result = requests.get(baseUrl+"/cloudsearch", params={"keyword": keyword})
+search_result = requests.get(baseUrl+"/cloudsearch", params={"keywords": keywords})
 
-print(search_result.content)
-breakpoint()
+search_result_json = search_result.json() # check search_result.json
+# breakpoint()
+code = search_result_json["code"]
+result = search_result_json["result"]
+
+if code == 200: # no error here.
+    songs = result["songs"]
+    mySong = songs[0]
+    mySongName = mySong["name"]
+    mySongId = mySong["id"]
+    mySongArtists = mySong["ar"] # reserved for further use. like find other songs by the artist.
+
+    print("SELECTED SONG:")
+    print(mySongName, mySongId, mySongArtists)
+
+    # download that thing.
+    download_result = requests.get(baseUrl + "/song/download/url", params = {"id":mySongId})
+    download_result_json = download_result.json()
+
+    print(download_result_json) # no download url!
+    breakpoint()
