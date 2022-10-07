4748bab20b4fd4d0ef5e50f382150b444030da3a
GitJournal <app@gitjournal.io>
2022-10-07 13:44:06 +0800

    [2022-10-07 05:44:06]
    FILE:
    tests/ffmpeg_audio_volume_detect_adjust/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/ffmpeg_audio_volume_detect_adjust/test.py
+++ tests/ffmpeg_audio_volume_detect_adjust/test.py
@@ -2,23 +2,27 @@
 # shall we get the output?
 # we can also detect if the stream does not have audio stream.
 import sys
-pyjom_path = '/root/Desktop/works/pyjom'
+
+pyjom_path = "/root/Desktop/works/pyjom"
 sys.path.append(pyjom_path)
 import ffmpeg
 from pyjom.audiotoolbox import getAudioDuration
+
 
 def create_black_video_without_audio(duration, mediapath):
     # ffmpeg -f lavfi -i color=c=black:s=1280x720:r=5 -i audio.mp3 -crf 0 -c:a copy -shortest output.mp4
     # length is in seconds.
     videoInput = "color=c=black:s=1280x720:r=5"
-    ffmpeg.input(videoInput, f="lavfi", t=duration).output(mediapath, crf=0).run(overwrite_output=True)
+    ffmpeg.input(videoInput, f="lavfi", t=duration).output(mediapath, crf=0).run(
+        overwrite_output=True
+    )
 
 
 def create_test_video_with_editly(audio):  # length is calculated by the audio length.
-    audio_duration= getAudioDuration(audio)
+    audio_duration = getAudioDuration(audio)
 
 
 def detect_volume_average(mediapath):
     # ffmpeg -i input.wav -filter:a volumedetect -f null /dev/null
     audio = ffmpeg.input(mediapath).audio
-    audio.filter('volumedetect').output('/dev/null',f="null").run()
+    audio.filter("volumedetect").output("/dev/null", f="null").run()
