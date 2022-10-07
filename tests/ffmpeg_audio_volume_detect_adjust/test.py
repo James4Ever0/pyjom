d95acaca7c21d3a4702eb6ae2658616e00b705f8
GitJournal <app@gitjournal.io>
2022-10-07 22:29:37 +0800

    [2022-10-07 14:29:35]
    FILE:
    tests/ffmpeg_audio_volume_detect_adjust/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/ffmpeg_audio_volume_detect_adjust/test.py
+++ tests/ffmpeg_audio_volume_detect_adjust/test.py
@@ -216,7 +216,7 @@ def adjustVolumeInMedia(
         "max": 0.0,
     },  # what is the real value anyway? we want the volume fetched from web.
     overwrite_output=False,
-    algorithm: Literal["rms", "ebu", "peak"] = "ebu",
+    algorithm: Literal["rms", "ebu", "peak"] = "rms",
 ):  # must set target volume.
 
     # use ffmpeg-normalize?
