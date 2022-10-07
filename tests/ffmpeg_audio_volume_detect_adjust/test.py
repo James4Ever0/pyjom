7a6119d530be3b03d5ad934d24f6c8177943b342
GitJournal <app@gitjournal.io>
2022-10-07 16:06:55 +0800

    [2022-10-07 08:06:54]
    FILE:
    tests/ffmpeg_audio_volume_detect_adjust/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/ffmpeg_audio_volume_detect_adjust/test.py
+++ tests/ffmpeg_audio_volume_detect_adjust/test.py
@@ -129,7 +129,7 @@ def adjustVolumeInMedia(
     # use aac for mp4 output. let's do it!
     target_level = targets.get("mean", None)
     true_peak = targets.get("max", None)
-    commandline = ["ffmpeg-normalize", "-o", outputPath, "-pr"]
+    commandline = ["ffmpeg-normalize", "-o", outputPath, "-pr", '-nt','rms']
     # commandline = ["ffmpeg-normalize", "-o", outputPath, "-pr"]
     if outputPath.lower().endswith(".mp4"):
         commandline += ["-c:a", "aac"]
