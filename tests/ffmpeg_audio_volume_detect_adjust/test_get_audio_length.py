6f691c547e876679f1ddb296c28db3ea73b12209
GitJournal <app@gitjournal.io>
2022-10-08 01:43:38 +0800

    [2022-10-07 17:43:36]
    FILE:
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/ffmpeg_audio_volume_detect_adjust/test_get_audio_length.py
+++ tests/ffmpeg_audio_volume_detect_adjust/test_get_audio_length.py
@@ -5,3 +5,5 @@ audioPath = "/root/Desktop/works/pyjom/tests/music_analysis/exciting_bgm.mp3"
 
 audioDuration = getAudioDuration(audioPath)
 print("audioDuration:", audioDuration)
+# audioDuration: 302.915918367
+# obviously floating point 
