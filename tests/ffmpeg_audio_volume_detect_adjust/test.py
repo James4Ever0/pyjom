6c4a4dde568d0b563cd96a28e36d30bd6eb08a97
GitJournal <app@gitjournal.io>
2022-10-07 18:17:09 +0800

    [2022-10-07 10:17:05]
    FILE:
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/ffmpeg_audio_volume_detect_adjust/test.py
+++ tests/ffmpeg_audio_volume_detect_adjust/test.py
@@ -130,7 +130,7 @@ Ogg Vorbis audio file - .ogg
 MIDI audio file - .midi or .mid
 CD audio file - .cda
 AIF audio file - .aif"""
-    media
+    mediaFileExtensionToMeaningDict = {""}
 
 
 def detect_volume_average(mediapath, debug=False):
