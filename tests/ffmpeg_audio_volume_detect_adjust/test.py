c57def531dc08d2aecc19f43b006dee79e081383
GitJournal <app@gitjournal.io>
2022-10-08 00:04:17 +0800

    [2022-10-07 16:04:15]
    FILE:
    tests/ffmpeg_audio_volume_detect_adjust/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/ffmpeg_audio_volume_detect_adjust/test.py
+++ tests/ffmpeg_audio_volume_detect_adjust/test.py
@@ -289,4 +289,5 @@ def getMediaBitrate(mediaPath, audioOnly=False, videoOnly=False):
     elif videoOnly:
         commandArguments +=["-select_streams", "v:0",]
     commandArguments+=["-show_entries", "stream=bit_rate", "-hide_banner","-print_format","json"]
-    subprocess.run(commandArguments,capture_output=True, encoding='UTF-8')
+    result = subprocess.run(commandArguments,capture_output=True, encoding='UTF-8')
+    result.stdout
