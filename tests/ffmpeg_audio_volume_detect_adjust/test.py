ea505384346360f8a77bf095073afcfa81e129ef
GitJournal <app@gitjournal.io>
2022-10-07 14:51:11 +0800

    [2022-10-07 06:51:10]
    FILE:
    tests/ffmpeg_audio_volume_detect_adjust/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/ffmpeg_audio_volume_detect_adjust/test.py
+++ tests/ffmpeg_audio_volume_detect_adjust/test.py
@@ -77,8 +77,9 @@ def create_test_video_with_editly(audio):  # length is calculated by the audio l
 
 def detect_volume_average(mediapath, debug=False):
     # ffmpeg -i input.wav -filter:a volumedetect -f null /dev/null
-    audio = ffmpeg.input(mediapath)
-    # audio = ffmpeg.input(mediapath).audio
+    # audio = ffmpeg.input(mediapath)
+    audio = ffmpeg.input(mediapath).audio
+    # does not have audio track, so error occurs.
     # might have exception. what to do with it then??
     volDict = {}
     error = False
