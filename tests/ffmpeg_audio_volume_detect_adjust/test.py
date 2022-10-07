1e971360d54f3f8c37670fe18d44d8113d69fe0a
GitJournal <app@gitjournal.io>
2022-10-07 16:08:15 +0800

    [2022-10-07 08:08:13]
    FILE:
    tests/ffmpeg_audio_volume_detect_adjust/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/ffmpeg_audio_volume_detect_adjust/test.py
+++ tests/ffmpeg_audio_volume_detect_adjust/test.py
@@ -120,7 +120,7 @@ import subprocess
 
 
 def adjustVolumeInMedia(
-    mediaPath, outputPath, targets={"mean": -10.8, "max": 0.0}, overwrite_output=False, algorithm=[]
+    mediaPath, outputPath, targets={"mean": -10.8, "max": 0.0}, overwrite_output=False, algorithm:Literal[]
 ):  # must set target volume.
 
     # use ffmpeg-normalize?
