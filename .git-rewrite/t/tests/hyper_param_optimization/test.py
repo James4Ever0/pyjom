a5f65264e3a010f475d502bc4b3f2e8b44586c64
GitJournal <app@gitjournal.io>
2022-10-07 19:16:15 +0800

    [2022-10-07 11:16:11]
    FILE:
    .git-rewrite/t/tests/hyper_param_optimization/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- .git-rewrite/t/tests/hyper_param_optimization/test.py
+++ .git-rewrite/t/tests/hyper_param_optimization/test.py
@@ -3,14 +3,22 @@
 
 from hyperopt import hp
 
-space = hp.choice(
-    "a",
-    [("case 1", 1 + hp.lognormal("c1", 0, 1)), ("case 2", hp.uniform("c2", -10, 10))],
-)
-
+# space = hp.choice(
+#     "a",
+#     [("case 1", 1 + hp.lognormal("c1", 0, 1)), ("case 2", hp.uniform("c2", -10, 10))],
+# )
 import hyperopt.pyll.stochastic as stochastic
+
+space = hp.choice("lambda",[lambda :1, lambda:2]) # if it is lambda, function will not resolve. however, after passing this thing into the main criterion function, it will utilize the lambda function.
 
 for _ in range(10):
     sample = stochastic.sample(space)
-    print("SAMPLE: ", sample) # this will return the tuple. can we put some custom functions here?
-    # there must be some integrations with custom functions. 
+    print("SAMPLE:", sample) # this will return the tuple. can we put some custom functions here?
+    # there must be some integrations with custom functions. for example: scikit-learn
+
+print("_______________________________") # splited.
+
+from hyperopt.pyll import scope
+
+@scope.define
+def my_func(a,b=1):
