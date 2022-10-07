51f3effc9e754e078c3bb4df0ece4b11b33704eb
GitJournal <app@gitjournal.io>
2022-10-07 15:50:55 +0800

    [2022-10-07 07:50:55]
    FILE:
    tests/ffmpeg_audio_volume_detect_adjust/test_output_file_error.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/ffmpeg_audio_volume_detect_adjust/test_output_file_error.py
+++ tests/ffmpeg_audio_volume_detect_adjust/test_output_file_error.py
@@ -7,7 +7,8 @@ normalizedOutputPath = "normalized.mp4"
 # wtf are you talking about?
 adjustVolumeInMedia(output_path, normalizedOutputPath)
 detect_volume_average(normalizedOutputPath, debug=True)
-VOLUME NORMALIZATION SUCCESSFUL
-MEDIA PATH: normalized.mp4
-VOLUME: {'mean': -25.1, 'max': -8.8}
-ERROR STATUS: False
+# even worse with default settings.
+# VOLUME NORMALIZATION SUCCESSFUL
+# MEDIA PATH: normalized.mp4
+# VOLUME: {'mean': -25.1, 'max': -8.8}
+# ERROR STATUS: False
