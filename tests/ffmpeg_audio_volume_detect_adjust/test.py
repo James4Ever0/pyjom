6d149455043ab613585c7d5106651773d0d79a9b
GitJournal <app@gitjournal.io>
2022-10-08 00:42:54 +0800

    [2022-10-07 16:42:51]
    FILE:
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/ffmpeg_audio_volume_detect_adjust/test.py
+++ tests/ffmpeg_audio_volume_detect_adjust/test.py
@@ -28,7 +28,7 @@ import json
 #     assert returncode == 0
 #     print("RENDER SUCCESSFUL")
 
-
+# videotoolbox?
 def create_black_video_without_audio(duration, mediapath):
     # ffmpeg -f lavfi -i color=c=black:s=1280x720:r=5 -i audio.mp3 -crf 0 -c:a copy -shortest output.mp4
     # length is in seconds.
