ec7374fa79b2d51514057aba4c03c04c295568e0
GitJournal <app@gitjournal.io>
2022-10-08 02:24:43 +0800

    [2022-10-07 18:24:41]
    FILE:
    tests/test_auto_dog_video_giphy_online_producer.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/test_auto_dog_video_giphy_online_producer.py
+++ tests/test_auto_dog_video_giphy_online_producer.py
@@ -13,15 +13,15 @@ wbRev = OnlineAutoContentProducer(
         {
             "music": {
                 "filepath": "/root/Desktop/works/pyjom/tests/music_analysis/exciting_bgm.mp3",  # these things were not right.
-                "lyric_path": "/root/Desktop/works/pyjom/tests/music_analysis/exciting_bgm.lrc", ## you can choose not to pass the lyric_path anyway. also format different than .lrc is on the way?
+                "lyric_path": "/root/Desktop/works/pyjom/tests/music_analysis/exciting_bgm.lrc",  ## you can choose not to pass the lyric_path anyway. also format different than .lrc is on the way?
             },
             "font": "/root/.local/share/fonts/simhei.ttf",
             # "font": "/root/.local/share/fonts/simyou.ttf", # 幼圆可能打不出来
             "policy": {},
             "maxtime": 7.8,
-            "mintime": 2, # we've write this shit!
+            "mintime": 2,  # we've write this shit!
             "fast": True,  # pass this flag to medialang export engine
-            "render_ass":True
+            "render_ass": True,
         }
     ],
     # you can also translate funny videos from youtube.
@@ -35,17 +35,19 @@ def completeTest():
     wbRev.main()
 
 
-def partialMedialangRenderTest(medialangScript,medialangTmpdir,verbose=True):
+def partialMedialangRenderTest(medialangScript, medialangTmpdir, verbose=True):
     # copy that script to my dear clipboard please?
-    medialangObject = Medialang(script=medialangScript, verbose=verbose, medialangTmpdir=medialangTmpdir)
+    medialangObject = Medialang(
+        script=medialangScript, verbose=verbose, medialangTmpdir=medialangTmpdir
+    )
     result = medialangObject.execute()
     return result
 
 
-def PMRT_0(scriptFilePath,medialangTmpdir, verbose=True):
+def PMRT_0(scriptFilePath, medialangTmpdir, verbose=True):
     with open(scriptFilePath, "r") as f:
         medialangScript = f.read()
-    return partialMedialangRenderTest(medialangScript,medialangTmpdir, verbose=verbose)
+    return partialMedialangRenderTest(medialangScript, medialangTmpdir, verbose=verbose)
 
 
 from lazero.filesystem import tmpdir
@@ -81,8 +83,9 @@ from lazero.filesystem import tmpdir
 #         return False
 if __name__ == "__main__":
     import argparse
+
     parser = argparse.ArgumentParser()
-    parser.add_argument("-p","--partial", action="store_true", default=False)
+    parser.add_argument("-p", "--partial", action="store_true", default=False)
     args = parser.parse_args()
 
     COMPLETE_TEST = not args.partial
@@ -96,9 +99,11 @@ if __name__ == "__main__":
         # a special hack
         # import tempfile
         with tmpdir(path="/dev/shm/medialang") as medialangTmpdir:
-            print("MEDIALANG SUPER TMPDIR:", medialangTmpdir) # as some sort of protection.
+            print(
+                "MEDIALANG SUPER TMPDIR:", medialangTmpdir
+            )  # as some sort of protection.
             # /dev/shm/medialang/<randomString>/<randomUUID>.mp4 -> /dev/shm/medialang/<randomUUID>.mp4
-            result = PMRT_0(scriptFilePath, medialangTmpdir,verbose=False)
+            result = PMRT_0(scriptFilePath, medialangTmpdir, verbose=False)
             editly_outputPath, medialang_item_list = result  # this just return none!
             # data -> editly json
             # this output path is modified. we shall change this.
