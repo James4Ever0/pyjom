7d9312a8fcedf185390f9d324f5b77225072bdc1
GitJournal <app@gitjournal.io>
2022-10-08 04:09:59 +0800

    [2022-10-07 20:08:17]
    FILE:
    tests/aiohttp_python_clash_delay_proxy_set_proxy/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

---
+++ tests/aiohttp_python_clash_delay_proxy_set_proxy/download_from_multiple_websites_at_once.py
@@ -0,0 +1,38 @@
+import asyncio
+from contextlib import closing
+
+import aiohttp
+
+
+async def download_file(session: aiohttp.ClientSession, url: str):
+    async with session.get(url) as response:
+        assert response.status == 200
+        # For large files use response.content.read(chunk_size) instead.
+        return url, await response.read()
+
+
+@asyncio.coroutine
+def download_multiple(session: aiohttp.ClientSession):
+    urls = (
+        'http://cnn.com',
+        'http://nytimes.com',
+        'http://google.com',
+        'http://leagueoflegends.com',
+        'http://python.org',
+    )
+    download_futures = [download_file(session, url) for url in urls]
+    print('Results')
+    for download_future in asyncio.as_completed(download_futures):
+        result = yield from download_future
+        print('finished:', result)
+    return urls
+
+
+def main():
+    with closing(asyncio.get_event_loop()) as loop:
+        with aiohttp.ClientSession() as session:
+            result = loop.run_until_complete(download_multiple(session))
+            print('finished:', result)
+
+
+main()
