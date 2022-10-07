914c4a754c3ee7a48817c6b8e22527007e30b75e
GitJournal <app@gitjournal.io>
2022-10-07 15:47:28 +0800

    [2022-10-07 07:47:27]
    FILE:
    tests/ffmpeg_audio_volume_detect_adjust/test_output_file_error.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/ffmpeg_audio_volume_detect_adjust/test_output_file_error.py
+++ tests/ffmpeg_audio_volume_detect_adjust/test_output_file_error.py
@@ -3,5 +3,7 @@ from test import detect_volume_average, adjustVolumeInMedia
 output_path = "volDetect_test.mp4"
 # detect_volume_average(output_path, debug=True)
 normalizedOutputPath = "normalized.mp4"
+# Output extension mp4 does not support PCM audio. Please choose a suitable audio codec with the -c:a option.
+# wtf are you talking about?
 adjustVolumeInMedia(output_path, normalizedOutputPath)
 detect_volume_average(normalizedOutputPath, debug=True)
