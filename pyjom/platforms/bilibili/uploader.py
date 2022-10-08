6f05845634f9099f074a5db26fdeff215a5d8645
GitJournal <app@gitjournal.io>
2022-10-08 23:10:50 +0800

    [2022-10-08 15:10:48]
    FILE:
    pyjom/platforms/bilibili/uploader.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/platforms/bilibili/uploader.py
+++ pyjom/platforms/bilibili/uploader.py
@@ -22,7 +22,7 @@ async def main(sessdata="", bili_jct="", buvid3=""):
             "up_close_danmaku": close_danmaku,
             "up_close_reply": close_reply
         }
-    page = video_uploader.VideoUploaderPage(video_stream=open(videoPath, 'rb'), title='test', description='', extension=videoExtension)
+    page = video_uploader.VideoUploaderPage(video_stream=open(videoPath, 'rb'), title=title, description='', extension=videoExtension)
     uploader = video_uploader.VideoUploader([page], meta, credential, threads=1)
 
     @uploader.on("__ALL__")
