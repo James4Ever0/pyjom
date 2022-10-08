25a4e7c34ba8beee0f481946431324b183ff0cfc
GitJournal <app@gitjournal.io>
2022-10-08 14:27:22 +0800

    [2022-10-08 06:27:21]
    FILE:
    tests/aiohttp_python_clash_delay_proxy_set_proxy/download_from_multiple_websites_at_once.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/aiohttp_python_clash_delay_proxy_set_proxy/download_from_multiple_websites_at_once.py
+++ tests/aiohttp_python_clash_delay_proxy_set_proxy/download_from_multiple_websites_at_once.py
@@ -1,37 +1,14 @@
-import asyncio
-from contextlib import closing
-
 import aiohttp
+import asyncio
 
-
-async def download_file(session: aiohttp.ClientSession, url: str):
-    async with session.get(url) as response:
-        assert response.status == 200
-        # For large files use response.content.read(chunk_size) instead.
-        return url, await response.read()
-
-
-@asyncio.coroutine
-def download_multiple(session: aiohttp.ClientSession):
-    urls = (
-        'http://cnn.com',
-        'http://nytimes.com',
-        'http://google.com',
-        'http://leagueoflegends.com',
-        'http://python.org',
-    )
-    download_futures = [download_file(session, url) for url in urls]
-    print('Results')
-    for download_future in asyncio.as_completed(download_futures):
-        result = yield from download_future
-        print('finished:', result)
-    return urls
-
-async def main(loop):
-    # with closing(asyncio.get_event_loop()) as loop:
-    async with aiohttp.ClientSession(loop=loop) as session:
-        result = [wx for x in download_multiple(session)]
-        print('finished:', result)
-
+async def get(url):
+    async with aiohttp.ClientSession() as session:
+          async with session.get(url):
+                return response
 loop = asyncio.get_event_loop()
-loop.run_until_complete(main(loop))
+
+multiple_requests = [get("http://your-website.com") for _ in range(10)]
+
+results = loop.run_until_complete(asyncio.gather(*multiple_requests))
+
+print("Results: %s" % results)
