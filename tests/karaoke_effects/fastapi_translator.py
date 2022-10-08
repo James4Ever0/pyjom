b602f65a5ad9c3468a7c5e782aec09a80a046979
GitJournal <app@gitjournal.io>
2022-10-09 05:11:13 +0800

    [2022-10-08 21:11:11]
    FILE:
    tests/karaoke_effects/fastapi_translator.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/karaoke_effects/fastapi_translator.py
+++ tests/karaoke_effects/fastapi_translator.py
@@ -83,10 +83,11 @@ def deeplTranslator(text, sleep=2, timeout=5, mod=40):
     useProxy(False)
     import random
 
-    refreshProxyCounter += 1
     if refreshProxyCounter % mod == (mod - 1):
         proxyList = getTestedProxyList()
         refreshProxyCounter = 0
+    refreshProxyCounter += 1
+
     proxyName = random.choice([proxy["name"] for proxy in proxyList] + ["DIRECT"])
     setProxyWithSelector(proxyName)
     # better use proxy instead. you need to config it here, and make sure the deepl adaptor uses the proxy.
