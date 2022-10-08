4324b904702f3a4c68c99725082bb0e6877a2701
GitJournal <app@gitjournal.io>
2022-10-09 01:00:33 +0800

    [2022-10-08 17:00:31]
    FILE:
    tests/bilibili_login/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_login/test.py
+++ tests/bilibili_login/test.py
@@ -1,7 +1,7 @@
 from bilibili_api import login, user, sync
 print("请登录：")
 credential = login.login_with_qrcode() # what are you doing here?
-# this is some GUI 
+# this is some GUI function. no terminal support!
 try:
     credential.raise_for_no_bili_jct() # 判断是否成功
     credential.raise_for_no_sessdata() # 判断是否成功
