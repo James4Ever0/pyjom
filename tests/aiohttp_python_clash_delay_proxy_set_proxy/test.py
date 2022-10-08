ea8563ba362d61ca093a0618d8dd8e1e872f3fab
GitJournal <app@gitjournal.io>
2022-10-08 18:37:27 +0800

    [2022-10-08 10:37:24]
    FILE:
    tests/aiohttp_python_clash_delay_proxy_set_proxy/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/aiohttp_python_clash_delay_proxy_set_proxy/test.py
+++ tests/aiohttp_python_clash_delay_proxy_set_proxy/test.py
@@ -42,4 +42,4 @@ if __name__ == "__main__":
     # pprint.pprint(result)
     delayList = testProxyList(proxyList)
     proxyDelayList = zip(delayList,proxyList)
-    for delay, proxyName in 
+    for delay, proxyName in delayList:
