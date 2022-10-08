a2acd8743585b3feb22021692705eb6dd3b16171
GitJournal <app@gitjournal.io>
2022-10-08 19:45:24 +0800

    [2022-10-08 11:45:23]
    FILE:
    tests/aiohttp_python_clash_delay_proxy_set_proxy/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/aiohttp_python_clash_delay_proxy_set_proxy/test.py
+++ tests/aiohttp_python_clash_delay_proxy_set_proxy/test.py
@@ -41,7 +41,7 @@ def setProxyWithSelector(
     proxyName, selector="GLOBAL", port: int = 9911, debug=False
 ):  # how to make sure it will use 'GLOBAL'? it needs to be done with the config.
     if debug:
-        print('select proxy %s with selector %s' % (proxyName, selector))
+        print("select proxy %s with selector %s" % (proxyName, selector))
     clashUrl = localhostWithPort(port) + "/proxies/{}".format(selector)
     r = requests.put(
         clashUrl, data=json.dumps({"name": proxyName}, ensure_ascii=False).encode()
@@ -63,7 +63,9 @@ def setProxyWithSelector(
 def setProxyConfig(
     port: int = 9911,
     http_port: Union[None, int] = None,
-    mode: Literal["Global", "Rule", "Direct", None] = None, # currently this mode is configured as 'rule' so everything related to 'deepl' will be redirected.
+    mode: Literal[
+        "Global", "Rule", "Direct", None
+    ] = None,  # currently this mode is configured as 'rule' so everything related to 'deepl' will be redirected.
 ):
     # https://clash.gitbook.io/doc/restful-api/config
     # sure you can patch more things but that's enough for now.
@@ -83,13 +85,14 @@ def getConnectionGateway(
     clashUrl = localhostWithPort(port) + "/configs"
     r = requests.get(clashUrl)
     configs = r.json()
-    http_port = configs['port']
+    http_port = configs["port"]
     gateway = localhostWithPort(http_port)
     return gateway
 
 
 if __name__ == "__main__":
     from pprint import pprint
+
     validProxyDelayList = []
     proxyList = getProxyList(debug=True)
     # pprint.pprint(result)
@@ -102,24 +105,24 @@ if __name__ == "__main__":
             info.update({"name": proxyName})
             validProxyDelayList.append(info)
     validProxyDelayList.sort(key=lambda x: x["delay"])
-#     pprint(gateway)
-#     {'allow-lan': True,
-#  'authentication': [],
-#  'bind-address': '*',
-#  'ipv6': False,
-#  'log-level': 'info',
-#  'mixed-port': 0,
-#  'mode': 'rule',
-#  'port': 8381,
-#  'redir-port': 0,
-#  'socks-port': 0,
-#  'tproxy-port': 0}
+    #     pprint(gateway)
+    #     {'allow-lan': True,
+    #  'authentication': [],
+    #  'bind-address': '*',
+    #  'ipv6': False,
+    #  'log-level': 'info',
+    #  'mixed-port': 0,
+    #  'mode': 'rule',
+    #  'port': 8381,
+    #  'redir-port': 0,
+    #  'socks-port': 0,
+    #  'tproxy-port': 0}
     gateway = getConnectionGateway()
-    validProxyName = validProxyDelayList[3]['name']
+    validProxyName = validProxyDelayList[3]["name"]
     setProxyWithSelector(validProxyName, debug=True)
     # now use the proxy!
-    r = requests.get('https://deepl.com', proxies={'http': gateway,'https':gateway})
+    r = requests.get("https://deepl.com", proxies={"http": gateway, "https": gateway})
     print()
     print(r.content[:100])
     print(r.status_code)
-    print('deepl response')
+    print("deepl response")
