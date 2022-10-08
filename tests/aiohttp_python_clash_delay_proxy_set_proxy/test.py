5a75b7d146b775dc2c9b8b3846e390f8a2f5ec1f
GitJournal <app@gitjournal.io>
2022-10-08 18:32:20 +0800

    [2022-10-08 10:32:18]
    FILE:
    tests/aiohttp_python_clash_delay_proxy_set_proxy/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/aiohttp_python_clash_delay_proxy_set_proxy/test.py
+++ tests/aiohttp_python_clash_delay_proxy_set_proxy/test.py
@@ -24,7 +24,6 @@ def testProxyList(
 ):  # test the speed for given url
     # first, generate the proper list of requests.
     params = {'timeout': timeout, 'url': url}
-
     for proxyName in proxyList:
         testUrl = localhostWithPort(port) + "/proxies/{}/delay".format(proxyName)
         r = requests.get(testUrl, params=params)
