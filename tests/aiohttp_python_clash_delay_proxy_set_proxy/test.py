2b87bee43c26eee95a1d0acbab261666249985c2
GitJournal <app@gitjournal.io>
2022-10-08 19:13:58 +0800

    [2022-10-08 11:13:55]
    FILE:
    tests/aiohttp_python_clash_delay_proxy_set_proxy/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/aiohttp_python_clash_delay_proxy_set_proxy/test.py
+++ tests/aiohttp_python_clash_delay_proxy_set_proxy/test.py
@@ -46,7 +46,7 @@ def setProxyWithSelector(proxyName, selector='GLOBAL',port=9911):
         try:
             print(r.content)
             print('error code:', r.status_code)
-        print("error when setting proxy with selector"")
+        print("error when setting proxy %s with selector %s" % (proxyName, selector))
 
 
 def getConnectionGateway(port=9911):  # get the clash local http proxy connection port.
