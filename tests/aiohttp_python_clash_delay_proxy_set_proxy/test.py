e7328446a2c794a466ed45347ccff89279cf1352
GitJournal <app@gitjournal.io>
2022-10-08 14:43:18 +0800

    [2022-10-08 06:43:16]
    FILE:
    tests/aiohttp_python_clash_delay_proxy_set_proxy/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/aiohttp_python_clash_delay_proxy_set_proxy/test.py
+++ tests/aiohttp_python_clash_delay_proxy_set_proxy/test.py
@@ -2,8 +2,10 @@ from download_from_multiple_websites_at_once import concurrentGet
 
 # so, how do you get the proxy list and test the speed for deepl.com?
 def getProxyList():
+    ...
 
 def testProxyList():
+    ...
 
 def setProxy():
     ...
