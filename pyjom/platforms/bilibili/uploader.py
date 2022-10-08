d7b56bc385f62d7b252f78b7edc8b46853c5dd2d
GitJournal <app@gitjournal.io>
2022-10-08 23:12:58 +0800

    [2022-10-08 15:12:57]
    FILE:
    pyjom/platforms/bilibili/uploader.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/platforms/bilibili/uploader.py
+++ pyjom/platforms/bilibili/uploader.py
@@ -1,6 +1,6 @@
 from bilibili_api import sync, video_uploader, Credential
 
-async def main(sessdata="", bili_jct="", buvid3=""):
+async def main(sessdata="", bili_jct="", buvid3="",):
     credential = Credential(sessdata=sessdata, bili_jct=bili_jct, buvid3=buvid3)
     # 具体请查阅相关文档
     meta = {
