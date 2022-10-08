80255ece1a8342e2f3bddd7c01466279d518e9ae
GitJournal <app@gitjournal.io>
2022-10-08 20:11:35 +0800

    [2022-10-08 12:11:33]
    FILE:
    tests/aiohttp_python_clash_delay_proxy_set_proxy/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/aiohttp_python_clash_delay_proxy_set_proxy/test.py
+++ tests/aiohttp_python_clash_delay_proxy_set_proxy/test.py
@@ -129,7 +129,7 @@ if __name__ == "__main__":
     #  'socks-port': 0,
     #  'tproxy-port': 0}
     gateway = getConnectionGateway()
-    validProxyName = validProxyDelayList[11]["name"]
+    validProxyName = validProxyDelayList[2]["name"]
     setProxyConfig(mode="Global")
     # you can switch to 'Rule' if you want the baidu translation
     setProxyWithSelector(validProxyName, debug=True)
