8b98495af98b582c90ac67cdd643bff52b2c1f26
GitJournal <app@gitjournal.io>
2022-10-08 17:36:57 +0800

    [2022-10-08 09:36:55]
    FILE:
    tests/aiohttp_python_clash_delay_proxy_set_proxy/download_from_multiple_websites_at_once.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/aiohttp_python_clash_delay_proxy_set_proxy/download_from_multiple_websites_at_once.py
+++ tests/aiohttp_python_clash_delay_proxy_set_proxy/download_from_multiple_websites_at_once.py
@@ -3,15 +3,17 @@ import asyncio
 
 # clearly it is not clean enough.
 # also i worry about the memory leakage, open file limit exceeding.
-async def get(url, processor=lambda x: x, params = {}):
+async def get(url, processor=lambda x: x, params={}):
     async with aiohttp.ClientSession() as session:
         async with session.get(url, params=params) as response:
             return processor(response)
 
 
-def concurrentGet(url_list, processor=lambda x: x, params = {} debug=False):
+def concurrentGet(url_list, processor=lambda x: x, params={}, debug=False):
     loop = asyncio.get_event_loop()
-    multiple_requests = [get(url, processor=processor, params=params) for url in url_list]
+    multiple_requests = [
+        get(url, processor=processor, params=params) for url in url_list
+    ]
     results = loop.run_until_complete(asyncio.gather(*multiple_requests))
     if debug:
         print("Results: %s" % results)
