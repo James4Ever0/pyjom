0fdf0db8f29357eb49f86d2df2ec27b7e684f844
GitJournal <app@gitjournal.io>
2022-10-09 00:59:54 +0800

    [2022-10-08 16:59:52]
    FILE:
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/bilibili_login/
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

---
+++ tests/bilibili_login/test.py
@@ -0,0 +1,10 @@
+from bilibili_api import login, user, sync
+print("请登录：")
+credential = login.login_with_qrcode()
+try:
+    credential.raise_for_no_bili_jct() # 判断是否成功
+    credential.raise_for_no_sessdata() # 判断是否成功
+except:
+    print("登陆失败。。。")
+    exit()
+print("欢迎，", sync(user.get_self_info(credential))['name'], "!")
