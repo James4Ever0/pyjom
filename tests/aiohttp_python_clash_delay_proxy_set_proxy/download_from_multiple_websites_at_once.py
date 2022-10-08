968c71aa65ed57717301efd351fb92c1d593ac8f
GitJournal <app@gitjournal.io>
2022-10-08 14:01:29 +0800

    [2022-10-08 06:01:28]
    FILE:
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/aiohttp_python_clash_delay_proxy_set_proxy/download_from_multiple_websites_at_once.py
+++ tests/aiohttp_python_clash_delay_proxy_set_proxy/download_from_multiple_websites_at_once.py
@@ -34,4 +34,4 @@ async def main():
             result = loop.run_until_complete(download_multiple(session))
             print('finished:', result)
 
-asyncio.run_coroutine_threadsafe(main)
+asyncio.run_coroutine_threadsafe(main, )
