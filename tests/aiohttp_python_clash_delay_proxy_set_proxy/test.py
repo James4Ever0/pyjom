47d62836c46d66f68c6463a05651ead8fcb37204
GitJournal <app@gitjournal.io>
2022-10-08 14:45:47 +0800

    [2022-10-08 06:45:46]
    FILE:
    tests/aiohttp_python_clash_delay_proxy_set_proxy/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/aiohttp_python_clash_delay_proxy_set_proxy/test.py
+++ tests/aiohttp_python_clash_delay_proxy_set_proxy/test.py
@@ -4,7 +4,7 @@ from download_from_multiple_websites_at_once import concurrentGet
 def getProxyList(port=...):
     ...
 
-def testProxyList(proxyList,port=...): # test the speed for given url
+def testProxyList(proxyList,port=..., url=..., timeout=...): # test the speed for given url
     ...
 
 def setProxy(proxy, port=...):
