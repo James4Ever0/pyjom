2dbf7fd6f13e0b08149eeb0a4dbd67af1271932e
GitJournal <app@gitjournal.io>
2022-10-08 21:22:46 +0800

    [2022-10-08 13:22:38]
    FILE:
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/karaoke_effects/fastapi_translator.py
+++ tests/karaoke_effects/fastapi_translator.py
@@ -75,7 +75,7 @@ from lazero.network.proxy.clash import (
     clashProxyStateManager,
 )
 
-proxyList = getProxyList()
+proxyList = getProxyList(timeout=5000)
 refreshProxyCounter = 0
 
 
@@ -86,7 +86,7 @@ def deeplTranslator(text, sleep=2, timeout=5, mod=40):
 
     refreshProxyCounter += 1
     if refreshProxyCounter % mod == (mod - 1):
-        proxyList = getProxyList()
+        proxyList = getProxyList(timeout=5000)
     proxyName = random.choice([proxy["name"] for proxy in proxyList] + ["DIRECT"])
     setProxyWithSelector(proxyName)
     # better use proxy instead. you need to config it here, and make sure the deepl adaptor uses the proxy.
