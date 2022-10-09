67308308497fe6a0498923068154a4521b18eb6b
GitJournal <app@gitjournal.io>
2022-10-09 20:29:33 +0800

    [2022-10-09 12:29:31]
    FILE:
    pyjom/platforms/bilibili/credentials.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/platforms/bilibili/credentials.py
+++ pyjom/platforms/bilibili/credentials.py
@@ -48,7 +48,7 @@ def removeCredentialByDedeUserId(dedeuserid):
         return False
 
 
-def getCredentialByDedeUserId(dedeuserid):
+def getCredentialByDedeUserId(dedeuserid:str="397424026"):
     dataList = db.search(User.dedeuserid == dedeuserid)
     if len(dataList) !=1:
         if len(dataList) != 0:
@@ -75,10 +75,12 @@ def getCredentialByDedeUserId(dedeuserid):
             removeCredentialByDedeUserId(dedeuserid)
     # anyway if you are here, nothing in database related to this dedeuserid now.
     # you choose to login via SMS.
-    phone = input("请输入手机号：")
-    print("正在登录。")
-    send_sms(PhoneNumber(phone, country="+86"))  # 默认设置地区为中国大陆
-    code = input("请输入验证码：")
-    c = login_with_sms(PhoneNumber(phone, country="+86"), code)
-    credential = c
-    print("登录成功")
+    while True:
+        phone = input("请输入手机号：")
+        print("正在登录。")
+        send_sms(PhoneNumber(phone, country="+86"))  # 默认设置地区为中国大陆
+        code = input("请输入验证码：")
+        c = login_with_sms(PhoneNumber(phone, country="+86"), code)
+        credential = c
+        
+        print("登录成功")
