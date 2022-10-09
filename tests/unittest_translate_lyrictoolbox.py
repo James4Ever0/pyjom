f68a67fc6c9985e25a6b1d46748b7b5aff07ee7e
GitJournal <app@gitjournal.io>
2022-10-09 13:39:12 +0800

    [2022-10-09 05:39:10]
    FILE:
    tests/unittest_translate_lyrictoolbox.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_translate_lyrictoolbox.py
+++ tests/unittest_translate_lyrictoolbox.py
@@ -3,6 +3,6 @@ from pyjom.lyrictoolbox import translate
 from lazero.utils.logger import sprint
 sources = ['are you ok', 'are you happy','are you good','are you satisfied']
 for source in sources:
-    result = translate(source, backend='baidu') # this is cached. no matter what backend you use.
+    result = translate(source, backend='deepl') # this is cached. no matter what backend you use.
     print('source:',source)
     sprint('result:',result)
