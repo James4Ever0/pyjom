f62455990f549ba94e2a128971f6114acf51a4db
GitJournal <app@gitjournal.io>
2022-10-10 00:58:38 +0800

    [2022-10-09 16:58:36]
    FILE:
    pyjom/modules/feedbackCollecting/dummyFeedback.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/feedbackCollecting/dummyFeedback.py
+++ pyjom/modules/feedbackCollecting/dummyFeedback.py
@@ -2,7 +2,7 @@ from pyjom.commons import *
 
 
 @decorator
-def dummyFeedback(content, iterate=False):
+def dummyFeedback(content, iterate=False): # anyway, it is dummy. i don't expect nothing.
     if iterate:
         for _ in content:
             ...
