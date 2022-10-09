28da452ed449adc0f699c74ebc5ef6ca73b75d4d
GitJournal <app@gitjournal.io>
2022-10-10 00:15:24 +0800

    [2022-10-09 16:15:22]
    FILE:
    pyjom/primitives/onlinePrimitives.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/primitives/onlinePrimitives.py
+++ pyjom/primitives/onlinePrimitives.py
@@ -47,7 +47,7 @@ class OnlineAutoContentProducer(ContentProducer):
                     template_configs=template_configs,
                 ),
                 "poster": keywordDecorator(
-                    BilibiliPoster, iterate=True, contentType=contentType,postMeta
+                    BilibiliPoster, iterate=True, contentType=contentType,postMetadataGenerator=postMetadataGenerator,
                 )  # just for debugging.
                 # you also need to change the logic below, for other 'dummy' stuffs.
                 # 'poster':keywordDecorator(dummyPoster, iterate=True) # just for debugging.
