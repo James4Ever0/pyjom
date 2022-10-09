f5b4a827577a38bc7cf098adc96b613adc0b6498
GitJournal <app@gitjournal.io>
2022-10-10 02:13:42 +0800

    [2022-10-09 18:13:40]
    FILE:
    pyjom/modules/globalOptimizer/dummyOptimizer.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/globalOptimizer/dummyOptimizer.py
+++ pyjom/modules/globalOptimizer/dummyOptimizer.py
@@ -1,15 +1,19 @@
 from pyjom.commons import *
-from lazero.program.functools import iterateWithTempDirectory # you can also switch to 'AUTO'
+from lazero.program.functools import (
+    iterateWithTempDirectory,
+)  # you can also switch to 'AUTO'
 
-@decorator # called 'iterateWithTempDirectory'
-def dummyOptimizer(topic, feedback): # wtf is this?
+
+@decorator  # called 'iterateWithTempDirectory'
+def dummyOptimizer(topic, feedback):  # wtf is this?
     # not optimized. need schedule.
     @iterateWithTempDirectory()
     def inner(elem):
-        print('current topic: %s' % topic)
+        print("current topic: %s" % topic)
         print("from feedback:", elem)
         return "pending"
-    return inner(feedback, iterate=iterate)
+
+    return inner(feedback)
 
 
 @decorator
