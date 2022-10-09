9101f30b77b0dd58a7446b5d0f51b14246de9c68
GitJournal <app@gitjournal.io>
2022-10-10 01:58:51 +0800

    [2022-10-09 17:58:49]
    FILE:
    tests/generator_yield_from_python_extract_element_one_by_one/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/generator_yield_from_python_extract_element_one_by_one/test.py
+++ tests/generator_yield_from_python_extract_element_one_by_one/test.py
@@ -36,6 +36,7 @@ def generator3(myGenerator, tempfile):
 
 if __name__ == "__main__":
     tempfile = "tmp_test"
+    generator
     if os.path.exists(tempfile):
         os.remove(tempfile)
     myGenerator = generator2(tempfile)
