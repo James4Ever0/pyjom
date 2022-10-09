9958dfe20fa44a85d6157f0be6e7bc5533fc3314
GitJournal <app@gitjournal.io>
2022-10-09 15:05:58 +0800

    [2022-10-09 07:05:56]
    FILE:
    tests/bilibili_login/test_login_successiful.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_login/test_login_successiful.py
+++ tests/bilibili_login/test_login_successiful.py
@@ -1,6 +1,6 @@
 from bilibili_api.user import get_self_info
 # from bilibili_api import settings
 # how to load credential from our stored things?
-credential = 
+credential = Credential()
 from bilibili_api import sync
 name = sync(get_self_info(credential))['name']
