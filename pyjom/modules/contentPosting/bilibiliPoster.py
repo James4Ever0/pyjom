d2cec80e5cbfd95a2aef8ebfc27548581090be4e
GitJournal <app@gitjournal.io>
2022-10-10 00:07:01 +0800

    [2022-10-09 16:06:59]
    FILE:
    pyjom/modules/contentPosting/bilibiliPoster.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/contentPosting/bilibiliPoster.py
+++ pyjom/modules/contentPosting/bilibiliPoster.py
@@ -13,22 +13,22 @@ contentType='video', dedeuserid:str = "397424026", tempdir = '/dev/shm/medialang
     getPostMetadata = lambda: postMetadataGenerator.__next__()
     def postContent(elem): # what is this elem? please check for video producer.
         with tmpdir(path=tempdir):
-        postMetadata = getPostMetadata()
-        print("READY TO POST CONTENT FROM:", elem)# this elem is video location for me.
-        if contentType == 'video': # single video upload without grouping.
-            videoPath = elem
-            cover_path = getRandomFileNameUnderDirectoryWithExtension('png',tempdir)
-            cover_target, mTagSeries, mTitle, mBgm, mDescription, dog_or_cat_original, search_tid = postMetadata # assumptions on video type.
-            # what is the bgm here used for?
-            # you can fetch this from database. you can pickle this thing.
-            tagString = ",".join(mTagSeries)
-            with tmpfile(cover_path):
-                cv2.imwrite(cover_path, cover_target)
-            # you need to save this 'cover_target' to file.
-                contentId = uploadVideo(dedeuserid = dedeuserid,description = mDescription,dynamic=mDescription, tagString = tagString,tagId = search_tid,cover_path=cover_path, videoPath=videoPath, title=mTitle) # choose to upload and get bvid.
-        else:
-            raise Exception('unknown content type to upload for bilibiliPoster:', contentType)
-        return "bilibili://{}/{}/{}".format(dedeuserid, contentType, contentId)
+            postMetadata = getPostMetadata()
+            print("READY TO POST CONTENT FROM:", elem)# this elem is video location for me.
+            if contentType == 'video': # single video upload without grouping.
+                videoPath = elem
+                cover_path = getRandomFileNameUnderDirectoryWithExtension('png',tempdir)
+                cover_target, mTagSeries, mTitle, mBgm, mDescription, dog_or_cat_original, search_tid = postMetadata # assumptions on video type.
+                # what is the bgm here used for?
+                # you can fetch this from database. you can pickle this thing.
+                tagString = ",".join(mTagSeries)
+                with tmpfile(cover_path):
+                    cv2.imwrite(cover_path, cover_target)
+                # you need to save this 'cover_target' to file.
+                    contentId = uploadVideo(dedeuserid = dedeuserid,description = mDescription,dynamic=mDescription, tagString = tagString,tagId = search_tid,cover_path=cover_path, videoPath=videoPath, title=mTitle) # choose to upload and get bvid.
+            else:
+                raise Exception('unknown content type to upload for bilibiliPoster:', contentType)
+            return "bilibili://{}/{}/{}".format(dedeuserid, contentType, contentId)
     
     def postContentIterate(content):
         for elem in content:
