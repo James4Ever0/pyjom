0c2bbe27a67b1a4974fc4321806e19e7d5d1aa77
GitJournal <app@gitjournal.io>
2022-10-09 15:57:57 +0800

    [2022-10-09 07:57:55]
    FILE:
    tests/bilibili_login/dump_favourite_list.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_login/dump_favourite_list.py
+++ tests/bilibili_login/dump_favourite_list.py
@@ -2,4 +2,4 @@
 from bilibili_api import favourite_list
 # that favourite list is public. i just want that.
 dedeuserid = "397424026"
-favourite_list = fget_video_favorite_list()
+favourite_list = favourite_list.get_video_favorite_list()
