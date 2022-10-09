c21b12e620bdd6431a5b9d93785a5332c9cf2e30
GitJournal <app@gitjournal.io>
2022-10-10 01:00:35 +0800

    [2022-10-09 17:00:33]
    FILE:
    pyjom/modules/globalOptimizer/dummyOptimizer.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/globalOptimizer/dummyOptimizer.py
+++ pyjom/modules/globalOptimizer/dummyOptimizer.py
@@ -4,6 +4,10 @@ from pyjom.commons import *
 @decorator
 def dummyOptimizer(topic, feedback):
     # not optimized. need schedule.
+        if not iterate:
+        content = [content]
+    for elem in content:
+        print("from poster:", elem)
     return "pending"
 
 
