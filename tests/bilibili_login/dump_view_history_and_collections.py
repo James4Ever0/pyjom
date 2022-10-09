220da0a1f6f77e89118252634c638ae6a572d11a
GitJournal <app@gitjournal.io>
2022-10-09 15:38:35 +0800

    [2022-10-09 07:38:34]
    FILE:
    tests/bilibili_login/dump_view_history_and_collections.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_login/dump_view_history_and_collections.py
+++ tests/bilibili_login/dump_view_history_and_collections.py
@@ -49,4 +49,4 @@ if len(dataList) == 1:
         for elem in result:
             # it has description.
             videoData = {key: elem[key] for key in ["bvid", "desc", "title"]}
-            db.upsert(videoData, User.bvid == videoData["bvid"])
+            dbHistory.upsert(videoData, User.bvid == videoData["bvid"])
