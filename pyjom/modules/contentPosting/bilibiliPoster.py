cacc620401ecbfea94dd05f5a30a4f12b5b0a281
GitJournal <app@gitjournal.io>
2022-10-10 00:03:39 +0800

    [2022-10-09 16:03:38]
    FILE:
    pyjom/modules/contentPosting/bilibiliPoster.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/contentPosting/bilibiliPoster.py
+++ pyjom/modules/contentPosting/bilibiliPoster.py
@@ -24,6 +24,8 @@ contentType='video', dedeuserid:str = "397424026", tempdir = '/dev/shm/medialang
                 cv2.imwrite(cover_path, cover_target)
             # you need to save this 'cover_target' to file.
                 contentId = uploadVideo(dedeuserid = dedeuserid,description = mDescription,dynamic=mDescription, tagString = tagString,tagId = search_tid,cover_path=cover_path, videoPath=videoPath) # choose to upload and get bvid.
+        else:
+            raise Exception('unknown content type ')
         return "bilibili://{}/{}/{}".format(dedeuserid, contentType, contentId)
     
     def postContentIterate(content):
