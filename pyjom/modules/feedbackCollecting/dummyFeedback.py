670ad8588304ec623cf0882ebc92f8da12e33536
GitJournal <app@gitjournal.io>
2022-10-10 02:10:40 +0800

    [2022-10-09 18:10:39]
    FILE:
    pyjom/modules/feedbackCollecting/dummyFeedback.py
    pyjom/modules/globalOptimizer/dummyOptimizer.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/feedbackCollecting/dummyFeedback.py
+++ pyjom/modules/feedbackCollecting/dummyFeedback.py
@@ -1,19 +1,13 @@
 from pyjom.commons import *
+from lazero.program.functools import iterateWithTempDirectory # you can also switch to 'AUTO'
 
 
 @decorator
 def dummyFeedback(
-    content, iterate=False # i think i need another function decorator for this. really? this will break pattern, for sure. temp files will be missing? so you need some tempdir decorator?
+    content, iterate='auto' # i think i need another function decorator for this. really? this will break pattern, for sure. temp files will be missing? so you need some tempdir decorator?
 ):  # anyway, it is dummy. i don't expect nothing.
+    @iterateWithTempDirectory #
     def inner(elem):
         print("from poster:", elem)
         return "pending"
-
-    def innerIterator(content):
-        for elem in content:
-            yield inner(elem)
-
-    if not iterate:
-        return inner(content)
-    else:
-        return innerIterator(content)
+    return 
--- pyjom/modules/globalOptimizer/dummyOptimizer.py
+++ pyjom/modules/globalOptimizer/dummyOptimizer.py
@@ -2,10 +2,11 @@ from pyjom.commons import *
 from lazero.program.functools import iterateWithTempDirectory # you can also switch to 'AUTO'
 
 @decorator # called 'iterateWithTempDirectory'
-def dummyOptimizer(topic, feedback, iterate=False): # wtf is this?
+def dummyOptimizer(topic, feedback, iterate='auto'): # wtf is this?
     # not optimized. need schedule.
     @iterateWithTempDirectory()
     def inner(elem):
+        print('current topic: %s' % topic)
         print("from feedback:", elem)
         return "pending"
     return inner(feedback, iterate=iterate)
