8ee04a6f391c622fe1fe4e965220629d35855c77
GitJournal <app@gitjournal.io>
2022-10-09 14:57:17 +0800

    [2022-10-09 06:57:16]
    FILE:
    tests/bilibili_login/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_login/test.py
+++ tests/bilibili_login/test.py
@@ -51,7 +51,7 @@ import os
 home = getHomeDirectory()
 dbPath = os.path.join(home,".bilibili_api.json")
 import tinydb
-db = tinydb.
+db = tinydb.database(dbPath)
 if credential != None:
     name = sync(get_self_info(credential))['name']
     print(f"欢迎，{name}!")
