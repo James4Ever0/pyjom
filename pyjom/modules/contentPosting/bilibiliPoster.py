765a77343a49c74c2de8de9d54c3e8803fa142ac
GitJournal <app@gitjournal.io>
2022-10-10 00:08:06 +0800

    [2022-10-09 16:08:04]
    FILE:
    pyjom/modules/contentPosting/bilibiliPoster.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/contentPosting/bilibiliPoster.py
+++ pyjom/modules/contentPosting/bilibiliPoster.py
@@ -1,38 +1,75 @@
 from pyjom.commons import *
 from pyjom.platforms.bilibili.uploader import uploadVideo
 from typing import Generator
-from lazero.filesystem.temp import tmpdir, getRandomFileNameUnderDirectoryWithExtension, tmpfile
+from lazero.filesystem.temp import (
+    tmpdir,
+    getRandomFileNameUnderDirectoryWithExtension,
+    tmpfile,
+)
+
 # that generator you must put beforehand.
 import cv2
+
+
 @decorator
-def BilibiliPoster(content, iterate=False, postMetadataGenerator:Generator=...,# must be a generator. a called generator function.
-contentType='video', dedeuserid:str = "397424026", tempdir = '/dev/shm/medialang/bilibiliPoster'):
+def BilibiliPoster(
+    content,
+    iterate=False,
+    postMetadataGenerator: Generator = ...,  # must be a generator. a called generator function.
+    contentType="video",
+    dedeuserid: str = "397424026",
+    tempdir="/dev/shm/medialang/bilibiliPoster",
+):
     # are you sure this 'postMetadataGenerator' will generate valid data for us?
     # anyway let's write for video.
     # there are two generators. what do you want?
     getPostMetadata = lambda: postMetadataGenerator.__next__()
-    def postContent(elem): # what is this elem? please check for video producer.
+
+    def postContent(elem):  # what is this elem? please check for video producer.
         with tmpdir(path=tempdir):
             postMetadata = getPostMetadata()
-            print("READY TO POST CONTENT FROM:", elem)# this elem is video location for me.
-            if contentType == 'video': # single video upload without grouping.
+            print(
+                "READY TO POST CONTENT FROM:", elem
+            )  # this elem is video location for me.
+            if contentType == "video":  # single video upload without grouping.
                 videoPath = elem
-                cover_path = getRandomFileNameUnderDirectoryWithExtension('png',tempdir)
-                cover_target, mTagSeries, mTitle, mBgm, mDescription, dog_or_cat_original, search_tid = postMetadata # assumptions on video type.
-                # what is the bgm here used for?
+                cover_path = getRandomFileNameUnderDirectoryWithExtension(
+                    "png", tempdir
+                )
+                (
+                    cover_target,
+                    mTagSeries,
+                    mTitle,
+                    mBgm,
+                    mDescription,
+                    dog_or_cat_original,
+                    search_tid,
+                ) = postMetadata  # assumptions on video type.
                 # you can fetch this from database. you can pickle this thing.
                 tagString = ",".join(mTagSeries)
                 with tmpfile(cover_path):
                     cv2.imwrite(cover_path, cover_target)
-                # you need to save this 'cover_target' to file.
-                    contentId = uploadVideo(dedeuserid = dedeuserid,description = mDescription,dynamic=mDescription, tagString = tagString,tagId = search_tid,cover_path=cover_path, videoPath=videoPath, title=mTitle) # choose to upload and get bvid.
+                    # you need to save this 'cover_target' to file.
+                    contentId = uploadVideo(
+                        dedeuserid=dedeuserid,
+                        description=mDescription,
+                        dynamic=mDescription,
+                        tagString=tagString,
+                        tagId=search_tid,
+                        cover_path=cover_path,
+                        videoPath=videoPath,
+                        title=mTitle,
+                    )  # choose to upload and get bvid.
             else:
-                raise Exception('unknown content type to upload for bilibiliPoster:', contentType)
+                raise Exception(
+                    "unknown content type to upload for bilibiliPoster:", contentType
+                )
             return "bilibili://{}/{}/{}".format(dedeuserid, contentType, contentId)
-    
+
     def postContentIterate(content):
         for elem in content:
             yield postContent(elem)
+
     if iterate:
         return postContentIterate(content)
     else:
