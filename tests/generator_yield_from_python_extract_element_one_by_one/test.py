693a641962bc27d28e18a87c3ff23fc860a8dd63
GitJournal <app@gitjournal.io>
2022-10-10 02:22:08 +0800

    [2022-10-09 18:22:07]
    FILE:
    tests/generator_yield_from_python_extract_element_one_by_one/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/generator_yield_from_python_extract_element_one_by_one/test.py
+++ tests/generator_yield_from_python_extract_element_one_by_one/test.py
@@ -9,9 +9,8 @@ def checkFileExists(filePath, debug=False):
         print('exists?', result)
 
 def generator(tempfile):
-        # for index in range(12): # 0 to 11 means 12
-        for index in range(11): # what if it is 11? -> StopIteration and shit get cleaned.
-
+    # for index in range(12): # 0 to 11 means 12
+    for index in range(11): # what if it is 11? -> StopIteration and shit get cleaned.
         with tmpfile(tempfile):
             pathlib.Path(tempfile).touch()
             yield index
