75432b5793b9220287991ea6aeb3a71d67ce5d7d
GitJournal <app@gitjournal.io>
2022-10-09 14:19:53 +0800

    [2022-10-09 06:19:52]
    FILE:
    tests/bilibili_login/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_login/test.py
+++ tests/bilibili_login/test.py
@@ -1,51 +1,27 @@
+from bilibili_api import login, user, sync
 
-from bilibili_api.login import login_with_password, login_with_sms, send_sms, PhoneNumber, Check
-from bilibili_api.user import get_self_info
-from bilibili_api import settings
-from bilibili_api import sync
+while True:
+    print("请登录：")
+    credential = login.login_with_qrcode()  # what are you doing here?
+    # this is some GUI function. no terminal support!
+    # does not support login anymore? cannot scan this goddamn barcode!
+    try:
+        credential.raise_for_no_bili_jct()  # 判断是否成功
+        credential.raise_for_no_sessdata()  # 判断是否成功
+        credential.raise_for_no_buvid3()
+        credential.raise_for_no_dedeuserid()
+        break
+    except:
+        print("登陆失败。。。")
+    # exit()
+    # do not exit! retry!
+userName = sync(user.get_self_info(credential))["name"]
+print("欢迎，", userName, "!")
+# dict(credential)
+buvid3 = credential.buvid3
+bili_jct = credential.bili_jct
+sessdata = credential.sessdata
+userId = credential.dedeuserid  # this is userid, better use this instead?
+breakpoint()
+# check validity of my credentials?
 
-# mode = int(input("""请选择登录方式：
-# 1. 密码登录
-# 2. 验证码登录
-# 请输入 1/2
-# """))
-mode = 2
-
-credential = None
-
-# 关闭自动打开 geetest 验证窗口
-settings.geetest_auto_open = False
-
-if mode == 1:
-    # 密码登录
-    username = input("请输入手机号/邮箱：")
-    password = input("请输入密码：")
-    print("正在登录。")
-    c = login_with_password(username, password)
-    if isinstance(c, Check):
-        # 还需验证
-        phone = input("需要验证。请输入手机号：")
-        c.set_phone(PhoneNumber(phone, country="+86")) # 默认设置地区为中国大陆
-        c.send_code()
-        print("已发送验证码。")
-        code = input("请输入验证码：")
-        credential = c.login(code)
-        print("登录成功！")
-    else:
-        credential = c
-elif mode == 2:
-    # 验证码登录
-    phone = input("请输入手机号：")
-    print("正在登录。")
-    send_sms(PhoneNumber(phone, country="+86")) # 默认设置地区为中国大陆
-    code = input("请输入验证码：")
-    c = login_with_sms(PhoneNumber(phone, country="+86"), code)
-    credential = c
-    print("登录成功")
-else:
-    print("请输入 1/2 ！")
-    exit()
-
-if credential != None:
-    name = sync(get_self_info(credential))['name']
-    print(f"欢迎，{name}!")
