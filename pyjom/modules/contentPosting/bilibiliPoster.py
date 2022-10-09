ed523a96cde62eed5ba01e08f80dd8757edebe4a
GitJournal <app@gitjournal.io>
2022-10-09 21:49:48 +0800

    [2022-10-09 13:49:47]
    FILE:
    pyjom/modules/contentPosting/bilibiliPoster.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/contentPosting/bilibiliPoster.py
+++ pyjom/modules/contentPosting/bilibiliPoster.py
@@ -8,7 +8,7 @@ def BilibiliPoster(content, iterate=False, postMetadataGenerator=None,contentTyp
     # there are two generators. what do you want?
     # def postContent(elem):
         print("READY TO POST CONTENT FROM:", elem)
-    for postMedadata in postMetadataGenerator()
+    for postMedadata in postMetadataGenerator
     if iterate:
         for elem in content:
     # content id?
