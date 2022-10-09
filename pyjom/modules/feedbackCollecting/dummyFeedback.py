a17facf9117f9d0c5ce12f7ce48ed17a68a2a4ac
GitJournal <app@gitjournal.io>
2022-10-10 01:05:10 +0800

    [2022-10-09 17:05:08]
    FILE:
    pyjom/modules/feedbackCollecting/dummyFeedback.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/feedbackCollecting/dummyFeedback.py
+++ pyjom/modules/feedbackCollecting/dummyFeedback.py
@@ -8,11 +8,12 @@ def dummyFeedback(
     def inner(elem):
         print("from poster:", elem)
         return "pending"
+
     def innerIterator(content):
         for elem in content:
             yield inner(elem)
+
     if not iterate:
         return inner(content)
     else:
         return innerIterator(content)
-        
