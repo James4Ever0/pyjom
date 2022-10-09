7ccb452a5e56f3b21ff2f12b34c058a5a7fce169
GitJournal <app@gitjournal.io>
2022-10-09 15:10:41 +0800

    [2022-10-09 07:10:40]
    FILE:
    tests/bilibili_login/test_login_successiful.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_login/test_login_successiful.py
+++ tests/bilibili_login/test_login_successiful.py
@@ -11,7 +11,7 @@ dbPath = os.path.join(home, ".bilibili_api.json")
 import tinydb
 db = tinydb.TinyDB(dbPath)
 User = tinydb.Query()
-db.search(User.dedeuseriid == 'John')
+db.search(User.dedeuserid == )
 credential = Credential()
 from bilibili_api import sync
 name = sync(get_self_info(credential))['name']
