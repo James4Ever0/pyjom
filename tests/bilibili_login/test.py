7af88c200f80d00b657879eff73ebede31a4b05c
GitJournal <app@gitjournal.io>
2022-10-09 15:01:22 +0800

    [2022-10-09 07:01:20]
    FILE:
    tests/bilibili_login/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_login/test.py
+++ tests/bilibili_login/test.py
@@ -1,5 +1,10 @@
-
-from bilibili_api.login import login_with_password, login_with_sms, send_sms, PhoneNumber, Check
+from bilibili_api.login import (
+    login_with_password,
+    login_with_sms,
+    send_sms,
+    PhoneNumber,
+    Check,
+)
 from bilibili_api.user import get_self_info
 from bilibili_api import settings
 from bilibili_api import sync, Credential
@@ -25,7 +30,7 @@ if mode == 1:
     if isinstance(c, Check):
         # 还需验证
         phone = input("需要验证。请输入手机号：")
-        c.set_phone(PhoneNumber(phone, country="+86")) # 默认设置地区为中国大陆
+        c.set_phone(PhoneNumber(phone, country="+86"))  # 默认设置地区为中国大陆
         c.send_code()
         print("已发送验证码。")
         code = input("请输入验证码：")
@@ -37,7 +42,7 @@ elif mode == 2:
     # 验证码登录
     phone = input("请输入手机号：")
     print("正在登录。")
-    send_sms(PhoneNumber(phone, country="+86")) # 默认设置地区为中国大陆
+    send_sms(PhoneNumber(phone, country="+86"))  # 默认设置地区为中国大陆
     code = input("请输入验证码：")
     c = login_with_sms(PhoneNumber(phone, country="+86"), code)
     credential = c
@@ -48,18 +53,29 @@ else:
 
 from lazero.search.api import getHomeDirectory
 import os
+
 home = getHomeDirectory()
-dbPath = os.path.join(home,".bilibili_api.json")
+dbPath = os.path.join(home, ".bilibili_api.json")
 import tinydb
+
 db = tinydb.database(dbPath)
 if credential != None:
-    name = sync(get_self_info(credential))['name']
+    name = sync(get_self_info(credential))["name"]
     print(f"欢迎，{name}!")
     buvid3 = credential.buvid3
     bili_jct = credential.bili_jct
     sessdata = credential.sessdata
-    userId = credential.dedeuserid  # this is userid, better use this instead?
+    dedeuserid = credential.dedeuserid  # this is userid, better use this instead?
     User = tinydb.Query()
-    db.upsert({}, User.)
+    db.upsert(
+        {
+            "name": name,
+            "dedeuserid": dedeuserid,
+            "bili_jct": bili_jct,
+            "buvid3": buvid3,
+            "sessdata": sessdata,
+        },
+        User.dedeuserid,
+    )
     # how to perform atomic insert in tinydb?
     breakpoint()
