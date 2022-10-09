5f9eca26641df7f27ee62275737ed1069ec090c2
GitJournal <app@gitjournal.io>
2022-10-10 01:12:37 +0800

    [2022-10-09 17:12:36]
    FILE:
    pyjom/modules/feedbackCollecting/dummyFeedback.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/feedbackCollecting/dummyFeedback.py
+++ pyjom/modules/feedbackCollecting/dummyFeedback.py
@@ -3,7 +3,7 @@ from pyjom.commons import *
 
 @decorator
 def dummyFeedback(
-    content, iterate=False # i think i need another function decorator for this. really? this will break pattern, for sure. temp files will be missing? so you need some tempdir shit?
+    content, iterate=False # i think i need another function decorator for this. really? this will break pattern, for sure. temp files will be missing? so you need some tempdir decorator?
 ):  # anyway, it is dummy. i don't expect nothing.
     def inner(elem):
         print("from poster:", elem)
