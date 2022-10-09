d1f8026b98bd8088163ad4ef76bb0b8944fea47f
GitJournal <app@gitjournal.io>
2022-10-09 15:06:16 +0800

    [2022-10-09 07:06:14]
    FILE:
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_login/test_login_successiful.py
+++ tests/bilibili_login/test_login_successiful.py
@@ -1,6 +1,13 @@
 from bilibili_api.user import get_self_info
 # from bilibili_api import settings
 # how to load credential from our stored things?
+
+from lazero.search.api import getHomeDirectory
+import os
+
+home = getHomeDirectory()
+dbPath = os.path.join(home, ".bilibili_api.json")
+import tinydb
 credential = Credential()
 from bilibili_api import sync
 name = sync(get_self_info(credential))['name']
