1dfff8f157879b56251eaff4f969d49fc2e6baea
GitJournal <app@gitjournal.io>
2022-10-09 13:01:34 +0800

    [2022-10-09 05:01:32]
    FILE:
    tests/unittest_translate_lyrictoolbox.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_translate_lyrictoolbox.py
+++ tests/unittest_translate_lyrictoolbox.py
@@ -1,6 +1,6 @@
 from test_commons import *
 from pyjom.lyrictoolbox import translate
 for source in sources:
-result = translate, backend='deepl')
-print('source:',source)
-print('result:',result)
+    result = translate, backend='deepl')
+    print('source:',source)
+    print('result:',result)
