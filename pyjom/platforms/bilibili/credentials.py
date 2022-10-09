a083a453b46229dfc4b7f02586190e244abc22c5
GitJournal <app@gitjournal.io>
2022-10-09 19:36:17 +0800

    [2022-10-09 11:36:14]
    FILE:
    pyjom/platforms/bilibili/credentials.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/platforms/bilibili/credentials.py
+++ pyjom/platforms/bilibili/credentials.py
@@ -1,5 +1,8 @@
 # you need to manage login/logout and credential storage.
 # first you need to get 'home' directory
 from lazero.search.api import getHomeDirectory
+import os
 
 home = getHomeDirectory()
+dbPath = os.path.join(home, ".bilibili_api.json")
+import tinydb
