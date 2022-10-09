403cdf20c1bd0307cb9f37471f80e845fb1d644a
GitJournal <app@gitjournal.io>
2022-10-09 15:59:44 +0800

    [2022-10-09 07:59:42]
    FILE:
    tests/bilibili_login/dump_favourite_list.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_login/dump_favourite_list.py
+++ tests/bilibili_login/dump_favourite_list.py
@@ -1,6 +1,36 @@
 
-from bilibili_api import favorite_list, sync
+from bilibili_api import favorite_list
 # that favourite list is public. i just want that.
 # dedeuserid = "397424026"
+# how to?
+from bilibili_api.user import get_self_info
+from bilibili_api import Credential
+
+# how to load credential from our stored things?
+from bilibili_api import user
+from lazero.search.api import getHomeDirectory
+import os
+import tinydb
+
+home = getHomeDirectory()
+dbPath = os.path.join(home, ".bilibili_api.json")
+import tinydb
+
+db = tinydb.TinyDB(dbPath)
+User = tinydb.Query()
+dedeuserid = "397424026"  # pass it before you do shit!
+dataList = db.search(User.dedeuserid == dedeuserid)  # this will never change i suppose?
+if len(dataList) == 1:
+    data = dataList[0].copy()
+    print("try to login credential fetched from db:", data)
+    oldName = data.pop("name")
+    credential = Credential(**data)
+    from bilibili_api import sync
+
+    name = sync(get_self_info(credential))["name"]
+    if oldName != name:
+        data["name"] = name
+        db.upsert(data, User.dedeuserid == dedeuserid)
+    print("login successful:", name)
 favourite_list = sync(favorite_list.get_video_favorite_list(int(dedeuserid)))
 print(favourite_list) # None? wtf?
