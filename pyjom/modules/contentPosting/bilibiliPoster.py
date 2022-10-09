0a75667e0526244cfe20c00dbe137649e1f27b96
GitJournal <app@gitjournal.io>
2022-10-09 23:47:33 +0800

    [2022-10-09 15:47:31]
    FILE:
    pyjom/modules/contentPosting/bilibiliPoster.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/contentPosting/bilibiliPoster.py
+++ pyjom/modules/contentPosting/bilibiliPoster.py
@@ -13,7 +13,7 @@ contentType='video', dedeuserid:str = "397424026"):
     def postContent(elem): # what is this elem? please check for video producer.
         postMetadata = getPostMetadata()
         print("READY TO POST CONTENT FROM:", elem)# this elem is video location for me.
-        if contentType == 'video':
+        if contentType == 'video': # single video upload without grouping.
         contentId = uploadVideo(dedeuserid,descript)
         return "bilibili://{}/{}/{}".format(dedeuserid, contentType, contentId)
     
