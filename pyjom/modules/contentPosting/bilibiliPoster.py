275fc636da1efccf0a983e617d1e86f90c88ac54
GitJournal <app@gitjournal.io>
2022-10-09 21:49:55 +0800

    [2022-10-09 13:49:54]
    FILE:
    pyjom/modules/contentPosting/bilibiliPoster.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/contentPosting/bilibiliPoster.py
+++ pyjom/modules/contentPosting/bilibiliPoster.py
@@ -2,13 +2,14 @@ from pyjom.commons import *
 from pyjom.platforms.bilibili.uploader import uploadVideo
 
 @decorator
-def BilibiliPoster(content, iterate=False, postMetadataGenerator=None,contentType='video'):
+def BilibiliPoster(content, iterate=False, postMetadataGenerator=None #
+,contentType='video'):
     # are you sure this 'postMetadataGenerator' will generate valid data for us?
     # anyway let's write for video.
     # there are two generators. what do you want?
     # def postContent(elem):
         print("READY TO POST CONTENT FROM:", elem)
-    for postMedadata in postMetadataGenerator
+    for postMedadata in postMetadataGenerator:
     if iterate:
         for elem in content:
     # content id?
