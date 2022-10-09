4119a205ae648f7c800bb5dc61f3421dfccf6c78
GitJournal <app@gitjournal.io>
2022-10-10 00:19:45 +0800

    [2022-10-09 16:19:43]
    FILE:
    pyjom/modules/contentPosting/onlinePoster.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/contentPosting/onlinePoster.py
+++ pyjom/modules/contentPosting/onlinePoster.py
@@ -1,3 +1,4 @@
 
 
-{'bilibili':BilibiliPoster}
+def OnlinePoster(content, iterate=False, contentType='video',postMetadataGenerator=postMetadataGenerator, platform='bilibili'):
+    {'bilibili':BilibiliPoster}
