fa2c60c0af7217500e48b65d3bd1de0bf19ea17f
GitJournal <app@gitjournal.io>
2022-10-10 03:04:18 +0800

    [2022-10-09 19:04:15]
    FILE:
    pyjom/musictoolbox.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/musictoolbox.py
+++ pyjom/musictoolbox.py
@@ -1,4 +1,4 @@
 # you will have a better name for other toolboxs.
 # for now, the musictoolbox is responsible for music retrieval, track separation, bpm
 # pitch shift, speedup/slowdown is for audiotoolbox
-# 
+# voice change is for 
