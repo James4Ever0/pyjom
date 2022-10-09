0d73148ec12deca3843e75d70abc224ac8714933
GitJournal <app@gitjournal.io>
2022-10-09 21:47:34 +0800

    [2022-10-09 13:47:32]
    FILE:
    pyjom/modules/contentPosting/bilibiliPoster.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/contentPosting/bilibiliPoster.py
+++ pyjom/modules/contentPosting/bilibiliPoster.py
@@ -4,6 +4,7 @@ from pyjom.platforms.bilibili.uploader import uploadVideo
 @decorator
 def BilibiliPoster(content, iterate=False, postMetadataGenerator=None,contentType='video'):
     # are you sure this 'postMetadataGenerator' will generate valid data for us?
+    # anyway let's write for video.
     if iterate:
         for elem in content:
             print("READY TO POST CONTENT FROM:", elem)
