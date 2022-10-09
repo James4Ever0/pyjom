3921ce6ab01cd8e0da769a1746b0cdc8040a2a78
GitJournal <app@gitjournal.io>
2022-10-09 17:30:01 +0800

    [2022-10-09 09:29:56]
    FILE:
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

---
+++ tests/bilibili_login_get_credential_view_data/test_login_successful copy.py
@@ -0,0 +1,26 @@
+from bilibili_api.user import get_self_info
+from bilibili_api import Credential
+# how to load credential from our stored things?
+
+from lazero.search.api import getHomeDirectory
+import os
+import tinydb
+
+home = getHomeDirectory()
+dbPath = os.path.join(home, ".bilibili_api.json")
+import tinydb
+db = tinydb.TinyDB(dbPath)
+User = tinydb.Query()
+dedeuserid = "397424026" # pass it before you do shit!
+dataList = db.search(User.dedeuserid == dedeuserid) # this will never change i suppose?
+if len(dataList) == 1:
+    data = dataList[0].copy()
+    print('try to login credential fetched from db:', data)
+    oldName = data.pop('name')
+    credential = Credential(**data)
+    from bilibili_api import sync
+    name = sync(get_self_info(credential))['name']
+    if oldName !=name:
+        data['name']=name
+        db.upsert(data, User.dedeuserid == dedeuserid)
+    print('login successful:', name)
