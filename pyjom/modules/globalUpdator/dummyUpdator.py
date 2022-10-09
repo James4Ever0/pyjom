36efcb8efa1f3a08dbe92ef476bd08fb0be6f501
GitJournal <app@gitjournal.io>
2022-10-10 02:29:27 +0800

    [2022-10-09 18:29:25]
    FILE:
    pyjom/modules/globalUpdator/dummyUpdator.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/globalUpdator/dummyUpdator.py
+++ pyjom/modules/globalUpdator/dummyUpdator.py
@@ -3,5 +3,5 @@ from pyjom.commons import *
 
 @decorator
 def dummyUpdator(optimized_result):
-    def inner()
+    def inner(optimized)
     return "updated. since it is pending we will schedule another optimization"
