8b201ab8db7641a7dfe572876aa36da837fe9848
GitJournal <app@gitjournal.io>
2022-10-09 20:12:11 +0800

    [2022-10-09 12:12:09]
    FILE:
    pyjom/platforms/bilibili/credentials.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/platforms/bilibili/credentials.py
+++ pyjom/platforms/bilibili/credentials.py
@@ -33,5 +33,4 @@ def verifyCredential(credential,returnName=True):
 
 def getCredentialByDedeUserId(dedeuserid):
     dataList = db.search(User.dedeuserid == dedeuserid)
-    if len(dataList) == 0:
-        
+    if len(dataList) !=1:
