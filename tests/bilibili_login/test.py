1a64e4e5e8f5b7aa7c48ef6539f21ee6e34de707
GitJournal <app@gitjournal.io>
2022-10-09 14:13:12 +0800

    [2022-10-09 06:13:08]
    FILE:
    tests/bilibili_login/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_login/test.py
+++ tests/bilibili_login/test.py
@@ -1,16 +1,16 @@
 from bilibili_api import login, user, sync
 
 while True:
-print("请登录：")
-credential = login.login_with_qrcode()  # what are you doing here?
-# this is some GUI function. no terminal support!
-try:
-    credential.raise_for_no_bili_jct()  # 判断是否成功
-    credential.raise_for_no_sessdata()  # 判断是否成功
-    credential.raise_for_no_buvid3()
-    credential.raise_for_no_dedeuserid()
-except:
-    print("登陆失败。。。")
+    print("请登录：")
+    credential = login.login_with_qrcode()  # what are you doing here?
+    # this is some GUI function. no terminal support!
+    try:
+        credential.raise_for_no_bili_jct()  # 判断是否成功
+        credential.raise_for_no_sessdata()  # 判断是否成功
+        credential.raise_for_no_buvid3()
+        credential.raise_for_no_dedeuserid()
+    except:
+        print("登陆失败。。。")
     # exit()
     # do not exit! retry!
 userName = sync(user.get_self_info(credential))["name"]
