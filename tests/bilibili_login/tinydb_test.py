f74c2d9f6f3172a0c3baad5489c6fc95744a11a0
GitJournal <app@gitjournal.io>
2022-10-09 14:33:10 +0800

    [2022-10-09 06:33:07]
    FILE:
    tests/bilibili_login/tinydb_test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_login/tinydb_test.py
+++ tests/bilibili_login/tinydb_test.py
@@ -5,3 +5,4 @@ db = tinydb.TinyDB(dbLocation)
 # table = db.table('mytable')
 User = tinydb.Query()
 # db.upsert({'abc': 'def', 'ghi': 123}, User.ghi == 123) # please specify a condition!
+db.update()
