db3aa03b9ab8303cf3b2b7e9d3dd94ce8e130054
GitJournal <app@gitjournal.io>
2022-10-10 02:10:51 +0800

    [2022-10-09 18:10:50]
    FILE:
    pyjom/modules/feedbackCollecting/dummyFeedback.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/feedbackCollecting/dummyFeedback.py
+++ pyjom/modules/feedbackCollecting/dummyFeedback.py
@@ -6,8 +6,8 @@ from lazero.program.functools import iterateWithTempDirectory # you can also swi
 def dummyFeedback(
     content, iterate='auto' # i think i need another function decorator for this. really? this will break pattern, for sure. temp files will be missing? so you need some tempdir decorator?
 ):  # anyway, it is dummy. i don't expect nothing.
-    @iterateWithTempDirectory #
+    @iterateWithTempDirectory()
     def inner(elem):
         print("from poster:", elem)
         return "pending"
-    return 
+    return inner()
