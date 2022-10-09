d818d60aca2d8363bbbe5cb412ebca8a3313cf08
GitJournal <app@gitjournal.io>
2022-10-09 23:58:03 +0800

    [2022-10-09 15:58:02]
    FILE:
    pyjom/modules/contentPosting/bilibiliPoster.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/contentPosting/bilibiliPoster.py
+++ pyjom/modules/contentPosting/bilibiliPoster.py
@@ -11,7 +11,6 @@ contentType='video', dedeuserid:str = "397424026", tempdir = '/dev/shm/medialang
     # there are two generators. what do you want?
     getPostMetadata = lambda: postMetadataGenerator.__next__()
     def postContent(elem): # what is this elem? please check for video producer.
-        
         with tmpdir(path=tempdir):
         postMetadata = getPostMetadata()
         print("READY TO POST CONTENT FROM:", elem)# this elem is video location for me.
