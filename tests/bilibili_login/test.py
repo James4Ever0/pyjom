28654d08e408144b91dd4cb7e07cc9a36ad573f6
GitJournal <app@gitjournal.io>
2022-10-09 15:03:40 +0800

    [2022-10-09 07:03:38]
    FILE:
    tests/bilibili_login/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_login/test.py
+++ tests/bilibili_login/test.py
@@ -67,6 +67,7 @@ if credential != None:
     sessdata = credential.sessdata
     dedeuserid = credential.dedeuserid  # this is userid, better use this instead?
     User = tinydb.Query()
+    # assume that we are here
     db.upsert(
         {
             "name": name,
