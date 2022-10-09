d7061cb14f0471e4c1989110b95c6d63502c756a
GitJournal <app@gitjournal.io>
2022-10-10 00:28:14 +0800

    [2022-10-09 16:28:12]
    FILE:
    tests/generator_yield_from_python_extract_element_one_by_one/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/generator_yield_from_python_extract_element_one_by_one/test.py
+++ tests/generator_yield_from_python_extract_element_one_by_one/test.py
@@ -5,6 +5,8 @@ def generator():
 def generator2():
     yield from generator() # this is to simplifying the process of iteration.
 
+
+
 myGenerator = generator2()
 getNextNumber = lambda: myGenerator.__next__()
 for _ in range(20):
