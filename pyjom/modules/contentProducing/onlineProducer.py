39f90698587490edec13798368ad6bb4e1e544aa
GitJournal <app@gitjournal.io>
2022-10-10 10:26:32 +0800

    [2022-10-10 02:26:30]
    FILE:
    pyjom/modules/contentProducing/onlineProducer.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/contentProducing/onlineProducer.py
+++ pyjom/modules/contentProducing/onlineProducer.py
@@ -2,6 +2,7 @@ from pyjom.commons import *
 from pyjom.modules.contentProducing.producerTemplates import getProducerTemplate
 
 from lazero.filesystem.temp import tmpdir
+
 @decorator
 def OnlineProducer(processed_info_generator, source = 'giphy',template=None, template_configs=None, fast:bool=True, medialangTmpdirBase="/dev/shm/medialang", debug=False):
     # template_configs is a generator, it generate configs.
