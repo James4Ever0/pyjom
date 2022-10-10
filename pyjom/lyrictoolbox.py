118ce7001323197652325b5bfda707ee26a52b7f
GitJournal <app@gitjournal.io>
2022-10-10 16:42:19 +0800

    [2022-10-10 08:42:17]
    FILE:
    pyjom/lyrictoolbox.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/lyrictoolbox.py
+++ pyjom/lyrictoolbox.py
@@ -364,6 +364,16 @@ def textArrayWithTranslatedListToAss(
     import math
     import jinja2
     from lazero.filesystem.io import readFile
+    default_template_configs =  {
+    "defaultFontname": "Arial",
+    "defaultFontsize": 48,  # integer?
+    "translationFontname": "Migu 1P",
+    "translationFontsize": 48,
+    "kanjiFontname": "Migu 1P",
+    "kanjiFontsize": 46,
+    "romajiFontname": "Migu 1P",
+    "romajiFontsize": 38,
+}
 
     template = jinja2.Template(source=readFile(template_path))
     template_configured = template.render(**template_configs)
