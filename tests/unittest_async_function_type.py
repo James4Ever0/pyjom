f1bab7a57aed62bb2c73bce766806acaff843021
GitJournal <app@gitjournal.io>
2022-10-10 17:09:25 +0800

    [2022-10-10 09:09:23]
    FILE:
    tests/unittest_async_function_type.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_async_function_type.py
+++ tests/unittest_async_function_type.py
@@ -1,9 +1,10 @@
 async def randomFunction():
     return 1
 async def randomFunctionGenerator():
-    
+
 import types
 type0=  type(randomFunction)
 type1 = type(randomFunction())
 type2 = types.AsyncGeneratorType
 type3 = types.
+type4 = types.CoroutineType
