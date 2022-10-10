8bc1a9af962bca30c78157567fb47c6091f194fd
GitJournal <app@gitjournal.io>
2022-10-10 14:30:00 +0800

    [2022-10-10 06:29:59]
    FILE:
    tests/test_auto_dog_video_giphy_online_producer.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/test_auto_dog_video_giphy_online_producer.py
+++ tests/test_auto_dog_video_giphy_online_producer.py
@@ -28,6 +28,7 @@ wbRev = OnlineAutoContentProducer(
     postMetadataGenerator=postMetadataGenerator,
     template_configs=[
         {
+            'debug': True, # we need to preview this video.
             "music": {
                 "filepath": "/root/Desktop/works/pyjom/tests/music_analysis/exciting_bgm.mp3",  # these things were not right.
                 "lyric_path": "/root/Desktop/works/pyjom/tests/music_analysis/exciting_bgm.lrc",  ## you can choose not to pass the lyric_path anyway. also format different than .lrc is on the way?
