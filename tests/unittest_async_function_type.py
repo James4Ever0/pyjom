fd12eafc1ace0a61fa085d72bf4a0b63e89e421b
GitJournal <app@gitjournal.io>
2022-10-10 17:22:01 +0800

    [2022-10-10 09:21:57]
    FILE:
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_async_function_type.py
+++ tests/unittest_async_function_type.py
@@ -1,17 +1,22 @@
 async def randomFunction():
     return 1
+
+
 async def randomFunctionGenerator():
     yield await randomFunction()
+
+
 from bilibili_api import sync
 import types
-type0=  type(randomFunction)
+
+type0 = type(randomFunction)
 type1 = type(randomFunction())
 type2 = types.AsyncGeneratorType
 type3 = type(randomFunctionGenerator())
 type4 = types.CoroutineType
 type5 = type(randomFunctionGenerator)
 print(type0, type1, type2, type3, type4, type5)
-print(type1== type4)
+print(type1 == type4)
 print(type2 == type3)
 # async generator can only be used within async methods.
 # no breakpoint support for async functions? wtf?
@@ -19,4 +24,4 @@ print(type2 == type3)
 data = randomFunction()
 data = sync(data)
 # # not good.
-print(type(data),data)
+print(type(data), data)
