9c55265b830483fc983a7314ba40013e5e72985a
GitJournal <app@gitjournal.io>
2022-10-10 15:59:11 +0800

    [2022-10-10 07:59:09]
    FILE:
    pyjom/lyrictoolbox.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/lyrictoolbox.py
+++ pyjom/lyrictoolbox.py
@@ -348,7 +348,7 @@ def textArrayWithTranslatedListToAss(
     shiftAdjust=600,
     censor=True,
     puncturalRemoval=True,
-    template_path = "/root/Desktop/works/pyjom/tests/karaoke_effects/in2.ass" # but the style. you know.
+    template_path = "/root/Desktop/works/pyjom/tests/karaoke_effects/in2.ass.j2" # but the style. you know.
     # editly does not support to put the .ass subtitle directly.
 ):
     # newTextArray = [] # dummy shit. must be removed immediately.
