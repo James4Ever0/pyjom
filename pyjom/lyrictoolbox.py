b50235d26d394ddc1c313c309480608e08cd2cc5
GitJournal <app@gitjournal.io>
2022-10-10 14:36:59 +0800

    [2022-10-10 06:36:57]
    FILE:
    pyjom/lyrictoolbox.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/lyrictoolbox.py
+++ pyjom/lyrictoolbox.py
@@ -517,7 +517,6 @@ def textArrayWithTranslatedListToAss(
             # l.end_time = line.end_time - 25 * (len(line.syls) - syl.i) + delay + 100
             # l.end_time = line.end_time - 25 * (len(line.syls) - syl.i) + delay + 100
             l.dur = l.end_time - l.start_time
-            l.
             if l.dur <= 0:
                 continue
 
