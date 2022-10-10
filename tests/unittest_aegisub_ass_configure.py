af608187bb5ea2a4ecc309ddcad705346c6d2d42
GitJournal <app@gitjournal.io>
2022-10-10 16:10:57 +0800

    [2022-10-10 08:10:56]
    FILE:
    tests/unittest_aegisub_ass_configure.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_aegisub_ass_configure.py
+++ tests/unittest_aegisub_ass_configure.py
@@ -1,7 +1,16 @@
 from lazero.filesystem.io import readFile
 import jinja2
-template_configs = {'defaultFontname':None,'defaultFontsize':None, 
-"translationFontname":None, "translationFontsize":None,'kanjiFontname':None, 'kanjiFontsize':None, 'romajiFontname':None,'romajiFontsize':None}
+
+template_configs = {
+    "defaultFontname": None,
+    "defaultFontsize": None,
+    "translationFontname": None,
+    "translationFontsize": None,
+    "kanjiFontname": None,
+    "kanjiFontsize": None,
+    "romajiFontname": None,
+    "romajiFontsize": None,
+}
 # template_configs = {'defaultFontname':'Anonymous Pro'}
 template_path = "/root/Desktop/works/pyjom/tests/karaoke_effects/in2.ass.j2"
 template = jinja2.Template(source=readFile(template_path))
