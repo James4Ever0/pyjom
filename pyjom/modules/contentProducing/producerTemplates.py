f0382e27dc8ef80d52d103847b91be8725c4f801
GitJournal <app@gitjournal.io>
2022-10-10 16:48:30 +0800

    [2022-10-10 08:48:28]
    FILE:
    pyjom/modules/contentProducing/producerTemplates.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/contentProducing/producerTemplates.py
+++ pyjom/modules/contentProducing/producerTemplates.py
@@ -351,8 +351,8 @@ def petsWithMusicOnlineProducer(
             render_ass = config.get("render_ass", False)
                 ass_template_configs={},
     assStyleConfig={}
-        config.get(ass_template_configs',{})
-    assStyleConfig',{})
+        config.get('ass_template_configs',{})
+    config.get('assStyleConfig',{})
             parsed_result = getMusicInfoParsed(config)
             # print(parsed_result)
             # breakpoint()
