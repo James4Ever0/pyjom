5d47247a126307825f7de187f1aa1d7aeaa11781
GitJournal <app@gitjournal.io>
2022-10-10 17:07:24 +0800

    [2022-10-10 09:07:21]
    FILE:
    pyjom/platforms/bilibili/utils.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/platforms/bilibili/utils.py
+++ pyjom/platforms/bilibili/utils.py
@@ -4,7 +4,7 @@ from bilibili_api import sync
 # wtf is async generator type?
 def bilibiliSync(func):
     def wrapper(*args, **kwargs):
-        if type(func) == types.:
+        if type(func) == types.CoroutineType:
             return sync(func(*args, **kwargs))
         else:
             return func(*args, **kwargs)
