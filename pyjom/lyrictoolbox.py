aab41f9e479e9a8e264f3a6143542fec5832496f
GitJournal <app@gitjournal.io>
2022-10-10 16:41:41 +0800

    [2022-10-10 08:41:38]
    FILE:
    pyjom/lyrictoolbox.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/lyrictoolbox.py
+++ pyjom/lyrictoolbox.py
@@ -352,16 +352,16 @@ def textArrayWithTranslatedListToAss(
     puncturalRemoval=True,
     template_path="/root/Desktop/works/pyjom/tests/karaoke_effects/in2.ass.j2",  # but the style. you know.
     # aegisub use system fonts. you pass font name into it.
- template_configs= {
-    "defaultFontname": "Arial",
-    "defaultFontsize": 48,  # integer?
-    "translationFontname": "Migu 1P",
-    "translationFontsize": 48,
-    "kanjiFontname": "Migu 1P",
-    "kanjiFontsize": 46,
-    "romajiFontname": "Migu 1P",
-    "romajiFontsize": 38,
-},
+    template_configs={
+        "defaultFontname": "Arial",
+        "defaultFontsize": 48,  # integer?
+        "translationFontname": "Migu 1P",
+        "translationFontsize": 48,
+        "kanjiFontname": "Migu 1P",
+        "kanjiFontsize": 46,
+        "romajiFontname": "Migu 1P",
+        "romajiFontsize": 38,
+    },
     styleConfig={
         "original": {"method": "romaji", "style": "Romaji"},
         "translated": {"method": "kanji", "style": "Kanji"},
