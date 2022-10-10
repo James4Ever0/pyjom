82dab091af62cf281a7ed108e54f03969529a31c
GitJournal <app@gitjournal.io>
2022-10-10 22:53:13 +0800

    [2022-10-10 14:53:12]
    FILE:
    samples/medialang/target/larger_play.sh
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

---
+++ samples/medialang/target/larger_play.sh
@@ -0,0 +1,3 @@
+SUBTITLE="/root/Desktop/works/pyjom/samples/medialang/target/ass_larger_text_size.ass"
+VIDEO="/root/Desktop/works/pyjom/samples/medialang/target/halfdone_without_ass_dogcat_sample.mp4"
+mpv --sub-file=$SUBTITLE --fs $VIDEO
