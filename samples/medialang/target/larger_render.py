71cdf978973a93a2eb246d942494596162941815
GitJournal <app@gitjournal.io>
2022-10-10 23:14:28 +0800

    [2022-10-10 15:14:24]
    FILE:
    samples/medialang/target/larger_render.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- samples/medialang/target/larger_render.py
+++ samples/medialang/target/larger_render.py
@@ -1,10 +1,15 @@
-ass_file_path=SUBTITLE="/root/Desktop/works/pyjom/samples/medialang/target/ass_larger_text_size.ass"
-videoInput=VIDEO="/root/Desktop/works/pyjom/samples/medialang/target/halfdone_without_ass_dogcat_sample.mp4"
-
+ass_file_path = (
+    SUBTITLE
+) = "/root/Desktop/works/pyjom/samples/medialang/target/ass_larger_text_size.ass"
+rendered_media_location = (
+    VIDEO
+) = "/root/Desktop/works/pyjom/samples/medialang/target/halfdone_without_ass_dogcat_sample.mp4"
+final_output_location = "larger_render_test.mp4"
 import ffmpeg
+
 videoInput = ffmpeg.input(rendered_media_location).video
 audioInput = ffmpeg.input(rendered_media_location).audio
-videoInput = videoInput.filter(
-    "ass", ass_file_path
+videoInput = videoInput.filter("ass", ass_file_path)
+ffmpeg.output(videoInput, audioInput, final_output_location, acodec="copy").run(
+    overwrite_output=True
 )
-ffmpeg.output(videoInput,audioInput,final_output_location,acodec='copy').run(overwrite_output=True)
