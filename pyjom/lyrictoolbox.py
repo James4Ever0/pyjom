4515c60e48d0bcc39f13038d4e3e6822640b24b6
GitJournal <app@gitjournal.io>
2022-10-10 16:35:01 +0800

    [2022-10-10 08:34:58]
    FILE:
    pyjom/lyrictoolbox.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/lyrictoolbox.py
+++ pyjom/lyrictoolbox.py
@@ -848,6 +848,8 @@ def textArrayWithTranslatedListToAss(
         source = lineMod.copy()
         target = lineMod.copy()
         if translatedText:
+            source.style = 
+            target.style = 
             kanji(source,target) # writing 'kanji' style to romaji?
             lineMod2 = lineMod.copy()
             lineMod2.style =  kanjiStyleName
