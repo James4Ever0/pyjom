02a032053b801149c9f5799810d5e08815782e80
GitJournal <app@gitjournal.io>
2022-10-10 10:09:36 +0800

    [2022-10-10 02:09:35]
    FILE:
    pyjom/musictoolbox.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/musictoolbox.py
+++ pyjom/musictoolbox.py
@@ -7,3 +7,26 @@
 # voice change/synthesis is for voicetoolbox.
 
 # diffusion based painter, ai colorization, video editing is for artworktoolbox. maybe the naming is not right/necessary.
+
+
+# musictoolbox
+def audioOwlAnalysis(myMusic):
+    # get sample rate
+    # info = MediaInfo(filename = myMusic)
+    # info = info.getInfo()
+    info = get_media_info(myMusic)
+    audioSampleRate = info["audioSamplingRate"]
+    audioSampleRate = int(audioSampleRate)
+
+    waveform = audioowl.get_waveform(myMusic, sr=audioSampleRate)
+    data = audioowl.analyze_file(myMusic, sr=audioSampleRate)  # how fucking long?
+
+    a, b, c, d = [
+        data[k] for k in ["beat_samples", "duration", "sample_rate", "tempo_float"]
+    ]
+    bpm = data["tempo_float"]
+    # single_bpm_time = 60/d
+
+    beat_times = [x / c for x in a]
+    return beat_times, bpm
+
