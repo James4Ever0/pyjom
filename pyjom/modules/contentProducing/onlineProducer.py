b44b570ae1045660b2ac70cb88570ec00d5e5744
GitJournal <app@gitjournal.io>
2022-10-10 10:26:49 +0800

    [2022-10-10 02:26:47]
    FILE:
    pyjom/modules/contentProducing/onlineProducer.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/contentProducing/onlineProducer.py
+++ pyjom/modules/contentProducing/onlineProducer.py
@@ -3,22 +3,37 @@ from pyjom.modules.contentProducing.producerTemplates import getProducerTemplate
 
 from lazero.filesystem.temp import tmpdir
 
+
 @decorator
-def OnlineProducer(processed_info_generator, source = 'giphy',template=None, template_configs=None, fast:bool=True, medialangTmpdirBase="/dev/shm/medialang", debug=False):
+def OnlineProducer(
+    processed_info_generator,
+    source="giphy",
+    template=None,
+    template_configs=None,
+    fast: bool = True,
+    medialangTmpdirBase="/dev/shm/medialang",
+    debug=False,
+):
     # template_configs is a generator, it generate configs.
     # print("PROCESSED_INFO_GENERATOR: ", processed_info_generator)
     # breakpoint()
     import uuid
+
     medialangTmpdir = os.path.join(medialangTmpdirBase, str(uuid.uuid4()))
-    with tmpdir(path=medialangTmpdir) as TD:# must use another level of abstraction
-        if source == 'giphy':
+    with tmpdir(path=medialangTmpdir) as TD:  # must use another level of abstraction
+        if source == "giphy":
             template_function = getProducerTemplate(template)
             # print("TEMPLATE FUNCTION ACQUIRED %s" % template_function)
             # breakpoint()
-            exported_media_locations = template_function(processed_info_generator,configs=template_configs, fast=fast, medialangTmpdir=TD) # a generator!
+            exported_media_locations = template_function(
+                processed_info_generator,
+                configs=template_configs,
+                fast=fast,
+                medialangTmpdir=TD,
+            )  # a generator!
             # i guess the title/tags/cover are actually belongs to the poster, not producer.
             for exported_media_location in exported_media_locations:
-                print('exported media location:', exported_media_location)
+                print("exported media location:", exported_media_location)
                 if debug:
                     breakpoint()
                 yield exported_media_location
