dc3fcc35b10ce66542710dc9d2bace5277414071
GitJournal <app@gitjournal.io>
2022-10-10 16:05:03 +0800

    [2022-10-10 08:05:01]
    FILE:
    tests/unittest_aegisub_ass_configure.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_aegisub_ass_configure.py
+++ tests/unittest_aegisub_ass_configure.py
@@ -1,7 +1,7 @@
 from lazero.filesystem.io import readFile
 import jinja2
 # template_configs = {'defaultFontName':None}
-# template_configs = {'defaultFontName':...}
+template_configs = {'defaultFontName':''}
 template_path = "/root/Desktop/works/pyjom/tests/karaoke_effects/in2.ass.j2"
 template = jinja2.Template(source=readFile(template_path))
 template_configured = template.render(**template_configs)
