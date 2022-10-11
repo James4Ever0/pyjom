092635cbb04cc49d19ffdac812dea35a7625faa1
GitJournal <app@gitjournal.io>
2022-10-11 15:42:04 +0800

    [2022-10-11 07:42:02]
    FILE:
    tests/soundhound_houndify_midomi_sound_recognize_music/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/soundhound_houndify_midomi_sound_recognize_music/test.py
+++ tests/soundhound_houndify_midomi_sound_recognize_music/test.py
@@ -1,11 +1,13 @@
-url = "wss://houndify.midomi.com/"
+# url = "wss://houndify.midomi.com/"
 
-import asyncio
-import websockets
+# import asyncio
+# import websockets
 
-async def hello():
-    async with websockets.connect(url) as websocket:
-        await websocket.send({ "version": "1.0" })
-        await websocket.recv()
+# async def hello():
+#     async with websockets.connect(url) as websocket:
+#         await websocket.send({ "version": "1.0" })
+#         await websocket.recv()
 
-asyncio.run(hello())
+# asyncio.run(hello())
+# the nodejs works for soundhound right now.
+# move upon other platforms.
