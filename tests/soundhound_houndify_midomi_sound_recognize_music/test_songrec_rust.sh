6c866e50d6df4afe9684d84f511ba974cb03e438
GitJournal <app@gitjournal.io>
2022-10-11 18:04:50 +0800

    [2022-10-11 10:04:44]
    FILE:
    tests/soundhound_houndify_midomi_sound_recognize_music/test_songrec_rust.sh
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

---
+++ tests/soundhound_houndify_midomi_sound_recognize_music/test_songrec_rust.sh
@@ -0,0 +1,6 @@
+songrec audio-file-to-recognized-song /root/Desktop/works/pyjom/tests/music_recognization/exciting_bgm_cut_10seconds.mp3
+{
+  "matches": [],
+  "retryms": 12000,
+  "tagid": "961d7abe-2c78-4b8d-85c3-76f8b081fabb"
+}
