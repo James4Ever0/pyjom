da18c2f93ab2e4abc308de449090f82dd15e567e
GitJournal <app@gitjournal.io>
2022-10-12 01:47:29 +0800

    [2022-10-11 17:47:27]
    FILE:
    tests/ast_parser_formatter_skipexception/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/ast_parser_formatter_skipexception/test.py
+++ tests/ast_parser_formatter_skipexception/test.py
@@ -6,3 +6,8 @@
 # finally, throw it to our dearly 'skipException'
     print('aaa'
         ) # there is no repairing on this bracket.
+
+# about the dog_or_cat recognition of our cover:
+# 1. throw away unqualified ones (using pop?)
+# 2. lower the threshold of yolo
+# 3. downscale picture before passing to yolo
