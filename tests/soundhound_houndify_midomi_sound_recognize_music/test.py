807243bee7543d6b041ee9b17e8fbf348567fd31
GitJournal <app@gitjournal.io>
2022-10-11 15:26:58 +0800

    [2022-10-11 07:25:14]
    FILE:
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/soundhound_houndify_midomi_sound_recognize_music/test.py
+++ tests/soundhound_houndify_midomi_sound_recognize_music/test.py
@@ -1,2 +1,11 @@
 url = "wss://houndify.midomi.com/"
 
+import asyncio
+import websockets
+
+async def hello():
+    async with websockets.connect(url) as websocket:
+        await websocket.send({})
+        await websocket.recv()
+
+asyncio.run(hello())
