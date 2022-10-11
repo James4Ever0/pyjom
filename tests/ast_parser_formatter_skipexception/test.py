ab7d2cd60df0d8e203a4de4897d05a1732fa81da
GitJournal <app@gitjournal.io>
2022-10-12 01:44:28 +0800

    [2022-10-11 17:44:27]
    FILE:
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/ast_parser_formatter_skipexception/
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/ast_parser_formatter_skipexception/test.py
+++ tests/ast_parser_formatter_skipexception/test.py
@@ -3,5 +3,6 @@
 # use autopep8?
 # first, autopep8, next, black
 # both with 'unlimited' line of code.
+# finally, throw it to our dearly 'skipException'
     print('aaa'
         ) # there is no repairing on this bracket.
