c6cde544cd424a0a4647fa497bfd0cd6706e9ba9
GitJournal <app@gitjournal.io>
2022-10-11 16:09:14 +0800

    [2022-10-11 08:09:12]
    FILE:
    tests/soundhound_houndify_midomi_sound_recognize_music/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/soundhound_houndify_midomi_sound_recognize_music/test.py
+++ tests/soundhound_houndify_midomi_sound_recognize_music/test.py
@@ -12,3 +12,5 @@
 # the nodejs works for soundhound right now.
 # move upon other platforms: shazam (2 tools), netease.
 # shazam works for our chinese songs. one problem: it has traditional chinese.
+# better convert traditional chinese to simplified chinese, for better searching experience.
+# or you bet it. maybe another way of censorship circumvention?
