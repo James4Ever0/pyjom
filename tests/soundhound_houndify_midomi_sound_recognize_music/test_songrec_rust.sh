b6681721b81d836c1005348f62896e43c2015ba3
GitJournal <app@gitjournal.io>
2022-10-11 18:07:47 +0800

    [2022-10-11 10:07:45]
    FILE:
    tests/soundhound_houndify_midomi_sound_recognize_music/test_songrec_rust.sh
    tests/soundhound_houndify_midomi_sound_recognize_music/output_songrec_shazam.json
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/soundhound_houndify_midomi_sound_recognize_music/test_songrec_rust.sh
+++ tests/soundhound_houndify_midomi_sound_recognize_music/test_songrec_rust.sh
@@ -1,6 +1,10 @@
-songrec audio-file-to-recognized-song /root/Desktop/works/pyjom/tests/music_recognization/exciting_bgm_cut_10seconds.mp3
-{
-  "matches": [],
-  "retryms": 12000,
-  "tagid": "961d7abe-2c78-4b8d-85c3-76f8b081fabb"
-}
+songrec audio-file-to-recognized-song /root/Desktop/works/pyjom/tests/music_analysis/exciting_bgm.mp3 # this is quick and stable. no need to pass shit over it.
+# warning: we can only have preview for this song.
+# even with lyrics. but the time? where?
+# songrec audio-file-to-recognized-song /root/Desktop/works/pyjom/tests/music_recognization/exciting_bgm_cut_10seconds.mp3
+# {
+#   "matches": [],
+#   "retryms": 12000,
+#   "tagid": "961d7abe-2c78-4b8d-85c3-76f8b081fabb"
+# }
+# no matches?
