506c0460981a161aceb82f405f58921a2980561c
GitJournal <app@gitjournal.io>
2022-10-11 22:46:08 +0800

    [2022-10-11 14:46:02]
    FILE:
    tests/soundhound_houndify_midomi_sound_recognize_music/test_shazamio_recognize_music.sh
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/soundhound_houndify_midomi_sound_recognize_music/test_shazamio_recognize_music.sh
+++ tests/soundhound_houndify_midomi_sound_recognize_music/test_shazamio_recognize_music.sh
@@ -1,2 +1,3 @@
-python3 shazamio_recognize_music.py --file /root/Desktop/works/pyjom/tests/music_analysis/exciting_bgm.mp3 # this is quick and stable. no need to pass shit over it.
+python3 shazamio_recognize_music.py --file 20secs_exciting_bgm.mp3
+# python3 shazamio_recognize_music.py --file /root/Desktop/works/pyjom/tests/music_analysis/exciting_bgm.mp3 
 # taking longer than expected. why?
