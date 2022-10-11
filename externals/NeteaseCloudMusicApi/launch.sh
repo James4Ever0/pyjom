794e96ae40c4f6744ab76cf9aa3770e869890185
GitJournal <app@gitjournal.io>
2022-10-11 21:30:29 +0800

    [2022-10-11 13:30:25]
    FILE:
    externals/NeteaseCloudMusicApi/launch.sh
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- externals/NeteaseCloudMusicApi/launch.sh
+++ externals/NeteaseCloudMusicApi/launch.sh
@@ -1,2 +1,7 @@
 cd /root/Desktop/works/pyjom/externals/NeteaseCloudMusicApi/
-env PORT=4000 node app.js
+env PORT=4000 node app.js # what is the port you want?
+# shit. the port is 4000.
+
+# demo files under test directory:
+# download_exciting_bgm_with_lyric.py
+# lyric_change_detector/download_lyric.sh
