0100e7833b65a501518ab4bd1bd253ae3e81dd48
GitJournal <app@gitjournal.io>
2022-10-11 18:10:47 +0800

    [2022-10-11 10:10:45]
    FILE:
    tests/soundhound_houndify_midomi_sound_recognize_music/test_songrec_rust.sh
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/soundhound_houndify_midomi_sound_recognize_music/test_songrec_rust.sh
+++ tests/soundhound_houndify_midomi_sound_recognize_music/test_songrec_rust.sh
@@ -1,5 +1,6 @@
 songrec audio-file-to-recognized-song /root/Desktop/works/pyjom/tests/music_analysis/exciting_bgm.mp3 # this is quick and stable. no need to pass shit over it.
-# warning: we can only have preview for this song.
+# warning: we can only have preview for this song on apple music for free.
+# use youtube music?
 # even with lyrics. but the time? where?
 # songrec audio-file-to-recognized-song /root/Desktop/works/pyjom/tests/music_recognization/exciting_bgm_cut_10seconds.mp3
 # {
