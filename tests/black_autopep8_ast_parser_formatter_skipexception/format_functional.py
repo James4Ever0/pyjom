ba7e68250d3b11d221abb2d6d11eb38c64135e41
GitJournal <app@gitjournal.io>
2022-10-12 13:41:04 +0800

    [2022-10-12 05:41:01]
    FILE:
    tests/black_autopep8_ast_parser_formatter_skipexception/format_functional.py
    tests/black_autopep8_ast_parser_formatter_skipexception/format_test.sh
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/black_autopep8_ast_parser_formatter_skipexception/format_functional.py
+++ tests/black_autopep8_ast_parser_formatter_skipexception/format_functional.py
@@ -2,3 +2,5 @@ with open('test.py', 'r') as f:
     code = f.read()
 
 import subprocess
+command = 'autopep8 --max-line-length $MAXINT - | black -l $MAXINT -C -'
+commandLine = subprocess.run( [ 'bash','-c',command ], input=code )
--- tests/black_autopep8_ast_parser_formatter_skipexception/format_test.sh
+++ tests/black_autopep8_ast_parser_formatter_skipexception/format_test.sh
@@ -1,3 +1,4 @@
 # view only. no change on original content.
-MAXINT=1000000
+# of course, for lines with long content, we will have trouble.
+MAXINT=1000000000
 cat test.py | autopep8 --max-line-length $MAXINT - | black -l $MAXINT -C - 2>/dev/null
