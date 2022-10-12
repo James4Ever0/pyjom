cb56d80b1db23565eda1f72c00640aa448c42dab
GitJournal <app@gitjournal.io>
2022-10-12 13:28:28 +0800

    [2022-10-12 05:28:26]
    FILE:
    tests/ast_parser_formatter_skipexception/format_test.sh
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/ast_parser_formatter_skipexception/format_test.sh
+++ tests/ast_parser_formatter_skipexception/format_test.sh
@@ -1,3 +1,3 @@
 # view only. no change on original content.
-MAXINT=10000000
-cat test.py | autopep8 --max-line-length $MAXINT | black -l $MAXINT -C
+MAXINT=10000
+cat test.py | autopep8 --max-line-length $MAXINT -
