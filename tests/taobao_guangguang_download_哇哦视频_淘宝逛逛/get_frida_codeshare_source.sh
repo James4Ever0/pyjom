b0f5ebfe51cfb592d60fc86a9f015bc0ec8a2f1f
GitJournal <app@gitjournal.io>
2022-10-13 02:52:33 +0800

    [2022-10-12 18:52:31]
    FILE:
    tests/taobao_guangguang_download_哇哦视频_淘宝逛逛/get_frida_codeshare_source.sh
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/taobao_guangguang_download_哇哦视频_淘宝逛逛/get_frida_codeshare_source.sh
+++ tests/taobao_guangguang_download_哇哦视频_淘宝逛逛/get_frida_codeshare_source.sh
@@ -1,4 +1,5 @@
 # warning! potential code injection.
 # better use some json5 formatter instead.
-curl "https://codeshare.frida.re/@Gand3lf/xamarin-antiroot/" 2>/dev/null | grep "projectSource: " | sed 's/projectSource:/"projectSource":/;s/^/{/;s/,$//;s/$/}/'  
+# this is strict! fuck.
+curl "https://codeshare.frida.re/@Gand3lf/xamarin-antiroot/" 2>/dev/null | grep "projectSource: " | sed 's/projectSource:/"projectSource":/;s/^/{/;s/,$//;s/$/}/'   | python3 -c "d=input();import json;p="
 # curl "https://codeshare.frida.re/@Gand3lf/xamarin-antiroot/" 2>/dev/null | grep "projectSource: " | sed 's/^/var a={/;s/$/}\; console.log(a.projectSource);/' | node
