eea6b267d244da0190ecf2c5e65e617763532430
GitJournal <app@gitjournal.io>
2022-10-12 13:47:04 +0800

    [2022-10-12 05:47:03]
    FILE:
    tests/black_autopep8_ast_parser_formatter_skipexception/format_functional.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/black_autopep8_ast_parser_formatter_skipexception/format_functional.py
+++ tests/black_autopep8_ast_parser_formatter_skipexception/format_functional.py
@@ -1,19 +1,26 @@
-with open('test.py', 'r') as f:
+with open("test.py", "r") as f:
     code = f.read()
 
 # need binary data.
-code_encoded = code.encode('utf-8')
+code_encoded = code.encode("utf-8")
 
 import subprocess
-command = 'autopep8 --max-line-length $MAXINT - | black -l $MAXINT -C -'
-commandLine = [ 'bash','-c',command ]
-result = subprocess.run( commandLine, input=code_encoded, capture_output=True)
+
+MAXINT = 10000000000
+command = "autopep8 --max-line-length {MAXINT} - | black -l {MAXINT} -C -".format(
+    MAXINT=MAXINT
+)
+commandLine = ["bash", "-c", command]
+result = subprocess.run(commandLine, input=code_encoded, capture_output=True)
 try:
     assert result.returncode == 0
-    code_formatted = result.stdout.decode('utf-8')
+    code_formatted = result.stdout.decode("utf-8")
 except:
     import traceback
+
     traceback.print_exc()
+    print("STDOUT", result.stdout)
+    print("STDERR", result.stderr)
     code_formatted = code
 
 print(code_formatted)
