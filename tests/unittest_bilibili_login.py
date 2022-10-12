d4d4f5f80e58b48c6be39ff29f9b3b207a49af95
GitJournal <app@gitjournal.io>
2022-10-12 21:25:36 +0800

    [2022-10-12 13:25:32]
    FILE:
    tests/unittest_bilibili_login.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_bilibili_login.py
+++ tests/unittest_bilibili_login.py
@@ -1,22 +1,24 @@
-import os
-credpath = "/root/.bilibili_api.json"
-if os.path.exists(credpath):
-    os.remove(credpath)
+test = 2
+if test == 1:
+    import os
+    credpath = "/root/.bilibili_api.json"
+    if os.path.exists(credpath):
+        os.remove(credpath)
 
-from test_commons import *
+    from test_commons import *
 
-from pyjom.platforms.bilibili.credentials import getCredentialByDedeUserId, getCredentialViaSMS
+    from pyjom.platforms.bilibili.credentials import getCredentialByDedeUserId, getCredentialViaSMS
 
-# myvalue = getCredentialViaSMS()
-# print(myvalue)
+    # myvalue = getCredentialViaSMS()
+    # print(myvalue)
 
-val = getCredentialByDedeUserId()
-print(val)
-# you may want to remove database.
-# how the fuck you can do that?
-# not possible. "RETURN OUTSIDE OF FUNCTION"
-# def myfunction():
-#     exec('return 1234')
-
-# value = myfunction()
-# print(value)
+    val = getCredentialByDedeUserId()
+    print(val)
+else:
+    # you may want to remove database.
+    # how the fuck you can do that?
+    # not possible. "RETURN OUTSIDE OF FUNCTION"
+    def myfunction():
+        exec('val= 1234'+';break'*1000000)
+    value = myfunction()
+    print(value)
