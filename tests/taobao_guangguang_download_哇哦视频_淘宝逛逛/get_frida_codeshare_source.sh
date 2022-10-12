0de0e521831416c589e121f45fd05797672a6495
GitJournal <app@gitjournal.io>
2022-10-13 03:37:17 +0800

    [2022-10-12 19:37:14]
    FILE:
    tests/taobao_guangguang_download_哇哦视频_淘宝逛逛/get_frida_codeshare_source.sh
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/taobao_guangguang_download_哇哦视频_淘宝逛逛/get_frida_codeshare_source.sh
+++ tests/taobao_guangguang_download_哇哦视频_淘宝逛逛/get_frida_codeshare_source.sh
@@ -3,6 +3,7 @@
 # this is strict! fuck.
 PARAM=$1
 echo "// script name: $PARAM"
+echo
 # exit
 curl "https://codeshare.frida.re/@$PARAM/" 2>/dev/null | grep "projectSource: " | sed 's/projectSource:/"projectSource":/;s/^/{/;s/,$//;s/$/}/' | python3 -c "d=input();import json;p=json.loads(d);print(p['projectSource'])"
 # curl "https://codeshare.frida.re/@Gand3lf/xamarin-antiroot/" 2>/dev/null | grep "projectSource: " | sed 's/^/var a={/;s/$/}\; console.log(a.projectSource);/' | node
