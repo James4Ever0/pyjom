ded6d813ae9acaf0af6e70b28859b4aa6b39086c
GitJournal <app@gitjournal.io>
2022-10-12 21:10:31 +0800

    [2022-10-12 13:10:25]
    FILE:
    tests/unittest_bilibili_login.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_bilibili_login.py
+++ tests/unittest_bilibili_login.py
@@ -1,13 +1,13 @@
 from test_commons import *
 from pyjom.platforms.bilibili.credentials import getCredentialViaSMS
 
-# myvalue = getCredentialViaSMS()
-# print(myvalue)
+myvalue = getCredentialViaSMS()
+print(myvalue)
 
 # how the fuck you can do that?
 # not possible. "RETURN OUTSIDE OF FUNCTION"
-def myfunction():
-    exec('return 1234')
+# def myfunction():
+#     exec('return 1234')
 
-value = myfunction()
-print(value)
+# value = myfunction()
+# print(value)
