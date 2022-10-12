62d1720e339f9c5592434abbafdebe08e2d7b1de
GitJournal <app@gitjournal.io>
2022-10-12 15:18:28 +0800

    [2022-10-12 07:18:26]
    FILE:
    pyjom/platforms/bilibili/credentials.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/platforms/bilibili/credentials.py
+++ pyjom/platforms/bilibili/credentials.py
@@ -63,6 +63,18 @@ def removeCredentialByDedeUserId(dedeuserid: str):
         )
         return False
 
+from lazero.program.functools import skipException
+@skipException()
+def getCredentialViaSMS():
+    phone = input("请输入手机号：")
+    print("正在登录。")
+    send_sms(PhoneNumber(phone, country="+86"))  # 默认设置地区为中国大陆
+    code = input("请输入验证码：")
+    c = login_with_sms(PhoneNumber(phone, country="+86"), code)
+    credential = c
+    # first, check if this is a valid credential.
+    name = verifyCredential(credential)
+    return credential, name
 
 def getCredentialByDedeUserId(dedeuserid: str = "397424026"):
     dataList = db.search(User.dedeuserid == dedeuserid)
@@ -91,27 +103,21 @@ def getCredentialByDedeUserId(dedeuserid: str = "397424026"):
     # you choose to login via SMS.
     while True:
         # could be troublesome.
-        def getCredentialViaSMS():
-            phone = input("请输入手机号：")
-            print("正在登录。")
-            send_sms(PhoneNumber(phone, country="+86"))  # 默认设置地区为中国大陆
-            code = input("请输入验证码：")
-            c = login_with_sms(PhoneNumber(phone, country="+86"), code)
-            credential = c
-            # first, check if this is a valid credential.
-            name = verifyCredential(credential)
-        if name != False:
-            print("登录成功")
-            # update with this credential!
-            # next, check if this is the credential we need.
-            if credential.dedeuserid == dedeuserid:
-                return credential
-            else:
-                print("dedeuserid not right.")
-                print(
-                    "user %s (dedeuserid: %s) does not have dedeuserid: %s"
-                    % (name, credential.dedeuserid, dedeuserid)
-                )
+        result = getCredentialViaSMS()
+        if result !=None:
+            credential, name = result
+            if name != False:
+                print("登录成功")
+                # update with this credential!
+                # next, check if this is the credential we need.
+                if credential.dedeuserid == dedeuserid:
+                    return credential
+                else:
+                    print("dedeuserid not right.")
+                    print(
+                        "user %s (dedeuserid: %s) does not have dedeuserid: %s"
+                        % (name, credential.dedeuserid, dedeuserid)
+                    )
         else:
             print("登陆失败")
 
