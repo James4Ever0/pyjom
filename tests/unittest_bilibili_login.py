a641caf91546c0cf425d82116056e089f7863f47
GitJournal <app@gitjournal.io>
2022-10-12 21:00:42 +0800

    [2022-10-12 13:00:39]
    FILE:
    tests/unittest_bilibili_login.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_bilibili_login.py
+++ tests/unittest_bilibili_login.py
@@ -1,5 +1,10 @@
 from test_commons import *
 from pyjom.platforms.bilibili.credentials import getCredentialViaSMS
 
-myvalue = getCredentialViaSMS()
-print(myvalue)
+# myvalue = getCredentialViaSMS()
+# print(myvalue)
+def myfunction():
+    exec('return 1234')
+
+value = myfunction()
+print(value)
