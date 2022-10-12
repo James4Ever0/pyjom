f536ffb7c0a4afcf8812613a4f34a65daed43cbd
GitJournal <app@gitjournal.io>
2022-10-12 13:31:29 +0800

    [2022-10-12 05:31:28]
    FILE:
    tests/ast_parser_formatter_skipexception/format_test.sh
    tests/ast_parser_formatter_skipexception/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/ast_parser_formatter_skipexception/format_test.sh
+++ tests/ast_parser_formatter_skipexception/format_test.sh
@@ -1,3 +1,3 @@
 # view only. no change on original content.
-MAXINT=10000
-cat test.py | autopep8 --max-line-length $MAXINT -
+MAXINT=1000000
+cat test.py | autopep8 --max-line-length $MAXINT - | black -l $MAXINT -C - 2>/dev/null
--- tests/ast_parser_formatter_skipexception/test.py
+++ tests/ast_parser_formatter_skipexception/test.py
@@ -4,10 +4,32 @@
 # first, autopep8, next, black
 # both with 'unlimited' line of code.
 # finally, throw it to our dearly 'skipException'
-    print('aaa'
+    from lib2to3.pgen2.pgen import DFAState
+from mimetypes import suffix_map
+from os import SCHED_FIFO
+from socket import _SendableFile
+from xml.dom.pulldom import SAX2DOM
+
+
+print('aaa'
         ) # there is no repairing on this bracket for autopep8
 
 # about the dog_or_cat recognition of our cover:
 # 1. throw away unqualified ones (using pop?)
 # 2. lower the threshold of yolo
 # 3. downscale picture before passing to yolo
+
+# we can go wild here.
+@redisLRUCache(dfsji,
+asdif[dfk,DFAState,
+sdfkg])
+def shit(aaa, bbb,
+ccc,ddd):
+    dd = 2314
+    ee = suffix_map[SAX2DOM,
+    df23, ddd][sdf,
+    sdf,sdf]
+    ss = efldife.dfief(_SendableFile,
+    saif,SCHED_FIFO,
+    asdif[fjisd,
+    sfdsif,sdf])
