169a988a2d393be9ff6a1925473aaf2f6efcd593
GitJournal <app@gitjournal.io>
2022-10-13 19:00:12 +0800

    [2022-10-13 11:00:10]
    FILE:
    tests/video_phash_deduplication/test_image_hash.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/video_phash_deduplication/test_image_hash.py
+++ tests/video_phash_deduplication/test_image_hash.py
@@ -1,8 +1,10 @@
 pic_0= 'cat.png'
 pic_0_similar = "cat2.png"
 pic_1 = "/root/Desktop/works/pyjom/samples/image/dick.png"
-
+from PIL import Image
+# >>> import imagehash
+# >>> hash = imagehash.average_hash(Image.open
 import imagehash
 
 pics = [pic_0, pic_0_similar, pic_1]
-hashs = [ for pic in pics]
+hashs = [imagehash.average_hash(Image.open(pic)) for pic in pics]
