a499a1f4584038057882673c739d05d25077b9b5
GitJournal <app@gitjournal.io>
2022-10-13 20:22:15 +0800

    [2022-10-13 12:21:52]
    FILE:
    tests/video_phash_deduplication/config_milvus.sh
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

---
+++ tests/video_phash_deduplication/config_milvus.sh
@@ -0,0 +1,5 @@
+# Create Milvus file
+$ mkdir -p /home/$USER/milvus/conf
+$ cd /home/$USER/milvus/conf
+$ wget https://raw.githubusercontent.com/milvus-io/milvus/v0.8.0/core/conf/demo/server_config.yaml
+$ wget https://raw.githubusercontent.com/milvus-io/milvus/v0.8.0/core/conf/demo/log_config.conf
