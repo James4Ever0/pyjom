e94fc082659762016476fadf333e88e5a773771e
GitJournal <app@gitjournal.io>
2022-10-13 19:18:26 +0800

    [2022-10-13 11:18:23]
    FILE:
    pyjom/musictoolbox.py
    tests/video_phash_deduplication/test_video_hash.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/musictoolbox.py
+++ pyjom/musictoolbox.py
@@ -245,33 +245,7 @@ from typing import Literal
 import subprocess
 import traceback
 
-
-def runCommandGetJson(
-    commandLine: list[str],
-    timeout: int = 5,
-    debug: bool = False,
-    shell: bool = False,
-    workingDirectory: Union[str, None] = None,
-):
-
-    try:
-        result = subprocess.run(
-            commandLine,
-            timeout=timeout,
-            capture_output=True,
-            shell=shell,
-            cwd=workingDirectory,
-        )
-        assert result.returncode == 0
-        stdout = result.stdout
-        stdout = stdout.decode("utf-8")
-        output = json.loads(stdout)
-        return True, output
-    except:
-        if debug:
-            traceback.print_exc()
-    return False, {}
-
+from lazero.program.subprocess import runCommandGetJson
 
 def runCommandAndProcessSongRecognizationJson(
     commandLine: list[str],
--- tests/video_phash_deduplication/test_video_hash.py
+++ tests/video_phash_deduplication/test_video_hash.py
@@ -1,1 +1,5 @@
 # use some delogo stuff.
+from lazero.
+# these two are similar. can be used as threshold.
+# aaaa3d8a2eaa1f8a delogo
+# aaaa398a2faa5d8a not delogoed.
