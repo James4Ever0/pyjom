e4d16fbe737e5ab7d58b08bda30a5adb3f7309bd
GitJournal <app@gitjournal.io>
2022-10-13 20:27:57 +0800

    [2022-10-13 12:27:55]
    FILE:
    tests/launch_test_enviroment.sh
    tests/video_phash_deduplication/config_milvus.sh
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/launch_test_enviroment.sh
+++ tests/launch_test_enviroment.sh
@@ -1,3 +1,6 @@
+# make sure milvus is running.
+bash /root/Desktop/works/pyjom/tests/video_phash_deduplication/config_milvus.sh
+
 # launch netease api server. we need it to download new music, currently.
 # video phash is the last step among all filters.
 cd /root/Desktop/works/pyjom/externals/NeteaseCloudMusicApi
--- tests/video_phash_deduplication/config_milvus.sh
+++ tests/video_phash_deduplication/config_milvus.sh
@@ -1,5 +1,8 @@
-# Create Milvus file
-$ mkdir -p /home/$USER/milvus/conf
-$ cd /home/$USER/milvus/conf
-$ wget https://raw.githubusercontent.com/milvus-io/milvus/v0.8.0/core/conf/demo/server_config.yaml
-$ wget https://raw.githubusercontent.com/milvus-io/milvus/v0.8.0/core/conf/demo/log_config.conf
+# # Create Milvus file
+# $ mkdir -p /home/$USER/milvus/conf
+# $ cd /home/$USER/milvus/conf
+# $ wget https://raw.githubusercontent.com/milvus-io/milvus/v0.8.0/core/conf/demo/server_config.yaml
+# $ wget https://raw.githubusercontent.com/milvus-io/milvus/v0.8.0/core/conf/demo/log_config.conf
+sudo systemctl start milvus
+sudo systemctl start milvus-etcd
+sudo systemctl start milvus-minio
