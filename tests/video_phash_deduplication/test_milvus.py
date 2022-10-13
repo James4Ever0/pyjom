8f2c98e290449b2df645b1f855765b22fe0c3efa
GitJournal <app@gitjournal.io>
2022-10-13 21:47:44 +0800

    [2022-10-13 13:47:42]
    FILE:
    tests/video_phash_deduplication/test_milvus.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/video_phash_deduplication/test_milvus.py
+++ tests/video_phash_deduplication/test_milvus.py
@@ -1,4 +1,3 @@
-
 # duplicate -> remove, do not insert
 # not duplicate -> get the data, insert
 
@@ -6,57 +5,65 @@
 
 # import pymilvus
 from pymilvus import connections
-connection = connections.connect(
-  alias="default", 
-  host='localhost', 
-  port='19530'
-)
 
+connection = connections.connect(alias="default", host="localhost", port="19530")
+collection_name = "video_deduplication"
 
 
 from pymilvus import Collection
+# Collection(collection_name)
+from pymilvus import utility
+utility.drop_collection("book")
 
 from pymilvus import CollectionSchema, FieldSchema, DataType
+
 book_id = FieldSchema(
-  name="video_semantic_id", 
-  dtype=DataType.INT64, 
-  is_primary=True, 
+    name="video_semantic_id",
+    dtype=DataType.INT64,
+    is_primary=True,
 )
 word_count = FieldSchema(
-  name="video_length", 
-  dtype=DataType.FLOAT,  
+    name="video_length",
+    dtype=DataType.FLOAT,
 )
-book_intro = FieldSchema(
-  name="video_phash", 
-  dtype=DataType.BINARY_VECTOR, 
-  dim=2
-)
+book_intro = FieldSchema(name="video_phash", dtype=DataType.BINARY_VECTOR, dim=(8,8))
 schema = CollectionSchema(
-  fields=[book_id, word_count, book_intro], 
-  description="Test video deduplication"
+    fields=[book_id, word_count, book_intro], description="Test video deduplication"
 )
-collection_name = "video_deduplication"
 
 # collection = Collection("video")      # Get an existing collection.
 collection = Collection(
-    name=collection_name, 
-    schema=schema, 
-    using='default', 
+    name=collection_name,
+    schema=schema,
+    using="default",
     shards_num=2,
-    )
+)
 # is this demo collection?
 collection.load()
 
 # seems hard to setup.
 # not started!
 search_params = {"metric_type": "L2", "params": {"nprobe": 10}}
+import numpy as np
 
+queryData = np.array(
+    [
+        [True, True, True, False, False, True, False, True],
+        [True, False, False, True, False, True, True, False],
+        [True, False, False, True, True, False, False, True],
+        [True, True, True, True, True, False, False, True],
+        [True, False, False, True, False, True, True, False],
+        [False, True, True, False, False, False, False, True],
+        [True, True, False, False, False, True, True, False],
+        [False, False, True, False, False, True, False, False],
+    ]
+)
 results = collection.search(
-	data=[[0.1, 0.2]],  # this is the float dimension.
-	anns_field="video_phash", 
-	param=search_params, 
-	limit=10,
-	expr=None,
-	consistency_level="Strong"
+    data=queryData,  # this is the float dimension.
+    anns_field="video_phash",
+    param=search_params,
+    limit=10,
+    expr=None,
+    consistency_level="Strong",
 )
 print(results)
