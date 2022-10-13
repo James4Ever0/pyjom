fb91c2bfce54702b2b4c781f8f0e700a5d23cc9b
GitJournal <app@gitjournal.io>
2022-10-13 22:33:06 +0800

    [2022-10-13 14:33:04]
    FILE:
    tests/video_phash_deduplication/test_milvus.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/video_phash_deduplication/test_milvus.py
+++ tests/video_phash_deduplication/test_milvus.py
@@ -11,15 +11,17 @@ collection_name = "video_deduplication"
 
 
 from pymilvus import Collection
+
 # Collection(collection_name)
 # remote this thing.
 from pymilvus import utility
+
 try:
-  utility.drop_collection(collection_name)
+    utility.drop_collection(collection_name)
 except:
-  import traceback
-  traceback.print_exc()
-  print('maybe the collection does not exist')
+    import traceback
+    traceback.print_exc()
+    print("maybe the collection does not exist")
 
 from pymilvus import CollectionSchema, FieldSchema, DataType
 
@@ -32,10 +34,11 @@ video_length = FieldSchema(
     name="video_length",
     dtype=DataType.FLOAT,
 )
-video_phash = FieldSchema(name="video_phash", dtype=DataType.BINARY_VECTOR, dim=8) # 64
+video_phash = FieldSchema(name="video_phash", dtype=DataType.BINARY_VECTOR, dim=64)  # 64
 # single dimension? no multi dimension support?
 schema = CollectionSchema(
-    fields=[video_semantic_id,  video_length, video_phash], description="Test video deduplication"
+    fields=[video_semantic_id, video_length, video_phash],
+    description="Test video deduplication",
 )
 
 # collection = Collection("video")      # Get an existing collection.
@@ -46,7 +49,6 @@ collection = Collection(
     shards_num=2,
 )
 # is this demo collection?
-# collection.load()
 
 # seems hard to setup.
 # not started!
@@ -66,20 +68,24 @@ queryData = np.array(
     ]
 )
 queryData = queryData.reshape(-1).tolist()
-queryData = ['1' if x else '0' for x in queryData]
+queryData = ["1" if x else "0" for x in queryData]
 import bitarray
-queryData = bitarray.bitarray("".join(queryData), endian='little')
+
+queryData = bitarray.bitarray("".join(queryData), endian="little")
 queryData = queryData.tobytes()
 # dimension: 8*8=64
-collection.insert([[1],[b'\x07']])
+collection.insert([[1], [np.float32(3.5)], [queryData]])
 # print(len(queryData), len(queryData)*8)
 # # print(queryData.shape)
 # breakpoint()
+collection.load()
+collection.
+
 # # 1,64
 # what is wrong? wtf?
 # queryData = queryData.tolist()
 results = collection.search(
-    data=queryData,  # this is the float dimension.
+    data=[queryData],  # this is the float dimension.
     anns_field="video_phash",
     param=search_params,
     limit=10,
