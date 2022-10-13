a038f5b75a60b8c84875d1804e8166472b7f87a8
GitJournal <app@gitjournal.io>
2022-10-13 19:42:34 +0800

    [2022-10-13 11:42:32]
    FILE:
    tests/video_phash_deduplication/test_video_hash.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/video_phash_deduplication/test_video_hash.py
+++ tests/video_phash_deduplication/test_video_hash.py
@@ -3,13 +3,22 @@ from lazero.program.subprocess import runCommandGetJson
 # these two are similar. can be used as threshold.
 # aaaa3d8a2eaa1f8a delogo
 # aaaa398a2faa5d8a not delogoed.
-import os
-import imagehash
-assert os.path.exists(filepath)
-assert os.path.isfile(filepath)
-if not os.path.isabs(filepath):
-    filepath = os.path.abspath(filepath)
-commandLine = ["/root/Desktop/works/pyjom/tests/video_phash_deduplication/videohashes/videohashes-linux","-json",filepath]
-myJson = runCommandGetJson(commandLine)
-phashString = myJson['phash']
-phash = imagehash.
+# aaaa3c8a2faa5e8a mp4 (very similar to delogoed version)
+
+videoPaths = ['cute_cat_gif.mp4','cute_cat_gif.gif','cat_delogo.gif']
+
+def getVideoPHash(filepath, debug=False):
+    import os
+    import imagehash
+    assert os.path.exists(filepath)
+    assert os.path.isfile(filepath)
+    if not os.path.isabs(filepath):
+        filepath = os.path.abspath(filepath)
+    commandLine = ["/root/Desktop/works/pyjom/tests/video_phash_deduplication/videohashes/videohashes-linux","-json",filepath]
+    myJson = runCommandGetJson(commandLine)
+    phashString = myJson['phash']
+    phash =imagehash.hex_to_hash(phashString)
+    if debug:
+        print("FILEPATH: %s" % filepath)
+        print("PHASH: " + phash)
+    return phash
