91ad7584e2baf17d3630bde9746a17d69f0e7ec2
GitJournal <app@gitjournal.io>
2022-10-13 19:58:29 +0800

    [2022-10-13 11:57:39]
    FILE:
    tests/video_phash_deduplication/test_image_hash_milvus_database_search.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

---
+++ tests/video_phash_deduplication/test_image_hash_milvus_database_search.py
@@ -0,0 +1,22 @@
+pic_0= 'cat.png'
+pic_0_similar = "cat3.png"
+pic_1 = "/root/Desktop/works/pyjom/samples/image/dick.png"
+from PIL import Image
+# >>> import imagehash
+# >>> hash = imagehash.average_hash(Image.open
+import imagehash
+
+pics = [pic_0, pic_0_similar, pic_1]
+hashs = [imagehash.phash(Image.open(pic)) for pic in pics]
+
+dis0 = hashs[0]-hashs[1]
+dis1 = hashs[1]-hashs[2]
+
+print(dis0, dis1)
+# 0 24
+# 6 24
+
+# well, let's check?
+# print(hashs)
+# three truth tables.
+# 2^4 = 16, total 2*8 digits=16?
