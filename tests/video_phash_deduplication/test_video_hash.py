b8ce18ebd36d7c9c766b78c7196eff9a7a48c8eb
GitJournal <app@gitjournal.io>
2022-10-13 19:24:27 +0800

    [2022-10-13 11:24:25]
    FILE:
    tests/video_phash_deduplication/test_video_hash.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/video_phash_deduplication/test_video_hash.py
+++ tests/video_phash_deduplication/test_video_hash.py
@@ -1,5 +1,15 @@
 # use some delogo stuff.
-from lazero.
+from lazero.program.subprocess import runCommandGetJson
 # these two are similar. can be used as threshold.
 # aaaa3d8a2eaa1f8a delogo
 # aaaa398a2faa5d8a not delogoed.
+import os
+import imagehash
+assert os.path.exists(filepath)
+assert os.path.isfile(filepath)
+if not os.path.isabs(filepath):
+    filepath = os.path.abspath(filepath)
+commandLine = ["/root/Desktop/works/pyjom/tests/video_phash_deduplication/videohashes/videohashes-linux","-json",filepath]
+myJson = runCommandGetJson(commandLine)
+phashString = myJson['phash']
+phash = 
