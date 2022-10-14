5731a93852dd9040ca6cb24dee9d1bf4441f625a
GitJournal <app@gitjournal.io>
2022-10-14 17:02:38 +0800

    [2022-10-14 09:02:34]
    FILE:
    tests/search_engine_suggestion_based_qa_bot/parse_baidu_title_abstract.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_suggestion_based_qa_bot/parse_baidu_title_abstract.py
+++ tests/search_engine_suggestion_based_qa_bot/parse_baidu_title_abstract.py
@@ -11,13 +11,20 @@ for perm in permitted:
     if perm in punctuations:
         punctuations.remove(perm)
     
-def processQueryResult(abstract, minMaxDict={'min':5,'max':15}):
+
+def removeTimeInfo(phrase):
+    import re
+    timeinfos = re.findall(r'\d+年\d+月\d+日', phrase)
+    for timeinfo in timeinfos:
+        phrase = phrase.replace(timeinfo, "")
+    return phrase
+def processQueryResult(abstract, minMaxDict={'min':5,'max':20}):
     for punc in punctuations:
         abstract = abstract.replace(punc, "\n")
     abstract = abstract.split("\n")
     for phrase in abstract:
-        phrase = phrase.strip()
         phrase = removeTimeInfo(phrase)
+        phrase = phrase.strip()
         if not checkMinMaxDict(len(phrase), minMaxDict):
             continue
         else:
@@ -26,3 +33,4 @@ for elem in data:
     title = elem.get('title')
     abstract = elem.get('abstract')
     for phrase in processQueryResult(abstract):
+        print(phrase)
