d4f3967de986bcb42e35f3a983104bb69ad4fb7e
GitJournal <app@gitjournal.io>
2022-10-15 00:06:19 +0800

    [2022-10-14 16:06:16]
    FILE:
    tests/search_engine_suggestion_based_qa_bot/search_image_with_keywords.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_suggestion_based_qa_bot/search_image_with_keywords.py
+++ tests/search_engine_suggestion_based_qa_bot/search_image_with_keywords.py
@@ -1,1 +1,9 @@
 # not sure if it relates.
+
+from baiduspider import BaiduSpider
+spider=BaiduSpider()
+from pprint import pprint
+query = "绝对领域"
+result = spider.search_img(query, pn= 1)
+print(result)
+breakpoint()
