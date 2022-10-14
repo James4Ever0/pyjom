883ce8aa82c2a370ed6eb205b7e8719e261d7f22
GitJournal <app@gitjournal.io>
2022-10-14 16:56:34 +0800

    [2022-10-14 08:56:31]
    FILE:
    tests/search_engine_suggestion_based_qa_bot/parse_baidu_title_abstract.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_suggestion_based_qa_bot/parse_baidu_title_abstract.py
+++ tests/search_engine_suggestion_based_qa_bot/parse_baidu_title_abstract.py
@@ -1,8 +1,16 @@
 from lazero.filesystem.io import readJsonObjectFromFile
-
+from lazero. import checkMinMaxDict
 data = readJsonObjectFromFile('result_baidu.json')
-
+import string
+from zhon import hanzi
 for elem in data:
     title = elem.get('title')
     abstract = elem.get('abstract')
-    
+    punctuations = set(list(string.punctuation+hanzi.punctuation))
+    punctuations.remove(" ")
+    for punc in punctuations:
+        abstract = abstract.replace(punc, "\n")
+    abstract = abstract.split("\n")
+    for phrase in abstract:
+        if not checkMinMaxDict(len(phrase), minMaxDict):
+    return abstract
