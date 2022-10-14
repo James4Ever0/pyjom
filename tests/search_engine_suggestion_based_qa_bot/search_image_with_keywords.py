a92d3187a09e60991d11d4f5774af62b4bc95fd2
GitJournal <app@gitjournal.io>
2022-10-15 00:12:21 +0800

    [2022-10-14 16:12:19]
    FILE:
    tests/search_engine_suggestion_based_qa_bot/search_image_with_keywords.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_suggestion_based_qa_bot/search_image_with_keywords.py
+++ tests/search_engine_suggestion_based_qa_bot/search_image_with_keywords.py
@@ -7,4 +7,7 @@ query = "绝对领域"
 result = spider.search_pic(query, pn= 1) # are we fucked?
 # yeah we have result.
 print(result)
+result.plain
 breakpoint()
+# 'title', 'url', 'host'
+# can we search for gif?
