cb97289d8a48510758c6b23712488b9679630f34
GitJournal <app@gitjournal.io>
2022-10-14 21:51:06 +0800

    [2022-10-14 13:51:04]
    FILE:
    tests/search_engine_suggestion_based_qa_bot/parse_baidu_search_ajax.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_suggestion_based_qa_bot/parse_baidu_search_ajax.py
+++ tests/search_engine_suggestion_based_qa_bot/parse_baidu_search_ajax.py
@@ -7,5 +7,11 @@ from lazero.filesystem.io import readJsonObjectFromFile
 obj = readJsonObjectFromFile('ajax_baidu.json')
 processed_obj = pyjq.first(command,obj)
 import pandas as pd
-df = pd.DataFrame(processed_obj)
-print(df)
+from pprint import pprint
+pprint(processed_obj)
+df_title_snippets = pyjq.first("{}",procecssed_obj)
+# [('titles', 15), ('snippets', 15), ('imgs_src', 43), ('simi', 43)]
+# 15, 15, 43, 43
+# df = pd.DataFrame(processed_obj)
+# print(df)
+breakpoint()
