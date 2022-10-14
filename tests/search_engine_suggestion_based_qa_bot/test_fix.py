052e96032061dd518c3b566ad878f8abd82076fa
GitJournal <app@gitjournal.io>
2022-10-15 00:18:29 +0800

    [2022-10-14 16:18:24]
    FILE:
    tests/search_engine_suggestion_based_qa_bot/test_fix.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

---
+++ tests/search_engine_suggestion_based_qa_bot/test_fix.py
@@ -0,0 +1,11 @@
+query = "python有个问题想请教一下 为什么我这个函数跑不通"
+
+from baiduspider import BaiduSpider
+spider=BaiduSpider()
+from pprint import pprint
+
+result = spider.search_web(query, pn= 1)
+print(result.plain)
+# change the div class name.
+# https://github.com/BaiduSpider/BaiduSpider/pull/151/files
+breakpoint()
