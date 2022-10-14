8d22e881b07dc983a6a2ba4377294e9cf8a3ad1b
GitJournal <app@gitjournal.io>
2022-10-14 16:53:33 +0800

    [2022-10-14 08:53:30]
    FILE:
    tests/search_engine_suggestion_based_qa_bot/parse_baidu_title_abstract.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_suggestion_based_qa_bot/parse_baidu_title_abstract.py
+++ tests/search_engine_suggestion_based_qa_bot/parse_baidu_title_abstract.py
@@ -1,1 +1,8 @@
-with open('')
+from lazero.filesystem.io import readJsonObjectFromFile
+
+data = readJsonObjectFromFile('result_baidu.json')
+
+for elem in data:
+    title = elem.get('title')
+    abstract = elem.get('abstract')
+    
