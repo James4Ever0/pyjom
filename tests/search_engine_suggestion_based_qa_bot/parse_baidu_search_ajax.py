158bc99b2f16fcdcb5e57745822fcbc56349c5ae
GitJournal <app@gitjournal.io>
2022-10-14 22:02:21 +0800

    [2022-10-14 14:02:17]
    FILE:
    tests/search_engine_suggestion_based_qa_bot/parse_baidu_search_ajax.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_suggestion_based_qa_bot/parse_baidu_search_ajax.py
+++ tests/search_engine_suggestion_based_qa_bot/parse_baidu_search_ajax.py
@@ -11,14 +11,16 @@ import pandas as pd
 # from pprint import pprint
 # pprint(processed_obj)
 title_snippets = pyjq.first("{titles, snippets}", processed_obj)
-img_sim = pyjq.first("", processed_obj)
-img_sim['simi'] = img_sim['simi']
+img_sim = pyjq.first("(.simi[]|=tonumber )|{imgs_src, simi}", processed_obj)
+img_sim["simi"] = img_sim["simi"]
 # [('titles', 15), ('snippets', 15), ('imgs_src', 43), ('simi', 43)]
 # 15, 15, 43, 43
 df_title_snippets = pd.DataFrame(title_snippets)
 df_img_sim = pd.DataFrame(img_sim)
 print(df_title_snippets.head())
 print(df_img_sim.head())
-elem = df_img_sim['simi'][0]
-print(type(elem), elem) # str?
+elem = df_img_sim["simi"][0]
+print(type(elem), elem)  # str?
 # breakpoint()
+from urllib.parse import parse_qs
+df_img_sim['imgs_src'].split("?")
