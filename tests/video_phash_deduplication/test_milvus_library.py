67efa50afdc434e2f22179583ee84709c9888bce
GitJournal <app@gitjournal.io>
2022-10-16 11:34:58 +0800

    [2022-10-16 03:34:56]
    FILE:
    tests/video_phash_deduplication/test_milvus_library.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/video_phash_deduplication/test_milvus_library.py
+++ tests/video_phash_deduplication/test_milvus_library.py
@@ -193,8 +193,8 @@
 import sys
 import os
 
-os.chdir("../../")
-sys.path.append(".")
+# os.chdir("../../")
+sys.path.append("../../")
 # ignore the global proxy now, we are not going to use that.
 os.environ["http_proxy"] = ""
 os.environ["https_proxy"] = ""
