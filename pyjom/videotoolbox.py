a47d6fa6f56f20b9931618093f1cedb77c4e19d3
GitJournal <app@gitjournal.io>
2022-10-16 09:42:24 +0800

    [2022-10-16 01:42:22]
    FILE:
    pyjom/videotoolbox.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:
    /media/root/parrot/pyjom/pyjom/videotoolbox.py

--- pyjom/videotoolbox.py
+++ pyjom/videotoolbox.py
@@ -2572,6 +2572,217 @@ def getVideoTextAreaRatio(
 
 ########################### VIDEO DEDUPLICATION WITH MILVUS #########################
 
+from lazero.program.subprocess import runCommandGetJson
+
+# these two are similar. can be used as threshold.
+# aaaa3d8a2eaa1f8a delogo
+# aaaa398a2faa5d8a not delogoed.
+# aaaa3c8a2faa5e8a mp4 (very similar to delogoed version)
+
+def getVideoPHash(filepath,debug=False, timeout=100):
+    import os
+    import imagehash
+    assert os.path.exists(filepath)
+    assert os.path.isfile(filepath)
+    if not os.path.isabs(filepath):
+        filepath = os.path.abspath(filepath)
+    commandLine = [
+        "videohashes", # installed in path.
+        # "/root/Desktop/works/pyjom/tests/video_phash_deduplication/videohashes/videohashes-linux",
+        "-json",
+        filepath,
+    ]
+    success, myJson = runCommandGetJson(commandLine, debug=debug, timeout=timeout)
+    if debug:
+        print("SUCCESS?", success)
+        print(myJson, type(myJson))
+    if not success:
+        return
+    # breakpoint()
+    phashString = myJson["phash"]
+    phash = imagehash.hex_to_hash(phashString)
+    if debug:
+        print("FILEPATH: %s" % filepath)
+        print(myJson)
+        print("PHASH:", phash)
+    # if withDuration:
+    #     duration = myJson["duration"]
+    #     return duration, phash
+    # duration is inaccurate
+    return phash
+
+
+# connectMilvusDatabase()
+# connectMilvusDatabase() # will not connect again.
+from pymilvus import Collection
+from pymilvus import utility
+from pymilvus import CollectionSchema, FieldSchema, DataType
+
+import traceback
+
+
+def getMilvusVideoDeduplicationCollection(
+    get_existing: bool = False,
+):  # most of the time we just use the same
+    collection_name = "video_deduplication"
+    try:
+        if utility.has_collection(collection_name):  # be prudent.
+            if get_existing:
+                return Collection(collection_name)
+            utility.drop_collection(collection_name)
+    except:
+        traceback.print_exc()
+        print("maybe the collection does not exist")
+    video_semantic_id = FieldSchema(  # how to insert this shit without prior knowledge?
+        name="video_semantic_id",
+        dtype=DataType.INT64,
+        is_primary=True,  # if is primary, will do check for 'not duplicate' or something.
+        auto_id=True,  # no need for id generation.
+    )
+    video_length = FieldSchema(
+        name="video_length",
+        dtype=DataType.FLOAT,
+    )
+    video_phash = FieldSchema(
+        name="video_phash", dtype=DataType.BINARY_VECTOR, dim=64
+    )  # 64
+    # single dimension? no multi dimension support?
+    schema = CollectionSchema(
+        fields=[video_semantic_id, video_length, video_phash],
+        description="Test video deduplication",
+    )
+
+    collection = Collection(
+        name=collection_name,
+        schema=schema,
+        using="default",
+        shards_num=2,
+    )
+    # is this demo collection?
+    return collection
+
+
+# seems hard to setup.
+# not started!
+# https://milvus.io/docs/v2.0.0/metric.md#binary
+# the metric is important to us.
+import numpy as np
+import bitarray
+
+
+@lru_cache(maxsize=1)
+def transformVideoPhash(videoPhash):
+    # we need the raw phash.
+    queryData = videoPhash.hash  # videoPhashTruthTable8x8 or something
+    queryData = queryData.reshape(-1).tolist()
+    queryData = ["1" if x else "0" for x in queryData]
+    queryData = bitarray.bitarray("".join(queryData), endian="little")
+    queryData = queryData.tobytes()
+    return queryData
+
+
+# dimension: 8*8=64
+
+
+def indexVideoWithVideoDurationAndPhash(collection, videoDuration, videoPhash):
+    queryData = transformVideoPhash(videoPhash)
+    collection.insert([[np.float32(videoDuration)], [queryData]])
+
+
+# can release even if not loaded.
+
+from test_video_hash import getVideoPHash
+import caer
+
+
+@lru_cache(maxsize=1)
+def getVideoDurationAndPhashFromFile(videoFilePath):
+    videoDuration = caer.video.frames_and_fps.get_duration(videoFilePath)
+    videoPhash = getVideoPHash(videoFilePath)
+    return videoDuration, videoPhash
+
+
+def indexVideoWithVideoDurationAndPhashFromFile(collection, videoFilePath):
+    videoDuration, videoPhash = getVideoDurationAndPhashFromFile(videoFilePath)
+    indexVideoWithVideoDurationAndPhash(collection, videoDuration, videoPhash)
+
+
+def reloadMilvusCollection(collection):
+    collection.release()  # unload.
+    collection.load()
+
+
+# make it into some library!
+# insert after load?
+
+# # 1,64
+# what is wrong? wtf?
+# queryData = queryData.tolist()
+def getDistancesBySearchingDuplicatedVideoInMilvusByFile(
+    collection,
+    videoFilePath,
+    search_params={"metric_type": "Jaccard", "params": {"nprobe": 10}},
+    autoreload: bool = True,
+    span: float = 2,
+    debug: bool = False,
+    limit: int = 10,
+):
+    if autoreload:
+        reloadMilvusCollection(collection)
+    videoDuration, videoPhash = getVideoDurationAndPhashFromFile(videoFilePath)
+    queryData = transformVideoPhash(videoPhash)
+    minVideoLength = max(0, videoDuration - span)
+    maxVideoLength = videoDuration + span
+    results = collection.search(
+        data=[queryData],  # this is the float dimension.
+        anns_field="video_phash",
+        param=search_params,
+        output_fields=["video_length"],
+        limit=limit,
+        expr="video_length > {minVideoLength} and video_length < {maxVideoLength}".format(
+            minVideoLength=minVideoLength, maxVideoLength=maxVideoLength
+        ),
+    )
+    theHit = results[0]
+    # print(theHit)
+    # so we can perform search without filtering afterwards.
+    # results[0][0].entity.get('video_length')
+    # print(results[0].ids)
+    # now, we want to have the 'distance' parameter.
+    # print(results[0])
+    # print(theHit)
+    distances = list(theHit.distances)
+    if debug:
+        print("distances: %s" % distances)
+
+    return distances
+    # what is the distance? we need to try.
+    # returh the closest distance?
+    # results = [x for x in theHit]
+    # hits = len(theHit)
+    # breakpoint()
+    # how to get document by id? wtf
+
+
+def checkDuplicatedVideoAndInsertVector(
+    collection,
+    videoPath,
+    threshold: float = 0.15,  # are you sure?
+    insertDuplicatedVector: bool = True,
+    debug: bool = True,
+):
+    reloadMilvusCollection(collection)
+    distances = getDistancesBySearchingDuplicatedVideoInMilvusByFile(
+        collection, videoPath, debug=debug
+    )
+
+    minDistance = min(distances + [1])  # empty!
+    duplicated = minDistance < threshold
+    if insertDuplicatedVector or (not duplicated):
+        indexVideoWithVideoDurationAndPhashFromFile(
+            collection, videoPath
+        )  # anyway let's do this.
+    return duplicated
 
 
 ########################### VIDEO DEDUPLICATION WITH MILVUS #########################
