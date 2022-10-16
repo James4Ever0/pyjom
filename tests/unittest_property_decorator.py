a36c15537ebc4740a5002e8496f6478fc094fa6b
GitJournal <app@gitjournal.io>
2022-10-16 18:34:27 +0800

    [2022-10-16 10:34:23]
    FILE:
    tests/unittest_property_decorator.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_property_decorator.py
+++ tests/unittest_property_decorator.py
@@ -1,12 +1,22 @@
-
 # a dynamic property in set
+
 
 class Obj:
     def __init__(self):
-    
+        self.val = 0
+
     @property
     def prop(self):
-        return
+        self.val += 1
+        return self.val
+
+
 obj = Obj()
-mproperty = obj.prop
-myData = [{"a":mproperty}]*2
+# mproperty = obj.prop
+myData = [{"a": lambda: obj.prop}] * 2
+
+for d in myData:
+    val = d["a"]()
+    print(val)
+# for _ in range(3):
+#     print(obj.prop) # strange.
