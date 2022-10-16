632e90c5c7473488b33b41dd317b222a4c0c9147
GitJournal <app@gitjournal.io>
2022-10-16 18:22:19 +0800

    [2022-10-16 10:22:15]
    FILE:
    tests/test_auto_dog_video_giphy_online_producer.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/test_auto_dog_video_giphy_online_producer.py
+++ tests/test_auto_dog_video_giphy_online_producer.py
@@ -21,7 +21,7 @@ dog_or_cat = "dog"
 # we need preconfigured things.
 bgmCacheSetName = 'bilibili_cached_bgm_set'
 postMetadataGenerator = getBilibiliPostMetadataForDogCat(
-    dog_or_cat=dog_or_cat, bgmCacheSetName = bgmCacheSetName
+    dog_or_cat=dog_or_cat, bgmCacheSetName = bgmCacheSetName, bgmCacheAutoPurge=True,
 )  # metadata you can fetch from database, maybe you can preprocess this.
 
 metaTopics = {
