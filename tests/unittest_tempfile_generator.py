fc4d28f55d8c4296ed9681c0b4de39be3bffee3e
GitJournal <app@gitjournal.io>
2022-10-17 18:38:20 +0800

    [2022-10-17 10:38:12]
    FILE:
    tests/unittest_tempfile_generator.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_tempfile_generator.py
+++ tests/unittest_tempfile_generator.py
@@ -1,5 +1,19 @@
 import tempfile
 
 def generateFile():
+    data = b"abc"
     while True:
-        with tempfile(suffix=".")
+        with tempfile.NamedTemporaryFile('wb',suffix=".data") as f:
+            name = f.name
+            print('tempfile name:',name)
+            f.write(data)
+            # what the fuck?
+            # f.close()
+            yield name
+
+if __name__ == '__main__':
+    grt = generateFile()
+    filepath = grt.__next__()
+    with open(filepath, 'rb') as f:
+        content = f.read()
+        print('content in {}:'.format(filepath), content)
