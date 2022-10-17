9740ae6609fb01ff7879bdbf86b7f76c1c383dd1
GitJournal <app@gitjournal.io>
2022-10-17 22:19:09 +0800

    [2022-10-17 14:19:02]
    FILE:
    tests/bilibili_video_recommendation_server/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/test.py
+++ tests/bilibili_video_recommendation_server/test.py
@@ -13,16 +13,24 @@ from bilibili_api import sync, search, user
 
 # also make a decorator for refreshing status, add it to every function.
 # thie refresher is scheduled.
-# you may want to run this
+# you may want to run this beforehand...
 
 import schedule
-
-def refresh_status(func)
 
 def getUserObject(dedeuserid:int=):
     credential = 
     u = user.User(dedeuserid, credential=credential)
+def refresh_status():
 
+refresh_status()
+schedule.every(20).minutes.do(refresh_status)
+def refresh_status_decorator(func):
+    def wrapper(*args, **kwargs):
+        schedule.run_pending()
+        return func(*args, **kwargs)
+    return wrapper
+
+@refresh_status_decorator
 def searchVideos(query):
     # search the thing directly? or you distill keywords from it?
     # or you use some baidu magic?
@@ -34,8 +42,11 @@ def searchVideos(query):
 from pyjom.platforms.bilibili.credentials import bilibiliCredential
 # you need my credential!
 # better reuse the code.
+@refresh_status_decorator
 def searchMyVideos():
     # better use semantic search. but now we use hybrid search instead.
     # hybrid search: metatopic plus bm25
 
+
+# no need to decorate this thing. only put some 'unchecked' video into array.
 def submitMyVideo(): # this is the video i just post. must be regularly checked then add to candidate list. you can check it when another call for my videos has been issued.
