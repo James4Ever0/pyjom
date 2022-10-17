c2434d3d7f07586983e3cef59bbd89ee74a725c0
GitJournal <app@gitjournal.io>
2022-10-17 23:28:34 +0800

    [2022-10-17 15:28:30]
    FILE:
    tests/bilibili_video_recommendation_server/upload_img.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/upload_img.py
+++ tests/bilibili_video_recommendation_server/upload_img.py
@@ -1,1 +1,6 @@
 url = "https://om.qq.com/image/orginalupload"
+
+"""
+add something like this in your website
+<img src='http://inews.gtimg.com/newsapp_ls/0/14966062446/0' width="200"  referrerpolicy="no-referrer" />
+"""
