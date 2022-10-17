b8ecd3b5b0a368313607c52e6becc284cae59178
GitJournal <app@gitjournal.io>
2022-10-17 20:17:57 +0800

    [2022-10-17 12:17:55]
    FILE:
    tests/bilibili_video_recommendation_server/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/test.py
+++ tests/bilibili_video_recommendation_server/test.py
@@ -1,8 +1,12 @@
 # serve my video, serve my cat video, dog video, set priority, serve others video
 # by means of query? or just directly ask me for it.
 
+# you'd better mimic the video that you have never recommend, and these audience have never seen before.
+
 def searchVideos():
 
 def searchMyVideos():
+    # better use semantic search. but now we use hybrid search instead.
+    # hybrid search: metatopic plus bm25
 
 def submitMyVideo(): # this is the video i just post. must be regularly checked then add to candidate list. you can check it when another call for my videos has been issued.
