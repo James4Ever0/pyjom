98db448e3229d3d75bb31ca05c1d827d5e8cc97a
GitJournal <app@gitjournal.io>
2022-10-18 03:46:45 +0800

    [2022-10-17 19:46:43]
    FILE:
    tests/bilibili_video_recommendation_server/test_upload_get_link.sh
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/bilibili_video_recommendation_server/robopost/
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/bilibili_video_recommendation_server/robopost/
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/test_upload_get_link.sh
+++ tests/bilibili_video_recommendation_server/test_upload_get_link.sh
@@ -1,3 +1,5 @@
 cd free-img
+
 PICTURE=/root/Desktop/works/pyjom/tests/bilibili_video_recommendation_server/test_cover.jpg
-python3 cli/free-img.py -s gtimg -i $PICTURE 
+
+python3 cli/free-img.py -s gtimg -i $PICTURE
