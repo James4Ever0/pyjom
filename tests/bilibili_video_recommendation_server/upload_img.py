a3b87f25bf803d2afa1dcee192caa6877e1b7265
GitJournal <app@gitjournal.io>
2022-10-17 23:31:36 +0800

    [2022-10-17 15:31:32]
    FILE:
    tests/bilibili_video_recommendation_server/upload_img.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/upload_img.py
+++ tests/bilibili_video_recommendation_server/upload_img.py
@@ -1,4 +1,6 @@
 url = "https://om.qq.com/image/orginalupload"
+# php tencent: http://www.ygbks.com/2501.html gtimg
+# python sina: https://www.52pojie.cn/thread-1446200-1-1.html
 
 """
 add something like this in your website
