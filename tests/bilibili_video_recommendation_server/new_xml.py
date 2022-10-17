af0757ef96bc232ed02b5a20003f948b90cec121
GitJournal <app@gitjournal.io>
2022-10-18 01:42:15 +0800

    [2022-10-17 17:42:11]
    FILE:
    tests/bilibili_video_recommendation_server/new_xml.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

---
+++ tests/bilibili_video_recommendation_server/new_xml.py
@@ -0,0 +1,33 @@
+source=qq\u0026amp;bbid=XY1BB721B1F97348DBDE
+6da03739849b4655                             │                                            │4297FE1B4ABE26BAA\u0026amp;ts=1666023406285\
+2022/10/18 01:38:59.669 [M]  POST /v1/LuaApiC│(base) root@alpharetta:~/Desktop/works/pyjom│" icon=\"http://miniapp.gtimg.cn/public/appi
+aller?funcname=GetGroupList&qq=1281727431&tim│#  conda deactivate                         │con/432b76be3a548fc128acaa6c1ec90131_200.jpg
+eout=20     2.922446ms                       │root@alpharetta:~/Desktop/root@alpharetta:~/│\" name=\"哔哩哔哩\" appid=\"0\" action=\"we
+2022/10/18 01:38:59.703 [A]  New Client Conne│Desktop/works/pyjom#  cd tasks/qq/qq_red_pac│b\" actionData=\"\" a_actionData=\"tencent0:
+cted Id YZgTX30Wne1DcAH-If6m                 │ket_collect                                 │//\" i_actionData=\"\"/\u003e\u003c/msg\u003
+2022/10/18 01:38:59.704 [M]  GET  /socket.io/│root@alpharetta:~/Desktop/works/pyjom/tasks/│e"} seq 13092 time 1666028442  MsgRandom 920
+?transport=websocket&EIO=3&t=1666028339.67839│qq/qq_red_packet_collect#  python3 botoy_red│063093
+
+
+
+
+
+
+
+2022/10/18 01:38:53.108 [M]  Channel 62f6f72d│root@alpharetta ~/D/w/pyjom (main)#  bash   │85\" serviceID=\"1\" action=\"web\" [11/156$
+6da03739849b4655                             │                                            │ta=\"\" a_actionData=\"\" i_actionData=\"\" 
+2022/10/18 01:38:59.669 [M]  POST /v1/LuaApiC│(base) root@alpharetta:~/Desktop/works/pyjom│brief=\"[QQ小程序]哔哩哔哩\" flag=\"0\"\u003
+aller?funcname=GetGroupList&qq=1281727431&tim│#  conda deactivate                         │e\u003citem layout=\"2\"\u003e\u003cpicture 
+eout=20     2.922446ms                       │root@alpharetta:~/Desktop/root@alpharetta:~/│cover=\"http://pubminishare-30161.picsz.qpic
+2022/10/18 01:38:59.703 [A]  New Client Conne│Desktop/works/pyjom#  cd tasks/qq/qq_red_pac│.cn/c099bdd6-9e61-43d9-b82f-c9d5354ace68\"/\
+cted Id YZgTX30Wne1DcAH-If6m                 │ket_collect                                 │u003e\u003ctitle\u003e哔哩哔哩\u003c/title\u
+2022/10/18 01:38:59.704 [M]  GET  /socket.io/│root@alpharetta:~/Desktop/works/pyjom/tasks/│003e\u003csummary\u003e【AI动画】妮露PV动画 
+?transport=websocket&EIO=3&t=1666028339.67839│qq/qq_red_packet_collect#  python3 botoy_red│风转换【NovelAI】\u003c/summary\u003e\u003c/
+98  23.177928ms                              │packet_collect.py 1>/dev/null 2> >(tee -a lo│item\u003e\u003csource url=\"https://b23.tv/
+                                             │gs/redPacketLog_0.log >&2)                  │uHML5mi?share_medium=android\u0026amp;share_
+^[[A                                         │^B^[[C^B^[[D                                │source=qq\u0026amp;bbid=XY1BB721B1F97348DBDE
+
+
+
+
+
