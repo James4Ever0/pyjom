d518d3f6de93ec7c0b9847ac80bf2258fefae249
GitJournal <app@gitjournal.io>
2022-10-19 00:29:17 +0800

    [2022-10-18 16:29:15]
    FILE:
    tests/bilibili_video_recommendation_server/get_bilibili_shortlink.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/get_bilibili_shortlink.py
+++ tests/bilibili_video_recommendation_server/get_bilibili_shortlink.py
@@ -6,15 +6,27 @@ url = "https://api.bilibili.com/x/share/click"
 # the damn picture?
 # the damn picture is generated. it needs to be uploaded to tencent.
 
-apiUrl ="https://service-ijd4slqi-1253419200.gz.apigw.tencentcs.com/release/short_url"
+apiUrl = "https://service-ijd4slqi-1253419200.gz.apigw.tencentcs.com/release/short_url"
 longUrl = "https://www.bilibili.com/video/BV1Wv41157Wz"
-params= {"url":longUrl,"href":"https://xiaojuzi.fun/bili-short-url/"}
+import urllib.parse as urlparse
+params = {"url": longUrl, "href": "https://xiaojuzi.fun/bili-short-url/"}
 
-
-
+headers = {
+    "accept": "*/*",
+    "accept-language": "en-US,en;q=0.9",
+    "if-none-match": 'W/"35-oPDNsqBGaZKqGe83GW6wem+lkww"',
+    "sec-ch-ua": '"Google Chrome";v="105", "Not)A;Brand";v="8", "Chromium";v="105"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": '"macOS"',
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "cross-site",
+    "Referer": "https://xiaojuzi.fun/",
+    "Referrer-Policy": "strict-origin-when-cross-origin",
+}
 
 import requests
 
-r = requests.get(apiUrl, params=params)
+r = requests.get(apiUrl, params=params, headers=headers)
 if r.status_code == 200:
     print(r.json())
