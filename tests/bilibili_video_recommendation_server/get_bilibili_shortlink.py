1816e394c58213bc99191878703bf720df48f21f
GitJournal <app@gitjournal.io>
2022-10-19 00:23:17 +0800

    [2022-10-18 16:23:15]
    FILE:
    tests/bilibili_video_recommendation_server/get_bilibili_shortlink.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/bilibili_video_recommendation_server/xml_and_json_qq_send/
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/get_bilibili_shortlink.py
+++ tests/bilibili_video_recommendation_server/get_bilibili_shortlink.py
@@ -5,3 +5,11 @@ url = "https://api.bilibili.com/x/share/click"
 # use post.
 # the damn picture?
 # the damn picture is generated. it needs to be uploaded to tencent.
+
+apiUrl ="https://service-ijd4slqi-1253419200.gz.apigw.tencentcs.com/release/short_url"
+longUrl = "https://www.bilibili.com/video/BV1Wv41157Wz"
+params= {"url":longUrl,"href":"https://xiaojuzi.fun/bili-short-url/"}
+
+import requests
+
+r = requests.get(apiUrl, )
