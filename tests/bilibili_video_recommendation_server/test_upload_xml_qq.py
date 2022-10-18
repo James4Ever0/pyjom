1d449e34d077d1f330628647f09a59b0ef794217
GitJournal <app@gitjournal.io>
2022-10-18 12:48:09 +0800

    [2022-10-18 04:48:06]
    FILE:
    tests/bilibili_video_recommendation_server/test_upload_xml_qq.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/test_upload_xml_qq.py
+++ tests/bilibili_video_recommendation_server/test_upload_xml_qq.py
@@ -2,14 +2,16 @@ from botoy import Action
 
 qq = 917521610
 port = 8784
-action = Action(qq=qq, port=port, host='127.0.0.1')
-user=1281727431
+action = Action(qq=qq, port=port, host="127.0.0.1")
+user = 1281727431
 # but the goddamn xml format is not right.
 # cover = "http://inews.gtimg.com/newsapp_bt/0/15352117085/640"
 cover = "http://pubminishare-30161.picsz.qpic.cn/d4ad36fa-833e-4018-b994-a2da810f2d54"
 # just need a better cover. i don't know.
 # cover = "https://i0.hdslb.com/bfs/archive/c5a0d18ee077fb6a4ac0970ccb0a3788e137d14f.jpg" # works.
-content = """<?xml version='1.0' encoding='UTF-8' standalone='yes'?><msg templateID="123" url="https://b23.tv/5K7qh7K?share_medium=android&amp;share_source=qq&amp;bbid=XY46C7C4C74C8D645671EF7E8F4CC7810054A&amp;ts=1657521142233" serviceID="1" action="web" actionData="" a_actionData="" i_actionData="" brief="[QQ小程序]哔哩哔哩" flag="0"><item layout="2"><picture cover="{}"/><title>哔哩哔哩</title><summary>【C语言】《带你学C带你飞》</summary></item><source url="https://b23.tv/5K7qh7K?share_medium=android&amp;share_source=qq&amp;bbid=XY46C7C4C74C8D645671EF7E8F4CC7810054A&amp;ts=1657521142233" icon="https://open.gtimg.cn/open/app_icon/00/95/17/76/100951776_100_m.png?t=1657091104" name="哔哩哔哩" appid="0" action="web" actionData="" a_actionData="tencent0://" i_actionData=""/></msg>""".format(cover)
+content = """<?xml version='1.0' encoding='UTF-8' standalone='yes'?><msg templateID="123" url="https://b23.tv/5K7qh7K?share_medium=android&amp;share_source=qq&amp;bbid=XY46C7C4C74C8D645671EF7E8F4CC7810054A&amp;ts=1657521142233" serviceID="1" action="web" actionData="" a_actionData="" i_actionData="" brief="[QQ小程序]哔哩哔哩" flag="0"><item layout="2"><picture cover="{}"/><title>哔哩哔哩</title><summary>【C语言】《带你学C带你飞》</summary></item><source url="https://b23.tv/5K7qh7K?share_medium=android&amp;share_source=qq&amp;bbid=XY46C7C4C74C8D645671EF7E8F4CC7810054A&amp;ts=1657521142233" icon="https://open.gtimg.cn/open/app_icon/00/95/17/76/100951776_100_m.png?t=1657091104" name="哔哩哔哩" appid="0" action="web" actionData="" a_actionData="tencent0://" i_actionData=""/></msg>""".format(
+    cover
+)
 # content = """<?xml version="1.0" encoding="UTF-8" standalone="yes"?><msg templateID="123" url="https://b23.tv/uHML5mi?share_medium=android&amp;share_source=qq&amp;bbid=XY1BB721B1F97348DBDE4297FE1B4ABE26BAA&amp;ts=1666023406285" serviceID="1" action="web" actionData="" a_actionData="" i_actionData="" brief="[QQ小程序]哔哩哔哩" flag="0"><item layout="2"><picture cover="{}"/><title>【AI动画】妮露PV动画 风转换【NovelAI】</title></item><source url="https://b23.tv/uHML5mi?share_medium=android&amp;share_source=qq&amp;bbid=XY1BB721B1F97348DBDE4297FE1B4ABE26BAA&amp;ts=1666023406285" icon="http://miniapp.gtimg.cn/public/appicon/432b76be3a548fc128acaa6c1ec90131_200.jpg" name="哔哩哔哩" appid="0" action="app" actionData="" a_actionData="tencent0://" i_actionData=""/></msg>""".format(cover)
 # i guess, it is just the way it send the data is different.
 # result= action.sendFriendXml(user=user, content=content)
@@ -23,6 +25,18 @@ group = 543780931
 # result = action.sendGroupText(group=group,content='test')
 # result = action.sendGroupText(group=group,content=content)
 funcname = "SendMsgV2"
-payload={"ToUserUid":group,"SendToType":2,"SendMsgType":"XmlMsg","Content":content}
-result = action.baseRequest(method="POST",funcname=funcname,path="/v1/LuaApiCaller",params={'qq':qq, 'funcname':funcname}, payload=payload)
+content = """<?xml version='1.0' encoding='UTF-8' standalone='yes' ?><msg serviceID='1' templateID='1' action='' brief='&#91;群僵尸任务&#93;' sourceMsgId='0' url='' flag='2' adverSign='0' multiMsgFlag='0'><item layout='0'><title size='38' color='#9900CC' style='1'>🆕已经启动🆕</title></item><item layout='0'><hr hidden='false' style='0' /></item><item layout='6'><summary color='#FF0033'>1⃣️</summary><summary color='#FF0099'>💪正在扫秒僵尸💪</summary></item><source name='' icon='' action='' appid='-1' /></msg>"""
+payload = {
+    "ToUserUid": group,
+    "SendToType": 2,
+    "SendMsgType": "XmlMsg",
+    "Content": content,
+}
+result = action.baseRequest(
+    method="POST",
+    funcname=funcname,
+    path="/v1/LuaApiCaller",
+    params={"qq": qq, "funcname": funcname},
+    payload=payload,
+)
 print(result)
