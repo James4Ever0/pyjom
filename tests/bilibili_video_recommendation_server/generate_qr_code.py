8cd26a60c358a33a181eb587fc5521079a7fbcda
GitJournal <app@gitjournal.io>
2022-10-19 01:39:02 +0800

    [2022-10-18 17:38:56]
    FILE:
    tests/bilibili_video_recommendation_server/generate_qr_code.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/generate_qr_code.py
+++ tests/bilibili_video_recommendation_server/generate_qr_code.py
@@ -1,1 +1,10 @@
-url=""
+data="https://b23.tv/DPn1G4p"
+# Importing library
+import qrcode
+
+# Encoding data using make() function
+img = qrcode.make(data)
+print("image type:", type(img))
+# <class 'qrcode.image.pil.PilImage'>
+# Saving as an image file
+img.save('MyQRCode1.png')
