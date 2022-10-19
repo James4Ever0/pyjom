364e48ffd7b5f6e6ae43218d422e2225d663052c
GitJournal <app@gitjournal.io>
2022-10-20 00:33:08 +0800

    [2022-10-19 16:33:00]
    FILE:
    tests/dapp_ethereum_python_crypto/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/dapp_ethereum_python_crypto/test.py
+++ tests/dapp_ethereum_python_crypto/test.py
@@ -1,13 +1,13 @@
 from web3 import Web3
 
 # https://hackernoon.com/hands-on-creating-your-own-local-private-geth-node-beginner-friendly-3d45902cc612
-link="/root/.ethereum/geth.ipc"
+link = "/root/.ethereum/geth.ipc"
 
 web3 = Web3(Web3.IPCProvider(link))
 
 print(web3.isConnected())
 
-account_genesis = "0xde478bde26d711414fae26133e759d8a82a202ab" # aka: eth.coinbase
+account_genesis = "0xde478bde26d711414fae26133e759d8a82a202ab"  # aka: eth.coinbase
 password_genesis = "abcdefg"
 # let's see!
 
@@ -20,8 +20,14 @@ target_account = "0x033799af9b29e1d7dbf3c8dd64647df345f67bf1"
 b = web3.eth.get_balance(web3.eth.coinbase)
 print(b)
 # proof of authority, puppeth
-web3.eth.send_transaction(web3.eth.coinbase, target_account)
+web3.eth.send_transaction(
+    {
+        "to": target_account,
+        "from": web3.eth.coinbase,
+        "value": 12345,
+    }
+)
 
 
-b = web3.eth.get_balance(target_account))
+b = web3.eth.get_balance(target_account)
 print(b)
