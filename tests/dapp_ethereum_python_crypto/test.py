7374b88b187fc6f1cd733f23d8ecd18fbfbf40ed
GitJournal <app@gitjournal.io>
2022-10-19 23:59:49 +0800

    [2022-10-19 15:59:42]
    FILE:
    tests/dapp_ethereum_python_crypto/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/dapp_ethereum_python_crypto/test.py
+++ tests/dapp_ethereum_python_crypto/test.py
@@ -1,12 +1,13 @@
 from web3 import Web3
 
+# https://hackernoon.com/hands-on-creating-your-own-local-private-geth-node-beginner-friendly-3d45902cc612
 link="/root/.ethereum/geth.ipc"
 
 web3 = Web3(Web3.IPCProvider(link))
 
 print(web3.isConnected())
 
-account_genesis = "0xde478bde26d711414fae26133e759d8a82a202ab"
+account_genesis = "0xde478bde26d711414fae26133e759d8a82a202ab" # aka: eth.coinbase
 password_genesis = "abcdefg"
 # let's see!
 
