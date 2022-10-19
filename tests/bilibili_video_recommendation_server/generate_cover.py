05cd7404243bd621b0d729033bcbd8f3e2c60c4b
GitJournal <app@gitjournal.io>
2022-10-19 14:48:36 +0800

    [2022-10-19 06:48:34]
    FILE:
    tests/bilibili_video_recommendation_server/generate_cover.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/generate_cover.py
+++ tests/bilibili_video_recommendation_server/generate_cover.py
@@ -2,8 +2,24 @@
 pic_file = "sample_cover.jpg"
 qrcode_file = "MyQRCode1.png"
 # we need some font for this.
-font_location = "./truetype/wqy/wqy-zenhei.ttc
-microhei.ttc
-root@alpharetta /u/s/fonts# pwd
-/usr/share/fonts"
-import 
+font_location = "/usr/share/fonts/truetype/wqy/wqy-microhei.ttc"
+
+import pixie
+
+font = pixie.read_font(font_location)
+font.size=20
+
+text = "Typesetting is the arrangement and composition of text in graphic design and publishing in both digital and traditional medias."
+image = pixie.Image(200, 200)
+image.fill(pixie.Color(1, 1, 1, 1))
+
+image.fill_text(
+    font,
+    text,
+    bounds = pixie.Vector2(180, 180),
+    transform = pixie.translate(10, 10)
+)
+
+print('image type:', type(image))
+
+image.write_file("text.png")
