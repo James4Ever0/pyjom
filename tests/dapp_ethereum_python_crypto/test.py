56405ac31e10eb6390833ea453fef5554a52c0d1
GitJournal <app@gitjournal.io>
2022-10-20 01:42:43 +0800

    [2022-10-19 17:42:35]
    FILE:
    tests/dapp_ethereum_python_crypto/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/dapp_ethereum_python_crypto/test.py
+++ tests/dapp_ethereum_python_crypto/test.py
@@ -8,12 +8,14 @@ web3 = Web3(Web3.IPCProvider(link))
 print(web3.isConnected())
 
 # account_genesis = "0xde478bde26d711414fae26133e759d8a82a202ab"  # aka: eth.coinbase
-account_genesis = "0x6fe20a7157fdb705278fffda4ea0ebf4694f31ea"
+# account_genesis = "0x6fe20a7157fdb705278fffda4ea0ebf4694f31ea"
+account_genesis = "0xd6e79c8d5b7d41cc1a3b98373c98618ea267852f"
 account_genesis = Web3.toChecksumAddress(account_genesis)
 password_genesis = "abcdefg"
 # let's see!
 
-target_account = "0x033799af9b29e1d7dbf3c8dd64647df345f67bf1"
+# target_account = "0x033799af9b29e1d7dbf3c8dd64647df345f67bf1"
+target_account = "
 target_account = Web3.toChecksumAddress(target_account)
 # you was connected ethereum to mainnet! not good.
 
