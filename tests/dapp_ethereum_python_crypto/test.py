32738df79ed19114cb292066c5c22c012ed176d6
GitJournal <app@gitjournal.io>
2022-10-20 00:39:13 +0800

    [2022-10-19 16:39:05]
    FILE:
    tests/dapp_ethereum_python_crypto/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/dapp_ethereum_python_crypto/test.py
+++ tests/dapp_ethereum_python_crypto/test.py
@@ -8,11 +8,12 @@ web3 = Web3(Web3.IPCProvider(link))
 print(web3.isConnected())
 
 account_genesis = "0xde478bde26d711414fae26133e759d8a82a202ab"  # aka: eth.coinbase
+account_genesis = Web3.toChecksumAddress(account_genesis)
 password_genesis = "abcdefg"
 # let's see!
 
 target_account = "0x033799af9b29e1d7dbf3c8dd64647df345f67bf1"
-
+target_account = Web3.toChecksumAddress(target_account)
 # you was connected ethereum to mainnet! not good.
 
 # anyway, we need money!
@@ -22,15 +23,19 @@ print(b)
 # proof of authority, puppeth
 
 ## need password!
-web3.eth.
+web3.geth.personal.unlock_account(web3.eth.coinbase, password_genesis)
 web3.eth.send_transaction(
     {
-        "to": Web3.toChecksumAddress(target_account),
+        "to": target_account,
         "from": web3.eth.coinbase,
-        "value": 12345,
+        "value": 1,
     }
 )
+web3.geth.personal.lock_account(web3.eth.coinbase)
+# you can choose to use 'with' statement.
 
 
 b = web3.eth.get_balance(target_account)
 print(b)
+
+# still no money! fuck.
