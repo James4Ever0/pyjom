4459f8edf1fae10393e179f333fcd6b1e1ec372c
GitJournal <app@gitjournal.io>
2022-10-20 02:30:57 +0800

    [2022-10-19 18:30:55]
    FILE:
    tests/dapp_ethereum_python_crypto/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/dapp_ethereum_python_crypto/test.py
+++ tests/dapp_ethereum_python_crypto/test.py
@@ -3,7 +3,7 @@ from web3 import Web3
 # mine only when pending transaction happens:
 # https://ethereum.stackexchange.com/questions/3151/how-to-make-miner-to-mine-only-when-there-are-pending-transactions
 
-# maybe you want money even if without transaction
+# maybe you want money even if without transaction, or low in cash.
 
 # https://hackernoon.com/hands-on-creating-your-own-local-private-geth-node-beginner-friendly-3d45902cc612
 link = "/root/.ethereum/geth.ipc"
