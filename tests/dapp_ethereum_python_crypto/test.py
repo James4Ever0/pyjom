72222d0119b6a0289ac26b1e2ae677a632add526
GitJournal <app@gitjournal.io>
2022-10-19 22:50:03 +0800

    [2022-10-19 14:49:56]
    FILE:
    tests/dapp_ethereum_python_crypto/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/dapp_ethereum_python_crypto/test.py
+++ tests/dapp_ethereum_python_crypto/test.py
@@ -5,3 +5,7 @@ link="/root/.ethereum/geth.ipc"
 web3 = Web3(Web3.IPCProvider(link))
 
 print(web3.isConnected())
+
+account = "0x90316d605bbe3e8caa2bb90f5d63a35dbfada842"
+
+# anyway, we need money!
