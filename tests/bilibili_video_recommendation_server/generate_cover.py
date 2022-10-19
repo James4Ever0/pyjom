6ccefef58821d11da2fc6c3c3e02086d23920acd
GitJournal <app@gitjournal.io>
2022-10-19 14:45:34 +0800

    [2022-10-19 06:45:32]
    FILE:
    tests/bilibili_video_recommendation_server/generate_cover.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/generate_cover.py
+++ tests/bilibili_video_recommendation_server/generate_cover.py
@@ -1,4 +1,9 @@
 # let's try to make it right.
 pic_file = "sample_cover.jpg"
 qrcode_file = "MyQRCode1.png"
+# we need some font for this.
+font_location = "./truetype/wqy/wqy-zenhei.ttc
+microhei.ttc
+root@alpharetta /u/s/fonts# pwd
+/usr/share/fonts"
 import 
