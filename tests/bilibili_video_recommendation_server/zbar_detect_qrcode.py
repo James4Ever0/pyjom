6ee2041abbd1c0a3309fa3932646ca4d1d2047de
GitJournal <app@gitjournal.io>
2022-10-20 11:21:08 +0800

    [2022-10-20 03:21:01]
    FILE:
    tests/bilibili_video_recommendation_server/zbar_detect_qrcode.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

---
+++ tests/bilibili_video_recommendation_server/zbar_detect_qrcode.py
@@ -0,0 +1,31 @@
+
+# import sys
+import cv2
+# import imutils
+from PIL import Image
+from pyzbar.pyzbar import decode, ZBarSymbol
+
+# @function 'detect_qr' detect and decode qrcode from frame using pyzbar lib
+# @param 'inputFrame' type <class 'numpy.ndarray'>
+# @return if detected type 'bool'
+def detect_qr(inputFrame):
+    img = Image.fromarray(inputFrame)
+    decodedImg = decode(img, symbols=[ZBarSymbol.QRCODE])
+    # it reads the content. but where is the code?
+    print(len(decodedImg))
+    # length: 2
+
+    if len(decodedImg) > 0:
+        decodedBytes = decodedImg[0].data
+        stringData = decodedBytes.decode("utf-8")
+        print("QRCode content:")
+        print(stringData)
+
+        return True
+    else:
+        return False
+image = "output_qrcode2.png"
+
+inputImage = cv2.imread(image)
+# frame = imutils.resize(inputImage, width=400)
+print(detect_qr(inputImage))
