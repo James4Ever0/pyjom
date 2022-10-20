77a4ada312d651c5bb7fe142888d6824e66fc460
GitJournal <app@gitjournal.io>
2022-10-20 21:54:44 +0800

    [2022-10-20 13:54:36]
    FILE:
    tests/bilibili_video_recommendation_server/send_image_botoy.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/send_image_botoy.py
+++ tests/bilibili_video_recommendation_server/send_image_botoy.py
@@ -1,2 +1,10 @@
 # test to broadcast all these things.
 
+from botoy import Action
+
+qq = 917521610
+port = 8784
+action = Action(qq=qq, port=port, host="127.0.0.1")
+user = 1281727431
+
+action.sendGroupPic(group=)
