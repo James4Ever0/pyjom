4cb33f75c23824ada8c9d2d40ea3c3df031a80a3
GitJournal <app@gitjournal.io>
2022-10-21 00:01:07 +0800

    [2022-10-20 16:00:59]
    FILE:
    tests/bilibili_video_recommendation_server/get_masked_gif.sh
    tests/qq_opqbot/opq_arm64/Logs/OPQBot.2022-10-21.001.log
    tests/qq_opqbot/opq_arm64_another_account/Logs/OPQBot.2022-10-21.001.log
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/get_masked_gif.sh
+++ tests/bilibili_video_recommendation_server/get_masked_gif.sh
@@ -1,3 +1,4 @@
 WIDTH=936
 HEIGHT=598
-ffmpeg -y -i anime.gif -loop 1 -t 1 -i ad_2_mask.png -filter_complex "[0:v]scale=$WIDTH:$HEIGHT;[1:v]alphaextract[m];[v0][1:v]alphamerge‌​"  anime_masked.gif
+
+ffmpeg -y -i anime.gif -loop 1 -t 1 -i ad_2_mask.png -filter_complex "[0:v]scale=$WIDTH:$HEIGHT[v0];[1:v]alphaextract[m];[v0][m]alphamerge‌​"  anime_masked.gif
