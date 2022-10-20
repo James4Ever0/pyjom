63f29728395dd48319fa7a856856397e276d1b50
GitJournal <app@gitjournal.io>
2022-10-20 17:37:01 +0800

    [2022-10-20 09:36:53]
    FILE:
    tests/bilibili_video_recommendation_server/ad_template_2.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/ad_template_2.py
+++ tests/bilibili_video_recommendation_server/ad_template_2.py
@@ -1,3 +1,11 @@
 import pixie
 
 ad_width, ad_height = 1000,1000
+
+font_path = "./wqy-microhei0.ttf"
+cover_path = "sample_cover.jpg"
+qrcode_path = "MyQRCode1.png"
+play_button_path = "play_button.png"
+bilibili_logo_path = 
+
+image = 
