52d7041b5a1c080efa18822789d62e6019a8e0b3
GitJournal <app@gitjournal.io>
2022-10-20 17:40:03 +0800

    [2022-10-20 09:39:54]
    FILE:
    tests/bilibili_video_recommendation_server/ad_template_2.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/ad_template_2.py
+++ tests/bilibili_video_recommendation_server/ad_template_2.py
@@ -1,11 +1,17 @@
 import pixie
-
+from lazero.utils.importers import cv2_custom_build_init
+cv2_custom_build_init()
+import cv2
+def getImageW2H(image_path):
+    height, width, _ = cv2.imread(image_path)
+    w2h = width/height
+    return w2h
 ad_width, ad_height = 1000,1000
-
 font_path = "./wqy-microhei0.ttf"
 cover_path = "sample_cover.jpg"
 qrcode_path = "MyQRCode1.png"
 play_button_path = "play_button.png"
-bilibili_logo_path = 
+bilibili_logo_path = "bilibili.png"
 
-image = 
+
+image = pixie.Image(ad_width, ad_height)
