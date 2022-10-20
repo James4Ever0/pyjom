4c873d4c3500e2cbe43a8344fd201d813824bf89
GitJournal <app@gitjournal.io>
2022-10-20 10:44:47 +0800

    [2022-10-20 02:44:41]
    FILE:
    tests/bilibili_video_recommendation_server/detect_qr_code.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/detect_qr_code.py
+++ tests/bilibili_video_recommendation_server/detect_qr_code.py
@@ -1,6 +1,7 @@
 image = "test_image_with_qr_code.png"
 
 from lazero.utils.importers import cv2_custom_build_init
+
 cv2_custom_build_init()
 import cv2
 
@@ -11,12 +12,20 @@ data, bbox, _ = detector.detectAndDecode(img)
 
 if bbox is not None:
     # display the image with lines
+    print(bbox)
+    breakpoint()
     for i in range(len(bbox)):
         # draw all lines
-        cv2.line(img, tuple(bbox[i][0]), tuple(bbox[(i+1) % len(bbox)][0]), color=(255, 0, 0), thickness=2)
+        cv2.line(
+            img,
+            tuple(bbox[i][0]),
+            tuple(bbox[(i + 1) % len(bbox)][0]),
+            color=(255, 0, 0),
+            thickness=2,
+        )
     if data:
         print("[+] QR Code detected, data:", data)
 # display the result
-cv2.imshow("img", img)  
+cv2.imshow("img", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
