598d848d727a00fe38ab08cad0e92ba111fd6aa9
GitJournal <app@gitjournal.io>
2022-10-20 22:03:51 +0800

    [2022-10-20 14:03:43]
    FILE:
    tests/bilibili_video_recommendation_server/send_image_botoy.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/send_image_botoy.py
+++ tests/bilibili_video_recommendation_server/send_image_botoy.py
@@ -7,7 +7,13 @@ port = 8784
 action = Action(qq=qq, port=port, host="127.0.0.1")
 user = 1281727431
 
-
+link = "https://b23.tv/DPn1G4p"
 title_text = "真·朋克！揭秘《赛博朋克2077》屏幕之外的魔幻换弹操作"
-content = "{} 观看视频:{}".format(link)
-action.sendFriendPic(user=user, picUrl="",content = content)
+content = "{} 观看视频:{}".format(title_text,link)
+
+import base64
+picture_path = "ad_2_standalone_cover.png"
+with open(picture_path, "rb") as img_file:
+    b64_string = base64.b64encode(img_file.read())
+# print(b64_string)
+action.sendFriendPic(user=user, picBase64Buf=b64_string.decode('utf-8'),content = content)
