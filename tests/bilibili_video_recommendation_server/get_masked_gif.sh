ef74c426890e42b3ab47cc46903972a7c81d5a91
GitJournal <app@gitjournal.io>
2022-10-21 00:16:19 +0800

    [2022-10-20 16:16:09]
    FILE:
    tests/bilibili_video_recommendation_server/get_masked_gif.sh
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/get_masked_gif.sh
+++ tests/bilibili_video_recommendation_server/get_masked_gif.sh
@@ -1,4 +1,7 @@
 WIDTH=936
 HEIGHT=598
 
-ffmpeg -i anime.gif -i ad_2_mask.png -filter_complex "[0:v]scale=936:598[v0];[1:v]alphaextract[alpha];[v0][alpha]alphamerge‌[vt]​"  anime_masked.gif
+
+# ffmpeg -i anime.gif -i ad_2_mask.png -filter_complex "[0]scale=936:598[v0]​" anime_masked.gif
+
+ffmpeg -i anime.gif -i ad_2_mask.png -filter_complex "scale=$WIDTH:$HEIGHT"
