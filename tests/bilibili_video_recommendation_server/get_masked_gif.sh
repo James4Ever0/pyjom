be6f3e45831e5d8e98a3a3f01958f486176b34f2
GitJournal <app@gitjournal.io>
2022-10-20 23:52:03 +0800

    [2022-10-20 15:51:55]
    FILE:
    tests/bilibili_video_recommendation_server/get_masked_gif.sh
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/get_masked_gif.sh
+++ tests/bilibili_video_recommendation_server/get_masked_gif.sh
@@ -1,8 +1,3 @@
-ffmpeg -y -i anime.gif -loop 1 -t 1 -i ad_2_mask.png
-  -filter_complex
-      "color=black:d=1[c];[c][0]scale2ref[cs][v];[cs]setsar=1[ct];
-       [1:v]alphaextract,negate[m];[m][ct]scale2ref[ms][ol];[ms]setsar=1[alf];
-       [ol][alf]alphamerge[fin];
-       [v][fin]overlay,scale‌​=$WIDTH:$HEIGHT:force_orig‌​inal_aspect_ratio=de‌​crease[fv];
-       [fv]pad=6‌$WIDTH:$HEIGHT:(ow-iw)/2:(o‌​h-ih)/2:#000000@1[v]‌​"
--map "[v]" -map 0:a output.mp4
+WIDTH=936
+HEIGHT=598
+ffmpeg -y -i anime.gif -loop 1 -t 1 -i ad_2_mask.png -filter_complex "color=black:d=1[c];[c][0]scale2ref[cs][v];[cs]setsar=1[ct];[1:v]alphaextract,negate[m];[m][ct]scale2ref[ms][ol];[ms]setsar=1[alf];[ol][alf]alphamerge[fin];[v][fin]overlay,scale‌​=$WIDTH:$HEIGHT:force_orig‌​inal_aspect_ratio=de‌​crease[fv];[fv]pad=‌$WIDTH:$HEIGHT:(ow-iw)/2:(o‌​h-ih)/2:#000000@1[v]‌​" -map "[v]" anime_masked.gif
