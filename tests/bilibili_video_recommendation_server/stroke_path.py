392e98be21af75be3ff25a9c3eb795ce646d6ae5
GitJournal <app@gitjournal.io>
2022-10-20 17:03:49 +0800

    [2022-10-20 09:03:41]
    FILE:
    tests/bilibili_video_recommendation_server/stroke_path.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/stroke_path.py
+++ tests/bilibili_video_recommendation_server/stroke_path.py
@@ -1,2 +1,14 @@
 import pixie
 
+image = pixie.Image(200,200)
+
+path = pixie.Path()
+path.rounded_rect(20,20,100,100,25,25,25,25)
+paint = pixie.Paint(pixie.SOLID_PAINT)
+paint.color = pixie.Color(0,1,0,1)
+
+image.stroke_path(path, paint=paint, stroke_width=3)
+
+image.write_file('stroke_round_rect.png')
+
+# stroke on a transparent background. well shit.
