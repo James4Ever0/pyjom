605fbb690f5ab5182e7ff2ef287cb49c10a3943f
GitJournal <app@gitjournal.io>
2022-10-20 18:55:33 +0800

    [2022-10-20 10:55:24]
    FILE:
    tests/bilibili_video_recommendation_server/ad_template_2.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/ad_template_2.py
+++ tests/bilibili_video_recommendation_server/ad_template_2.py
@@ -122,8 +122,8 @@ image.fill_text(
 font = pixie.read_font(font_path)
 font.size = int(ad_width*0.06)
 font.paint.color = pixie.Color(0.2,0.8,0.5,1)
-title_text_transform = pixie.translate(int(ad_width-qrcode_width*1.1-font.size*1.2), int(ad_height-qrcode_height*1.1))
-title_text_bounds = pixie.Vector2(qrcode_scan_text_transform_x -font)
+title_text_transform = pixie.translate(int(font.size*1.2), int(ad_height-qrcode_height*1.1))
+title_text_bounds = pixie.Vector2(int(qrcode_scan_text_transform_x -font.size*1.1),int(qrcode_height))
 image.fill_text(
     font,title_text, bounds = title_text_bounds,transform=title_text_transform
 )
