75a981437e0285969cff6f69f002cf3ff8a08a38
GitJournal <app@gitjournal.io>
2022-10-20 17:09:49 +0800

    [2022-10-20 09:09:41]
    FILE:
    tests/bilibili_video_recommendation_server/get_text_boundaries.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/get_text_boundaries.py
+++ tests/bilibili_video_recommendation_server/get_text_boundaries.py
@@ -7,7 +7,7 @@ font_location = "./wqy-microhei0.ttf"
 
 font = pixie.read_font(font_location)
 font.size = 20
-
+font.paint.color = pixie.Color(1,1,1,1)
 
 image.fill_text(
     font, text, bounds=pixie.Vector2(180, 180), transform=pixie.translate(10, 10)
@@ -20,9 +20,13 @@ cv2_custom_build_init()
 import cv2
 
 img = cv2.imread(export_path)
-print(img.shape)
-exit()
-img = cv2.cvtColor(img, cv2.COLOR)
+# print(img.shape) #(200,200,3)
+# exit()
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# print(img_gray)
 
-rects = cv2.boundingRect(img)
+rects = cv2.boundingRect(img_gray)
+# fuck?
 print(rects)
+# (10, 13, 130, 21)
+# x,y,w,h?
