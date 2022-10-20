b3832a334b7ae47523ed47a6974248fa91f30017
GitJournal <app@gitjournal.io>
2022-10-20 23:36:55 +0800

    [2022-10-20 15:36:46]
    FILE:
    tests/bilibili_video_recommendation_server/send_image_gif.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/send_image_gif.py
+++ tests/bilibili_video_recommendation_server/send_image_gif.py
@@ -18,7 +18,7 @@ content = "观看视频:\n{}\n{}".format(link, title_text)
 import base64
 
 # picture_path = "qrcode.gif"
-picture_path = "anime.gif"
+picture_path = "anime_overlay.gif"
 # where is the gif? my god?
 
 # there is no way to scan the code in the gif. better send the link instead.
