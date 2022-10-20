67e9cb97001fd1e963b81fb272dd78a8b86f0a80
GitJournal <app@gitjournal.io>
2022-10-20 10:38:45 +0800

    [2022-10-20 02:38:39]
    FILE:
    tests/bilibili_video_recommendation_server/ad_template_0.py
    tests/bilibili_video_recommendation_server/detect_qr_code.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/ad_template_0.py
+++ tests/bilibili_video_recommendation_server/ad_template_0.py
@@ -8,7 +8,7 @@ font_location = "./wqy-microhei0.ttf"
 import pixie
 
 font = pixie.read_font(font_location)
-font.size=20
+font.size = 20
 
 text = "中文能够显示么 超出了字符边缘能不能显示 Typesetting is the arrangement and composition of text in graphic design and publishing in both digital and traditional medias."
 # 可以显示 但是边缘的字符需要被注意到 看看是不是超出了边界
@@ -17,10 +17,7 @@ image = pixie.Image(200, 200)
 image.fill(pixie.Color(1, 1, 1, 1))
 
 image.fill_text(
-    font,
-    text,
-    bounds = pixie.Vector2(180, 180),
-    transform = pixie.translate(10, 10)
+    font, text, bounds=pixie.Vector2(180, 180), transform=pixie.translate(10, 10)
 )
 
 # print('image type:', type(image))
@@ -28,29 +25,31 @@ image.fill_text(
 # hard to say.
 
 path = pixie.Path()
-path.rounded_rect(0,0, 100, 100, 25, 25, 25, 25)
+path.rounded_rect(0, 0, 100, 100, 25, 25, 25, 25)
 # how to use mask?
-cover_width, cover_height = 100,100
-mask = pixie.Mask(cover_width, cover_height) # must match mask size?
+cover_width, cover_height = 100, 100
+mask = pixie.Mask(cover_width, cover_height)  # must match mask size?
 mask.fill_path(path)
 
 picture = pixie.read_image(pic_file)
 # we need to reshape this.
-picture = picture.resize(cover_width, cover_height) # recommend to do this in pyjom.imagetoolbox since that will be safer.
+picture = picture.resize(
+    cover_width, cover_height
+)  # recommend to do this in pyjom.imagetoolbox since that will be safer.
 picture.mask_draw(mask)
-transform=pixie.translate(50,50)
+transform = pixie.translate(50, 50)
 
-qrcode_width=qrcode_height = 50
+qrcode_width = qrcode_height = 50
 
 qrcode_image = pixie.read_image(qrcode_file)
 qrcode_image = qrcode_image.resize(qrcode_width, qrcode_height)
-qrcode_transform = pixie.translate(150,150)
+qrcode_transform = pixie.translate(150, 150)
 
 
-image.draw(picture,transform=transform)
+image.draw(picture, transform=transform)
 # image.draw(picture)
 # image.draw(picture,transform=transform)
-image.draw(qrcode_image,transform=qrcode_transform)
+image.draw(qrcode_image, transform=qrcode_transform)
 image.write_file("ad_0.png")
 
 # now we try to reverse engineer that thing.
---
+++ tests/bilibili_video_recommendation_server/detect_qr_code.py
@@ -0,0 +1,7 @@
+image = "test_image_with_qr_code.png"
+
+from lazero.utils.importers import cv2_custom_build_init
+cv2_custom_build_init()
+import cv2
+
+cv2.imshow()
