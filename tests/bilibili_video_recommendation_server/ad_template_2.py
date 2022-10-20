b81c6a0f8b1d2ec746b040f0d976c5526f753971
GitJournal <app@gitjournal.io>
2022-10-20 17:46:05 +0800

    [2022-10-20 09:45:55]
    FILE:
    tests/bilibili_video_recommendation_server/ad_template_2.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/ad_template_2.py
+++ tests/bilibili_video_recommendation_server/ad_template_2.py
@@ -1,22 +1,32 @@
 import pixie
 from lazero.utils.importers import cv2_custom_build_init
+
 cv2_custom_build_init()
 import cv2
 
-def getSolidColorFromCode():
-    color = pixie.Color()
+
 def getImageW2H(image_path):
     height, width, _ = cv2.imread(image_path)
-    w2h = width/height
+    w2h = width / height
     return w2h
-ad_width, ad_height = 1000,1000
+
+
+ad_width, ad_height = 1000, 1000
 font_path = "./wqy-microhei0.ttf"
 cover_path = "sample_cover.jpg"
 qrcode_path = "MyQRCode1.png"
 play_button_path = "play_button.png"
 bilibili_logo_path = "bilibili.png"
-
+white = pixie.Color(1, 1, 1, 1)
 image = pixie.Image(ad_width, ad_height)
 # we are creating this, not replacing qr code.
-image.fill()
-image.write('ad_2.png')
+image.fill(white)
+
+# place the cover.
+cover_w2h = getImageW2H(cover_path)
+cover_width = int(ad_width*0.8)
+cover_height = int(cover_width/cover_w2h)
+cover = pixie.image
+
+
+image.write_file("ad_2.png")
