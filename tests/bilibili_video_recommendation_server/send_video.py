eebc72e690075fc14d153dc2abb2d392d9c51030
GitJournal <app@gitjournal.io>
2022-10-21 20:15:06 +0800

    [2022-10-21 12:14:59]
    FILE:
    tests/bilibili_video_recommendation_server/send_video.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/send_video.py
+++ tests/bilibili_video_recommendation_server/send_video.py
@@ -1,5 +1,23 @@
-# botoy only can repost video.
+# botoy can only repost video.
 # repostVideo2Group	转发视频到群聊
 # repostVideo2Friend	转发视频给好友
 # getVideoURL	获取短视频链接
 
+# cqhttp can post video.
+# https://docs.go-cqhttp.org/cqcode/#%E7%9F%AD%E8%A7%86%E9%A2%91
+
+baseUrl = "http://0.0.0.0:5700"
+
+group = 543780931
+
+import requests
+
+url = baseUrl + "/send_group_msg"
+content = 
+
+message = "[CQ:video,data={}]".format(content) 
+data = {"group_id": group, "message": message, "auto_escape": False}
+r = requests.post(url, data=data)
+print(r.json())
+# cannot send json. wtf?
+# 请参考 go-cqhttp 端输出
