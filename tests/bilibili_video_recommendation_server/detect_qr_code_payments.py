606ae3a6ad227776d02120eb013edf23d0fee05c
GitJournal <app@gitjournal.io>
2022-10-21 21:21:54 +0800

    [2022-10-21 13:21:41]
    FILE:
    tests/bilibili_video_recommendation_server/detect_qr_code_payments.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

---
+++ tests/bilibili_video_recommendation_server/detect_qr_code_payments.py
@@ -0,0 +1,44 @@
+# image = "test_image_with_qr_code.png"
+# fail to obtain the qrcode.
+# but we might want use our original qrcode.
+# image = "output_qrcode2.png"
+images=['alipay_payment_code.png','wechat_payment_code.jpg']
+
+from lazero.utils.importers import cv2_custom_build_init
+
+from PIL import Image
+from pyzbar.pyzbar import decode, ZBarSymbol
+
+# @function 'detect_qr' detect and decode qrcode from frame using pyzbar lib
+# @param 'inputFrame' type <class 'numpy.ndarray'>
+# @return if detected type 'bool'
+def detect_qr(inputFrame):
+    img = Image.fromarray(inputFrame)
+    decodedImg = decode(img, symbols=[ZBarSymbol.QRCODE])
+    # it reads the content. but where is the code?
+    print('total %d qrcode detected' % len(decodedImg))
+    # breakpoint()
+    # length: 2
+
+    if len(decodedImg) > 0:
+        for code in decodedImg:
+            decodedBytes = code.data
+            stringData = decodedBytes.decode("utf-8")
+            print("QRCode content:")
+            print(stringData)
+            polygon = code.polygon
+            print('POLYGON CONTENT:')
+            print(polygon)
+
+        return True
+    else:
+        return False
+
+cv2_custom_build_init()
+import cv2
+for image in images:
+# shit! for picture with 2 qrcodes it fails to detect.
+# bbox return None
+    img = cv2.imread(image)
+    detect_qr(img)
+    print("_"*20)
