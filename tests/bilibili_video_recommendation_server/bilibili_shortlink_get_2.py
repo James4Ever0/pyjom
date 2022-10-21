ede611f0c6a73660848b8e8c41ebbf72bcc908f6
GitJournal <app@gitjournal.io>
2022-10-21 17:57:20 +0800

    [2022-10-21 09:57:12]
    FILE:
    tests/bilibili_video_recommendation_server/bilibili_shortlink_get_2.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/bilibili_shortlink_get_2.py
+++ tests/bilibili_video_recommendation_server/bilibili_shortlink_get_2.py
@@ -9,7 +9,8 @@ url = "https://api.bilibili.com/x/share/click"
 # burl="https://www.bilibili.com/video/BV1Wv41157Wz"
 # burl = "https://www.bilibili.com/v/pay/charge?upmid=85300402&upurl=%2F%2Fspace.bilibili.com%2F85300402&upname=J4D&upavatar=https%3A%2F%2Fi1.hdslb.com%2Fbfs%2Fface%2F73f1323696c857eb5f47f4a8bd03c1115a056af1.jpg&oid=85300402&otype=up&from=zone"
 # only from 
-burl = "https://space.bilibili.com/85300402"
+# burl = "https://space.bilibili.com/85300402" # my space.
+burl = "https://www.bilibili.com/read/cv19232041" # my article with e-begging
 data = {
     "build": 6700300,
         "buvid": 0,
