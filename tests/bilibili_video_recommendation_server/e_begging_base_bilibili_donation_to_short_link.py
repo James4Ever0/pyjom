4062d45e986929d3b807366ebe047b140f56361b
GitJournal <app@gitjournal.io>
2022-10-21 16:22:37 +0800

    [2022-10-21 08:22:30]
    FILE:
    tests/bilibili_video_recommendation_server/e_begging_base_bilibili_donation_to_short_link.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/e_begging_base_bilibili_donation_to_short_link.py
+++ tests/bilibili_video_recommendation_server/e_begging_base_bilibili_donation_to_short_link.py
@@ -3,11 +3,9 @@
 # well we could do something evil here
 # my shit.
 
-# https://www.bilibili.com/v/pay/charge?upmid=85300402&upurl=%2F%2Fspace.bilibili.com%2F85300402&upname=J4D&upavatar=https%3A%2F%2Fi1.hdslb.com%2Fbfs%2Fface%2F73f1323696c857eb5f47f4a8bd03c1115a056af1.jpg&oid=85300402&otype=up&from=zone
+my_donation_link = "https://www.bilibili.com/v/pay/charge?upmid=85300402&upurl=%2F%2Fspace.bilibili.com%2F85300402&upname=J4D&upavatar=https%3A%2F%2Fi1.hdslb.com%2Fbfs%2Fface%2F73f1323696c857eb5f47f4a8bd03c1115a056af1.jpg&oid=85300402&otype=up&from=zone"
 
 
 # for other famous uploader:
-other_donation_link = https://www.bilibili.com/v/pay/charge?upmid=441381282&upurl=%2F%2Fspace.bilibili.com%2F441381282&upname=%E8%8A%B1%E4%B8%B8%E6%99%B4%E7%90%89Official&upavatar=http%3A%2F%2Fi0.hdslb.com%2Fbfs%2Fface%2F7328c4d2bd7f59a10be61589d3595880a8ebcff5.jpg&oid=441381282&otype=up&from=zone
+other_donation_link = "https://www.bilibili.com/v/pay/charge?upmid=441381282&upurl=%2F%2Fspace.bilibili.com%2F441381282&upname=%E8%8A%B1%E4%B8%B8%E6%99%B4%E7%90%89Official&upavatar=http%3A%2F%2Fi0.hdslb.com%2Fbfs%2Fface%2F7328c4d2bd7f59a10be61589d3595880a8ebcff5.jpg&oid=441381282&otype=up&from=zone"
 
-
-donation_link = 
