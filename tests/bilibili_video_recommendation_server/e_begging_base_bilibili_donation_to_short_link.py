a178301da755bb1309e5251eacfda3bce3c7f41b
GitJournal <app@gitjournal.io>
2022-10-21 16:10:33 +0800

    [2022-10-21 08:10:24]
    FILE:
    tests/bilibili_video_recommendation_server/e_begging_base_bilibili_donation_to_short_link.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/e_begging_base_bilibili_donation_to_short_link.py
+++ tests/bilibili_video_recommendation_server/e_begging_base_bilibili_donation_to_short_link.py
@@ -1,7 +1,8 @@
 # about e-begging, way more easier than e-commerce.
 
 # well we could do something evil here
+# my shit.
 
-https://www.bilibili.com/v/pay/charge?upmid=85300402&upurl=%2F%2Fspace.bilibili.com%2F85300402&upname=J4D&upavatar=https%3A%2F%2Fi1.hdslb.com%2Fbfs%2Fface%2F73f1323696c857eb5f47f4a8bd03c1115a056af1.jpg&oid=85300402&otype=up&from=zone
+# https://www.bilibili.com/v/pay/charge?upmid=85300402&upurl=%2F%2Fspace.bilibili.com%2F85300402&upname=J4D&upavatar=https%3A%2F%2Fi1.hdslb.com%2Fbfs%2Fface%2F73f1323696c857eb5f47f4a8bd03c1115a056af1.jpg&oid=85300402&otype=up&from=zone
 
 donation_link = 
