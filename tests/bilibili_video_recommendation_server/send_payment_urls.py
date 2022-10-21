0d9c505889901f54c0308cb9b92691eb909d59df
GitJournal <app@gitjournal.io>
2022-10-21 21:27:50 +0800

    [2022-10-21 13:27:43]
    FILE:
    tests/bilibili_video_recommendation_server/send_payment_urls.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/send_payment_urls.py
+++ tests/bilibili_video_recommendation_server/send_payment_urls.py
@@ -1,4 +1,3 @@
-
 baseUrl = "http://0.0.0.0:5700"
 
 group = 543780931
@@ -6,8 +5,10 @@ group = 543780931
 import requests
 
 url = baseUrl + "/send_group_msg"
-payment_urls = [# https://qr.alipay.com/tsx10243tdewwaxrvullge8
-# wxp://f2f0V92qUQI0aBO5PXtWezujxMm-C1KFub6qCi1Obt3cn1KjZqDPqoWKn8ICCcwdt8zU"]
+payment_urls = [
+    "https://qr.alipay.com/tsx10243tdewwaxrvullge8",
+    "wxp://f2f0V92qUQI0aBO5PXtWezujxMm-C1KFub6qCi1Obt3cn1KjZqDPqoWKn8ICCcwdt8zU",
+]
 message = "\n".join(payment_urls)
 data = {"group_id": group, "message": message, "auto_escape": False}
 r = requests.post(url, data=data)
