eeedfabab74c19e5f9268b42bd61435df2605dd6
GitJournal <app@gitjournal.io>
2022-10-23 18:17:58 +0800

    [2022-10-23 10:17:50]
    FILE:
    tests/unittest_sqlalchemy_1.3.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_sqlalchemy_1.3.py
+++ tests/unittest_sqlalchemy_1.3.py
@@ -4,7 +4,12 @@
 
 # the data we put into are some timestamps.
 
-import sqlalchemy
+from sqlalchemy import Column, ForeignKey, Integer, String
+
+# there are some tutorials on this.
+
+from sqlalchemy import create_engine
 
 dbpath = "test_sqlalchemy.db"
 
+engine = create_engine('sqlite:///{}'.format(dbpath)) # where are you going to store this shit?
