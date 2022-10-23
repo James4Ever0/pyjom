51e4d4669fea695caf3d447879469b888dc3b7a9
GitJournal <app@gitjournal.io>
2022-10-23 18:36:06 +0800

    [2022-10-23 10:35:59]
    FILE:
    tests/unittest_sqlalchemy_1.3_alternative_wrapper_peewee.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_sqlalchemy_1.3_alternative_wrapper_peewee.py
+++ tests/unittest_sqlalchemy_1.3_alternative_wrapper_peewee.py
@@ -9,4 +9,6 @@
 # https://github.com/coleifer/peewee
 
 # 1.3.24 original sqlalchemy version, for our dearly chatterbot.
-# currently:-1.4.42
+
+# currently: 1.4.42
+# warning! might be incompatible.
