c2c09155b59cd0c2f5499226b62734e8d27fa10e
GitJournal <app@gitjournal.io>
2022-10-23 18:30:03 +0800

    [2022-10-23 10:29:55]
    FILE:
    tests/unittest_sqlalchemy_1.3_alternative_wrapper.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

---
+++ tests/unittest_sqlalchemy_1.3_alternative_wrapper.py
@@ -0,0 +1,10 @@
+# now we try to create and persist a database.
+
+# do not delete it. we will check again.
+
+# the data we put into are some timestamps.
+
+# some peewee by the same guy who developed some database.
+
+# https://github.com/coleifer/peewee
+
