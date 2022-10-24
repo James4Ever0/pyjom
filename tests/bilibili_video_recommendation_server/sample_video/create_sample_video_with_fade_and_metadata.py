61353ac1ae4304b26359ec4b1027512384ccb579
GitJournal <app@gitjournal.io>
2022-10-24 23:55:17 +0800

    [2022-10-24 15:55:09]
    FILE:
    tests/bilibili_video_recommendation_server/sample_video/create_sample_video_with_fade_and_metadata.py
    tests/bilibili_video_recommendation_server/sample_video/template.json
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/sample_video/create_sample_video_with_fade_and_metadata.py
+++ tests/bilibili_video_recommendation_server/sample_video/create_sample_video_with_fade_and_metadata.py
@@ -2,46 +2,46 @@
 # the title of the video, intro, outro.
 
 video_path = "/root/Desktop/works/pyjom/tests/bilibili_video_recommendation_server/sample_video/sample_video.mp4"
-up_image_path = "/root/Desktop/works/pyjom/tests/bilibili_video_recommendation_server/up_image.jpg"
-output_path = 'output.mp4'
+up_image_path = (
+    "/root/Desktop/works/pyjom/tests/bilibili_video_recommendation_server/up_image.jpg"
+)
+output_path = "output.mp4"
 title = "世上所有的小猫都是天使变的！"
 template_name = "template.json"
 from caer.video.frames_and_fps import get_duration, get_res
+
 video_duration = get_duration(video_path)
 video_width, video_height = get_res(video_path)
 # we shall use editly to do this job shall we?
 
 min_video_scalar = min(video_width, video_height)
-up_image_scaler = int(min_video_scalar*0.2)
+up_image_scaler = int(min_video_scalar * 0.2)
 
 # image overlay can be done in editly
 
 editlyJson = {
-    'outPath':output_path,
-    'width':video_width,
-    'height': video_height,
-    'fps':30, # different from the default value.
-    'fast':False,
-    'keepSourceAudio':True,
-    'clips':[
+    "outPath": output_path,
+    "width": video_width,
+    "height": video_height,
+    "fps": 30,  # different from the default value.
+    "fast": True, # just for preview. if not turning this on, will be too slow.
+    "keepSourceAudio": True, # it does!
+    "clips": [
         {
-            "trasition":"fade", # or we just use random?
-            "duration":video_duration,
-            "layers":[
+            "trasition": "fade",  # or we just use random?
+            "duration": video_duration,
+            "layers": [
+                {"type": "video", "path": video_path}, # order is important.
                 {
-                    "type":"image-overlay",
-                    "path":up_image_path,
-                    "position":"top-left",
-                    "width":up_image_scaler,
-                    "height":up_image_scaler,
+                    "type": "image-overlay",
+                    "path": up_image_path,
+                    "position": "top-left",
+                    "width": up_image_scaler,
+                    "height": up_image_scaler,
                 },
-                {
-                    "type":"video",
-                    "path":video_path
-                }
-            ]
+            ],
         }
-    ]
+    ],
 }
 
 from lazero.filesystem.io import writeJsonObjectToFile
@@ -49,6 +49,11 @@ from lazero.filesystem.io import writeJsonObjectToFile
 writeJsonObjectToFile(template_name, editlyJson)
 
 import subprocess
+
 # use xvfb you SOB
-command = ['xvfb-run','editly',template_name] # no need to specify --out outputPath here
+command = [
+    "xvfb-run",
+    "editly",
+    template_name,
+]  # no need to specify --out outputPath here
 subprocess.run(command)
