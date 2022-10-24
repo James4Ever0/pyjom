e0a9005dd1ef338972466cd88f836527e22964b0
GitJournal <app@gitjournal.io>
2022-10-24 15:17:59 +0800

    [2022-10-24 07:17:48]
    FILE:
    tests/bilibili_video_recommendation_server/ebegging_template.py
    tests/qq_opqbot/opq_arm64/Logs/OPQBot.log
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/ebegging_template.py
+++ tests/bilibili_video_recommendation_server/ebegging_template.py
@@ -4,4 +4,53 @@
 alipay_link = "https://qr.alipay.com/tsx10243tdewwaxrvullge8"
 wechat_link = "wxp://f2f0V92qUQI0aBO5PXtWezujxMm-C1KFub6qCi1Obt3cn1KjZqDPqoWKn8ICCcwdt8zU"
 
-import 
+from generate_qr_code import makeAndSaveQrcode
+
+qrcode_path = "test_ebegging.png"
+makeAndSaveQrcode(alipay_link, qrcode_path)
+
+# now we plan to draw this thing.
+# how big is the canvas? no fill?
+
+import pixie
+
+qrcode = pixie.read_image(qrcode_path)
+qrcode_width = qrcode_height = int(0.3 * ad_width)
+qrcode = qrcode.resize(qrcode_width, qrcode_height)
+
+
+font = pixie.read_font(font_path)
+font.size = int(ad_width * 0.04)
+if night_mode:
+    font.paint.color = pixie.Color(1, 1, 1, 1)
+else:
+    font.paint.color = pixie.Color(0, 0, 0, 1)
+qrcode_scan_text_transform_x = int(ad_width - qrcode_width * 1.1 - font.size * 1)
+qrcode_scan_text_transform = pixie.translate(
+    qrcode_scan_text_transform_x + qrcode_width, int(ad_height - qrcode_height * 1.1)
+)
+image.fill_text(font, qrcode_scan_text, transform=qrcode_scan_text_transform)
+
+qrcode_transform = pixie.translate(
+    int(ad_width - qrcode_width * 1.1 - font.size * 1.2),
+    int(ad_height - qrcode_height * 1.1),
+)
+
+
+qrcode_rounded_corner = int(0.05 * ad_width)
+qrcode_stroke_path = pixie.Path()
+qrcode_stroke_path.rounded_rect(
+    0, 0, qrcode_width, qrcode_height, *([qrcode_rounded_corner] * 4)
+)
+image.stroke_path(
+    qrcode_stroke_path,
+    cover_stroke_paint,
+    qrcode_transform,
+    stroke_width=stroke_width,
+)
+
+qrcode_mask = pixie.Mask(qrcode_width, qrcode_width)
+qrcode_mask.fill_path(qrcode_stroke_path)
+qrcode.mask_draw(qrcode_mask)
+
+image.draw(qrcode, qrcode_transform)
