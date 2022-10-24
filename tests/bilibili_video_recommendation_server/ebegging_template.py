0681d87e78e6e448001d2e55ac83c569f9cfe29e
GitJournal <app@gitjournal.io>
2022-10-24 19:38:08 +0800

    [2022-10-24 11:38:00]
    FILE:
    tests/bilibili_video_recommendation_server/ebegging_template.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/ebegging_template.py
+++ tests/bilibili_video_recommendation_server/ebegging_template.py
@@ -1,6 +1,8 @@
 # this is to make it look better than before
 # maybe.
 
+# according to the blend mode, that guy seems to be using some special blend mode to hide his qrcode from being detected. so you may want to do the same when you add this to your own picture.
+
 alipay_link = "https://qr.alipay.com/tsx10243tdewwaxrvullge8"
 wechat_link = (
     "wxp://f2f0V92qUQI0aBO5PXtWezujxMm-C1KFub6qCi1Obt3cn1KjZqDPqoWKn8ICCcwdt8zU"
