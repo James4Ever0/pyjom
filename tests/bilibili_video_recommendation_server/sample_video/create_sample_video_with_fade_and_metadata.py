570a10ee0f4ce80d3b58da307de7dd82bb720ec7
GitJournal <app@gitjournal.io>
2022-10-24 23:21:55 +0800

    [2022-10-24 15:21:46]
    FILE:
    tests/bilibili_video_recommendation_server/sample_video/create_sample_video_with_fade_and_metadata.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/sample_video/create_sample_video_with_fade_and_metadata.py
+++ tests/bilibili_video_recommendation_server/sample_video/create_sample_video_with_fade_and_metadata.py
@@ -2,16 +2,21 @@
 # the title of the video, intro, outro.
 
 video_path = "/root/Desktop/works/pyjom/tests/bilibili_video_recommendation_server/sample_video/sample_video.mp4"
+up_image_path = "/root/Desktop/works/pyjom/tests/bilibili_video_recommendation_server/up_image.jpg"
+output_path = 'output.mp4'
 title = "世上所有的小猫都是天使变的！"
 from caer.video.frames_and_fps import get_duration, get_res
 video_duration = get_duration(video_path)
 video_width, video_height = get_res(video_path)
 # we shall use editly to do this job shall we?
 
+min_video_scalar = min(video_width, video_height)
+up_image_scaler = int(min_video_scalar*0.2)
+
 # image overlay can be done in editly
 
 editlyJson = {
-    'outPath':'output.mp4',
+    'outPath':output_path,
     'width':video_width,
     'height': video_height,
     'fps':30, # different from the default value.
@@ -24,10 +29,10 @@ editlyJson = {
             "layers":[
                 {
                     "type":"image-overlay",
-                    "path":"/root/Desktop/works/pyjom/tests/bilibili_video_recommendation_server/up_image.jpg",
-                    "position":,
-                    "width":,
-                    "height":
+                    "path":up_image_path,
+                    "position":"top-left",
+                    "width":up_image_scalar,
+                    "height"::
                 }
             ]
         }
