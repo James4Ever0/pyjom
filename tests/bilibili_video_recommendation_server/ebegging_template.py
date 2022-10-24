ff9f178acd0bb6785aaad2d8a9e9b629d47e1197
GitJournal <app@gitjournal.io>
2022-10-24 15:14:57 +0800

    [2022-10-24 07:14:47]
    FILE:
    tests/bilibili_video_recommendation_server/ebegging_template.py
    tests/bilibili_video_recommendation_server/generate_qr_code.py
    tests/qq_opqbot/opq_arm64/Logs/OPQBot.log
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/ebegging_template.py
+++ tests/bilibili_video_recommendation_server/ebegging_template.py
@@ -1,5 +1,7 @@
 # this is to make it look better than before
 # maybe.
 
-alipay_link = ""
+alipay_link = "https://qr.alipay.com/tsx10243tdewwaxrvullge8"
+wechat_link = "wxp://f2f0V92qUQI0aBO5PXtWezujxMm-C1KFub6qCi1Obt3cn1KjZqDPqoWKn8ICCcwdt8zU"
 
+import 
--- tests/bilibili_video_recommendation_server/generate_qr_code.py
+++ tests/bilibili_video_recommendation_server/generate_qr_code.py
@@ -1,10 +1,16 @@
-data="https://b23.tv/DPn1G4p"
 # Importing library
 import qrcode
 
 # Encoding data using make() function
-img = qrcode.make(data)
-print("image type:", type(img))
+def makeAndSaveQrcode(data, save_path, debug=False):
+    img = qrcode.make(data)
+    if debug:
+        print("image type:", type(img))
+    img.save(save_path)
+
 # <class 'qrcode.image.pil.PilImage'>
 # Saving as an image file
-img.save('MyQRCode1.png')
+if __name__ == "__main__":
+    data="https://b23.tv/DPn1G4p"
+    save_path = 'MyQRCode1.png'
+    makeAndSaveQrcode(data, save_path)
