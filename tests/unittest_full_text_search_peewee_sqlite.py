a0ac8b141c759d377156b24dce734a130c4f558d
GitJournal <app@gitjournal.io>
2022-10-27 19:06:22 +0800

    [2022-10-27 11:06:15]
    FILE:
    tests/unittest_full_text_search_peewee_sqlite.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_full_text_search_peewee_sqlite.py
+++ tests/unittest_full_text_search_peewee_sqlite.py
@@ -1,9 +1,17 @@
 from peewee import *
-from playhouse.sqlite_ext import SqliteExtDatabase
+from playhouse.sqlite_ext import SqliteExtDatabase, FTSModel
 
 db_path = "test_fulltext_search.db"
 
 db = SqliteExtDatabase(db_path, pragmas={
     'journal_mode': 'wal',
     'cache_size': -1024 * 64})
+
+class BilibiliVideoIndex(FTSModel):
+    rowid = RowIDField()
+    title = SearchField()
+    content = SearchField()
+    class Meta:
+        database = db
+        options = {'tokenize': 'porter'}
 
