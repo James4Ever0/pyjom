32f3c9facf3c44b9367c1c5b6e82d7d379caaf2d
GitJournal <app@gitjournal.io>
2022-10-27 21:33:40 +0800

    [2022-10-27 13:33:30]
    FILE:
    tests/bilibili_video_recommendation_server/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/test.py
+++ tests/bilibili_video_recommendation_server/test.py
@@ -331,7 +331,8 @@ def searchUserVideos(
         # export all video? shit?
         # you should tokenize the thing.
         # but this search does not have limitations!
-        results = BilibiliVideoIndex.search_bm25(keyword).where().limit(limit)
+        user_video_id
+        results = BilibiliVideoIndex.search_bm25(keyword).where(BilibiliVideoIndex.rowid in ).limit(limit)
         []
         for video_index in results:
             bilibiliVideo = BilibiliVideo.get(id = video_index.id)
