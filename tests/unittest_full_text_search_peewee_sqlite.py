3c429d4a7515ce91e2073d341b9cba2fe5f23900
GitJournal <app@gitjournal.io>
2022-10-27 19:03:21 +0800

    [2022-10-27 11:03:13]
    FILE:
    tests/unittest_full_text_search_peewee_sqlite.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_full_text_search_peewee_sqlite.py
+++ tests/unittest_full_text_search_peewee_sqlite.py
@@ -1,1 +1,9 @@
 from peewee import *
+from playhouse.sqlite_ext import SqliteExtDatabase
+
+db_path = "test_fulltext_search.db"
+
+db = SqliteExtDatabase(db_path, pragmas={
+    'journal_mode': 'wal',
+    'cache_size': -1024 * 64})
+
