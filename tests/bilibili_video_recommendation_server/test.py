bedd2e2548aa3bf59a1c3b054401b8a0a6575597
GitJournal <app@gitjournal.io>
2022-10-28 10:32:09 +0800

    [2022-10-28 02:32:07]
    FILE:
    tests/bilibili_video_recommendation_server/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/test.py
+++ tests/bilibili_video_recommendation_server/test.py
@@ -266,7 +266,7 @@ def getUserVideos(
     keyword="",
     order=VideoOrder.PUBDATE,
     dedeuserid: str = "397424026",
-    use_credential: bool =True,
+    use_credential: bool =False,
     stop_on_duplicate: bool = True,
 ):  # all videos? just at init.
     # some stop condition for early termination.
