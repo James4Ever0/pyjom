679d96500d41562809bfe7986067e8cb318a568a
GitJournal <app@gitjournal.io>
2022-10-29 13:41:01 +0800

    [2022-10-29 05:40:52]
    FILE:
    tests/unittest_get_subtid_name_and_majortid_name.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_get_subtid_name_and_majortid_name.py
+++ tests/unittest_get_subtid_name_and_majortid_name.py
@@ -5,7 +5,8 @@ for key,value in BSP.all.tids.__dict__.items():
     try:
         major_tid = value.tid
         print("MAJOR",key, major_tid)
-        for subkey, subvalue in 
+        for subkey, subvalue in value.__dict__.items():
+            minor_tid = 
     except:
         pass
 
