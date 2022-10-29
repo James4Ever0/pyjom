4c56bbe58ea10b2bb6ad754947db12a766a0f802
GitJournal <app@gitjournal.io>
2022-10-29 13:47:14 +0800

    [2022-10-29 05:47:06]
    FILE:
    tests/unittest_get_subtid_name_and_majortid_name.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_get_subtid_name_and_majortid_name.py
+++ tests/unittest_get_subtid_name_and_majortid_name.py
@@ -2,23 +2,24 @@ from bilibili_api import search
 
 BSP = search.bilibiliSearchParams
 
-majorMinorMappings = {}
-for key, value in BSP.all.tids.__dict__.items():
-    try:
-        major_tid = value.tid
-        if debug:
-            print("MAJOR", key, major_tid)
-        content = {"major": {"tid": major_tid, "name": key}}
-        majorMinorMappings.update({major_tid: content, key: content})
-        for subkey, subvalue in value.__dict__.items():
-            if subkey != "tid" and type(subvalue) == int:
-                if debug:
-                    print("MINOR", subkey, subvalue)
-                content = {
-                    "major": {"tid": major_tid, "name": key},
-                    "minor": {"tid": subvalue, "name": subkey},
-                }
-                majorMinorMappings.update({subvalue: content, subkey: content})
-    except:
-        pass
-return majorMinorMappings
+def getMajorMinorTidsMappings()
+    majorMinorMappings = {}
+    for key, value in BSP.all.tids.__dict__.items():
+        try:
+            major_tid = value.tid
+            if debug:
+                print("MAJOR", key, major_tid)
+            content = {"major": {"tid": major_tid, "name": key}}
+            majorMinorMappings.update({major_tid: content, key: content})
+            for subkey, subvalue in value.__dict__.items():
+                if subkey != "tid" and type(subvalue) == int:
+                    if debug:
+                        print("MINOR", subkey, subvalue)
+                    content = {
+                        "major": {"tid": major_tid, "name": key},
+                        "minor": {"tid": subvalue, "name": subkey},
+                    }
+                    majorMinorMappings.update({subvalue: content, subkey: content})
+        except:
+            pass
+    return majorMinorMappings
