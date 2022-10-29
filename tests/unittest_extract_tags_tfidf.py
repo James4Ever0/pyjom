492b2dbbfb99fa0fc0a91dbb261b34777a5575ec
GitJournal <app@gitjournal.io>
2022-10-29 19:21:34 +0800

    [2022-10-29 11:21:31]
    FILE:
    tests/unittest_extract_tags_tfidf.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_extract_tags_tfidf.py
+++ tests/unittest_extract_tags_tfidf.py
@@ -1,13 +1,14 @@
 text = "Flask的路由,视图和相关配置" # just a sample please?
 
 from nltk.corpus import stopwords
-myStopwords = stopwords(['chinese', 'english'])
+myStopwords = stopwords.words(['chinese', 'english'])
 
 import jieba.analyse as ana
 import jieba
 
 text_splited = " ".join(jieba.lcut(text))
 
-ana.set_stop_words(myStopwords)
+
+
 tags=ana.extract_tags(text_splited,topK=5,)
 print(tags)
