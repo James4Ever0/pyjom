8690f889f82a365327d91acd870066f4e57dc11b
GitJournal <app@gitjournal.io>
2022-10-29 13:46:33 +0800

    [2022-10-29 05:46:25]
    FILE:
    tests/unittest_get_subtid_name_and_majortid_name.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_get_subtid_name_and_majortid_name.py
+++ tests/unittest_get_subtid_name_and_majortid_name.py
@@ -3,34 +3,19 @@ from bilibili_api import search
 BSP = search.bilibiliSearchParams
 
 majorMinorMappings = {}
-
 for key, value in BSP.all.tids.__dict__.items():
     try:
         major_tid = value.tid
-        # print("MAJOR", key, major_tid)
+        print("MAJOR", key, major_tid)
         content = {"major": {"tid": major_tid, "name": key}}
-        majorMinorMappings.update(
-            {major_tid: content,key:content}
-        )
+        majorMinorMappings.update({major_tid: content, key: content})
         for subkey, subvalue in value.__dict__.items():
             if subkey != "tid" and type(subvalue) == int:
                 print("MINOR", subkey, subvalue)
-                content ={
-                            "major": {"tid": major_tid, "name": key},
-                            "minor": {"tid": subvalue, "name": subkey},
-                        }
-                majorMinorMappings.update(
-                    {
-                        subvalue: content, subkey:content
-                    }
-                )
-                majorMinorMappings.update(
-                    {
-                        subkey: {
-                            "major": {"tid": major_tid, "name": key},
-                            "minor": {"tid": subvalue, "name": subkey},
-                        }
-                    }
-                )
+                content = {
+                    "major": {"tid": major_tid, "name": key},
+                    "minor": {"tid": subvalue, "name": subkey},
+                }
+                majorMinorMappings.update({subvalue: content, subkey: content})
     except:
         pass
