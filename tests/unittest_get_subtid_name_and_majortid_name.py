c99a25a8530f0116ed91124048a99a0646af8d9d
GitJournal <app@gitjournal.io>
2022-10-29 13:39:02 +0800

    [2022-10-29 05:38:53]
    FILE:
    tests/unittest_get_subtid_name_and_majortid_name.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_get_subtid_name_and_majortid_name.py
+++ tests/unittest_get_subtid_name_and_majortid_name.py
@@ -1,7 +1,8 @@
 from bilibili_api import search
-
+from typing
 BSP = search.bilibiliSearchParams
 
 for key,value in BSP.all.tids.__dict__.items():
-    if type(value)
+    if type(value) == :
+        print(key, value)
 
