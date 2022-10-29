a8ae16bace1f9a94bbc9ddee2c884889d001235c
GitJournal <app@gitjournal.io>
2022-10-29 13:48:20 +0800

    [2022-10-29 05:48:12]
    FILE:
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_get_subtid_name_and_majortid_name.py
+++ tests/unittest_get_subtid_name_and_majortid_name.py
@@ -11,7 +11,7 @@ def getMajorMinorTopicMappings(debug:bool=False):
             if debug:
                 print("MAJOR", key, major_tid)
             content = {"major": {"tid": major_tid, "name": key}}
-            majorMinorMappings.update({major_tid: content, key: content})
+            majorMinorMappings.update({major_tid: content, key: content, str(major_tid):content})
             for subkey, subvalue in value.__dict__.items():
                 if subkey != "tid" and type(subvalue) == int:
                     if debug:
@@ -20,7 +20,7 @@ def getMajorMinorTopicMappings(debug:bool=False):
                         "major": {"tid": major_tid, "name": key},
                         "minor": {"tid": subvalue, "name": subkey},
                     }
-                    majorMinorMappings.update({subvalue: content, subkey: content})
+                    majorMinorMappings.update({subvalue: content, subkey: content, str()})
         except:
             pass
     return majorMinorMappings
