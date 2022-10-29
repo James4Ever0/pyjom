82202160fc4450d8e7452909c97a6dc42ff5010e
GitJournal <app@gitjournal.io>
2022-10-29 13:43:07 +0800

    [2022-10-29 05:42:59]
    FILE:
    tests/unittest_get_subtid_name_and_majortid_name.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_get_subtid_name_and_majortid_name.py
+++ tests/unittest_get_subtid_name_and_majortid_name.py
@@ -10,7 +10,7 @@ for key,value in BSP.all.tids.__dict__.items():
         for subkey, subvalue in value.__dict__.items():
             if subkey !='tid' and type(subvalue) == int:
                 print("MINOR",subkey, subvalue)
-                majorMinorMappings.append({subvalue:{'major':}})
+                majorMinorMappings.append({subvalue:{'major':{'tid':}}})
     except:
         pass
 
