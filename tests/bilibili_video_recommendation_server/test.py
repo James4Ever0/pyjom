1d022e47cb56eecf679b019d0795c2e45b082d80
GitJournal <app@gitjournal.io>
2022-10-29 13:54:06 +0800

    [2022-10-29 05:53:58]
    FILE:
    tests/bilibili_video_recommendation_server/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/test.py
+++ tests/bilibili_video_recommendation_server/test.py
@@ -76,6 +76,43 @@ BSP = search.bilibiliSearchParams
 # make it into a dashboard like thing.
 
 
+@lru_cache(maxsize=1)
+def getMajorMinorTopicMappings(debug:bool=False):
+    majorMinorMappings = {}
+    for key, value in BSP.all.tids.__dict__.items():
+        try:
+            major_tid = value.tid
+            if debug:
+                print("MAJOR", key, major_tid)
+            content = {"major": {"tid": major_tid, "name": key}}
+            majorMinorMappings.update({major_tid: content, key: content, str(major_tid):content})
+            for subkey, subvalue in value.__dict__.items():
+                if subkey != "tid" and type(subvalue) == int:
+                    if debug:
+                        print("MINOR", subkey, subvalue)
+                    content = {
+                        "major": {"tid": major_tid, "name": key},
+                        "minor": {"tid": subvalue, "name": subkey},
+                    }
+                    majorMinorMappings.update({subvalue: content, subkey: content, str(subvalue):content})
+        except:
+            pass
+    return majorMinorMappings
+
+
+def getTagStringFromTid(tid):
+    majorMinorTopicMappings = getMajorMinorTopicMappings()
+    topic = majorMinorTopicMappings.get(tid,None)
+    tags = []
+    if topic:
+        majorTopic = topic.get('major',{}).get('name',None)
+        minorTopic = topic.get('minor',{}).get('name',None)
+        if majorTopic:
+            tags.append(majorTopic)
+            if minorTopic:
+                tags.append(minorTopic)
+    return ",".join(tags)
+
 # also make a decorator for refreshing status, add it to every function.
 # thie refresher is scheduled.
 # you may want to run this beforehand...
