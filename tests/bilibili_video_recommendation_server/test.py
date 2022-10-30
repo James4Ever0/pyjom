03060b69ac0afffd57aa79bb6460e3be512509f4
GitJournal <app@gitjournal.io>
2022-10-30 20:47:12 +0800

    [2022-10-30 12:47:10]
    FILE:
    tests/bilibili_video_recommendation_server/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/test.py
+++ tests/bilibili_video_recommendation_server/test.py
@@ -824,7 +824,7 @@ def refresh_status(
     now_minus_check_interval = datetime.datetime.now() - check_interval
     selector = BilibiliVideo.select(BilibiliVideo.bvid).where(
         (BilibiliVideo.last_check < now_minus_check_interval)
-        & (BilibiliVideo.visible == False) # only check invisible videos.
+        & (BilibiliVideo.visible == False) # only check invisible videos. invisible videos will be removed 
     )  # need check or not?
     print("refreshing video status")
     for bvid in progressbar.progressbar(selector):
