1ae916b252720f5b3fdfba3b082e755e934aac4e
GitJournal <app@gitjournal.io>
2022-10-30 21:06:14 +0800

    [2022-10-30 13:06:06]
    FILE:
    tests/bilibili_video_recommendation_server/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/test.py
+++ tests/bilibili_video_recommendation_server/test.py
@@ -868,41 +868,42 @@ if __name__ == "__main__":
     db = getBilibiliVideoDatabaseAndCreateTables()
     refresh_status()  # ensure the database is connected.
     schedule.every(20).minutes.do(refresh_status)
-    objective = 'test'
-    objective = 'test'
-    test = "searchVideos"
-    # test = "searchUserVideos"
-    # test = "textPreprocessing"
-    # test = 'registerMyVideo'
-    if test == "textPreprocessing":
-        text = "猫  咪  钢  琴  家 searchUserVideos have a nice day 新闻联播,动物圈,汪星人,喵星人"
-        result = textPreprocessing(text)
-        print("RESULT:", result)
-    elif test == "searchUserVideos":
-        query = "猫"
-        # for v in searchUserVideos(query):
-        for v in searchUserVideos(query, method="bm25"):
-            # print("fetched value:", v)
-            breakpoint()
-    elif test == "registerMyVideo":
-        bvid = "BV1fR4y1w7BL"  # that's surely yours.
-        dedeuserid = "397424026"
-        registerUserVideo(bvid, dedeuserid)
-    elif test == "searchVideos":
-        query = "cod19"  # recent hot videos.
-        for v in searchAndRegisterVideos(query):
-            print(v) # warning: title containing markup language.
-            breakpoint()
-        # you want to select video after search?
-        # no keywords? are you kidding?
-        # results = getMyVideos()
-        # print(results)
-        # video_bvid_invisible = "BV1pd4y1y7cu"  # too fucking fast. i can't see shit.
-        # # some hard rule on this? like being invisible for how long we will disable video source for good?
-        # video_bvid_abnormal = "BV1x84y1B7Nb"
-        # video_bvid_visible = "BV1Fs411k7e9"  # 老戴的视频
-        # # 啊叻？视频不见了？
-        # checkPublishedVideo(video_bvid_invisible)
-        # checkPublishedVideo(video_bvid_visible)
-        # checkPublishedVideo(video_bvid_abnormal)
-        # 视频撞车了 需要原创视频哦
+    # objective = 'test'
+    objective = 'server'
+    if objective == 'test':
+        test = "searchVideos"
+        # test = "searchUserVideos"
+        # test = "textPreprocessing"
+        # test = 'registerMyVideo'
+        if test == "textPreprocessing":
+            text = "猫  咪  钢  琴  家 searchUserVideos have a nice day 新闻联播,动物圈,汪星人,喵星人"
+            result = textPreprocessing(text)
+            print("RESULT:", result)
+        elif test == "searchUserVideos":
+            query = "猫"
+            # for v in searchUserVideos(query):
+            for v in searchUserVideos(query, method="bm25"):
+                # print("fetched value:", v)
+                breakpoint()
+        elif test == "registerMyVideo":
+            bvid = "BV1fR4y1w7BL"  # that's surely yours.
+            dedeuserid = "397424026"
+            registerUserVideo(bvid, dedeuserid)
+        elif test == "searchVideos":
+            query = "cod19"  # recent hot videos.
+            for v in searchAndRegisterVideos(query):
+                print(v) # warning: title containing markup language.
+                breakpoint()
+            # you want to select video after search?
+            # no keywords? are you kidding?
+            # results = getMyVideos()
+            # print(results)
+            # video_bvid_invisible = "BV1pd4y1y7cu"  # too fucking fast. i can't see shit.
+            # # some hard rule on this? like being invisible for how long we will disable video source for good?
+            # video_bvid_abnormal = "BV1x84y1B7Nb"
+            # video_bvid_visible = "BV1Fs411k7e9"  # 老戴的视频
+            # # 啊叻？视频不见了？
+            # checkPublishedVideo(video_bvid_invisible)
+            # checkPublishedVideo(video_bvid_visible)
+            # checkPublishedVideo(video_bvid_abnormal)
+            # 视频撞车了 需要原创视频哦
