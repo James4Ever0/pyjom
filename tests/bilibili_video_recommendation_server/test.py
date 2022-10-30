9c77d00c8ce5659c02ace7555e9f0e10b2321d7b
GitJournal <app@gitjournal.io>
2022-10-30 20:59:53 +0800

    [2022-10-30 12:59:49]
    FILE:
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/test.py
+++ tests/bilibili_video_recommendation_server/test.py
@@ -12,7 +12,7 @@ import random
 sys.path.append("/root/Desktop/works/pyjom/")
 # you might want to add this to bilibili platform api, if there's no use of pyjom.commons
 from pyjom.platforms.bilibili.credentials import getCredentialByDedeUserId
-from pyjom.platforms.bilibili.utils import linkFixer, videoDurationStringToSeconds
+from pyjom.platforms.bilibili.utils import linkFixer, videoDurationStringToSeconds, clearHtmlTags
 
 from lazero.search.preprocessing import getFourVersionsOfProcessedLine
 import jieba
