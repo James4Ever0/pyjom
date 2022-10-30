a176ab89daf43fc346a528dd6b200a776ced103f
GitJournal <app@gitjournal.io>
2022-10-30 11:00:51 +0800

    [2022-10-30 03:00:49]
    FILE:
    tests/bilibili_video_recommendation_server/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/test.py
+++ tests/bilibili_video_recommendation_server/test.py
@@ -776,7 +776,7 @@ if __name__ == "__main__":
     schedule.every(20).minutes.do(refresh_status)
     # test = 'searchVideos'
     # test = "searchUserVideos"
-    test = "textPreprocessing"
+    # test = "textPreprocessing"
     # test = 'registerMyVideo'
     if test == "textPreprocessing":
         text = "猫  咪  钢  琴  家 searchUserVideos have a nice day 新闻联播,动物圈,汪星人,喵星人"
