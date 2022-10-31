5e20f994a46cd66731059e7bb11b3db54755cfd5
GitJournal <app@gitjournal.io>
2022-10-31 13:22:21 +0800

    [2022-10-31 05:22:19]
    FILE:
    tests/bilibili_video_recommendation_server/create_invisible_blend_transparent_ebegging.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/create_invisible_blend_transparent_ebegging.py
+++ tests/bilibili_video_recommendation_server/create_invisible_blend_transparent_ebegging.py
@@ -7,22 +7,24 @@ import pixie
 bgImage = pixie.read_image(background)
 ebImage = pixie.read_image(ebegging)
 
-ratio_0 = 1200 / min(bgImage.width,bgImage.height)
-b_w, b_h = int(ratio_0*bgImage.width),int(ratio_0*bgImage.height
+ratio_0 = 1200 / min(bgImage.width, bgImage.height)
+b_w, b_h = int(ratio_0 * bgImage.width), int(ratio_0 * bgImage.height)
 
-bgImage = bgImage.resize(b_w,b_h)
+bgImage = bgImage.resize(b_w, b_h)
 
 factor = 5
-ratio = min(bgImage.width, bgImage.height)/(max(ebImage.width, ebImage.height)*factor)
+ratio = min(bgImage.width, bgImage.height) / (
+    max(ebImage.width, ebImage.height) * factor
+)
 
-e_w, e_h = int(ratio*ebImage.width), int(ratio*ebImage.height)
+e_w, e_h = int(ratio * ebImage.width), int(ratio * ebImage.height)
 # print(e_w, e_h)
 # print(ratio)
 # print(bgImage.width, bgImage.height)
 
 ebImage = ebImage.resize(e_w, e_h)
 ebImage.apply_opacity(0.5)
-t_w, t_h = bgImage.width-e_w, bgImage.height-e_h
+t_w, t_h = bgImage.width - e_w, bgImage.height - e_h
 
-bgImage.draw(ebImage,transform = pixie.translate(t_w, t_h))
-bgImage.write_file('ebegging_setu_transparent.png')
+bgImage.draw(ebImage, transform=pixie.translate(t_w, t_h))
+bgImage.write_file("ebegging_setu_transparent.png")
