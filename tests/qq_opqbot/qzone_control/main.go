2f24e42706f6ea0508abf7aa693d258ed4db9bfe
GitJournal <app@gitjournal.io>
2022-10-31 21:09:53 +0800

    [2022-10-31 13:09:44]
    FILE:
    tests/qq_opqbot/qzone_control/main.go
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/qq_opqbot/qzone_control/main.go
+++ tests/qq_opqbot/qzone_control/main.go
@@ -4,7 +4,7 @@ import "fmt"
 import "github.com/mcoo/OPQBot"
 func main() {
     fmt.Println("Hello, World!")
-    var qqNumber int64 =917521610
+    qqNumber:=int64(917521610)
     serverPort:="8784"
     opqBot := OPQBot.NewBotManager(qqNumber,"http://localhost:"+serverPort) //前面是机器人的QQ号,后面参数是机器人接口的地址
     err := opqBot.Start()
