fc3082d3f14de41c00a9935e182386936de5b652
GitJournal <app@gitjournal.io>
2022-10-31 12:45:23 +0800

    [2022-10-31 04:45:21]
    FILE:
    tests/bilibili_video_recommendation_server/create_invisible_blend_transparent_ebegging.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/create_invisible_blend_transparent_ebegging.py
+++ tests/bilibili_video_recommendation_server/create_invisible_blend_transparent_ebegging.py
@@ -12,6 +12,6 @@ ratio = min(bgImage.width, bgImage.height)/(max(ebImage.width, bgImage.height)*4
 e_w, e_h = int(ratio*ebImage.width), int(ratio*ebImage.height)
 
 ebImage = ebImage.resize(e_w, e_h)
-t_w, t_h = bgImage.width-e_w, bgImage.height
+t_w, t_h = bgImage.width-e_w, bgImage.height-e_h
 
-bgImage.draw(ebImage,transform = pixie.translate(t_w, t_h) )
+bgImage.draw(ebImage,transform = pixie.translate(t_w, t_h))
