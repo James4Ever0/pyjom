f1a940373ba77d775d341c3cf0f4c11339c7ba9b
GitJournal <app@gitjournal.io>
2022-10-31 12:58:14 +0800

    [2022-10-31 04:58:12]
    FILE:
    tests/bilibili_video_recommendation_server/send_image_botoy.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/send_image_botoy.py
+++ tests/bilibili_video_recommendation_server/send_image_botoy.py
@@ -17,21 +17,21 @@ import base64
 objective = "send_image"
 
 if objective == 'send_video_ad':
-link = "https://b23.tv/DPn1G4p"
-title_text = "真·朋克！揭秘《赛博朋克2077》屏幕之外的魔幻换弹操作"
-content = "观看视频:\n{}\n{}".format(link, title_text)
+    link = "https://b23.tv/DPn1G4p"
+    title_text = "真·朋克！揭秘《赛博朋克2077》屏幕之外的魔幻换弹操作"
+    content = "观看视频:\n{}\n{}".format(link, title_text)
 
 
-picture_path = "ad_2_standalone_cover.png"
-with open(picture_path, "rb") as img_file:
-    b64_string = base64.b64encode(img_file.read())
-# print(b64_string)
-result = action.sendFriendPic(user=user, picBase64Buf=b64_string.decode("utf-8"))
+    picture_path = "ad_2_standalone_cover.png"
+    with open(picture_path, "rb") as img_file:
+        b64_string = base64.b64encode(img_file.read())
+    # print(b64_string)
+    result = action.sendFriendPic(user=user, picBase64Buf=b64_string.decode("utf-8"))
 
-print(result)
-action.sendFriendText(user=user, content=content)
-# send separately.
-# result = action.sendFriendPic(user=user, picBase64Buf=b64_string.decode('utf-8'),content = content)
+    print(result)
+    action.sendFriendText(user=user, content=content)
+    # send separately.
+    # result = action.sendFriendPic(user=user, picBase64Buf=b64_string.decode('utf-8'),content = content)
 
 
 # picture_path = "ad_2.png"
