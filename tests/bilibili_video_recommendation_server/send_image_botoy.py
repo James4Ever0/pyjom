23b6da264f9e5bbad9d96a59fee2a740da3edcf6
GitJournal <app@gitjournal.io>
2022-10-31 12:58:05 +0800

    [2022-10-31 04:58:02]
    FILE:
    tests/bilibili_video_recommendation_server/send_image_botoy.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/send_image_botoy.py
+++ tests/bilibili_video_recommendation_server/send_image_botoy.py
@@ -14,6 +14,9 @@ action = Action(qq=qq, port=port, host="127.0.0.1")
 user = 1281727431
 import base64
 
+objective = "send_image"
+
+if objective == 'send_video_ad':
 link = "https://b23.tv/DPn1G4p"
 title_text = "真·朋克！揭秘《赛博朋克2077》屏幕之外的魔幻换弹操作"
 content = "观看视频:\n{}\n{}".format(link, title_text)
