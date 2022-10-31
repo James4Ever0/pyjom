065d0d917762347a0e2caff5fc55bc34ec1ba7f7
GitJournal <app@gitjournal.io>
2022-10-31 19:57:45 +0800

    [2022-10-31 11:56:02]
    FILE:
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/e_begging_base_bilibili_donation_to_short_link.py
+++ tests/bilibili_video_recommendation_server/e_begging_base_bilibili_donation_to_short_link.py
@@ -3,6 +3,9 @@
 # well we could do something evil here
 # my shit.
 
+# it will generate a one-time payment code.
+# https://api.bilibili.com/x/ugcpay/trade/elec/pay/order/status?qr_token=1875b75b1299497888ce9c33089dc676
+
 my_donation_link = "https://www.bilibili.com/v/pay/charge?upmid=85300402&upurl=%2F%2Fspace.bilibili.com%2F85300402&upname=J4D&upavatar=https%3A%2F%2Fi1.hdslb.com%2Fbfs%2Fface%2F73f1323696c857eb5f47f4a8bd03c1115a056af1.jpg&oid=85300402&otype=up&from=zone"
 
 
