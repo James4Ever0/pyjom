06fc56bcd625aa945f7412aa566ae806c171c4c9
GitJournal <app@gitjournal.io>
2022-10-31 22:16:00 +0800

    [2022-10-31 14:15:57]
    FILE:
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/qq_opqbot/opqbot_python_bindings/init.sh
+++ tests/qq_opqbot/opqbot_python_bindings/init.sh
@@ -4,6 +4,10 @@ go mod init dummy.com/dum
 # go get $PKGNAME
 # gopy build -output=bindings -vm=python3 $PKGNAME
 
+# PKGNAME=github.com/mcoo/OPQBot/qzone # wtf?
+# go get $PKGNAME
+# gopy build -output=bindings_qzone -vm=python3 $PKGNAME
+
 PKGNAME=github.com/mcoo/OPQBot/qzone # wtf?
 go get $PKGNAME
 gopy build -output=bindings_qzone -vm=python3 $PKGNAME
