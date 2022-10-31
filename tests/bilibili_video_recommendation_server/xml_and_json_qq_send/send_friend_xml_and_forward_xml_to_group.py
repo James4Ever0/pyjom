4247bff70763d67f4699a91d3448b3931aa8766c
GitJournal <app@gitjournal.io>
2022-10-31 15:52:01 +0800

    [2022-10-31 07:51:53]
    FILE:
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/xml_and_json_qq_send/send_friend_xml_and_forward_xml_to_group.py
+++ tests/bilibili_video_recommendation_server/xml_and_json_qq_send/send_friend_xml_and_forward_xml_to_group.py
@@ -12,13 +12,13 @@ baseUrl = "http://0.0.0.0:5700" # the thing is not ready yet.
 import requests
 
 # url = baseUrl + "/send_private_msg"
-# message = '[CQ:share,url=http://baidu.com,title=百度]'
+message = '[CQ:share,url=http://baidu.com,title=百度]'
 
 # data = {"user_id": user, "message": message, "auto_escape": False}
 # r = requests.post(url, data=data)
 # print(r.json())
 
-message = "[CQ:forward,id={}]".format(291457889)
+# message = "[CQ:forward,id={}]".format(291457889)
 
 url = baseUrl+"/send_group_msg"
 data = {"group_id":group, "message": message, "auto_escape": False}
