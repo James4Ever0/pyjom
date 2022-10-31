7cc89c2cb603fc7c7735816b0084ec707841408b
GitJournal <app@gitjournal.io>
2022-10-31 15:02:22 +0800

    [2022-10-31 07:02:20]
    FILE:
    tests/bilibili_video_recommendation_server/xml_and_json_qq_send/send_friend_xml_and_forward_xml_to_group.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/xml_and_json_qq_send/send_friend_xml_and_forward_xml_to_group.py
+++ tests/bilibili_video_recommendation_server/xml_and_json_qq_send/send_friend_xml_and_forward_xml_to_group.py
@@ -1,3 +1,9 @@
 cover = "https://i0.hdslb.com/bfs/archive/c5a0d18ee077fb6a4ac0970ccb0a3788e137d14f.jpg" # works.
 content = """<?xml version="1.0" encoding="UTF-8" standalone="yes"?><msg templateID="123" url="https://b23.tv/uHML5mi?share_medium=android&amp;share_source=qq&amp;bbid=XY1BB721B1F97348DBDE4297FE1B4ABE26BAA&amp;ts=1666023406285" serviceID="1" action="web" actionData="" a_actionData="" i_actionData="" brief="[QQ小程序]哔哩哔哩" flag="0"><item layout="2"><picture cover="{}"/><title>【AI动画】妮露PV动画 风转换【NovelAI】</title></item><source url="https://b23.tv/uHML5mi?share_medium=android&amp;share_source=qq&amp;bbid=XY1BB721B1F97348DBDE4297FE1B4ABE26BAA&amp;ts=1666023406285" icon="http://miniapp.gtimg.cn/public/appicon/432b76be3a548fc128acaa6c1ec90131_200.jpg" name="哔哩哔哩" appid="0" action="app" actionData="" a_actionData="tencent0://" i_actionData=""/></msg>""".format(cover)
 
+from botoy import Action
+
+qq = 917521610
+port = 8784
+action = Action(qq=qq, port=port, host="127.0.0.1")
+user = 1281727431
