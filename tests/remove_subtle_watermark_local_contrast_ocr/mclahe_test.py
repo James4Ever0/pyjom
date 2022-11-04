fc65f9aadd910d691bdba1b201fa9ff22c2b2fdc
GitJournal <app@gitjournal.io>
2022-11-05 00:13:47 +0800

    [2022-11-04 16:13:39]
    FILE:
    tests/remove_subtle_watermark_local_contrast_ocr/mclahe_test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/remove_subtle_watermark_local_contrast_ocr/mclahe_test.py
+++ tests/remove_subtle_watermark_local_contrast_ocr/mclahe_test.py
@@ -6,6 +6,6 @@ colorimage = cv2.imread("IWWS.jpeg")
 # print(colorimage.shape)
 
 k = (30,30,3)
-colorimage_clahe = mclahe.mclahe(colorimage,kernel_size=k,n_bins=400)
+colorimage_clahe = mclahe.mclahe(colorimage,kernel_size=k,n_bins=400,adaptive_hist_range=True)
 
-# cv2.imwrite("clahe_image.jpeg", colorimage_clahe)
+cv2.imwrite("clahe_image.jpeg", colorimage_clahe)
