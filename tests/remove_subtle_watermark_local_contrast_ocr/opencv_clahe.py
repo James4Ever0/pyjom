edcb0bcea943b9bfabf58fbd354e49177b006067
GitJournal <app@gitjournal.io>
2022-11-04 23:51:30 +0800

    [2022-11-04 15:51:28]
    FILE:
    tests/remove_subtle_watermark_local_contrast_ocr/opencv_clahe.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/remove_subtle_watermark_local_contrast_ocr/opencv_clahe.py
+++ tests/remove_subtle_watermark_local_contrast_ocr/opencv_clahe.py
@@ -6,7 +6,8 @@ import numpy as np
 # Reading the image from the present directory
 image = cv2.imread("IWWS.jpeg")
 # Resizing the image for compatibility
-image = cv2.resize(image, (500, 600))
+# image = cv2.resize(image, (500, 600))
+# why?
 
 # The initial processing of the image
 # image = cv2.medianBlur(image, 3)
