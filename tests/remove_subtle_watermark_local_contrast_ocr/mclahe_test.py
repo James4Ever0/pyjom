fffe48565722a3e942084136bc561fed0dbb8489
GitJournal <app@gitjournal.io>
2022-11-05 00:16:11 +0800

    [2022-11-04 16:16:03]
    FILE:
    tests/remove_subtle_watermark_local_contrast_ocr/mclahe_test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/remove_subtle_watermark_local_contrast_ocr/mclahe_test.py
+++ tests/remove_subtle_watermark_local_contrast_ocr/mclahe_test.py
@@ -5,7 +5,7 @@ import cv2
 colorimage = cv2.imread("IWWS.jpeg")
 # print(colorimage.shape)
 
-k = (30,30,3)
-colorimage_clahe = mclahe.mclahe(colorimage, kernel_size=k)
+k = (30,30,1)
+colorimage_clahe = mclahe.mclahe(colorimage, kernel_size=k) # not working! what the fuck?
 
 cv2.imwrite("clahe_image_mclahe.jpeg", colorimage_clahe)
