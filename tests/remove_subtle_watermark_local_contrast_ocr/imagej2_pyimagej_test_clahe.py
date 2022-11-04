38721f0458236d1b5a1a95e547c44dec2709c411
GitJournal <app@gitjournal.io>
2022-11-04 22:52:41 +0800

    [2022-11-04 14:52:38]
    FILE:
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/remove_subtle_watermark_local_contrast_ocr/imagej2_pyimagej_test_clahe.py
+++ tests/remove_subtle_watermark_local_contrast_ocr/imagej2_pyimagej_test_clahe.py
@@ -18,5 +18,5 @@ image = ij.py.from_java(jimage)
 # d = dir(ij)
 # print(d)
 # ['IJ', 'ResultsTable', 'RoiManager', 'WindowManager', '__class__', '__del__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', '_access_legacy_class', '_check_legacy_active', 'animation', 'app', 'appEvent', 'command', 'compareTo', 'console', 'context', 'convert', 'dataset', 'display', 'dispose', 'equals', 'event', 'eventHistory', 'get', 'getApp', 'getClass', 'getContext', 'getIdentifier', 'getInfo', 'getLocation', 'getPriority', 'getShortName', 'getTitle', 'getVersion', 'hashCode', 'icon', 'imageDisplay', 'input', 'io', 'launch', 'legacy', 'log', 'lut', 'main', 'menu', 'module', 'notebook', 'notify', 'notifyAll', 'object', 'op', 'options', 'overlay', 'platform', 'plugin', 'prefs', 'py', 'recentFile', 'rendering', 'sampler', 'scifio', 'screenCapture', 'script', 'setContext', 'setInfo', 'setPriority', 'startup', 'status', 'text', 'thread', 'toString', 'tool', 'ui', 'update', 'uploader', 'wait', 'widget', 'window']
-p = ij.plugin
-print(dir(p))
+# p = ij.plugin
+# print(dir(p))
