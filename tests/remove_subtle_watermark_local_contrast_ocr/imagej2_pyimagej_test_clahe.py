4f04aa34e096e44b3dcb1fa242998365043bd1b3
GitJournal <app@gitjournal.io>
2022-11-04 23:25:26 +0800

    [2022-11-04 15:25:24]
    FILE:
    tests/remove_subtle_watermark_local_contrast_ocr/imagej2_pyimagej_test_clahe.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/remove_subtle_watermark_local_contrast_ocr/imagej2_pyimagej_test_clahe.py
+++ tests/remove_subtle_watermark_local_contrast_ocr/imagej2_pyimagej_test_clahe.py
@@ -5,6 +5,8 @@ jpype.addClassPath("/root/Desktop/works/pyjom/tests/remove_subtle_watermark_loca
 jpype.addClassPath("/root/Desktop/works/pyjom/tests/remove_subtle_watermark_local_contrast_ocr/imagej_fiji_linux/Fiji.app/plugins/*")
 jpype.addClassPath("/root/Desktop/works/pyjom/tests/remove_subtle_watermark_local_contrast_ocr/imagej_fiji_linux/Fiji.app/plugins/*/*")
 
+jpype.
+
 # # Create an ImageJ2 gateway with the newest available version of ImageJ2.
 # # fiji_path = "/root/Desktop/works/pyjom/tests/remove_subtle_watermark_local_contrast_ocr/imagej_fiji_linux/Fiji.app"
 # # ij = imagej.init(fiji_path)
