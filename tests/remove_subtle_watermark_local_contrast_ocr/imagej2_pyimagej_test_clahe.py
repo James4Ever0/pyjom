49c15e5abc9f367b59e088acce46d1b12abe13ec
GitJournal <app@gitjournal.io>
2022-11-04 23:27:32 +0800

    [2022-11-04 15:27:29]
    FILE:
    tests/remove_subtle_watermark_local_contrast_ocr/imagej2_pyimagej_test_clahe.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/remove_subtle_watermark_local_contrast_ocr/imagej2_pyimagej_test_clahe.py
+++ tests/remove_subtle_watermark_local_contrast_ocr/imagej2_pyimagej_test_clahe.py
@@ -7,7 +7,44 @@ jpype.addClassPath("/root/Desktop/works/pyjom/tests/remove_subtle_watermark_loca
 
 jpype.startJVM()
 
-from ij import ImageJ
+#!/usr/bin/env jython
+
+from ij import IJ
+import os
+
+from mpicbg.ij.clahe import Flat
+from ij.process import ImageConverter
+
+# http://fiji.sc/wiki/index.php/Enhance_Local_Contrast_(CLAHE)
+# http://fiji.sc/cgi-bin/gitweb.cgi?p=mpicbg.git;a=blob;f=mpicbg/ij/clahe/PlugIn.java;h=663153764493547de560c08ee11f2e6b1e7e1a32;hb=HEAD
+
+dir = "/usr/people/tmacrina/seungmount/research/Julimaps/datasets/AIBS_pilot_v1/0_raw/"
+
+blocksize = 63
+histogram_bins = 255
+maximum_slope = 3
+mask = "*None*"
+composite = False
+mask = None
+
+# files = os.listdir(dir)
+# files.sort()
+# for file in files:
+#      if file.endswith(".tif")
+fn = os.path.join(dir, 'original.tif')
+imp = IJ.openImage(fn)
+output_fn = os.path.splitext(fn)[0] + "_CLAHE_8bit.tif"
+imp = IJ.openImage(fn)
+  
+Flat.getFastInstance().run( imp, 
+                        blocksize,
+                        histogram_bins,
+                        maximum_slope,
+                        mask,
+                        composite )
+ImageConverter(imp).convertToGray8()
+
+IJ.save(imp, output_fn)
 
 # # Create an ImageJ2 gateway with the newest available version of ImageJ2.
 # # fiji_path = "/root/Desktop/works/pyjom/tests/remove_subtle_watermark_local_contrast_ocr/imagej_fiji_linux/Fiji.app"
