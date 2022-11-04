aa95d87b4c5b7a763a55ae9f6b27acf65a5c10f4
GitJournal <app@gitjournal.io>
2022-11-05 00:07:45 +0800

    [2022-11-04 16:07:37]
    FILE:
    tests/remove_subtle_watermark_local_contrast_ocr/opencv_clahe.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/remove_subtle_watermark_local_contrast_ocr/opencv_clahe.py
+++ tests/remove_subtle_watermark_local_contrast_ocr/opencv_clahe.py
@@ -4,7 +4,7 @@ import cv2
 # import numpy as np
 
 # Reading the image from the present directory
-image = cv2.imread("IWWS.jpeg")
+color_image = cv2.imread("IWWS.jpeg")
 # Resizing the image for compatibility
 # image = cv2.resize(image, (500, 600))
 # why?
@@ -15,10 +15,15 @@ image = cv2.imread("IWWS.jpeg")
 
 # The declaration of CLAHE
 # clipLimit -> Threshold for contrast limiting
-clahe = cv2.createCLAHE(clipLimit = 5)
+clahe_model = cv2.createCLAHE(clipLimit = 5)
 
-# you may use the luminosity 
-final_img = clahe.apply(image)
+# you may use grayscale image for the luminosity output.
+# final_img = clahe.apply(image)
+
+# For ease of understanding, we explicitly equalize each channel individually
+colorimage_b = clahe_model.apply(colorimage[:,:,0])
+colorimage_g = clahe_model.apply(colorimage[:,:,1])
+colorimage_r = clahe_model.apply(colorimage[:,:,2])
 
 # Ordinary thresholding the same image
 # _, ordinary_img = cv2.threshold(image_bw, 155, 255, cv2.THRESH_BINARY)
