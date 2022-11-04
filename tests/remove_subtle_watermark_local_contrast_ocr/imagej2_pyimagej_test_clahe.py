e72b163cf24e2a6c54f68aa9fba77b3ed8822111
GitJournal <app@gitjournal.io>
2022-11-04 23:03:30 +0800

    [2022-11-04 15:03:27]
    FILE:
    tests/remove_subtle_watermark_local_contrast_ocr/imagej2_pyimagej_test_clahe.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/remove_subtle_watermark_local_contrast_ocr/imagej2_pyimagej_test_clahe.py
+++ tests/remove_subtle_watermark_local_contrast_ocr/imagej2_pyimagej_test_clahe.py
@@ -6,7 +6,7 @@ import scyjava
 # plugins_dir = '/Applications/Fiji.app/plugins'
 plugins_dir = "/root/Desktop/works/pyjom/tests/remove_subtle_watermark_local_contrast_ocr/imagej_fiji_linux/Fiji.app/plugins"
 # scyjava.config.add_option(f'-Dplugins.dir={plugins_dir}')
-scyjava.
+scyjava.config.add_option(f'')
 ij = imagej.init()
 
 # Load an image.
