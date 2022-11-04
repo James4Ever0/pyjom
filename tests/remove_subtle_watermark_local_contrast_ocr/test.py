7d33df827e6c96c828bc238047281c2ac21ab31f
GitJournal <app@gitjournal.io>
2022-11-04 18:20:34 +0800

    [2022-11-04 10:20:31]
    FILE:
    tests/remove_subtle_watermark_local_contrast_ocr/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/remove_subtle_watermark_local_contrast_ocr/test.py
+++ tests/remove_subtle_watermark_local_contrast_ocr/test.py
@@ -6,6 +6,6 @@ with Image(filename ='IWWS.jpeg') as image:
 	# Clone the image in order to process
 	with image.clone() as local_contrast:
 		# Invoke local_contrast function with radius 12 and sigma 3
-		local_contrast.local_contrast(12, 3)
+		local_contrast.local_contrast(12, 3) # radius, sigma (0-100)
 		# Save the image
 		local_contrast.save(filename ='local_contrast1.jpg')
