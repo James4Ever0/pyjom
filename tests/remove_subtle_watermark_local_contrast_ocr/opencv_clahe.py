c835efdc8687633e1ac0da0868eba6c1781bdbef
GitJournal <app@gitjournal.io>
2022-11-04 23:52:57 +0800

    [2022-11-04 15:52:55]
    FILE:
    tests/remove_subtle_watermark_local_contrast_ocr/opencv_clahe.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/remove_subtle_watermark_local_contrast_ocr/opencv_clahe.py
+++ tests/remove_subtle_watermark_local_contrast_ocr/opencv_clahe.py
@@ -16,7 +16,7 @@ image_bw = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 # The declaration of CLAHE
 # clipLimit -> Threshold for contrast limiting
 clahe = cv2.createCLAHE(clipLimit = 5)
-final_img = clahe.apply(image_bw) + 30
+final_img = clahe.apply(image_bw)
 
 # Ordinary thresholding the same image
 # _, ordinary_img = cv2.threshold(image_bw, 155, 255, cv2.THRESH_BINARY)
