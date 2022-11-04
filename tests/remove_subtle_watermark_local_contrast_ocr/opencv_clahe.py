291fcd61c2cc3f7da33980d83982a399793a42f8
GitJournal <app@gitjournal.io>
2022-11-04 23:51:13 +0800

    [2022-11-04 15:51:10]
    FILE:
    tests/remove_subtle_watermark_local_contrast_ocr/opencv_clahe.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/remove_subtle_watermark_local_contrast_ocr/opencv_clahe.py
+++ tests/remove_subtle_watermark_local_contrast_ocr/opencv_clahe.py
@@ -1,1 +1,25 @@
 # https://www.geeksforgeeks.org/clahe-histogram-eqalization-opencv/
+
+import cv2
+import numpy as np
+
+# Reading the image from the present directory
+image = cv2.imread("image.jpg")
+# Resizing the image for compatibility
+image = cv2.resize(image, (500, 600))
+
+# The initial processing of the image
+# image = cv2.medianBlur(image, 3)
+image_bw = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# The declaration of CLAHE
+# clipLimit -> Threshold for contrast limiting
+clahe = cv2.createCLAHE(clipLimit = 5)
+final_img = clahe.apply(image_bw) + 30
+
+# Ordinary thresholding the same image
+_, ordinary_img = cv2.threshold(image_bw, 155, 255, cv2.THRESH_BINARY)
+
+# Showing all the three images
+cv2.imshow("ordinary threshold", ordinary_img)
+cv2.imshow("CLAHE image", final_img)
