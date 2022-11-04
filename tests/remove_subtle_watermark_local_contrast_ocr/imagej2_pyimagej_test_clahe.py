485aab08719c125c9a9aad6dcfa6cc4d2b9bc478
GitJournal <app@gitjournal.io>
2022-11-04 18:53:44 +0800

    [2022-11-04 10:53:40]
    FILE:
    tests/remove_subtle_watermark_local_contrast_ocr/imagej2_pyimagej_test_clahe.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

---
+++ tests/remove_subtle_watermark_local_contrast_ocr/imagej2_pyimagej_test_clahe.py
@@ -0,0 +1,14 @@
+# Create an ImageJ2 gateway with the newest available version of ImageJ2.
+import imagej
+ij = imagej.init()
+
+# Load an image.
+image_url = "."
+jimage = ij.io().open(image_url)
+
+# Convert the image from ImageJ2 to xarray, a package that adds
+# labeled datasets to numpy (http://xarray.pydata.org/en/stable/).
+image = ij.py.from_java(jimage)
+
+# Display the image (backed by matplotlib).
+ij.py.show(image, cmap='gray')
