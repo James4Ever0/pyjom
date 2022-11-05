d0c7790910d0ade8ffb019bfef082f31c97cdf77
GitJournal <app@gitjournal.io>
2022-11-05 12:18:46 +0800

    [2022-11-05 04:18:38]
    FILE:
    tests/remove_subtle_watermark_local_contrast_ocr/jython_imagej_test_clahe.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/remove_subtle_watermark_local_contrast_ocr/jython_imagej_test_clahe.py
+++ tests/remove_subtle_watermark_local_contrast_ocr/jython_imagej_test_clahe.py
@@ -12,3 +12,43 @@ for d in cpdirs:
         sys.path.append(abs_jarpath)
 
 # now begin work.
+
+from ij import IJ
+import os
+
+from mpicbg.ij.clahe import Flat
+from ij.process import ImageConverter
+
+# http://fiji.sc/wiki/index.php/Enhance_Local_Contrast_(CLAHE)
+# http://fiji.sc/cgi-bin/gitweb.cgi?p=mpicbg.git;a=blob;f=mpicbg/ij/clahe/PlugIn.java;h=663153764493547de560c08ee11f2e6b1e7e1a32;hb=HEAD
+
+# dir = "/usr/people/tmacrina/seungmount/research/Julimaps/datasets/AIBS_pilot_v1/0_raw/"
+
+blocksize = 40
+histogram_bins = 255
+maximum_slope = 5
+mask = "*None*"
+composite = False
+mask = None
+
+# files = os.listdir(dir)
+# files.sort()
+# for file in files:
+#      if file.endswith(".tif")
+# fn = os.path.join(dir, 'original.tif')
+fn = "IWWS.jpeg"
+imp = IJ.openImage(fn)
+output_fn = "imagej_output_jython.jpg"
+imp = IJ.openImage(fn)
+
+Flat.getFastInstance().run(
+    imp, blocksize, histogram_bins, maximum_slope, mask, composite
+)
+IJ.save(imp, output_fn)
+
+Flat.getFastInstance().run(
+    imp, blocksize, histogram_bins, maximum_slope, mask, composite
+)
+# ImageConverter(imp).convertToGray8()
+
+IJ.save(imp, "imagej_double_jython.jpg")
