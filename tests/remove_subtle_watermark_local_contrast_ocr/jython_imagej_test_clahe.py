cdae024e3412addc2683e4752c6d1efac0aebd14
GitJournal <app@gitjournal.io>
2022-11-05 12:20:26 +0800

    [2022-11-05 04:20:19]
    FILE:
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/remove_subtle_watermark_local_contrast_ocr/jython_imagej_test_clahe.py
+++ tests/remove_subtle_watermark_local_contrast_ocr/jython_imagej_test_clahe.py
@@ -1,7 +1,10 @@
 import os
 import sys
 
-cpdirs = ["/root/Desktop/works/pyjom/tests/remove_subtle_watermark_local_contrast_ocr/imagej_fiji_linux/Fiji.app/jars/","/root/Desktop/works/pyjom/tests/remove_subtle_watermark_local_contrast_ocr/imagej_fiji_linux/Fiji.app/plugins/"]
+cpdirs = [
+    "/root/Desktop/works/pyjom/tests/remove_subtle_watermark_local_contrast_ocr/imagej_fiji_linux/Fiji.app/jars/",
+    "/root/Desktop/works/pyjom/tests/remove_subtle_watermark_local_contrast_ocr/imagej_fiji_linux/Fiji.app/plugins/",
+]
 
 for d in cpdirs:
     abspath = os.path.abspath(d)
@@ -14,6 +17,7 @@ for d in cpdirs:
 # now begin work.
 
 from ij import IJ
+
 # import os
 
 from mpicbg.ij.clahe import Flat
