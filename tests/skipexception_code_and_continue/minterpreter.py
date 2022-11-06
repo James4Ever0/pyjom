f61a14acad9fe104cd32018ee2b36c491fd2bfb0
GitJournal <app@gitjournal.io>
2022-11-06 21:39:38 +0800

    [2022-11-06 13:39:36]
    FILE:
    tests/skipexception_code_and_continue/minterpreter.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/skipexception_code_and_continue/minterpreter.py
+++ tests/skipexception_code_and_continue/minterpreter.py
@@ -7,13 +7,12 @@ for line in source_code:
     line = line.replace("\n", "")
     indentLevel = int(len(line.replace(line.strip(), "")) / 4)
     # print(registeredLevels)
-    exceptCodes = "print('current execution code:')"
+    exceptCodes = ["print('current execution code:')"]
     if len(registeredLevels) > 0 and indentLevel <= registeredLevels[-1]:
         mIndentLevel = registeredLevels.pop(-1)
         print((mIndentLevel * 2) * 4 * " " + "except:")
-    for exceptCode in exceptCodes:
-
-        print((mIndentLevel * 2+1) * 4 * " " +exceptCode)
+        for exceptCode in exceptCodes:
+            print((mIndentLevel * 2+1) * 4 * " " +exceptCode)
     if line == "":
         continue
     print((indentLevel * 2) * 4 * " " + "try:")
