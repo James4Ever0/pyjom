a09beba5de70384ca3d85fb07f1eeb4afb6983a4
GitJournal <app@gitjournal.io>
2022-11-06 22:17:21 +0800

    [2022-11-06 14:17:18]
    FILE:
    tests/skipexception_code_and_continue/standardFunction.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/skipexception_code_and_continue/standardFunction.py
+++ tests/skipexception_code_and_continue/standardFunction.py
@@ -1,14 +1,20 @@
 def troubleFunction():
     statementsAtSameLevel = ['f = 12','return a','return b','return c','return d','return e','return f']
     for statement in statementsAtSameLevel:
+        print('executing statement:', statement)
         while True:
             try:
                 if statement.startswith('return '):
+                    print('eval return statement')
                     val = eval(statement.replace('return ',''))
                     return val
                 else:
+                    print('exec normal statement')
                     exec(statement)
+                break
             except:
+                import traceback
+                traceback.print_exc()
                 print('error code:',statement)
                 statement = input('please enter remedy:\n')
 
