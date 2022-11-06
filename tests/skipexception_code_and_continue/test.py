5bf5c7462bd4cb3c17a60fa5306ffa35d06f7073
GitJournal <app@gitjournal.io>
2022-11-06 21:09:46 +0800

    [2022-11-06 13:09:38]
    FILE:
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/skipexception_code_and_continue/test.py
+++ tests/skipexception_code_and_continue/test.py
@@ -1,1 +1,12 @@
+def troubleFunction():
+    a = 0
+    print(b) # skipped
+    return b # also skipped
+    return a # successfully returned with value of a
 
+val = troubleFunction()
+print(val) # 0
+
+c = 0
+print(d) # skipped
+print(c) # printing value of c
