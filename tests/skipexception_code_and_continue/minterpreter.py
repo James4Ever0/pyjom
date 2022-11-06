9f28c3272e6954d961c51c509ede01777af1a127
GitJournal <app@gitjournal.io>
2022-11-06 21:15:39 +0800

    [2022-11-06 13:15:32]
    FILE:
    tests/skipexception_code_and_continue/minterpreter.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/skipexception_code_and_continue/minterpreter.py
+++ tests/skipexception_code_and_continue/minterpreter.py
@@ -5,3 +5,4 @@ source_code = open('test.py', 'r').readlines()
 for line in source_code:
     indentLevel = int(len(line.replace(line.strip(),"").replace('\n',''))/4)
     print(line, indentLevel)
+    if line.startswith('')
