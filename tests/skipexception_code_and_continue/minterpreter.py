bf56f38227a5791849fab53c108147262028f20d
GitJournal <app@gitjournal.io>
2022-11-06 21:51:20 +0800

    [2022-11-06 13:51:16]
    FILE:
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/skipexception_code_and_continue/minterpreter.py
+++ tests/skipexception_code_and_continue/minterpreter.py
@@ -18,7 +18,7 @@ for lineNumber, line in enumerate(source_code):
         continue
     print((indentLevel * 2) * 4 * " " + "try:")
     print((indentLevel * 2 + 1) * 4 * " " + "print('entering code line: {}')".format(lineNumber))
-    if line.startswith('return '):
+    if not line.startswith('return '):
         print((indentLevel * 2 + 1) * 4 * " " + line.strip(), "# indent[{}]".format(indentLevel))
     if line.startswith("def "):
         registeredLevels.append(indentLevel)
