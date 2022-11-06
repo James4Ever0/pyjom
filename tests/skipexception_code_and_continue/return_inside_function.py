e573815ca3c7c556d990b2f6b3a446118a34729f
GitJournal <app@gitjournal.io>
2022-11-06 21:57:52 +0800

    [2022-11-06 13:57:49]
    FILE:
    tests/skipexception_code_and_continue/return_inside_function.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/skipexception_code_and_continue/return_inside_function.py
+++ tests/skipexception_code_and_continue/return_inside_function.py
@@ -5,8 +5,8 @@ def error_func():
         print('you must somehow return something')
         code = input('please set some return statement.')
         if code.startswith('return '):
-        exec("myReturnValue = {}".format(code.replace('return ','')))
-        return val
+            exec("myReturnValue = {}".format(code.replace('return ','')))
+            return myReturnValue
 
 val = error_func()
 print('value returned:',val)
