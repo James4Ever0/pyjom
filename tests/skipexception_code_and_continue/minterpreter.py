35e84aee9d34ab9d24e786a16552f3ba4e6a3fd8
GitJournal <app@gitjournal.io>
2022-11-06 21:28:48 +0800

    [2022-11-06 13:28:41]
    FILE:
    tests/skipexception_code_and_continue/minterpreter.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/skipexception_code_and_continue/minterpreter.py
+++ tests/skipexception_code_and_continue/minterpreter.py
@@ -1,19 +1,19 @@
 # shall the code share modified version of statements?
 
-source_code = open('test.py', 'r').readlines()
+source_code = open("test.py", "r").readlines()
 
 registeredLevels = []
 for line in source_code:
-    line=line.replace('\n','')
-    indentLevel = int(len(line.replace(line.strip(),""))/4)
-    if len(registeredLevels)>0 and indentLevel< registeredLevels[-1]:
+    line = line.replace("\n", "")
+    indentLevel = int(len(line.replace(line.strip(), "")) / 4)
+    if len(registeredLevels) > 0 and indentLevel < registeredLevels[-1]:
         mIndentLevel = registeredLevels.pop(-1)
-        print((mIndentLevel*2)*4*" "+'except: pass')
-
-    if line == "": continue
-    print((indentLevel*2)*4*" "+'try:')
-    print((indentLevel*2+1)*4*" "+line, "[{}]".format(indentLevel))
+        print((mIndentLevel * 2) * 4 * " " + "except: pass")
+    if line == "":
+        continue
+    print((indentLevel * 2) * 4 * " " + "try:")
+    print((indentLevel * 2 + 1) * 4 * " " + line, "[{}]".format(indentLevel))
     if line.startswith("def "):
         registeredLevels.append(indentLevel)
         continue
-    print((indentLevel*2)*4*" "+'except: pass')
+    print((indentLevel * 2) * 4 * " " + "except: pass")
