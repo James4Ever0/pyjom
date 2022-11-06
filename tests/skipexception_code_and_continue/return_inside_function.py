f1a4f5d3be1c15daf3e68bdd4588f592368e1400
GitJournal <app@gitjournal.io>
2022-11-06 21:59:21 +0800

    [2022-11-06 13:59:19]
    FILE:
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/skipexception_code_and_continue/return_inside_function.py
+++ tests/skipexception_code_and_continue/return_inside_function.py
@@ -4,7 +4,7 @@ def error_func():
     except:
         print('you must somehow return something')
         while True:
-            code = input('please set some return statement.')
+            code = input('please set some return statement.\n')
             if code.startswith('return '):
                 myReturnValue = eval("{}".format(code.replace('return ','')))
                 print('about to return value')
