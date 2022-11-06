15aa4499dc5511dac74fff200e205d2423184fba
GitJournal <app@gitjournal.io>
2022-11-06 21:24:31 +0800

    [2022-11-06 13:24:22]
    FILE:
    tests/skipexception_code_and_continue/minterpreter.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/skipexception_code_and_continue/minterpreter.py
+++ tests/skipexception_code_and_continue/minterpreter.py
@@ -2,11 +2,14 @@
 
 source_code = open('test.py', 'r').readlines()
 
+registeredLevel = -1
+unclosed
 for line in source_code:
     line=line.replace('\n','')
     indentLevel = int(len(line.replace(line.strip(),""))/4)
     if line == "": continue
     print((indentLevel*2)*4*" "+'try:')
     print((indentLevel*2+1)*4*" "+line, "[{}]".format(indentLevel))
-    if line.startswith("def "): continue
+    if line.startswith("def "):
+        continue
     print((indentLevel*2)*4*" "+'except: pass')
