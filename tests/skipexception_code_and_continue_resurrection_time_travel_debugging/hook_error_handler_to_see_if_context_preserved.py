ef17bed6047c0e34f816df65f17f422deb2e15b8
GitJournal <app@gitjournal.io>
2022-11-07 16:27:40 +0800

    [2022-11-07 08:27:38]
    FILE:
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/hook_error_handler_to_see_if_context_preserved.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/skipexception_code_and_continue_resurrection_time_travel_debugging/hook_error_handler_to_see_if_context_preserved.py
+++ tests/skipexception_code_and_continue_resurrection_time_travel_debugging/hook_error_handler_to_see_if_context_preserved.py
@@ -4,7 +4,8 @@ import tempfile
 
 def customExceptHook():
     print("context preserved! please take action!")
-    
+    while True:
+        input('exit? (y for exit)\n')
     print('closing program now!')
 
 sys.excepthook=customExceptHook
