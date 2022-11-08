b436525c0fdec9c619ae0ef21a8ecd3655494838
GitJournal <app@gitjournal.io>
2022-11-08 19:03:39 +0800

    [2022-11-08 11:03:36]
    FILE:
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_code/pyjom_recover_and_rewrite.py
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_code/recover_and_rewrite.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_code/pyjom_recover_and_rewrite.py
+++ tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_code/pyjom_recover_and_rewrite.py
@@ -1,24 +1,30 @@
 from recover_and_rewrite import recover_and_rewrite
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import os
+    from comby import Comby
+
+    comby = Comby()
     dirpath = "/root/Desktop/works/pyjom/pyjom"
+
     def change_file_at_path(path):
-        with  open(path,'r') as f:
+        with open(path, "r") as f:
             source_old = f.read()
             source_new = recover_and_rewrite(source_old)
-        with open(path,'w') as f:
+        with open(path, "w+") as f:
             f.write(source_new)
+
     pyfiles = []
     import progressbar
+
     for basedir, dirs, files in os.walk(dirpath):
         for fname in files:
             fpath = os.path.join(basedir, fname)
-            if fname.endswith('.py'):
+            if fname.endswith(".py"):
                 pyfiles.append(fpath)
                 print(fpath)
-    # mod=100
-    # for index, pyfile in progressbar.progressbar(enumerate(pyfiles)):
-    #     if index % mod == 0:
-    #         print('processing file at path: %s' % pyfile)
-    #         change_file_at_path(pyfile)
+    mod=100
+    for index, pyfile in progressbar.progressbar(enumerate(pyfiles)):
+        if index % mod == 0:
+            print('processing file at path: %s' % pyfile)
+        change_file_at_path(pyfile)
--- tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_code/recover_and_rewrite.py
+++ tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_code/recover_and_rewrite.py
@@ -7,8 +7,8 @@ def recover_and_rewrite(source_old):
     return source_new
 
 if __name__ == '__main__':
+    from comby import Comby
     comby = Comby()
-
     source_old = open('new_test.py','r').read()
     source_new = recover_and_rewrite(source_old)
     print(source_new)
