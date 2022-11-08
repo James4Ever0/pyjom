1c4dd9531066b5275121eb83459b7a6f2b89c3c4
GitJournal <app@gitjournal.io>
2022-11-08 18:00:54 +0800

    [2022-11-08 10:00:51]
    FILE:
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_code/test.sh
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_code/test.sh
+++ tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_code/test.sh
@@ -2,6 +2,6 @@
 
 # you can first replace the code with the selected 
 
-cat new_test.py | comby ':[prefix~@reloading.*$]def :[functionName](:[args]):'  'def :[functionName](:[args]):' -stdin -stdout -matcher .py |  comby 'from reloading import reloading"  '' -stdin -stdout -matcher .py 
+cat new_test.py | comby ':[prefix~@reloading.*$]def :[functionName](:[args]):'  'def :[functionName](:[args]):' -stdin -stdout -matcher .py |  comby "from reloading import reloading\n"  '' -stdin -stdout -matcher .py 
 
 # cat test.py | comby ':[prefix~(@.+)*$]def :[functionName](:[args]):'  ':[prefix] @reloading def :[functionName](:[args]):' -rule 'where match :[prefix] { | :[_@someRandomDecorator.*] -> true | :[_] -> false } ' -stdin -stdout -matcher .py -match-only
