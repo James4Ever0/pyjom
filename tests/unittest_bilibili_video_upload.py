a83d1b824f29c728a203acf85a661496ee0f54e3
GitJournal <app@gitjournal.io>
2022-11-08 23:38:30 +0800

    [2022-11-08 15:36:42]
    FILE:
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_bilibili_video_upload.py
+++ tests/unittest_bilibili_video_upload.py
@@ -1,5 +1,7 @@
 from test_commons import *
 
 from pyjom.platforms.bilibili.uploader import uploadVideo
+import uuid
 
-uploadVideo()
+randomString = str(uuid.uuid4())
+uploadVideo(description='test video',dynamic='nothing', tagString='狗狗',title='just a test {}'.format(randomString),videoPath=videoPath, cover_path=cover_path, ) # it is with credential right now.
