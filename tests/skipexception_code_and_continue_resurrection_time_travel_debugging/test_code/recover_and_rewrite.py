0db19718304a1dbd91dad1b6fc4070b3a201c187
GitJournal <app@gitjournal.io>
2022-11-08 18:32:34 +0800

    [2022-11-08 10:32:32]
    FILE:
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_code/recover_and_rewrite.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_code/recover_and_rewrite.py
+++ tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_code/recover_and_rewrite.py
@@ -3,6 +3,8 @@ from rewrite import rewrite
 
 def recover_and_rewrite(source_old):
     intermediate = recover(source_old)
-    return rewrite(intermediate)
+    source_new = rewrite(intermediate)
+    return source_new
 
 if __name__ == '__main__':
+    source_old = open('')
