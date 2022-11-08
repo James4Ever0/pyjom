eea663368b4d487fe05d15d3a3ffb7a7f6c5646b
GitJournal <app@gitjournal.io>
2022-11-08 16:34:16 +0800

    [2022-11-08 08:34:06]
    FILE:
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_code/new_test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

---
+++ tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_code/new_test.py
@@ -0,0 +1,13 @@
+
+# decorate here!
+@dec
+@someRandomDecorator
+@reloading
+def someFunction (a,b,c,d=1,f=2
+):
+    # not touching this function!
+    @decorator
+    def inner_function (h,i,j,
+    k):
+        return hjkl
+    return abcdefg # I need you to decorate this thing.
