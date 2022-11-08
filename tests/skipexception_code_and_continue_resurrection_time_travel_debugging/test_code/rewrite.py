45a9a38c3340bbd913772c1ec443c0a4034a8dea
GitJournal <app@gitjournal.io>
2022-11-09 01:02:02 +0800

    [2022-11-08 17:01:55]
    FILE:
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_code/rewrite.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_code/rewrite.py
+++ tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_code/rewrite.py
@@ -13,8 +13,8 @@ def rewrite(source_old):
     return source_new
 
 if __name__ == "__main__":
-    # source_old = open('test2.py','r').read()
-    source_old = open('/root/Desktop/works/pyjom/pyjom/platforms/bilibili/uploader.py','r').read()
+    source_old = open('test2.py','r').read()
+    # source_old = open('/root/Desktop/works/pyjom/pyjom/platforms/bilibili/uploader.py','r').read()
     # source_old = open('/root/Desktop/works/pyjom/pyjom/platforms/bilibili/uploader.py','r').read()
     # comby = Comby()
 
