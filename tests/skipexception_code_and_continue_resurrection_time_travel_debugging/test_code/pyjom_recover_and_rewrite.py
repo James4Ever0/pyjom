2e777a857a03c94cb09968d0cf1d350adfdbd590
GitJournal <app@gitjournal.io>
2022-11-08 18:55:14 +0800

    [2022-11-08 10:55:11]
    FILE:
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_code/pyjom_recover_and_rewrite.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_code/pyjom_recover_and_rewrite.py
+++ tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_code/pyjom_recover_and_rewrite.py
@@ -4,12 +4,16 @@ if __name__ == '__main__':
     import os
     dirpath = "/root/Desktop/works/pyjom/pyjom"
     def change_file_at_path(path):
-        # print('processing file at path: %s' % path)
         with  open(path,'r') as f:
             source_old = f.read()
             source_new = recover_and_rewrite(source_old)
         with open(path,'w') as f:
             f.write(source_new)
+    pyfiles = []
+    import progressbar
     for basedir, dirs, files in os.walk(dirpath):
         for fname in files:
             fpath = os.path.join(basedir, fname)
+            if fname.endswith('.py'):
+    
+    
