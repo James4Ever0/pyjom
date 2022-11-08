37fb4b2f91f2bd9e8d561c530ef7a2ba05a977dc
GitJournal <app@gitjournal.io>
2022-11-08 23:41:47 +0800

    [2022-11-08 15:41:37]
    FILE:
    tests/unittest_bilibili_video_upload.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_bilibili_video_upload.py
+++ tests/unittest_bilibili_video_upload.py
@@ -8,14 +8,15 @@ randomString = str(uuid.uuid4())
 # how about let's generate shit?
 # use multithread uploader instead of that.
 import tempfile
+with tempfile.NamedTemporaryFile(suffix='.jpeg')
 with tempfile.NamedTemporaryFile(suffix=".mp4") as f:
     fpath = f.name
-uploadVideo(
-    description="test video",
-    dynamic="nothing",
-    tagString="狗狗",
-    title="just a test {}".format(randomString),
-    videoPath=videoPath,
-    cover_path=cover_path,
-    multithread=True,
-)  # it is with credential right now.
+    uploadVideo(
+        description="test video",
+        dynamic="nothing",
+        tagString="狗狗",
+        title="just a test {}".format(randomString),
+        videoPath=videoPath,
+        cover_path=cover_path,
+        multithread=True,
+    )  # it is with credential right now.
