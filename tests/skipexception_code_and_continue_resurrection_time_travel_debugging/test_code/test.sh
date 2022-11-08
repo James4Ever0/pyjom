b3c467a25ee57d78399a5c76225b44b6b5c626f9
GitJournal <app@gitjournal.io>
2022-11-08 22:40:39 +0800

    [2022-11-08 14:40:36]
    FILE:
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_code/test.sh
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_code/test.sh
+++ tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_code/test.sh
@@ -5,3 +5,5 @@
 cat new_test.py | comby ':[prefix~@reloading.*$]def :[functionName](:[args]):'  'def :[functionName](:[args]):' -stdin -stdout -matcher .py |  comby "from reloading import reloading"  '' -stdin -stdout -matcher .py 
 
 # cat test.py | comby ':[prefix~(@.+)*$]def :[functionName](:[args]):'  ':[prefix] @reloading def :[functionName](:[args]):' -rule 'where match :[prefix] { | :[_@someRandomDecorator.*] -> true | :[_] -> false } ' -stdin -stdout -matcher .py -match-only
+
+cat /root/Desktop/works/pyjom/pyjom/platforms/bilibili/utils.py 
