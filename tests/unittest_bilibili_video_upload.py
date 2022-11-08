45212fa065a0e7ee4441a3709a5fc26306ed7d76
GitJournal <app@gitjournal.io>
2022-11-09 00:43:00 +0800

    [2022-11-08 16:42:57]
    FILE:
    tests/unittest_bilibili_video_upload.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_bilibili_video_upload.py
+++ tests/unittest_bilibili_video_upload.py
@@ -4,14 +4,17 @@ from pyjom.platforms.bilibili.uploader import uploadVideo
 import uuid
 
 randomString = str(uuid.uuid4())
-import ffmpeg
+# import ffmpeg
 # how about let's generate shit?
 # use multithread uploader instead of that.
 import tempfile
 with tempfile.NamedTemporaryFile(suffix='.jpeg') as pic:
     cover_path = pic.name
     with tempfile.NamedTemporaryFile(suffix=".mp4") as f:
-        videoPath = f.name
+        videoPath = f.namffmpeg -f lavfi -i nullsrc=s=1280x720 -filter_complex "geq=random(1)*255:128:128;aevalsrc=-2+random(0)" \
+-t 5 output.mkv
+        command = v
+        os.system(command)
         uploadVideo(
             description="test video",
             dynamic="nothing",
