038a4b65f5ff840a61768a1a4b1440041bead968
GitJournal <app@gitjournal.io>
2022-11-08 18:46:57 +0800

    [2022-11-08 10:46:55]
    FILE:
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_code/new_test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_code/new_test.py
+++ tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_code/new_test.py
@@ -7,10 +7,11 @@ from reloading import reloading
 def someFunction (a,b,c,d=1,f=2
 ):
     # not touching this function!
-    @decorator
-    def inner_function (h,i,j,
-    k):
-        return hjkl
+    # @decorator
+    # def inner_function (h,i,j,
+    # k):
+    abcdefg=1234
+    #     return hjkl
     return abcdefg # I need you to decorate this thing.
 
 async def shitfunction():
