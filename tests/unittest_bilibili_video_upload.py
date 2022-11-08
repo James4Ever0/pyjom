218bc56a8420f29253a944b621431573c93962bc
GitJournal <app@gitjournal.io>
2022-11-08 23:39:31 +0800

    [2022-11-08 15:39:23]
    FILE:
    tests/unittest_bilibili_video_upload.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_bilibili_video_upload.py
+++ tests/unittest_bilibili_video_upload.py
@@ -4,4 +4,5 @@ from pyjom.platforms.bilibili.uploader import uploadVideo
 import uuid
 
 randomString = str(uuid.uuid4())
-uploadVideo(description='test video',dynamic='nothing', tagString='狗狗',title='just a test {}'.format(randomString),videoPath=videoPath, cover_path=cover_path, ) # it is with credential right now.
+# use multithread uploader instead of that.
+uploadVideo(description='test video',dynamic='nothing', tagString='狗狗',title='just a test {}'.format(randomString),videoPath=videoPath, cover_path=cover_path, multithread=True) # it is with credential right now.
