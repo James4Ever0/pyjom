f82ea3015178c97ce152a82a08017964462c6109
GitJournal <app@gitjournal.io>
2022-11-08 16:28:25 +0800

    [2022-11-08 08:28:19]
    FILE:
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_code/test.sh
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_code/test.sh
+++ tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_code/test.sh
@@ -1,2 +1,2 @@
 # are you sensitive to return?
-cat test.py | comby ':[prefix~$]def :[functionName](:[args]):'  ':[prefix]:[~\n]@reloading def :[functionName](:[args]):' .py  -matcher .py 
+cat test.py | comby ':[prefix~$]def :[functionName](:[args]):'  ':[prefix]:[~\n]@reloading def :[functionName](:[args]):' 'where rewrite :[x] { a:[t]. -> :[t] }' .py  -matcher .py 
