0300354a3c29c980885be22c7f547ebf054bb5ee
GitJournal <app@gitjournal.io>
2022-11-08 17:54:14 +0800

    [2022-11-08 09:54:11]
    FILE:
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_code/rewrite.py
+++ tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_code/rewrite.py
@@ -1,9 +1,9 @@
 from comby import Comby
 
 comby = Comby()
+source_old = open('test.py','r').read()
 match = ':[prefix~$]def :[functionName](:[args]):'
 rewrite = ':[prefix]\n@reloading\ndef :[functionName](:[args]):'
-source_old = open('test.py','r').read()
 source_new = comby.rewrite(source_old, match, rewrite)
 # -> 'print("hello world")
 source_new = 'from reloading import reloading\n'+source_new
