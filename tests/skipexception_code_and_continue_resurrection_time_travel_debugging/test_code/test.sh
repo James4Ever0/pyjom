b309b1488d7eb686cf8a348622b9216ab69e1f8e
GitJournal <app@gitjournal.io>
2022-11-08 16:47:06 +0800

    [2022-11-08 08:47:03]
    FILE:
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_code/test.sh
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_code/test.sh
+++ tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_code/test.sh
@@ -2,5 +2,6 @@
 
 # you can first replace the code with the selected 
 
-cat test.py | comby ':[prefix~(@.+)*$]def :[functionName](:[args]):'  ':[prefix] @reloading def :[functionName](:[args]):' -rule 'where match :[prefix] { | :[_@someRandomDecorator] -> true | :[_] -> false } '-stdin -stdout -matcher .py 
+cat new_test.py | comby ':[prefix~(@.+)*$]def :[functionName](:[args]):'  ':[prefix] @reloading def :[functionName](:[args]):' -rule 'where match :[prefix] { | :[_@someRandomDecorator] -> true | :[_] -> false } '-stdin -stdout -matcher .py 
+
 # cat test.py | comby ':[prefix~(@.+)*$]def :[functionName](:[args]):'  ':[prefix] @reloading def :[functionName](:[args]):' -rule 'where match :[prefix] { | :[_@someRandomDecorator] -> true | :[_] -> false } '-stdin -stdout -matcher .py 
