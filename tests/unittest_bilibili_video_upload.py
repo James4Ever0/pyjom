54102ab8d41d41627e08e1d19fb461d9caf5a658
GitJournal <app@gitjournal.io>
2022-11-09 00:46:48 +0800

    [2022-11-08 16:46:46]
    FILE:
    tests/unittest_bilibili_video_upload.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_bilibili_video_upload.py
+++ tests/unittest_bilibili_video_upload.py
@@ -16,6 +16,7 @@ with tempfile.NamedTemporaryFile(suffix='.jpeg') as pic:
         videoPath = f.name
         command = f"""ffmpeg -f lavfi -i nullsrc=s=1280x720 -filter_complex "geq=random(1)*255:128:128;aevalsrc=-2+random(0)" -t {duration:.2f} {videoPath}"""
         os.system(command)
+        print('uploading video')
         uploadVideo(
             description="test video",
             dynamic="nothing",
