a28c0f2094119d783a667dac4105ce045b514075
GitJournal <app@gitjournal.io>
2022-11-08 16:11:39 +0800

    [2022-11-08 08:11:30]
    FILE:
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_code/test.sh
+++ tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_code/test.sh
@@ -1,2 +1,2 @@
 # are you sensitive to return?
-cat test.py | comby ':[$]def :[functionName](:[args]):'  .py -match-only -matcher .py
+cat test.py | comby ':[~$]def :[functionName](:[args]):'  .py -match-only -matcher .py
