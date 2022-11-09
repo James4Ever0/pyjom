fda1a0362e7374bc83fc02bb298fd0b8fb5c4c10
GitJournal <app@gitjournal.io>
2022-11-09 11:46:23 +0800

    [2022-11-09 03:46:21]
    FILE:
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_code/bowler_test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_code/bowler_test.py
+++ tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_code/bowler_test.py
@@ -20,7 +20,7 @@ pattern="""(
 )"""
 
 q = bowler.Query(src)
-f = q.select(pattern).is_def().diff(interactive=True)
+f = q.select(pattern).is_def().dump()
 print(f, dir(f))
 # for x in f:
 #     print(x)
