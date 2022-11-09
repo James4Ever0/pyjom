2395f39cf05de55354639d026f8b203721c894f7
GitJournal <app@gitjournal.io>
2022-11-09 19:52:58 +0800

    [2022-11-09 11:52:56]
    FILE:
    pyjom/platforms/bilibili/uploader.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/platforms/bilibili/uploader.py
+++ pyjom/platforms/bilibili/uploader.py
@@ -308,7 +308,7 @@ def videoMultithreadUploader(
             }
             params.update(metadata)
             # 版权判断, 转载无版权
-            copyright = 2 if params.get('source') else 1
+            params['copyright'] = 2 if params.get('source') else 1
             if source:
                 del params["no_reprint"]
             # tag设置
