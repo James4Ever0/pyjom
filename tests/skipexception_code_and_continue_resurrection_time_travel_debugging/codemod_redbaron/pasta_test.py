e8aaf6e8f0d3248fe2b1c170161da2ac1145c5d4
GitJournal <app@gitjournal.io>
2022-11-09 16:03:18 +0800

    [2022-11-09 08:03:14]
    FILE:
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta_test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta_test.py
+++ tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta_test.py
@@ -16,9 +16,7 @@ for i in range(len(tree)):
             del f.decorator_list[index]
         f.decorator_list.append(ast.Name('reloading')) #seems good?
     # ast.FunctionDef and ast.AsyncFunctionDef are different.
-    dec = f.decorator_list
-    # print(dec)
-    c0=pasta.dump(tree)
+c0=pasta.dump(tree)
     # print(dec[1].id) # now this is not a name object. it is a call object.
     # print("___")
     # print(c0)
