44c6cfbc9222b48c8ec9fdf1347e329dc896e1bf
GitJournal <app@gitjournal.io>
2022-11-09 15:22:24 +0800

    [2022-11-09 07:22:22]
    FILE:
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/test2.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/test2.py
+++ tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/test2.py
@@ -17,6 +17,6 @@ def b():
 async def shit():
     ...
 
-# not supported anywhere. not redbaron, not baron.
+# not supported anywhere. not redbaron, not pasta.
 # with (re() as a, re2() as b):
 #     print(fuck)
