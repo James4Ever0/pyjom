7e7b4ee922a173477ef92e93056a8c4072305c5f
GitJournal <app@gitjournal.io>
2022-11-09 20:38:36 +0800

    [2022-11-09 12:38:34]
    FILE:
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta_test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta_test.py
+++ tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta_test.py
@@ -3,33 +3,35 @@ import ast
 
 
 def recover_and_rewrite(c):
-    c = c.replace("from reloading import reloading\n","")
-    c = "from reloading import reloading\n"+c
-    tree=pasta.parse(c)
+    c = c.replace("from reloading import reloading\n", "")
+    c = "from reloading import reloading\n" + c
+    tree = pasta.parse(c)
     # print(dir(tree))
     for i in range(len(tree.body)):
         f = tree.body[i]
         if type(f) == ast.FunctionDef:
-            cached=False
+            cached = False
             removeList = []
             for index, elem in enumerate(f.decorator_list):
                 if type(elem) == ast.Name:
-                    if elem.id == 'reloading':
+                    if elem.id == "reloading":
                         removeList.append(index)
-                    elif 'lru_cache' in elem.id:
-                        cached=True
+                    elif "lru_cache" in elem.id: # are you sure you won't call that again?
+                        cached = True
                 elif type(elem) == ast.Call:
-                    if 'lru_cache' in elem.func.id:
-                        cached=True
+                    if "lru_cache" in elem.func.id:
+                        cached = True
             for index in removeList:
                 del f.decorator_list[index]
             # if len(f.decorator_list) == 0: # are you sure this will be ok?
-            f.decorator_list.append(ast.Name('reloading')) #seems good?
+            if not cached:
+                f.decorator_list.append(ast.Name("reloading"))  # seems good?
         # ast.FunctionDef and ast.AsyncFunctionDef are different.
-    c0=pasta.dump(tree)
+    c0 = pasta.dump(tree)
     return c0
 
+
 if __name__ == "__main__":
-    c=open("test2.py","r").read()
+    c = open("test2.py", "r").read()
     c0 = recover_and_rewrite(c)
     print(c0)
