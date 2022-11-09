35f4cbf072702f63924e917c98550f5873f006f3
GitJournal <app@gitjournal.io>
2022-11-09 11:33:16 +0800

    [2022-11-09 03:33:14]
    FILE:
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_code/bowler_test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_code/bowler_test.py
+++ tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_code/bowler_test.py
@@ -3,22 +3,22 @@ import bowler
 src ='test2.py'
 
 pattern="""
-        (
-            decorated=decorated<
-                decorators=decorators
-                function_def=funcdef<
-                    'def' function_name='{name}'
-                    function_parameters=parameters< '(' function_arguments=any* ')' >
-                    any*
-                >
-            >
-        |
-            function_def=funcdef<
-                'def' function_name='{name}'
-                function_parameters=parameters< '(' function_arguments=any* ')' >
-                any*
-            >
-    )
+(
+    decorated=decorated<
+        decorators=decorators
+        function_def=funcdef<
+            'def' function_name='{name}'
+            function_parameters=parameters< '(' function_arguments=any* ')' >
+            any*
+        >
+    >
+|
+    function_def=funcdef<
+        'def' function_name='{name}'
+        function_parameters=parameters< '(' function_arguments=any* ')' >
+        any*
+    >
+)
 """
 
 q = bowler.Query(src)
