ff2b6f7a870556dbc9a37d0c3ba14d3af965c92e
GitJournal <app@gitjournal.io>
2022-11-09 16:43:06 +0800

    [2022-11-09 08:43:04]
    FILE:
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_code/recover.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_code/recover.py
+++ tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_code/recover.py
@@ -1,20 +1,29 @@
 from comby import Comby
 
 comby = Comby()
+
+
 def recover(source_old):
-    kws = ['from reloading import reloading', "@reloading"]
+    kws = ["from reloading import reloading", "@reloading"]
     # source_old = source_old.replace(kw,"") # obliterate this thing. shall we?
-    source_old = [line for line in source_old.split("\n") if not any(line.startswith(elem) for elem in kws)]
+    source_old = "\n".join(
+        [
+            line
+            for line in source_old.split("\n")
+            if not any(line.startswith(elem) for elem in kws)
+        ]
+    )
 
-    match = ':[prefix~@reloading.*$]def :[functionName](:[args]):'
-    rewrite = 'def :[functionName](:[args]):'
+    match = ":[prefix~@reloading.*$]def :[functionName](:[args]):"
+    rewrite = "def :[functionName](:[args]):"
 
-    source_new = comby.rewrite(source_old, match, rewrite, language='.py')
+    source_new = comby.rewrite(source_old, match, rewrite, language=".py")
     return source_new
+
 
 if __name__ == "__main__":
     # comby = Comby()
 
-    source_old = open('new_test.py','r').read()
+    source_old = open("new_test.py", "r").read()
     source_new = recover(source_old)
     print(source_new)
