5f94a955a7995948f8b64ec13a7f4f62828f18ac
GitJournal <app@gitjournal.io>
2022-11-09 16:40:31 +0800

    [2022-11-09 08:40:29]
    FILE:
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_code/pyjom_recover_and_rewrite.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_code/pyjom_recover_and_rewrite.py
+++ tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_code/pyjom_recover_and_rewrite.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append("/root/Desktop/works/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron")
-from pasta_test import recover_and_rewrite
-from recover_and_rewrite import recover_and_rewrite
+from pasta_test import recover_and_rewrite as rar1
+from recover_and_rewrite import recover_and_rewrite as rar2
 
 if __name__ == "__main__":
     import os
@@ -16,6 +16,7 @@ if __name__ == "__main__":
             source_old = f.read()
             if len(source_old) < 20 or "\ndef " not in source_old:
                 return
+            try:
             source_new = recover_and_rewrite(source_old)
         with open(path, "w+") as f:
             f.write(source_new)
