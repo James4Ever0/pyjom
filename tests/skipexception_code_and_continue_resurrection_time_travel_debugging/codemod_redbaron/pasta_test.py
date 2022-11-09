4172694faf1496c67444bc1d03f40413940894b0
GitJournal <app@gitjournal.io>
2022-11-09 15:57:19 +0800

    [2022-11-09 07:57:17]
    FILE:
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta_test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta_test.py
+++ tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta_test.py
@@ -7,14 +7,14 @@ tree=pasta.parse(c)
 f=tree.body[0]
 # print(dir(f))
 # del f.decorator_list[0]
-f.decorator_list.append(ast.Name('newdec')) #seems good?
+# f.decorator_list.append(ast.Name('newdec')) #seems good?
 # ast.FunctionDef and ast.AsyncFunctionDef are different.
 dec = f.decorator_list
-print(dec)
+# print(dec)
 c0=pasta.dump(tree)
 # print(dec[1].id) # now this is not a name object. it is a call object.
-print("___")
-print(c0)
+# print("___")
+# print(c0)
 # ast.FunctionDef 
 
 if __name__ == "__main__":
