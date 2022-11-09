9dfb031eab1116a67901692b75b82a16ce9513c7
GitJournal <app@gitjournal.io>
2022-11-09 20:09:24 +0800

    [2022-11-09 12:09:14]
    FILE:
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/platforms/bilibili/uploader.py
+++ pyjom/platforms/bilibili/uploader.py
@@ -55,6 +55,7 @@ import traceback
 
 
 # you better embed it inside your function? what a creep?
+# but that will 
 class MultithreadUploader(object):
     ## what is the cookie string look like?
     def __init__(self, cookie_string):
