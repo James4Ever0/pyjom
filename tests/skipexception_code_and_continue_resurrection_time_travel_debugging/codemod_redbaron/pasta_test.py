a9ccfdcb49428a6cad155d56cc542c7b5828822e
GitJournal <app@gitjournal.io>
2022-11-09 15:17:10 +0800

    [2022-11-09 07:17:08]
    FILE:
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta_test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta_test.py
+++ tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta_test.py
@@ -6,9 +6,13 @@ tree=pasta.parse(c)
 print(dir(tree))
 f=tree.body[0]
 print(dir(f))
+import ast
+f.decorator_list.append(ast.Name('newdec')) #seems good?
+
 dec = f.decorator_list
 
 print(dec)
-#c0=pasta.dump(tree)
-print(dec[1].id) # now this is not a name object. it is a call object.
-#print(c0)
+c0=pasta.dump(tree)
+# print(dec[1].id) # now this is not a name object. it is a call object.
+print("___")
+print(c0)
