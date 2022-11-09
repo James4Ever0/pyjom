9e5668b6ed492272398704225a9fd7e8cd79e777
GitJournal <app@gitjournal.io>
2022-11-09 16:04:29 +0800

    [2022-11-09 08:04:24]
    FILE:
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta_test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta_test.py
+++ tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta_test.py
@@ -4,21 +4,23 @@ import ast
 
 def recover_and_rewrite(c):
     c = c.replace("from reloading import reloading\n","")
-tree=pasta.parse(c)
-# print(dir(tree))
-for i in range(len(tree.body)):
-    f = tree.body[i]
-    if type(f) == ast.FunctionDef:
-        removeList = []
-        for index, elem in f.decorator_list:
-            if type(elem) == ast.Name:
-                if elem.id == 'reloading':
-                    removeList.append(index)
-        for index in removeList:
-            del f.decorator_list[index]
-        f.decorator_list.append(ast.Name('reloading')) #seems good?
-    # ast.FunctionDef and ast.AsyncFunctionDef are different.
-c0=pasta.dump(tree)
+    c = "from reloading import reloading\n"+c
+    tree=pasta.parse(c)
+    # print(dir(tree))
+    for i in range(len(tree.body)):
+        f = tree.body[i]
+        if type(f) == ast.FunctionDef:
+            removeList = []
+            for index, elem in f.decorator_list:
+                if type(elem) == ast.Name:
+                    if elem.id == 'reloading':
+                        removeList.append(index)
+            for index in removeList:
+                del f.decorator_list[index]
+            f.decorator_list.append(ast.Name('reloading')) #seems good?
+        # ast.FunctionDef and ast.AsyncFunctionDef are different.
+    c0=pasta.dump(tree)
 
 if __name__ == "__main__":
     c=open("test2.py","r").read()
+    
