96dccf88e17b23c268378157dd4c2d7ad23563e8
GitJournal <app@gitjournal.io>
2022-11-11 02:43:24 +0800

    [2022-11-10 18:41:42]
    FILE:
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/test.py
+++ tests/bilibili_video_recommendation_server/test.py
@@ -1,6 +1,7 @@
 from reloading import reloading
 from lazero.utils.json import jsonify
 
+ellipsis = type(...)
 # serve my video, serve my cat video, dog video, set priority, serve others video
 # by means of query? or just directly ask me for it.
 
