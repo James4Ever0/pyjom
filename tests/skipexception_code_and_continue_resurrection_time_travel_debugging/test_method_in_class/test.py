daa6d7c2d136fc289a7777f30be4ef41cd6d318b
GitJournal <app@gitjournal.io>
2022-11-10 16:59:20 +0800

    [2022-11-10 08:59:17]
    FILE:
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_method_in_class/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_method_in_class/test.py
+++ tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_method_in_class/test.py
@@ -5,7 +5,7 @@ class a:
     def someMethod(self):
         # @reloading # this will not work.
         def someothermethod():
-            raise Exception('another shit')
+            raise Exception('just another shit')
 
         # raise Exception('just another shit')
         val = someothermethod()
