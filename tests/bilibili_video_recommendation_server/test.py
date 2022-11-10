7d3476e46ab668647ceff738aa1590bc1d57058a
GitJournal <app@gitjournal.io>
2022-11-11 03:49:32 +0800

    [2022-11-10 19:49:29]
    FILE:
    tests/bilibili_video_recommendation_server/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/test.py
+++ tests/bilibili_video_recommendation_server/test.py
@@ -629,10 +629,10 @@ def searchRegisteredVideosAndGetResultList(
         # condition &= BilibiliVideo.poster.rel_field in [int(userid) for userid in dedeuserids]
     # vlist = [v for v in (BilibiliVideo.select().where(condition) or [])]
     # http://docs.peewee-orm.com/en/latest/peewee/relationships.html#relationships
-    vgen = BilibiliVideo.select(BilibiliVideo.id).where(condition)
+    vgen = BilibiliVideo.select(BilibiliVideo.id, BilibiliVideo.poster).where(condition)
     # breakpoint()
     if dedeuserids:
-        vgen = vgen.join(Bilibi)
+        vgen = vgen.join(Bilibili)
     user_video_ids = [
         v.id for v in vgen or []
     ]
