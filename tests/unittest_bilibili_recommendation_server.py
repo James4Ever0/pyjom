1bad24a8450ef513118d9a4eb51416f98e7ae54f
GitJournal <app@gitjournal.io>
2022-11-10 14:16:03 +0800

    [2022-11-10 06:16:00]
    FILE:
    tests/unittest_bilibili_recommendation_server.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_bilibili_recommendation_server.py
+++ tests/unittest_bilibili_recommendation_server.py
@@ -3,6 +3,6 @@ import requests
 port = 7341
 baseurl = "http://localhost:{}".format(port)
 
-params = {}
+params = {'params':{''}}
 r = requests.get(baseurl+"/searchVideos", params=params)
 print(r.text)
