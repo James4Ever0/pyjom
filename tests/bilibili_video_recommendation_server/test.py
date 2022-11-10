b0e28d83e7ad934872fff23d7ecb72774b035c9d
GitJournal <app@gitjournal.io>
2022-11-11 00:05:21 +0800

    [2022-11-10 16:05:18]
    FILE:
    tests/bilibili_video_recommendation_server/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/test.py
+++ tests/bilibili_video_recommendation_server/test.py
@@ -946,7 +946,7 @@ def search_videos(form: searchVideoForm):
 @app.get("/searchRegisteredVideos")
 @reloading
 def search_registered_videos():
-    searchRegisteredVideos(form.keyword, form.tid, form.dedeuserid,form.videoOrder,)
+    videoList = [video for searchRegisteredVideos(form.keyword, form.tid, form.dedeuserid,form.videoOrder,form.limit)
 
 @app.get("/searchUserVideos")
 @reloading
