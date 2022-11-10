085f9fefc7f229d293dc31b75631c1f751daf9d8
GitJournal <app@gitjournal.io>
2022-11-11 04:47:56 +0800

    [2022-11-10 20:47:53]
    FILE:
    tests/unittest_bilibili_recommendation_server.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_bilibili_recommendation_server.py
+++ tests/unittest_bilibili_recommendation_server.py
@@ -28,9 +28,8 @@ elif objective == "searchRegisteredVideos":
     # print(j)
     # exit()
 elif objective == "searchUserVideos":
-    params = dict(query='猫',method='bm25')
+    params = dict(query="猫", method="bm25", videoOrder="click")
     # params = dict(query='猫',method='bm25')
-    videoOrder="click"
     # params = dict(query='猫',method='bm25', dedeuserid=None)
 else:
     raise Exception("invalid objective: %s" % objective)
