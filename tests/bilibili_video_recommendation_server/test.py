ec37ea5c3e727547e301842d226f02299831fd54
GitJournal <app@gitjournal.io>
2022-11-11 04:14:33 +0800

    [2022-11-10 20:14:31]
    FILE:
    tests/bilibili_video_recommendation_server/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/test.py
+++ tests/bilibili_video_recommendation_server/test.py
@@ -638,7 +638,7 @@ def searchRegisteredVideosAndGetResultList(
     else:
         vgen = vgen.where(condition)
     user_video_ids = [v.id for v in vgen or []]
-    print('user of videos',user_video_ids)
+    print('user of videos',set([v.poster.]))
 
     results = (
         BilibiliVideoIndex.search_bm25(keyword)
