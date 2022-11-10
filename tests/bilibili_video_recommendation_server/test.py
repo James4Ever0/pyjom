83cfcf8d5be679814587bcb34b7afcfd4aac114a
GitJournal <app@gitjournal.io>
2022-11-11 03:29:00 +0800

    [2022-11-10 19:28:57]
    FILE:
    tests/bilibili_video_recommendation_server/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/test.py
+++ tests/bilibili_video_recommendation_server/test.py
@@ -619,10 +619,11 @@ def searchRegisteredVideosAndGetResultList(
         # print("DEDEUSERIDS:", dedeuserids)
         # from functools import reduce
 
-        condition &= reduce(
-            lambda a, b: a | b,
-            [BilibiliVideo.poster in registerUser(userid) for userid in dedeuserids],
-        )
+        # condition &= reduce(
+        #     lambda a, b: a | b,
+        #     [BilibiliVideo.poster in registerUser(userid) for userid in dedeuserids],
+        # )
+        condition &= [BilibiliVideo.poster in registerUser(userid) for userid in dedeuserids]
     user_video_ids = [
         v.id for v in (BilibiliVideo.select(BilibiliVideo.id).where(condition) or [])
     ]
