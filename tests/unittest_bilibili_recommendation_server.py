ec4e877e2dafacd104ea9709526f1d7af7519e37
GitJournal <app@gitjournal.io>
2022-11-11 04:57:57 +0800

    [2022-11-10 20:57:54]
    FILE:
    tests/unittest_bilibili_recommendation_server.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/unittest_bilibili_recommendation_server.py
+++ tests/unittest_bilibili_recommendation_server.py
@@ -32,7 +32,7 @@ elif objective == "searchUserVideos":
     # params = dict(query="猫", method="bm25", videoOrder="click")
     params = dict(query='猫',method='bm25')
     # params = dict(query='猫',method='bm25', dedeuserid=None)
-    params=dict(bvid="BV1MN4y1P7mq",dedeuserid=, is_mine=F, form.visible)
+    params=dict(bvid="BV1MN4y1P7mq",dedeuserid=, is_mine=False, visible)
 else:
     raise Exception("invalid objective: %s" % objective)
 
