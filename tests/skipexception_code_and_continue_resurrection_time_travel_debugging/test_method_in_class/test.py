0c329fb8ed5c31f3539c45125e7cb7fa565976ac
GitJournal <app@gitjournal.io>
2022-11-10 19:32:10 +0800

    [2022-11-10 11:32:07]
    FILE:
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_method_in_class/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_method_in_class/test.py
+++ tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_method_in_class/test.py
@@ -1,42 +1,49 @@
 from reloading import reloading
 import asyncio
 
+
 @reloading
 class mClass:
     someValue = 2
+
     @reloading
     def someMethod(self):
         @reloading
         def someInnerMethod():
-            raise Exception('exception1')
-            return 'inside function return'
-        raise Exception('exception2')
+            raise Exception("exception1")
+            return "inside function return"
+
+        raise Exception("exception2")
         val = someInnerMethod()
         return val
+
     @reloading
     async def someOtherMethod(self):
         # raise Exception('async exception1')
         @reloading
         async def asyncInside():
-            raise Exception('async exception2')
-            return 'async inside return'
+            raise Exception("async exception2")
+            return "async inside return"
+
         val = await asyncInside()
         return val
+
     @reloading
     def runAsync(self):
-        loop= asyncio.get_event_loop()
+        loop = asyncio.get_event_loop()
         val = loop.run_until_complete(self.someOtherMethod())
-        print('value from async func:', val)
+        print("value from async func:", val)
         return val
+
 
 @reloading
 def main():
     MClass = mClass()
     print(MClass)
     val = MClass.someMethod()
-    print('return value:', val)
+    print("return value:", val)
     val = MClass.runAsync()
-    print('return async value:', val)
-    print('success!')
+    print("return async value:", val)
+    print("success!")
 
 main()
