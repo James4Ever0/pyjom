acdc04a20ee36a06108004a44dd0bdf680dfd004
GitJournal <app@gitjournal.io>
2022-11-10 14:15:11 +0800

    [2022-11-10 06:15:07]
    FILE:
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/test.py
+++ tests/bilibili_video_recommendation_server/test.py
@@ -890,7 +890,7 @@ if __name__ == "__main__":
         # just asking. post or get?
         @app.get("/searchVideos")  # what do you want to have? all fields?
         def search_videos(
-            query: str, iterate: bool = False, page_start: int = 1, params={}
+            query: str, iterate: bool = False, page_start: int = 1, params:dict={}
         ):
             params = {"duration": BSP.all.duration._10分钟以下}.update(params) # this is default parameter.
             for v in searchAndRegisterVideos(
