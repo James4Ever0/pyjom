7cf97fdff75a2fc72e3d6f1d9d6ac20f7fecf3e6
GitJournal <app@gitjournal.io>
2022-11-10 17:09:49 +0800

    [2022-11-10 09:09:45]
    FILE:
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_method_in_class/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_method_in_class/test.py
+++ tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_method_in_class/test.py
@@ -12,7 +12,8 @@ class a:
         return val
     @reloading
     async def someOtherMethod(self): # cannot decorate async function?
-        raise Exception('just another shit')
+        # raise Exception('just another shit')
+        return val
     @reloading
     def runAsync(self):
         loop= asyncio.get_event_loop()
