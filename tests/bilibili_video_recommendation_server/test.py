946d78c32d737e9b3df0c8558df9287f27daea7d
GitJournal <app@gitjournal.io>
2022-11-11 00:18:39 +0800

    [2022-11-10 16:18:36]
    FILE:
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/test.py
+++ tests/bilibili_video_recommendation_server/test.py
@@ -1012,7 +1012,7 @@ def search_user_videos(form: searchUserVideoForm):
 @app.get("/registerUserVideo")
 @reloading
 def register_user_video():
-    registerUserVideo()
+    registerUserVideo(form.bvid, form.dedeuserid, form.is_mine, form.visible)
 
 
 # you should recommend by label instead of by name. but whatever.
