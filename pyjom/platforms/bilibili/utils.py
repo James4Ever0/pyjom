73a104657921807785ca7e458fd343056263d30f
GitJournal <app@gitjournal.io>
2022-11-10 15:15:31 +0800

    [2022-11-10 07:15:23]
    FILE:
    pyjom/platforms/bilibili/utils.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/platforms/bilibili/utils.py
+++ pyjom/platforms/bilibili/utils.py
@@ -96,7 +96,7 @@ import re
 def videoDurationStringToSeconds(durationString, method: Literal["vtc", "basic"] = "vtc"):
     if durationString in ["-", None]:
         return None
-    if re.findall(r'\d',durationString) ==0:
+    if re.findall(r'\d',durationString) ==[]:
         return NOne
     if method == "vtc":
         import vtc
