0bb9ed331098da5769c7e4b51557c4666239dff6
GitJournal <app@gitjournal.io>
2022-11-10 17:21:55 +0800

    [2022-11-10 09:21:53]
    FILE:
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_method_in_class/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_method_in_class/test.py
+++ tests/skipexception_code_and_continue_resurrection_time_travel_debugging/test_method_in_class/test.py
@@ -7,9 +7,29 @@ def mreload(mclass):
             print('good')
     return newclass
 
+class Mydecorator:
+    #accept the class as argument
+    def __init__(self, student):
+        self.student = student
+    
+    #accept the class's __init__ method arguments
+    def __call__(self, name):
+        #define a new display method
+        def newdisplay(self):
+            print('Name: ', self.name)
+            print('Subject: Python')
+            
+        #replace display with newdisplay
+        self.student.display = newdisplay
+        
+        #return the instance of the class
+        obj = self.student(name)
+        return obj
+
 # @mreload
+@Mydecorator
 class a:
-    # raise Exception('shit')
+    raise Exception('shit')
     # cannot raise exception here. the compiler will complain.
     # good?
     @reloading # does this work?
