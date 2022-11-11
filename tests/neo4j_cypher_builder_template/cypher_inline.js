7d09bee64c59e2e597c0abcf5f0abe0dd84a4f8c
GitJournal <app@gitjournal.io>
2022-11-11 14:42:48 +0800

    [2022-11-11 06:42:46]
    FILE:
    tests/neo4j_cypher_builder_template/cypher_inline.js
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/neo4j_cypher_builder_template/cypher_inline.js
+++ tests/neo4j_cypher_builder_template/cypher_inline.js
@@ -2,10 +2,11 @@ var cypher = function(str) { return str; }
 var sql = function(str) { return str; }
 var a = cypher `create (n)-[:married]->(r)`; // well that's good.
 console.log(a);
-const query = sql`SELECT * FROM users`;
+const query = sql `SELECT * FROM users`;
 console.log(query);
-function myfunc(){
+
+function myfunc() {
     return query;
 }
 // __export__
-module.exports ={myfunc:myfunc}
+module.exports = { myfunc: myfunc }
