8bc0966fc245e69d79ea12327de52077f1d0636e
GitJournal <app@gitjournal.io>
2022-11-11 20:07:00 +0800

    [2022-11-11 12:06:57]
    FILE:
    pyjom/platforms/bilibili/database.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/platforms/bilibili/database.py
+++ pyjom/platforms/bilibili/database.py
@@ -1020,7 +1020,7 @@ def bilibiliRecommendationServer(
         # you are going to inherit this.
 
         @property
-        def query_for_search(self): # make sure the preprocessing is only called once.
+        def query_for_search(self): # make sure the preprocessing is only called once. really?
             if self.query_for_search_cached is None:
                 query = self.query
                 self.query_for_search_cached = textPreprocessing(query)
