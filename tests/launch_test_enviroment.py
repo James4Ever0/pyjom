1b9f2b3a360d3d492423a7e17503c56e58e2bbb5
GitJournal <app@gitjournal.io>
2022-11-11 20:24:48 +0800

    [2022-11-11 12:24:44]
    FILE:
    tests/launch_test_enviroment.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/launch_test_enviroment.py
+++ tests/launch_test_enviroment.py
@@ -24,13 +24,13 @@ pyjom_directory = "/root/Desktop/works/pyjom"
 pyjom_tests = os.path.join(pyjom_directory, "tests")
 
 # launch billibili recommendation server
-[os.path.join(pyjom_tests,"/bilibili_video_recommendation_server","python3","test.py"],{}
+["/root/Desktop/works/pyjom/tests/bilibili_video_recommendation_server","python3","test.py"],{}
 
 # launch qq cqhttp
-[os.path.join(pyjom_tests,"/qq_go_cqhttp","bash","launch.sh"],{}
+["/root/Desktop/works/pyjom/tests/qq_go_cqhttp","bash","launch.sh"],{}
 
 # make sure milvus is running.
-[os.path.join(pyjom_tests,"/video_phash_deduplication/","bash","config_milvus.sh",dict(no_terminal=True)
+["/root/Desktop/works/pyjom/tests/video_phash_deduplication/","bash","config_milvus.sh",dict(no_terminal=True)
 
 # launch netease api server. we need it to download new music, currently.
 # video phash is the last step among all filters.
@@ -39,12 +39,12 @@ pyjom_tests = os.path.join(pyjom_directory, "tests")
 
 # how to check avaliability of netease cloud music api?
 
-[os.path.join(pyjom_tests,"/karaoke_effects/","bash","load_translator.sh"]
+["/root/Desktop/works/pyjom/tests/karaoke_effects/","bash","load_translator.sh"]
 
-[os.path.join(pyjom_tests,"/redis_music_info_persistance","bash", "launch_redis.sh"],dict(sleep=1)
-
-
-[os.path.join(pyjom_tests,"/random_giphy_gifs/","node","nodejs_server.js"],dict(sleep=1)
+["/root/Desktop/works/pyjom/tests/redis_music_info_persistance","bash", "launch_redis.sh"],dict(sleep=1)
 
 
-[os.path.join(pyjom_tests,"/nsfw_violence_drug_detection","node","nsfwjs_test.js"],dict(sleep=1)
+["/root/Desktop/works/pyjom/tests/random_giphy_gifs/","node","nodejs_server.js"],dict(sleep=1)
+
+
+["/root/Desktop/works/pyjom/tests/nsfw_violence_drug_detection","node","nsfwjs_test.js"],dict(sleep=1)
