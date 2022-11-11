f454858e2d480cbf00faa79462456959aac5a882
GitJournal <app@gitjournal.io>
2022-11-11 20:10:46 +0800

    [2022-11-11 12:10:41]
    FILE:
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

---
+++ tests/launch_test_enviroment copy.sh
@@ -0,0 +1,33 @@
+# launch billibili recommendation server
+cd /root/Desktop/works/pyjom/tests/bilibili_video_recommendation_server
+gnome-terminal -- python3 /root/Desktop/works/pyjom/tests/bilibili_video_recommendation_server/test.py
+
+# launch qq cqhttp
+cd /root/Desktop/works/pyjom/tests/qq_go_cqhttp
+gnome-terminal -- bash /root/Desktop/works/pyjom/tests/qq_go_cqhttp/launch.sh
+
+# make sure milvus is running.
+cd /root/Desktop/works/pyjom/tests/video_phash_deduplication/
+bash /root/Desktop/works/pyjom/tests/video_phash_deduplication/config_milvus.sh
+
+# launch netease api server. we need it to download new music, currently.
+# video phash is the last step among all filters.
+cd /root/Desktop/works/pyjom/externals/NeteaseCloudMusicApi
+gnome-terminal -- bash /root/Desktop/works/pyjom/externals/NeteaseCloudMusicApi/launch.sh # port is 4042. port 4000 is used. don't know why.
+
+# how to check avaliability of netease cloud music api?
+
+cd /root/Desktop/works/pyjom/tests/karaoke_effects/
+gnome-terminal -- bash /root/Desktop/works/pyjom/tests/karaoke_effects/load_translator.sh
+
+sleep 1
+cd /root/Desktop/works/pyjom/tests/redis_music_info_persistance
+gnome-terminal -- bash /root/Desktop/works/pyjom/tests/redis_music_info_persistance/launch_redis.sh
+
+sleep 1
+cd /root/Desktop/works/pyjom/tests/random_giphy_gifs/
+gnome-terminal -- node /root/Desktop/works/pyjom/tests/random_giphy_gifs/nodejs_server.js
+
+sleep 1
+cd /root/Desktop/works/pyjom/tests/nsfw_violence_drug_detection
+gnome-terminal -- node /root/Desktop/works/pyjom/tests/nsfw_violence_drug_detection/nsfwjs_test.js
