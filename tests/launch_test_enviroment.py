8f79d47b409c09996287e04d7015453cffd842bd
GitJournal <app@gitjournal.io>
2022-11-11 20:27:16 +0800

    [2022-11-11 12:27:13]
    FILE:
    tests/launch_test_enviroment.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/launch_test_enviroment.py
+++ tests/launch_test_enviroment.py
@@ -40,13 +40,13 @@ launchList = [[[os.path.join(pyjom_tests,"bilibili_video_recommendation_server")
 ,
 # how to check avaliability of netease cloud music api?
 [
-[os.path.join(pyjom_tests,"karaoke_effects"),"bash","load_translator.sh"],{}]
+[os.path.join(pyjom_tests,"karaoke_effects"),"bash","load_translator.sh"],{}],
 [
 [os.path.join(pyjom_tests,"redis_music_info_persistance"),"bash", "launch_redis.sh"],dict(sleep=1)
-]
+],
 [
 [os.path.join(pyjom_tests,"random_giphy_gifs"),"node","nodejs_server.js"],dict(sleep=1)]
 
-
+,
 [[os.path.join(pyjom_tests,"nsfw_violence_drug_detection"),"node","nsfwjs_test.js"],dict(sleep=1)],
 ]
