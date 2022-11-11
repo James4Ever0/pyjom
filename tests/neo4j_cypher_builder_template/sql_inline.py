364a22663aa6041785bba127d9c0fc12c781d050
GitJournal <app@gitjournal.io>
2022-11-11 14:39:05 +0800

    [2022-11-11 06:39:02]
    FILE:
    tests/neo4j_cypher_builder_template/sql_inline.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/neo4j_cypher_builder_template/sql_inline.py
+++ tests/neo4j_cypher_builder_template/sql_inline.py
@@ -4,6 +4,6 @@ a="select * from user"
 b="create (n:person)" # cypher # not working!
 from javascript import require, globalThis
 
-chalk, fs = require("cypher_inline")
+chalk = require("cypher_inline")
 
- 
+print(dir(chalk))
