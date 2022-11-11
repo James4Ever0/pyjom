bc1a84dd24787dbd24827f41bf569745a552e102
GitJournal <app@gitjournal.io>
2022-11-11 20:17:37 +0800

    [2022-11-11 12:17:28]
    FILE:
    tests/launch_test_enviroment.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/launch_test_enviroment.py
+++ tests/launch_test_enviroment.py
@@ -4,8 +4,11 @@ import os
 def launchProgramWithTerminal(directory, intepreter,executable, sleep=None, no_terminal=False):
     if sleep:
         time.sleep(sleep)
+    directory = os.path.abspath(directory)
+    assert os.path.exists(directory)
     os.chdir(directory)
     executable_path = os.path.join(directory, executable)
+    assert 
     command = f'{"gnome-terminal -- " if not no_terminal else ""}{intepreter} {executable_path}'
     os.system(command)
 
