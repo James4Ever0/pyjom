4ce17bb47b2096ddb40305f1b3efb5d4dde9436f
GitJournal <app@gitjournal.io>
2022-11-11 19:22:09 +0800

    [2022-11-11 11:22:06]
    FILE:
    tests/bilibili_video_recommendation_server/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/bilibili_video_recommendation_server/test.py
+++ tests/bilibili_video_recommendation_server/test.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.append("/root/Desktop/works/pyjom/")
-from pyjom.platforms.bilibili.database import bilibiliRecommendationServer,bootstrap, textPreprocessing,searchUserVideos,
+from pyjom.platforms.bilibili.database import bilibiliRecommendationServer,bootstrap, textPreprocessing,searchUserVideos,registerUserVideo,
 
 # you should recommend by label instead of by name. but whatever.
 if __name__ == "__main__":
