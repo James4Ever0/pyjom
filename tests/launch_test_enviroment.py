4ca70cfef2fa426167e6b3d22498f4f3eec7f508
GitJournal <app@gitjournal.io>
2022-11-11 20:28:10 +0800

    [2022-11-11 12:28:06]
    FILE:
    tests/launch_test_enviroment.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/launch_test_enviroment.py
+++ tests/launch_test_enviroment.py
@@ -1,7 +1,10 @@
 import time
 import os
 
-def launchProgramWithTerminal(directory, intepreter,executable, sleep=None, no_terminal=False):
+
+def launchProgramWithTerminal(
+    directory, intepreter, executable, sleep=None, no_terminal=False
+):
     try:
         if sleep:
             time.sleep(sleep)
@@ -11,14 +14,18 @@ def launchProgramWithTerminal(directory, intepreter,executable, sleep=None, no_t
         executable_path = os.path.join(directory, executable)
         assert os.path.exists(executable_path)
         command = f'{"gnome-terminal -- " if not no_terminal else ""}{intepreter} {executable_path}'
-        print('executing command:', command)
+        print("executing command:", command)
         os.system(command)
     except:
         import traceback
+
         traceback.print_exc()
-        print('failed while launching program with parameters:')
-        print(f"[D]:{directory}\n[I]{intepreter}\n[E]{executable}\n[C]{dict(sleep=sleep, no_terminal=no_terminal)}")
+        print("failed while launching program with parameters:")
+        print(
+            f"[D]:{directory}\n[I]{intepreter}\n[E]{executable}\n[C]{dict(sleep=sleep, no_terminal=no_terminal)}"
+        )
         breakpoint()
+
 
 pyjom_directory = "/root/Desktop/works/pyjom"
 pyjom_tests = os.path.join(pyjom_directory, "tests")
@@ -26,29 +33,51 @@ pyjom_externals = os.path.join(pyjom_directory, "externals")
 
 # launch billibili recommendation server
 launchList = [
-    [[os.path.join(pyjom_tests,"bilibili_video_recommendation_server"),"python3","test.py"],{}
-    ]
-,
-# launch qq cqhttp
-[[os.path.join(pyjom_tests,"qq_go_cqhttp"),"bash","launch.sh"],{}
-],
-# make sure milvus is running.
-[[os.path.join(pyjom_tests,"video_phash_deduplication"),"bash","config_milvus.sh"],dict(no_terminal=True)]
-,
-# launch netease api server. we need it to download new music, currently.
-# video phash is the last step among all filters.
-[[os.path.join(pyjom_externals,"NeteaseCloudMusicApi"),
-"bash","launch.sh"],{} ]# port is 4042. port 4000 is used. don't know why.
-,
-# how to check avaliability of netease cloud music api?
-[
-[os.path.join(pyjom_tests,"karaoke_effects"),"bash","load_translator.sh"],{}],
-[
-[os.path.join(pyjom_tests,"redis_music_info_persistance"),"bash", "launch_redis.sh"],dict(sleep=1)
-],
-[
-[os.path.join(pyjom_tests,"random_giphy_gifs"),"node","nodejs_server.js"],dict(sleep=1)]
-
-,
-[[os.path.join(pyjom_tests,"nsfw_violence_drug_detection"),"node","nsfwjs_test.js"],dict(sleep=1)],
+    [
+        [
+            os.path.join(pyjom_tests, "bilibili_video_recommendation_server"),
+            "python3",
+            "test.py",
+        ],
+        {},
+    ],
+    # launch qq cqhttp
+    [[os.path.join(pyjom_tests, "qq_go_cqhttp"), "bash", "launch.sh"], {}],
+    # make sure milvus is running.
+    [
+        [
+            os.path.join(pyjom_tests, "video_phash_deduplication"),
+            "bash",
+            "config_milvus.sh",
+        ],
+        dict(no_terminal=True),
+    ],
+    # launch netease api server. we need it to download new music, currently.
+    # video phash is the last step among all filters.
+    [
+        [os.path.join(pyjom_externals, "NeteaseCloudMusicApi"), "bash", "launch.sh"],
+        {},
+    ],  # port is 4042. port 4000 is used. don't know why.
+    # how to check avaliability of netease cloud music api?
+    [[os.path.join(pyjom_tests, "karaoke_effects"), "bash", "load_translator.sh"], {}],
+    [
+        [
+            os.path.join(pyjom_tests, "redis_music_info_persistance"),
+            "bash",
+            "launch_redis.sh",
+        ],
+        dict(sleep=1),
+    ],
+    [
+        [os.path.join(pyjom_tests, "random_giphy_gifs"), "node", "nodejs_server.js"],
+        dict(sleep=1),
+    ],
+    [
+        [
+            os.path.join(pyjom_tests, "nsfw_violence_drug_detection"),
+            "node",
+            "nsfwjs_test.js",
+        ],
+        dict(sleep=1),
+    ],
 ]
