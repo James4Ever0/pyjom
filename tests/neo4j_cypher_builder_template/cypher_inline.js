1d65ea917ee07d5b9f8640fefa5174c86b5c49bb
GitJournal <app@gitjournal.io>
2022-11-11 15:52:36 +0800

    [2022-11-11 07:52:34]
    FILE:
    tests/neo4j_cypher_builder_template/cypher_inline.js
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/neo4j_cypher_builder_template/cypher_inline.js
+++ tests/neo4j_cypher_builder_template/cypher_inline.js
@@ -1,6 +1,6 @@
 var cypher = function(str) { return str; }
 var sql = function(str) { return str; }
-var a = cypher `create (n)-[:married]->(r)`; // well that's good.
+var a = cypher `create (n)-[:married]->(r) ${myexpression}`; // well that's good.
 console.log(a);
 const query = sql `SELECT * FROM users`;
 console.log(query);
