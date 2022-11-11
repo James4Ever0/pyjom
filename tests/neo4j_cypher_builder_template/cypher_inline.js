50e944b2e1c04551d93078e00c1f218f6c7f699e
GitJournal <app@gitjournal.io>
2022-11-11 15:59:10 +0800

    [2022-11-11 07:59:07]
    FILE:
    tests/neo4j_cypher_builder_template/cypher_inline.js
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/neo4j_cypher_builder_template/cypher_inline.js
+++ tests/neo4j_cypher_builder_template/cypher_inline.js
@@ -1,5 +1,6 @@
 var cypher = function(strArray, opt, opt1) { // this is bad.
     console.log('input:', strArray, opt, opt1) // here we've got the thing.
+    // what will be your 
     return strArray; }
 var sql = function(str) { return str; }
 var myexpression = '2'; // not supplied to cypher?
