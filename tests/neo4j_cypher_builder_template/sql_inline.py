909a2fe29571cc0507812e3572d7418b584f578f
GitJournal <app@gitjournal.io>
2022-11-11 14:46:50 +0800

    [2022-11-11 06:46:48]
    FILE:
    tests/neo4j_cypher_builder_template/sql_inline.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/neo4j_cypher_builder_template/sql_inline.py
+++ tests/neo4j_cypher_builder_template/sql_inline.py
@@ -1,7 +1,7 @@
-a="select * from user"
+a = "select * from user"
 
 
-b="create (n:person)" # cypher # not working!
+b = "create (n:person)"  # cypher # not working!
 from javascript import require, globalThis
 
 chalk = require("./cypher_inline.js")
@@ -9,4 +9,4 @@ chalk = require("./cypher_inline.js")
 # print(dir(chalk))
 # what the fuck?
 val = chalk.myfunc()
-print('VALUE',val, type(val))
+print("VALUE", list(val), type(val))  # it can be converted.
