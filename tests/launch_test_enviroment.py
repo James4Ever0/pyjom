ab699c5287ad3cf694e6229cfe04290613fbe7a3
GitJournal <app@gitjournal.io>
2022-12-14 16:56:18 +0800

    [2022-12-14 08:56:09]
    FILE:
    tests/launch_test_enviroment.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/launch_test_enviroment.py
+++ tests/launch_test_enviroment.py
@@ -16,7 +16,8 @@ def launchProgramWithTerminal(
         os.chdir(directory)
         executable_path = os.path.join(directory, executable)
         assert os.path.exists(executable_path)
-        mkeep_on = ("{}" if not keep_on else "bash -c \"{}; if [[ '$!' -ne 0 ]] then; echo; echo 'error log above...'; date; bash; fi;\"")
+        mkeep_on = ("{}" if not keep_on  else "bash -c \"{}; echo; echo 'error log above...'; date; bash;\"")
+        # mkeep_on = ("{}" if not keep_on else "bash -c \"{}; if [[ '$!' -ne 0 ]] then; echo; echo 'error log above...'; date; bash; fi;\"")
         command = f'{"gnome-terminal -- " if not no_terminal else ""}{mkeep_on.format(f"{intepreter} {executable_path}")}'
         return command
     except:
