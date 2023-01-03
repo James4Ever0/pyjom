996dfcb42d8edb46a1e80008e1559becff15bf12
GitJournal <app@gitjournal.io>
2023-01-04 06:08:16 +0800

    [2023-01-03 22:08:13]
    FILE:
    tests/split_long_image_into_video/generate_video.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/split_long_image_into_video/generate_video.py
+++ tests/split_long_image_into_video/generate_video.py
@@ -1,1 +1,7 @@
 # to get a proper cover, let's simply crop.
+
+# to find a proper title for this video, extract keywords, generate title and find the best cover by embeddings.
+
+# first, get picture aspect.
+
+from caer.core import 
