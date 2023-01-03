2abe976156c3ff5f595b4320494805c8f01d4411
GitJournal <app@gitjournal.io>
2023-01-04 06:02:13 +0800

    [2023-01-03 22:02:11]
    FILE:
    tests/split_long_image_into_video/init.sh
    tests/split_long_image_into_video/generate_video.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

---
+++ tests/split_long_image_into_video/generate_video.py
@@ -0,0 +1,1 @@
+# to get a proper cover, let's simply crop.
--- tests/split_long_image_into_video/init.sh
+++ tests/split_long_image_into_video/init.sh
@@ -1,2 +1,3 @@
 # first, let's download the bgm used by many funny videos, recognized by shazam
-curl -L -o the_happy_troll.mp3 "https://ge-sycdn.kuwo.cn/a573fcf0d69bd0cd5912bf9a96cff3dc/63b4a35f/resource/n3/1/70/3124049952.mp3"
+# curl -L -o the_happy_troll.mp3 "https://ge-sycdn.kuwo.cn/a573fcf0d69bd0cd5912bf9a96cff3dc/63b4a35f/resource/n3/1/70/3124049952.mp3"
+curl -O "https://tmpfiles.org/dl/620815/long_and_funny_image_about_ai_painting.jpg"
