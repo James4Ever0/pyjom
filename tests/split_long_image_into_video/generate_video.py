51de29bc61a46bfdb999c1965ac4fdaff2efde65
GitJournal <app@gitjournal.io>
2023-01-04 06:44:31 +0800

    [2023-01-03 22:44:29]
    FILE:
    tests/split_long_image_into_video/generate_video.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/split_long_image_into_video/generate_video.py
+++ tests/split_long_image_into_video/generate_video.py
@@ -6,28 +6,35 @@
 
 import cv2
 
+
 def getWidthHeight(impath):
     d = cv2.imread(impath)
     # print(d.shape)
     height, width, channels = d.shape
     return width, height
 
+
 im0 = "long_and_funny_image_about_ai_painting.jpg"
 im1 = "intermediate.png"
 # very high, low width.
 # calculate actual output?
-mheight, mwidth = 1080,1920
+mheight, mwidth = 1080, 1920
 width, height = getWidthHeight(im0)
 import ffmpeg
-ffmpeg.input(im0).filter("scale", w=mwidth, h=-1).output(im1, overwrite_output=True).run()
+
+ffmpeg.input(im0).filter("scale", w=mwidth, h=-1).output(
+    im1, overwrite_output=True
+).run()
 width0, height0 = getWidthHeight(im1)
-pad_total = height0%mheight
+pad_total = height0 % mheight
 if pad_total != 0:
     im2 = "intermediate_0.png"
-    pad_above = pad_total//2
-    pad_below = pad_total-pad_above
+    pad_above = pad_total // 2
+    pad_below = pad_total - pad_above
     # then you must rewrite this shit.
-    ffmpeg.input(im1).filter("pad",w='iw',h='ih+{}'.format(pad_total),x=0, y=pad_above,color='white').output(im2, overwrite_output=True).run()
+    ffmpeg.input(im1).filter(
+        "pad", w="iw", h="ih+{}".format(pad_total), x=0, y=pad_above, color="white"
+    ).output(im2, overwrite_output=True).run()
 else:
     im2 = im1
 
@@ -35,31 +42,41 @@ else:
 import os
 import shutil
 
-mdir ="output"
+mdir = "output"
+fout = "output%d.png"
 
 if os.path.exists(mdir):
     shutil.rmtree(mdir)
 os.mkdir(mdir)
-mfout =os.path.join(mdir,fout)
+mfout = os.path.join(mdir, fout)
 import math
-fout = "output%d.png"
-mh = math.ceil(height0/mheight)
+
+mh = math.ceil(height0 / mheight)
 mlayout = "1x{}".format(mh)
-ffmpeg.input(im2).filter("untile",layout=mlayout).output(mfout, overwrite_output=True).run()
-ffmpeg.input(os.path.join(mdir,fout)).filter()
+ffmpeg.input(im2).filter("untile", layout=mlayout).output(
+    mfout, overwrite_output=True
+).run()
+ffmpeg.input(os.path.join(mdir, fout)).filter()
 
 mfiles = os.listdir(mdir)
 import re
-mfiles.sort(key=lambda x: int(re.findall(r'[0-9]+',x)[0]))
+
+mfiles.sort(key=lambda x: int(re.findall(r"[0-9]+", x)[0]))
 editly_script = {
-  "width":mwidth,
-  "height":mheight,
-  "outPath": './image.mp4',
-  "defaults": {
-    "transition": ,
-    "duration": 3,
-  },
-  "clips": [
-    { "layers": [{ "type": 'image', "path": os.path.join(mdir,mfile) }] } for mfile in mfiles
-  ]
+    "width": mwidth,
+    "height": mheight,
+    "outPath": "./output.mp4",
+    "defaults": {
+        "transition": {
+            "duration": 0.5,
+            "name": "random",
+            "audioOutCurve": "tri",
+            "audioInCurve": "tri",
+        },
+        "duration": 3,
+    },
+    "clips": [
+        {"layers": [{"type": "image", "path": os.path.join(mdir, mfile)}]}
+        for mfile in mfiles
+    ],
 }
