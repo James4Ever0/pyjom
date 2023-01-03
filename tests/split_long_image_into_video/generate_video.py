d0407de1bcec4f9c43a8737403f1eb45105d8bf3
GitJournal <app@gitjournal.io>
2023-01-04 06:14:18 +0800

    [2023-01-03 22:14:15]
    FILE:
    tests/split_long_image_into_video/generate_video.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/split_long_image_into_video/generate_video.py
+++ tests/split_long_image_into_video/generate_video.py
@@ -5,9 +5,18 @@
 # first, get picture aspect.
 
 import cv2
-d = cv2.imread("long_and_funny_image_about_ai_painting.jpg")
-# print(d.shape)
-height, width, channels = d.shape
+
+def getWidthHeight(impath):
+    d = cv2.imread(impath)
+    # print(d.shape)
+    height, width, channels = d.shape
+    return width, height
+
+im0 = "long_and_funny_image_about_ai_painting.jpg"
+im1 = "intermediate.png"
 # very high, low width.
 # calculate actual output?
 mheight, mwidth = 1080,1920
+width, height = getWidthHeight(im0)
+import ffmpeg
+ffmpeg.input(im0).
