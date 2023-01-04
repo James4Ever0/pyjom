288e960ad11b4b27d93e483bd1a0d8546ae0e927
GitJournal <app@gitjournal.io>
2023-01-05 07:34:46 +0800

    [2023-01-04 23:34:43]
    FILE:
    tasks/qq/qq_red_packet_collect/adtools.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tasks/qq/qq_red_packet_collect/adtools.py
+++ tasks/qq/qq_red_packet_collect/adtools.py
@@ -1,6 +1,16 @@
 def checkCatOrDog(Content:str):
     # cat? dog? None?
-    catSignals = []
+    catSignals = [
+            "喵喵", "猫", "猫咪", "喵"]
+
+    dogSignals = [
+                "狗狗",
+                "狗",
+                "汪汪",
+                "修勾",
+                "汪",
+                "狗子",
+            ]
     dogSignals = []
     elemDict = {"cat":catSignals, "dog":dogSignals}
     for key, elems in elemDict.items():
