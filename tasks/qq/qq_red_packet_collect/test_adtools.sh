e4d4c9cde52881def4b85568dda23403eeb02f71
GitJournal <app@gitjournal.io>
2023-01-06 01:29:26 +0800

    [2023-01-05 17:29:22]
    FILE:
    tasks/qq/qq_red_packet_collect/test_adtools.sh
    tasks/qq/qq_red_packet_collect/test_make_dog_cat_connection.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tasks/qq/qq_red_packet_collect/test_adtools.sh
+++ tasks/qq/qq_red_packet_collect/test_adtools.sh
@@ -1,2 +1,2 @@
 cd . # really sure that is just because of insufficient power supply for external drives?
-pytest-3 -s test_make_dog_cat_connections.py::test_query
+pytest-3 -s test_make_dog_cat_connection.py::test_query
--- tasks/qq/qq_red_packet_collect/test_make_dog_cat_connection.py
+++ tasks/qq/qq_red_packet_collect/test_make_dog_cat_connection.py
@@ -1,10 +1,12 @@
 
 from adtools import makeCatOrDogConnections,getCatOrDogAd
-def test_main():
-    makeCatOrDogConnections('123','345','cat', debug=True)
-def test_delete():
-    makeCatOrDogConnections('123','345','cat', debug=True, delete=True)
+# def test_main():
+#     makeCatOrDogConnections('123','345','cat', debug=True)
+# def test_delete():
+#     makeCatOrDogConnections('123','345','cat', debug=True, delete=True)
 
 def test_query():
     response = getCatOrDogAd("cat",debug=True)
     response = getCatOrDogAd("dog",debug=True)
+
+if __name__ == "__main__":
