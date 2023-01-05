ebf6b0aaf0e46976c73fbd4bcd641fad1ed01c4a
GitJournal <app@gitjournal.io>
2023-01-05 22:27:50 +0800

    [2023-01-05 14:27:44]
    FILE:
    tasks/qq/qq_red_packet_collect/adtools.py
    tasks/qq/qq_red_packet_collect/test_make_dog_cat_connection.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tasks/qq/qq_red_packet_collect/test_make_dog_cat_connection.py
+++ tasks/qq/qq_red_packet_collect/test_make_dog_cat_connection.py
@@ -1,3 +1,6 @@
+from numpy import True_
 from adtools import makeCatOrDogConnections
 def test_main():
     makeCatOrDogConnections('123','345','cat', debug=True)
+def test_delete():
+    makeCatOrDogConnections('123','345','cat', debug=True, delete=True)
