8d650d755b0aa6ba884de9ab6b88aa74b9f08552
GitJournal <app@gitjournal.io>
2023-01-06 06:46:53 +0800

    [2023-01-05 22:46:48]
    FILE:
    tasks/qq/qq_red_packet_collect/ad_template_2_functional.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tasks/qq/qq_red_packet_collect/ad_template_2_functional.py
+++ tasks/qq/qq_red_packet_collect/ad_template_2_functional.py
@@ -80,7 +80,44 @@ def prepareMaterials(tmpDirPath: str = TMP_DIR_PATH, resourcePath: str = RESOURC
         shutil.copy(os.path.join(tmpDirPath, path), os.path.join(resourcePath, path))
 
 def generateBilibiliShortLinkMethod2(videoLink: str):
-    ...
+
+    apiUrl = "https://service-ijd4slqi-1253419200.gz.apigw.tencentcs.com/release/short_url"
+    # longUrl = "https://www.bilibili.com/video/BV1Wv41157Wz"
+    longUrl = videoLink
+    import urllib.parse as urlparse
+    # params = {"url": longUrl}
+    params = {"url": urlparse.quote(longUrl).replace("/","%2F"), 'href':"https://xiaojuzi.fun/bili-short-url/"}
+    # print(params)
+    # exit()
+
+    headers = {
+        "accept": "*/*",
+        "accept-language": "en-US,en;q=0.9",
+        "if-none-match": 'W/"35-oPDNsqBGaZKqGe83GW6wem+lkww"',
+        "sec-ch-ua": '"Google Chrome";v="105", "Not)A;Brand";v="8", "Chromium";v="105"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"macOS"',
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "cross-site",
+        "Referer": "https://xiaojuzi.fun/",
+        "Referrer-Policy": "strict-origin-when-cross-origin",
+        'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36' # this is important.
+    }
+
+    import requests
+    request_url = apiUrl+"?url={url}&href={href}".format(**params)
+    # request_url = 'https://service-ijd4slqi-1253419200.gz.apigw.tencentcs.com/release/short_url?url=https%3A%2F%2Fwww.bilibili.com%2Fvideo%2FBV1Wv41157Wz&href=https://xiaojuzi.fun/bili-short-url/'
+    # print(request_url)
+    r = requests.get(request_url, headers=headers)
+    if r.status_code == 200:
+        # print(r.json())
+        r_json = r.json()
+        success = r_json.get('success', False)
+        if success:
+            short_url = r_json.get('short_url', None)
+            print(short_url)
+    # starts with 'https://b23.tv'
 
 def generateBilibiliShortLinkMethod1(videoLink: str): # get bilibili user email address by asking them from chat. if they give the email address, send setu as gift. for other users, you may improvise. send video link, recommendations
     url = "https://api.bilibili.com/x/share/click"
@@ -108,7 +145,18 @@ def generateBilibiliShortLinkMethod1(videoLink: str): # get bilibili user email 
             print(link)
             return link
     # fail, obviously.
-    # you improvise.
+
+def generateBilibiliShortLink(videoLink:str):
+    link = None
+    try:
+        link = generateBilibiliShortLinkMethod1(videoLink)
+        assert link is not None
+    except:
+        import traceback
+        traceback.print_exc()
+        link = generateBilibiliShortLinkMethod2(videoLink)
+        assert link is not None
+    return link
 
 
 def makeQRCode(content: str, savePath: str):
