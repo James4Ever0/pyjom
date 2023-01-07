700a459b4ea7c92efce61a385ebc9bd406429e86
GitJournal <app@gitjournal.io>
2023-01-07 23:50:45 +0800

    [2023-01-07 15:50:37]
    FILE:
    tasks/qq/qq_red_packet_collect/botoy_redpacket_collect.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tasks/qq/qq_red_packet_collect/botoy_redpacket_collect.py
+++ tasks/qq/qq_red_packet_collect/botoy_redpacket_collect.py
@@ -258,7 +258,7 @@ def catOrDogAsyncThread(group_id:str, sender_id:str,Content:str):
                 penalty += 40
             adBuffer[str(group_id)] = penalty
         # decrease that counter by standard group messages.
-
+AD_INIT_COUNTER = 10
 @bot.on_group_msg
 def group(ctx: GroupMsg, groupInitReplyDelayRange=(4, 15)):
     # too broad for groupInitReplyDelayRange to be (2, 20)
@@ -271,7 +271,7 @@ def group(ctx: GroupMsg, groupInitReplyDelayRange=(4, 15)):
     group_id = data_dict["FromGroupId"]
 
     # decrease that ad counter.
-    adCounter = adBuffer.get(str(group_id), 0)
+    adCounter = adBuffer.get(str(group_id), AD_INIT_COUNTER)
     if adCounter > 0:
         adCounter -= 1
     adBuffer[str(group_id)] = adCounter
