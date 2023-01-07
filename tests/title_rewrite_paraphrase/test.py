665de244227734f7301b5947831a030caa22bb13
GitJournal <app@gitjournal.io>
2023-01-08 03:28:46 +0800

    [2023-01-07 19:28:34]
    FILE:
    tests/title_rewrite_paraphrase/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/title_rewrite_paraphrase/test.py
+++ tests/title_rewrite_paraphrase/test.py
@@ -2,5 +2,14 @@ title="世上所有小猫都是天使变的！"
 
 # use our free api first. yes?
 import yaml
-with open("clueai_api.json",'r') as f:
-    apiKey=yaml.load(f)['api_key']
+with open("clueai_api.yaml",'r') as f:
+    apiKey=yaml.load(f,Loader=yaml.FullLoader)['api_key']
+    print("Key?",apiKey)
+
+import clueai
+
+# initialize the Clueai Client with an API Key
+# 微调用户finetune_user=True
+cl = clueai.Client(apiKey)
+print(cl.check_usage(finetune_user=False)) 
+# {'使用量': 0, '剩余量': 5000, '用户类型': '免费用户'}
