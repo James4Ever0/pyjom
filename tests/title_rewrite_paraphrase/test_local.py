2e936415d206ffe41d1bf76ee4faf0136152766a
GitJournal <app@gitjournal.io>
2023-01-08 03:50:01 +0800

    [2023-01-07 19:49:50]
    FILE:
    tests/title_rewrite_paraphrase/test.py
    tests/title_rewrite_paraphrase/test_local.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/title_rewrite_paraphrase/test_local.py
+++ tests/title_rewrite_paraphrase/test_local.py
@@ -3,7 +3,7 @@ from transformers import T5Tokenizer, T5ForConditionalGeneration
 modelID="ClueAI/PromptCLUE-base-v1-5"
 
 tokenizer = T5Tokenizer.from_pretrained(modelID,local_first=True)
-model = T5ForConditionalGeneration.from_pretrained(modelID,local_first=True,device="cpu")
+model = T5ForConditionalGeneration.from_pretrained(modelID,local_first=True,device="cpu") # oh shit! 1G model
 
 def preprocess(text):
   return text.replace("\n", "_")
@@ -22,3 +22,11 @@ def answer(text, sample=False, top_p=0.8,device='cpu'):
     out = model.generate(**encoding, return_dict_in_generate=True, output_scores=False, max_length=64, do_sample=True, top_p=top_p)
   out_text = tokenizer.batch_decode(out["sequences"], skip_special_tokens=True)
   return postprocess(out_text[0])
+
+q = '''
+生成与下列文字相同意思的句子：
+支持几十个不同类型的任务，具有较好的零样本学习能力和少样本学习能力。
+答案：
+'''
+r = answer(q)
+print(r)
