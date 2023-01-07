3e4e63e3857e24a8182fbfe4b1073880d34c9517
GitJournal <app@gitjournal.io>
2023-01-08 03:53:01 +0800

    [2023-01-07 19:52:52]
    FILE:
    tests/title_rewrite_paraphrase/test.py
    tests/title_rewrite_paraphrase/test_local.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/title_rewrite_paraphrase/test.py
+++ tests/title_rewrite_paraphrase/test.py
@@ -1,28 +1,31 @@
-title="世上所有小猫都是天使变的！"
+title = "世上所有小猫都是天使变的！"
 
 # use our free api first. yes?
 import yaml
-with open("clueai_api.yaml",'r') as f:
-    apiKey=yaml.load(f,Loader=yaml.FullLoader)['api_key']
-    print("Key?",apiKey)
+
+with open("clueai_api.yaml", "r") as f:
+    apiKey = yaml.load(f, Loader=yaml.FullLoader)["api_key"]
+    print("Key?", apiKey)
 
 import clueai
 
 # initialize the Clueai Client with an API Key
 # 微调用户finetune_user=True
 # cl = clueai.Client(apiKey)
-# print(cl.check_usage(finetune_user=False)) 
+# print(cl.check_usage(finetune_user=False))
 # shit. we are on trial.
 # {'使用量': 0, '剩余量': 5000, '用户类型': '免费用户'}
 
 
 # initialize the Clueai Client with an API Key
-cl = clueai.Client("", check_api_key=False) # good without API key
-prompt= '''
+cl = clueai.Client("", check_api_key=False)  # good without API key
+prompt = """
 生成与下列文字相同意思的句子：
 {}
 答案：
-'''.format(title) #shit.
+""".format(
+    title
+)  # shit.
 # generate a prediction for a prompt
 
 generate_config = {
@@ -31,13 +34,13 @@ generate_config = {
     "max_length": 128,
     "min_length": 5,
     "length_penalty": 1.0,
-    "num_beams": 1
-  }
+    "num_beams": 1,
+}
 # 如果需要自由调整参数自由采样生成，添加额外参数信息设置方式：generate_config=generate_config
 prediction = cl.generate(
-        model_name='clueai-base',
-        prompt=prompt,generate_config=generate_config)
+    model_name="clueai-base", prompt=prompt, generate_config=generate_config
+)
 # 需要返回得分的话，指定return_likelihoods="GENERATION"
 
 # print the predicted text
-print('prediction: {}'.format(prediction.generations[0].text))
+print("prediction: {}".format(prediction.generations[0].text))
--- tests/title_rewrite_paraphrase/test_local.py
+++ tests/title_rewrite_paraphrase/test_local.py
@@ -1,32 +1,55 @@
 # 加载模型
 from transformers import T5Tokenizer, T5ForConditionalGeneration
-modelID="ClueAI/PromptCLUE-base-v1-5"
 
-tokenizer = T5Tokenizer.from_pretrained(modelID,local_first=True)
-model = T5ForConditionalGeneration.from_pretrained(modelID,local_first=True,device="cpu") # oh shit! 1G model
+modelID = "ClueAI/PromptCLUE-base-v1-5"
+
+tokenizer = T5Tokenizer.from_pretrained(modelID, local_first=True)
+model = T5ForConditionalGeneration.from_pretrained(
+    modelID, local_first=True, device="cpu"
+)  # oh shit! 1G model
+
 
 def preprocess(text):
-  return text.replace("\n", "_")
+    return text.replace("\n", "_")
+
 
 def postprocess(text):
-  return text.replace("_", "\n")
+    return text.replace("_", "\n")
 
-def answer(text, sample=False, top_p=0.8,device='cpu'):
-  '''sample：是否抽样。生成任务，可以设置为True;
-  top_p：0-1之间，生成的内容越多样'''
-  text = preprocess(text)
-  encoding = tokenizer(text=[text], truncation=True, padding=True, max_length=768, return_tensors="pt").to(device) 
-  if not sample:
-    out = model.generate(**encoding, return_dict_in_generate=True, output_scores=False, max_length=128, num_beams=4, length_penalty=0.6)
-  else:
-    out = model.generate(**encoding, return_dict_in_generate=True, output_scores=False, max_length=64, do_sample=True, top_p=top_p)
-  out_text = tokenizer.batch_decode(out["sequences"], skip_special_tokens=True)
-  return postprocess(out_text[0])
 
-q = '''
+def answer(text, sample=False, top_p=0.8, device="cpu"):
+    """sample：是否抽样。生成任务，可以设置为True;
+    top_p：0-1之间，生成的内容越多样"""
+    text = preprocess(text)
+    encoding = tokenizer(
+        text=[text], truncation=True, padding=True, max_length=768, return_tensors="pt"
+    ).to(device)
+    if not sample:
+        out = model.generate(
+            **encoding,
+            return_dict_in_generate=True,
+            output_scores=False,
+            max_length=128,
+            num_beams=4,
+            length_penalty=0.6
+        )
+    else:
+        out = model.generate(
+            **encoding,
+            return_dict_in_generate=True,
+            output_scores=False,
+            max_length=64,
+            do_sample=True,
+            top_p=top_p
+        )
+    out_text = tokenizer.batch_decode(out["sequences"], skip_special_tokens=True)
+    return postprocess(out_text[0])
+
+
+q = """
 生成与下列文字相同意思的句子：
 支持几十个不同类型的任务，具有较好的零样本学习能力和少样本学习能力。
 答案：
-'''
+"""
 r = answer(q)
 print(r)
