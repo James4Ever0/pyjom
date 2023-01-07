52a5eae195d8199019b34497e68d886c4a7b52cb
GitJournal <app@gitjournal.io>
2023-01-08 03:34:43 +0800

    [2023-01-07 19:34:35]
    FILE:
    tests/title_rewrite_paraphrase/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/title_rewrite_paraphrase/test.py
+++ tests/title_rewrite_paraphrase/test.py
@@ -11,6 +11,33 @@ import clueai
 # initialize the Clueai Client with an API Key
 # 微调用户finetune_user=True
 # cl = clueai.Client(apiKey)
-print(cl.check_usage(finetune_user=False)) 
+# print(cl.check_usage(finetune_user=False)) 
 # shit. we are on trial.
 # {'使用量': 0, '剩余量': 5000, '用户类型': '免费用户'}
+
+
+# initialize the Clueai Client with an API Key
+cl = clueai.Client("", check_api_key=False)
+prompt= '''
+生成与下列文字相同意思的句子：
+长期通胀前景在今天可能不确定性更大。
+答案：
+''' #shit.
+# generate a prediction for a prompt
+
+generate_config = {
+    "do_sample": True,
+    "top_p": 0.8,
+    "max_length": 128,
+    "min_length": 10,
+    "length_penalty": 1.0,
+    "num_beams": 1
+  }
+# 如果需要自由调整参数自由采样生成，添加额外参数信息设置方式：generate_config=generate_config
+prediction = cl.generate(
+        model_name='clueai-base',
+        prompt=prompt)
+# 需要返回得分的话，指定return_likelihoods="GENERATION"
+
+# print the predicted text
+print('prediction: {}'.format(prediction.generations[0].text))
