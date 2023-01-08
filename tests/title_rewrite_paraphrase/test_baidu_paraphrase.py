d22439f87bcdd50cbb6cea84e74a66e625dc9832
GitJournal <app@gitjournal.io>
2023-01-09 04:31:50 +0800

    [2023-01-08 20:31:47]
    FILE:
    tests/title_rewrite_paraphrase/test_baidu_paraphrase.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/title_rewrite_paraphrase/test_baidu_paraphrase.py
+++ tests/title_rewrite_paraphrase/test_baidu_paraphrase.py
@@ -1,5 +1,16 @@
-language_id = 
 
+content = ""
 
+target_language_id = ...
 
-if is_chinese:
+all_middle_languages = [] # english, japanese, chinese
+
+non_target_middle_languages = [langid for langid in all_middle_languages if langid is not target_language_id]
+
+import random
+
+middle_language_id = random.choice(non_target_middle_languages)
+
+middle_content = baidu_translate(content, source = target_language_id, target = middle_language_id)
+
+output_content = baidu_translate(middle_content, source = middle_language_id, target = target_language_id)
