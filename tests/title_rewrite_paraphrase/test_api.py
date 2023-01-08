8b645ee224249dc65a8747bb6d564cdb15a50408
GitJournal <app@gitjournal.io>
2023-01-09 05:47:31 +0800

    [2023-01-08 21:47:28]
    FILE:
    tests/title_rewrite_paraphrase/test_api.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/title_rewrite_paraphrase/test_api.py
+++ tests/title_rewrite_paraphrase/test_api.py
@@ -1,23 +1,29 @@
 import requests
 
-content:str
+def chineseParaphraserAPI(    content:str,
+debug:bool=False,
+    target_id:int =0,
+    timeout:int=10,
+    providers:list[str]=["http://www.wzwyc.com/api.php?key=", "http://ai.guiyigs.com/api.php?key="] # it is about to close! fuck. "本站于2023年2月19日关站" buy code from "1900373358"
+    ):
 
-target_id:int =0
-timeout:int=10
-providers:list[str]=["http://www.wzwyc.com/api.php?key=", "http://ai.guiyigs.com/api.php?key="] # it is about to close! fuck. "本站于2023年2月19日关站" buy code from "1900373358"
 
-target = providers[
-    target_id
-]  # all the same?
-data = {"info": content}
+    target = providers[
+        target_id
+    ]  # all the same?
+    data = {"info": content}
 
-# target = "http://www.xiaofamaoai.com/result.php"
-# xfm_uid = "342206661e655450c1c37836d23dc3eb"
-# data = {"contents":content, "xfm_uid":xfm_uid, "agreement":"on"}
-# nothing? fuck?
+    # target = "http://www.xiaofamaoai.com/result.php"
+    # xfm_uid = "342206661e655450c1c37836d23dc3eb"
+    # data = {"contents":content, "xfm_uid":xfm_uid, "agreement":"on"}
+    # nothing? fuck?
 
-r = requests.post(target, data=data,timeout=timeout)
-output = r.text
-print(output)
+    r = requests.post(target, data=data,timeout=timeout)
+    output = r.text
+    success = output.strip()!= content.strip()
+    if debug:
+        print(output)
+    return output, success
 
 content =  "支持几十个不同类型的任务，具有较好的零样本学习能力和少样本学习能力。"
+output,success =
