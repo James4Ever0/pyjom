f577a5c762402fcba7ce67f2fc8df2f0f24a12c1
GitJournal <app@gitjournal.io>
2023-01-08 08:34:06 +0800

    [2023-01-08 00:34:02]
    FILE:
    tests/karaoke_effects/test_baidu_language_recognize.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

---
+++ tests/karaoke_effects/test_baidu_language_recognize.py
@@ -0,0 +1,9 @@
+
+import paddlehub as hub
+
+language_translation_model = hub.Module(name="baidu_translate")
+language_recognition_model = hub.Module(name="baidu_language_recognition")
+
+text='hello world'
+language_code = language_recognition_model.recognize(text)
+print('language_code: %s' % language_code)
