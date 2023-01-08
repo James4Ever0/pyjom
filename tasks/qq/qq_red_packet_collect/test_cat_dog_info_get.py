5582e0dcd0677bdea2a8d06624b98e236591eb59
GitJournal <app@gitjournal.io>
2023-01-08 10:08:08 +0800

    [2023-01-08 02:07:59]
    FILE:
    tasks/qq/qq_red_packet_collect/test_cat_dog_info_get.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tasks/qq/qq_red_packet_collect/test_cat_dog_info_get.py
+++ tasks/qq/qq_red_packet_collect/test_cat_dog_info_get.py
@@ -1,7 +1,8 @@
-
+from adtools import getCatOrDogAd
+import random
+cat_or_dog='cat'
+for _ in range(2):
     responses = getCatOrDogAd(cat_or_dog)
-    success = False
-
-    with getAdLock():
-        if responses != []:
-            videoInfo = random.choice(responses[:recentLimits])
+    videoInfo = random.choice(responses[:20])
+    print(videoInfo)
+    print()
