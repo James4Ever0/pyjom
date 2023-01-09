d802b03d1c549d0615d382d672af1ca914ad8273
GitJournal <app@gitjournal.io>
2023-01-10 02:49:24 +0800

    [2023-01-09 18:49:11]
    FILE:
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    tests/patch_requests_timeout/
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

---
+++ tests/patch_requests_timeout/client.py
@@ -0,0 +1,36 @@
+
+import patchy
+from requests.adapters import HTTPAdapter
+
+REQUESTS_TIMEOUT=30 
+
+def patch_requests_default_timeout() -> None:
+    """
+    Set a default timeout for all requests made with “requests”.
+
+    Upstream is waiting on this longstanding issue:
+    https://github.com/psf/requests/issues/3070
+    """
+
+    patchy.patch(
+        HTTPAdapter.send,
+        f"""\
+        @@ -14,6 +14,8 @@
+             :param proxies: (optional) The proxies dictionary to apply to the request.
+             :rtype: requests.Response
+             \"""
+        +    if timeout is None:
+        +        timeout = {REQUESTS_TIMEOUT}
+
+             try:
+                 conn = self.get_connection(request.url, proxies)
+        """,
+    )
+
+patch_requests_default_timeout()
+
+import requests
+
+
+
+requests.get("http://localhost:{}")
