9c6dc7ad3d6271fbab7b56c8bfb08a256ee3cc6a
GitJournal <app@gitjournal.io>
2023-01-09 08:34:04 +0800

    [2023-01-09 00:33:53]
    FILE:
    tests/post_numpy_array/server.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/post_numpy_array/server.py
+++ tests/post_numpy_array/server.py
@@ -3,3 +3,12 @@ import numpy as np
 from typing import Union
 class Image(BaseModel):
     image:Union[str,np.ndarray]
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.post("/books/")
+def create_book(book: Image):
+    # return book
+    print book.image.
