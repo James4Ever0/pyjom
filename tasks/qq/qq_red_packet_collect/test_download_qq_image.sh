a5a80a19ab58343b248bff9606d4436607d26e50
GitJournal <app@gitjournal.io>
2023-01-11 05:36:24 +0800

    [2023-01-10 21:36:15]
    FILE:
    tasks/qq/qq_red_packet_collect/botoy_redpacket_collect.py
    tasks/qq/qq_red_packet_collect/test_download_qq_image.sh
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tasks/qq/qq_red_packet_collect/test_download_qq_image.sh
+++ tasks/qq/qq_red_packet_collect/test_download_qq_image.sh
@@ -2,3 +2,5 @@
 # but you can download image when it has not expired.
 # what is the type of this image?
 curl -L -o qq_image_received.image "http://gchat.qpic.cn/gchatpic_new/3318506826/205569604-2534335053-7D3B7BA4B3476AD210837D8C86F2E8DA/0?vuin=917521610&term=255&pictype=0"
+
+# cv2 can handle that.
