08e3d3debbae7f268c92cb0293b4ee0327d82555
GitJournal <app@gitjournal.io>
2023-01-11 05:27:21 +0800

    [2023-01-10 21:27:10]
    FILE:
    tasks/qq/qq_red_packet_collect/test_download_qq_image.sh
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

---
+++ tasks/qq/qq_red_packet_collect/test_download_qq_image.sh
@@ -0,0 +1,3 @@
+# you cannot receive qq voice with opq!
+http://gchat.qpic.cn/gchatpic_new/3318506826/205569604-2534335053-7D3B7BA4B3476AD210837D8C86F2E8DA/0?vuin=9175216
+2023/01/10 13:31:38.397 [M]  Channel 62f6f72d│ "sender_id": 1369859871, "content": "（2+根│10\u0026term=255\u0026pictype=0
