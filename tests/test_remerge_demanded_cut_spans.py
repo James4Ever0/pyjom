8dd0a2d976724b88c08811601462334b707f7f3d
GitJournal <app@gitjournal.io>
2023-01-10 10:44:06 +0800

    [2023-01-10 02:43:56]
    FILE:
    tests/test_remerge_demanded_cut_spans.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/test_remerge_demanded_cut_spans.py
+++ tests/test_remerge_demanded_cut_spans.py
@@ -1,31 +1,33 @@
-cut_spans = [(0,1),(1,2),(2,9),(9,100),(100,101),(101,102)]
+cut_spans = [(0, 1), (1, 2), (2, 9), (9, 100), (100, 101), (101, 102)]
 
 from test_commons import *
 from pyjom.lyrictoolbox import remergeDemandedCutSpans
 
-def test_cut_spans_valid(list_of_spans, min_span=1.5, max_span=10,no_range_test=False):
+
+def test_cut_spans_valid(list_of_spans, min_span=1.5, max_span=10, no_range_test=False):
     start = list_of_spans[0][0]
     init_end = list_of_spans[0][1]
-    minit_duration = list_of_spans[0][1]-start
+    minit_duration = list_of_spans[0][1] - start
     if not no_range_test:
         assert start < list_of_spans[0][1]
-        assert minit_duration >=min_span and minit_duration <=max_span
+        assert minit_duration >= min_span and minit_duration <= max_span
     # end = list_of_spans[-1][1]
-    for i,span in enumerate(list_of_spans[1:]):
+    for i, span in enumerate(list_of_spans[1:]):
         mstart, mend = span
         try:
             assert mstart == init_end
         except:
-            print(mstart, mend, init_end,i+1)
-            print(list_of_spans[max(0,i-2):min(len(list_of_spans),i+2)])
+            print(mstart, mend, init_end, i + 1)
+            print(list_of_spans[max(0, i - 2) : min(len(list_of_spans), i + 2)])
             breakpoint()
         assert mstart < mend
-        duration = mend-mstart
+        duration = mend - mstart
         if not no_range_test:
-            assert duration >=min_span and duration <= max_span
+            assert duration >= min_span and duration <= max_span
         init_end = mend
 
-test_cut_spans_valid(cut_spans,no_range_test=True)
+
+test_cut_spans_valid(cut_spans, no_range_test=True)
 new_spans = remergeDemandedCutSpans(cut_spans)
-print('new spans?',new_spans)
+print("new spans?", new_spans)
 test_cut_spans_valid(new_spans)
