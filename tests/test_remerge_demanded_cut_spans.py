b8efd8f7515735af26989606f3e7a0ba74f7633b
GitJournal <app@gitjournal.io>
2023-01-10 10:28:57 +0800

    [2023-01-10 02:28:47]
    FILE:
    tests/test_remerge_demanded_cut_spans.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/test_remerge_demanded_cut_spans.py
+++ tests/test_remerge_demanded_cut_spans.py
@@ -1,20 +1,26 @@
 cut_spans = [(0,1),(1,2),(2,9),(9,100),(100,101),(101,102)]
 
-from import 
+from test_commons import *
 from pyjom.lyrictoolbox import remergeDemandedCutSpans
 
-def test_cut_spans_valid(list_of_spans, min_span=1.5, max_span=10):
+def test_cut_spans_valid(list_of_spans, min_span=1.5, max_span=10,no_range_test=False):
     start = list_of_spans[0][0]
-    assert start < list_of_spans[0][1]
+    init_end = list_of_spans[0][1]
     minit_duration = list_of_spans[0][1]-start
-    assert minit_duration >=min_span and minit_duration <=max_span
+    if not no_range_test:
+        assert start < list_of_spans[0][1]
+        assert minit_duration >=min_span and minit_duration <=max_span
     # end = list_of_spans[-1][1]
     for span in list_of_spans[1:]:
         mstart, mend = span
+        assert mstart == init_end
         assert mstart < mend
         duration = mend-mstart
-        assert duration >=min_span and duration <= max_span
+        if not no_range_test:
+            assert duration >=min_span and duration <= max_span
+        init_end = mend
 
-
+test_cut_spans_valid(cut_spans,no_range_test=True)
 
 new_spans = remergeDemandedCutSpans(cut_spans)
+test_cut_spans_valid(new_spans)
