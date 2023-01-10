0101470edd0cbe316b73b21c766178ce71b39d72
GitJournal <app@gitjournal.io>
2023-01-11 03:02:22 +0800

    [2023-01-10 19:02:10]
    FILE:
    tests/moviepy_loop_video_till_target/loop_till_target.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/moviepy_loop_video_till_target/loop_till_target.py
+++ tests/moviepy_loop_video_till_target/loop_till_target.py
@@ -1,10 +1,15 @@
 import os
+import moviepy
+
 def main(f_in:str, target_secs:float,f_out:str="", in_place:bool=True, accuracy_float:int=4):
     assert os.path.exists(f_in)
     assert target_secs >0
-    
+    target_secs_str =("{"+f':.{accuracy_float}f'+"}").format(target_secs)
     if not in_place:
         assert f_out != ""
 
 if __name__ == '__main__':
     import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input", dest="input file", required=True)
+    parser.add_argument("-o", "--output", dest="output file", default="")
