8fea115d209b383da2c1b2bec0727a9afb32087f
GitJournal <app@gitjournal.io>
2023-01-10 10:25:55 +0800

    [2023-01-10 02:25:44]
    FILE:
    tests/test_remerge_demanded_cut_spans.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/test_remerge_demanded_cut_spans.py
+++ tests/test_remerge_demanded_cut_spans.py
@@ -3,10 +3,18 @@ cut_spans = [(0,1),(1,2),(2,9),(9,100),(100,101),(101,102)]
 from import 
 from pyjom.lyrictoolbox import remergeDemandedCutSpans
 
-def test_cut_spans_valid(list_of_spans):
+def test_cut_spans_valid(list_of_spans, min_span=1.5, max_span=10):
     start = list_of_spans[0][0]
-    end = list_of_spans[-1][1]
-    for span in list_of_spans:
-        
+    assert start < list_of_spans[0][1]
+    minit_duration = list_of_spans[0][1]-start
+    assert minit_duration >=min_span and minit_duration <=max_span
+    # end = list_of_spans[-1][1]
+    for span in list_of_spans[1:]:
+        mstart, mend = span
+        assert mstart < mend
+        duration = mend-mstart
+        assert duration >=min_span and duration <= max_span
+
+
 
 new_spans = remergeDemandedCutSpans(cut_spans)
