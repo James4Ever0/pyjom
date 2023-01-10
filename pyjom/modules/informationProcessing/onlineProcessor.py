ebac2367f7093fea9551a2c46e9b61a9147dfc44
GitJournal <app@gitjournal.io>
2023-01-11 02:19:57 +0800

    [2023-01-10 18:19:46]
    FILE:
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/informationProcessing/onlineProcessor.py
+++ pyjom/modules/informationProcessing/onlineProcessor.py
@@ -56,7 +56,7 @@ def OnlineProcessor(
                 # duration = get_duration(local_video_location)
                 from pyjom.commons import checkMinMaxDict
 
-                duration_filter = {"min": 0.6, "max": 12}
+                duration_filter = {"min": 2, "max": 15}
                 fps_filter = {"min": 7, "max": 60}
                 # fps_float = get_fps_float(local_video_location)
                 # duration_valid = checkMinMaxDict(duration,duration_filter)
