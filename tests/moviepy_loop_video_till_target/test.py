187d519babc6a5e26f1dd2d0fa2f92d1fcc3c23b
GitJournal <app@gitjournal.io>
2023-01-11 08:25:49 +0800

    [2023-01-11 00:25:38]
    FILE:
    tests/moviepy_loop_video_till_target/test.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/moviepy_loop_video_till_target/test.py
+++ tests/moviepy_loop_video_till_target/test.py
@@ -3,9 +3,9 @@ from loop_till_target import main
 target_secs = 20
 video_in = "/root/Desktop/works/pyjom/samples/video/cute_cat_gif.mp4"
 
-# no right codec! fuck.
+# no right codec! fuck. GIF not supported?
 
-video_out = f"/root/Desktop/works/pyjom/samples/video/cute_cat_gif_{target_secs}_secs_plus.gif"
+video_out = f"/root/Desktop/works/pyjom/samples/video/cute_cat_gif_{target_secs}_secs_plus.mp4"
 
-fvd = main(video_in, target_secs ,f_out=video_out ,in_place=False,debug=True)
+fvd = main(video_in, target_secs, f_out=video_out, in_place=False,debug=True)
 assert fvd >=target_secs
