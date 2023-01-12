ab536f1df596e4a362a0b7d825dd73dca7cdd2fa
GitJournal <app@gitjournal.io>
2023-01-13 07:34:34 +0800

    [2023-01-12 23:34:24]
    FILE:
    tasks/qq/qq_red_packet_collect/test_get_all_my_videos.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tasks/qq/qq_red_packet_collect/test_get_all_my_videos.py
+++ tasks/qq/qq_red_packet_collect/test_get_all_my_videos.py
@@ -2,7 +2,9 @@ import requests
 
 port, endpoint = 7341, "searchUserVideos"
 
-postData = {"query":"", "tid": 0, "method": "online"}
+postData = {"query":"", "tid": 0, "method": "online"} # this is to get latest video of my own. fuck. better turned into registration based method.
 r = requests.post(f"http://localhost:{port}/{endpoint}", json=postData)
 data = r.json()
-print("data",data)
+print("data")
+import rich
+rich.print(data)
