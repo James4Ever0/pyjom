88a497de69fa2f8b7018fca55b4eb33744d7f472
GitJournal <app@gitjournal.io>
2023-01-12 11:14:01 +0800

    [2023-01-12 03:13:59]
    FILE:
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/informationProcessing/onlineProcessor.py
+++ pyjom/modules/informationProcessing/onlineProcessor.py
@@ -63,7 +63,7 @@ def OnlineProcessor(
                 # usually we want to make video short.
                 # mode: up/down
 
-                def tuneVideoSpeedToBeat(video_phase:float, music_phase:float,mode:)
+                def tuneVideoSpeedToBeat(video_phase:float, music_phase:float,mode:Literal['up','down'])
 
                 music_beat_duration = ... # get from 
 
