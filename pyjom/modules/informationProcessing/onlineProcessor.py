430c7fd2e3035bb23362aa3d03de53bc2f996294
GitJournal <app@gitjournal.io>
2023-01-12 11:29:17 +0800

    [2023-01-12 03:29:14]
    FILE:
    pyjom/modules/informationProcessing/onlineProcessor.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/modules/informationProcessing/onlineProcessor.py
+++ pyjom/modules/informationProcessing/onlineProcessor.py
@@ -85,6 +85,7 @@ def OnlineProcessor(
                         else:
                             raise Exception("Unknown speed change mode: %s" % mode)
                 
+                # TODO: tune video speed to 
                 valid_video = corruptVideoFilter(local_video_location)
                 if not valid_video:
                     continue
