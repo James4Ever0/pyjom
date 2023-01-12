2dd14ebf747a96cfe5e67ff9d7a8b66977b9d036
GitJournal <app@gitjournal.io>
2023-01-13 07:27:49 +0800

    [2023-01-12 23:27:39]
    FILE:
    tasks/qq/qq_red_packet_collect/test_get_all_my_videos.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tasks/qq/qq_red_packet_collect/test_get_all_my_videos.py
+++ tasks/qq/qq_red_packet_collect/test_get_all_my_videos.py
@@ -1,6 +1,6 @@
 import requests
 port, endpoint = 7341,"searchUserVideos"
-params = {}
-r = requests.get(f"http://localhost:{port}/{endpoint}", params=params)
+postData = {}
+r = requests.post(f"http://localhost:{port}/{endpoint}", data=postData)
 data = r.json()
 print("data")
