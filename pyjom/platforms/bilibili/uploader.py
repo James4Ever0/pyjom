42364498a52eb441bd689821e8ef70e4841140a8
GitJournal <app@gitjournal.io>
2023-01-13 10:01:35 +0800

    [2023-01-13 02:01:33]
    FILE:
    pyjom/platforms/bilibili/uploader.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/platforms/bilibili/uploader.py
+++ pyjom/platforms/bilibili/uploader.py
@@ -418,7 +418,7 @@ def uploadVideo(
             raise Exception('videoMultithreadUploader error')
         try:
             code, message = mresult.get('code'), mresult.get('message')
-            assert code == 0 
+            assert code == 0  # why 
             assert message == '0'
         except:
             raise Exception('videoMultithreadUploader error: invalid response:', mresult)
