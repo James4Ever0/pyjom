049de9bbff3446fa7330eb013f6dd85c3adef9cf
GitJournal <app@gitjournal.io>
2023-01-13 08:25:19 +0800

    [2023-01-13 00:25:13]
    FILE:
    tests/download_sections_video_portion_partial_download_youtube_yt_dlp_bilibili/test_bilibili.sh
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/download_sections_video_portion_partial_download_youtube_yt_dlp_bilibili/test_bilibili.sh
+++ tests/download_sections_video_portion_partial_download_youtube_yt_dlp_bilibili/test_bilibili.sh
@@ -1,4 +1,4 @@
 URL="https://www.bilibili.com/video/BV1Fs411k7e9"
 # --force-keyframes-at-cuts
-yt-dlp --download-sections "*1:00:00-1:00:30" "$URL"
+yt-dlp --download-sections "*0:05:00-0:06:30" "$URL"
 # download-sections can be used multiple times?
