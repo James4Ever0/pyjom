a3354554c46dcac30e78f22e59116d5af32e580e
GitJournal <app@gitjournal.io>
2023-01-13 16:01:23 +0800

    [2023-01-13 08:01:14]
    FILE:
    tests/download_sections_video_portion_partial_download_youtube_yt_dlp_bilibili/test_bilibili.sh
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/download_sections_video_portion_partial_download_youtube_yt_dlp_bilibili/test_bilibili.sh
+++ tests/download_sections_video_portion_partial_download_youtube_yt_dlp_bilibili/test_bilibili.sh
@@ -37,6 +37,9 @@ URL="https://www.bilibili.com/video/BV1Fs411k7e9" #老戴 马克思佩恩 分p�
 
 # yt-dlp --download-sections "*0:05:00-0:06:40" --playlist-items "1" --cookies-from-browser firefox "$URL" # pass cookies.
 
+# what if we download multiple sections?
+
+# yt-dlp --download-sections "*0:05:00-0:05:40"  --download-sections "*0:05:00-0:05:40" --playlist-items "1" --cookies-from-browser firefox "$URL" # pass cookies.
 
 
 # just want metadata?
