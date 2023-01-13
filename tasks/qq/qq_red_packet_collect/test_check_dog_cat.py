27b8c5599efc6ee1a9b3f6b58161cd403ff2e210
GitJournal <app@gitjournal.io>
2023-01-13 18:31:32 +0800

    [2023-01-13 10:31:24]
    FILE:
    tasks/qq/qq_red_packet_collect/test_check_dog_cat.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tasks/qq/qq_red_packet_collect/test_check_dog_cat.py
+++ tasks/qq/qq_red_packet_collect/test_check_dog_cat.py
@@ -1,6 +1,8 @@
-from adtools import checkIsCatOrDogImage
+from adtools import checkIsCatOrDogImage, rateLimits
 
 image_url = "http://gchat.qpic.cn/gchatpic_new/3318506826/205569604-2534335053-7D3B7BA4B3476AD210837D8C86F2E8DA/0?vuin=917521610&term=255&pictype=0"
+
+print("RATE LIMITS?", rateLimits)
 
 for _ in range(3):
     r= checkIsCatOrDogImage(image_url) # will raise Exception! warning
