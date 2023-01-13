fadc2bc7caf7c026196561b80020847ec9f396d2
GitJournal <app@gitjournal.io>
2023-01-13 17:00:47 +0800

    [2023-01-13 09:00:37]
    FILE:
    tests/download_sections_video_portion_partial_download_youtube_yt_dlp_bilibili/test_bilibili.sh
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/download_sections_video_portion_partial_download_youtube_yt_dlp_bilibili/test_bilibili.sh
+++ tests/download_sections_video_portion_partial_download_youtube_yt_dlp_bilibili/test_bilibili.sh
@@ -44,7 +44,7 @@ URL="https://www.bilibili.com/video/BV1Fs411k7e9" #老戴 马克思佩恩 分p�
 # what if we download multiple sections?
 
 # no combination? shit.
-yt-dlp --download-sections "*0:04:50-0:05:40"  --download-sections "*0:05:50-0:06:40" --playlist-items "1" --cookies-from-browser firefox -o "%(uploader_id)s-%(id)s-%(title)s-%(autonumber)s.%(ext)s" "$URL" # pass cookies.
+yt-dlp --download-sections "*0:04:50-0:05:40" --download-sections "*0:05:50-0:06:40" --playlist-items "1" --cookies-from-browser firefox -o "%(uploader_id)s-%(id)s-%(title)s-%(autonumber)s.%(ext)s" "$URL" # pass cookies.
 # like this: '2142762-BV1Fs411k7e9_p1-老戴《马克思佩恩 3》全收集流程攻略【共14期完结】 p01 EP-01-00002.mp4'
 # https://github.com/yt-dlp/yt-dlp#readme -> "OUTPUT TEMPLATE"
 # https://github.com/yt-dlp/yt-dlp/issues/4579
