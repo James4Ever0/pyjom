5bc933c617609d9d4028c0740eda84d7ee9f18b9
GitJournal <app@gitjournal.io>
2023-01-13 09:16:47 +0800

    [2023-01-13 01:16:45]
    FILE:
    tests/download_sections_video_portion_partial_download_youtube_yt_dlp_bilibili/test_bilibili.sh
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/download_sections_video_portion_partial_download_youtube_yt_dlp_bilibili/test_bilibili.sh
+++ tests/download_sections_video_portion_partial_download_youtube_yt_dlp_bilibili/test_bilibili.sh
@@ -8,7 +8,7 @@ URL="https://www.bilibili.com/video/BV1Fs411k7e9" #老戴 马克思佩恩 分p�
 # --force-keyframes-at-cuts
 # man i just need the first chapter.
 
-yt-dlp --download-sections "*0:05:00-0:06:30" --playlist-items "1" "$URL" # only first video.
+# yt-dlp --download-sections "*0:05:00-0:06:30" --playlist-items "1" "$URL" # only first video.
 
 # just want metadata?
 # yt-dlp --write-info-json  --write-playlist-metafiles --skip-download "$URL" # this will get metadata main playlist and every video in the playlist in separate json files.
@@ -16,5 +16,5 @@ yt-dlp --download-sections "*0:05:00-0:06:30" --playlist-items "1" "$URL" # only
 
 # you can get comments with this tool.
 ## no comments?
-yt-dlp --write-info-json --skip-download "$URL"
+# yt-dlp --write-info-json --skip-download "$URL"
 # download-sections can be used multiple times?
