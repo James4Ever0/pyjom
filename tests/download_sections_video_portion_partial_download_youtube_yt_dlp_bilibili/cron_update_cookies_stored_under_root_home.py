1c957d9d074882636d10afff79fe27094f66a3c7
GitJournal <app@gitjournal.io>
2023-01-13 16:16:23 +0800

    [2023-01-13 08:16:15]
    FILE:
    tests/download_sections_video_portion_partial_download_youtube_yt_dlp_bilibili/cron_update_cookies_stored_under_root_home.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/download_sections_video_portion_partial_download_youtube_yt_dlp_bilibili/cron_update_cookies_stored_under_root_home.py
+++ tests/download_sections_video_portion_partial_download_youtube_yt_dlp_bilibili/cron_update_cookies_stored_under_root_home.py
@@ -20,5 +20,5 @@ browser_names = ["firefox","chromium"]
 
 for browser_name in browser_names:
     cookies = yt_dlp.cookies.extract_cookies_from_browser(browser_name)
-    filepath = os.path.join(cookies_path,"{}.cookies")
+    filepath = os.path.join(cookies_path,"{}.cookie")
     cookies.save()
