558355695c5cfbaf5798179315c5c06fa348db2b
GitJournal <app@gitjournal.io>
2023-01-13 14:53:31 +0800

    [2023-01-13 06:53:23]
    FILE:
    tests/karaoke_effects/clash_auto_update.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/karaoke_effects/clash_auto_update.py
+++ tests/karaoke_effects/clash_auto_update.py
@@ -18,7 +18,9 @@ os.environ["https_proxy"] = ""
 
 # CLASH_CONFIG_DOWNLOAD_URL = "https://subconverter.speedupvpn.com/sub?target=clash&url=https%3A%2F%2Fjsd.cdn.zzko.cn%2Fgh%2FPawdroid%2FFree-servers%40main%2Fsub&insert=false&emoji=true&list=false&tfo=false&scv=false&fdn=false&sort=false&new_name=true" # change this to the direct link you sucker.
 
-CLASH_CONFIG_DOWNLOAD_URL = 
+import urllib.parse
+DIRECT_LINK = ""
+CLASH_CONFIG_DOWNLOAD_URL = f"{urllib.parse.quote(DIRECT_LINK)}"
 
 
 ALL_PROXIES_LOCATION = ["proxies", "🔰 节点选择", "all"]
