f30b3c40ba54cc237f25a6b8d1da8e7b93076421
GitJournal <app@gitjournal.io>
2023-01-14 12:36:47 +0800

    [2023-01-14 04:36:38]
    FILE:
    tests/redis_music_info_persistance/launch_redis.sh
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/redis_music_info_persistance/launch_redis.sh
+++ tests/redis_music_info_persistance/launch_redis.sh
@@ -1,2 +1,2 @@
-ps aux | grep "redis-server"| grep 9291 | awk '{print $2}' | xargs -iabc kill -s KILL abc
+ps aux | grep "redis-server"| grep 9291 | grep -v grep | awk '{print $2}' | xargs -iabc kill -s KILL abc
 redis-server --port 9291
