f82399887645ea311f32d27622a167e4b0fe6f01
GitJournal <app@gitjournal.io>
2023-01-18 13:00:16 +0800

    [2023-01-18 05:00:13]
    FILE:
    pyjom/imagetoolbox.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/imagetoolbox.py
+++ pyjom/imagetoolbox.py
@@ -749,7 +749,8 @@ def bezierPaddleHubResnet50ImageDogCatDetectorServer(
                         import requests
                         img_bytes = requests.get(image, proxies=None, timeout=download_timeout).content
                         # warning! you deal with gif somehow!
-                        
+                        import tempfile
+                        tempfile.NamedTemporaryFile("w+",)
                         # nparr = np.fromstring(img_bytes, np.uint8)
                         # image = cv2.imdecode(nparr, flags=1)
                     elif os.path.exists(image):
