7d39bda06dd477dd6a7cf631abe13fa3d80bf89f
GitJournal <app@gitjournal.io>
2023-01-18 13:02:15 +0800

    [2023-01-18 05:02:12]
    FILE:
    pyjom/imagetoolbox.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/imagetoolbox.py
+++ pyjom/imagetoolbox.py
@@ -760,6 +760,8 @@ def bezierPaddleHubResnet50ImageDogCatDetectorServer(
                                     success, image = cap.read()
                                     if not success:
                                         image=None
+                            except:
+                                
                         # nparr = np.fromstring(img_bytes, np.uint8)
                         # image = cv2.imdecode(nparr, flags=1)
                     elif os.path.exists(image):
