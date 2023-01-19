33afe32b118c9f61fa536114e60c23ea60fffacf
GitJournal <app@gitjournal.io>
2023-01-19 17:28:58 +0800

    [2023-01-19 09:28:49]
    FILE:
    tests/render_and_recognize_long_text_to_filter_unwanted_characters/test_render.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/render_and_recognize_long_text_to_filter_unwanted_characters/test_render.py
+++ tests/render_and_recognize_long_text_to_filter_unwanted_characters/test_render.py
@@ -23,5 +23,5 @@ word_surface = font.render(textContent, False, black)
 word_width, word_height = word_surface.get_size()
 size = 
 image.fill(white)
-image.blit(word_surface)
+image.blit(word_surface,(0,0))
 pygame.image.save(image,output_name)
