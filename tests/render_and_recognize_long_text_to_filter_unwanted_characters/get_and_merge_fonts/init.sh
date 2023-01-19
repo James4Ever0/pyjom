35d8e2a66ad42e35c4dee5ca1c92238493c145b7
GitJournal <app@gitjournal.io>
2023-01-19 21:11:07 +0800

    [2023-01-19 13:10:58]
    FILE:
    tests/render_and_recognize_long_text_to_filter_unwanted_characters/get_and_merge_fonts/init.sh
    tests/render_and_recognize_long_text_to_filter_unwanted_characters/get_and_merge_fonts/fonts.log
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/render_and_recognize_long_text_to_filter_unwanted_characters/get_and_merge_fonts/init.sh
+++ tests/render_and_recognize_long_text_to_filter_unwanted_characters/get_and_merge_fonts/init.sh
@@ -1,13 +1,3 @@
 URL_PREFIX="https://github.com/satbyy/go-noto-universal/releases/download/v5.2"
 
-echo "GoNotoAfricaMiddleEast.ttf
-GoNotoAncient.ttf
-GoNotoAncientSerif.ttf
-GoNotoAsiaHistorical.ttf
-GoNotoCJKCore.ttf
-GoNotoCurrent.ttf
-GoNotoCurrentSerif.ttf
-GoNotoEastAsia.ttf
-GoNotoEuropeAmericas.ttf
-GoNotoSouthAsia.ttf
-GoNotoSouthEastAsia.ttf" | xargs -iabc curl -O -L "$URL_PREFIX/abc"
+cat fonts.log | xargs -iabc curl -O -L "$URL_PREFIX/abc"
