084a64c0fc3758c7f7814f98d3abb878f3546456
GitJournal <app@gitjournal.io>
2023-01-19 21:11:27 +0800

    [2023-01-19 13:11:19]
    FILE:
    tests/render_and_recognize_long_text_to_filter_unwanted_characters/get_and_merge_fonts/init.sh
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/render_and_recognize_long_text_to_filter_unwanted_characters/get_and_merge_fonts/init.sh
+++ tests/render_and_recognize_long_text_to_filter_unwanted_characters/get_and_merge_fonts/init.sh
@@ -1,3 +1,3 @@
-URL_PREFIX="https://github.com/satbyy/go-noto-universal/releases/download/v5.2"
+URL_PREFIX="https://proxy.zyun.vip/https://github.com/satbyy/go-noto-universal/releases/download/v5.2"
 
 cat fonts.log | xargs -iabc curl -O -L "$URL_PREFIX/abc"
