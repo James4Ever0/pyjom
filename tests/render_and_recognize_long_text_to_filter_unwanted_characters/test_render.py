122e0f7525fa912bc573238301da59935b14d12b
GitJournal <app@gitjournal.io>
2023-01-19 17:04:56 +0800

    [2023-01-19 09:04:52]
    FILE:
    tests/render_and_recognize_long_text_to_filter_unwanted_characters/test_render.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/render_and_recognize_long_text_to_filter_unwanted_characters/test_render.py
+++ tests/render_and_recognize_long_text_to_filter_unwanted_characters/test_render.py
@@ -3,6 +3,7 @@ import pygame
 fontName = "notosans"
 
 # pygame.font.get_fonts()
+# install that please? but why all lower case font names?
 
 textContent = "".join([f"{index+1}" for index in range(100)]) # will see [100] at the end of text if successful.
 
