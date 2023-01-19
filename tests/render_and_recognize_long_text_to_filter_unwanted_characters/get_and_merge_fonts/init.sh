3dd00ec504ecb79fe242abfeb767010c5cb5039e
GitJournal <app@gitjournal.io>
2023-01-19 21:08:45 +0800

    [2023-01-19 13:08:42]
    FILE:
    tests/render_and_recognize_long_text_to_filter_unwanted_characters/get_and_merge_fonts/init.sh
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/render_and_recognize_long_text_to_filter_unwanted_characters/get_and_merge_fonts/init.sh
+++ tests/render_and_recognize_long_text_to_filter_unwanted_characters/get_and_merge_fonts/init.sh
@@ -4,35 +4,13 @@ curl -O -L
 
 
 GoNotoAfricaMiddleEast.ttf
-4.53 MB
-Oct 24, 2
 GoNotoAncient.ttf
-8.65 MB
-Oct 24, 2
 GoNotoAncientSerif.ttf
-4.16 MB
-Oct 24, 2
 GoNotoAsiaHistorical.ttf
-3.54 MB
-Oct 24, 2
 GoNotoCJKCore.ttf
-17 MB
-Oct 24, 2
 GoNotoCurrent.ttf
-14.2 MB
-Oct 24, 2
 GoNotoCurrentSerif.ttf
-3.02 MB
-Oct 24, 2
 GoNotoEastAsia.ttf
-5.75 MB
-Oct 24, 2
 GoNotoEuropeAmericas.ttf
-7.56 MB
-Oct 24, 2
 GoNotoSouthAsia.ttf
-4.66 MB
-Oct 24, 2
 GoNotoSouthEastAsia.ttf
-2.51 MB
-Oct 24, 2
