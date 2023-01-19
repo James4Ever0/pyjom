e9f546b9963f0e3d9a77e1f1eecc9f30f9e256a6
GitJournal <app@gitjournal.io>
2023-01-19 17:13:33 +0800

    [2023-01-19 09:13:24]
    FILE:
    tests/render_and_recognize_long_text_to_filter_unwanted_characters/test_render.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/render_and_recognize_long_text_to_filter_unwanted_characters/test_render.py
+++ tests/render_and_recognize_long_text_to_filter_unwanted_characters/test_render.py
@@ -7,3 +7,5 @@ fontName = "notosans"
 
 textContent = "".join([f"{index+1}" for index in range(100)]) # will see [100] at the end of text if successful.
 
+fontSize = 20
+font = pygame.font.SysFont(fontName,fontSize)
