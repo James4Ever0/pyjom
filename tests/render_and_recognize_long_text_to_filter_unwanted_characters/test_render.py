22693fbe7b7b504d37e4d22583736712b389966b
GitJournal <app@gitjournal.io>
2023-01-19 17:19:17 +0800

    [2023-01-19 09:19:09]
    FILE:
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/render_and_recognize_long_text_to_filter_unwanted_characters/test_render.py
+++ tests/render_and_recognize_long_text_to_filter_unwanted_characters/test_render.py
@@ -3,7 +3,7 @@ import pygame
 # pillow can also do that
 # https://plainenglish.io/blog/generating-text-on-image-with-python-eefe4430fe77
 
-textContent = "".join(["中",""]+[f"{index+1}" for index in range(100)]) # will see [100] at the end of text if successful.
+textContent = "".join(["中","ぁ"]+[f"{index+1}" for index in range(100)]) # will see [100] at the end of text if successful.
 
 # pygame.font.get_fonts()
 # install your font to system please? but why all lower case font names?
