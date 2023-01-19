e83db18a06890a332053eb4c3e1a4bac3cc42702
GitJournal <app@gitjournal.io>
2023-01-19 09:53:04 +0800

    [2023-01-19 01:52:53]
    FILE:
    pyjom/platforms/bilibili/postMetadata.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- pyjom/platforms/bilibili/postMetadata.py
+++ pyjom/platforms/bilibili/postMetadata.py
@@ -5,7 +5,7 @@ from bilibili_api import sync, search
 from lazero.utils.tools import flattenUnhashableList  # one of my classic methods
 from lazero.utils.logger import sprint
 
-# you know the drill. if it really contains nonsuitable characters, you use notofu font for rendering and OCR for recognition
+# you know the drill. if it really contains nonacceptable characters (currently, must be some rule changes), you use notofu font for rendering and OCR for recognition
 from pyjom.languagetoolbox import filterNonChineseOrEnglishOrJapaneseCharacters
 
 def filterTitleWithCoreTopicSet(title, core_topic_set, debug=False):
