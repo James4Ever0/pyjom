5b127a6a8e46dba24dbd981abdc208bc8e543cfc
GitJournal <app@gitjournal.io>
2023-01-19 17:32:05 +0800

    [2023-01-19 09:31:56]
    FILE:
    tests/render_and_recognize_long_text_to_filter_unwanted_characters/test_render.py
    
    DIR:
    tests/blur_image_detection_mask/BlurDetection2
    tests/deepl_free_translator/deepl-api-rs
    tests/music_recognization/AmadeusCore
    tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /media/root/parrot/pyjom/tests/blur_image_detection_mask/BlurDetection2
    /media/root/parrot/pyjom/tests/deepl_free_translator/deepl-api-rs
    /media/root/parrot/pyjom/tests/music_recognization/AmadeusCore
    /media/root/parrot/pyjom/tests/skipexception_code_and_continue_resurrection_time_travel_debugging/codemod_redbaron/pasta
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/render_and_recognize_long_text_to_filter_unwanted_characters/test_render.py
+++ tests/render_and_recognize_long_text_to_filter_unwanted_characters/test_render.py
@@ -8,12 +8,13 @@ black, white = pygame.Color('black'), pygame.Color('white')
 # pillow can also do that
 # https://plainenglish.io/blog/generating-text-on-image-with-python-eefe4430fe77
 
-textContent = "".join(["中","ぁ"]+[f"{index+1}" for index in range(100)]) # will see [100] at the end of text if successful.
+textContent = "".join(["中","ぁ"]+[f"[{index+1}]" for index in range(100)]) # will see [100] at the end of text if successful.
 
 # pygame.font.get_fonts()
 # install your font to system please? but why all lower case font names?
 
-fontName = "notosans"
+# fontName = "notosans"
+# this font is bad.
 fontSize = 40
 
 font = pygame.font.SysFont(fontName,fontSize)
