f323fc6dc5d4623b9b29ad637e27679b373bbba4
jamesbrown <randomvoidmail@foxmail.com>
2023-01-23 19:46:06 +0800

    [2023-01-23 11:46:00]
    FILE:
    tests/render_and_recognize_long_text_to_filter_unwanted_characters/get_and_merge_fonts/mac_init.sh
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/render_and_recognize_long_text_to_filter_unwanted_characters/get_and_merge_fonts/mac_init.sh
+++ tests/render_and_recognize_long_text_to_filter_unwanted_characters/get_and_merge_fonts/mac_init.sh
@@ -4,6 +4,7 @@ rm -rf *.ttf
 rm -rf *.aria2
 
 #cat fonts.log | xargs -iabc aria2c -x 16 "$URL_PREFIX/abc"
+aria2c -x 16 "$URL_PREFIX/GoNotoCurrent.ttf"
 
 #cp /usr/lib/python3/dist-packages/nototools/merge_fonts.py .
 #cp /usr/lib/python3/dist-packages/nototools/merge_noto.py .
