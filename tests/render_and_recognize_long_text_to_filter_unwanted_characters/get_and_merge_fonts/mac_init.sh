e600ce7996b622720f7b17c960ca6cc47fae433f
jamesbrown <randomvoidmail@foxmail.com>
2023-01-23 19:45:52 +0800

    [2023-01-23 11:45:40]
    FILE:
    tests/render_and_recognize_long_text_to_filter_unwanted_characters/get_and_merge_fonts/mac_init.sh
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

---
+++ tests/render_and_recognize_long_text_to_filter_unwanted_characters/get_and_merge_fonts/mac_init.sh
@@ -0,0 +1,9 @@
+URL_PREFIX="https://ghdl.feizhuqwq.cf/https://github.com/satbyy/go-noto-universal/releases/download/v5.2"
+
+rm -rf *.ttf
+rm -rf *.aria2
+
+#cat fonts.log | xargs -iabc aria2c -x 16 "$URL_PREFIX/abc"
+
+#cp /usr/lib/python3/dist-packages/nototools/merge_fonts.py .
+#cp /usr/lib/python3/dist-packages/nototools/merge_noto.py .
