5ba3da1ffc939ba35ce77794fe26ef62d3d4209a
jamesbrown <randomvoidmail@foxmail.com>
2023-01-23 19:42:50 +0800

    [2023-01-23 11:42:44]
    FILE:
    tests/render_and_recognize_long_text_to_filter_unwanted_characters/get_and_merge_fonts/init.sh
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/render_and_recognize_long_text_to_filter_unwanted_characters/get_and_merge_fonts/init.sh
+++ tests/render_and_recognize_long_text_to_filter_unwanted_characters/get_and_merge_fonts/init.sh
@@ -5,5 +5,5 @@ rm -rf *.aria2
 
 cat fonts.log | xargs -iabc aria2c -x 16 "$URL_PREFIX/abc"
 
-cp /usr/lib/python3/dist-packages/nototools/merge_fonts.py .
-cp /usr/lib/python3/dist-packages/nototools/merge_noto.py .
+#cp /usr/lib/python3/dist-packages/nototools/merge_fonts.py .
+#cp /usr/lib/python3/dist-packages/nototools/merge_noto.py .
