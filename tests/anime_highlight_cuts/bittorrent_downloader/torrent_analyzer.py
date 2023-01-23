5ac506bcf40d21430422334df83bb3df3b5ecc9b
jamesbrown <randomvoidmail@foxmail.com>
2023-01-23 21:03:45 +0800

    [2023-01-23 13:03:39]
    FILE:
    tests/anime_highlight_cuts/bittorrent_downloader/torrent_analyzer.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/bittorrent_downloader/torrent_analyzer.py
+++ tests/anime_highlight_cuts/bittorrent_downloader/torrent_analyzer.py
@@ -3,4 +3,5 @@
 
 import os
 torrent_path = "[桜都字幕組] 不當哥哥了！ _ Onii-chan wa Oshimai! [01][1080p][繁體內嵌].torrent"
-torrent_path = os.path.join()
+basepath = ""
+torrent_path = os.path.join(basepath, torrent_path)
