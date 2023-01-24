d9b49622b83856c50e3e9f6bdcf3101957d8ec0a
jamesbrown <randomvoidmail@foxmail.com>
2023-01-24 22:35:21 +0800

    [2023-01-24 14:35:09]
    FILE:
    tests/anime_highlight_cuts/theme_collector/anidb_search_parse.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/anidb_search_parse.py
+++ tests/anime_highlight_cuts/theme_collector/anidb_search_parse.py
@@ -1,2 +1,2 @@
-url = https://anidb.net/anime/
-{adb.search"=Yahari%20Ore%20no%20Seishun%20Lovecome%20wa%20Machigatte%20Iru.&do.update"=Search&noalias"=1}
+url = "https://anidb.net/anime/"
+{"adb.search":query,"do.update":'Search',"noalias":1}
