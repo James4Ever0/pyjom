2e8694a71e9ffbe45a4bd4c2e90e6ab90987413f
jamesbrown <randomvoidmail@foxmail.com>
2023-01-24 21:17:55 +0800

    [2023-01-24 13:17:46]
    FILE:
    tests/anime_highlight_cuts/theme_collector/screenshot_saucenao.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/screenshot_saucenao.py
+++ tests/anime_highlight_cuts/theme_collector/screenshot_saucenao.py
@@ -27,6 +27,8 @@ with open(filepath,'rb') as f:
     part = best_data.get('part', None) # not always.
     title = best.title
     est_time =best_data.get('est_time',None) # be like: '00:16:21 / 00:25:12'
+    if est_time:
+        start_end = est_time.split("/")]
     anidb_aid = best_data.get('anidb_aid',None)
     mal_id = best_data.get('mal_id',None)
     anilist_id = best_data.get('anilist_id',None)
