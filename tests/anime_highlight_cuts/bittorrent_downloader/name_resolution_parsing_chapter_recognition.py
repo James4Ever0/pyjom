85b6bb42d4bf4797e72cd255ad566af9ea203cf9
jamesbrown <randomvoidmail@foxmail.com>
2023-01-24 15:10:39 +0800

    [2023-01-24 07:10:32]
    FILE:
    tests/anime_highlight_cuts/bittorrent_downloader/name_resolution_parsing_chapter_recognition.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/bittorrent_downloader/name_resolution_parsing_chapter_recognition.py
+++ tests/anime_highlight_cuts/bittorrent_downloader/name_resolution_parsing_chapter_recognition.py
@@ -1,3 +1,5 @@
 subtitle_types = ["ass",'srt']
 video_types = ['mkv','mov','mp4','flv','avi','ogv','webm','ts','wmv','webm','m4v','3gp']
 # use ffmpeg for subtitle extraction?
+import json
+for fname in 
