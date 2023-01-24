9ba78fc466c84d94f1a7bf2a1faeeb3ac5755d3a
jamesbrown <randomvoidmail@foxmail.com>
2023-01-24 16:44:15 +0800

    [2023-01-24 08:44:08]
    FILE:
    tests/anime_highlight_cuts/bittorrent_downloader/download_given_file_to_given_name.sh
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/bittorrent_downloader/download_given_file_to_given_name.sh
+++ tests/anime_highlight_cuts/bittorrent_downloader/download_given_file_to_given_name.sh
@@ -4,4 +4,4 @@ BASE_PATH=""
 DOWNLOAD_FILE_PATH=""
 TORRENT_PATH=""
 
-aria2c -x 16 --file-allocation=none "$TORRENT_PATH" 
+aria2c -x 16 --on-download-complete "" --file-allocation=none "$TORRENT_PATH" 
