802242420f21ba4ca28c38600ae7cd4faff08926
jamesbrown <randomvoidmail@foxmail.com>
2023-01-24 19:13:30 +0800

    [2023-01-24 11:13:21]
    FILE:
    tests/anime_highlight_cuts/theme_collector/screenshot_saucenao.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/screenshot_saucenao.py
+++ tests/anime_highlight_cuts/theme_collector/screenshot_saucenao.py
@@ -3,6 +3,6 @@
 from pysaucenao import SauceNao
 sauce = SauceNao()
 filepath = "/Users/jamesbrown/Downloads/anime_download/dress_test_pictures/女装0.jpeg"
-results = await sauce.from_file(filepath)
+results = asyncio.run_until_comppletesauce.from_file(filepath)
 # results = await sauce.from_url('https://i.imgur.com/QaKpV3s.png')
 print(results)
