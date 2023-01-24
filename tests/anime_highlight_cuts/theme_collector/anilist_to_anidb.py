bc9c8455842570c437a2572273a7c07bbe65a9ad
jamesbrown <randomvoidmail@foxmail.com>
2023-01-24 22:21:02 +0800

    [2023-01-24 14:20:53]
    FILE:
    tests/anime_highlight_cuts/theme_collector/anilist_to_anidb.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/anilist_to_anidb.py
+++ tests/anime_highlight_cuts/theme_collector/anilist_to_anidb.py
@@ -18,4 +18,5 @@ for anilist_id in anilist_ids:
     # genres = anime.get("genres", []) # not so important. we don't have understanding.
 
     # and you will search again.
-    anilist.get_anime(romaji, deepsearch=True, manual_select=True)
+    # what is this manual select?
+    animeanilist.get_anime(romaji, deepsearch=True, manual_select=True)
