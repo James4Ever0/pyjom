6bde762d90c72129c900518a80b28fab1795eeab
jamesbrown <randomvoidmail@foxmail.com>
2023-01-24 14:13:59 +0800

    [2023-01-24 06:13:50]
    FILE:
    tests/anime_highlight_cuts/bittorrent_downloader/nyaa_torrent_file_list.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/bittorrent_downloader/nyaa_torrent_file_list.py
+++ tests/anime_highlight_cuts/bittorrent_downloader/nyaa_torrent_file_list.py
@@ -11,4 +11,11 @@ json_data = utils.parse_single(request_text=r.text, site=SITE)
 
 data_class = torrent.json_to_class(json_data)
 
+import rich
+
+rich.print(json_data)
+print()
+print("_"*20)
+print()
+rich.print(data_class)
 breakpoint()
