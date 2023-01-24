5b3ffa5903b2f6820eadbbd11b5dbfa79206569a
jamesbrown <randomvoidmail@foxmail.com>
2023-01-24 18:11:19 +0800

    [2023-01-24 10:11:10]
    FILE:
    tests/anime_highlight_cuts/bittorrent_downloader/download_given_file_to_given_name.sh
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/bittorrent_downloader/download_given_file_to_given_name.sh
+++ tests/anime_highlight_cuts/bittorrent_downloader/download_given_file_to_given_name.sh
@@ -6,6 +6,7 @@ TORRENT_PATH="$BASE_PATH/[Kamigami&VCB-Studio] Yahari Ore no Seishun Lovecome wa
 echo "ps aux | grep sample.webp | grep -v grep | awk '{print \$1}' | xargs -Iabc kill -s INT abc" > kill_aria2c.sh
 FILE_ID="117"
 # timeout set to what?
-rm "$DOWNLOAD_FILE_PATH"
+# rm "$DOWNLOAD_FILE_PATH"
+# this will be ignored.
 
-aria2c -x 16 --select-file="$FILE_ID" --on-download-complete "bash kill_aria2c.sh" --file-allocation=none "$TORRENT_PATH" -o "$DOWNLOAD_FILE_PATH"
+aria2c -x 16 --select-file="$FILE_ID" --on-download-complete "bash kill_aria2c.sh" --file-allocation=none "$TORRENT_PATH" 
