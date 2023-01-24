571553f093cdd7a30cad2b41ec09587b83964ce0
jamesbrown <randomvoidmail@foxmail.com>
2023-01-24 14:28:42 +0800

    [2023-01-24 06:28:34]
    FILE:
    tests/anime_highlight_cuts/bittorrent_downloader/torrent_analyzer.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/bittorrent_downloader/torrent_analyzer.py
+++ tests/anime_highlight_cuts/bittorrent_downloader/torrent_analyzer.py
@@ -4,9 +4,9 @@
 import os
 
 # single file.
-torrent_path = "[桜都字幕組] 不當哥哥了！ _ Onii-chan wa Oshimai! [01][1080p][繁體內嵌].torrent"
+# torrent_path = "[桜都字幕組] 不當哥哥了！ _ Onii-chan wa Oshimai! [01][1080p][繁體內嵌].torrent"
 
-# torrent_path = "[Kamigami&VCB-Studio] Yahari Ore no Seishun Lovecome wa Machigatte Iru. [Ma10p_1080p].torrent"
+torrent_path = "[Kamigami&VCB-Studio] Yahari Ore no Seishun Lovecome wa Machigatte Iru. [Ma10p_1080p].torrent"
 basepath = "/Users/jamesbrown/Downloads/anime_download"
 
 torrent_path = os.path.join(basepath, torrent_path)
@@ -23,7 +23,7 @@ rich.print(data)
 
 # length will be total length?
 # data['info']['length']
-breakpoint()
+# breakpoint()
 
 # does it preserve the order?
 # for fileInfo in data['info']['files']:
