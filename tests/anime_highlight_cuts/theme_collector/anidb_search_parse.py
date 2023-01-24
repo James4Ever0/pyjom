609140e397f696ab4ff869cbff94dfab316aa6d7
jamesbrown <randomvoidmail@foxmail.com>
2023-01-24 22:41:09 +0800

    [2023-01-24 14:41:01]
    FILE:
    tests/anime_highlight_cuts/theme_collector/anidb_search_parse.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/anidb_search_parse.py
+++ tests/anime_highlight_cuts/theme_collector/anidb_search_parse.py
@@ -5,7 +5,7 @@ params = {"adb.search": query, "do.update": "Search", "noalias": 1}
 import requests
 import fake_useragent
 ua = fake_useragent.UserAgent()
-r = requests.get(url, params=params, headers={"User-Agent":ua.random})
+r = requests.get(url, params=params, headers={"User-Agent":ua.random}) # beautiful. really?
 text = r.text
 from bs4 import BeautifulSoup
 soup = BeautifulSoup(text, "html.parser")
