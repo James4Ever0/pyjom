e46b974e0d13569433d70e20ed181225fefe80f6
jamesbrown <randomvoidmail@foxmail.com>
2023-01-24 23:04:13 +0800

    [2023-01-24 15:04:05]
    FILE:
    tests/anime_highlight_cuts/theme_collector/anidb_search_parse.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/anidb_search_parse.py
+++ tests/anime_highlight_cuts/theme_collector/anidb_search_parse.py
@@ -29,5 +29,4 @@ if table:
         title = videoData['Title']
         # where's the damn link? we don't need such thing.
         aired, ended = videoData['Aired'], videoData['Ended']
-        print(title)
-        print(f'[{index}])
+        print(f'[{index}] - {title}')
