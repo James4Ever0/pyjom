6d5ab999f34f50dc40a18b911183e79d31aa3a2a
jamesbrown <randomvoidmail@foxmail.com>
2023-01-24 21:00:54 +0800

    [2023-01-24 13:00:44]
    FILE:
    tests/anime_highlight_cuts/theme_collector/screenshot_tracemoe.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/screenshot_tracemoe.py
+++ tests/anime_highlight_cuts/theme_collector/screenshot_tracemoe.py
@@ -1,2 +1,7 @@
 # anilist has typo on "Yahari Ore no Seishun Lovecome wa Machigatte Iru." which might be harmful.
 
+import requests
+requests.post("https://api.trace.moe/search",
+  data=open(imagePath, "rb"), # since this is smallest
+  headers={"Content-Type": "image/jpeg"}
+).json()
