a392e194a6c269b06fec1f2c2c434b1b1fd12ac5
jamesbrown <randomvoidmail@foxmail.com>
2023-01-24 18:01:11 +0800

    [2023-01-24 10:01:04]
    FILE:
    tests/anime_highlight_cuts/bittorrent_downloader/download_given_file_to_given_name.sh
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/bittorrent_downloader/download_given_file_to_given_name.sh
+++ tests/anime_highlight_cuts/bittorrent_downloader/download_given_file_to_given_name.sh
@@ -1,9 +1,8 @@
 # how to end downloading when finished?
 # using some command?
 BASE_PATH="/Users/jamesbrown/Downloads/anime_download"
-DOWNLOAD_FILE_PATH="$BASE_PATH/"
+DOWNLOAD_FILE_PATH="$BASE_PATH/sample.webp"
 TORRENT_PATH="$BASE_PATH/[Kamigami&VCB-Studio] Yahari Ore no Seishun Lovecome wa Machigatte Iru. [Ma10p_1080p].torrent"
 echo "" > kill_aria2c.sh
-="117"
-
-aria2c -x 16 --on-download-complete "bash kill_aria2c.sh" --file-allocation=none "$TORRENT_PATH" 
+FILE_ID="117"
+aria2c -x 16 --on-download-complete "bash kill_aria2c.sh" --file-allocation=none "$TORRENT_PATH" -o "$DOWNLOAD_FILE_PATH"
