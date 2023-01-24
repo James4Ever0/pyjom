b24594a54dded5cc23cd8439bf4d08badbee277a
jamesbrown <randomvoidmail@foxmail.com>
2023-01-24 14:12:59 +0800

    [2023-01-24 06:12:52]
    FILE:
    tests/anime_highlight_cuts/bittorrent_downloader/nyaa_torrent_file_list.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/bittorrent_downloader/nyaa_torrent_file_list.py
+++ tests/anime_highlight_cuts/bittorrent_downloader/nyaa_torrent_file_list.py
@@ -2,7 +2,10 @@ url = "https://nyaa.si/view/1627038"
 
 import requests
 
-from NyaaPy import utils
+from NyaaPy import utils, torrent
+
+r = requests.get(url)
+SITE = 
 json_data = utils.parse_single(request_text=r.text, site=self.SITE)
 
 return torrent.json_to_class(json_data)
