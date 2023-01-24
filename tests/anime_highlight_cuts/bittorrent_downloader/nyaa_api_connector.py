6eabd47040c18af54d6684e3032d53b500211914
jamesbrown <randomvoidmail@foxmail.com>
2023-01-24 10:21:07 +0800

    [2023-01-24 02:21:01]
    FILE:
    tests/anime_highlight_cuts/bittorrent_downloader/nyaa_api_connector.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/bittorrent_downloader/nyaa_api_connector.py
+++ tests/anime_highlight_cuts/bittorrent_downloader/nyaa_api_connector.py
@@ -6,7 +6,7 @@ import requests
 url = "https://nyaa.si"
 
 query = "oniichan wa oshimai! 01"
-s = "seeders"
+sort_term = "seeders"
 category='1_0' # anime
 params = dict(f=0,c=category,q=query,s=sort_term, o="desc")
 
