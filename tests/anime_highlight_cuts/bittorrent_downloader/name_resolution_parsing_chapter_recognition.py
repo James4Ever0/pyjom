162e4b57058d00d001b899c897cef741ced566ba
jamesbrown <randomvoidmail@foxmail.com>
2023-01-24 16:13:00 +0800

    [2023-01-24 08:12:52]
    FILE:
    tests/anime_highlight_cuts/bittorrent_downloader/name_resolution_parsing_chapter_recognition.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/bittorrent_downloader/name_resolution_parsing_chapter_recognition.py
+++ tests/anime_highlight_cuts/bittorrent_downloader/name_resolution_parsing_chapter_recognition.py
@@ -46,6 +46,7 @@ for fname in fnames:
             break
     print(f"<{current_file_type}> {fname}")
     print(fname_lower_alphanumeric)
-    numbers = re.fname_lower_alphanumeric
+    numbers = re.search(r"\d+",fname_lower_alphanumeric)
+    print(?NUMBERS?)
     substring_location_start = fname_lower_alphanumeric.find(bangume_name_lower_alphanumeric)
     substring_location_end = 
