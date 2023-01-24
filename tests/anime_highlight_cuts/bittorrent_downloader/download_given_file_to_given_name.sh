3a2458e5faab3e48c17f131d076e1cc351bbbbac
jamesbrown <randomvoidmail@foxmail.com>
2023-01-24 18:00:05 +0800

    [2023-01-24 09:59:57]
    FILE:
    tests/anime_highlight_cuts/bittorrent_downloader/download_given_file_to_given_name.sh
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/bittorrent_downloader/download_given_file_to_given_name.sh
+++ tests/anime_highlight_cuts/bittorrent_downloader/download_given_file_to_given_name.sh
@@ -4,4 +4,6 @@ BASE_PATH="/Users/jamesbrown/Downloads/anime_download"
 DOWNLOAD_FILE_PATH="$BASE_PATH/"
 TORRENT_PATH="$BASE_PATH/[Kamigami&VCB-Studio] Yahari Ore no Seishun Lovecome wa Machigatte Iru. [Ma10p_1080p].torrent"
 echo "" > kill_aria2c.sh
+="117"
+
 aria2c -x 16 --on-download-complete "bash kill_aria2c.sh" --file-allocation=none "$TORRENT_PATH" 
