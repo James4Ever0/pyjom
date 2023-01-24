72f82c36c947d130e3084dd967e5c3746d3f1eb3
jamesbrown <randomvoidmail@foxmail.com>
2023-01-24 18:24:40 +0800

    [2023-01-24 10:24:32]
    FILE:
    tests/anime_highlight_cuts/bittorrent_downloader/download_given_file_to_given_name.sh
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/bittorrent_downloader/download_given_file_to_given_name.sh
+++ tests/anime_highlight_cuts/bittorrent_downloader/download_given_file_to_given_name.sh
@@ -5,7 +5,7 @@ BASE_PATH="/Users/jamesbrown/Downloads/anime_download"
 TORRENT_NAME="[Kamigami&VCB-Studio] Yahari Ore no Seishun Lovecome wa Machigatte Iru. [Ma10p_1080p]"
 # torrent name might be different.
 TORRENT_PATH="$BASE_PATH/$TORRENT_NAME.torrent"
-echo "ps aux | grep '$TORRENT_NAME' | grep -v grep | awk '{print \$1}' | xargs -Iabc kill -s INT abc" > kill_aria2c.sh
+# echo "ps aux | grep '$TORRENT_NAME' | grep -v grep | awk '{print \$1}' | xargs -Iabc kill -s INT abc" > kill_aria2c.sh
 FILE_ID="117"
 
 # timeout set to what?
