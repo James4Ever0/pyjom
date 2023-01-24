1534653deb9e8478a9d0729819d5432838b6ef05
jamesbrown <randomvoidmail@foxmail.com>
2023-01-24 22:36:43 +0800

    [2023-01-24 14:36:30]
    FILE:
    tests/anime_highlight_cuts/theme_collector/anidb_search_parse.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/anidb_search_parse.py
+++ tests/anime_highlight_cuts/theme_collector/anidb_search_parse.py
@@ -1,7 +1,8 @@
 url = "https://anidb.net/anime/"
 query = "Yahari Ore no Seishun Lovecome wa Machigatte Iru."
-params = {"adb.search":query,"do.update":'Search',"noalias":1}
+params = {"adb.search": query, "do.update": "Search", "noalias": 1}
 
 import requests
 
 r = requests.get(url, params=params)
+text = r.text
