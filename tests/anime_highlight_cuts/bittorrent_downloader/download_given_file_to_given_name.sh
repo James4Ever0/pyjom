08cb0d50b3cc4d86ee93a13a1973851a299676e9
jamesbrown <randomvoidmail@foxmail.com>
2023-01-24 18:16:57 +0800

    [2023-01-24 10:16:49]
    FILE:
    tests/anime_highlight_cuts/bittorrent_downloader/download_given_file_to_given_name.sh
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/bittorrent_downloader/download_given_file_to_given_name.sh
+++ tests/anime_highlight_cuts/bittorrent_downloader/download_given_file_to_given_name.sh
@@ -2,8 +2,8 @@
 # using some command?
 BASE_PATH="/Users/jamesbrown/Downloads/anime_download"
 # DOWNLOAD_FILE_PATH="$BASE_PATH/sample.webp"
-TORRENT_NAME="[Kamigami&VCB-Studio] Yahari Ore no Seishun Lovecome wa Machigatte Iru. [Ma10p_1080p].torrent"
-TORRENT_PATH="$BASE_PATH/$TORRENT_NAME"
+TORRENT_NAME="[Kamigami&VCB-Studio] Yahari Ore no Seishun Lovecome wa Machigatte Iru. [Ma10p_1080p]"
+TORRENT_PATH="$BASE_PATH/$TORRENT_NAME.torrent"
 echo "ps aux | grep '$TORRENT_NAME' | grep -v grep | awk '{print \$1}' | xargs -Iabc kill -s INT abc" > kill_aria2c.sh
 FILE_ID="117"
 
