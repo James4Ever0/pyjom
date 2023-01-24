e28c5ae7ebf01dc9f72a63cbe0fff45b8a3a62a4
jamesbrown <randomvoidmail@foxmail.com>
2023-01-24 10:16:56 +0800

    [2023-01-24 02:16:49]
    FILE:
    tests/anime_highlight_cuts/bittorrent_downloader/nyaa_api_connector.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/bittorrent_downloader/nyaa_api_connector.py
+++ tests/anime_highlight_cuts/bittorrent_downloader/nyaa_api_connector.py
@@ -6,6 +6,7 @@ import requests
 url = "https://nyaa.si"
 
 query = "oniichan"
-params = dict(f=0,c="1_0",q=query,s=sort)
+s = 
+params = dict(f=0,c="1_0",q=query,s=sort_term, o="desc")
 
 # better parse it yourself first huh?
