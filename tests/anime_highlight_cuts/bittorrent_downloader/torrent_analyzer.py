b8eb10c3be8b39f41bc3570752fd0f01dbaec04e
jamesbrown <randomvoidmail@foxmail.com>
2023-01-24 14:23:33 +0800

    [2023-01-24 06:23:25]
    FILE:
    tests/anime_highlight_cuts/bittorrent_downloader/torrent_analyzer.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/bittorrent_downloader/torrent_analyzer.py
+++ tests/anime_highlight_cuts/bittorrent_downloader/torrent_analyzer.py
@@ -3,7 +3,10 @@
 
 import os
 
-torrent_path = "[桜都字幕組] 不當哥哥了！ _ Onii-chan wa Oshimai! [01][1080p][繁體內嵌].torrent"
+# single file.
+# torrent_path = "[桜都字幕組] 不當哥哥了！ _ Onii-chan wa Oshimai! [01][1080p][繁體內嵌].torrent"
+
+torrent_path = "[Kamigami&VCB-Studio] Yahari Ore no Seishun Lovecome wa Machigatte Iru. [Ma10p_1080p].torrent"
 basepath = "/Users/jamesbrown/Downloads/anime_download"
 
 torrent_path = os.path.join(basepath, torrent_path)
