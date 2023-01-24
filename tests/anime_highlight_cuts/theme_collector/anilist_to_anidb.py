30ddbbb1158932361b8b8ba7a9067a7d66a1773c
jamesbrown <randomvoidmail@foxmail.com>
2023-01-24 22:05:31 +0800

    [2023-01-24 14:05:23]
    FILE:
    tests/anime_highlight_cuts/theme_collector/anilist_to_anidb.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/anilist_to_anidb.py
+++ tests/anime_highlight_cuts/theme_collector/anilist_to_anidb.py
@@ -3,6 +3,7 @@ anilist_ids = 112788
 # first let's get name.
 from AnilistPython import Anilist
 anilist = Anilist()
+
 for anilist_id in anilist_ids:
     anime = anilist.get_anime_with_id(anilist_id)
     # what about alias?
