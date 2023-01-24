5db11dbafd753669e87bc3aa025ad3857db7ea76
jamesbrown <randomvoidmail@foxmail.com>
2023-01-24 23:19:38 +0800

    [2023-01-24 15:19:29]
    FILE:
    tests/anime_highlight_cuts/theme_collector/anidb_anime_parse.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/anidb_anime_parse.py
+++ tests/anime_highlight_cuts/theme_collector/anidb_anime_parse.py
@@ -4,4 +4,7 @@ url = "https://anidb.net/anime/9310"
 import requests
 import fake_useragent
 ua = fake_useragent.UserAgent()
-r = requests.get(url, params=params, headers={"User-Agent":ua.random}) #
+r = requests.get(url, headers={"User-Agent":ua.random}) 
+
+text = r.text
+with open("")
