b41d210a9f5e127013d899b54d046307dee47a43
jamesbrown <randomvoidmail@foxmail.com>
2023-01-24 19:13:08 +0800

    [2023-01-24 11:13:01]
    FILE:
    tests/anime_highlight_cuts/theme_collector/screenshot_saucenao.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/screenshot_saucenao.py
+++ tests/anime_highlight_cuts/theme_collector/screenshot_saucenao.py
@@ -2,7 +2,7 @@
 # use proxies, since we are using free tiers.
 from pysaucenao import SauceNao
 sauce = SauceNao()
-
-results = await sauce.from_file('/path/to/image.png')
+filepath = "/Users/jamesbrown/Downloads/anime_download/dress_test_pictures/女装0.jpeg"
+results = await sauce.from_file(filepath)
 # results = await sauce.from_url('https://i.imgur.com/QaKpV3s.png')
 print(results)
