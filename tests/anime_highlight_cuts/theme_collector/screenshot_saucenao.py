2b8b07df40f24f8622a6b7cdc1143717c599373e
jamesbrown <randomvoidmail@foxmail.com>
2023-01-24 19:23:16 +0800

    [2023-01-24 11:23:03]
    FILE:
    tests/anime_highlight_cuts/theme_collector/screenshot_saucenao.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/screenshot_saucenao.py
+++ tests/anime_highlight_cuts/theme_collector/screenshot_saucenao.py
@@ -4,11 +4,11 @@ import os
 SAUCENAO_API_KEY=os.environ.get('SAUCENAO_API_KEY')
 print("API KEY?", SAUCENAO_API_KEY)
 
-sauce = SauceNao(api_key=SAUCENAO_API_KEY) # shit. not working!
+# sauce = SauceNao(api_key=SAUCENAO_API_KEY) # shit. not working!
 filepath = "/Users/jamesbrown/Downloads/anime_download/dress_test_pictures/女装0.jpeg"
-import asyncio
-loop = asyncio.get_event_loop()
-results = loop.run_until_complete(sauce.from_file(filepath))
+# import asyncio
+# loop = asyncio.get_event_loop()
+# results = loop.run_until_complete(sauce.from_file(filepath))
 # results = await sauce.from_url('https://i.imgur.com/QaKpV3s.png')
 # no api key. fuck.
 print(results)
