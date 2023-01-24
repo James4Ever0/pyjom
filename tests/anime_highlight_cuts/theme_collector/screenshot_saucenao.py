bceb7094beb14481d3aacfb68b2bfd2b648911af
jamesbrown <randomvoidmail@foxmail.com>
2023-01-24 19:11:42 +0800

    [2023-01-24 11:11:34]
    FILE:
    tests/anime_highlight_cuts/theme_collector/screenshot_saucenao.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/screenshot_saucenao.py
+++ tests/anime_highlight_cuts/theme_collector/screenshot_saucenao.py
@@ -1,2 +1,8 @@
 # saucenao (if fail, use trace.moe)
 # use proxies, since we are using free tiers.
+from pysaucenao import SauceNao
+sauce = SauceNao()
+
+# results = await sauce.from_file('/path/to/image.png')
+results = await sauce.from_url('https://i.imgur.com/QaKpV3s.png')
+repr(results)
