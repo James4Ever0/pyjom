29c0f197eaadc829f822b5f98b1aa670dd733929
jamesbrown <randomvoidmail@foxmail.com>
2023-01-24 15:46:34 +0800

    [2023-01-24 07:46:27]
    FILE:
    tests/anime_highlight_cuts/bittorrent_downloader/name_resolution_parsing_chapter_recognition.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/bittorrent_downloader/name_resolution_parsing_chapter_recognition.py
+++ tests/anime_highlight_cuts/bittorrent_downloader/name_resolution_parsing_chapter_recognition.py
@@ -1,24 +1,37 @@
-subtitle_types = ["ass",'srt']
-video_types = ['mkv','mov','mp4','flv','avi','ogv','webm','ts','wmv','webm','m4v','3gp']
+subtitle_types = ["ass", "srt"]
+video_types = [
+    "mkv",
+    "mov",
+    "mp4",
+    "flv",
+    "avi",
+    "ogv",
+    "webm",
+    "ts",
+    "wmv",
+    "webm",
+    "m4v",
+    "3gp",
+]
 # use ffmpeg for subtitle extraction?
-filetypes = {'subtitle':subtitle_types,'video':video_types}
+filetypes = {"subtitle": subtitle_types, "video": video_types}
 
-Bangumi_Name = 'Yahari Ore no Seishun Lovecome wa Machigatte Iru.'
+Bangumi_Name = "Yahari Ore no Seishun Lovecome wa Machigatte Iru."
 episodeIndex = 3
 
-chinese_simplified_sub_types = ['chs','简体','简日']
-chinese_simplified_sub_types = [繁日 繁体 繁體'cht'
+chinese_simplified_sub_types = ["chs", "简体", "简日"]
+chinese_traditional_sub_types = ["繁日", "繁体", "繁體", "cht"]
 import json
 
 # replace non-alphanumeric charcters.
 episode_formatter = lambda episode_index: str(episode_index).zfill(2)
 
-with open("test_filenames.json",'r') as f:
+with open("test_filenames.json", "r") as f:
     fnames = json.loads(f.read())
 for fname in fnames:
     fname_lower = fname.lower()
     file_extension = fname_lower.split(".")[-1]
-    current_file_type= "unknown"
+    current_file_type = "unknown"
 
     for filetype, file_extensions in filetypes.items():
         if file_extension in file_extensions:
