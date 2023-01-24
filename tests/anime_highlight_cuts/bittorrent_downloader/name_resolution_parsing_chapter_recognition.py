ebfd4bf88899f569026c23234a7efec9f429b576
jamesbrown <randomvoidmail@foxmail.com>
2023-01-24 15:29:53 +0800

    [2023-01-24 07:29:47]
    FILE:
    tests/anime_highlight_cuts/bittorrent_downloader/name_resolution_parsing_chapter_recognition.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/bittorrent_downloader/name_resolution_parsing_chapter_recognition.py
+++ tests/anime_highlight_cuts/bittorrent_downloader/name_resolution_parsing_chapter_recognition.py
@@ -3,7 +3,7 @@ video_types = ['mkv','mov','mp4','flv','avi','ogv','webm','ts','wmv','webm','m4v
 # use ffmpeg for subtitle extraction?
 filetypes = {'subtitle':subtitle_types,'video':video_types}
 
-Bangumi_Name = 
+Bangumi_Name = 'Yahari Ore no Seishun Lovecome wa Machigatte Iru.'
 
 import json
 with open("test_filenames.json",'r') as f:
