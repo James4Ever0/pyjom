1ee7ed7e82c54fdbb9c463a802a8710a7dcd7965
jamesbrown <randomvoidmail@foxmail.com>
2023-01-24 23:35:36 +0800

    [2023-01-24 15:35:27]
    FILE:
    tests/anime_highlight_cuts/theme_collector/anidb_anime_parse.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/anidb_anime_parse.py
+++ tests/anime_highlight_cuts/theme_collector/anidb_anime_parse.py
@@ -14,6 +14,7 @@ text = f.read()
 
 from bs4 import BeautifulSoup
 soup = BeautifulSoup(text, 'html.parser')
+# must be non-empty.
 similarAnime = str(soup.select("#similaranime")[0])
 indirectRelated = str(soup.select('#relations_indirect')[0])
 directRelated = str(soup.select('#relations_direct')[0])
