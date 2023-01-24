7c72d881459a1b21a38f7c43d2115b50b120278b
jamesbrown <randomvoidmail@foxmail.com>
2023-01-24 14:30:12 +0800

    [2023-01-24 06:30:03]
    FILE:
    tests/anime_highlight_cuts/bittorrent_downloader/torrent_analyzer.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/bittorrent_downloader/torrent_analyzer.py
+++ tests/anime_highlight_cuts/bittorrent_downloader/torrent_analyzer.py
@@ -31,4 +31,4 @@ for index, fileInfo in enumerate(data['info']['files']):
     length = fileInfo['length']
     path = fileInfo['path'] # multiple strings in a list
     joined_path = "/".join(path)
-    
+    print("[{}] ** {}")
