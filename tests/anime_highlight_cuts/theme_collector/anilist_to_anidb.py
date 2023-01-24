a53f1541199bc3009cf1285a00baafefc98d90bd
jamesbrown <randomvoidmail@foxmail.com>
2023-01-24 22:16:44 +0800

    [2023-01-24 14:16:35]
    FILE:
    tests/anime_highlight_cuts/theme_collector/anilist_to_anidb.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/anilist_to_anidb.py
+++ tests/anime_highlight_cuts/theme_collector/anilist_to_anidb.py
@@ -1,14 +1,17 @@
-anilist_ids = [112788, #海边的异邦人
-14813 #Yahari Ore no Seishun Love Come wa Machigatteiru.
+anilist_ids = [
+    112788,  # 海边的异邦人
+    14813,  # Yahari Ore no Seishun Love Come wa Machigatteiru.
 ]
 
 # first let's get name.
 from AnilistPython import Anilist
+
 anilist = Anilist()
 
 for anilist_id in anilist_ids:
     anime = anilist.get_anime_with_id(anilist_id)
     # what about alias?
     print(anime)
-    print("="*20)
-    romaji = anime['name_romaji']
+    print("=" * 20)
+    romaji = anime.get("name_romaji", None)
+    english = anime.get("name_english", None)
