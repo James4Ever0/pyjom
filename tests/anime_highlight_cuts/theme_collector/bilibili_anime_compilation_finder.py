8e04d31d062e83fa11891e2b7caeabc6a8c7cffe
jamesbrown <randomvoidmail@foxmail.com>
2023-01-25 16:38:01 +0800

    [2023-01-25 08:37:53]
    FILE:
    tests/anime_highlight_cuts/theme_collector/bilibili_anime_compilation_finder.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/bilibili_anime_compilation_finder.py
+++ tests/anime_highlight_cuts/theme_collector/bilibili_anime_compilation_finder.py
@@ -2,4 +2,15 @@
 # you may find uploaders, keywords, tags, recommended videos, video collections/playlists and filter by analyzers (check if is in the target format)
 
 tags = ['动画','综合','名场面','补番推荐','动漫盘点'] # 其他标签就是和视频主题有关的 属于小分类
-tags = []
+tags = [
+补番推荐
+万恶之源
+动画
+动漫杂谈
+二次元
+动漫
+多素材
+盘点排行
+综合
+综漫
+名场
