6748882695d864fc27f8f99c5be9112f37823c6e
jamesbrown <randomvoidmail@foxmail.com>
2023-01-25 16:59:49 +0800

    [2023-01-25 08:59:43]
    FILE:
    tests/anime_highlight_cuts/theme_collector/bilibili_anime_compilation_finder.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/bilibili_anime_compilation_finder.py
+++ tests/anime_highlight_cuts/theme_collector/bilibili_anime_compilation_finder.py
@@ -2,7 +2,7 @@
 # you may find uploaders, keywords, tags, recommended videos, video collections/playlists and filter by analyzers (check if is in the target format)
 
 # 其他标签就是和视频主题有关的 属于小分类
-tags = { # select 1 for each.
+tags = {  # select 1 for each.
     "static": [
         ["二次元", "动画", "动漫"],
         [
@@ -13,8 +13,8 @@ tags = { # select 1 for each.
             "综漫",
         ],
     ],
-    "optional": [ # select one or not select any.
-        ["动漫推荐", "补番推荐", "动漫盘点", "盘点排行", "盘点推荐",'新番推荐'],
+    "optional": [  # select one or not select any.
+        ["动漫推荐", "补番推荐", "动漫盘点", "盘点排行", "盘点推荐", "新番推荐"],
         ["名场", "名场面", "万恶之源"],
     ],
     # these are seeds. you may have different tags added along the way.
