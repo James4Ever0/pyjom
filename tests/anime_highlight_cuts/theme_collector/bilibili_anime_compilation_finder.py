b974cfb4261e74093ab77977b62bd8359f7a6d6d
jamesbrown <randomvoidmail@foxmail.com>
2023-01-25 16:35:28 +0800

    [2023-01-25 08:35:19]
    FILE:
    tests/anime_highlight_cuts/theme_collector/bilibili_anime_compilation_finder.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/bilibili_anime_compilation_finder.py
+++ tests/anime_highlight_cuts/theme_collector/bilibili_anime_compilation_finder.py
@@ -1,2 +1,4 @@
 # find our target video.
 # you may find uploaders, keywords, tags, recommended videos, video collections/playlists and filter by analyzers (check if is in the target format)
+
+tags = ['动画','','']
