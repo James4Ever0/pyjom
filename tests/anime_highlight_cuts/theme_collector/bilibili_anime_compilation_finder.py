ee99e06cb78bf07a87186ab8900eb84b6bb8eb41
jamesbrown <randomvoidmail@foxmail.com>
2023-01-25 16:57:41 +0800

    [2023-01-25 08:57:35]
    FILE:
    tests/anime_highlight_cuts/theme_collector/bilibili_anime_compilation_finder.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/bilibili_anime_compilation_finder.py
+++ tests/anime_highlight_cuts/theme_collector/bilibili_anime_compilation_finder.py
@@ -14,7 +14,7 @@ tags = { # select 1 for each.
         ],
     ],
     "optional": [ # select or not select any.
-        ["动漫推荐", "补番推荐", "动漫盘点", "盘点排行", "盘点推荐"],
+        ["动漫推荐", "补番推荐", "动漫盘点", "盘点排行", "盘点推荐",'新番推荐'],
         ["名场", "名场面", "万恶之源"],
     ],
     # these are seeds. you may have different tags added along the way.
