cdccbd0680c56fbb13106359b23c8998acb215de
jamesbrown <randomvoidmail@foxmail.com>
2023-01-25 18:49:24 +0800

    [2023-01-25 10:49:16]
    FILE:
    tests/anime_highlight_cuts/theme_collector/anime_compilation_video_checker.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/anime_compilation_video_checker.py
+++ tests/anime_highlight_cuts/theme_collector/anime_compilation_video_checker.py
@@ -10,7 +10,7 @@ import re
 from pymonad import maybe
 
 def extractBVID(chars:str):
-    bvid = maybe(re.findall(r"/(BV[a-zA-Z0-9]+)", "https://www.bilibili.com/video/BV1e54y1y7qy"))[0]
+    bvid = maybe(re.findall(r"/(BV[a-zA-Z0-9]+)",chars))[0]
     return bvid
 
 ## remember the video is always scrapable via av id.
