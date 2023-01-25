79971f59e8f211c54ecd82cf86f079eabc3691a2
jamesbrown <randomvoidmail@foxmail.com>
2023-01-25 17:16:41 +0800

    [2023-01-25 09:16:35]
    FILE:
    tests/anime_highlight_cuts/theme_collector/anime_compilation_video_checker.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/anime_compilation_video_checker.py
+++ tests/anime_highlight_cuts/theme_collector/anime_compilation_video_checker.py
@@ -9,3 +9,5 @@ videoLink = "https://www.bilibili.com/video/BV1e54y1y7qy" # 女攻男受 emm
 # why i need to download whole damn video? i need to cut it into bite-sized video!
 
 # for some video there's no possibility to determine the source.
+# let's see the video metadata.
+
