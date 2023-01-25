9b94fcbcca4098f424bee413ca074c68a22d8a55
jamesbrown <randomvoidmail@foxmail.com>
2023-01-25 23:17:39 +0800

    [2023-01-25 15:17:30]
    FILE:
    tests/anime_highlight_cuts/theme_collector/anime_compilation_video_checker.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/anime_compilation_video_checker.py
+++ tests/anime_highlight_cuts/theme_collector/anime_compilation_video_checker.py
@@ -58,8 +58,8 @@ for videoLink in videoLinks:
     r.raise_for_status()
 
     data = r.json()
-    assert data['code'] == 0
     # it must be json.
     import rich
     rich.print(data)
+    assert data['code'] == 0
     breakpoint()
