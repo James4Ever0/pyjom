675cac3c5fac7a02d3568d8626a7897f72e8cbd0
jamesbrown <randomvoidmail@foxmail.com>
2023-01-25 13:31:55 +0800

    [2023-01-25 05:31:46]
    FILE:
    tests/anime_highlight_cuts/theme_collector/anidb_anime_parse.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/anidb_anime_parse.py
+++ tests/anime_highlight_cuts/theme_collector/anidb_anime_parse.py
@@ -4,41 +4,50 @@ from black import NothingChanged
 url = "https://anidb.net/anime/9310"
 from pymonad.maybe import Nothing, Just
 
+
 def checkNothing(value):
-    if value in [None, 0, -1, [], {},()]:
+    if value in [None, 0, -1, [], {}, ()]:
         return Nothing
     return Just(value)
 
+
 import requests
 import fake_useragent
+
 ua = fake_useragent.UserAgent()
-r = requests.get(url, headers={"User-Agent":ua.random}) 
+r = requests.get(url, headers={"User-Agent": ua.random})
 
 text = r.text
-with open("anidb_info.html", 'w+') as f:
+with open("anidb_info.html", "w+") as f:
     f.write(text)
 
 text = f.read()
 
 from bs4 import BeautifulSoup
-soup = BeautifulSoup(text, 'html.parser')
+
+soup = BeautifulSoup(text, "html.parser")
 # must be non-empty.
-similarAnime = soup.find(attrs={"id":"similaranime"})
-indirectRelated = soup.find(attrs={"id":"relations_indirect"})
-directRelated = soup.find(attrs={"id":"relations_direct"}) # it could be none.
+similarAnime = soup.find(attrs={"id": "similaranime"})
+indirectRelated = soup.find(attrs={"id": "relations_indirect"})
+directRelated = soup.find(attrs={"id": "relations_direct"})  # it could be none.
 
-tables = soup.find_all('table') # shit.
+tables = soup.find_all("table")  # shit.
 
-videoInfo = checkNothing(soup.find("div", attrs={"class":["pane", "info"]})).maybe(None, lambda x: x.find("table"))
-if videoInfo:
-    videoInfo = videoInfo.find('table')
-videoTitles = checkNothing(soup.find("div", attrs={"class":["pane", "titles"]})).maybe(None, lambda x: x.find("table"))
-if videoTitles:
-    videoTitles = videoTitles.find('table')
+videoInfo = checkNothing(soup.find("div", attrs={"class": ["pane", "info"]})).maybe(
+    None, lambda x: x.find("table")
+)
+# if videoInfo:
+# videoInfo = videoInfo.find('table')
+videoTitles = checkNothing(soup.find("div", attrs={"class": ["pane", "titles"]})).maybe(
+    None, lambda x: x.find("table")
+)
+# if videoTitles:
+# videoTitles = videoTitles.find('table')
 
 # i think monad is good.
 
 import pandas
+
 SAData = pandas.read_html(similarAnime)
 print(SAData)
 breakpoint()
