1b08b6cfe045820027d787614dc37df59e6a8965
jamesbrown <randomvoidmail@foxmail.com>
2023-01-25 18:52:20 +0800

    [2023-01-25 10:52:06]
    FILE:
    tests/anime_highlight_cuts/theme_collector/anime_compilation_video_checker.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/anime_compilation_video_checker.py
+++ tests/anime_highlight_cuts/theme_collector/anime_compilation_video_checker.py
@@ -4,7 +4,7 @@ videoLinks = [
     "https://www.bilibili.com/video/BV1e54y1y7qy",  # 女攻男受 emm
     "https://www.bilibili.com/video/BV1P441197oV",  # in which you shall never find anything interesting.
     "https://www.bilibili.com/video/BV1Fs411k7e9", # multiple chapters, you shall not find this interesting.
-    "" # 
+    "https://www.bilibili.com/video/av5842509" # aid version of video link.
 ]
 
 import re
