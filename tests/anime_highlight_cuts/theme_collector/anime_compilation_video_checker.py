3863bc3cf162c2625996fb847ffbf68eb34b0a33
jamesbrown <randomvoidmail@foxmail.com>
2023-01-25 23:21:54 +0800

    [2023-01-25 15:21:46]
    FILE:
    tests/anime_highlight_cuts/theme_collector/anime_compilation_video_checker.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/anime_compilation_video_checker.py
+++ tests/anime_highlight_cuts/theme_collector/anime_compilation_video_checker.py
@@ -55,6 +55,7 @@ for videoLink in videoLinks:
             continue
     
     print("PARAMS?",params)
+    r = requests.get(url,data=params)
     # r = requests.get(url,data=params,headers={"User-Agent":ua.random})
     r = requests.get("https://api.bilibili.com/x/web-interface/view?bvid=BV1e54y1y7qy")
     r.raise_for_status()
