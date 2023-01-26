84b31887bf2dcd21912535609fbe19b334f8b5e8
jamesbrown <randomvoidmail@foxmail.com>
2023-01-26 22:11:51 +0800

    [2023-01-26 14:11:44]
    FILE:
    tests/anime_highlight_cuts/theme_collector/view_boundingbox.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/view_boundingbox.py
+++ tests/anime_highlight_cuts/theme_collector/view_boundingbox.py
@@ -1,10 +1,11 @@
-x,y,w,h=[1118.5,       545.5,        1585 ,       1069]
-min_x, min_y =int(x-(w/2)), int(y-(h/2))
+x, y, w, h = [1118.5, 545.5, 1585, 1069]
+min_x, min_y = int(x - (w / 2)), int(y - (h / 2))
 import cv2
-imagePath=""
+
+imagePath = ""
 
 image = cv2.imread(imagePath)
 p0, p1 = (min_x, min_y), (min_x + w, min_y + h)
 
-cv2.rectangle(image, p0,p1,(0,255,0),3)
-cv2.imshow("PIP",image)
+cv2.rectangle(image, p0, p1, (0, 255, 0), 3)
+cv2.imshow("PIP", image)
