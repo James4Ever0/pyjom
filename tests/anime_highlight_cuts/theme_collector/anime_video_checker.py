f2cab1514c53c4bcf4504ded6adcf545e0b84854
jamesbrown <randomvoidmail@foxmail.com>
2023-01-26 13:56:17 +0800

    [2023-01-26 05:56:09]
    FILE:
    tests/anime_highlight_cuts/theme_collector/anime_video_checker.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/anime_video_checker.py
+++ tests/anime_highlight_cuts/theme_collector/anime_video_checker.py
@@ -7,6 +7,7 @@ url = f"https://www.bilibili.com/video/{videoID}" # only one single page.
 # 290 seconds.
 # section example:
 # 0:05:00-0:06:30
-cmd=f'yt-dlp --download-sections "*{start}-{end}" --playlist-items "1"'
+items='1'
+cmd=f'yt-dlp --download-sections "*{start}-{end}" --playlist-items "{items}" "{url}"'
 
 os.system(cmd)
