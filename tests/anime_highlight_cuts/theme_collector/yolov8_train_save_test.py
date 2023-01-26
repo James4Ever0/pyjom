1575b8e65793bd540a612ff49e892c9a73672a2a
jamesbrown <randomvoidmail@foxmail.com>
2023-01-26 20:17:50 +0800

    [2023-01-26 12:17:42]
    FILE:
    tests/anime_highlight_cuts/theme_collector/yolov8_train_save_test.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/yolov8_train_save_test.py
+++ tests/anime_highlight_cuts/theme_collector/yolov8_train_save_test.py
@@ -1,4 +1,5 @@
 from ultralytics import YOLO
+
 # pip install opencv-python==4.5.5.64
 # shit?
 # https://github.com/asweigart/pyautogui/issues/706
@@ -11,12 +12,13 @@ model = YOLO("yolov8n.pt")
 
 # breakpoint()
 import rich
-train_result = model.train(epochs=3,data="./pip_dataset/pip_dataset.yaml")
+
+train_result = model.train(epochs=3, data="./pip_dataset/pip_dataset.yaml")
 
 print("TRAIN RESULT?")
 rich.print(train_result)
 
-val_result =model.val()
+val_result = model.val()
 
 print("VALIDATION RESULT?")
 rich.print(val_result)
@@ -24,11 +26,14 @@ rich.print(val_result)
 test_result = model("./pip_dataset/images/test/000000003099.png")
 test_boxes = test_result[0].boxes
 
-test_classes, test_xywh, test_confidence = test_boxes.cls.numpy(), test_boxes.xywh.numpy(), test_boxes.conf.numpy()
+test_classes, test_xywh, test_confidence = (
+    test_boxes.cls.numpy(),
+    test_boxes.xywh.numpy(),
+    test_boxes.conf.numpy(),
+)
 
 print("XYWH?", test_xywh)
 print("CLASSES?", test_classes)
 print("CONFIDENCE?", test_confidence)
 
-
-model.export(format='pytorch',path='./pip_detector.pth')
+model.export(format="pytorch", path="./pip_detector.pth")
