677c8e8b122ffb4d64195798a692ec1692b72410
jamesbrown <randomvoidmail@foxmail.com>
2023-01-26 22:06:07 +0800

    [2023-01-26 14:05:56]
    FILE:
    tests/anime_highlight_cuts/theme_collector/view_boundingbox.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/view_boundingbox.py
+++ tests/anime_highlight_cuts/theme_collector/view_boundingbox.py
@@ -1,2 +1,8 @@
 x,y,w,h=[1118.5,       545.5,        1585 ,       1069]
-min_x, min_y =x-(w/2), 
+min_x, min_y =x-(w/2), y-(h/2)
+import cv2
+imagePath=""
+
+image = cv2.imread(imagePath)
+cv2.rectangle()
+cv2.imshow("PIP",image)
