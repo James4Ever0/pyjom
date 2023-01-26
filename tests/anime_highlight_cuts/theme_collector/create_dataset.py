8e58535068b03e3ca01536d0b9e8a50c8401a8dc
jamesbrown <randomvoidmail@foxmail.com>
2023-01-26 18:54:43 +0800

    [2023-01-26 10:54:36]
    FILE:
    tests/anime_highlight_cuts/theme_collector/create_dataset.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/create_dataset.py
+++ tests/anime_highlight_cuts/theme_collector/create_dataset.py
@@ -5,11 +5,9 @@ index.zfill(12)
 train_path = "images/train"
 test_path = "images/test"
 data = {
-"path": "./pip_dataset",  # dataset root dir
-"train": train_path,  # train images (relative to 'path') 128 images
-"val": train_path,  # val images (relative to 'path') 128 images
-"test": "images/test_path,
-
-"names":
-  {0: "active_frame"}
-  }
+    "path": "./pip_dataset",  # dataset root dir
+    "train": train_path,  # train images (relative to 'path') 128 images
+    "val": train_path,  # val images (relative to 'path') 128 images
+    "test": test_path,
+    "names": {0: "active_frame"},
+}
