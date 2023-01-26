69777354672b554c951864cd941b286555757a86
jamesbrown <randomvoidmail@foxmail.com>
2023-01-26 19:35:08 +0800

    [2023-01-26 11:34:59]
    FILE:
    tests/anime_highlight_cuts/theme_collector/create_dataset.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/create_dataset.py
+++ tests/anime_highlight_cuts/theme_collector/create_dataset.py
@@ -76,6 +76,8 @@ testVideo = "output.mp4"
 w, h = 1152, 648
 x, y = 384, 216
 
+print('creating test dataset')
+
 cap = cv2.VideoCapture(testVideo)
 frame_height, frame_width = cap.get(cv2.CAP_PROP_FRAME_HEIGHT), cap.get(
     cv2.CAP_PROP_FRAME_WIDTH
