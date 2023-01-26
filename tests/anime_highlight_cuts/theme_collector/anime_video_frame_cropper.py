da219ae443bd4b618153cc8acc27ad4ec610b7ee
jamesbrown <randomvoidmail@foxmail.com>
2023-01-26 14:39:37 +0800

    [2023-01-26 06:39:29]
    FILE:
    tests/anime_highlight_cuts/theme_collector/anime_video_frame_cropper.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/anime_video_frame_cropper.py
+++ tests/anime_highlight_cuts/theme_collector/anime_video_frame_cropper.py
@@ -1,1 +1,8 @@
 # check if is the video we want and extract data or discard.
+
+videos = [
+"286760784_part1-00001.mp4",
+"329297394_part1-00001.mp4",
+"541755429_part1-00001.mp4",
+"842224692_part1-00001.mp4",
+]
