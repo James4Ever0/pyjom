2867a958734bb37c950ea6cc0c54e8fbdddff417
jamesbrown <randomvoidmail@foxmail.com>
2023-01-26 14:47:14 +0800

    [2023-01-26 06:47:05]
    FILE:
    tests/anime_highlight_cuts/theme_collector/make_picture_in_picture_challange.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/make_picture_in_picture_challange.py
+++ tests/anime_highlight_cuts/theme_collector/make_picture_in_picture_challange.py
@@ -2,7 +2,7 @@ basepath = "/Users/jamesbrown/Downloads/anime_download"
 
 
 import os
-source_video = os.path.join(basepath,"")
+source_video = os.path.join(basepath,"[Sakurato] Onii-chan wa Oshimai! [未删减][02][AVC-8bit 1080p AAC][CHT].mp4")
 
 background_video = os.path.join(basepath,"")
 
