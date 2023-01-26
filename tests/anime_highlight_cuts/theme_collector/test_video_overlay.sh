f65d42596b3c8d356506557358aa92daa0496353
jamesbrown <randomvoidmail@foxmail.com>
2023-01-26 14:59:44 +0800

    [2023-01-26 06:59:34]
    FILE:
    tests/anime_highlight_cuts/theme_collector/test_video_overlay.sh
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/test_video_overlay.sh
+++ tests/anime_highlight_cuts/theme_collector/test_video_overlay.sh
@@ -2,4 +2,4 @@ video_0="[Sakurato] Onii-chan wa Oshimai! [未删减][02][AVC-8bit 1080p AAC][CH
 video_1="[MLU-S] Onii-chan wa Oshimai! - 03 [1080p][Multi Subs].mkv"
 basepath="/Users/jamesbrown/Downloads/anime_download"
 
-ffmpeg -y -t 0:00:10 -i "$basepath/$video_0" -t 0:00:10 -i "$basepath/$video_1" -filter_complex "[0:v]scale=(1920*0.8[v0];[1:v][v0]overlay=100:100" output.mp4
+ffmpeg -y -t 0:00:10 -i "$basepath/$video_0" -t 0:00:10 -i "$basepath/$video_1" -filter_complex "[0:v]scale=1152:648[v0];[1:v][v0]overlay=" output.mp4
