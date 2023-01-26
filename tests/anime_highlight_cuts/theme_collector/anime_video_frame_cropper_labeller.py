4d356f9ecf59b880d163b912adb671c97643a1aa
jamesbrown <randomvoidmail@foxmail.com>
2023-01-26 16:33:21 +0800

    [2023-01-26 08:33:13]
    FILE:
    tests/anime_highlight_cuts/theme_collector/anime_video_frame_cropper_labeller.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/anime_video_frame_cropper_labeller.py
+++ tests/anime_highlight_cuts/theme_collector/anime_video_frame_cropper_labeller.py
@@ -9,7 +9,7 @@ videos = [
     "842224692_part1-00001.mp4",
 ]
 
-# we create dataset here. 
+# we create dataset here.
 # use some short cuts for progression.
 
 frame_step = 10
@@ -21,12 +21,13 @@ from sklearn.semi_supervised import LabelSpreading
 
 lastKey = ["not_c"]
 
+
 def on_press(key):
-    lastKey[0] = 'not_c'
+    lastKey[0] = "not_c"
     try:
         # print("alphanumeric key {0} pressed".format(key.char))
-        if key.char in ['c', 'C']:
-            lastKey[0] = 'c'
+        if key.char in ["c", "C"]:
+            lastKey[0] = "c"
     except AttributeError:
         # print("special key {0} pressed".format(key))
         ...
@@ -44,10 +45,10 @@ listener.start()
 
 # how do you arrange such data?
 
-fields = ['filename', 'frame_index', 'ROI']
+fields = ["filename", "frame_index", "ROI"]
 import csv
 
-with open('labels.csv', 'w+') as csvfile:
+with open("labels.csv", "w+") as csvfile:
     csvwriter = csv.writer(csvfile)
     csvwriter.writerow(fields)
 
@@ -55,23 +56,25 @@ with open('labels.csv', 'w+') as csvfile:
         print("reading video:", index)
         roi = None
         cap = cv2.VideoCapture(video)
-        for vindex in progressbar.progressbar(range(0, int(cap.get(cv2.CAP_PROP_FRAME_COUNT)),frame_step)):
+        for vindex in progressbar.progressbar(
+            range(0, int(cap.get(cv2.CAP_PROP_FRAME_COUNT)), frame_step)
+        ):
             cap.set(cv2.CAP_PROP_POS_FRAMES, vindex)
             succ, image = cap.read()
             if succ:
-                roi_new = cv2.selectROI('roi',image)
+                roi_new = cv2.selectROI("roi", image)
                 # key=cv2.waitKey(0)
-                print('roi_new:',roi_new)
-                print('last key:',lastKey[0])
+                print("roi_new:", roi_new)
+                print("last key:", lastKey[0])
                 # print()
                 # print('keycode:',key)
-                if roi_new == (0,0,0,0):
-                    if lastKey[0] == 'c':
+                if roi_new == (0, 0, 0, 0):
+                    if lastKey[0] == "c":
                         # this is cancelled. roi will be nothing!
-                        roi=None
+                        roi = None
                 else:
-                    roi=roi_new
-                print('roi:', roi)
+                    roi = roi_new
+                print("roi:", roi)
                 for i in range(frame_step):
-                    roi_index = vindex+i
+                    roi_index = vindex + i
         cap.release()
