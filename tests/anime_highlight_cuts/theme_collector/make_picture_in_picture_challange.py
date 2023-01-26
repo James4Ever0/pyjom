29cd1bb8ceaf66371b9ab7565953712060d19044
jamesbrown <randomvoidmail@foxmail.com>
2023-01-26 17:17:39 +0800

    [2023-01-26 09:17:31]
    FILE:
    tests/anime_highlight_cuts/theme_collector/make_picture_in_picture_challange.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/make_picture_in_picture_challange.py
+++ tests/anime_highlight_cuts/theme_collector/make_picture_in_picture_challange.py
@@ -2,10 +2,14 @@ basepath = "/Users/jamesbrown/Downloads/anime_download"
 
 import os
 
-source_video = os.path.join(basepath,"[Sakurato] Onii-chan wa Oshimai! [未删减][02][AVC-8bit 1080p AAC][CHT].mp4")
+source_video = os.path.join(
+    basepath, "[Sakurato] Onii-chan wa Oshimai! [未删减][02][AVC-8bit 1080p AAC][CHT].mp4"
+)
 
-background_video = os.path.join(basepath,"[MLU-S] Onii-chan wa Oshimai! - 03 [1080p][Multi Subs].mkv")
+background_video = os.path.join(
+    basepath, "[MLU-S] Onii-chan wa Oshimai! - 03 [1080p][Multi Subs].mkv"
+)
 
-video_duration = 10 # just for test.
+video_duration = 10  # just for test.
 
 # use ffplay?
