4f13ffa94443291ed322f4b957b02f5ee3054f5f
jamesbrown <randomvoidmail@foxmail.com>
2023-01-26 14:16:05 +0800

    [2023-01-26 06:15:57]
    FILE:
    tests/anime_highlight_cuts/theme_collector/anime_video_checker.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/anime_video_checker.py
+++ tests/anime_highlight_cuts/theme_collector/anime_video_checker.py
@@ -10,11 +10,11 @@ url = f"https://www.bilibili.com/video/{videoID}" # only one single page.
 import time
 secondsToHHMMSS = lambda seconds:time.strftime('%H:%M:%S', time.gmtime(seconds))
 playlistIndex='1'
-start = secondsToHHMMSS(150)
-end = secondsToHHMMSS(200)
+start = secondsToHHMMSS(100)
+end = secondsToHHMMSS(150)
 nameFormat = "%(id)s-%(autonumber)s.%(ext)s"
-cmd=f'yt-dlp --playlist-items "{playlistIndex}" -o "{nameFormat}" "{url}"'
-# cmd=f'yt-dlp --download-sections "*{start}-{end}" --playlist-items "{playlistIndex}" -o "{nameFormat}" "{url}"'
+# cmd=f'yt-dlp --playlist-items "{playlistIndex}" -o "{nameFormat}" "{url}"'
+cmd=f'yt-dlp --download-sections "*{start}-{end}" --playlist-items "{playlistIndex}" -o "{nameFormat}" "{url}"'
 
 os.system(cmd)
 autonumber = "1".zfill(5)
