11d7d21e495246791297eefbe00166bea2d26c57
jamesbrown <randomvoidmail@foxmail.com>
2023-01-26 14:35:53 +0800

    [2023-01-26 06:35:44]
    FILE:
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/anime_video_checker.py
+++ tests/anime_highlight_cuts/theme_collector/anime_video_checker.py
@@ -2,17 +2,21 @@
 
 # we name downloaded video using some agreements.
 import os
-videoBVIDs=["BV1e54y1y7qy",
-"BV1Qf4y197bt",# great challange
+
+videoBVIDs = [
+    "BV1e54y1y7qy",
+    "BV1Qf4y197bt",  # great challange
 ]
-videoBVID =videoBVIDs[1]
+videoBVID = videoBVIDs[1]
 from bv2av import bv_av_conversion
+
 videoAVID = bv_av_conversion(videoBVID)
 import re
-print(videoAVID, type(videoAVID))
-videoAID = re.findall(r'\d+',videoAVID)[0]
 
-url = f"https://www.bilibili.com/video/{videoBVID}" # only one single page.
+print(videoAVID, type(videoAVID))
+videoAID = re.findall(r"\d+", videoAVID)[0]
+
+url = f"https://www.bilibili.com/video/{videoBVID}"  # only one single page.
 # 290 seconds.
 # section example:
 # 0:05:00-0:06:30
@@ -21,7 +25,7 @@ url = f"https://www.bilibili.com/video/{videoBVID}" # only one single page.
 
 # some formats are not working. fuck.
 
-playlistIndex='1'
+playlistIndex = "1"
 
 # start = secondsToHHMMSS(100)
 # end = secondsToHHMMSS(150)
@@ -29,13 +33,13 @@ playlistIndex='1'
 # print('TIMESPAN:',start, end)
 
 nameFormat = "%(id)s-%(autonumber)s.%(ext)s"
-cmd=f'yt-dlp --playlist-items "{playlistIndex}" -o "{nameFormat}" "{url}"'
+cmd = f'yt-dlp --playlist-items "{playlistIndex}" -o "{nameFormat}" "{url}"'
 # cmd=f'yt-dlp --download-sections "*{start}-{end}" --playlist-items "{playlistIndex}" -o "{nameFormat}"  "{url}"'
 
 os.system(cmd)
 autonumber = "1".zfill(5)
 expectedNamePrefix = f"{videoAID}_part{playlistIndex}-{autonumber}"
-print('expected filename prefix:', expectedNamePrefix)
+print("expected filename prefix:", expectedNamePrefix)
 files = os.listdir(".")
 
 for fname in files:
