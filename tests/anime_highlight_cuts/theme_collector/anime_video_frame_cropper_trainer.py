265ec412c0c32ce068cd801673fa4f36f0b3ca01
jamesbrown <randomvoidmail@foxmail.com>
2023-01-26 16:10:49 +0800

    [2023-01-26 08:10:41]
    FILE:
    tests/anime_highlight_cuts/theme_collector/anime_video_frame_cropper_trainer.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/anime_video_frame_cropper_trainer.py
+++ tests/anime_highlight_cuts/theme_collector/anime_video_frame_cropper_trainer.py
@@ -23,15 +23,17 @@ lastKey = ["not_c"]
 def on_press(key):
     lastKey[0] = 'not_c'
     try:
-        print("alphanumeric key {0} pressed".format(key.char))
+        # print("alphanumeric key {0} pressed".format(key.char))
         if key.char in ['c', 'C']:
             lastKey[0] = 'c'
     except AttributeError:
-        print("special key {0} pressed".format(key))
+        # print("special key {0} pressed".format(key))
+        ...
 
 
 def on_release(key):
-    print("{0} released".format(key))
+    # print("{0} released".format(key))
+    ...
 
 
 listener = Listener(on_press=on_press, on_release=on_release)
