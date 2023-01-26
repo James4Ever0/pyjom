0c358944031e23b2b6df75693a777211f1edde79
jamesbrown <randomvoidmail@foxmail.com>
2023-01-26 22:50:10 +0800

    [2023-01-26 14:50:04]
    FILE:
    tests/anime_highlight_cuts/theme_collector/yolov8_test.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/yolov8_test.py
+++ tests/anime_highlight_cuts/theme_collector/yolov8_test.py
@@ -11,4 +11,5 @@ image= cv2.imread(imagePath)
 for xyxy in output[0].boxes.xyxy.numpy().astype(int).tolist():
     x0,y0, x1,y1 = xyxy
     cv2.rectangle(image,(x0, y0), (x1,y1), (0,0,255), thickness=1)
-cv2.imshow()
+cv2.imshow("PIP", image)
+cv2.waitKey(0)
