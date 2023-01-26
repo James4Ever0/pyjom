22e22cc388e99f7fa0295b1d0059ad2d89cf1422
jamesbrown <randomvoidmail@foxmail.com>
2023-01-26 18:56:32 +0800

    [2023-01-26 10:56:22]
    FILE:
    tests/anime_highlight_cuts/theme_collector/create_dataset.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/create_dataset.py
+++ tests/anime_highlight_cuts/theme_collector/create_dataset.py
@@ -4,14 +4,14 @@ index.zfill(12)
 
 train_path = "images/train"
 test_path = "images/test"
+basepath = "./pip_dataset"
 data = {
-    "path": "./pip_dataset",  # dataset root dir
+    "path": basepath,  # dataset root dir
     "train": train_path,  # train images (relative to 'path') 128 images
     "val": train_path,  # val images (relative to 'path') 128 images
     "test": test_path,
     "names": {0: "active_frame"},
 }
-import shutil
 import os
 os.makedirs(os.path.join(basepath, train_path),exists_ok=True)
-os.makedirs(os.path.join(basepath, train_path),exists_ok=True)
+os.makedirs(os.path.join(basepath, test_path),exists_ok=True)
