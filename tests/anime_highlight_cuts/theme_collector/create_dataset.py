588a1afdc8f479fcc53eaa4c38f6b91516fa0710
jamesbrown <randomvoidmail@foxmail.com>
2023-01-26 18:05:15 +0800

    [2023-01-26 10:05:06]
    FILE:
    tests/anime_highlight_cuts/theme_collector/create_dataset.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/create_dataset.py
+++ tests/anime_highlight_cuts/theme_collector/create_dataset.py
@@ -1,7 +1,7 @@
 import yaml
 data={}
 index.zfill(12)
-path: ../datasets/coco128  # dataset root dir
+path: ./pip_dataset  # dataset root dir
 train: images/train  # train images (relative to 'path') 128 images
 val: images/train  # val images (relative to 'path') 128 images
 test: images/test
