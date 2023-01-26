c9865475e9f20a567219470a9e6357df7926a0bc
jamesbrown <randomvoidmail@foxmail.com>
2023-01-26 13:31:17 +0800

    [2023-01-26 05:31:05]
    FILE:
    tests/anime_highlight_cuts/theme_collector/anime_compilation_video_checker.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/anime_compilation_video_checker.py
+++ tests/anime_highlight_cuts/theme_collector/anime_compilation_video_checker.py
@@ -93,6 +93,8 @@ for videoLink in videoLinks:
     title = data['title']
     pic = data['pic']
     tid,tname = data['tid'],data['tname']
+    # 27, "综合"
+    # 
     dynamic = data['dynamic'] # we can copy that.
     desc = data['desc']
     owner_mid = data['owner']['mid']
