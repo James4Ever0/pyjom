9e8be50914286ab6832eca5952ae4112aa0b92fd
jamesbrown <randomvoidmail@foxmail.com>
2023-01-26 16:03:18 +0800

    [2023-01-26 08:03:07]
    FILE:
    tests/anime_highlight_cuts/theme_collector/keyboard_listener.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/keyboard_listener.py
+++ tests/anime_highlight_cuts/theme_collector/keyboard_listener.py
@@ -1,24 +1,18 @@
 from pynput.keyboard import Key, Listener
-  
-def on_press(key):
-     
-    keys.append(key)
-     
-    try:
-        print('alphanumeric key {0} pressed'.format(key.char))
-    except AttributeError:
-        print('special key {0} pressed'.format(key))
-          
-def on_release(key):
-                     
-    print('{0} released'.format(key))
-    if key == Key.esc:
-        # Stop listener
-        return False
-                     
 
-listener = Listener(on_press = on_press,
-              on_release = on_release)
+
+def on_press(key):
+    try:
+        print("alphanumeric key {0} pressed".format(key.char))
+    except AttributeError:
+        print("special key {0} pressed".format(key))
+
+
+def on_release(key):
+    print("{0} released".format(key))
+
+
+listener = Listener(on_press=on_press, on_release=on_release)
 # listener.start()
 with listener:
     listener.join()
