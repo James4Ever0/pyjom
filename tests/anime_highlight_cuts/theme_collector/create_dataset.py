4430857230fc8e7438dbbb61c0771c65a146ff9e
jamesbrown <randomvoidmail@foxmail.com>
2023-01-26 18:01:25 +0800

    [2023-01-26 10:01:19]
    FILE:
    tests/anime_highlight_cuts/theme_collector/create_dataset.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/create_dataset.py
+++ tests/anime_highlight_cuts/theme_collector/create_dataset.py
@@ -6,7 +6,5 @@ train: images/train2017  # train images (relative to 'path') 128 images
 val: images/train2017  # val images (relative to 'path') 128 images
 test:  # test images (optional)
 
-# Classes (80 COCO classes)
 names:
   0: anime_frame
-  1: others
