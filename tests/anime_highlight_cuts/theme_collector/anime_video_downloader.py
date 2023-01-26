c83133bd51db45464a641f1c2aadf82e6121ec98
jamesbrown <randomvoidmail@foxmail.com>
2023-01-26 14:38:27 +0800

    [2023-01-26 06:38:17]
    FILE:
    tests/anime_highlight_cuts/theme_collector/anime_video_checker.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/anime_video_downloader.py
+++ tests/anime_highlight_cuts/theme_collector/anime_video_downloader.py
@@ -7,7 +7,7 @@ videoBVIDs = [
     "BV1e54y1y7qy",  # 842224692_part1-00001
     "BV1Qf4y197bt",  # great challange, 286760784
     "BV1bi4y1g7Gd",  # watermark, full screen, 541755429
-    "BV1PA411n7N6",  # shit jumping around,
+    "BV1PA411n7N6",  # shit jumping around, 329297394
 ]
 videoBVID = videoBVIDs[3]
 from bv2av import bv_av_conversion
