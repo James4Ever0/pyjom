86d0dc03aa6a58b300e3dbbe2b64a606d3c863a1
jamesbrown <randomvoidmail@foxmail.com>
2023-01-26 22:47:14 +0800

    [2023-01-26 14:47:01]
    FILE:
    tests/anime_highlight_cuts/theme_collector/yolov8_test.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/yolov8_test.py
+++ tests/anime_highlight_cuts/theme_collector/yolov8_test.py
@@ -1,12 +1,14 @@
 from ultralytics import YOLO
 
 model = YOLO("genesis.pt")
-
-output = model("simple_pip.png")
+imagePath ="simple_pip.png"
+output = model(imagePath)
 # print(output)
 # breakpoint()
 
 import cv2
+image= cv2.imread(imagePath)
 for xyxy in output[0].boxes.xyxy.numpy().astype(int).tolist():
     x0,y0, x1,y1 = xyxy
     cv2.rectangle(image,(x0, y0), (x1,y1), (0,0,255), thickness=1)
+cv2.imshow()
