197c5c5c32c79542e3d5cdf10d568df24e3ed3a8
jamesbrown <randomvoidmail@foxmail.com>
2023-01-26 18:03:48 +0800

    [2023-01-26 10:03:39]
    FILE:
    tests/anime_highlight_cuts/theme_collector/create_dataset.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/create_dataset.py
+++ tests/anime_highlight_cuts/theme_collector/create_dataset.py
@@ -2,9 +2,9 @@ import yaml
 data={}
 
 path: ../datasets/coco128  # dataset root dir
-train: images/train2017  # train images (relative to 'path') 128 images
-val: images/train2017  # val images (relative to 'path') 128 images
-test: images/
+train: images/train  # train images (relative to 'path') 128 images
+val: images/train  # val images (relative to 'path') 128 images
+test: images/test
 
 names:
   0: anime_frame
