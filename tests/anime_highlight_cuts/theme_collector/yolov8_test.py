c14640b621190e016d89c7c1bab4be6b1d7c2b3c
jamesbrown <randomvoidmail@foxmail.com>
2023-01-26 23:27:36 +0800

    [2023-01-26 15:27:28]
    FILE:
    tests/anime_highlight_cuts/theme_collector/yolov8_test.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/yolov8_test.py
+++ tests/anime_highlight_cuts/theme_collector/yolov8_test.py
@@ -1,20 +1,21 @@
 from ultralytics import YOLO
 
 model = YOLO("ver2.pt")
-# imagePath ="simple_pip.png"
-imagePaths = ["000000003099.png","simple_pip.png","no_border_0.jpg",
-'has_border_0.jpg',
-'has_border_1.jpg',
-'has_border_2.jpg',
+imagePaths = [
+    "000000003099.png",
+    "simple_pip.png",
+    "no_border_0.jpg",
+    "has_border_0.jpg",
+    "has_border_1.jpg",
+    "has_border_2.jpg",
 ]
 import cv2
 
 for imagePath in imagePaths:
     output = model(imagePath)
-
-    image= cv2.imread(imagePath)
+    image = cv2.imread(imagePath)
     for xyxy in output[0].boxes.xyxy.numpy().astype(int).tolist():
-        x0,y0, x1,y1 = xyxy
-        cv2.rectangle(image,(x0, y0), (x1,y1), (0,0,255), thickness=1)
+        x0, y0, x1, y1 = xyxy
+        cv2.rectangle(image, (x0, y0), (x1, y1), (0, 0, 255), thickness=10)
     cv2.imshow("PIP", image)
     cv2.waitKey(0)
