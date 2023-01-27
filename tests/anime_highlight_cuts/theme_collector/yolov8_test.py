c4e50c4e16a05bd6af0361c5771c5930e67b2c24
jamesbrown <randomvoidmail@foxmail.com>
2023-01-27 16:08:31 +0800

    [2023-01-27 08:08:23]
    FILE:
    tests/anime_highlight_cuts/theme_collector/yolov8_test.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/yolov8_test.py
+++ tests/anime_highlight_cuts/theme_collector/yolov8_test.py
@@ -7,7 +7,7 @@ from ultralytics import YOLO
 # no one will miss `genesis.pt`, right?
 
 model = YOLO("general_ver1.pt")
-## TODO: create dataset to prevent segmentation of 
+## TODO: create dataset to prevent detection of borders 
 # model = YOLO("ver3.pt")
 
 # find trained weights on huggingface:
