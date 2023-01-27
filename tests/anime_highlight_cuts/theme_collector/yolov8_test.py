d5ee23cf0f25485bc0408147b8b1fe98ba0f50e4
jamesbrown <randomvoidmail@foxmail.com>
2023-01-27 16:04:21 +0800

    [2023-01-27 08:04:13]
    FILE:
    tests/anime_highlight_cuts/theme_collector/yolov8_test.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/yolov8_test.py
+++ tests/anime_highlight_cuts/theme_collector/yolov8_test.py
@@ -38,9 +38,9 @@ for imagePath in imagePaths:
         x0, y0, x1, y1 = xyxy
         candidates.append((x0,y0,x1,y1))
         # sort it by area, then by centrality?
-    candidates.sort(key=lambda points: (points[2]-points[0])*(points[3]-points[1]))
+    candidates.sort(key=lambda points: -(points[2]-points[0])*(points[3]-points[1]))
     candidates=candidates[:3]
-    candidates.sort(key=lambda points:)
+    candidates.sort(key=lambda points:*)
     cv2.rectangle(image, (x0, y0), (x1, y1), (0, 0, 255), thickness=10)
     cv2.imshow("PIP", image)
     cv2.waitKey(0)
