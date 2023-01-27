34abfd9ecb2f168a5090aaf21624fa07363bea9f
jamesbrown <randomvoidmail@foxmail.com>
2023-01-27 13:13:32 +0800

    [2023-01-27 05:13:24]
    FILE:
    tests/anime_highlight_cuts/theme_collector/anime_video_frame_cropper_labeller.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/anime_video_frame_cropper_labeller.py
+++ tests/anime_highlight_cuts/theme_collector/anime_video_frame_cropper_labeller.py
@@ -2,12 +2,14 @@
 
 # maybe you want some challange, so you can make one, right?
 
-videos = [
-    "286760784_part1-00001.mp4",
-    "329297394_part1-00001.mp4",
-    "541755429_part1-00001.mp4",
-    "842224692_part1-00001.mp4",
-]
+# videos = [
+#     "286760784_part1-00001.mp4",
+#     "329297394_part1-00001.mp4",
+#     "541755429_part1-00001.mp4",
+#     "842224692_part1-00001.mp4",
+# ]
+
+videos= 
 
 # we create dataset here.
 # use some short cuts for progression.
