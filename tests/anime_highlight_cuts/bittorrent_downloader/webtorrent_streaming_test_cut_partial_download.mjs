afe2e22ace4bed3097fc19921740a94435a274e3
jamesbrown <randomvoidmail@foxmail.com>
2023-01-27 22:51:18 +0800

    [2023-01-27 14:51:09]
    FILE:
    tests/anime_highlight_cuts/bittorrent_downloader/webtorrent_streaming_test_cut_partial_download.mjs
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/bittorrent_downloader/webtorrent_streaming_test_cut_partial_download.mjs
+++ tests/anime_highlight_cuts/bittorrent_downloader/webtorrent_streaming_test_cut_partial_download.mjs
@@ -3,12 +3,11 @@ var torrentPath = "/Users/jamesbrown/Downloads/anime_download/[Kamigami&VCB-Stud
 // require_esm =require('esm')(module)
 // const{WebTorrent} = require_esm('webtorrent').default
 // console.log('IMPORT PATH?',process.env.NODE_PATH)
-var ffmpeg_lib_path = 'fluent-ffmpeg'
-import ffmpeg from ffmpeg_lib_path
-
+import ffmpeg from (process.env.NODE_PATH+'/fluent-ffmpeg'
+)
 // fuck it. let's symlink the NODE_PATH to here.
 
-// import WebTorrent from 'webtorrent'
+import WebTorrent from 'webtorrent'
 // // const WebTorrent =await import('webtorrent')
 console.log("WEBTORRENT OBJECT?",WebTorrent)
 const client = new WebTorrent()
