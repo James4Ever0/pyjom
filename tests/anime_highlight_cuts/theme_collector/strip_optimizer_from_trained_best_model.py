2c9f9aaaf5a48d0d84200c44913c0b89a13b44d8
jamesbrown <randomvoidmail@foxmail.com>
2023-01-27 17:10:52 +0800

    [2023-01-27 09:10:38]
    FILE:
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/strip_optimizer_from_trained_best_model.py
+++ tests/anime_highlight_cuts/theme_collector/strip_optimizer_from_trained_best_model.py
@@ -3,4 +3,4 @@ from ultralytics.yolo.utils.torch_utils import strip_optimizer
 model_path = "general_ver1_with_optimizer.pt"
 
 export_path = "general_ver1.pt"
-strip_optimizer(f=model_path,s=export_path)
+strip_optimizer(f=model_path, s=export_path)
