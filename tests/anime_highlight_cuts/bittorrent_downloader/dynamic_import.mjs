25cb91456ca501376d510c1a7c1013463df72ef8
jamesbrown <randomvoidmail@foxmail.com>
2023-01-27 23:33:34 +0800

    [2023-01-27 15:33:27]
    FILE:
    tests/anime_highlight_cuts/bittorrent_downloader/dynamic_import.mjs
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/bittorrent_downloader/dynamic_import.mjs
+++ tests/anime_highlight_cuts/bittorrent_downloader/dynamic_import.mjs
@@ -1,5 +1,5 @@
-const ffmpeg = (await import("fluent-ffmpeg")).default 
+const ffmpeg = (await import(`${process.env.fluent-ffmpeg")).default 
 // promise!
-// shit this 
+// shit this ESM can directly use await statements.
 
 console.log(ffmpeg)
