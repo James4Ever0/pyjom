0ca3b2568d504651353065898879fa4a60f2c0cf
jamesbrown <randomvoidmail@foxmail.com>
2023-01-27 21:56:43 +0800

    [2023-01-27 13:56:35]
    FILE:
    tests/anime_highlight_cuts/bittorrent_downloader/webtorrent_streaming_test_cut_partial_download.js
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/bittorrent_downloader/webtorrent_streaming_test_cut_partial_download.js
+++ tests/anime_highlight_cuts/bittorrent_downloader/webtorrent_streaming_test_cut_partial_download.js
@@ -1,6 +1,7 @@
 torrentPath = "/Users/jamesbrown/Downloads/anime_download/[Kamigami&VCB-Studio] Yahari Ore no Seishun Lovecome wa Machigatte Iru. [Ma10p_1080p].torrent"
 
-const{ WebTorrent } =require('webtorrent')
+require_esm =require('esm')(module)
+
 
 const client = new WebTorrent()
 
