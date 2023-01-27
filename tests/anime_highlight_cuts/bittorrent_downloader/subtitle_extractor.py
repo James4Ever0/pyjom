693d40eab24f99f03e6fe0e174010a3592438e8f
jamesbrown <randomvoidmail@foxmail.com>
2023-01-27 20:50:35 +0800

    [2023-01-27 12:50:29]
    FILE:
    tests/anime_highlight_cuts/bittorrent_downloader/subtitle_extractor.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/bittorrent_downloader/subtitle_extractor.py
+++ tests/anime_highlight_cuts/bittorrent_downloader/subtitle_extractor.py
@@ -4,4 +4,4 @@
 # use ffmpeg:
 # https://github.com/fdenivac/ffextract-subtitles/blob/master/ffextract-subtitles.py
 
-# use ocr related, since this is bangumi, it is easy to extract from fixed location.
+# use ocr to extract subtitles. since this is bangumi, it is easy to extract from fixed location.
