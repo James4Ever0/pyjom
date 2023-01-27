70023e9e03bfc2525e93c83297b240db492ba525
jamesbrown <randomvoidmail@foxmail.com>
2023-01-27 22:27:47 +0800

    [2023-01-27 14:27:38]
    FILE:
    tests/anime_highlight_cuts/bittorrent_downloader/webtorrent_streaming_test_cut_partial_download.mjs
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/bittorrent_downloader/webtorrent_streaming_test_cut_partial_download.mjs
+++ tests/anime_highlight_cuts/bittorrent_downloader/webtorrent_streaming_test_cut_partial_download.mjs
@@ -1,19 +1,19 @@
-torrentPath = "/Users/jamesbrown/Downloads/anime_download/[Kamigami&VCB-Studio] Yahari Ore no Seishun Lovecome wa Machigatte Iru. [Ma10p_1080p].torrent"
+var torrentPath = "/Users/jamesbrown/Downloads/anime_download/[Kamigami&VCB-Studio] Yahari Ore no Seishun Lovecome wa Machigatte Iru. [Ma10p_1080p].torrent"
 
 // require_esm =require('esm')(module)
 // const{WebTorrent} = require_esm('webtorrent').default
-console.log('IMPORT PATH?',process.env.NODE_PATH)
-import ffmpeg from 'fluent-ffmpeg'
-import WebTorrent from 'webtorrent'
-// const WebTorrent =await import('webtorrent')
-console.log("WEBTORRENT OBJECT?",WebTorrent)
-const client = WebTorrent()
+// console.log('IMPORT PATH?',process.env.NODE_PATH)
+// import ffmpeg from 'fluent-ffmpeg'
+// import WebTorrent from 'webtorrent'
+// // const WebTorrent =await import('webtorrent')
+// console.log("WEBTORRENT OBJECT?",WebTorrent)
+// const client = WebTorrent()
 
-client.add(torrentPath,torrent =>{
-    var selectedFiles = torrent.files.find(file =>{
-        console.log("FILENAME?", file.name)
-        return file.name.endswith('.mkv')
-    })
-    console.log("SELECTED FILES?")
-    console.log(selectedFiles)
-})
+// client.add(torrentPath,torrent =>{
+//     var selectedFiles = torrent.files.find(file =>{
+//         console.log("FILENAME?", file.name)
+//         return file.name.endswith('.mkv')
+//     })
+//     console.log("SELECTED FILES?")
+//     console.log(selectedFiles)
+// })
