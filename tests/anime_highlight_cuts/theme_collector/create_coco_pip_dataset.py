a87b6f4079f3a080485fd35ba3448ebcefc0a0d8
jamesbrown <randomvoidmail@foxmail.com>
2023-01-28 20:19:10 +0800

    [2023-01-28 12:19:04]
    FILE:
    tests/anime_highlight_cuts/theme_collector/create_coco_pip_dataset.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/create_coco_pip_dataset.py
+++ tests/anime_highlight_cuts/theme_collector/create_coco_pip_dataset.py
@@ -11,5 +11,5 @@ alphabets = "abcdefghijklmnopqrstuvwxyz"
 ALPHABETS = alphabets.upper()
 numbers = "0123456789"
 from string import punctuation
-character
+characterList = list(alphabets+ALPHABETS+numbers+)
 getRandomCharacters
