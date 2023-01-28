fb16b5dc40a9dff1e92e19d3e1531031e6eb62e9
jamesbrown <randomvoidmail@foxmail.com>
2023-01-28 16:41:31 +0800

    [2023-01-28 08:41:25]
    FILE:
    tests/anime_highlight_cuts/bittorrent_downloader/webtorrent_streaming_test_cut_partial_download.mjs
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/bittorrent_downloader/webtorrent_streaming_test_cut_partial_download.mjs
+++ tests/anime_highlight_cuts/bittorrent_downloader/webtorrent_streaming_test_cut_partial_download.mjs
@@ -33,10 +33,10 @@ import WebTorrent from 'webtorrent'
 console.log("WEBTORRENT OBJECT?",WebTorrent)
 const client=new WebTorrent({dht: true}) // nothing reading out. guess this is fucked.
 
-// const serverPort=8970
+const serverPort=8970
 
-// const instance=client.createServer()
-// instance.server.listen(serverPort) // not random port? not zero? 
+const instance=client.createServer()
+instance.server.listen(serverPort) // not random port? not zero? 
 
 client.add(torrentPath,torrent => {
     var selectedFile=torrent.files.find(file => {
