ce21dbf024ae4dc778cbaecb68b74b4e706a319a
jamesbrown <randomvoidmail@foxmail.com>
2023-01-29 00:09:43 +0800

    [2023-01-28 16:09:37]
    FILE:
    tests/anime_highlight_cuts/theme_collector/create_coco_pip_dataset.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/create_coco_pip_dataset.py
+++ tests/anime_highlight_cuts/theme_collector/create_coco_pip_dataset.py
@@ -42,7 +42,7 @@ colors = [
     (0, 0, 255),
     (255, 0, 0),
 ]
-colorsNumpyArray = [np.array(color) for color in colorsNumpyArray]
+colorsNumpyArray = [np.array(color) for color in colors]
 colorsWithIndex = [(index, color) for index, color in enumerate(colors)]
 # we are not doing this while testing.
 
