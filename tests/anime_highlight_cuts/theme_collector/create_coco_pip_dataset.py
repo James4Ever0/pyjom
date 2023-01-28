05970dbf4aedb84c651f27cd412eb29cd3f14971
jamesbrown <randomvoidmail@foxmail.com>
2023-01-28 21:10:23 +0800

    [2023-01-28 13:10:17]
    FILE:
    tests/anime_highlight_cuts/theme_collector/create_coco_pip_dataset.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/create_coco_pip_dataset.py
+++ tests/anime_highlight_cuts/theme_collector/create_coco_pip_dataset.py
@@ -26,4 +26,4 @@ getRandomLinesOfCharacters = lambda lineCount, charCount: "\n".join(
 
 imageFormats = ["1", "2", "4"]
 textFormats=['up','down','none']
-backgroundFormats = ['pure','horizontal-strips','vertical-stripes']
+backgroundFormats = ['pure','horizontalStripes','verticalStripes','horizontalGradients','verticalGradients']
