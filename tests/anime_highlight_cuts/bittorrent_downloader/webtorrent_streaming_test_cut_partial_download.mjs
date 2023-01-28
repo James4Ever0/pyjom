aec8d68b94b99214e9897729df6a7140c3d6e714
jamesbrown <randomvoidmail@foxmail.com>
2023-01-28 16:39:35 +0800

    [2023-01-28 08:39:28]
    FILE:
    tests/anime_highlight_cuts/bittorrent_downloader/webtorrent_streaming_test_cut_partial_download.mjs
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/bittorrent_downloader/webtorrent_streaming_test_cut_partial_download.mjs
+++ tests/anime_highlight_cuts/bittorrent_downloader/webtorrent_streaming_test_cut_partial_download.mjs
@@ -102,7 +102,7 @@ client.add(torrentPath,torrent => {
 
     ffmpeg(stream).seekInput('0:05').duration("0:10").on('progress',function(progress) {
         console.log('FFmpeg Processing: '+progress.percent+'% done');
-    }).outputOptions('-c copy -y').output('output.mkv').run() // still not working?
+    }).outputOptions(['-c copy','-y']).output('output.mkv').run() // still not working?
 
     // *******************READSTREAM RELATED*******************
 
