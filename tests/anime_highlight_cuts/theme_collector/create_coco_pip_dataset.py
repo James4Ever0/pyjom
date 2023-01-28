8edadb7dde80717b0646368b8f92acf3a7787938
jamesbrown <randomvoidmail@foxmail.com>
2023-01-28 20:20:51 +0800

    [2023-01-28 12:20:45]
    FILE:
    tests/anime_highlight_cuts/theme_collector/create_coco_pip_dataset.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/create_coco_pip_dataset.py
+++ tests/anime_highlight_cuts/theme_collector/create_coco_pip_dataset.py
@@ -2,16 +2,21 @@
 imageBasePath = ""
 imagePaths = []
 width = 800
-half_width = width/2 # either use 1,2,4 images.
-textTotalHeight = 300 # either add to top or bottom.
+half_width = width / 2  # either use 1,2,4 images.
+textTotalHeight = 300  # either add to top or bottom.
 
-imageIndex=0 # shall be increased on demand.
+imageIndex = 0  # shall be increased on demand.
 
 alphabets = "abcdefghijklmnopqrstuvwxyz"
 ALPHABETS = alphabets.upper()
 numbers = "0123456789"
 from string import punctuation
-characterList = list(alphabets+ALPHABETS+numbers+punctuation+" ")
+
+characterList = list(alphabets + ALPHABETS + numbers + punctuation + " ")
 import random
+
 getRandomCharacter = lambda: random.choice(characterList)
-getRandomCharacters = lambda length: "".join([getRandomCharacter() for _ in range(length)])
+getRandomCharacters = lambda length: "".join(
+    [getRandomCharacter() for _ in range(length)]
+)
+getRandomLinesOfCharacters
