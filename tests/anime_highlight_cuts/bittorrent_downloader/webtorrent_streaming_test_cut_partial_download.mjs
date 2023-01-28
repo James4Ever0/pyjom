a9ab0c6bcf99b43ea926bddd873ef76717d9ac97
jamesbrown <randomvoidmail@foxmail.com>
2023-01-28 18:49:06 +0800

    [2023-01-28 10:48:59]
    FILE:
    tests/anime_highlight_cuts/bittorrent_downloader/webtorrent_streaming_test_cut_partial_download.mjs
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/bittorrent_downloader/webtorrent_streaming_test_cut_partial_download.mjs
+++ tests/anime_highlight_cuts/bittorrent_downloader/webtorrent_streaming_test_cut_partial_download.mjs
@@ -7,7 +7,7 @@
 
 // you make countdowns. you use managed temporary directories. you use port within a range.
 
-// you might want a single, unified instance
+// you might want a single, unified server instance. in that case you will manage resources within server.
 
 var torrentPath="/Users/jamesbrown/Downloads/anime_download/[Kamigami&VCB-Studio] Yahari Ore no Seishun Lovecome wa Machigatte Iru. [Ma10p_1080p].torrent"
 
