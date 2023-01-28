ca5a199f42b9e47e6b038629c28ddc514b2050d8
jamesbrown <randomvoidmail@foxmail.com>
2023-01-28 22:51:26 +0800

    [2023-01-28 14:51:18]
    FILE:
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/bittorrent_downloader/webtorrent_streaming_test_cut_partial_download.mjs
+++ tests/anime_highlight_cuts/bittorrent_downloader/webtorrent_streaming_test_cut_partial_download.mjs
@@ -124,6 +124,7 @@ client.add(torrentPath,config,(torrent) => {
 
 
     ffmpeg(stream).seekInput('0:00').duration("0:20").on('progress',function(progress) {
+        // why not showing progress?
         console.log('FFmpeg Processing: '+progress.percent+'% done');
     }).on('end',() => {
         console.log("FFMPEG EXECUTION COMPLETE?")
