a71c7568c1db870e6f1cad04161a44af98e24188
jamesbrown <randomvoidmail@foxmail.com>
2023-01-28 18:30:00 +0800

    [2023-01-28 10:29:54]
    FILE:
    tests/anime_highlight_cuts/bittorrent_downloader/webtorrent_streaming_test_cut_partial_download.mjs
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/bittorrent_downloader/webtorrent_streaming_test_cut_partial_download.mjs
+++ tests/anime_highlight_cuts/bittorrent_downloader/webtorrent_streaming_test_cut_partial_download.mjs
@@ -41,7 +41,9 @@ const serverPort=8970
 const instance=client.createServer()
 instance.server.listen(serverPort) // not random port? not zero? 
 
-const config={path: announce: null}
+const config={}
+config.path = ""
+// config.announce=""
 
 const torrent=client.add(torrentPath,config)
 
