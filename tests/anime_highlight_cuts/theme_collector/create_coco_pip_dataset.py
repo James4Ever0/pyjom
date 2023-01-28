8ff67844a293e84818480388c3ff877e9636eedd
jamesbrown <randomvoidmail@foxmail.com>
2023-01-28 21:13:08 +0800

    [2023-01-28 13:13:01]
    FILE:
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/create_coco_pip_dataset.py
+++ tests/anime_highlight_cuts/theme_collector/create_coco_pip_dataset.py
@@ -1,6 +1,6 @@
 # use what? better use some standard library.
 imageBasePath = ""
-imagePaths = []
+imagePaths = [""]
 width = 800
 half_width = width / 2  # either use 1,2,4 images.
 textTotalHeight = 300  # either add to top or bottom.
