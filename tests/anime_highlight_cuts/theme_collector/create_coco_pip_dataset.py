7068215643d1ffdc11f32a4b126cab1287bffb21
jamesbrown <randomvoidmail@foxmail.com>
2023-01-29 00:20:47 +0800

    [2023-01-28 16:20:40]
    FILE:
    tests/anime_highlight_cuts/theme_collector/create_coco_pip_dataset.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/create_coco_pip_dataset.py
+++ tests/anime_highlight_cuts/theme_collector/create_coco_pip_dataset.py
@@ -106,5 +106,4 @@ for imageFormat, textFormat, backgroundFormat in itertools.product(
             ),
         )[0]
         textColor = textColorNumpyArray.tolist()
-        # let's crop it!
-        if textFormat =="":
+        if textFormat == "":
