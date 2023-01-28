1102046a73995f4bec95976b451fd6c58ac3267c
jamesbrown <randomvoidmail@foxmail.com>
2023-01-28 21:17:11 +0800

    [2023-01-28 13:17:05]
    FILE:
    tests/anime_highlight_cuts/theme_collector/create_coco_pip_dataset.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/create_coco_pip_dataset.py
+++ tests/anime_highlight_cuts/theme_collector/create_coco_pip_dataset.py
@@ -1,6 +1,6 @@
 # use what? better use some standard library.
 imageBasePath = ""
-imagePaths = [""]
+imagePaths = ["Screen Shot 2023-01-17 at 15.35.29.png"]
 width = 800
 half_width = width / 2  # either use 1,2,4 images.
 textTotalHeight = 300  # either add to top or bottom.
