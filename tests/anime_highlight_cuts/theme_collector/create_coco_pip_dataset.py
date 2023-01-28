5c584ec743e3146cf6bc6f38a9512b76769b41b4
jamesbrown <randomvoidmail@foxmail.com>
2023-01-29 00:00:39 +0800

    [2023-01-28 16:00:33]
    FILE:
    tests/anime_highlight_cuts/theme_collector/create_coco_pip_dataset.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/create_coco_pip_dataset.py
+++ tests/anime_highlight_cuts/theme_collector/create_coco_pip_dataset.py
@@ -79,6 +79,7 @@ for imageFormat, textFormat, backgroundFormat in itertools.product(
     backgroundImage = np.zeros(backgroundShape,dtype=np.uint8)
     if backgroundFormat in ["horizontalStripes", "verticalStripes", "gradients"]:
         color_a, color_b = sortedColors[:2]
-        if backgroundFormat in ["horizontalStripes", "verticalStripes"]
+        if backgroundFormat in ["horizontalStripes", "verticalStripes"]:
+            stripeCount = random.randint(2,5)
     else:
         color_main = sortedColors[0]
