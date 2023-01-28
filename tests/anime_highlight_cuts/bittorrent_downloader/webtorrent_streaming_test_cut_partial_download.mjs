c95c21cf6f9593ee78f572e026cfc60a6837c2fb
jamesbrown <randomvoidmail@foxmail.com>
2023-01-28 14:37:04 +0800

    [2023-01-28 06:36:54]
    FILE:
    tests/anime_highlight_cuts/bittorrent_downloader/webtorrent_streaming_test_cut_partial_download.mjs
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/bittorrent_downloader/webtorrent_streaming_test_cut_partial_download.mjs
+++ tests/anime_highlight_cuts/bittorrent_downloader/webtorrent_streaming_test_cut_partial_download.mjs
@@ -49,7 +49,9 @@ client.add(torrentPath,torrent => {
     // https://github.com/leeroybrun/webtorrent-transcode
     var stream=selectedFile.createReadStream() // not working! fuck.
     // var stream = fs.createReadStream("/Users/jamesbrown/Downloads/anime_download/[Sakurato] Onii-chan wa Oshimai! [01][AVC-8bit 1080p AAC][CHT].mp4")
-
+    stream.on('error', function(err) {
+        console.log('error', err);
+    }
     // console.log("STREAM?",stream)
     // while(true) {
     //     var buffer=stream.read(200)
