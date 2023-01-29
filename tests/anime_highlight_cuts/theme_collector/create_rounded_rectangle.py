cae42f7bb5d90466877749b3c43619ccc872e0b9
jamesbrown <randomvoidmail@foxmail.com>
2023-01-29 15:47:13 +0800

    [2023-01-29 07:47:07]
    FILE:
    tests/anime_highlight_cuts/theme_collector/create_rounded_rectangle.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/create_rounded_rectangle.py
+++ tests/anime_highlight_cuts/theme_collector/create_rounded_rectangle.py
@@ -12,11 +12,12 @@ def rectangle():
     draw.rectangle((200, 100, 300, 200), fill="white")
     # Draw a rounded rectangle
     draw.rounded_rectangle((50, 50, 150, 150), fill="white", radius=20)
-    npArray = np.array(image) # uint8? great.
+    npArray = np.array(image)/255 # uint8? great.
     # print(npArray)
     # print(npArray.shape, npArray.dtype, npArray.max()) # 255?
     cv2.imshow("mask", npArray)
-    # maybe we just want "1" instead of "0"
+    # maybe we just want "1" instead of "255"
+    # divide by 255 then.
     cv2.waitKey(0)
 
 rectangle()
