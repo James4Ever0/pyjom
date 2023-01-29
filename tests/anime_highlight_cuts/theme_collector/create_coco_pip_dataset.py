eaf717d245e172c6d0ff4c416ce24b928a53aabf
jamesbrown <randomvoidmail@foxmail.com>
2023-01-29 17:46:04 +0800

    [2023-01-29 09:45:58]
    FILE:
    tests/anime_highlight_cuts/theme_collector/create_coco_pip_dataset.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/create_coco_pip_dataset.py
+++ tests/anime_highlight_cuts/theme_collector/create_coco_pip_dataset.py
@@ -18,9 +18,7 @@ imagePaths = [
 width = 800
 half_width = int(width / 2)  # either use 1,2,4 images.
 textTotalHeight = 300  # either add to top or bottom.
-getMarginRatio = lambda: random.randrange(
-    0, 0.1
-)  # this margin is used randomly. we can make it 0 or as is.
+getMarginRatio = lambda: random.random()*0.15  # this margin is used randomly. we can make it 0 or as is.
 
 textOrigin = (-30, 30)
 fontScale = 1
