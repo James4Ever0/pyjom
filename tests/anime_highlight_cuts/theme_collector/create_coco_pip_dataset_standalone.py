b867b0a7e27803cee730e424915d31d0f2c24dfa
jamesbrown <randomvoidmail@foxmail.com>
2023-01-29 18:34:51 +0800

    [2023-01-29 10:34:45]
    FILE:
    tests/anime_highlight_cuts/theme_collector/create_coco_pip_dataset_standalone.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/create_coco_pip_dataset_standalone.py
+++ tests/anime_highlight_cuts/theme_collector/create_coco_pip_dataset_standalone.py
@@ -12,9 +12,7 @@ import itertools
 from PIL import Image, ImageDraw
 
 imageBasePath = "/Users/jamesbrown/Desktop/"
-imagePaths = [
-    "Screen Shot 2023-01-17 at 15.35.29.png"
-] * 4  # let's all be the same, for testing.
+imagePaths = os.listdir(imageBasePath)
 width = 800
 half_width = int(width / 2)  # either use 1,2,4 images.
 textTotalHeight = 300  # either add to top or bottom.
