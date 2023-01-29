805829386263c31bfa97b3856fc47840676e34d5
jamesbrown <randomvoidmail@foxmail.com>
2023-01-29 15:40:21 +0800

    [2023-01-29 07:40:15]
    FILE:
    tests/anime_highlight_cuts/theme_collector/create_rounded_rectangle.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/create_rounded_rectangle.py
+++ tests/anime_highlight_cuts/theme_collector/create_rounded_rectangle.py
@@ -1,7 +1,7 @@
 
 from PIL import Image, ImageDraw
 def rectangle(output_path):
-    image = Image.new("BGR", (400, 400), "green")
+    image = Image.new("BGR", (400, 400), "black")
     draw = ImageDraw.Draw(image)
     # Draw a regular rectangle
     draw.rectangle((200, 100, 300, 200), fill="red")
