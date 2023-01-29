4959d1c30d4d1cce5a6630d3bbc647f9050857f8
jamesbrown <randomvoidmail@foxmail.com>
2023-01-29 15:49:52 +0800

    [2023-01-29 07:49:46]
    FILE:
    tests/anime_highlight_cuts/theme_collector/create_rounded_rectangle.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/create_rounded_rectangle.py
+++ tests/anime_highlight_cuts/theme_collector/create_rounded_rectangle.py
@@ -1,23 +1,23 @@
-
 from PIL import Image, ImageDraw
 import cv2
 import numpy as np
 
+
 def rectangle():
-    image = Image.new("RGB", (800, # width?
-     400 # height?
-     ), "black")
+    image = Image.new("RGB", (800, 400), "black")  # width?  # height?
     draw = ImageDraw.Draw(image)
     # Draw a regular rectangle
     draw.rectangle((200, 100, 300, 200), fill="white")
     # Draw a rounded rectangle
     draw.rounded_rectangle((50, 50, 150, 150), fill="white", radius=20)
-    npArray = np.array(image)/255 # uint8? then float64? great.
+    npArray = np.array(image)  # /255
+    # uint8? then float64? great.
     print(npArray)
-    print(npArray.shape, npArray.dtype, npArray.max()) # 255?
+    print(npArray.shape, npArray.dtype, npArray.max())  # 255?
     cv2.imshow("mask", npArray)
     # maybe we just want "1" instead of "255"
     # divide by 255 then.
     cv2.waitKey(0)
+
 
 rectangle()
