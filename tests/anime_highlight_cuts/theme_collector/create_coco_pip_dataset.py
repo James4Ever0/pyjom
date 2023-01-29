365ec4074b45c5a248d3e73cb8585275d72298a9
jamesbrown <randomvoidmail@foxmail.com>
2023-01-29 16:52:50 +0800

    [2023-01-29 08:52:42]
    FILE:
    tests/anime_highlight_cuts/theme_collector/create_coco_pip_dataset.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/create_coco_pip_dataset.py
+++ tests/anime_highlight_cuts/theme_collector/create_coco_pip_dataset.py
@@ -220,7 +220,8 @@ for imageFormat, textFormat, backgroundFormat in itertools.product(
             draw.rectangle((x0, y0, x1, y1), fill="white")
         else:
             draw.rounded_rectangle((x0, y0, x1, y1), fill="white", radius=radius)
-        print()
+        print(imageCanvas.shape)
+        print(image.shape)
 
         imageCanvas[y0 : image.shape[0] + y0, x0 : image.shape[1] + x0, :] = image
     else:
