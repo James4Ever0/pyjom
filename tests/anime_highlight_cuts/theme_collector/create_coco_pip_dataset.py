e0f21454e7c5273d77f88bc88e0a0a45fd1d62e9
jamesbrown <randomvoidmail@foxmail.com>
2023-01-29 16:26:34 +0800

    [2023-01-29 08:26:27]
    FILE:
    tests/anime_highlight_cuts/theme_collector/create_coco_pip_dataset.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/create_coco_pip_dataset.py
+++ tests/anime_highlight_cuts/theme_collector/create_coco_pip_dataset.py
@@ -208,7 +208,7 @@ for imageFormat, textFormat, backgroundFormat in itertools.product(
             imageShape = (int(base * (imageWidth / imageHeight)), int(base))
         else:
             imageShape = (int(base), int(base * (imageHeight / imageWidth)))
-            
+
         image = image.reshape(imageShape)
 
         x0 = y0 = int(margin*imageCanvasShape[0])
@@ -217,6 +217,8 @@ for imageFormat, textFormat, backgroundFormat in itertools.product(
             draw.rectangle((x0,y0,x1,y1),fill='white')
         else:
             draw.rounded_rectangle((x0,y0,x1,y1),fill='white',radius=radius)
+        
+        imageCanvas[:,:,:] = image
 
     ## preview
     previewImageName = f"{imageFormat}_{textFormat}_{backgroundFormat}.png"
