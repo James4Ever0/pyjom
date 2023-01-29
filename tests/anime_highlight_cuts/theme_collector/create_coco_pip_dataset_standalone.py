5502a2d0ba52751d054bb75956d7b021878dc0b3
jamesbrown <randomvoidmail@foxmail.com>
2023-01-29 18:39:57 +0800

    [2023-01-29 10:39:50]
    FILE:
    tests/anime_highlight_cuts/theme_collector/create_coco_pip_dataset_standalone.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/create_coco_pip_dataset_standalone.py
+++ tests/anime_highlight_cuts/theme_collector/create_coco_pip_dataset_standalone.py
@@ -39,7 +39,7 @@ fontThickness = 2
 getRadius = lambda: random.randint(1, 30)
 
 
-imageIndex = sorted([int(fpath.split(".")[0]) os.listdir(train_path_relative)])  # shall be increased on demand.
+imageIndex = sorted([int(fpath.split(".")[0]) for fpath in os.listdir(train_path_relative)])  # shall be increased on demand.
 
 MAX_COCO_PIP_IMAGE_COUNT = 10000  # well, super huge. is it?
 
