a80645642c9bb828dfe2b53ec031d0be7a0834c6
jamesbrown <randomvoidmail@foxmail.com>
2023-01-29 17:02:00 +0800

    [2023-01-29 09:01:54]
    FILE:
    tests/anime_highlight_cuts/theme_collector/create_coco_pip_dataset.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/create_coco_pip_dataset.py
+++ tests/anime_highlight_cuts/theme_collector/create_coco_pip_dataset.py
@@ -225,7 +225,7 @@ for imageFormat, textFormat, backgroundFormat in itertools.product(
         print(imageCanvas.shape)
         print(image.shape)
         print(x0,x1,x1-x0)
-        print(y0,y1xy1-y0)
+        print(y0,y1,y1-y0)
         print("___")
 
         imageCanvas[y0 : image.shape[0] + y0, x0 : image.shape[1] + x0, :] = image
