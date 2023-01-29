150afdb54f4e222c456a638ff6c68190b3b51703
jamesbrown <randomvoidmail@foxmail.com>
2023-01-29 15:08:33 +0800

    [2023-01-29 07:08:27]
    FILE:
    tests/anime_highlight_cuts/theme_collector/create_coco_pip_dataset.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/create_coco_pip_dataset.py
+++ tests/anime_highlight_cuts/theme_collector/create_coco_pip_dataset.py
@@ -170,8 +170,8 @@ for imageFormat, textFormat, backgroundFormat in itertools.product(
         )[0]
         textColor = textColorNumpyArray.tolist()
         # let's paint it all over the place!
-        textContent = getRandomLinesOfCharacters(
-            2, 10
+        textContent = getRandomCharacters(
+            10
         )  # TODO: check if string is **just enough** to fill the background.
         backgroundImage = cv2.putText(
             backgroundImage,
