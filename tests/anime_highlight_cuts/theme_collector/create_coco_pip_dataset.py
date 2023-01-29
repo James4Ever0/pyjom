4b86e1a7344f8a4187ab3c4595c6f1fb3a998d8b
jamesbrown <randomvoidmail@foxmail.com>
2023-01-29 16:29:53 +0800

    [2023-01-29 08:29:47]
    FILE:
    tests/anime_highlight_cuts/theme_collector/create_coco_pip_dataset.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/anime_highlight_cuts/theme_collector/create_coco_pip_dataset.py
+++ tests/anime_highlight_cuts/theme_collector/create_coco_pip_dataset.py
@@ -220,7 +220,7 @@ for imageFormat, textFormat, backgroundFormat in itertools.product(
         
         imageCanvas[y0:image.shape[0]+y0,x0:image.shape[1]+x0,:] = image
     else:
-        basePoints = [(0,0),(0,1),(1,0),(1,1)] # width, height
+        basePoints = [for x,y in [(0,0),(1,0),(1,1),(0,1)]] # width, height
 
         for image in selectedImages:
 
